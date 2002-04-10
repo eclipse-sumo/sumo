ca7bf72df242c76f33801cf2548c2c78253443a3
Christian Roessel <roessel@zpr.uni-koeln.de>
2002-04-10 15:02:46 +0000

    Get the job done version.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/sumo/microsim/MSDetector.cpp
+++ sumo/sumo/microsim/MSDetector.cpp
@@ -24,8 +24,8 @@ namespace
 }
 
 // $Log$
-// Revision 1.1  2002/04/08 07:21:23  traffic
-// Initial revision
+// Revision 1.2  2002/04/10 15:02:46  croessel
+// Get the job done version.
 //
 // Revision 1.1  2002/03/14 18:48:54  croessel
 // Initial commit.
@@ -37,12 +37,232 @@ namespace
 #endif // HAVE_CONFIG_H
 
 #include "MSDetector.h"
+#include "MSLane.h"
+#include "MSEdge.h"
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
 
 //---------------------------------------------------------------------------//
 
 MSDetector::~MSDetector()
 {
 
+}
+
+//---------------------------------------------------------------------------//
+
+MSDetector::MSDetector( string         id,
+                        MSLane*        lane,
+                        float          position,
+                        MSNet::Time    sampleIntervall,
+                        OutputStyle    style,
+                        ofstream&      file ) :
+    myID             ( id ),
+    myLane           ( lane ),
+    myPos            ( position ),
+    mySampleIntervall( sampleIntervall ),
+    myStyle          ( style ),
+    myFile           ( file ),
+    myPassedVeh      ( 0 ),
+    myPassingSpeed   ( 0 ),
+    myPassingTime    ( 0 ),
+    myNSamples       ( 0 ),
+    myNPassedVeh     ( 0 ),
+    myLocalDensitySum( 0 ),
+    mySpeedSum       ( 0 ),
+    myOccupSum       ( 0 ),
+    myVehLengthSum   ( 0 ),
+    myNIntervalls    ( 0 )
+{
+    assert( myPos > myLane->length() );
+
+    // Write header.
+    switch ( myStyle ) {
+        case GNUPLOT: 
+            {
+                ostringstream header;
+                header << "# Induction-loop-detector ID = " << myID << endl;
+                header << "#   on Lane     " << myLane->id() << endl;
+                header << "#   at position " << myPos << endl;
+                header << "#   sampleIntervall = " 
+                       << mySampleIntervall << " seconds" << endl << endl;
+                header << "# n   endOfInterv avgDensity avgFlow avgSpeed "
+                       << "avgOccup avgLength" << endl;
+                header << "#         [s]      [veh/km]  [veh/h]   [m/s]  "
+                       << "   [s]      [m]" << endl;
+                myFile << header.str() << endl;
+                break;
+            }
+        case CSV:
+            // No header. CSV has no spec for comments etc.
+            break;
+        default:
+            assert( true );
+    }
+}
+
+//---------------------------------------------------------------------------//
+
+void
+MSDetector::sample( double simSec )
+{
+    // If sampleIntervall is over, write the data to file and reset the 
+    // detector.
+    ++myNSamples;
+    if ( static_cast< float >( myNSamples ) * MSNet::deltaT() >= 
+         mySampleIntervall ) {
+
+        writeData();
+
+        // Reinitialize the detector completely.
+        myPassedVeh       = 0;
+        myPassingSpeed    = 0;
+        myPassingTime     = 0;
+        myNSamples        = 0;
+        myNPassedVeh      = 0;
+        myLocalDensitySum = 0;
+        mySpeedSum        = 0;
+        myOccupSum        = 0;
+        myVehLengthSum    = 0;
+        
+        ++myNIntervalls;
+    }
+
+    if ( myLane->empty() ) {
+
+        return;
+    }
+
+    // Search the vehicle that is closest to the detector with vehPos >
+    // detectorPos (the vehicle already passed the detector).
+    MSLane::VehCont::iterator currVeh =
+        find_if( myLane->myVehicles.begin(),
+                 myLane->myVehicles.end(),
+                 bind2nd( VehPosition(), myPos ) );
+
+    if ( currVeh == myLane->myVehicles.end() ||
+         *currVeh == myPassedVeh ) {
+
+        return;
+    }
+
+    // We have now a valid and not sampled vehicle. Update all data.
+    myLocalDensitySum += localDensity( **currVeh, simSec );
+    mySpeedSum        += ( *currVeh )->speed();
+    if ( ( *currVeh )->speed() > 0 ) {
+        myOccupSum += ( *currVeh )->length() / ( *currVeh )->speed();
+    }
+    myVehLengthSum += ( *currVeh )->length();
+    
+    // Remember the vehicle.
+    myPassedVeh = *currVeh;
+    ++myNPassedVeh;
+}
+
+//---------------------------------------------------------------------------//
+
+double 
+MSDetector::localDensity( const MSVehicle& veh, double simSec )
+{
+    // Local Density is calculated via the timeheadway and the speed
+    // of the leading vehicle. After the first detection there will
+    // always be a myPassedVeh.
+    double localDens = 0.0;
+    double currPassTime = 0.0;
+
+    if ( myPassedVeh != 0 ) {
+
+        currPassTime = simSec + ( veh.pos() - myPos ) / veh.speed();
+        double timeHeadWay = currPassTime - myPassingTime;
+        localDens = 1 / ( myPassingSpeed * timeHeadWay );
+        assert( localDens >= 0 );
+    }
+
+    // update members
+    myPassingSpeed = veh.speed();
+    if ( myPassingTime == 0.0 ) {
+        myPassingTime = simSec + ( veh.pos() - myPos ) / veh.speed();
+    }
+
+    return localDens;
+}
+
+//---------------------------------------------------------------------------//
+
+
+void   
+MSDetector::writeData()
+{
+
+    double avgDensity = myLocalDensitySum / ( myNPassedVeh - 1 ) 
+        * 1000; // [veh/km], first detectect vehicle doesn't contribute.
+    double avgFlow    = myNPassedVeh / mySampleIntervall * 60; // [veh/h]
+    double avgSpeed   = mySpeedSum / myNPassedVeh;  // [m/s]
+    double avgLength  = myVehLengthSum / myNPassedVeh; // [m]
+//    double avgOccup = myOccupancySum / myNPassedVeh;
+
+    switch ( myStyle ) {
+        case GNUPLOT: 
+            writeGnuPlot( myNIntervalls * mySampleIntervall,
+                          avgDensity,
+                          avgFlow,
+                          avgSpeed,
+                          myOccupSum,
+                          avgLength );
+            break;
+        case CSV:
+            writeCSV( myNIntervalls * mySampleIntervall,
+                      avgDensity,
+                      avgFlow,
+                      avgSpeed,
+                      myOccupSum,
+                      avgLength );
+            break;
+        default:
+            assert( true );
+    }
+}
+
+//---------------------------------------------------------------------------//
+
+void
+MSDetector::writeGnuPlot( MSNet::Time endOfInterv,
+                          double avgDensity,
+                          double avgFlow,
+                          double avgSpeed,
+                          double occup,
+                          double avgLength )
+{
+    myFile << setw( 5 ) << setprecision( 0 ) << myNIntervalls << " "
+           << setw(11 ) << setprecision( 0 ) << endOfInterv << " "
+           << setw(10 ) << setprecision( 2 ) << avgDensity << " "
+           << setw( 7 ) << setprecision( 1 ) << avgFlow << " "
+           << setw( 8 ) << setprecision( 3 ) << avgSpeed << " "
+           << setw( 8 ) << setprecision( 2 ) << occup << " "
+           << setw( 9 ) << setprecision( 3 ) << avgLength << endl;
+}
+
+//---------------------------------------------------------------------------//
+
+void
+MSDetector::writeCSV( MSNet::Time endOfInterv,
+                      double avgDensity,
+                      double avgFlow,
+                      double avgSpeed,
+                      double occup,
+                      double avgLength )
+{
+    myFile << setw( 4 ) << setprecision( 0 ) << myNIntervalls << ";"
+           << setw( 6 ) << setprecision( 0 ) << endOfInterv << ";"
+           << setw( 6 ) << setprecision( 2 ) << avgDensity << ";"
+           << setw( 4 ) << setprecision( 1 ) << avgFlow << ";"
+           << setw( 6 ) << setprecision( 3 ) << avgSpeed << ";"
+           << setw( 7 ) << setprecision( 2 ) << occup << ";"
+           << setw( 6 ) << setprecision( 3 ) << avgLength << endl;
 }
 
 //---------------------------------------------------------------------------//
