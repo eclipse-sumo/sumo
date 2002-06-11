2e7bb6b907cb2e8dc011d52a7189cd80f4c75284
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2002-06-11 06:19:20 +0000

    Route building moved to sumo
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@113 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/sumo/data/routedefs/mkroutes.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# Generates a simple route file for an example
+if($ARGV[1]) {
+    # read in the definition file
+    open(INDAT, "< $ARGV[0]") || die "Could not open '".$ARGV[0]."'";
+    $no = 0;
+    while($a=<INDAT>) {
+	if($a !~ '\#' && $a =~ '\:') {
+	    $routes[$no] = $a;
+	    $no++;
+	}
+    }
+    close(INDAT);
+    # build the output
+    open(OUTDAT, "> $ARGV[1]") || die "Could not open '".$ARGV[1]."'";
+    print OUTDAT "<routes>\n\n";
+    print OUTDAT "   <vtype id=\"0\" maxspeed=\"20\" length=\"5.0\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\"/>\n\n";
+
+    for($i=0; $i<$no; $i++) {
+	($name, $route, $norep, $vehoff, $noveh) = split(':', $routes[$i]);
+	# print the route
+	print OUTDAT "   <route id=\"".$name."\">";
+	for($j=0; $j<$norep; $j++) {
+	    if($j>0) {
+		print OUTDAT " ";
+	    }
+	    print OUTDAT $route;
+	}
+	print OUTDAT "</route>\n";
+	# print the leaving vehicles
+	$time = 0;
+	for($j=0; $j<$noveh; $j++) {
+	    print OUTDAT "   <vehicle type=\"0\" id=\"".$name.$j."\" depart=\"".$time."\" route=\"".$name."\"/>\n";
+	    $time = $time + $vehoff;
+	}
+	print OUTDAT "\n";
+    }
+    print OUTDAT "</routes>\n";
+    close(OUTDAT);
+} else {
+  print "Syntax Error!!!\n";
+  print "Syntax: mkroutes.pl <DEF_FILE> <OUTPUT_FILE>\n"
+}
+
