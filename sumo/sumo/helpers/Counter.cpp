60c6cef527c7c3b2e80818e809b2e9f54f599a9b
Christian Roessel <roessel@zpr.uni-koeln.de>
2002-06-18 16:34:27 +0000

    Initial commit. Made files cfront compliant.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@141 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/sumo/helpers/Counter.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+                          Counter.cpp  -  Counter-class.
+                             -------------------
+    begin                : Tue, 18 Jun 2002
+    copyright            : (C) 2001 by Christian Roessel
+    email                : roessel@zpr.uni-koeln.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+
+#include "Counter.h"
+
+
+template< typename T > 
+size_t Counter< T >::count = 0;
+
+
+//--------------- DO NOT DEFINE ANYTHING AFTER THIS POINT -------------------//
+#ifdef DISABLE_INLINE
+#include "Counter.icc"
+#endif
+
+
+// $Log$
+// Revision 1.1  2002/06/18 16:34:27  croessel
+// Initial commit. Made files cfront compliant.
+//
+
+
+// Local Variables:
+// mode:C++
+// End:
+
+
+
