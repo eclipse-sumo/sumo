692eb8a0855c646bd9a8c5bca29627e2b8b6e2b5
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2002-10-22 10:03:15 +0000

    help-strings repaired
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@293 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router_help.h
+++ sumo/src/router_help.h
@@ -13,10 +13,10 @@ char *help[] = {
     "  Configuration",
     "   -c, --configuration FILE       FILE will be used as configuration",
     "                                    1. Default: ./sumo-netcovert.cfg",
-    "  Network description:
+    "  Network description:",
     "   -n, --net-files FILE           FILE containing the SUMO-net",
     "   -w, --weights FILE             FILE containing the weights of the net",
-    "  Route descriptions:
+    "  Route descriptions:",
     "   -s, --sumo-input FILE          FILE containing SUMO-routes",
     "   --cell-input FILE              FILE containing cell routes",
     "   -t, --trip-defs FILE           FILE containing trip definitions",
@@ -27,8 +27,8 @@ char *help[] = {
     "   -t, --type-file FILE           FILE contains type definitions ",
     " ",
     " Output files:",
-    "   -o, --output-file FILE         The generated routes will be written to FILE",
-    "                                   Additionally, a file with route 
+    "   -o, --output-file FILE         Generated routes will be written to FILE",
+    "                                   Additionally, a file with route",
     "                                   alternatives will be generated as",
     "                                   'FILE.alt'",
     " ",
@@ -39,7 +39,7 @@ char *help[] = {
     "   --gA                           Gawron-A for DUE",
     "   --unsorted                     The routes are assumed to be unsorted",
     "   --save-cell-rindex             The index to cell-routes will be saved",
-    "   --intel-cell                   Set, when reading cell-routes computed
+    "   --intel-cell                   Set, when reading cell-routes computed",
     "                                   on an Intel-machine",
     "   --no-last-cell                 Use best, not the last cell-route",
     " ",
