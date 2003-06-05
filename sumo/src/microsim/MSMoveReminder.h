515d49d0b3cbb5c473123166e96f0822e8d6d59d
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-06-05 09:58:00 +0000

    Added method getLane() and member laneM.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@679 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSMoveReminder.h
+++ sumo/src/microsim/MSMoveReminder.h
@@ -22,19 +22,24 @@
 //---------------------------------------------------------------------------//
 
 // $Log$
+// Revision 1.3  2003/06/05 09:58:00  roessel
+// Added method getLane() and member laneM.
+//
 // Revision 1.2  2003/05/22 12:44:47  roessel
-// Changed void activateByEmit... to bool isActivatedByEmit. Not activated reminders will be erased from the vehicles reminder-list.
+// Changed void activateByEmit... to bool isActivatedByEmit. Not activated
+// reminders will be erased from the vehicles reminder-list.
 //
 // Revision 1.1  2003/05/21 16:21:45  dkrajzew
 // further work detectors
 //
 
 class MSVehicle;
+class MSLane;
 
 class MSMoveReminder
 {
 public:
-    MSMoveReminder( void ) {}
+    MSMoveReminder( MSLane* lane ) : laneM( lane ){}
     virtual ~MSMoveReminder( void ) {}
 
     virtual bool isStillActive( MSVehicle& veh,
@@ -43,7 +48,13 @@ public:
                                 double newSpeed ) = 0;
     virtual void dismissByLaneChange( MSVehicle& veh ) = 0;
     virtual bool isActivatedByEmitOrLaneChange( MSVehicle& veh ) = 0;
-
+    MSLane* getLane( void )
+        {
+            return laneM;
+        }
+    
+protected:
+    MSLane* laneM;
 };
 
 
