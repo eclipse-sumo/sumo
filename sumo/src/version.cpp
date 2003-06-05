ff72c2d4515a7bca5d929304b7a1709895dc03cf
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-06-05 14:45:28 +0000

    class templates applied; documentation added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@702 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/version.cpp
+++ sumo/src/version.cpp
@@ -1,2 +1,57 @@
+//---------------------------------------------------------------------------//
+//                        version.cpp -  
+//  A string containing the current program version
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Sept 2002
+//  copyright            : (C) 2002 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//          
+//   This program is free software; you can redistribute it and/or modify  
+//   it under the terms of the GNU General Public License as published by  
+//   the Free Software Foundation; either version 2 of the License, or     
+//   (at your option) any later version.                                   
+//
+//---------------------------------------------------------------------------//
+/*
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+*/
+// $Log$
+// Revision 1.2  2003/06/05 14:45:28  dkrajzew
+// class templates applied; documentation added
+//
+//
+
+
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+
+/* =========================================================================
+ * definitions
+ * ======================================================================= */
 const char *version = "0.8";
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifdef DISABLE_INLINE
+//#include "version.icc"
+//#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
 
