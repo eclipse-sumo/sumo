af66439ca0c5e7fa1c5251df9ce9f560f28776af
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-06-19 15:22:38 +0000

    inifinite loop on lane searching patched
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@786 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router/ROEdge.cpp
+++ sumo/src/router/ROEdge.cpp
@@ -23,6 +23,9 @@ namespace
     "$Id$";
 }
 // $Log$
+// Revision 1.7  2003/06/19 15:22:38  dkrajzew
+// inifinite loop on lane searching patched
+//
 // Revision 1.6  2003/06/18 11:20:54  dkrajzew
 // new message and error processing: output to user may be a message, warning or an error now; it is reported to a Singleton (MsgHandler); this handler puts it further to output instances. changes: no verbose-parameter needed; messages are exported to singleton
 //
@@ -115,6 +118,7 @@ void ROEdge::setLane(long timeBegin, long timeEnd,
             _usingTimeLine = true;
             return;
         }
+        i++;
     }
     MsgHandler::getErrorInstance()->inform(
         string("Un unknown lane '") + id
