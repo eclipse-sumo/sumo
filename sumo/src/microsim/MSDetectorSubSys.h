60e89a670f136b23ba562fed5097f8056b5bcb18
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-07-30 17:48:26 +0000

    Added file header.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@988 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSDetectorSubSys.h
+++ sumo/src/microsim/MSDetectorSubSys.h
@@ -1,8 +1,30 @@
 #ifndef MSDetectorSubSys_h
 #define MSDetectorSubSys_h
 
+/**
+ * @file   MSDetectorSubSys.h
+ * @author Daniel Krajzewicz
+ * @date   Tue Jul 29 10:41:01 2003
+ * @version $Id$
+ * @brief  Declaration of class MSDetectorSubSys
+ * 
+ */
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
 #include <string>
 #include <helpers/SingletonDictionary.h>
+
+
 
 class MSLaneState;
 class MSInductLoop;
