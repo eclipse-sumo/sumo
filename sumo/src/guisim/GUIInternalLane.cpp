addef640d61ae15b1297bb5a6513484aef0f70a5
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-09-05 15:02:47 +0000

    first steps for reading of internal lanes
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1083 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/guisim/GUIInternalLane.cpp
@@ -0,0 +1,238 @@
+//---------------------------------------------------------------------------//
+//                        GUIInternalLane.cpp -
+//  Lane within junctions, derived from the normal lane
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Thu, 04.09.2003
+//  copyright            : (C) 2003 by DLR http://ivf.dlr.de/
+//  author               : Daniel Krajzewicz
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+// $Log$
+// Revision 1.1  2003/09/05 15:02:47  dkrajzew
+// first steps for reading of internal lanes
+//
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include <string>
+#include <iostream> // !!!
+#include <utility>
+#include <utils/qutils/NewQMutex.h>
+#include <microsim/MSLane.h>
+#include <utils/geom/Position2D.h>
+#include <microsim/MSNet.h>
+#include "GUILaneWrapper.h"
+#include "GUIInternalLane.h"
+
+
+/* =========================================================================
+ * used namespaces
+ * ======================================================================= */
+using namespace std;
+
+
+/* =========================================================================
+ * some definitions (debugging only)
+ * ======================================================================= */
+#define DEBUG_OUT cout
+
+
+/* =========================================================================
+ * method definitions
+ * ======================================================================= */
+GUIInternalLane::GUIInternalLane(MSNet &net, std::string id,
+                             double maxSpeed, double length,
+                             MSEdge* edge, const Position2DVector &shape )
+    : MSInternalLane(net, id, maxSpeed, length, edge), myShape(shape)
+{
+}
+
+
+GUIInternalLane::~GUIInternalLane()
+{
+}
+
+/*
+void
+GUIInternalLane::moveNonCriticalSingle()
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveNonCriticalSingle();
+    _lock.unlock();//Display();
+}
+
+
+void
+GUIInternalLane::moveCriticalSingle()
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveCriticalSingle();
+    _lock.unlock();//Display();
+}
+
+
+void
+GUIInternalLane::moveNonCriticalMulti()
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveNonCriticalMulti();
+    _lock.unlock();//Display();
+}
+
+
+void
+GUIInternalLane::moveCriticalMulti()
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveCriticalMulti();
+    _lock.unlock();//Display();
+}
+
+*/
+void
+GUIInternalLane::moveNonCritical(/*const MSEdge::LaneCont::const_iterator &firstNeighLane,
+                               const MSEdge::LaneCont::const_iterator &lastNeighLane */)
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveNonCritical(/*firstNeighLane, lastNeighLane*/);
+    _lock.unlock();//Display();
+}
+
+
+void
+GUIInternalLane::moveCritical(/*const MSEdge::LaneCont::const_iterator &firstNeighLane,
+                            const MSEdge::LaneCont::const_iterator &lastNeighLane */)
+{
+    _lock.lock();//Display();
+    MSInternalLane::moveCritical(/*firstNeighLane, lastNeighLane*/);
+    _lock.unlock();//Display();
+}
+
+
+void
+GUIInternalLane::setCritical()
+{
+    _lock.lock();//Display();
+    MSInternalLane::setCritical();
+    _lock.unlock();//Display();
+}
+
+
+
+
+
+bool
+GUIInternalLane::emit( MSVehicle& newVeh )
+{
+    _lock.lock();//Display();
+    bool ret = MSInternalLane::emit(newVeh);
+    _lock.unlock();//Display();
+    return ret;
+}
+
+
+bool
+GUIInternalLane::isEmissionSuccess( MSVehicle* aVehicle )
+{
+    _lock.lock();//Display();
+    bool ret = MSInternalLane::isEmissionSuccess(aVehicle);
+    _lock.unlock();//Display();
+    return ret;
+}
+
+
+bool
+GUIInternalLane::push( MSVehicle* veh )
+{
+    _lock.lock();//Display();
+#ifdef ABS_DEBUG
+    if(myVehBuffer!=0) {
+	    DEBUG_OUT << "Push Failed on Lane:" << myID << endl;
+	    DEBUG_OUT << myVehBuffer->id() << ", " << myVehBuffer->pos() << ", " << myVehBuffer->speed() << endl;
+	    DEBUG_OUT << veh->id() << ", " << veh->pos() << ", " << veh->speed() << endl;
+    }
+#endif
+
+    // Insert vehicle only if it's destination isn't reached.
+    assert( myVehBuffer == 0 );
+    myVehBuffer = veh;
+    veh->enterLaneAtMove( this );
+    veh->_assertPos();
+    _lock.unlock();//Display();
+    return false;
+}
+
+
+void
+GUIInternalLane::releaseVehicles()
+{
+    _lock.unlock();
+}
+
+
+
+const MSInternalLane::VehCont &
+GUIInternalLane::getVehiclesSecure()
+{
+    _lock.lock();
+    return myVehicles;
+}
+
+
+void
+GUIInternalLane::swapAfterLaneChange()
+{
+    _lock.lock();
+    MSLane::swapAfterLaneChange();
+    _lock.unlock();
+}
+
+
+void
+GUIInternalLane::integrateNewVehicle()
+{
+    _lock.lock();
+    MSLane::integrateNewVehicle();
+    _lock.unlock();
+}
+
+
+GUILaneWrapper *
+GUIInternalLane::buildLaneWrapper(GUIGlObjectStorage &idStorage,
+                                bool allowAggregation)
+{
+    return new GUILaneWrapper(idStorage, *this, myShape, allowAggregation);
+}
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifdef DISABLE_INLINE
+//#include "GUIInternalLane.icc"
+//#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
+
---
+++ sumo/src/guisim/GUIInternalLane.h
@@ -0,0 +1,154 @@
+#ifndef GUIInternalLane_h
+#define GUIInternalLane_h
+//---------------------------------------------------------------------------//
+//                        GUIInternalLane.h -
+//  A MSLane extended by some values needed by the gui
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Thu, 04.09.2003
+//  copyright            : (C) 2003 by DLR http://ivf.dlr.de/
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+// $Log$
+// Revision 1.1  2003/09/05 15:02:47  dkrajzew
+// first steps for reading of internal lanes
+//
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include <string>
+#include <utility>
+#include <microsim/MSInternalLane.h>
+#include <microsim/MSEdge.h>
+#include <utils/geom/Position2D.h>
+#include <utils/geom/Position2DVector.h>
+#include <utils/qutils/NewQMutex.h>
+
+
+/* =========================================================================
+ * class declarations
+ * ======================================================================= */
+class MSVehicle;
+class MSNet;
+
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
+/**
+ * An extended MSLane. A mechanism to avoid concurrent
+ * visualisation and simulation what may cause problems when vehicles
+ * disappear is implemented using a mutex
+ */
+class GUIInternalLane :
+    public MSInternalLane {
+public:
+    /// constructor
+    GUIInternalLane( MSNet &net, std::string id, double maxSpeed,
+        double length, MSEdge* egde, const Position2DVector &shape );
+
+    /// destructor
+    ~GUIInternalLane();
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+//    void moveNonCriticalSingle();
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+//    void moveCriticalSingle();
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+//    void moveNonCriticalMulti();
+
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+//    void moveCriticalMulti();
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    void moveNonCritical(/*
+        const MSEdge::LaneCont::const_iterator &firstNeighLane,
+        const MSEdge::LaneCont::const_iterator &lastNeighLane */);
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    void moveCritical(/*
+        const MSEdge::LaneCont::const_iterator &firstNeighLane,
+        const MSEdge::LaneCont::const_iterator &lastNeighLane */);
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    void setCritical();
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    bool emit( MSVehicle& newVeh );
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    bool isEmissionSuccess( MSVehicle* aVehicle );
+
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    void integrateNewVehicle();
+
+    /// allows the processing of vehicles for threads
+    void releaseVehicles();
+
+    /// returns the vehicles closing their processing for other threads
+    const VehCont &getVehiclesSecure();
+
+    GUILaneWrapper *buildLaneWrapper(GUIGlObjectStorage &idStorage,
+        bool allowAggregation);
+
+    friend class GUILaneChanger;
+
+    friend class GUILaneWrapper;
+
+protected:
+    /** the same as in MSLane, but locks the access for the visualisation
+        first; the access will be granted at the end of this method */
+    bool push( MSVehicle* veh );
+
+    /// moves myTmpVehicles int myVehicles after a lane change procedure
+    void swapAfterLaneChange();
+
+private:
+    /// The mutex used to avoid concurrent updates of the vehicle buffer
+    NewQMutex _lock;
+
+    /// The shape of the lane
+    Position2DVector myShape;
+
+};
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifndef DISABLE_INLINE
+//#include "GUIInternalLane.icc"
+//#endif
+
+#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
