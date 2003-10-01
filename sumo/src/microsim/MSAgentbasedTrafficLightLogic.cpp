f19969120e4d83550a45e65dffc4437174d6a203
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-10-01 11:24:35 +0000

    agent-based traffic lights added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1212 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSAgentbasedTrafficLightLogic.cpp
@@ -0,0 +1,310 @@
+//---------------------------------------------------------------------------//
+//                        MSAgentbasedTrafficLightLogic.cpp -
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Wed, 01. Oct 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+// $Log$
+// Revision 1.1  2003/10/01 11:24:35  dkrajzew
+// agent-based traffic lights added
+//
+//
+
+
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+#include <utility>
+#include <vector>
+#include <bitset>
+#include "MSEventControl.h"
+//#include "MSDetector.h"
+#include "MSInductLoop.h"
+#include "MSLaneState.h"
+#include "MSNet.h"
+#include "MSTrafficLightLogic.h"
+#include "MSAgentbasedTrafficLightLogic.h"
+
+
+template< class _TInductLoop, class _TLaneState >
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>(
+        const std::string &id, const Phases &phases, size_t step,
+        const std::vector<MSLane*> &lanes, size_t delay)
+    : MSSimpleTrafficLightLogic(id, phases, step, delay),
+    _continue(false)
+{
+    sproutDetectors(lanes);
+}
+
+
+template< class _TInductLoop, class _TLaneState >
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::~MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>()
+{
+}
+
+
+template< class _TInductLoop, class _TLaneState > MSNet::Time
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::duration() const
+{
+    if(_continue) {
+        return 1;
+    }
+    assert(_phases.size()>_step);
+
+    // define the duration depending from the number of waiting vehicles of the actual phase
+
+    int duration = static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->minDuration;
+    for(typename LaneStateMap::const_iterator i=myLaneStates.begin(); i!=myLaneStates.end(); i++)   {
+        MSLane *lane = (*i).first;
+        const MSLinkCont &cont = lane->getLinkCont();
+        for(MSLinkCont::const_iterator j=cont.begin(); j!=cont.end(); j++)  {
+            if((*j)->myPrio)    {
+                double waiting = (*i).second->getCurrentNumberOfWaiting();
+                double passingTime = 1.9;
+                double tmpdur =  passingTime * waiting;
+                if (tmpdur > duration) {
+                    // here we cut the decimal places, because we have to return an integer
+                    duration = (int) tmpdur;
+                }
+                if (duration > (int) static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->maxDuration)  {
+                    return static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->maxDuration;
+                }
+            }
+        }
+    }
+
+    return duration;
+}
+
+
+
+
+template< class _TInductLoop, class _TLaneState > MSNet::Time
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::nextPhase()
+{
+    // checks if the actual phase should be continued
+    gapControl();
+    if(_continue) {
+        return duration();
+    }
+    // increment the index to the current phase
+    nextStep();
+    // reset the link priorities
+    setLinkPriorities();
+    // set the next event
+    return duration();
+}
+
+
+
+template< class _TInductLoop, class _TLaneState >
+void
+MSAgentbasedTrafficLightLogic<_TInductLoop,
+                            _TLaneState>::sproutDetectors(
+                                const std::vector<MSLane*> &lanes)
+{
+    // change values for setting the loops and lanestate-detectors, here
+    double inductLoopPosition = 10; // 10m from the end
+    MSNet::Time inductLoopInterval = 1; //
+    double laneStateDetectorLength = 100; // length of the detecor
+    // as the laneStateDetector shall end at the end of the lane, the position
+    // is calculated, not given
+    MSNet::Time laneStateDetectorInterval = 1; //
+
+    std::vector<MSLane*>::const_iterator i;
+    // build the induct loops
+    for(i=lanes.begin(); i!=lanes.end(); i++) {
+        MSLane *lane = (*i);
+        double length = lane->length();
+        double speed = lane->maxSpeed();
+        double gap = 2.5;
+        inductLoopPosition = gap * speed;
+        // check whether the lane is long enough
+        double ilpos = length - inductLoopPosition;
+        if(ilpos<0) {
+            ilpos = length;
+        }
+        // Build the induct loop and set it into the container
+        std::string id = "TL" + _id + "_InductLoopOn_" + lane->id();
+        _TInductLoop *loop = new _TInductLoop(id, lane, ilpos );
+        myInductLoops[lane] = loop;
+    }
+    // build the lane state-detectors
+    for(i=lanes.begin(); i!=lanes.end(); i++) {
+        MSLane *lane = (*i);
+        double length = lane->length();
+        // check whether the position is o.k. (not longer than the lane)
+        double lslen = laneStateDetectorLength;
+        if(lslen>length) {
+            lslen = length;
+        }
+        double lspos = length - lslen;
+        // Build the lane state detetcor and set it into the container
+        std::string id = "TL" + _id + "_LaneStateOf_" + lane->id();
+        MSLaneState* loop =
+            new MSLaneState( id, lane, lspos, lslen );
+        myLaneStates[lane] = loop;
+    }
+    // build the E2-detectors
+    for(i=lanes.begin(); i!=lanes.end(); i++) {
+        MSLane *lane = (*i);
+        double length = lane->length() - 0.1; // !!!
+        // check whether the position is o.k. (not longer than the lane)
+        double lslen = laneStateDetectorLength;
+        if(lslen>length) {
+            lslen = length;
+        }
+        double lspos = length - lslen;
+        // Build the lane state detetcor and set it into the container
+        std::string id = "TL" + _id + "_E2DetectorOn_" + lane->id();
+        MS_E2_ZS_Collector* det =
+            new MS_E2_ZS_Collector( id, lane, lspos, lslen );
+		det->addDetector( MS_E2_ZS_Collector::ALL, "detectors" );
+        myE2Detectors[lane] = det;
+    }
+}
+
+
+
+/*
+template< class _TInductLoop, class _TLaneState >
+const std::bitset<64> &
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::linkPriorities() const
+{
+    assert(_phases.size()>_step);
+    return _phases[_step].breakMask;
+}
+
+
+template< class _TInductLoop, class _TLaneState >
+const std::bitset<64> &
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::yellowMask() const
+{
+    assert(_phases.size()>_step);
+    return _phases[_step].yellowMask;
+}
+
+
+template< class _TInductLoop, class _TLaneState >
+const std::bitset<64> &
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::allowed() const
+{
+    assert(_phases.size()>_step);
+    return _phases[_step].driveMask;
+}
+*/
+
+
+
+template< class _TInductLoop, class _TLaneState >
+size_t
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::nextStep()
+{
+    // increment the index to the current phase
+    MSNet::Time phaseStart = static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->_lastSwitch;
+    MSNet::Time lastDuration = MSNet::globaltime - phaseStart;
+    static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->duration = lastDuration;
+    return _step;
+}
+
+template< class _TInductLoop, class _TLaneState >
+bool
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::gapControl()
+{
+    // Checks, if the maxDuration is kept. No phase should be longer send than maxDuration.
+    MSNet::Time actDuration = MSNet::globaltime - static_cast<MSActuatedPhaseDefinition*>(_phases[_step])->_lastSwitch;
+    if (actDuration >= currentPhaseDef()->maxDuration) {
+        return _continue = false;
+    }
+
+    // now the gapcontrol starts
+    for(typename InductLoopMap::const_iterator i=myInductLoops.begin(); i!=myInductLoops.end(); i++)   {
+        MSLane *lane = (*i).first;
+        const MSLinkCont &cont = lane->getLinkCont();
+        for(MSLinkCont::const_iterator j=cont.begin(); j!=cont.end(); j++)  {
+            if((*j)->myPrio)    {
+                double actualGap =
+                    (*i).second->getTimestepsSinceLastDetection();
+                double maxGap = 3.1;
+                if (actualGap < maxGap) {
+                return _continue = true;
+                }
+            }
+        }
+    }
+    return _continue = false;
+}
+
+
+// template< class _TInductLoop, class _TLaneState >
+// MSNet::DetectorCont
+// MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::getDetectorList() const
+// {
+//     MSNet::DetectorCont ret;
+//     for(typename InductLoopMap::const_iterator i=myInductLoops.begin(); i!=myInductLoops.end(); i++) {
+//         ret.push_back((*i).second);
+//     }
+// //     for(typename LaneStateMap::const_iterator j=myLaneStates.begin(); j!=myLaneStates.end(); j++) {
+// //         ret.push_back((*j).second);
+// //     }
+//     return ret;
+// }
+
+
+
+template< class _TInductLoop, class _TLaneState >
+MSActuatedPhaseDefinition *
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::currentPhaseDef()
+{
+    return static_cast<MSActuatedPhaseDefinition*>(_phases[_step]);
+}
+
+
+template< class _TInductLoop, class _TLaneState >
+double
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::currentForLane(
+		MS_E2_ZS_Collector::DetType what, MSLane *lane) const
+{
+	E2DetectorMap::const_iterator i=myE2Detectors.find(lane);
+	return (*i).second->getGurrent(what);
+}
+
+
+template< class _TInductLoop, class _TLaneState >
+double
+MSAgentbasedTrafficLightLogic<_TInductLoop, _TLaneState>::currentForLane(
+		MS_E2_ZS_Collector::DetType what, MSUnit::Seconds lanstNSeconds,
+		MSLane *lane) const
+{
+	E2DetectorMap::const_iterator i=myE2Detectors.find(lane);
+	return (*i).second->getAggregate(what, lanstNSeconds);
+}
+
+
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifdef DISABLE_INLINE
+//#include "MSAgentbasedTrafficLightLogic.icc"
+//#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
+
---
+++ sumo/src/microsim/MSAgentbasedTrafficLightLogic.h
@@ -0,0 +1,144 @@
+#ifndef MSAgentbasedTrafficLightLogic_h
+#define MSAgentbasedTrafficLightLogic_h
+//---------------------------------------------------------------------------//
+//                        MSAgentbasedTrafficLightLogic.h -
+//  The basic traffic light logic
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Wed, 01. Oct 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+// $Log$
+// Revision 1.1  2003/10/01 11:24:35  dkrajzew
+// agent-based traffic lights added
+//
+//
+
+
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include <utility>
+#include <vector>
+#include <bitset>
+#include <map>
+#include "MSEventControl.h"
+#include "MSNet.h"
+#include "MSTrafficLightLogic.h"
+#include "MSActuatedPhaseDefinition.h"
+#include "MSSimpleTrafficLightLogic.h"
+#include "MS_E2_ZS_Collector.h"
+
+class MSLane;
+class MSAgentbasedPhaseDefinition;
+
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
+/**
+ * @class MSAgentbasedTrafficLightLogic
+ * The implementation of a simple traffic light which only switches between
+ * it's phases and sets the lights to red in between.
+ * Some functions are called with an information about the current step. This
+ * is needed as a single logic may be used by many junctions and so the current
+ * step is stored within them, not within the logic.
+ */
+template< class _TInductLoop, class _TLaneState >
+class MSAgentbasedTrafficLightLogic :
+        public MSSimpleTrafficLightLogic
+{
+public:
+    /// Definition of a map from lanes to induct loops lying on them
+    typedef std::map<MSLane*, _TInductLoop*> InductLoopMap;
+
+    /// Definition of a map from lanes to lane state detectors lying on them
+    typedef std::map<MSLane*, MSLaneState*> LaneStateMap;
+
+    /// Definition of a map from lanes to lane state detectors lying on them
+    typedef std::map<MSLane*, MS_E2_ZS_Collector*> E2DetectorMap;
+
+public:
+    /// constructor
+    MSAgentbasedTrafficLightLogic(const std::string &id,
+        const MSSimpleTrafficLightLogic::Phases &phases,
+        size_t step, const std::vector<MSLane*> &lanes, size_t delay);
+
+    /// destructor
+    ~MSAgentbasedTrafficLightLogic();
+
+    /** @brief Switches to the next phase
+        Returns the time of the next switch */
+    virtual MSNet::Time nextPhase();
+
+    /// Returns the duration of the given step
+    virtual MSNet::Time duration() const;
+
+    /// Returns the index of the phase next to the given phase
+    /// and stores the duration of the phase, which was just sent
+    /// or stores the activation-time in _lastphase of the phase next
+    virtual size_t nextStep();
+
+    /// Desides, whether a phase should be continued by checking the gaps of vehicles having green
+    virtual bool gapControl();
+
+protected:
+    /// Builds the detectors
+    virtual void sproutDetectors(const std::vector<MSLane*> &lanes);
+
+    MSActuatedPhaseDefinition * currentPhaseDef();
+
+	double currentForLane(MS_E2_ZS_Collector::DetType what,
+		MSLane *lane) const;
+
+	double currentForLane(MS_E2_ZS_Collector::DetType what,
+		MSUnit::Seconds lanstNSeconds, MSLane *lane) const;
+
+protected:
+    /// A map from lanes to induct loops lying on them
+    InductLoopMap myInductLoops;
+
+    /// A map from lanes to lane states lying on them
+    LaneStateMap myLaneStates;
+
+	/// A map from lanes to E2-detectors lying on them
+	E2DetectorMap myE2Detectors;
+
+    /// information whether the current phase should be lenghtend
+    bool _continue;
+
+};
+
+#ifndef EXTERNAL_TEMPLATE_DEFINITION
+#ifndef MSVC
+#include "MSAgentbasedTrafficLightLogic.cpp"
+#endif
+#endif // EXTERNAL_TEMPLATE_DEFINITION
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifndef DISABLE_INLINE
+//#include "MSAgentbasedTrafficLightLogic.icc"
+//#endif
+
+#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
