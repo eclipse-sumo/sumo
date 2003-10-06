a185c9c19bec61c16ff02a4f502271111444eb1b
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-06 15:58:59 +0000

    Initial commit (code from MSDetectorContainer.h)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1236 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSDoubleDetectorContainer.h
@@ -0,0 +1,75 @@
+#ifndef MSDOUBLEDETECTORCONTAINER_H
+#define MSDOUBLEDETECTORCONTAINER_H
+
+/**
+ * @file   MSDoubleDetectorContainer.h
+ * @author Christian Roessel
+ * @date   Started Mon Oct  6 17:52:18 2003
+ * @version $Id$
+ * @brief  
+ * 
+ * 
+ */
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
+#include "MSDetectorContainerBase.h"
+
+class MSVehicle;
+
+struct MSDoubleDetectorContainer : public MSDetectorContainerBase
+{
+    typedef double Container;
+
+    void enterDetectorByMove( MSVehicle* veh )
+        {
+            ++containerM;
+        }
+
+    void enterDetectorByEmitOrLaneChange( MSVehicle* veh )
+        {
+            ++containerM;
+        }
+
+    void leaveDetectorByMove( void )
+        {
+            --containerM;
+        }
+
+    void leaveDetectorByLaneChange( MSVehicle* veh )
+        {
+            --containerM;
+        }
+
+    void update( void )
+        {}
+
+    MSDoubleDetectorContainer( void )
+        : containerM( 0 )
+        {}
+
+    virtual ~MSDoubleDetectorContainer( void )
+        {}
+
+    Container containerM;
+};
+
+namespace DetectorContainer
+{
+    typedef MSDoubleDetectorContainer Count;
+}
+
+#endif // MSDOUBLEDETECTORCONTAINER_H
+
+// Local Variables:
+// mode:C++
+// End:
