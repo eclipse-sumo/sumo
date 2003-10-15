fee3dd910d6d0a8345b4aa6d21e374ca2c301f3e
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-15 15:25:54 +0000

    Initial commit of new E2_ZS detector.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1275 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSSpaceMeanSpeed.h
@@ -0,0 +1,84 @@
+#ifndef MSSPACEMEANSPEED_H
+#define MSSPACEMEANSPEED_H
+
+/**
+ * @file   MSSpaceMeanSpeed.h
+ * @author Christian Roessel
+ * @date   Started Wed Oct 15 16:37:58 2003
+ * @version $Id$
+ * @brief  
+ * 
+ * 
+ */
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
+#include "MSDetectorContainer.h"
+//#include "MSOccupancyCorrection.h"
+#include "MSUnit.h"
+#include <numeric>
+
+class MSSpaceMeanSpeed //: virtual public MSOccupancyCorrection< double >
+{
+public:
+    typedef DetectorContainer::Vehicles::InnerCont::value_type Vehicle;
+    
+protected:
+    typedef double DetectorAggregate;
+    typedef DetectorContainer::Vehicles Container;
+    typedef Container::InnerCont VehicleCont;
+    
+    MSSpaceMeanSpeed( double lengthInMeters, const Container& vehicleCont ) 
+        : containerM( vehicleCont.containerM )
+        {}
+
+    virtual ~MSSpaceMeanSpeed( void )
+        {}
+    
+    DetectorAggregate getDetectorAggregate( void );
+    
+        static std::string getDetectorName( void )
+        {
+            return "spaceMeanSpeed";
+        }
+    
+private:
+    const VehicleCont& containerM;
+};
+
+namespace 
+{    
+    inline double speedSum( double sumSoFar,
+                            const MSSpaceMeanSpeed::Vehicle data )
+    {
+        return sumSoFar + data->speed();
+    }
+}
+
+MSSpaceMeanSpeed::DetectorAggregate 
+MSSpaceMeanSpeed::getDetectorAggregate( void )
+{
+    int size = containerM.size();
+    if ( size == 0 ) {
+        return 0;
+    }
+    double spaceMeanSpeed =
+        std::accumulate( containerM.begin(), containerM.end(),
+                         0.0, speedSum ) / containerM.size();
+    return MSUnit::getInstance()->getMetersPerSecond( spaceMeanSpeed );
+}   
+
+#endif // MSSPACEMEANSPEED_H
+
+// Local Variables:
+// mode:C++
+// End:
