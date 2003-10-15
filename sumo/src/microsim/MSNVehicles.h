c2a4bda8e7395e0302c219aa72c6c6218395ce45
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-15 12:37:46 +0000

    New detector. Initial commit.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1270 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSNVehicles.h
@@ -0,0 +1,61 @@
+#ifndef MSNVEHICLES_H
+#define MSNVEHICLES_H
+
+/**
+ * @file   MSNVehicles.h
+ * @author Christian Roessel
+ * @date   Started Wed Oct 15 13:29:34 2003
+ * @version $Id$
+ * @brief  
+ * 
+ * 
+ */
+
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
+#include <string>
+#include "MSOccupancyCorrection.h"
+#include "MSDoubleDetectorContainer.h"
+
+class MSNVehicles : virtual public MSOccupancyCorrection< double >
+{
+protected:
+    typedef double DetectorAggregate;
+    typedef DetectorContainer::Count Container;
+
+    MSNVehicles( double lengthInMeters, const Container& counter );
+
+    virtual ~MSNVehicles( void )
+        {}
+
+    DetectorAggregate getDetectorAggregate( void );
+    
+    static std::string getDetectorName( void )
+        {
+            return "nVehicles";
+        }
+
+private:
+    const Container& counterM;
+
+    MSNVehicles();
+    MSNVehicles( const MSNVehicles& );
+    MSNVehicles& operator=( const MSNVehicles& );    
+    
+};
+
+#endif // MSNVEHICLES_H
+
+// Local Variables:
+// mode:C++
+// End:
