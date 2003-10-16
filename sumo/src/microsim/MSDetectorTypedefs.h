73115df462fb2b88c6c2ece7d3bd19b646b74163
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-16 11:51:39 +0000

    Added new detectors.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1285 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSDetectorTypedefs.h
+++ sumo/src/microsim/MSDetectorTypedefs.h
@@ -1,6 +1,27 @@
 #ifndef MSDETECTORTYPEDEFS_H
 #define MSDETECTORTYPEDEFS_H
 
+/**
+ * @file   MSDetectorTypedefs.h
+ * @author Christian Roessel
+ * @date   Started Thu Oct 16 13:28:50 2003
+ * @version $Id$
+ * @brief  
+ * 
+ * 
+ */
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
 #include "MSMeanDetector.h"
 #include "MSSumDetector.h"
 #include "MSE2Detector.h"
@@ -11,6 +32,8 @@
 #include "MSJamLengthSum.h"
 #include "MSQueueLengthAheadOfTrafficLights.h"
 #include "MSNVehicles.h"
+#include "MSSpaceMeanSpeed.h"
+#include "MSCurrentHaltingDurationSumPerVehicle.h"
 
 namespace Detector 
 {
@@ -37,6 +60,14 @@ namespace Detector
     E2QueueLengthAheadOfTrafficLightsInMeters;
 
     typedef MSMeanDetector< MSE2Detector< MSNVehicles > > E2NVehicles;
+
+    typedef MSMeanDetector< MSE2Detector<
+        MSSpaceMeanSpeed > > E2SpaceMeanSpeed;
+
+    typedef MSMeanDetector< MSE2Detector<
+        MSCurrentHaltingDurationSumPerVehicle > >
+    E2CurrentHaltingDurationSumPerVehicle;
+    
 }
 
 #endif // MSE2DETECTORTYPEDEFS_H
