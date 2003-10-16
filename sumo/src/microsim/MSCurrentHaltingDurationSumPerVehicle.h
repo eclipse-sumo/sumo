00d33d2345ab21ed91d360de42d8f4aa1ff95bb2
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-16 11:23:07 +0000

    Initial commit of new E2_ZS detector.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1282 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSCurrentHaltingDurationSumPerVehicle.h
@@ -0,0 +1,83 @@
+#ifndef MSCURRENTHALTINGDURATIONSUMPERVEHICLE_H
+#define MSCURRENTHALTINGDURATIONSUMPERVEHICLE_H
+
+/**
+ * @file   MSCurrentHaltingDurationSumPerVehicle.h
+ * @author Christian Roessel
+ * @date   Started Thu Oct 16 11:44:47 2003
+ * @version $Id$
+ * @brief  
+ * 
+ * 
+ */
+
+/* Copyright (C) 2003 by German Aerospace Center (http://www.dlr.de) */
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+
+#include "MSHaltingDetectorContainer.h"
+#include <numeric>
+
+class MSCurrentHaltingDurationSumPerVehicle
+{
+public:
+    typedef DetectorContainer::Haltings::InnerCont::value_type Halting;
+
+protected:
+    typedef double DetectorAggregate;
+    typedef DetectorContainer::Haltings Container;
+    typedef Container::InnerCont VehicleCont;    
+
+    MSCurrentHaltingDurationSumPerVehicle( double lengthInMeters,
+                                           const Container& vehicleCont ) 
+        : containerM( vehicleCont.containerM )
+        {}
+
+    virtual ~MSCurrentHaltingDurationSumPerVehicle( void )
+        {}
+    
+    DetectorAggregate getDetectorAggregate( void ); // [m/s]
+    
+        static std::string getDetectorName( void )
+        {
+            return "currentHaltingDurationSumPerVehicle";
+        }
+    
+private:
+    const VehicleCont& containerM;    
+};
+
+namespace 
+{    
+    inline double haltingDurationSumUp(
+        double sumSoFar,
+        const MSCurrentHaltingDurationSumPerVehicle::Halting& data )
+    {
+        return sumSoFar + data.haltingDurationM;
+    }
+}
+
+MSCurrentHaltingDurationSumPerVehicle::DetectorAggregate
+MSCurrentHaltingDurationSumPerVehicle::getDetectorAggregate( void )
+{
+    int size = containerM.size();
+    if ( size == 0 ) {
+        return 0;
+    }
+    double haltingDurationSum =
+        std::accumulate( containerM.begin(), containerM.end(),
+                         0.0, haltingDurationSumUp ) / size;
+    return MSUnit::getInstance()->getSeconds( haltingDurationSum );
+}   
+#endif // MSCURRENTHALTINGDURATIONSUMPERVEHICLE_H
+
+// Local Variables:
+// mode:C++
+// End:
