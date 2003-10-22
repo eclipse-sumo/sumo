db8f58102ab7a5a7c6bdb8d31c85ed3e2a71e230
Christian Roessel <roessel@zpr.uni-koeln.de>
2003-10-22 14:41:07 +0000

    leaveDetectorByMove now searches for the vehicle to remove instead of erasing the last one.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1315 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSDetectorContainer.h
+++ sumo/src/microsim/MSDetectorContainer.h
@@ -54,9 +54,19 @@ struct MSDetectorContainer : public MSDetectorContainerBase
             containerM.insert( insertIt, ContainerItem( veh ) );
         }
 
-    void leaveDetectorByMove( void )
+    void leaveDetectorByMove( MSVehicle* veh )
         {
-            containerM.pop_back();
+            typedef typename InnerContainer::value_type ContainerItem;
+            typedef typename InnerContainer::iterator ContainerIt;
+            typedef typename Predicate::VehEqualsC< ContainerItem >
+                ErasePredicate;
+            ContainerIt eraseIt =
+                std::find_if( containerM.begin(), containerM.end(),
+                              std::bind2nd(
+                                  ErasePredicate(), veh ) );
+            assert(containerM.size()>0);
+            assert(eraseIt!=containerM.end());
+            containerM.erase( eraseIt );        
         }
 
     void leaveDetectorByLaneChange( MSVehicle* veh )
@@ -87,7 +97,6 @@ struct MSDetectorContainer : public MSDetectorContainerBase
         }
 
     InnerContainer containerM;
-
 };
 
 namespace DetectorContainer
@@ -95,6 +104,141 @@ namespace DetectorContainer
     typedef MSDetectorContainer< std::list< MSVehicle* > > Vehicles;
 }
 
+// #include <string>
+// template< >
+// struct MSDetectorContainer< std::list< MSVehicle* > > : public MSDetectorContainerBase
+// {
+//     typedef std::list< MSVehicle* >::const_iterator ContainerConstIt;
+//     typedef std::list< MSVehicle* > InnerContainer;
+//     typedef InnerContainer InnerCont;
+
+//     void printContainer(void)
+//         {
+//             int i = 0;
+//             for ( InnerContainer::iterator it = containerM.begin();
+//                   it != containerM.end(); ++it ){
+//                 std::cout << i << "-pos: " << (*it)->pos() << " from veh "
+//                           << (*it)->id() << std::endl;
+//                 ++i;
+//             }
+//         }
+    
+//     void enterDetectorByMove( MSVehicle* veh )
+//         {
+//             if (veh->id() == std::string("42")) {
+//                 std::cout << "42 enterDetectorByMove"
+//                           << " at step "
+//                           << MSNet::getInstance()->getCurrentTimeStep()
+//                           << std::endl;
+//                 printContainer();
+//             }
+            
+//             typedef InnerContainer::value_type ContainerItem;
+//             containerM.push_front( ContainerItem( veh ) );
+// //             std::cout << "insertByMove " << veh->id()
+// //                       << " at step "
+// //                       << MSNet::getInstance()->getCurrentTimeStep()
+// //                       << std::endl;
+// //             printContainer();
+//         }
+
+//     void enterDetectorByEmitOrLaneChange( MSVehicle* veh )
+//         {
+//             if (veh->id() == std::string("42")) {
+//                 std::cout << "42 enterDetectorByMove"
+//                           << " at step "
+//                           << MSNet::getInstance()->getCurrentTimeStep()
+//                           << std::endl;
+//                 printContainer();
+//             }           
+//             typedef InnerContainer::value_type ContainerItem;
+//             typedef InnerContainer::iterator ContainerIt;
+//             typedef Predicate::PosGreaterC< ContainerItem >
+//                 PosGreaterPredicate;
+//             ContainerIt insertIt =
+//                 std::find_if( containerM.begin(), containerM.end(),
+//                               std::bind2nd(
+//                                   PosGreaterPredicate(), veh->pos() ) );
+//             containerM.insert( insertIt, ContainerItem( veh ) );
+// //             std::cout << "insertByEmitOrLaneCh " << veh->id()
+// //                       << " at step "
+// //                       << MSNet::getInstance()->getCurrentTimeStep()
+// //                       << std::endl;
+// //             printContainer();
+//         }
+
+//     void leaveDetectorByMove( MSVehicle* veh )
+//         {
+//             if (veh->id() == std::string("42")) {
+//                 std::cout << "42 enterDetectorByMove"
+//                           << " at step "
+//                           << MSNet::getInstance()->getCurrentTimeStep()
+//                           << std::endl;
+//                 printContainer();
+//             }
+//             typedef InnerContainer::value_type ContainerItem;
+//             typedef InnerContainer::iterator ContainerIt;
+//             typedef Predicate::VehEqualsC< ContainerItem >
+//                 ErasePredicate;
+
+// //             std::cout << "leaveByMove " << veh->id()
+// //                       << " at step "
+// //                       << MSNet::getInstance()->getCurrentTimeStep()
+// //                       << std::endl;
+// //             containerM.pop_back();
+//             ContainerIt eraseIt =
+//                 std::find_if( containerM.begin(), containerM.end(),
+//                               std::bind2nd(
+//                                   ErasePredicate(), veh ) );
+//             assert(containerM.size()>0);
+//             assert(eraseIt!=containerM.end());
+//             containerM.erase( eraseIt );           
+//         }
+
+//     void leaveDetectorByLaneChange( MSVehicle* veh )
+//         {
+//             if (veh->id() == std::string("42")) {
+//                 std::cout << "42 enterDetectorByMove"
+//                           << " at step "
+//                           << MSNet::getInstance()->getCurrentTimeStep()
+//                           << std::endl;
+//                 printContainer();
+//             }
+//             typedef InnerContainer::value_type ContainerItem;
+//             typedef InnerContainer::iterator ContainerIt;
+//             typedef Predicate::VehEqualsC< ContainerItem >
+//                 ErasePredicate;
+// //             std::cout << "leaveBylaneCh " << veh->id()
+// //                       << " at step "
+// //                       << MSNet::getInstance()->getCurrentTimeStep()
+// //                       << std::endl;
+// //             std::cout << "size " << containerM.size() << std::endl;
+//             ContainerIt eraseIt =
+//                 std::find_if( containerM.begin(), containerM.end(),
+//                               std::bind2nd(
+//                                   ErasePredicate(), veh ) );
+//             assert(containerM.size()>0);
+//             assert(eraseIt!=containerM.end());
+//             containerM.erase( eraseIt );
+//         }
+
+//     virtual void update( void )
+//         {}
+
+//     MSDetectorContainer( void )
+//         : containerM()
+//         {}
+
+//     virtual ~MSDetectorContainer( void )
+//         {
+//             containerM.clear();
+//         }
+
+//     InnerContainer containerM;
+
+
+
+// };
 
 #endif // MSDETECTORCONTAINER_H
 
