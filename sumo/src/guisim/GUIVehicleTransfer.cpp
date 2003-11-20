77ffcdc9167fd00cdd58e91478bd19ef52cad0ec
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-11-20 14:41:02 +0000

    class documentation added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1413 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/guisim/GUIVehicleTransfer.cpp
+++ sumo/src/guisim/GUIVehicleTransfer.cpp
@@ -1,3 +1,34 @@
+/***************************************************************************
+                          GUIVehicleTransfer.cpp  -
+ The gui-version of a mover of vehicles that got stucked due to grid locks
+                             -------------------
+    begin                : Sep 2003
+    copyright            : (C) 2003 by DLR/IVF http://ivf.dlr.de/
+    author               : Daniel Krajzewicz
+    email                : Daniel Krajzewicz@dlr.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+// $Log$
+// Revision 1.3  2003/11/20 14:41:02  dkrajzew
+// class documentation added
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
 #include <iostream>
 #include <cassert>
 #include <utils/common/MsgHandler.h>
@@ -7,9 +38,16 @@
 #include "GUIVehicle.h"
 #include "GUIVehicleTransfer.h"
 
+
+/* =========================================================================
+ * used namespaces
+ * ======================================================================= */
 using namespace std;
 
 
+/* =========================================================================
+ * method definitions
+ * ======================================================================= */
 GUIVehicleTransfer::GUIVehicleTransfer()
 {
 }
@@ -23,3 +61,11 @@ GUIVehicleTransfer::removeVehicle(const std::string &id)
     static_cast<GUINet*>(MSNet::getInstance())->getIDStorage().remove(
         static_cast<GUIVehicle*>(veh)->getGlID());
 }
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+
+// Local Variables:
+// mode:C++
+// End:
+
