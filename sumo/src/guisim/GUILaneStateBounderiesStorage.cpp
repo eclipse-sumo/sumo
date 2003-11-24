7a8d7464edd73cd84252df2346ae6977763fa59b
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-11-24 10:16:08 +0000

    documentation added; division by zero-error patched
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1421 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/guisim/GUILaneStateBounderiesStorage.cpp
+++ sumo/src/guisim/GUILaneStateBounderiesStorage.cpp
@@ -1,6 +1,41 @@
+//---------------------------------------------------------------------------//
+//                        GUILaneStateBounderiesStorage.cpp -
+//  A class that stores the min and max-bounderies of lane state values
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Thu, 20th Oct 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+// $Log$
+// Revision 1.2  2003/11/24 10:16:08  dkrajzew
+// documentation added; division by zero-error patched
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
 #include "GUILaneStateBounderiesStorage.h"
 
 
+/* =========================================================================
+ * method definitions
+ * ======================================================================= */
 GUILaneStateBounderiesStorage::GUILaneStateBounderiesStorage()
     : myAmGlobalInitialised(false), myAmStepInitialised(false)
 {
@@ -21,12 +56,15 @@ GUILaneStateBounderiesStorage::addValue(MS_E2_ZS_Collector::DetType type,
         myAmGlobalInitialised = true;
         myTimeGlobalMaxes[type] = val;
         myTimeGlobalMins[type] = val;
+        myTimeGlobalRanges[type] = 0;
     } else {
         if(myTimeGlobalMaxes[type]<val) {
             myTimeGlobalMaxes[type] = val;
+            myTimeGlobalRanges[type] = val - myTimeGlobalMins[type];
         }
         if(myTimeGlobalMins[type]>val) {
             myTimeGlobalMins[type] = val;
+            myTimeGlobalRanges[type] = myTimeGlobalMaxes[type] - val;
         }
     }
     // for step values
@@ -34,12 +72,15 @@ GUILaneStateBounderiesStorage::addValue(MS_E2_ZS_Collector::DetType type,
         myAmStepInitialised = true;
         myTimeStepMaxes[type] = val;
         myTimeStepMins[type] = val;
+        myTimeStepRanges[type] = 0;
     } else {
         if(myTimeStepMaxes[type]<val) {
             myTimeStepMaxes[type] = val;
+            myTimeStepRanges[type] = val - myTimeStepMins[type];
         }
         if(myTimeStepMins[type]>val) {
             myTimeStepMins[type] = val;
+            myTimeStepRanges[type] = myTimeStepMaxes[type] - val;
         }
     }
 }
@@ -49,8 +90,20 @@ double
 GUILaneStateBounderiesStorage::timeStepNorm(double val,
                                             MS_E2_ZS_Collector::DetType type)
 {
+    // check whether any values were inserted, yet
+    if(!myAmStepInitialised) {
+        // return mean if not
+        return 0.5;
+    }
+    // check whether a range exists
+    double range = myTimeStepRanges[type];
+    if(range==0) {
+        // return mean if not
+        return 0.5;
+    }
+    // compute the normalised value
     val -= myTimeStepMins[type];
-    return val /= (myTimeStepMaxes[type]-myTimeStepMins[type]);
+    return val /= range;
 }
 
 
@@ -59,3 +112,12 @@ GUILaneStateBounderiesStorage::initStep()
 {
     myAmStepInitialised = false;
 }
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+
+// Local Variables:
+// mode:C++
+// End:
+
+
--- sumo/src/guisim/GUILaneStateBounderiesStorage.h
+++ sumo/src/guisim/GUILaneStateBounderiesStorage.h
@@ -1,23 +1,83 @@
 #ifndef GUILaneStateBounderiesStorage_h
 #define GUILaneStateBounderiesStorage_h
+//---------------------------------------------------------------------------//
+//                        GUILaneStateBounderiesStorage.h -
+//  A class that stores the min and max-bounderies of lane state values
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Thu, 20th Oct 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
 
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+// $Log$
+// Revision 1.2  2003/11/24 10:16:08  dkrajzew
+// documentation added; division by zero-error patched
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
 #include <map>
 #include <microsim/MS_E2_ZS_Collector.h>
 
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
 class GUILaneStateBounderiesStorage {
 public:
+    /// Constructor
     GUILaneStateBounderiesStorage();
+
+    /// Destructor
     ~GUILaneStateBounderiesStorage();
+
+    /** @brief Adds a value to
+        The bounderies will be adapted to the current values */
     void addValue(MS_E2_ZS_Collector::DetType type, double val);
+
+    /// Normalises the given value to the current step's min max
     double timeStepNorm(double val, MS_E2_ZS_Collector::DetType type);
+
+    /// Initialises the bounderies for a step
     void initStep();
+
 private:
+    /// Definition of a mapping of a detector type to values
     typedef std::map<MS_E2_ZS_Collector::DetType, double> ValueMap;
+
+    /// Storage for detector-dependent whole-simulation maximum bounderies
     ValueMap myTimeGlobalMaxes;
+
+    /// Storage for detector-dependent whole-simulation minimum bounderies
     ValueMap myTimeGlobalMins;
+
+    /// Storage for detector-dependent whole-simulation value ranges
+    ValueMap myTimeGlobalRanges;
+
+    /// Storage for detector-dependent time-step maximum bounderies
     ValueMap myTimeStepMaxes;
+
+    /// Storage for detector-dependent time-step minimum bounderies
     ValueMap myTimeStepMins;
+
+    /// Storage for detector-dependent time-step minimum value ranges
+    ValueMap myTimeStepRanges;
+
+    /// Information whether at least one value was added to the whole-simulation bounderies
     bool myAmGlobalInitialised;
+
+    /// Information whether at least one value was added to the time-step bounderies
     bool myAmStepInitialised;
 
 };
