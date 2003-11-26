e5912fd74ebef6a36fe2a2de52504becc94ed84a
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-11-26 09:51:10 +0000

    changes to allow more than a single function of an object to retrieve messages
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1435 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/MsgHandler.h
+++ sumo/src/utils/common/MsgHandler.h
@@ -22,6 +22,9 @@
  ***************************************************************************/
 
 // $Log$
+// Revision 1.3  2003/11/26 09:51:10  dkrajzew
+// changes to allow more than a single function of an object to retrieve messages
+//
 // Revision 1.2  2003/07/07 08:44:43  dkrajzew
 // a new interface for a joined output of messages added
 //
@@ -46,19 +49,30 @@ class MsgRetriever;
  */
 class MsgHandler {
 public:
+    /**
+     * @enum MsgType
+     * An enumeration to differ between different types of messages
+     * (errors, warning and information)
+     */
     enum MsgType {
+        /// The message is only something to show
         MT_MESSAGE,
+        /// The message is a warning
         MT_WARNING,
+        /// The message is an error
         MT_ERROR
     };
 
-
+    /// Returns the instance to add normal messages to
     static MsgHandler *getMessageInstance();
 
+    /// Returns the instance to add warnings to
     static MsgHandler *getWarningInstance();
 
+    /// Returns the instance to add errors to
     static MsgHandler *getErrorInstance();
 
+    /// Removes pending handler
     static void cleanupOnEnd();
 
     /// adds a new error to the list
@@ -72,13 +86,16 @@ public:
     /// Clears information whether an error occured previously
     void clear();
 
-    ///
+    /// Adds a further retriever to the instance responsible for a certain msg type
     void addRetriever(MsgRetriever *retriever);
 
+    /// Removes the retriever from the
     void removeRetriever(MsgRetriever *retriever);
 
+    /// Sets the information whether stdout shall be used as output device, too
     void report2cout(bool value);
 
+    /// Returns the information whether any messages were added
     bool wasInformed() const;
 
 private:
@@ -89,18 +106,23 @@ private:
     ~MsgHandler();
 
 private:
+    /// The instance to handle errors
     static MsgHandler *myErrorInstance;
 
+    /// The instance to handle warnings
     static MsgHandler *myWarningInstance;
 
+    /// The instance to handle normal messages
     static MsgHandler *myMessageInstance;
 
 private:
+    /// The type of the instance
     MsgType myType;
 
     /// information wehther an error occured at all
-    bool      myWasInformed;
+    bool myWasInformed;
 
+    /// Information whether stdout shall be used as output device, too
     bool myReport2COUT;
 
     /// Definition of the list of retrievers to inform
---
+++ sumo/src/utils/common/MsgRetrievingFunction.h
@@ -0,0 +1,86 @@
+#ifndef MsgRetrievingFunction_h
+#define MsgRetrievingFunction_h
+/***************************************************************************
+                          MsgRetrievingFunction.h  -
+    Encapsulates an object's method in order to use it as a MsgRetriever
+                             -------------------
+    project              : SUMO
+    begin                : Mon, 24 Oct 2003
+    copyright            : (C) 2001 by DLR/IVF http://ivf.dlr.de/
+    author               : Daniel Krajzewicz
+    email                : Daniel.Krajzewicz@dlr.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// $Log$
+// Revision 1.1  2003/11/26 09:51:10  dkrajzew
+// changes to allow more than a single function of an object to retrieve messages
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#include <string>
+#include "MsgRetriever.h"
+
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
+/**
+ * @class MsgRetrievingFunction
+ * A class that encapsulates an object with a given method to be usable as
+ *  a retriever of messages.
+ */
+template< class _T >
+class MsgRetrievingFunction : public MsgRetriever
+{
+public:
+    /// Type of the function to execute.
+    typedef void ( _T::* Operation )(const std::string &);
+
+    /// Constructor
+    MsgRetrievingFunction( _T* object, Operation operation ) :
+        myObject( object ),
+        myOperation( operation )
+        {}
+
+    /// Destructor.
+    ~MsgRetrievingFunction()
+        {}
+
+    /// Called to inform the object about a new message
+    void inform(const std::string &msg)
+        {
+            ( myObject->*myOperation )(msg);
+        }
+
+private:
+    /// The object the action is directed to.
+    _T* myObject;
+
+    /// The object's operation to perform.
+    Operation myOperation;
+
+};
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+//#ifndef DISABLE_INLINE
+//#include "MsgRetrievingFunction.icc"
+//#endif
+
+#endif
+
+// Local Variables:
+// mode:C++
+// End:
+
