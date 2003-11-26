dc0c9d402bdc43f48320928d320104b5b4b81c81
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2003-11-26 09:37:07 +0000

    moving of the view when reaching the left border implemented; display of a time scale implemented
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1432 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/gui/tlstracker/GUITLLogicPhasesTrackerWindow.cpp
+++ sumo/src/gui/tlstracker/GUITLLogicPhasesTrackerWindow.cpp
@@ -1,3 +1,32 @@
+//---------------------------------------------------------------------------//
+//                        GUITLLogicPhasesTrackerWindow.cpp -
+//  A window displaying the phase diagram of a tl-logic
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Oct/Nov 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+// $Log$
+// Revision 1.3  2003/11/26 09:37:07  dkrajzew
+// moving of the view when reaching the left border implemented; display of a time scale implemented
+//
+//
 /* =========================================================================
  * included modules
  * ======================================================================= */
@@ -5,6 +34,7 @@
 #include "config.h"
 #endif // HAVE_CONFIG_H
 
+#include <cassert>
 #include <vector>
 #include <qgl.h>
 #include <qdialog.h>
@@ -16,20 +46,31 @@
 #include <microsim/MSLink.h>
 #include <utils/convert/ToString.h>
 
-
 #ifndef WIN32
 #include "GUITLLogicPhasesTrackerWindow.moc"
 #endif
 
+
+/* =========================================================================
+ * used namespaces
+ * ======================================================================= */
 using namespace std;
 
+
+/* =========================================================================
+ * member method definitions
+ * ======================================================================= */
+/* -------------------------------------------------------------------------
+ * GUITLLogicPhasesTrackerWindow-methods
+ * ----------------------------------------------------------------------- */
 GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerWindow(
         GUIApplicationWindow &app, MSTrafficLightLogic &logic)
         : myApplication(app), myTLLogic(logic)
 {
+    size_t height = myTLLogic.getLinks().size() * 20 + 30;
     setCaption("Tracker");
-    setBaseSize(300, 200);
-    setMinimumSize(300, 200);
+    setBaseSize(300, height);
+    setMinimumSize(300, height);
     app.addChild(this, true);
     for(size_t i=0; i<myTLLogic.getLinks().size(); i++) {
         myLinkNames.push_back(toString<size_t>(i));
@@ -37,7 +78,6 @@ GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerWindow(
     myPanel = new
         GUITLLogicPhasesTrackerPanel(myApplication, *this);
     setCentralWidget(myPanel);
-//    myPanel->move(xpos, ypos);
     show();
 }
 
@@ -45,7 +85,6 @@ GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerWindow(
 GUITLLogicPhasesTrackerWindow::~GUITLLogicPhasesTrackerWindow()
 {
 }
-
 
 
 bool
@@ -88,12 +127,10 @@ GUITLLogicPhasesTrackerWindow::resizeEvent ( QResizeEvent *e )
 }
 
 
-
 void
 GUITLLogicPhasesTrackerWindow::drawValues(GUITLLogicPhasesTrackerPanel &caller)
 {
-    // compute which font to use
-    //
+    // begin drawing
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glMatrixMode(GL_MODELVIEW);
@@ -105,19 +142,19 @@ GUITLLogicPhasesTrackerWindow::drawValues(GUITLLogicPhasesTrackerPanel &caller)
     // draw the horizontal lines dividing the signal groups
     glColor3d(1, 1, 1);
     myFontRenderer.SetColor(1, 1, 1);
-    glBegin(GL_LINES);
-    //
+    // compute some values needed more than once
     double height = (double) caller.getHeightInPixels();
     double width = (double) caller.getWidthInPixels();
     double h4 = ((double) 4 / height);
     double h10 = ((double) 10 / height);
     double h16 = ((double) 16 / height);
     double h20 = ((double) 20 / height);
-
-
+    // draw the link names and the lines dividing them
+    myFontRenderer.SetActiveFont("std10");
     double h = 1.0 - h10;
     double h2 = 12;
     size_t i;
+    glBegin(GL_LINES);
     for(i=0; i<myTLLogic.getLinks().size()+1; i++) {
         // draw the bar
         glVertex2f(0, h);
@@ -129,30 +166,46 @@ GUITLLogicPhasesTrackerWindow::drawValues(GUITLLogicPhasesTrackerPanel &caller)
         }
         h -= h20;
     }
+    glVertex2f(0, h+h20);
+    glVertex2f(1.0, h+h20);
     glEnd();
-    // names closure
+
+    // draw the names closure (vertical line)
     h += (double) 20 / height;
     glColor3d(1, 1, 1);
     glBegin(GL_LINES);
     glVertex2f((double) 30 / width, 1.0);
     glVertex2f((double) 30 / width, h);
     glEnd();
-    // phases
+
+
+    // draw the phases
+        // disable value addition while drawing
     myLock.lock();
-    PhasesVector::iterator pi = myPhases.begin();
-    DurationsVector::iterator pd = myDurations.begin();
+        // determine the initial offset
     double x = (double) 31 / width;
-    for(i=30; i<caller.getWidthInPixels()&&pd!=myDurations.end(); ) {
-        size_t duration = *pd;
-        double h = 1.0 - h10;
+    computeOffsets(width, 31);
+        // and the initial phase information
+    PhasesVector::iterator pi = myPhases.begin() + myFirstPhase2Show;
+    DurationsVector::iterator pd = myDurations.begin() + myFirstPhase2Show;
+    size_t fpo = myFirstPhaseOffset;
+        // start drawing
+    for(i=30; i<width&&pd!=myDurations.end(); ) {
+        // the first phase may be drawn incompletely
+        size_t duration = *pd - fpo;
+        // compute the heigh and the width of the phase
+        h = 1.0 - h10;
         double x2 = x + (double) duration / width;
+        // go through the links
         for(size_t j=0; j<myTLLogic.getLinks().size(); j++) {
+            // determine the current link's color
             MSLink::LinkState state =
                 (*pi).first.test(j)==true
                 ? MSLink::LINKSTATE_TL_GREEN
                 : (*pi).second.test(j)==true
                     ? MSLink::LINKSTATE_TL_YELLOW
                     : MSLink::LINKSTATE_TL_RED;
+            // draw the bar (red is drawn as a line)
             switch(state) {
             case MSLink::LINKSTATE_TL_GREEN:
                 glColor3f(0, 1.0, 0);
@@ -182,89 +235,126 @@ GUITLLogicPhasesTrackerWindow::drawValues(GUITLLogicPhasesTrackerPanel &caller)
             default:
                 break;
             }
+            // proceed to next link
             h -= h20;
         }
+        // proceed to next phase
         i += duration;
         pi++;
         pd++;
         x = x2;
+        // all further phases are drawn in full
+        fpo = 0;
     }
+    // allow value addition
     myLock.unlock();
 
-    // just tests...
-    glBegin(GL_LINES);
-    glColor3d(1, 1, 0);
-    glVertex2f(0, 0);
-    glVertex2f(1, 1);
-    glColor3d(1, 0, 1);
-    glVertex2f(0, 1);
-    glVertex2f(1, 0);
-    glEnd();
+    if(myPhases.size()!=0) {
+        myFontRenderer.SetActiveFont("std8");
+        int tickDist = 60;
+        // draw time information
+        h = myTLLogic.getLinks().size() * 20 + 12;
+        float glh = 1.0 - myTLLogic.getLinks().size() * h20 - h10;
+            // current begin time
+        string timeStr = toString<MSNet::Time>(myFirstTime2Show);
+        myFontRenderer.StringOut(
+            31-myFontRenderer.GetStringWidth(timeStr), h, timeStr);
+            // time ticks
+        MSNet::Time currTime =
+            ((MSNet::Time) (myFirstTime2Show / tickDist) + 1) * tickDist;
+        size_t pos = 31 + currTime - myFirstTime2Show;
+        float glpos = (pos) / width;
+        glColor3d(1, 1, 1);
+        while(pos<width+50) {
+            timeStr = toString<MSNet::Time>(currTime);
+            myFontRenderer.StringOut(
+                pos-myFontRenderer.GetStringWidth(timeStr), h, timeStr);
+            pos += tickDist;
+            currTime += tickDist;
+            glBegin(GL_LINES);
+            glVertex2f(glpos, glh);
+            glVertex2f(glpos, glh-h4);
+            glEnd();
+            glpos += (tickDist/width);
+        }
+    }
+
+    // set written strings
     myFontRenderer.Draw(caller.getWidthInPixels(),
         caller.getHeightInPixels());
 }
 
 
 void
-GUITLLogicPhasesTrackerWindow::addValue(SimplePhaseDef def)
+GUITLLogicPhasesTrackerWindow::computeOffsets(size_t width,
+                                              size_t firstXPixelOffset)
 {
-    cout << "Ret:" << def.first << '/' << def.second  << endl;
+    myFirstPhase2Show = 0;
+    myFirstPhaseOffset = 0;
+    myFirstTime2Show = myBeginTime;
+    // check whether no phases are known at all
+    if(myDurations.size()==0) {
+        return;// std::pair<size_t, size_t>(0, 0);
+    }
+    // go through the phases starting at their end
+    int noSecs = width - firstXPixelOffset;
+    size_t timeOffset = 0;
+    DurationsVector::reverse_iterator i=myDurations.rbegin();
+    while(noSecs>=0&&i!=myDurations.rend()) {
+        timeOffset += (*i);
+        noSecs -= (*i++);
+    }
+    // check whether all known phases have enough place
+    if(i==myDurations.rend()) {
+        return;// std::pair<size_t, size_t>(0, 0);
+    }
+    // otherwise, return the offsets
+    i--;
+    size_t phaseOffset = distance(i, myDurations.rend()-1);
+    size_t firstOffset = (-noSecs);
+    myFirstTime2Show = myLastTime - (timeOffset-firstOffset);
+    if(*i==firstOffset) {
+        phaseOffset++;
+        firstOffset = 0;
+    }
+    myFirstPhase2Show = phaseOffset;
+    myFirstPhaseOffset = firstOffset;
+}
+
+
+void
+GUITLLogicPhasesTrackerWindow::addValue(CompletePhaseDef def)
+{
+    // do not draw while adding
     myLock.lock();
-    if(myPhases.size()==0||*(myPhases.end()-1)!=def) {
-        myPhases.push_back(def);
+    // set the first time if not set before
+    if(myPhases.size()==0) {
+        myBeginTime = def.first;
+    }
+    // append or set the phase
+    if(myPhases.size()==0||*(myPhases.end()-1)!=def.second) {
+        myPhases.push_back(def.second);
         myDurations.push_back(1);
     } else {
         *(myDurations.end()-1) += 1;
     }
+    // set the last time a phase was added at
+    myLastTime = def.first;
+    // allow drawing
     myLock.unlock();
 }
 
 void
 GUITLLogicPhasesTrackerWindow::setFontRenderer(GUITLLogicPhasesTrackerPanel &caller)
 {
+    myFontRenderer.add(myApplication.myFonts.get("std8"));
     myFontRenderer.add(myApplication.myFonts.get("std10"));
 }
 
-/*
-void
-GUITLLogicPhasesTrackerWindow::buildFileMenu()
-{
-    QPixmap saveIcon;
-    saveIcon = QPixmap( filesave );
-    // build the file-menu
-    _fileMenu = new QPopupMenu( this );
-    menuBar()->insertItem( "&File", _fileMenu );
-    _fileMenu->insertSeparator();
-    _fileMenu->insertItem( "&Close", this, SLOT(closeAllWindows()), CTRL+Key_W );
-}
 
-
-void
-GUITLLogicPhasesTrackerWindow::buildFileTools()
-{
-    QPixmap saveIcon;
-
-    fileTools = new QToolBar( "file operations", this);
-    addToolBar( fileTools, tr( "File Operations" ), Top, TRUE );
-
-    saveIcon = QPixmap( filesave );
-    QToolButton *fileSave = new QToolButton( saveIcon, "Open File",
-        QString::null, this, SLOT(load()), fileTools, "open file" );
-}
-*/
-/*
-void
-GUITLLogicPhasesTrackerWindow::addTracked(TrackerValueDesc *newTracked)
-{
-    myTracked.push_back(newTracked);
-}
-*/
-
-
-
-
-
-
+/* -------------------------------------------------------------------------
+ * GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerPanel-methods
+ * ----------------------------------------------------------------------- */
 GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerPanel::GUITLLogicPhasesTrackerPanel(
         GUIApplicationWindow &app, GUITLLogicPhasesTrackerWindow &parent)
     : QGLWidget(&parent),
@@ -342,11 +432,19 @@ GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerPanel::getHeightInPixels()
     return _heightInPixels;
 }
 
+
 size_t
 GUITLLogicPhasesTrackerWindow::GUITLLogicPhasesTrackerPanel::getWidthInPixels() const
 {
     return _widthInPixels;
 }
+
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+
+// Local Variables:
+// mode:C++
+// End:
 
 
 
--- sumo/src/gui/tlstracker/GUITLLogicPhasesTrackerWindow.h
+++ sumo/src/gui/tlstracker/GUITLLogicPhasesTrackerWindow.h
@@ -1,6 +1,29 @@
 #ifndef GUITLLogicPhasesTrackerWindow_h
 #define GUITLLogicPhasesTrackerWindow_h
+//---------------------------------------------------------------------------//
+//                        GUITLLogicPhasesTrackerWindow.h -
+//  A window displaying the phase diagram of a tl-logic
+//                           -------------------
+//  project              : SUMO - Simulation of Urban MObility
+//  begin                : Oct/Nov 2003
+//  copyright            : (C) 2003 by Daniel Krajzewicz
+//  organisation         : IVF/DLR http://ivf.dlr.de
+//  email                : Daniel.Krajzewicz@dlr.de
+//---------------------------------------------------------------------------//
 
+//---------------------------------------------------------------------------//
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//---------------------------------------------------------------------------//
+// $Log$
+// Revision 1.2  2003/11/26 09:37:07  dkrajzew
+// moving of the view when reaching the left border implemented; display of a time scale implemented
+//
+//
 /* =========================================================================
  * included modules
  * ======================================================================= */
@@ -15,29 +38,60 @@
 #include <qgl.h>
 #include <qdialog.h>
 #include <qmainwindow.h>
+#include <microsim/MSNet.h>
 #include <helpers/ValueRetriever.h>
 #include <utils/qutils/NewQMutex.h>
 #include <utils/glutils/lfontrenderer.h>
 
+
+/* =========================================================================
+ * class declarations
+ * ======================================================================= */
 class GUIApplicationWindow;
 class MSTrafficLightLogic;
 
+
+/* =========================================================================
+ * type definitions
+ * ======================================================================= */
+/** @brief Definition of a phase description
+    Within the first bitsets, all links having gree are marked, within the second
+    all links having yellow */
 typedef std::pair<std::bitset<64>, std::bitset<64> > SimplePhaseDef;
 
-class GUITLLogicPhasesTrackerWindow : public QMainWindow,
-    public ValueRetriever<SimplePhaseDef> {
+/// Definition of a complete phase information, including the time
+typedef std::pair<MSNet::Time, SimplePhaseDef> CompletePhaseDef;
+
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
+/**
+ * @class GUITLLogicPhasesTrackerWindow
+ * This window displays a phase diagram for a chosen tl-logic.
+ */
+class GUITLLogicPhasesTrackerWindow
+        : public QMainWindow,
+          public ValueRetriever<CompletePhaseDef> {
+
+    /// is a q-object
     Q_OBJECT
 public:
-    GUITLLogicPhasesTrackerWindow(GUIApplicationWindow &app, 
+    /// Constructor
+    GUITLLogicPhasesTrackerWindow(GUIApplicationWindow &app,
         MSTrafficLightLogic &logic);
+
+    /// Destructor
     ~GUITLLogicPhasesTrackerWindow();
+
     /// Returns the information about the largest width allowed for openGL-windows
     int getMaxGLWidth() const;
 
     /// Returns the information about the largest height allowed for openGL-windows
     int getMaxGLHeight() const;
 
-    void addValue(SimplePhaseDef def);
+    /// Adds a further phase definition
+    void addValue(CompletePhaseDef def);
 
 protected:
     /// Callback for events
@@ -49,9 +103,19 @@ protected:
     /// Callback for the paint-event
     void paintEvent ( QPaintEvent * );
 
+    /// Definition of a storage for phases
     typedef std::vector<SimplePhaseDef> PhasesVector;
+
+    /** @brief This list of stored phases
+        Only new phases are stored; The times are not stored at all, they
+        are just used to steer the output */
     typedef std::vector<size_t> DurationsVector;
 
+    /**
+     * @class GUITLLogicPhasesTrackerPanel
+     * The canvas for the visualisation.
+     * The drawing itself id done by the parent.
+     */
     class GUITLLogicPhasesTrackerPanel : public QGLWidget {
     public:
         /// Constructor
@@ -61,7 +125,10 @@ protected:
         /// Destructor
         ~GUITLLogicPhasesTrackerPanel();
 
+        /// Returns the height
         size_t getHeightInPixels() const;
+
+        /// Returns the width
         size_t getWidthInPixels() const;
 
         /// needed to update
@@ -98,21 +165,64 @@ public:
     /// Draws all values
     void drawValues(GUITLLogicPhasesTrackerPanel &caller);
 
+    /// Sets the fonts used
     void setFontRenderer(GUITLLogicPhasesTrackerPanel &caller);
 
+private:
+    /** @brief Computes the offsets that determine te first drawn item
+        The first offset is the index of the first item to draw within
+        the vector; the second offset is the offsets in pixels */
+    void computeOffsets(size_t width, size_t firstXPixelOffset);
 
 private:
     /// The main application
     GUIApplicationWindow &myApplication;
+
+    /// The logic to display
     MSTrafficLightLogic &myTLLogic;
+
+    /// The list of phases
     PhasesVector myPhases;
+
+    /// The list of phase durations
     DurationsVector myDurations;
+
+    /// The panel to draw on
     GUITLLogicPhasesTrackerPanel *myPanel;
+
+    /// A lock to avoid addition of new values while drawing
     NewQMutex myLock;
+
+    /** @brief The names of links
+        This holds an enumeration only - used to avoid time consuming
+        string representation of ints */
     std::vector<std::string> myLinkNames;
+
     /// The openGL-font drawer
     LFontRenderer myFontRenderer;
 
+    /// The index of the first phase that fits into the window
+    size_t myFirstPhase2Show;
+
+    /// The offset to draw the first phase (left offset)
+    size_t myFirstPhaseOffset;
+
+    /// The time the diagram begins at
+    MSNet::Time myFirstTime2Show;
+
+    /// The first time a phase was added at
+    MSNet::Time myBeginTime;
+
+    /// The last time a phase was added at
+    MSNet::Time myLastTime;
+
 };
 
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+
 #endif
+
+// Local Variables:
+// mode:C++
+// End:
