44197470d736b23cf13fa835f5a21f57c4ffaab4
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2004-01-26 07:17:49 +0000

    "specialisation" of the time line to handle interpolation added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1618 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/router/ValueTimeLine.cpp
+++ sumo/src/utils/router/ValueTimeLine.cpp
@@ -17,87 +17,106 @@
 //   (at your option) any later version.
 //
 //---------------------------------------------------------------------------//
-namespace
-{
-    const char rcsid[] =
-    "$Id$";
-}
 // $Log$
+// Revision 1.3  2004/01/26 07:17:49  dkrajzew
+// "specialisation" of the time line to handle interpolation added
+//
 // Revision 1.2  2003/02/07 10:52:57  dkrajzew
 // updated
 //
-//
-
-
 /* =========================================================================
  * included modules
  * ======================================================================= */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
+
 #include <utility>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 #include "ValueTimeLine.h"
 
+
+/* =========================================================================
+ * used namespaces
+ * ======================================================================= */
 using namespace std;
 
-ValueTimeLine::ValueTimeLine()
+
+/* =========================================================================
+ * method definitions
+ * ======================================================================= */
+template<typename T>
+ValueTimeLine<T>::ValueTimeLine<T>()
 {
 }
 
 
-ValueTimeLine::~ValueTimeLine()
+template<typename T>
+ValueTimeLine<T>::~ValueTimeLine<T>()
 {
 }
 
 
+template<typename T>
 void
-ValueTimeLine::addValue(long begin, long end, float value)
+ValueTimeLine<T>::addValue(unsigned int begin, unsigned int end, T value)
 {
     addValue(TimeRange(begin, end), value);
 }
 
 
+template<typename T>
 void
-ValueTimeLine::addValue(TimeRange range, float value)
+ValueTimeLine<T>::addValue(TimeRange range, T value)
 {
     ValuedTimeRange valued(range, value);
-    _values.push_back(valued);
+    myValues.push_back(valued);
 }
 
-float
-ValueTimeLine::getValue(long time) const
+
+template<typename T>
+T
+ValueTimeLine<T>::getValue(unsigned int time) const
 {
-    assert(_values.size()>0);
+    assert(myValues.size()>0);
     TimedValueVector::const_iterator i =
-        find_if(_values.begin(), _values.end(), range_finder(time));
-    if(i==_values.end()) {
-        i = _values.end() - 1;
+        find_if(myValues.begin(), myValues.end(), range_finder(time));
+    if(i==myValues.end()) {
+        i = myValues.end() - 1;
     }
     return (*i).second;
 }
 
 
+template<typename T>
 size_t
-ValueTimeLine::noDefinitions() const
+ValueTimeLine<T>::noDefinitions() const
 {
-    return _values.size();
+    return myValues.size();
 }
 
 
-const ValueTimeLine::TimeRange &
-ValueTimeLine::getRangeAtPosition(size_t pos) const
+template<typename T>
+const ValueTimeLine<T>::TimeRange &
+ValueTimeLine<T>::getRangeAtPosition(size_t pos) const
 {
-    return _values[pos].first;
+    return myValues[pos].first;
+}
+
+
+template<typename T>
+bool
+ValueTimeLine<T>::describesTime(unsigned int time) const
+{
+    TimedValueVector::const_iterator i =
+        find_if(myValues.begin(), myValues.end(), range_finder(time));
+    return(i!=myValues.end());
 }
 
 
 /**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
-//#ifdef DISABLE_INLINE
-//#include "ValueTimeLine.icc"
-//#endif
 
 // Local Variables:
 // mode:C++
--- sumo/src/utils/router/ValueTimeLine.h
+++ sumo/src/utils/router/ValueTimeLine.h
@@ -20,18 +20,21 @@
 //
 //---------------------------------------------------------------------------//
 // $Log$
+// Revision 1.3  2004/01/26 07:17:49  dkrajzew
+// "specialisation" of the time line to handle interpolation added
+//
 // Revision 1.2  2003/02/07 10:52:57  dkrajzew
 // updated
 //
-//
-
-
 /* =========================================================================
  * included modules
  * ======================================================================= */
+#include <vector>
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
+
 
 /* =========================================================================
  * class definitions
@@ -40,13 +43,14 @@
  * @class ValueTimeLine
  * A time line being a list of time period with assigned values.
  */
+template<typename T>
 class ValueTimeLine {
 public:
     /// Definition of a time period
-    typedef std::pair<long, long> TimeRange;
+    typedef std::pair<unsigned int, unsigned int> TimeRange;
 
     /// Definition of a time range with an assigned value
-    typedef std::pair<TimeRange, double> ValuedTimeRange;
+    typedef std::pair<TimeRange, T> ValuedTimeRange;
 
     /// A list of time periods having which have values assigned
     typedef std::vector<ValuedTimeRange> TimedValueVector;
@@ -59,13 +63,13 @@ public:
     ~ValueTimeLine();
 
     /// Adds a time period together with a value
-    void addValue(long begin, long end, float value);
+    void addValue(unsigned int begin, unsigned int end, T value);
 
     /// Adds a time period together with a value
-    void addValue(TimeRange range, float value);
+    void addValue(TimeRange range, T value);
 
     /// Returns the value for the given time
-    float getValue(long time) const;
+    T getValue(unsigned int time) const;
 
     /// Returns the number of known periods
     size_t noDefinitions() const;
@@ -73,36 +77,38 @@ public:
     /// Returns the time period description at the given position // !!! should not be public
     const TimeRange &getRangeAtPosition(size_t pos) const;
 
+    /// returns the information wehther the values for the given time are known
+    bool describesTime(unsigned int time) const;
+
 private:
     /// Searches for the range when a time point is given
     class range_finder {
     public:
         /** constructor */
-        explicit range_finder(long time) : _time(time) { }
+        explicit range_finder(unsigned int time)
+            : myTime(time) { }
 
         /** the comparing function */
         bool operator() (ValuedTimeRange vrange) {
             TimeRange range = vrange.first;
-            return range.first<=_time && range.second>=_time;
+            return range.first<=myTime && range.second>=myTime;
         }
 
     private:
         /// The time to search for
-        long _time;
+        unsigned int myTime;
 
     };
 
 private:
     /// The list of time periods (with values)
-    TimedValueVector _values;
+    TimedValueVector myValues;
+
 };
 
-
+#include "ValueTimeLine.cpp"
 
 /**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
-//#ifndef DISABLE_INLINE
-//#include "ValueTimeLine.icc"
-//#endif
 
 #endif
 
