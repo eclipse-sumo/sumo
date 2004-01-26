71f80821e4d0b7f06d4b3bf52df9c689b1597a96
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2004-01-26 07:22:46 +0000

    added a class theat allows to use csv-files more easily
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1620 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/utils/importio/CSVHelpers.cpp
@@ -0,0 +1,52 @@
+/***************************************************************************
+                          CVSHelpers.cpp
+			  Some helping functions to read csv-files
+                             -------------------
+    project              : SUMO
+    begin                : Thu, 22 Jan 2004
+    copyright            : (C) 2004 by DLR/IVF http://ivf.dlr.de/
+    author               : Daniel Krajzewicz
+    email                : Daniel.Krajzewicz@dlr.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+namespace
+{
+    const char rcsid[] =
+    "$Id$";
+}
+// $Log$
+// Revision 1.1  2004/01/26 07:22:46  dkrajzew
+// added a class theat allows to use csv-files more easily
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#include "CSVHelpers.h"
+#include "LineHandler.h"
+#include "LineReader.h"
+
+
+/* =========================================================================
+ * method definitions
+ * ======================================================================= */
+void 
+CSVHelpers::runParser(LineHandler &lh, const std::string &file)
+{
+    LineReader lr(file);
+    lr.readAll(lh);
+}
+
+/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
+
+// Local Variables:
+// mode:C++
+// End:
---
+++ sumo/src/utils/importio/CSVHelpers.h
@@ -0,0 +1,57 @@
+#ifndef CSVHelpers_h
+#define CSVHelpers_h
+/***************************************************************************
+                          CSVHelpers.h
+			  Some helping functions to read csv-files
+                             -------------------
+    project              : SUMO
+    begin                : Fri, 19 Jul 2004
+    copyright            : (C) 2004 by DLR/IVF http://ivf.dlr.de/
+    author               : Daniel Krajzewicz
+    email                : Daniel.Krajzewicz@dlr.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+// $Log$
+// Revision 1.1  2004/01/26 07:22:46  dkrajzew
+// added a class theat allows to use csv-files more easily
+//
+//
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#include <string>
+
+
+/* =========================================================================
+ * class declarations
+ * ======================================================================= */
+class LineHandler;
+
+
+/* =========================================================================
+ * class definitions
+ * ======================================================================= */
+/**
+ * @class CSVHelpers
+ * This class offers helper methods to read values from csv-files
+ */
+class CSVHelpers {
+public:
+    static void runParser(LineHandler &lh, const std::string &file);
+};
+
+/**************** DO NOT DECLARE ANYTHING AFTER THE INCLUDE ****************/
+
+#endif
+
+// Local Variables:
+// mode:C++
+// End:
