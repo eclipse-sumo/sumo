c68e09583f7f609fff93aa2ae0ba785deb3ebc0e
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2004-01-27 10:29:48 +0000

    made the instance pool being completely described within the .h-file
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1670 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/InstancePool.h
+++ sumo/src/utils/common/InstancePool.h
@@ -2,27 +2,47 @@
 #define InstancePool_h
 
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
 template<class T>
 class InstancePool {
 public:
-    InstancePool(bool deleteOnQuit);
-    ~InstancePool();
-    T* getFreeInstance();
-    void addFreeInstance(T *instance);
-    void addFreeInstances(const std::vector<T*> instances);
+    InstancePool(bool deleteOnQuit) { }
+    ~InstancePool() {
+	if(myDeleteOnQuit) {
+	    typedef std::vector<T*>::iterator It;
+	    for(It i=myFreeInstances.begin(); i!=myFreeInstances.end(); i++) {
+		delete *i;
+	    }
+	}
+    }
+
+    T* getFreeInstance() {
+	if(myFreeInstances.size()==0) {
+	    return 0;
+	} else {
+	    T *instance = myFreeInstances.back();
+	    myFreeInstances.pop_back();
+	    return instance;
+	}
+    }
+
+    void addFreeInstance(T *instance) {
+	myFreeInstances.push_back(instance);
+    }
+
+    void addFreeInstances(const std::vector<T*> instances) {
+	std::copy(instances.begin(), instances.end(),
+		  std::back_inserter(myFreeInstances));
+    }
+
 
 private:
     std::vector<T*> myFreeInstances;
     bool myDeleteOnQuit;
 
 };
-
-#ifndef MSVC
-#ifndef EXTERNAL_TEMPLATE_DEFINITION
-#include "InstancePool.cpp"
-#endif
-#endif
 
 
 #endif
