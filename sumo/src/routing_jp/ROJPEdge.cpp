3d25fb3bd6a539ed463b7e39477671ea73d6d2b1
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2004-02-02 16:21:06 +0000

    new default behaviour on incomplete edge propabilities
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1687 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/routing_jp/ROJPEdge.cpp
+++ sumo/src/routing_jp/ROJPEdge.cpp
@@ -23,6 +23,9 @@ namespace
     "$Id$";
 }
 // $Log$
+// Revision 1.3  2004/02/02 16:21:06  dkrajzew
+// new default behaviour on incomplete edge propabilities
+//
 // Revision 1.2  2004/01/26 09:57:14  dkrajzew
 // setting of the default direction percentages corrected
 //
@@ -92,18 +95,38 @@ ROJPEdge::addFollowerPropability(ROJPEdge *follower, unsigned int begTime,
 ROJPEdge *
 ROJPEdge::chooseNext(unsigned int time) const
 {
+    if(myFollowingEdges.size()==0) {
+        return 0;
+    }
     // check whether any definition exists
     FollowerUsageCont::const_iterator i;
-    // check whether a complete definition for the given time exitsts
+    // check how many edges are defined for the given time step
+    size_t noDescs = 0;
     bool hasDescription = true;
     for(i=myFollowingDefs.begin(); i!=myFollowingDefs.end()&&hasDescription; i++) {
-        if(!(*i).second->describesTime(time)) {
-            hasDescription = false;
+        if((*i).second->describesTime(time)) {
+            noDescs++;
         }
     }
+    // get a random number between zero and one
     double chosen = (double)rand() /
         ( static_cast<double>(RAND_MAX) + 1);
-    if(hasDescription) {
+    // if no description is given for the current time
+    if(noDescs==0) {
+        //  use the defaults
+        std::vector<float>::const_iterator j;
+        size_t pos = 0;
+        for(j=myParsedTurnings.begin(); j!=myParsedTurnings.end(); j++) {
+            chosen = chosen - (*j);
+            if(chosen<=0) {
+                return static_cast<ROJPEdge*>(myFollowingEdges[pos]);
+            }
+            pos++;
+        }
+        return static_cast<ROJPEdge*>(myFollowingEdges[0]);
+    }
+    // if the propabilities are given for all following edges
+    if(noDescs==myFollowingEdges.size()) {
         // use the loaded definition
             // choose the appropriate one from the list
         for(i=myFollowingDefs.begin(); i!=myFollowingDefs.end(); i++) {
@@ -114,15 +137,23 @@ ROJPEdge::chooseNext(unsigned int time) const
         }
         return (*myFollowingDefs.begin()).first;
     } else {
-        std::vector<float>::const_iterator j;
-        size_t pos = 0;
-        for(j=myParsedTurnings.begin(); j!=myParsedTurnings.end(); j++) {
-            chosen = chosen - (*j);
-            if(chosen<=0) {
-                return static_cast<ROJPEdge*>(myFollowingEdges[pos]);
+        // ok, at least one description is missing
+        //  the missing descriptions will be treated as zero
+        bool allZero = true; // we won't be as stupid as our users may be...
+        while(allZero) { // ... we may loop several times, but not forever
+            allZero = false;
+            for(i=myFollowingDefs.begin(); i!=myFollowingDefs.end(); i++) {
+                if((*i).second->describesTime(time)) {
+                    chosen = chosen - (*i).second->getValue(time);
+                    if(chosen<=0) {
+                        return (*i).first;
+                    }
+                    allZero = allZero |
+                        ((*i).second->getValue(time)!=0);
+                }
             }
         }
-        return static_cast<ROJPEdge*>(myFollowingEdges[0]);
+        return (*myFollowingDefs.begin()).first;
     }
 }
 
