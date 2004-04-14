e0fc572790b9c3274bcdce0c128a90d8f7979237
Christian Roessel <roessel@zpr.uni-koeln.de>
2004-04-14 14:13:26 +0000

    Rewrite of addValue(): addition to the container myValues will keep the container sorted.
    New method getSearchStateAndValue(): Binary-search for a
    ValuedTimeRange. Returns a T and a flag indicating the search-success.
    
    New Predicates TimeRangeLess: used in searching the container myValues.
    
    
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1850 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/router/ValueTimeLine.h
+++ sumo/src/utils/router/ValueTimeLine.h
@@ -6,7 +6,8 @@
 //                           -------------------
 //  project              : SUMO - Simulation of Urban MObility
 //  begin                : Sept 2002
-//  copyright            : (C) 2002 by Daniel Krajzewicz
+//  copyright            : (C) 2002, 2003, 2004 by Daniel Krajzewicz &
+//                       : Christian Roessel <christian.roessel@dlr.de>
 //  organisation         : IVF/DLR http://ivf.dlr.de
 //  email                : Daniel.Krajzewicz@dlr.de
 //---------------------------------------------------------------------------//
@@ -19,28 +20,20 @@
 //   (at your option) any later version.
 //
 //---------------------------------------------------------------------------//
-// $Log$
-// Revision 1.5  2004/03/02 15:55:44  roessel
-// Added an assert to avoid subtle bug.
-//
-// Revision 1.4  2004/02/06 08:47:11  dkrajzew
-// debugged things that came up during the compilation under linux
-//
-// Revision 1.3  2004/01/26 07:17:49  dkrajzew
-// "specialisation" of the time line to handle interpolation added
-//
-// Revision 1.2  2003/02/07 10:52:57  dkrajzew
-// updated
-//
+
+/// @version $Id$
+
 /* =========================================================================
  * included modules
  * ======================================================================= */
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <functional>
+#include <utility>
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif // HAVE_CONFIG_H
 
 
@@ -49,7 +42,11 @@
  * ======================================================================= */
 /**
  * @class ValueTimeLine
- * A time line being a list of time period with assigned values.
+ *
+ * A time line being a sorted container of time-ranges with assigned
+ * values. The container is sorted after the the first value of the
+ * time-range while being filled. Ranges with equivalent first values
+ * are sorted in the order in which they were inserted.
  */
 template<typename T>
 class ValueTimeLine {
@@ -64,7 +61,11 @@ public:
     typedef std::vector<ValuedTimeRange> TimedValueVector;
 
     typedef typename TimedValueVector::const_iterator CTVVIt;
-
+    typedef typename TimedValueVector::iterator TVVIt;
+    
+    /// Return-type of a search, indicating if search was successful.
+    typedef std::pair< bool, T > SearchResult;
+    
 public:
     /// Constructor
     ValueTimeLine() { }
@@ -72,17 +73,32 @@ public:
     /// Destructor
     ~ValueTimeLine() { }
 
-    /// Adds a time period together with a value
+    /// Adds a ValuedTimeRange into the classes container. This is
+    /// just a wrapper to addValue(TimeRange range, T value). Make
+    /// sure that begin < end.
+    ///
+    /// @param begin Begin of TimeRange.
+    /// @param end End of TimeRange.
+    /// @param value Value to store.
+    ///
     void addValue(unsigned int begin, unsigned int end, T value)
 	{
 	    addValue(TimeRange(begin, end), value);
 	}
 
-    /// Adds a time period together with a value
+    /// Adds a ValuedTimeRange into the classes container. The
+    /// container is sorted after the begin of the TimeRange.
+    ///
+    /// @param range TimeRange. Assure that begin < end.
+    /// @param value Value to store.
+    ///
     void addValue(TimeRange range, T value)
 	{
-	    ValuedTimeRange valued(range, value);
-	    myValues.push_back(valued);
+        assert( range.first < range.second );
+        unsigned intervalStart = range.first;
+        TVVIt insertPos = std::upper_bound( myValues.begin(), myValues.end(),
+                                            intervalStart, TimeRangeLess() );
+	    myValues.insert( insertPos, std::make_pair( range, value ) );
 	}
 
     /// Returns the value for the given time
@@ -97,13 +113,49 @@ public:
 	    return (*i).second;
 	}
 
+    /// Searches for a TimeRange in it's TimedValueVector myValues
+    /// that contains the parameter time. If there are several
+    /// possible TimeRanges, one of them will be returned, depending on
+    /// the boundary conditions. But in this case, fix the input!
+    ///
+    /// @param time Search parameter.
+    ///
+    /// @return A pair SearchResult, indicating if the search was
+    /// successful (first). In this case the associated value is
+    /// returned as "second". In the other case, the value of the last
+    /// element is returned.
+    ///
+    SearchResult getSearchStateAndValue( unsigned time ) const
+        {
+            assert( myValues.size() > 0 );
+            CTVVIt retIt = std::lower_bound( myValues.begin(), myValues.end(),
+                                             time, TimeRangeLess() );
+
+            if ( time < retIt->first.first ) {
+                if ( retIt == myValues.begin() ) {
+                    return std::make_pair( false, retIt->second );
+                }
+                --retIt;
+            }
+            if ( time <= retIt->first.second ) {
+                return std::make_pair( true, retIt->second );
+            }
+            if ( retIt == myValues.end() &&
+                 time <= (--retIt)->first.second ) {
+                return std::make_pair( true, retIt->second ); 
+            }
+            retIt = myValues.end() - 1;
+            return std::make_pair( false, retIt->second );
+        }
+    
     /// Returns the number of known periods
     size_t noDefinitions() const
 	{
 	    return myValues.size();
 	}
 
-    /// Returns the time period description at the given position // !!! should not be public
+    /// Returns the time period description at the given position //
+    /// !!! should not be public
     const TimeRange &getRangeAtPosition(size_t pos) const
 	{
         assert( pos < myValues.size() );
@@ -138,6 +190,29 @@ private:
 
     };
 
+    
+    /// Predicates for (binary) ordering and searching in a
+    /// TimedValueVector. The first value of TimeRange is used for
+    /// ordering and searching. If neccessary, check for the second
+    /// one yourself.
+    struct TimeRangeLess :
+        std::binary_function< unsigned, ValuedTimeRange, bool >
+    {
+        /// predicate to use with upper_bound.
+        bool operator()( const unsigned searchTime,
+                         const ValuedTimeRange& valuedRange ) const
+            {
+                return searchTime < valuedRange.first.first;
+            }
+        /// predicate to use with lower_bound
+        bool operator()( const ValuedTimeRange& valuedRange,
+                         const unsigned searchTime ) const
+            {
+                return searchTime > valuedRange.first.first;
+            }        
+    };
+    
+        
 private:
     /// The list of time periods (with values)
     TimedValueVector myValues;
