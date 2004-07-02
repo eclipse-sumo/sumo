f232322af0554e3cd3a5f7801bbf997a57abc49d
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2004-07-02 09:51:14 +0000

    error messages are passed to MsgHandler
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@1946 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/od2trips_main.cpp
+++ sumo/src/od2trips_main.cpp
@@ -1,12 +1,12 @@
 /***************************************************************************
                          od2trips_main.cpp
 
-	 The main procedure for converting OD an NET files to trip tables
+     The main procedure for converting OD an NET files to trip tables
 
-    project              : SUMO		 :
-	subproject           : OD2TRIPS
+    project              : SUMO      :
+    subproject           : OD2TRIPS
     begin                : Thu, 12 September 2002
-	modified			 : Thu, 25 March 2003, INVENT purposes
+    modified             : Thu, 25 March 2003, INVENT purposes
     copyright            : (C) 2002 by DLR/IVF http://ivf.dlr.de/
     authors              : Daniel Krajzewicz, Peter Mieth
     emails               : Daniel.Krajzewicz@dlr.de, Peter.Mieth@dlr.de
@@ -58,16 +58,16 @@ using namespace std;
 void
 fillOptions(OptionsCont &oc)
 {
-	oc.doRegister("configuration-file", 'c', new Option_FileName());
+    oc.doRegister("configuration-file", 'c', new Option_FileName());
     // register the file i/o options
     oc.doRegister("net-file", 'n', new Option_FileName());
-	oc.addSynonyme("net-file", "net");
+    oc.addSynonyme("net-file", "net");
     oc.doRegister("od-file", 'd', new Option_FileName());
-	oc.addSynonyme("od-file", "od");
+    oc.addSynonyme("od-file", "od");
     oc.doRegister("output-file", 'o', new Option_FileName());
-	oc.addSynonyme("output-file", "output");
-	oc.doRegister("od-path", 'i', new Option_FileName());
-	oc.addSynonyme("od-path", "path");
+    oc.addSynonyme("output-file", "output");
+    oc.doRegister("od-path", 'i', new Option_FileName());
+    oc.addSynonyme("od-path", "path");
     // register the report options
     oc.doRegister("verbose", 'v', new Option_Bool(false));
     oc.doRegister("suppress-warnings", 'W', new Option_Bool(false));
@@ -128,9 +128,9 @@ loadDistricts(OptionsCont &oc)
     // build the xml-parser and handler
     ODDistrictHandler handler(*ret);
     handler.setFileName(file);
-	XMLHelpers::runParser(handler, file);
+    XMLHelpers::runParser(handler, file);
     // check whether the loading was ok
-	if(ret->size()==0||MsgHandler::getErrorInstance()->wasInformed()) {
+    if(ret->size()==0||MsgHandler::getErrorInstance()->wasInformed()) {
         delete ret;
         return 0;
     }
@@ -142,12 +142,12 @@ loadDistricts(OptionsCont &oc)
 int
 cmpfun(long int a, long int b)
 {
-	if (a > b)
-		return 1;
-	else if (a < b)
-		return -1;
-	else
-		return 0;
+    if (a > b)
+        return 1;
+    else if (a < b)
+        return -1;
+    else
+        return 0;
 }
 
 
@@ -160,7 +160,7 @@ getFMatType(const std::string &file)
     if (file.find(".fma") != string::npos) {
         return 2; // old fma format
     }
-    cerr << "Error: Wrong od-file in *.cfg!" << endl;
+    MsgHandler::getErrorInstance()->inform("Error: Wrong od-file in *.cfg!");
     throw ProcessError();
 }
 
@@ -184,154 +184,154 @@ main(int argc, char **argv)
         srand(oc.getInt("srand"));
         string OD_filename = oc.getString("d");
         //
-		int fmatype = getFMatType(OD_filename);
-		string OD_outfile = oc.getString("o");
-		string OD_path = oc.getString("i");
+        int fmatype = getFMatType(OD_filename);
+        string OD_outfile = oc.getString("o");
+        string OD_path = oc.getString("i");
         std::vector<ODContent> content;
         std::vector<std::string> infiles;
-		// reads out some metadata from *.inp
-		if( fmatype == 1) {
+        // reads out some metadata from *.inp
+        if( fmatype == 1) {
             MsgHandler::getMessageInstance()->inform(
                 "**** VISSIM input data format **** ");
-			ODInpread (OD_filename, infiles, content/*,&maxfiles*/);
-		} else {
+            ODInpread (OD_filename, infiles, content/*,&maxfiles*/);
+        } else {
             MsgHandler::getMessageInstance()->inform(
-				"**** Original input data format **** ");
-		}
-		long maxele=50000; // initial number of ODs, finally derived from OD-inputfile
-		long int total_cars=0;  // total number of cars, finally derived from OD-inputfile
-		long int i, j;
-		float factor;
-		long int  start, finish;
-		bool ok_begin = oc.isDefault("begin");
-		bool ok_end = oc.isDefault("end");
-		// load districts
-		ODDistrictCont *districts = loadDistricts(oc);
-		int od_next=0;
-		// define dimensions
-		long int max_cars=3000000; // maximum number of cars
-		// temporary storage space
-		long int *when_all = new long int [max_cars];
-		string *source = new string [max_cars];
-		string *sink = 	new string [max_cars];
-		int *cartype = 	new int [max_cars];
-		long int *old_index= new long int [max_cars]; // holds the old index after sorting
-		int index, tmpk, k;
-		long int begin, end, period;
-		bool ini;
-		double tmprand, maxrand;
-		float scale, rest;
-		ini=true; // initialize random numbers with time, only first call
-		for (int ifile=0;ifile<infiles.size();ifile++) { // proceed for all *.fma files
-			// OD list
-			vector<OD_IN> od_in;
-			vector<OD_IN>::iterator it1;
-			if (fmatype == 1) {
-				OD_filename = OD_path + infiles[ifile];
-				MsgHandler::getMessageInstance()->inform(
-					"Processing " + OD_filename);
-			// Reads the OD Matrix
-			ODPtvread ( OD_filename, od_in, &maxele, &total_cars, &start, &finish, &factor );
-			// use settings if provided
-			} else
-				ODread ( OD_filename, od_in, &maxele, &total_cars, &start, &finish, &factor );
-			if(ok_begin) {
-				if(oc.getLong("begin") > start) start = oc.getLong("begin");
-			}
-			if(ok_end) {
-				if(oc.getLong("end") < finish) finish = oc.getLong("end");
-			}
-			// check permitted range
-			if( (start>finish) || (start<0) || (finish>86400) ) {
-				MsgHandler::getErrorInstance()->inform(
-				    "Wrong time range (begin/end)");
-				throw ProcessError();
-			}
-			period = finish - start;
-			// scale input
-			scale = oc.getFloat("scale");
-			scale = scale / factor;
-			total_cars=0;
-			for(it1=od_in.begin(); it1!=od_in.end(); it1++) {
-				rest = fmod ( double((*it1).how_many), double(scale)) / double(scale);
-				if(rest <= 0.5)
-					(*it1).how_many = int(double((*it1).how_many) / scale);
-				else
-					(*it1).how_many = int(double((*it1).how_many) / scale)+1;
-			}
-			// recompute total_cars
-			total_cars = 0;
-			for(it1=od_in.begin(); it1!=od_in.end(); it1++) {
-				total_cars += (*it1).how_many;
-			}
-			if(fmatype == 2) {
+                "**** Original input data format **** ");
+        }
+        long maxele=50000; // initial number of ODs, finally derived from OD-inputfile
+        long int total_cars=0;  // total number of cars, finally derived from OD-inputfile
+        long int i, j;
+        float factor;
+        long int  start, finish;
+        bool ok_begin = oc.isDefault("begin");
+        bool ok_end = oc.isDefault("end");
+        // load districts
+        ODDistrictCont *districts = loadDistricts(oc);
+        int od_next=0;
+        // define dimensions
+        long int max_cars=3000000; // maximum number of cars
+        // temporary storage space
+        long int *when_all = new long int [max_cars];
+        string *source = new string [max_cars];
+        string *sink =  new string [max_cars];
+        int *cartype =  new int [max_cars];
+        long int *old_index= new long int [max_cars]; // holds the old index after sorting
+        int index, tmpk, k;
+        long int begin, end, period;
+        bool ini;
+        double tmprand, maxrand;
+        float scale, rest;
+        ini=true; // initialize random numbers with time, only first call
+        for (int ifile=0;ifile<infiles.size();ifile++) { // proceed for all *.fma files
+            // OD list
+            vector<OD_IN> od_in;
+            vector<OD_IN>::iterator it1;
+            if (fmatype == 1) {
+                OD_filename = OD_path + infiles[ifile];
+                MsgHandler::getMessageInstance()->inform(
+                    "Processing " + OD_filename);
+            // Reads the OD Matrix
+            ODPtvread ( OD_filename, od_in, &maxele, &total_cars, &start, &finish, &factor );
+            // use settings if provided
+            } else
+                ODread ( OD_filename, od_in, &maxele, &total_cars, &start, &finish, &factor );
+            if(ok_begin) {
+                if(oc.getLong("begin") > start) start = oc.getLong("begin");
+            }
+            if(ok_end) {
+                if(oc.getLong("end") < finish) finish = oc.getLong("end");
+            }
+            // check permitted range
+            if( (start>finish) || (start<0) || (finish>86400) ) {
+                MsgHandler::getErrorInstance()->inform(
+                    "Wrong time range (begin/end)");
+                throw ProcessError();
+            }
+            period = finish - start;
+            // scale input
+            scale = oc.getFloat("scale");
+            scale = scale / factor;
+            total_cars=0;
+            for(it1=od_in.begin(); it1!=od_in.end(); it1++) {
+                rest = fmod ( double((*it1).how_many), double(scale)) / double(scale);
+                if(rest <= 0.5)
+                    (*it1).how_many = int(double((*it1).how_many) / scale);
+                else
+                    (*it1).how_many = int(double((*it1).how_many) / scale)+1;
+            }
+            // recompute total_cars
+            total_cars = 0;
+            for(it1=od_in.begin(); it1!=od_in.end(); it1++) {
+                total_cars += (*it1).how_many;
+            }
+            if(fmatype == 2) {
                 MsgHandler::getMessageInstance()->inform(
                     string("Total number of cars computed: ")
                     + toString<long int>(total_cars));
-			}
-			int *when = new int [period];
-			int *elements = new int [period];
-			// temporary variables with initial values
-			begin=0;
-			end=0;
-			for(i=0;i<period;i++) *(when+i)=0;
-			// loop over all ODs
-			for(i=0;i<maxele;i++) {
-				//OD_IN tmp = od_in[i];
-				end = od_in[i].how_many;
-				if(end!=0) {
-					// Gets random numbers which are the release times for the cars
-					end = Get_rand( end, period, start, elements, when, ini);
-					ini = false;
-					end = begin+end;
-					for(j=begin;j<end;j++)
-					{
-						*(old_index+j+od_next)=j+od_next; // initial order for index
-						// find dsource, dsink
-						//string pp=od_in[i].from;
-						//string p2=od_in[i].to;
-						*(source+j+od_next) = od_in[i].from;//
-						*(sink+j+od_next) = od_in[i].to;//
-						*(when_all+j+od_next) = *(when+j-begin);
-						// determine car type for VISSIM format
-						if (fmatype == 1) {
-							maxrand=0.;
-							for (k=0;k<content[ifile].max;k++) {
-								tmprand= rand() * content[ifile].fraction[k];
-									if (maxrand<tmprand) {
-										maxrand=tmprand;
-										tmpk=k;
-									}
-							}
-							*(cartype+j+od_next) = content[ifile].cartype[tmpk];
-						} else *(cartype+j+od_next) = 0; // cartype=0 for old format
-					}
-					begin=end;
-				}
-			}
-			total_cars = begin;
-			od_next = od_next + total_cars;
-			delete [] when; delete [] elements;
-		}
-		total_cars = od_next;
+            }
+            int *when = new int [period];
+            int *elements = new int [period];
+            // temporary variables with initial values
+            begin=0;
+            end=0;
+            for(i=0;i<period;i++) *(when+i)=0;
+            // loop over all ODs
+            for(i=0;i<maxele;i++) {
+                //OD_IN tmp = od_in[i];
+                end = od_in[i].how_many;
+                if(end!=0) {
+                    // Gets random numbers which are the release times for the cars
+                    end = Get_rand( end, period, start, elements, when, ini);
+                    ini = false;
+                    end = begin+end;
+                    for(j=begin;j<end;j++)
+                    {
+                        *(old_index+j+od_next)=j+od_next; // initial order for index
+                        // find dsource, dsink
+                        //string pp=od_in[i].from;
+                        //string p2=od_in[i].to;
+                        *(source+j+od_next) = od_in[i].from;//
+                        *(sink+j+od_next) = od_in[i].to;//
+                        *(when_all+j+od_next) = *(when+j-begin);
+                        // determine car type for VISSIM format
+                        if (fmatype == 1) {
+                            maxrand=0.;
+                            for (k=0;k<content[ifile].max;k++) {
+                                tmprand= rand() * content[ifile].fraction[k];
+                                    if (maxrand<tmprand) {
+                                        maxrand=tmprand;
+                                        tmpk=k;
+                                    }
+                            }
+                            *(cartype+j+od_next) = content[ifile].cartype[tmpk];
+                        } else *(cartype+j+od_next) = 0; // cartype=0 for old format
+                    }
+                    begin=end;
+                }
+            }
+            total_cars = begin;
+            od_next = od_next + total_cars;
+            delete [] when; delete [] elements;
+        }
+        total_cars = od_next;
         MsgHandler::getMessageInstance()->inform(
             string("Total number of released cars: ") + toString<int>(total_cars));
-		// sorts the time
+        // sorts the time
         MsgHandler::getMessageInstance()->inform("Sorting ...");
-		IndexSort(when_all, old_index, cmpfun, total_cars);
-		std::vector<OD_OUT> source_sink(max_cars); // output OD data
-		for(i=0;i<total_cars;i++)
-		{
-			index = old_index[i];
-			source_sink[i].from = *(source+index);
-			source_sink[i].to = *(sink+index);
-			source_sink[i].time = *(when_all+i);
-			source_sink[i].type = *(cartype+index);
-		}
-		// writes output to file
-		ODWrite( OD_outfile, source_sink, total_cars, *districts );
-		delete [] source; delete [] sink;
-		delete [] when_all; delete [] cartype;
+        IndexSort(when_all, old_index, cmpfun, total_cars);
+        std::vector<OD_OUT> source_sink(max_cars); // output OD data
+        for(i=0;i<total_cars;i++)
+        {
+            index = old_index[i];
+            source_sink[i].from = *(source+index);
+            source_sink[i].to = *(sink+index);
+            source_sink[i].time = *(when_all+i);
+            source_sink[i].type = *(cartype+index);
+        }
+        // writes output to file
+        ODWrite( OD_outfile, source_sink, total_cars, *districts );
+        delete [] source; delete [] sink;
+        delete [] when_all; delete [] cartype;
         MsgHandler::getMessageInstance()->inform("Success.");
     } catch (...) {
         ret = 1;
