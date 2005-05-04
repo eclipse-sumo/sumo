2e7d540eca1e2152cd4b55da2fce0f03b96f59a3
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2005-05-04 09:15:15 +0000

    added a default value retrieval
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@2215 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/gui/drawer/GUIBaseColorer.h
+++ sumo/src/utils/gui/drawer/GUIBaseColorer.h
@@ -1,12 +1,21 @@
 #ifndef GUIBaseColorer_h
 #define GUIBaseColorer_h
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <GL/gl.h>
+
 template<class _T>
 class GUIBaseColorer {
 public:
-    GUIBaseColorer() { }
-    virtual ~GUIBaseColorer() { }
-    virtual void setGlColor(const _T& i) const = 0;
+	GUIBaseColorer() { }
+	virtual ~GUIBaseColorer() { }
+	virtual void setGlColor(const _T& i) const = 0;
+    virtual void setGlColor(double val) const {
+        glColor3d(val, val, val);
+    }
 };
 
 
