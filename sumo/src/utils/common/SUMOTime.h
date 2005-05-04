81c4408420d7d56156809946d9921b26fa709ebc
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2005-05-04 08:58:59 +0000

    level 3 warnings removed; a certain SUMOTime time description added; simulation speed-up by avoiding multiplication with 1
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@2211 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/SUMOTime.h
+++ sumo/src/utils/common/SUMOTime.h
@@ -1,7 +1,26 @@
 #ifndef SUMOTime_h
 #define SUMOTime_h
+/* =========================================================================
+ * compiler pragmas
+ * ======================================================================= */
+#pragma warning(disable: 4786)
 
-typedef size_t SUMOTime;
+
+
+typedef int SUMOTime;
+
+// x*deltaT
+#define SPEED2DIST(x) x
+
+// x/deltaT
+#define DIST2SPEED(x) x
+
+// x*deltaT*deltaT
+#define ACCEL2DIST(x) x
+
+// x*deltaT
+#define ACCEL2SPEED(x) x
+
 
 //!!!
 typedef unsigned long ExtSUMOTime;
