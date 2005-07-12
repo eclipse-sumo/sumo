be48fd2dee5808e4523997ddce21f634ed947a56
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2005-07-12 12:44:49 +0000

    build number output added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@2270 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/utils/common/DevHelper.h
@@ -0,0 +1,19 @@
+#ifndef DevHelper_h
+#define DevHelper_h
+
+/* =========================================================================
+ * compiler pragmas
+ * ======================================================================= */
+#pragma warning(disable: 4786)
+
+
+class OptionsCont;
+
+
+
+class DevHelper {
+public:
+    static void insertDevOptions(OptionsCont &options);
+};
+
+#endif
