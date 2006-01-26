512675f9a664727523574a848500186cc7b6f677
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2006-01-26 08:39:57 +0000

    made the abstract router usable within microsim and routers
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@2502 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/helpers/SUMOAbstractRouter.h
+++ sumo/src/utils/helpers/SUMOAbstractRouter.h
@@ -1,12 +1,12 @@
-#ifndef ROAbstractRouter_h
-#define ROAbstractRouter_h
+#ifndef SUMOAbstractRouter_h
+#define SUMOAbstractRouter_h
 //---------------------------------------------------------------------------//
-//                        ROAbstractRouter.h -
+//                        SUMOAbstractRouter.h -
 //  The dijkstra-router
 //                           -------------------
 //  project              : SUMO - Simulation of Urban MObility
-//  begin                : Sept 2002
-//  copyright            : (C) 2002 by Daniel Krajzewicz
+//  begin                : 25.Jan 2006
+//  copyright            : (C) 2006 by Daniel Krajzewicz
 //  organisation         : IVF/DLR http://ivf.dlr.de
 //  email                : Daniel.Krajzewicz@dlr.de
 //---------------------------------------------------------------------------//
@@ -20,35 +20,9 @@
 //
 //---------------------------------------------------------------------------//
 // $Log$
-// Revision 1.7  2006/01/26 08:39:57  dkrajzew
+// Revision 1.1  2006/01/26 08:39:57  dkrajzew
 // made the abstract router usable within microsim and routers
 //
-// Revision 1.6  2005/10/07 11:42:15  dkrajzew
-// THIRD LARGE CODE RECHECK: patched problems on Linux/Windows configs
-//
-// Revision 1.5  2005/09/23 06:04:36  dkrajzew
-// SECOND LARGE CODE RECHECK: converted doubles and floats to SUMOReal
-//
-// Revision 1.4  2005/09/15 12:05:11  dkrajzew
-// LARGE CODE RECHECK
-//
-// Revision 1.3  2005/05/04 08:46:09  dkrajzew
-// level 3 warnings removed; a certain SUMOTime time description added
-//
-// Revision 1.2  2004/11/23 10:25:51  dkrajzew
-// debugging
-//
-// Revision 1.1  2004/01/26 08:02:27  dkrajzew
-// loaders and route-def types are now renamed in an senseful way;
-//  further changes in order to make both new routers work; documentation added
-//
-// --------------------------------------------
-// Revision 1.4  2003/06/18 11:36:50  dkrajzew
-// a new interface which allows to choose whether to stop after a route could
-//  not be computed or not; not very sphisticated, in fact
-//
-// Revision 1.3  2003/02/07 10:45:07  dkrajzew
-// updated
 //
 /* =========================================================================
  * compiler pragmas
@@ -67,27 +41,55 @@
 #endif
 #endif // HAVE_CONFIG_H
 
-#include <utils/helpers/SUMOAbstractRouter.h>
 #include <string>
-#include <functional>
 #include <vector>
-#include <set>
-#include <utils/common/InstancePool.h>
-#include "ROEdgeVector.h"
-#include "ROEdge.h"
-
-
-/* =========================================================================
- * class declarations
- * ======================================================================= */
-class RONet;
-class ROVehicle;
+#include <utils/common/SUMOTime.h>
 
 
 /* =========================================================================
  * class definitions
  * ======================================================================= */
-typedef SUMOAbstractRouter<ROEdge,ROVehicle> ROAbstractRouter;
+/**
+ * @class SUMOAbstractRouter
+ * The interface for routing the vehicles over the network.
+ */
+template<class E, class V>
+class SUMOAbstractRouter {
+public:
+	/**
+	 * @class ROAbstractEdgeEffortRetriever
+	 * This interface has to be implemented in order to get the real efforts of edges
+	 */
+	class ROAbstractEdgeEffortRetriever {
+	public:
+		/// Constructor
+		ROAbstractEdgeEffortRetriever() { }
+
+		/// Destructor
+		virtual ~ROAbstractEdgeEffortRetriever() { }
+
+		/// This function should return the effort to use
+		virtual SUMOReal getEffort(SUMOTime time, const E * const edge,
+            SUMOReal dist) = 0;
+
+		/// Returns the name of this retriever
+		virtual const std::string &getID() const = 0;
+
+	};
+
+public:
+    /// Constructor
+    SUMOAbstractRouter() { }
+
+    /// Destructor
+    virtual ~SUMOAbstractRouter() { }
+
+    /** @brief Builds the route between the given edges using the minimum afford at the given time
+        The definition of the afford depends on the wished routing scheme */
+    virtual void compute(const E *from, const E *to, const V * const vehicle,
+        SUMOTime time, std::vector<const E*> &into) = 0;
+
+};
 
 
 /**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
--- sumo/src/utils/helpers/SUMODijkstraRouter.h
+++ sumo/src/utils/helpers/SUMODijkstraRouter.h
@@ -20,6 +20,9 @@
 //
 //---------------------------------------------------------------------------//
 // $Log$
+// Revision 1.5  2006/01/26 08:39:57  dkrajzew
+// made the abstract router usable within microsim and routers
+//
 // Revision 1.4  2005/10/07 11:46:23  dkrajzew
 // THIRD LARGE CODE RECHECK: patched problems on Linux/Windows configs
 //
@@ -64,14 +67,8 @@
 #include <string>
 #include <utils/common/InstancePool.h>
 #include <utils/common/MsgHandler.h>
-
-
-/* =========================================================================
- * class declarations
- * ======================================================================= */
-class RONet;
-class ROEdgeCont;
-class ROEdge;
+#include <utils/common/StdDefs.h>
+#include "SUMOAbstractRouter.h"
 
 
 /* =========================================================================
@@ -79,14 +76,15 @@ class ROEdge;
  * ======================================================================= */
 /**
  * @class SUMODijkstraRouter
- * Lays the given route over the edges using the dijkstra algorithm
+ * @brief Lays the given route over the edges using the dijkstra algorithm
  */
-template<class E>
-class SUMODijkstraRouter {
+template<class E, class V>
+class SUMODijkstraRouter : public SUMOAbstractRouter<E, V> {
 public:
     /// Constructor
-    SUMODijkstraRouter(size_t noE)
-        : myNoE(noE), myReusableEdgeLists(true), myReusableEdgeInfoLists(true)
+    SUMODijkstraRouter(size_t noE, bool unbuildIsWarningOnly)
+        : myNoE(noE), myReusableEdgeLists(true), myReusableEdgeInfoLists(true),
+		myUnbuildIsWarningOnly(unbuildIsWarningOnly)
     { }
 
     /// Destructor
@@ -153,9 +151,10 @@ public:
 
     /** @brief Builds the route between the given edges using the minimum afford at the given time
         The definition of the afford depends on the wished routing scheme */
-    virtual std::deque<const E*> compute(const E *from, const E *to,
-        SUMOTime time, bool continueOnUnbuild) {
+    virtual void compute(const E *from, const E *to, const V * const vehicle,
+        SUMOTime time, std::vector<const E*> &into) {
 
+		// get structures to reuse
         std::vector<bool> *visited = myReusableEdgeLists.getFreeInstance();
         if(visited==0) {
             visited = new std::vector<bool>(myNoE, false);
@@ -169,19 +168,23 @@ public:
             storage = new EdgeInfoCont(myNoE);
         }
         storage->reset();
+
         // check the nodes
         if(from==0||to==0) {
-	  throw std::exception();
+			throw std::exception();
         }
+
+		// begin computation
         std::priority_queue<EdgeInfo*,
-	  std::vector<EdgeInfo*>,
+			std::vector<EdgeInfo*>,
             EdgeInfoByEffortComperator> frontierList;
-        // add begin node
+			// add begin node
 	    const E *actualKnot = from;
 	    if(from != 0) {
             EdgeInfo *ei = storage->add(actualKnot, 0, 0);
             frontierList.push(ei);
 	    }
+
         // loop
 	    while(!frontierList.empty()) {
             // use the node with the minimal length
@@ -190,11 +193,13 @@ public:
             frontierList.pop();
                 // check whether the destination node was already reached
             if(minEdge == to) {
-                std::deque<const E*> ret = buildPathFrom(minimumKnot);
+				buildPathFrom(minimumKnot, into);
                 clearTemporaryStorages(visited, storage);
-                return ret;
+				return;
+                //return ret;
             }
-            (*visited)[minEdge->getNumericalID()] = true; //minimumKnot->setExplored(true);
+			//assert(minEdge->getNumericalID()<9);
+            (*visited)[minEdge->getNumericalID()] = true;
             SUMOReal effort = (SUMOReal) (minimumKnot->effort
 		    	+ minEdge->getEffort((SUMOTime) (time + minimumKnot->effort)));
     		// check all ways from the node with the minimal length
@@ -203,27 +208,23 @@ public:
             for(i=0; i<length_size; i++) {
                 const E* help = minEdge->getFollower(i);
                 // check whether it can be used
-                if(std::find(myProhibited.begin(), myProhibited.end(), help)!=myProhibited.end()) {
-                    continue;
-                }
+				if(prohibited(help, vehicle)) {
+					continue;
+				}
                 //
-                if( !(*visited)[help->getNumericalID()] //&&//!help->isExplored() &&
-                    && effort < storage->getEffort(help) ) {
+                if( !(*visited)[help->getNumericalID()] && effort < storage->getEffort(help) ) {
                     if(help!=from) {
                         frontierList.push(storage->add(help, effort, minimumKnot));
                     }
                 }
 		    }
 	    }
-        if(!continueOnUnbuild) {
-            MsgHandler::getErrorInstance()->inform(
-                "No connection between '" + from->id()
-                + "' and '" + to->id() + "' found.");
+        if(!myUnbuildIsWarningOnly) {
+            MsgHandler::getErrorInstance()->inform("No connection between '" + from->getID() + "' and '" + to->getID() + "' found.");
         } else {
-            WRITE_WARNING("No connection between '" + from->id() + "' and '" + to->id() + "' found.");
+            WRITE_WARNING("No connection between '" + from->getID() + "' and '" + to->getID() + "' found.");
         }
         clearTemporaryStorages(visited, storage);
-        return std::deque<const E*>();
     }
 
     void prohibit(const std::vector<E*> &toProhibit) {
@@ -232,13 +233,14 @@ public:
 
 public:
     /// Builds the path from marked edges
-    std::deque<const E*> buildPathFrom(EdgeInfo *rbegin) {
-        std::deque<const E*> ret;
+    void buildPathFrom(EdgeInfo *rbegin, std::vector<const E *> &edges) {
+//        B ret;
+		std::deque<const E*> tmp;
         while(rbegin!=0) {
-            ret.push_front(rbegin->edge);
+            tmp.push_front((E *) rbegin->edge); // !!!
             rbegin = rbegin->prev;
         }
-        return ret;
+		std::copy(tmp.begin(), tmp.end(), std::back_inserter(edges));
     }
 
 public:
@@ -313,6 +315,14 @@ protected:
         myReusableEdgeInfoLists.addFreeInstance(consecutionList);
     }
 
+	virtual inline bool prohibited(const E *edge, const V *vehicle) {
+		if(std::find(myProhibited.begin(), myProhibited.end(), edge)!=myProhibited.end()) {
+			return true;
+		}
+		return edge->prohibits(vehicle);
+	}
+
+
 protected:
     /// The network to use
     size_t myNoE;
@@ -324,6 +334,8 @@ protected:
     InstancePool<EdgeInfoCont> myReusableEdgeInfoLists;
 
     std::vector<E*> myProhibited;
+
+	bool myUnbuildIsWarningOnly;
 
 
 };
