e8a681d1b533a66e130b0923bd47898c382e8e9f
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2006-01-26 08:51:39 +0000

    added new vehicle class definitions
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@2512 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/SUMOVehicleClass.cpp
+++ sumo/src/utils/common/SUMOVehicleClass.cpp
@@ -1,38 +1,94 @@
 #include "SUMOVehicleClass.h"
 #include <string>
 
+using namespace std;
+
+
+std::string sSVC_PRIVATE("private");
+std::string sSVC_PUBLIC_TRANSPORT("public_transport");
+std::string sSVC_PUBLIC_EMERGENCY("public_emergency");
+std::string sSVC_PUBLIC_AUTHORITY("public_authority");
+std::string sSVC_PUBLIC_ARMY("public_army");
+std::string sSVC_VIP("vip");
+
+std::string sSVC_HOV("hov");
+std::string sSVC_TAXI("taxi");
+std::string sSVC_BUS("bus");
+std::string sSVC_DELIVERY("delivery");
+std::string sSVC_TRANSPORT("transport");
+std::string sSVC_LIGHTRAIL("lightrail");
+std::string sSVC_CITYRAIL("cityrail");
+std::string sSVC_RAIL_SLOW("rail_slow");
+std::string sSVC_RAIL_FAST("rail_fast");
+std::string sSVC_MOTORCYCLE("motorcycle");
+std::string sSVC_BICYCLE("bicycle");
+std::string sSVC_PEDESTRIAN("pedestrian");
+
 
 std::string
 getVehicleClassName(SUMOVehicleClass id)
 {
-	switch(id) {
-	case SVC_UNKNOWN:
-		return "unknown";
-	case SVC_PRIVATE:
-		return "private";
+	std::string ret;
+	if((id&SVC_PRIVATE)!=0) {
+		ret += ("|" + sSVC_PRIVATE);
+	}
+	if((id&SVC_PUBLIC_TRANSPORT)!=0) {
+		ret += ("|" + sSVC_PUBLIC_TRANSPORT);
+	}
+	if((id&SVC_PUBLIC_EMERGENCY)!=0) {
+		ret += ("|" + sSVC_PUBLIC_EMERGENCY);
+	}
+	if((id&SVC_PUBLIC_AUTHORITY)!=0) {
+		ret += ("|" + sSVC_PUBLIC_AUTHORITY);
+	}
+	if((id&SVC_PUBLIC_ARMY)!=0) {
+		ret += ("|" + sSVC_PUBLIC_ARMY);
+	}
+	if((id&SVC_VIP)!=0) {
+		ret += ("|" + sSVC_VIP);
+	}
 
-	case SVC_PUBLIC_TRANSPORT_BUS:
-		return "public_bus";
-	case SVC_PUBLIC_TRANSPORT_LIGHTRAIL:
-		return "public_lightrail";
-	case SVC_PUBLIC_TRANSPORT_CITY_RAIL:
-		return "public_cityrail";
-	case SVC_PUBLIC_TRANSPORT_RAIL_SLOW:
-		return "public_rail_slow";
-	case SVC_PUBLIC_TRANSPORT_RAIL_FAST:
-		return "public_rail_fast";
+	if((id&SVC_HOV)!=0) {
+		ret += ("|" + sSVC_HOV);
+	}
+	if((id&SVC_TAXI)!=0) {
+		ret += ("|" + sSVC_TAXI);
+	}
+	if((id&SVC_BUS)!=0) {
+		ret += ("|" + sSVC_BUS);
+	}
+	if((id&SVC_DELIVERY)!=0) {
+		ret += ("|" + sSVC_DELIVERY);
+	}
+	if((id&SVC_TRANSPORT)!=0) {
+		ret += ("|" + sSVC_TRANSPORT);
+	}
+	if((id&SVC_LIGHTRAIL)!=0) {
+		ret += ("|" + sSVC_LIGHTRAIL);
+	}
+	if((id&SVC_CITYRAIL)!=0) {
+		ret += ("|" + sSVC_CITYRAIL);
+	}
+	if((id&SVC_RAIL_SLOW)!=0) {
+		ret += ("|" + sSVC_RAIL_SLOW);
+	}
+	if((id&SVC_RAIL_FAST)!=0) {
+		ret += ("|" + sSVC_RAIL_FAST);
+	}
+	if((id&SVC_MOTORCYCLE)!=0) {
+		ret += ("|" + sSVC_MOTORCYCLE);
+	}
+	if((id&SVC_BICYCLE)!=0) {
+		ret += ("|" + sSVC_BICYCLE);
+	}
+	if((id&SVC_PEDESTRIAN)!=0) {
+		ret += ("|" + sSVC_PEDESTRIAN);
+	}
 
-	case SVC_PUBLIC_EMERGENCY:
-		return "public_emergency";
-
-	case SVC_PRIVATE_MOTORCYCLE:
-		return "private_motorcycle";
-	case SVC_PRIVATE_BICYCLE:
-		return "private_bicycle";
-	case SVC_PRIVATE_PEDESTRIAN:
-		return "pedestrian";
-	default:
-		throw 1;
+	if(ret.length()>0) {
+		return ret.substr(1);
+	} else {
+		return ret;
 	}
 }
 
@@ -40,31 +96,63 @@ getVehicleClassName(SUMOVehicleClass id)
 SUMOVehicleClass
 getVehicleClassID(const std::string &name)
 {
-	if(name=="unknown") {
-		return SVC_UNKNOWN;
-	} else if(name=="private") {
-		return SVC_PRIVATE;
-
-	} else if(name=="public_bus") {
-		return SVC_PUBLIC_TRANSPORT_BUS;
-	} else if(name=="public_lightrail") {
-		return SVC_PUBLIC_TRANSPORT_LIGHTRAIL;
-	} else if(name=="public_cityrail") {
-		return SVC_PUBLIC_TRANSPORT_CITY_RAIL;
-	} else if(name=="public_rail_slow") {
-		return SVC_PUBLIC_TRANSPORT_RAIL_SLOW;
-	} else if(name=="public_rail_fast") {
-		return SVC_PUBLIC_TRANSPORT_RAIL_FAST;
-
-	} else if(name=="public_emergency") {
-		return SVC_PUBLIC_EMERGENCY;
-
-	} else if(name=="private_motorcycle") {
-		return SVC_PRIVATE_MOTORCYCLE;
-	} else if(name=="private_bicycle") {
-		return SVC_PRIVATE_BICYCLE;
-	} else if(name=="pedestrian") {
-		return SVC_PRIVATE_PEDESTRIAN;
+	SUMOVehicleClass ret = SVC_UNKNOWN;
+	if(name.find(sSVC_PRIVATE)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PRIVATE);
 	}
-	throw 1;
+	if(name.find(sSVC_PUBLIC_TRANSPORT)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PUBLIC_TRANSPORT);
+	}
+	if(name.find(sSVC_PUBLIC_EMERGENCY)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PUBLIC_EMERGENCY);
+	}
+	if(name.find(sSVC_PUBLIC_AUTHORITY)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PUBLIC_AUTHORITY);
+	}
+	if(name.find(sSVC_PUBLIC_ARMY)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PUBLIC_ARMY);
+	}
+	if(name.find(sSVC_VIP)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_VIP);
+	}
+
+
+	if(name.find(sSVC_HOV)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_HOV);
+	}
+	if(name.find(sSVC_TAXI)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_TAXI);
+	}
+	if(name.find(sSVC_BUS)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_BUS);
+	}
+	if(name.find(sSVC_DELIVERY)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_DELIVERY);
+	}
+	if(name.find(sSVC_TRANSPORT)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_TRANSPORT);
+	}
+	if(name.find(sSVC_LIGHTRAIL)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_LIGHTRAIL);
+	}
+	if(name.find(sSVC_CITYRAIL)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_CITYRAIL);
+	}
+	if(name.find(sSVC_RAIL_SLOW)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_RAIL_SLOW);
+	}
+	if(name.find(sSVC_RAIL_FAST)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_RAIL_FAST);
+	}
+	if(name.find(sSVC_MOTORCYCLE)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_MOTORCYCLE);
+	}
+	if(name.find(sSVC_BICYCLE)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_BICYCLE);
+	}
+	if(name.find(sSVC_PEDESTRIAN)!=string::npos) {
+		ret = (SUMOVehicleClass) ((int) ret | (int) SVC_PEDESTRIAN);
+	}
+
+	return ret;
 }
--- sumo/src/utils/common/SUMOVehicleClass.h
+++ sumo/src/utils/common/SUMOVehicleClass.h
@@ -3,22 +3,71 @@
 
 #include <string>
 
+/**
+ * Bits:
+ * @arg 0-7:  vehicle ownership
+ * @arg 8-23: vehicle size
+ *
+ * From NavTeq:
+ * @arg [0] All
+ * @arg [1] Passenger cars
+ * @arg [2] High Occupancy Vehicle
+ * @arg [3] Emergency Vehicle
+ * @arg [4] Taxi
+ * @arg [5] Public Bus
+ * @arg [6] Delivery Truck
+ * @arg [7] Transport Truck
+ * @arg [8] Bicycle
+ * @arg [9] Pedestrian
+ */
+
+
 enum SUMOVehicleClass {
-	SVC_UNKNOWN = -1,
+	SVC_UNKNOWN = 0,
 
-	SVC_PRIVATE = 0,
+	//{ vehicle ownership
+	/// private vehicles
+	SVC_PRIVATE = 1,
+	/// public transport vehicles
+	SVC_PUBLIC_TRANSPORT = 2,
+	/// public emergency vehicles
+	SVC_PUBLIC_EMERGENCY = 4,
+	/// authorities vehicles
+	SVC_PUBLIC_AUTHORITY = 8,
+	/// army vehicles
+	SVC_PUBLIC_ARMY = 16,
+	/// army vehicles
+	SVC_VIP = 32,
+	//}
 
-	SVC_PUBLIC_TRANSPORT_BUS,
-	SVC_PUBLIC_TRANSPORT_LIGHTRAIL,
-	SVC_PUBLIC_TRANSPORT_CITY_RAIL,
-	SVC_PUBLIC_TRANSPORT_RAIL_SLOW,
-	SVC_PUBLIC_TRANSPORT_RAIL_FAST,
 
-	SVC_PUBLIC_EMERGENCY,
+	//{ vehicle size
+	/// vehicle is a HOV
+	SVC_HOV = 256,
+	/// vehicle is a taxi
+	SVC_TAXI = 512,
+	/// vehicle is a bus
+	SVC_BUS = 1024,
+	/// vehicle is a small delivery vehicle
+	SVC_DELIVERY = 2048,
+	/// vehicle is a large tranposrt vehicle
+	SVC_TRANSPORT = 4096,
+	/// vehicle is a light rail
+	SVC_LIGHTRAIL = 8192,
+	/// vehicle is a city rail
+	SVC_CITYRAIL = 16384,
+	/// vehicle is a slow moving transport rail
+	SVC_RAIL_SLOW = 32768,
+	/// vehicle is a fast moving rail
+	SVC_RAIL_FAST = 65536,
 
-	SVC_PRIVATE_MOTORCYCLE,
-	SVC_PRIVATE_BICYCLE,
-	SVC_PRIVATE_PEDESTRIAN,
+	/// vehicle is a motorcycle
+	SVC_MOTORCYCLE = 131072,
+	/// vehicle is a bicycle
+	SVC_BICYCLE = 262144,
+	/// is a pedestrian
+	SVC_PEDESTRIAN = 524288,
+	// }
 
 };
 
