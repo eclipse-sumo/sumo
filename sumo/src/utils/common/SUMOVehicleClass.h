2d39b7cf2fb2b8bf75e14d94ef53f948d09ce89c
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-01-22 13:25:58 +0000

    added missing inclusion of config.h
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3244 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/SUMOVehicleClass.h
+++ sumo/src/utils/common/SUMOVehicleClass.h
@@ -1,6 +1,18 @@
 #ifndef SUMOVehicleClass_h
 #define SUMOVehicleClass_h
 
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#ifdef WIN32
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+#endif // HAVE_CONFIG_H
+
+
 #include <string>
 
 /**
--- sumo/src/utils/foxtools/MFXUtils.cpp
+++ sumo/src/utils/foxtools/MFXUtils.cpp
@@ -1,3 +1,14 @@
+/* =========================================================================
+ * included modules
+ * ======================================================================= */
+#ifdef HAVE_CONFIG_H
+#ifdef WIN32
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+#endif // HAVE_CONFIG_H
+
 #include "MFXUtils.h"
 
 void
