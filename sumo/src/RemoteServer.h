0a863c0db09321714abb55215f898fdaeb26fdaa
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-02-01 16:05:41 +0000

    debugging (please check)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3392 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/RemoteServer.h
+++ sumo/src/RemoteServer.h
@@ -1,6 +1,6 @@
 /****************************************************************************/
 /// @file    RemoteServer.h
-/// @author  unknown_author
+/// @author  Axel Wegener
 /// @date    20 May 2006
 /// @version $Id: $
 ///
@@ -19,40 +19,9 @@
 /****************************************************************************/
 #ifndef RemoteServer_h
 #define RemoteServer_h
-/***************************************************************************
-                          RemoteServer.h
-              Remote control sumo via XmlRpc messages
-                             -------------------
-    project              : SUMO
-    subproject           : remote control
-    begin                : 20 May 2006
-    copyright            : (C) 2001 by DLR/IVF http://ivf.dlr.de/
-    author               : Axel Wegener
-    email                : wegener@itm.uni-luebeck.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-#ifndef RemoteServer_h
-#define RemoteServer_h
-// ===========================================================================
-// compiler pragmas
-// ===========================================================================
-
-#ifdef _MSC_VER
-//#pragma once
-#endif
 // ===========================================================================
 // included modules
 // ===========================================================================
-
 #include "XmlRpc.h"
 
 #include <iostream>
