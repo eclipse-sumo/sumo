deb9dc5470d7fd331ddbe743e294e9117bf379ef
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-02-12 13:45:00 +0000

    code recheck
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3462 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/tools/NetCompare.cpp
+++ sumo/src/tools/NetCompare.cpp
@@ -1,33 +1,25 @@
-//---------------------------------------------------------------------------//
-//                        NetCompare.h -
-//
-//                           -------------------
-//  project              : SUMO - Simulation of Urban MObility
-//  begin                : Jun 2005
-//  copyright            : (C) 2005 by Danilo Boyom
-//  organisation         : IVF/DLR http://ivf.dlr.de
-//  email                : danilo.tete-boyom@dlr.com
-//---------------------------------------------------------------------------//
-
-//---------------------------------------------------------------------------//
+/****************************************************************************/
+/// @file    NetCompare.cpp
+/// @author  unknown_author
+/// @date    Jun 2005
+/// @version $Id: $
+///
+// missing_desc
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation; either version 2 of the License, or
 //   (at your option) any later version.
 //
-//---------------------------------------------------------------------------//
-namespace
-{
-    const char rcsid[] =
-    "$Id: $";
-}
-// $Log: $
-//
-//
-/* =========================================================================
- * included modules
- * ======================================================================= */
+/****************************************************************************/
+// ===========================================================================
+// included modules
+// ===========================================================================
 #ifdef WIN32
 #include <windows_config.h>
 #else
@@ -48,29 +40,27 @@ namespace
 #endif // _DEBUG
 
 
-/* =========================================================================
- * used namespaces
- * ======================================================================= */
+// ===========================================================================
+// used namespaces
+// ===========================================================================
 using namespace std;
 
 NetCompare::DictTypeJunction NetCompare::myJunctionDictA;
 NetCompare::DictTypeJunction NetCompare::myJunctionDictB;
 
-/* =========================================================================
- * member definitions
- * ======================================================================= */
+// ===========================================================================
+// member definitions
+// ===========================================================================
 //////////////////////////////////////////////////////////////////////
 // Constructor/Destructor
 //////////////////////////////////////////////////////////////////////
 
 NetCompare::NetCompare(const char *netA, const char *netB)
         : net_a(netA), net_b(netB)
-{
-}
+{}
 
 NetCompare::~NetCompare()
-{
-}
+{}
 
 //////////////////////////////////////////////////////////////////////
 // Definitions of the Methods
@@ -80,8 +70,8 @@ NetCompare::~NetCompare()
 void
 NetCompare::load(void)
 {
-	loadNet(net_a,1);
-	loadNet(net_b,2);
+    loadNet(net_a,1);
+    loadNet(net_b,2);
 
 }
 
@@ -89,115 +79,115 @@ NetCompare::load(void)
 void
 NetCompare::loadNet(const char *net, int dic)
 {
-	char buffer[_MAX_PATH];
-	getcwd(buffer,_MAX_PATH);
-	cout<<"Current directory is "<<buffer<<endl;
+    char buffer[_MAX_PATH];
+    getcwd(buffer,_MAX_PATH);
+    cout<<"Current directory is "<<buffer<<endl;
 
-	ifstream out(net);
+    ifstream out(net);
 
     if (!out) {
-      cerr << "cannot open file: " << net <<endl;
-      exit(-1);
-	}
+        cerr << "cannot open file: " << net <<endl;
+        exit(-1);
+    }
 
-	std::string buff;
+    std::string buff;
     cout<<endl<<"=====================Loading "<<net<< "============================="<<endl;
-	int l = 0;
-	while(!out.eof()) {
-		getline(out,buff);
-		if(buff.find("<junction id=")!=string::npos){
-			l = l + 1;
-			std::string id = buff.substr(buff.find("=")+2,buff.find(" t")-buff.find("=")-3);
-			NetCompare::Junction *junction = new Junction(id);
-       		std::string rest = buff.substr(buff.find("x=")+2,buff.find(">")-buff.find("x="));
-			cout<<l<<". Junction ID = "<<id<<endl;
+    int l = 0;
+    while (!out.eof()) {
+        getline(out,buff);
+        if (buff.find("<junction id=")!=string::npos) {
+            l = l + 1;
+            std::string id = buff.substr(buff.find("=")+2,buff.find(" t")-buff.find("=")-3);
+            NetCompare::Junction *junction = new Junction(id);
+            std::string rest = buff.substr(buff.find("x=")+2,buff.find(">")-buff.find("x="));
+            cout<<l<<". Junction ID = "<<id<<endl;
 
-			std::string  pos1 = rest.substr(1,rest.find(" ")-2);
-			std::string  pos2 = rest.substr(rest.find("y=")+3,rest.find(">")-rest.find("y=")-4);
+            std::string  pos1 = rest.substr(1,rest.find(" ")-2);
+            std::string  pos2 = rest.substr(rest.find("y=")+3,rest.find(">")-rest.find("y=")-4);
 
-			Position2D pos(atof(pos1.c_str()),atof(pos2.c_str()));
-			junction->pos = pos;
-			cout <<"       Position: x = "<<pos1<<" y = "<<pos2<<endl;
+            Position2D pos(atof(pos1.c_str()),atof(pos2.c_str()));
+            junction->pos = pos;
+            cout <<"       Position: x = "<<pos1<<" y = "<<pos2<<endl;
 
-			if (dic == 1){
-				myJunctionDictA[id] = junction;
-			}else{
-				myJunctionDictB[id] = junction;
-			}
-		}
+            if (dic == 1) {
+                myJunctionDictA[id] = junction;
+            } else {
+                myJunctionDictB[id] = junction;
+            }
+        }
 
-	}
-	out.close();
+    }
+    out.close();
 }
 
 
 void
 NetCompare::setJunctionA(std::string a,std::string b,std::string c)
 {
-	juncA1 = a;
-	juncA2 = b;
-	juncA3 = c;
+    juncA1 = a;
+    juncA2 = b;
+    juncA3 = c;
 }
 
 void
 NetCompare::setJunctionB(std::string a,std::string b,std::string c)
 {
-	juncB1 = a;
-	juncB2 = b;
-	juncB3 = c;
+    juncB1 = a;
+    juncB2 = b;
+    juncB3 = c;
 
 }
 
 void
 NetCompare::convertA(void)
 {
-	cout <<"==============convertA=================="<<endl;
+    cout <<"==============convertA=================="<<endl;
 
-	DictTypeJunction::iterator i;
-	i = myJunctionDictA.find(juncA1);
-    if(i==myJunctionDictA.end()) {
+    DictTypeJunction::iterator i;
+    i = myJunctionDictA.find(juncA1);
+    if (i==myJunctionDictA.end()) {
         cout << "Could not find junction '" << juncA1 << "'!" << endl;
         throw 1;
     }
-	Junction *j1 = (*i).second;
+    Junction *j1 = (*i).second;
 
-	i = myJunctionDictA.find(juncA2);
-    if(i==myJunctionDictA.end()) {
+    i = myJunctionDictA.find(juncA2);
+    if (i==myJunctionDictA.end()) {
         cout << "Could not find junction '" << juncA2 << "'!" << endl;
         throw 1;
     }
-	Junction *j2 = (*i).second;
+    Junction *j2 = (*i).second;
 
-	i = myJunctionDictA.find(juncA3);
-    if(i==myJunctionDictA.end()) {
+    i = myJunctionDictA.find(juncA3);
+    if (i==myJunctionDictA.end()) {
         cout << "Could not find junction '" << juncA3 << "'!" << endl;
         throw 1;
     }
-	Junction *j3 = (*i).second;
+    Junction *j3 = (*i).second;
 
-	SUMOReal xmin = minValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
-	SUMOReal xmax = maxValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
-	SUMOReal xw   = xmax - xmin ;
-	cout <<" xminValue "<<xmin<<endl;
-	cout <<" xmaxValue "<<xmax<<endl;
+    SUMOReal xmin = minValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
+    SUMOReal xmax = maxValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
+    SUMOReal xw   = xmax - xmin ;
+    cout <<" xminValue "<<xmin<<endl;
+    cout <<" xmaxValue "<<xmax<<endl;
     cout <<" xwidth "<<xw<<endl;
 
     SUMOReal ymin = minValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
-	SUMOReal ymax = maxValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
-	SUMOReal yw   = ymax - ymin ;
+    SUMOReal ymax = maxValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
+    SUMOReal yw   = ymax - ymin ;
     cout <<" yminValue "<<ymin<<endl;
-	cout <<" ymaxValue "<<ymax<<endl;
+    cout <<" ymaxValue "<<ymax<<endl;
     cout <<" ywidth "<<yw<<endl;
 
-	for(DictTypeJunction::iterator j=myJunctionDictA.begin(); j!=myJunctionDictA.end(); j++) {
-		cout<<"----------------------------------------------"<<endl;
-		cout <<"Junction ID = "<<(*j).second->id<<endl;
-		cout <<"     alte x= "<< (*j).second->pos.x() <<" alte y= "<<(*j).second->pos.y()<<endl;
-		SUMOReal nx = ((*j).second->pos.x() -xmin)/xw;
-		SUMOReal ny = ((*j).second->pos.y() -ymin)/yw;
+    for (DictTypeJunction::iterator j=myJunctionDictA.begin(); j!=myJunctionDictA.end(); j++) {
+        cout<<"----------------------------------------------"<<endl;
+        cout <<"Junction ID = "<<(*j).second->id<<endl;
+        cout <<"     alte x= "<< (*j).second->pos.x() <<" alte y= "<<(*j).second->pos.y()<<endl;
+        SUMOReal nx = ((*j).second->pos.x() -xmin)/xw;
+        SUMOReal ny = ((*j).second->pos.y() -ymin)/yw;
         cout <<"     neue x = "<< nx <<" neue y = "<< ny<<endl;
         ((*j).second->pos).set(nx,ny);
-	}
+    }
 
 
 }
@@ -207,112 +197,113 @@ NetCompare::convertA(void)
 void
 NetCompare::convertB(void)
 {
-	cout <<"===============convertB=================="<<endl;
+    cout <<"===============convertB=================="<<endl;
 
-	DictTypeJunction::iterator i;
-	i = myJunctionDictB.find(juncB1);
-    if(i==myJunctionDictB.end()) {
+    DictTypeJunction::iterator i;
+    i = myJunctionDictB.find(juncB1);
+    if (i==myJunctionDictB.end()) {
         cout << "Could not find junction '" << juncB1 << "'!" << endl;
         throw 1;
     }
-	Junction *j1 = (*i).second;
+    Junction *j1 = (*i).second;
 
-	i = myJunctionDictB.find(juncB2);
-    if(i==myJunctionDictB.end()) {
+    i = myJunctionDictB.find(juncB2);
+    if (i==myJunctionDictB.end()) {
         cout << "Could not find junction '" << juncB2 << "'!" << endl;
         throw 1;
     }
-	Junction *j2 = (*i).second;
+    Junction *j2 = (*i).second;
 
-	i = myJunctionDictB.find(juncB3);
-    if(i==myJunctionDictB.end()) {
+    i = myJunctionDictB.find(juncB3);
+    if (i==myJunctionDictB.end()) {
         cout << "Could not find junction '" << juncB3 << "'!" << endl;
         throw 1;
     }
-	Junction *j3 = (*i).second;
+    Junction *j3 = (*i).second;
 
-	SUMOReal xmin = minValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
-	SUMOReal xmax = maxValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
-	SUMOReal xw   = xmax - xmin ;
-	cout <<" xmin "<<xmin<<endl;
-	cout <<" xmax "<<xmax<<endl;
+    SUMOReal xmin = minValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
+    SUMOReal xmax = maxValue(j1->pos.x(),j2->pos.x(),j3->pos.x());
+    SUMOReal xw   = xmax - xmin ;
+    cout <<" xmin "<<xmin<<endl;
+    cout <<" xmax "<<xmax<<endl;
     cout <<" xwidth "<<xw<<endl;
 
     SUMOReal ymin = minValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
-	SUMOReal ymax = maxValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
-	SUMOReal yw   = ymax - ymin ;
+    SUMOReal ymax = maxValue(j1->pos.y(),j2->pos.y(),j3->pos.y());
+    SUMOReal yw   = ymax - ymin ;
     cout <<" ymin "<<ymin<<endl;
-	cout <<" ymax "<<ymax<<endl;
+    cout <<" ymax "<<ymax<<endl;
     cout <<" ywidth "<<yw<<endl;
 
-	for(DictTypeJunction::iterator j=myJunctionDictB.begin(); j!=myJunctionDictB.end(); j++) {
-		cout<<"-----------------------------------------"<<endl;
-		cout <<"Junction ID = "<<(*j).second->id<<endl;
-		cout <<"       alte x "<< (*j).second->pos.x() <<" alte y "<<(*j).second->pos.y()<<endl;
-		SUMOReal nx = ((*j).second->pos.x() -xmin)/xw;
-		SUMOReal ny = ((*j).second->pos.y() -ymin)/yw;
+    for (DictTypeJunction::iterator j=myJunctionDictB.begin(); j!=myJunctionDictB.end(); j++) {
+        cout<<"-----------------------------------------"<<endl;
+        cout <<"Junction ID = "<<(*j).second->id<<endl;
+        cout <<"       alte x "<< (*j).second->pos.x() <<" alte y "<<(*j).second->pos.y()<<endl;
+        SUMOReal nx = ((*j).second->pos.x() -xmin)/xw;
+        SUMOReal ny = ((*j).second->pos.y() -ymin)/yw;
         cout <<"       neue x= "<< nx <<" alte y= "<< ny<<endl;
         ((*j).second->pos).set(nx,ny);
-	}
+    }
 
 }
 
 void
-NetCompare::result(const char *output){
-	ofstream out(output);
-	for(DictTypeJunction::iterator i=myJunctionDictA.begin(); i!=myJunctionDictA.end(); i++) {
-		 SUMOReal minAbstand = 77777;
-		 std::string id = "";
-		 for(DictTypeJunction::iterator j=myJunctionDictB.begin(); j!=myJunctionDictB.end(); j++) {
-			 SUMOReal X = pow((*i).second->pos.x() - (*j).second->pos.x(),2);
-			 SUMOReal Y = pow((*i).second->pos.y() - (*j).second->pos.y(),2);
-			 SUMOReal nabstand = sqrt(X+Y);
-			 if(nabstand < minAbstand ){
-				 minAbstand = nabstand;
-				 id = (*j).second->id;
-			 }
-		 }
-		 out<<(*i).second->id << ":" << id <<endl;
-		 cout <<(*i).second->id <<"    Aequivalent zu    "<< id <<endl;
-	}
-	out.close();
+NetCompare::result(const char *output)
+{
+    ofstream out(output);
+    for (DictTypeJunction::iterator i=myJunctionDictA.begin(); i!=myJunctionDictA.end(); i++) {
+        SUMOReal minAbstand = 77777;
+        std::string id = "";
+        for (DictTypeJunction::iterator j=myJunctionDictB.begin(); j!=myJunctionDictB.end(); j++) {
+            SUMOReal X = pow((*i).second->pos.x() - (*j).second->pos.x(),2);
+            SUMOReal Y = pow((*i).second->pos.y() - (*j).second->pos.y(),2);
+            SUMOReal nabstand = sqrt(X+Y);
+            if (nabstand < minAbstand) {
+                minAbstand = nabstand;
+                id = (*j).second->id;
+            }
+        }
+        out<<(*i).second->id << ":" << id <<endl;
+        cout <<(*i).second->id <<"    Aequivalent zu    "<< id <<endl;
+    }
+    out.close();
 }
 
 SUMOReal
 NetCompare::maxValue(SUMOReal a, SUMOReal b, SUMOReal c)
 {
-	if(a>=b){
-		if(a>=c){
-			return a;
-		}else{
-			return c;
-		}
-	}else{
-		if(b>=c){
-			return b;
-		}else{
-			return c;
-		}
-	}
+    if (a>=b) {
+        if (a>=c) {
+            return a;
+        } else {
+            return c;
+        }
+    } else {
+        if (b>=c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 
 }
 
 SUMOReal
 NetCompare::minValue(SUMOReal a, SUMOReal b, SUMOReal c)
 {
-	if(a<=b){
-		if(a<=c){
-			return a;
-		}else{
-			return c;
-		}
-	}else{
-		if(b<=c){
-			return b;
-		}else{
-			return c;
-		}
-	}
+    if (a<=b) {
+        if (a<=c) {
+            return a;
+        } else {
+            return c;
+        }
+    } else {
+        if (b<=c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 }
 
 /* -------------------------------------------------------------------------
@@ -321,35 +312,33 @@ NetCompare::minValue(SUMOReal a, SUMOReal b, SUMOReal c)
 int main(int argc, char** argv)
 {
 
-	if (argc!=7) {
-    cerr << " syntax error please use:"
-		 << " NetCompare <netA> <netB> <junctionA1>:<junctionB1> "
-         << "            <junctionA2>:<junctionB2> <junctionA3>:<junctionB3> <output-file>" << endl;
-      return -1;
+    if (argc!=7) {
+        cerr << " syntax error please use:"
+        << " NetCompare <netA> <netB> <junctionA1>:<junctionB1> "
+        << "            <junctionA2>:<junctionB2> <junctionA3>:<junctionB3> <output-file>" << endl;
+        return -1;
     }
 
-	std::string arg3 = argv[3];
-	std::string arg4 = argv[4];
-	std::string arg5 = argv[5];
+    std::string arg3 = argv[3];
+    std::string arg4 = argv[4];
+    std::string arg5 = argv[5];
 
-	NetCompare *app = new NetCompare(argv[1],argv[2]);
+    NetCompare *app = new NetCompare(argv[1],argv[2]);
 
-	app->setJunctionA(arg3.substr(0,arg3.find(":")),
-		              arg4.substr(0,arg4.find(":")),
-		              arg5.substr(0,arg5.find(":")));
-	app->setJunctionB(arg3.substr(arg3.find(":")+1, arg3.length() - arg3.find(":")-1),
-		              arg4.substr(arg4.find(":")+1, arg4.length() - arg4.find(":")-1),
-					  arg5.substr(arg5.find(":")+1, arg5.length() - arg5.find(":")-1));
-	app->load();
-	app->convertA();
-	app->convertB();
-	app->result(argv[6]);
-	return 0;
+    app->setJunctionA(arg3.substr(0,arg3.find(":")),
+                      arg4.substr(0,arg4.find(":")),
+                      arg5.substr(0,arg5.find(":")));
+    app->setJunctionB(arg3.substr(arg3.find(":")+1, arg3.length() - arg3.find(":")-1),
+                      arg4.substr(arg4.find(":")+1, arg4.length() - arg4.find(":")-1),
+                      arg5.substr(arg5.find(":")+1, arg5.length() - arg5.find(":")-1));
+    app->load();
+    app->convertA();
+    app->convertB();
+    app->result(argv[6]);
+    return 0;
 }
 
 
-/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
 
-// Local Variables:
-// mode:C++
-// End:
+/****************************************************************************/
+
--- sumo/src/tools/NetCompare.h
+++ sumo/src/tools/NetCompare.h
@@ -1,34 +1,35 @@
-#ifndef NetCompare_h
-#define NetCompare_h
-//---------------------------------------------------------------------------//
-//                        NetCompare.h -
+/****************************************************************************/
+/// @file    NetCompare.h
+/// @author  unknown_author
+/// @date    Jun 2005
+/// @version $Id: $
+///
 //
-//                           -------------------
-//  project              : SUMO - Simulation of Urban MObility
-//  begin                : Jun 2005
-//  copyright            : (C) 2005 by Danilo Boyom
-//  organisation         : IVF/DLR http://ivf.dlr.de
-//  email                : danilo.tete-boyom@dlr.com
-//---------------------------------------------------------------------------//
-
-//---------------------------------------------------------------------------//
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation; either version 2 of the License, or
 //   (at your option) any later version.
 //
-//---------------------------------------------------------------------------//
-// $Log:  $
+/****************************************************************************/
+#ifndef NetCompare_h
+#define NetCompare_h
 
-/* =========================================================================
- * compiler pragmas
- * ======================================================================= */
+// ===========================================================================
+// compiler pragmas
+// ===========================================================================
+#ifdef _MSC_VER
 #pragma warning(disable: 4786)
+#endif
 
-/* =========================================================================
- * included modules
- * ======================================================================= */
+// ===========================================================================
+// included modules
+// ===========================================================================
 #ifdef WIN32
 #include <windows_config.h>
 #else
@@ -43,73 +44,75 @@
 #include <utils/geom/Position2DVector.h>
 
 
-/* =========================================================================
- * class definitions
- * ======================================================================= */
+// ===========================================================================
+// class definitions
+// ===========================================================================
 
 class NetCompare
 {
 
 
 public:
-	 /// Constructor
+    /// Constructor
     NetCompare();
 
     /// Constructor
-	NetCompare(const char *netA, const char *netB );
+    NetCompare(const char *netA, const char *netB);
 
     /// Destructor
     ~NetCompare();
 
-	class Junction{
-	public:
-		explicit Junction(const std::string nid){
-			id = nid;
-		}
-		~Junction();
-		std::string id;
-		Position2D pos;
-	};
+    class Junction
+    {
+    public:
+        explicit Junction(const std::string nid)
+        {
+            id = nid;
+        }
+        ~Junction();
+        std::string id;
+        Position2D pos;
+    };
 
 
     /// load net-file and save the Junctions into a dictionnary 1 or 2
-	void load(void);
+    void load(void);
     void loadNet(const char *net, int dic);
 
-	/// set the junctions names
-	void setJunctionA(std::string a, std::string b, std::string c);
-	void setJunctionB(std::string a, std::string b, std::string c);
+    /// set the junctions names
+    void setJunctionA(std::string a, std::string b, std::string c);
+    void setJunctionB(std::string a, std::string b, std::string c);
 
     // convert junction's positions in new positions
     void convertA(void);
     void convertB(void);
 
-	void result(const char *output);
+    void result(const char *output);
 
-	// return the min or max values
-	SUMOReal minValue(SUMOReal a, SUMOReal b, SUMOReal c);
-	SUMOReal maxValue(SUMOReal a, SUMOReal b, SUMOReal c);
+    // return the min or max values
+    SUMOReal minValue(SUMOReal a, SUMOReal b, SUMOReal c);
+    SUMOReal maxValue(SUMOReal a, SUMOReal b, SUMOReal c);
 
-	public:
+public:
 
     /// the Name of the net-file-A
     const char *net_a;
 
-	/// the Name of the net-file-B
+    /// the Name of the net-file-B
     const char *net_b;
 
-	/// Junctions name NetA
-	std::string juncA1;
-	std::string juncA2;
-	std::string juncA3;
+    /// Junctions name NetA
+    std::string juncA1;
+    std::string juncA2;
+    std::string juncA3;
 
-	/// Junction name NetB
-	std::string juncB1;
-	std::string juncB2;
-	std::string juncB3;
+    /// Junction name NetB
+    std::string juncB1;
+    std::string juncB2;
+    std::string juncB3;
 
 
-	/// Definition of the dictionary type
+    /// Definition of the dictionary type
     typedef std::map<std::string, Junction*> DictTypeJunction;
 
     /// The dictionaries
@@ -119,10 +122,7 @@ public:
 };
 
 
-/**************** DO NOT DEFINE ANYTHING AFTER THE INCLUDE *****************/
-
 #endif
 
-// Local Variables:
-// mode:C++
-// End:
+/****************************************************************************/
+
