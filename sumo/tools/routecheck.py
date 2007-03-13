8d117dbf0b2c3e907514ca46973265aef5e7c3c2
Michael Behrisch <oss@behrisch.de>
2007-03-13 12:07:57 +0000

    much better route fixing
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3634 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/routecheck.py
+++ sumo/tools/routecheck.py
@@ -9,9 +9,12 @@
 # if the route is too short (only one edge), if the route is disconnected
 # or if the first edge is too short for the vehicle.
 # If one specifies -f or --fix all subsequent route files will be fixed
-# (if possible). At the moment this means only removal of the first edge
-# if it is too short.
-import string, sys
+# (if possible). At the moment this means removing the first edge
+# if it is too short, adding an intermediate edge if just one link is missing
+# in a disconnected route, removing the whole route in any other case.
+# All changes are documented within the output file which has the same name
+# as the input file with an additional .fixed suffix.
+import os, string, sys, StringIO
 
 from xml.sax import saxutils, make_parser, handler
 
@@ -38,6 +41,12 @@ class NetReader(handler.ContentHandler):
 
     def getLength(self, edge):
         return self._edgeL[edge]
+
+    def getIntermediateEdge(self, orig, dest):
+        for inter in self._nb[orig]:
+            if dest in self._nb[inter]:
+                return inter
+        return ''
 
             
 class VehicleTypeReader(handler.ContentHandler):
@@ -67,12 +76,17 @@ class RouteReader(handler.ContentHandler):
         self._vID = ''
         self._routeID = ''
         self._routeString = ''
+        self._removedString = ''
+        self._addedString = ''
         self._net = net
         self._types = types
         if outFileName != '':
             self._out = open(outFileName, 'w')
         else:
             self._out = None
+        self._fileOut = None
+        self._isRouteValid = True
+        self._changed = False
         
     def startDocument(self):
         if self._out:
@@ -81,12 +95,39 @@ class RouteReader(handler.ContentHandler):
     def endDocument(self):
         if self._out:
             self._out.close()
+            if not self._changed:
+                os.remove(self._out.name)
+
+    def condOutputRedirect(self):
+        if self._out and not self._fileOut:
+            self._fileOut = self._out
+            self._out = StringIO.StringIO()
+
+    def endOutputRedirect(self):
+        if self._fileOut:
+            if not self._isRouteValid:
+                self._changed = True
+                self._fileOut.write("<!-- ")
+            self._fileOut.write(self._out.getvalue())
+            if not self._isRouteValid:
+                self._fileOut.write(" -->")
+            if self._removedString != '':
+                self._fileOut.write("<!-- removed edges: " + self._removedString + "-->")
+                self._removedString = ''
+            if self._addedString != '':
+                self._fileOut.write("<!-- added edges: " + self._addedString + "-->")
+                self._addedString = ''
+            self._out.close()
+            self._out = self._fileOut
+            self._fileOut = None
 
     def startElement(self, name, attrs):
-        if name == 'vehicle':
+        if name == 'vehicle' and not 'route' in attrs:
+            self.condOutputRedirect()
             self._vType = attrs['type']
             self._vID = attrs['id']
         if name == 'route':
+            self.condOutputRedirect()
             if 'id' in attrs:
                 self._routeID = attrs['id']
             else:
@@ -99,16 +140,23 @@ class RouteReader(handler.ContentHandler):
             self._out.write('>')
 
     def endElement(self, name):
+        routeSectionEnded = False
         if name == 'route':
-            self.testRoute()
+            self._isRouteValid = self.testRoute()
             if self._out:
                 self._out.write(self._routeString)
             self._routeID = ''
             self._routeString = ''
-        if name == 'vehicle':
+            if self._vID == '':
+                routeSectionEnded = True
+        if name == 'vehicle' and self._vID != '':
             self._vType = ''
+            self._vID = ''
+            routeSectionEnded = True
         if self._out:
             self._out.write('</%s>' % name)
+        if routeSectionEnded:
+            self.endOutputRedirect()
 
     def characters(self, content):
         if self._routeID != '':
@@ -118,22 +166,34 @@ class RouteReader(handler.ContentHandler):
 
     def testRoute(self):
         if self._routeID != '':
+            returnValue = True
             rdata = self._routeString.split()
             if len(rdata) < 2:
                 print "Warning: Route " + self._routeID + " too short"
+                returnValue = False
             doConnectivityTest = True
             doLengthTest = (len(rdata) > 0)
             for v in rdata:
                 if not self._net.hasEdge(v):
                     print "Warning: Unknown edge " + v + " in route " + self._routeID
+                    returnValue = False
                     doConnectivityTest = False
                     if v == rdata[0]:
                         doLengthTest = False
-            if doConnectivityTest:
+            while doConnectivityTest:
+                doConnectivityTest = False
                 for i, v in enumerate(rdata):
-                    if i < len(rdata) - 1:
-                        if not self._net.isNeighbor(v, rdata[i+1]):
-                            print "Warning: Route " + self._routeID + " disconnected between " + v + " and " + rdata[i+1]
+                    if i < len(rdata) - 1 and not self._net.isNeighbor(v, rdata[i+1]):
+                        print "Warning: Route " + self._routeID + " disconnected between " + v + " and " + rdata[i+1]
+                        interEdge = self._net.getIntermediateEdge(v, rdata[i+1])
+                        if interEdge != '':
+                            rdata.insert(i+1, interEdge)
+                            self._changed = True
+                            self._addedString += interEdge + " "
+                            self._routeString = string.join(rdata)
+                            doConnectivityTest = True
+                            break
+                        returnValue = False
             if doLengthTest:
                 edgeL = self._net.getLength(rdata[0]);
                 removeFirst = False
@@ -146,8 +206,12 @@ class RouteReader(handler.ContentHandler):
                         print "Warning: All vehicles too long for " + rdata[0] + ", first edge of route " + self._routeID
                         removeFirst = True
                 if removeFirst and self._out:
+                    self._changed = True
+                    self._removedString += rdata[0] + " "
                     self._routeString = string.join(rdata[1:])
-                    self.testRoute()
+                    return self.testRoute()
+            return returnValue
+        return False
                     
     def ignorableWhitespace(self, content):
         if self._out:
