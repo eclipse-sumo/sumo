8a0cf6b45bb7cca9810459a7b4ae474d1f128418
Michael Behrisch <oss@behrisch.de>
2007-04-04 10:28:09 +0000

    first flowrouter tests work
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3762 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/flowrouter.py
+++ sumo/tools/flowrouter.py
@@ -2,49 +2,90 @@
 # This script does flow routing similar to the dfrouter.
 # It needs three parameters, the SUMO net (.net.xml), a file
 # specifying detectors (with types!) and one for the flows.
-import os, string, sys, StringIO
+import os, string, sys
 
 from xml.sax import saxutils, make_parser, handler
 
+MAX_POS_DEVIATION = 1
+
+class Edge:
+
+    def __init__(self, label, source, target, kind="junction"):
+        self.label = label
+        self.source = source
+        self.target = target
+        self.capacity = sys.maxint
+        self.flow = 0
+        self.kind = kind
+
+    def __repr__(self):
+        cap = str(self.capacity)
+        if self.capacity == sys.maxint:
+            cap = "inf"
+        return self.kind+"_"+self.label+"<"+str(self.flow)+"|"+cap+">"
+    
+        
 class NetReader(handler.ContentHandler):
 
     def __init__(self):
+        self._currVertex = 0
         self._edge = ''
+        self._edges = {}
+        self._auxEdges = []
         self._inEdges = {}
         self._outEdges = {}
         self._lane2edge = {}
-        self._edge2det = {}
+        self._edgeDetPos = {}
         self._det2edge = {}
-        self._edgeCap = {}
-        self._edgeFlow = {}
-        self._sourceEdges = set()
-        self._sinkEdges = set()
+        self._source = self.newVertex()
+        self._sink = self.newVertex()
         self._edgeString = ''
 
+    def newVertex(self):
+        self._currVertex += 1
+        self._inEdges[self._currVertex] = set()
+        self._outEdges[self._currVertex] = set()
+        return self._currVertex
+
+    def addEdge(self, edgeObj):
+        self._outEdges[edgeObj.source].add(edgeObj)
+        self._inEdges[edgeObj.target].add(edgeObj)
+        if edgeObj.kind == "real":
+            self._edges[edgeObj.label] = edgeObj
+        else:
+            self._auxEdges.append(edgeObj)
+                
     def startElement(self, name, attrs):
         if name == 'edges':
             self._edgeString = ' '
         if name == 'edge' and (not 'function' in attrs or attrs['function'] != 'internal'):
             self._edge = attrs['id']
         if name == 'cedge':
-            self._outEdges[self._edge].add(attrs['id'])
-            self._inEdges[attrs['id']].add(self._edge)
+            fromEdge = self._edges[self._edge]
+            toEdge = self._edges[attrs['id']]
+            newEdge = Edge(self._edge+"_"+attrs['id'], fromEdge.target, toEdge.source)
+            self.addEdge(newEdge)
         if name == 'lane':
             self._lane2edge[attrs['id']] = self._edge
         if name == 'detector_definition':
             edge = self._lane2edge[attrs['lane']]
-            if edge in self._edge2det:
-                print "Warning! Edge " + edge + " has detector " + self._edge2det[edge] + ". Ignoring " + attrs['id']
+            pos = float(attrs['pos'])
+            if edge in self._edgeDetPos and abs(self._edgeDetPos[edge] - pos) > MAX_POS_DEVIATION:
+                print "Warning! Edge " + edge + " has detectors at multiple positions, ignoring " + attrs['id']
             else:
-                self._edge2det[edge] = attrs['id']
+                self._edgeDetPos[edge] = pos
                 self._det2edge[attrs['id']] = edge
             if not 'type' in attrs:
                 print "Warning! No type for detector " + attrs['id']
             else:
                 if attrs['type'] == 'source':
-                    self._sourceEdges.add(edge)
+                    edgeSource = self._edges[edge].source
+                    newEdge = Edge("s_"+edge, self._source, edgeSource, "source")
+                    self.addEdge(newEdge)
                 if attrs['type'] == 'sink':
-                    self._sinkEdges.add(edge)
+                    edgeTarget = self._edges[edge].target
+                    newEdge = Edge("t_"+edge, edgeTarget, self._sink, "sink")
+                    self.addEdge(newEdge)
 
     def characters(self, content):
         if self._edgeString != '':
@@ -53,81 +94,148 @@ class NetReader(handler.ContentHandler):
     def endElement(self, name):
         if name == 'edges':
             for edge in self._edgeString.split():
-                self._inEdges[edge] = set()
-                self._outEdges[edge] = set()
+                edgeObj = Edge(edge, self.newVertex(), self.newVertex(), "real")
+                self.addEdge(edgeObj)
             self._edgeString = ''
 
     def readFlows(self, flowFile):
+        headerSeen = False
         for l in file(flowFile):
             flowDef = l.split(';')
+            if not headerSeen and flowDef[0] == "Detector":
+                headerSeen = True
+                continue
             if not flowDef[0] in self._det2edge:
                 print "Warning! Unknown detector " + flowDef[0]
             else:
-                self._edgeCap[self._det2edge[flowDef[0]]] = int(flowDef[2])
+                edge = self._edges[self._det2edge[flowDef[0]]]
+                if edge.capacity == sys.maxint:
+                    edge.capacity = int(flowDef[2])
+                else:
+                    edge.capacity += int(flowDef[2])
 
-    def printCaps(self):
-        print self._sourceEdges
-        print self._edgeCap
-        print self._sinkEdges
+    def detectSourceSink(self):
+        for edge in self._edges.values():
+            if len(self._inEdges[edge.source]) == 0:
+                newEdge = Edge("s_"+edge.label, self._source, edge.source, "source")
+                self.addEdge(newEdge)
+            if len(self._outEdges[edge.target]) == 0:
+                newEdge = Edge("t_"+edge.label, edge.target, self._sink, "sink")
+                self.addEdge(newEdge)
+        
+    def checkNet(self, forcedSourceSinkDetection):
+        if not forcedSourceSinkDetection:
+            if len(self._inEdges[self._sink]) == 0:
+                print "Warning! No sinks, trying to find some."
+                forcedSourceSinkDetection = True
+            if len(self._outEdges[self._source]) == 0:
+                print "Warning! No sources, trying to find some."
+                forcedSourceSinkDetection = True
+        if forcedSourceSinkDetection:
+            self.detectSourceSink()
+            if len(self._inEdges[self._sink]) == 0:
+                print "Error! No sinks found."
+                return False
+            if len(self._outEdges[self._source]) == 0:
+                print "Error! No sources found."
+                return False
+        queue = [self._source]
+        seenVertices = set()
+        while len(queue) > 0:
+            currVertex = queue.pop(0)
+            if currVertex == self._sink:
+                print "Error! Path with unlimited capacity found."
+                return False
+            seenVertices.add(currVertex)
+            for edge in self._outEdges[currVertex]:
+                if not edge.target in seenVertices and edge.capacity == sys.maxint:
+                    queue.append(edge.target)
+        return True
 
-##    def adaptCapacities(self):
-##        self._edgeFlow.clear()
-##        routeFound = True
-##        while routeFound:
-##            routeFound = False
-##            queue = list(self._sourceEdges)
-##            while len(queue) > 0:
-##                currEdge = queue.pop(0)
-##                if currEdge in self._edgeCap:
-##                    if not currEdge in self._edgeFlow:
-##                        self._edgeFlow[currEdge] = 0
-##                    if self._edgeCap[currEdge] > self._edgeFlow[currEdge]:
-##                        if currEdge in self._sinkEdges:
-##                            routeFound = True
-##                            break
-##                        for edge in self._outEdges[currEdge]:
-##                            if not edge in queue:
-##                                pred[edge] = currEdge
-##                                queue.append(edge)
-##            if routeFound:
-##                self.printRouteAndUpdateFlow(pred, currEdge)
+    def updateFlow(self, pred):
+        flowDelta = sys.maxint
+        currVertex = self._sink
+        while not currVertex == self._source:
+            currEdge = pred[currVertex]
+            if currEdge.target == currVertex:
+                flowDelta = min(flowDelta, currEdge.capacity - currEdge.flow)
+                currVertex = currEdge.source
+            else:
+                flowDelta = min(flowDelta, currEdge.flow)
+                currVertex = currEdge.target
+        currVertex = self._sink
+        while not currVertex == self._source:
+            currEdge = pred[currVertex]
+            if currEdge.target == currVertex:
+                currEdge.flow += flowDelta
+                currVertex = currEdge.source
+            else:
+                currEdge.flow -= flowDelta
+                currVertex = currEdge.target
 
-    def printRouteAndUpdateFlow(self, pred, currEdge):
+    def adaptCapacities(self):
+        pathFound = True
+        while pathFound:
+            pathFound = False
+            queue = [self._source]
+            pred = {}
+            seenVertices = set()
+            while len(queue) > 0:
+                currVertex = queue.pop(0)
+                if currVertex == self._sink:
+                    self.updateFlow(pred)
+                    pathFound = True
+                    break
+                seenVertices.add(currVertex)
+                for edge in self._outEdges[currVertex]:
+                    if not edge.target in seenVertices and edge.flow < edge.capacity:
+                        queue.append(edge.target)
+                        pred[edge.target] = edge
+                for edge in self._inEdges[currVertex]:
+                    if not edge.source in seenVertices and edge.flow > 0:
+                        queue.append(edge.source)
+                        pred[edge.source] = edge
+        for edge in self._auxEdges + self._edges.values():
+            edge.capacity = edge.flow
+            edge.flow = 0
+
+    def printRouteAndUpdateFlow(self, pred, endEdge):
         route = ''
         flowDelta = sys.maxint
-        while not currEdge in self._sourceEdges:
-            route = currEdge + " " + route
-            flowDelta = min(flowDelta, self._edgeCap[currEdge] - self._edgeFlow[currEdge])
+        currEdge = endEdge
+        while not currEdge.source == self._source:
+            if currEdge.kind == "real":
+                route = currEdge.label + " " + route
+            flowDelta = min(flowDelta, currEdge.capacity - currEdge.flow)
             currEdge = pred[currEdge]
-        route = currEdge + " " + route
-        flowDelta = min(flowDelta, self._edgeCap[currEdge] - self._edgeFlow[currEdge])
-        for edge in route.split():
-            self._edgeFlow[currEdge] = self._edgeFlow[currEdge] + flowDelta
+        flowDelta = min(flowDelta, currEdge.capacity - currEdge.flow)
+        currEdge = endEdge
+        while not currEdge.source == self._source:
+            currEdge.flow += flowDelta
+            currEdge = pred[currEdge]
+        currEdge.flow += flowDelta
         print route, flowDelta
 
     def calcRoutes(self):
-##        self.adaptCapacities()
-        self._edgeFlow.clear()
+        self.adaptCapacities()
         routeFound = True
         while routeFound:
             routeFound = False
-            queue = list(self._sourceEdges)
+            queue = list(self._outEdges[self._source])
             pred = {}
+            seenVertices = set()
+            seenVertices.add(self._source)
             while len(queue) > 0:
                 currEdge = queue.pop(0)
-                if currEdge in self._edgeCap:
-                    if not currEdge in self._edgeFlow:
-                        self._edgeFlow[currEdge] = 0
-                    if self._edgeCap[currEdge] > self._edgeFlow[currEdge]:
-                        if currEdge in self._sinkEdges:
-                            routeFound = True
-                            break
-                        for edge in self._outEdges[currEdge]:
-                            if not edge in queue:
-                                pred[edge] = currEdge
-                                queue.append(edge)
-            if routeFound:
-                self.printRouteAndUpdateFlow(pred, currEdge)
+                if currEdge.kind != "real" or currEdge.flow < currEdge.capacity:
+                    if currEdge.target == self._sink:
+                        routeFound = True
+                        self.printRouteAndUpdateFlow(pred, currEdge)
+                        break
+                    for edge in self._outEdges[currEdge.target]:
+                        if not edge.target in seenVertices:
+                            pred[edge] = currEdge
+                            queue.append(edge)
 
 
             
@@ -140,5 +248,5 @@ parser.setContentHandler(net)
 parser.parse(sys.argv[1])
 parser.parse(sys.argv[2])
 net.readFlows(sys.argv[3])
-##net.printCaps()
-net.calcRoutes()
+if net.checkNet(False):
+    net.calcRoutes()
