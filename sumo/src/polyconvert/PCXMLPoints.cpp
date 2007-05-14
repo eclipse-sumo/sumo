7ddc28e243f586918467606e0b69310d45bc0f2f
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-05-14 06:44:03 +0000

    debugged building
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@3939 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/polyconvert/PCXMLPoints.cpp
@@ -0,0 +1,154 @@
+/****************************************************************************/
+/// @file    PCXMLPoints.cpp
+/// @author  Daniel Krajzewicz
+/// @date    Thu, 02.11.2006
+/// @version $Id: PCXMLPoints.cpp 3547 2007-02-21 14:38:24 +0100 (Mi, 21 Feb 2007) dkrajzew $
+///
+// A reader of pois stored in XML-format
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+// ===========================================================================
+// compiler pragmas
+// ===========================================================================
+#ifdef _MSC_VER
+#pragma warning(disable: 4786)
+#endif
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef WIN32
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <map>
+#include <fstream>
+#include <utils/common/UtilExceptions.h>
+#include <utils/common/MsgHandler.h>
+#include <utils/common/StringUtils.h>
+#include <utils/common/TplConvert.h>
+#include <utils/common/ToString.h>
+#include <utils/options/OptionsSubSys.h>
+#include <utils/options/OptionsCont.h>
+#include <utils/options/Option.h>
+#include <utils/importio/LineReader.h>
+#include <utils/common/StdDefs.h>
+#include <utils/gfx/GfxConvHelper.h>
+#include <polyconvert/PCPolyContainer.h>
+#include "PCXMLPoints.h"
+#include <utils/gfx/RGBColor.h>
+#include <utils/geom/GeomHelper.h>
+#include <utils/geom/Boundary.h>
+#include <utils/geom/Position2D.h>
+#include <utils/geoconv/GeoConvHelper.h>
+#include <utils/common/XMLHelpers.h>
+
+#ifdef CHECK_MEMORY_LEAKS
+#include <foreign/nvwa/debug_new.h>
+#endif // CHECK_MEMORY_LEAKS
+
+
+// ===========================================================================
+// used namespaces
+// ===========================================================================
+using namespace std;
+
+
+// ===========================================================================
+// method defintions
+// ===========================================================================
+PCXMLPoints::PCXMLPoints(PCPolyContainer &toFill,
+                         PCTypeMap &tm, OptionsCont &oc)
+        : SUMOSAXHandler("xml-poi-definition"),
+        myCont(toFill), myTypeMap(tm), myOptions(oc)
+{}
+
+
+PCXMLPoints::~PCXMLPoints()
+{}
+
+
+void
+PCXMLPoints::load(OptionsCont &oc)
+{
+    XMLHelpers::runParser(*this, oc.getString("xml-points"));
+}
+
+
+void 
+PCXMLPoints::myStartElement(int element, const std::string &name,
+                            const Attributes &attrs)
+{
+    if(name!="poi") {
+        return;
+    }
+    string id = getStringSecure(attrs, SUMO_ATTR_ID, "");
+    string type = getStringSecure(attrs, SUMO_ATTR_TYPE, "");
+    SUMOReal x = getFloatSecure(attrs, SUMO_ATTR_X, -1);
+    SUMOReal y = getFloatSecure(attrs, SUMO_ATTR_Y, -1);
+    x *= 100000.0;
+    y *= 100000.0;
+    Position2D pos(y, x); // !!! reverse!
+    GeoConvHelper::x2cartesian(pos);
+    // check the poi
+    if (id=="") {
+        MsgHandler::getErrorInstance()->inform("The name of a poi is missing.");
+        return;
+    }
+    // patch the values
+    bool discard = false;
+    int layer = myOptions.getInt("layer");
+    RGBColor color;
+    if (myTypeMap.has(type)) {
+        const PCTypeMap::TypeDef &def = myTypeMap.get(type);
+        id = def.prefix + id;
+        type = def.id;
+        color = GfxConvHelper::parseColor(def.color);
+        discard = def.discard;
+        layer = def.layer;
+    } else {
+        id = myOptions.getString("prefix") + id;
+        type = myOptions.getString("type");
+        color = GfxConvHelper::parseColor(myOptions.getString("color"));
+    }
+    if (!discard) {
+        bool ignorePrunning = false;
+        if (OptionsSubSys::helper_CSVOptionMatches("prune.ignore", id)) {
+            ignorePrunning = true;
+        }
+        PointOfInterest *poi = new PointOfInterest(id, type, pos, color);
+        myCont.insert(id, poi, layer, ignorePrunning);
+    }
+}
+
+
+void 
+PCXMLPoints::myCharacters(int , const std::string &,
+                          const std::string &)
+{
+}
+
+
+void 
+PCXMLPoints::myEndElement(int , const std::string &)
+{
+}
+
+
+
+/****************************************************************************/
+
---
+++ sumo/src/polyconvert/PCXMLPoints.h
@@ -0,0 +1,110 @@
+/****************************************************************************/
+/// @file    PCXMLPoints.h
+/// @author  Daniel Krajzewicz
+/// @date    Thu, 02.11.2006
+/// @version $Id: PCXMLPoints.h 3547 2007-02-21 14:38:24 +0100 (Mi, 21 Feb 2007) dkrajzew $
+///
+// A reader of pois stored in XML-format
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef PCXMLPoints_h
+#define PCXMLPoints_h
+// ===========================================================================
+// compiler pragmas
+// ===========================================================================
+#ifdef _MSC_VER
+#pragma warning(disable: 4786)
+#endif
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef WIN32
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include "PCPolyContainer.h"
+#include "PCTypeMap.h"
+#include <utils/xml/SUMOSAXHandler.h>
+
+
+// ===========================================================================
+// xerces 2.2 compatibility
+// ===========================================================================
+#if defined(XERCES_HAS_CPP_NAMESPACE)
+using namespace XERCES_CPP_NAMESPACE;
+#endif
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+class OptionsCont;
+
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+/**
+ * @class PCXMLPoints
+ * @brief A reader of pois stored in XML-format
+ *
+ * Reads pois stored as XML definition.
+ */
+class PCXMLPoints : public SUMOSAXHandler
+{
+public:
+    /// Constructor
+    PCXMLPoints(PCPolyContainer &toFill,
+                  PCTypeMap &tm, OptionsCont &oc);
+
+    /// Destructor
+    ~PCXMLPoints();
+
+    /// loads Elmar's data parsed from GDF
+    void load(OptionsCont &oc);
+
+protected:
+    /** implementation of the GenericSAXHandler-myStartElement - interface */
+    virtual void myStartElement(int element, const std::string &name,
+                                const Attributes &attrs);
+
+    /** implementation of the GenericSAXHandler-myCharacters - interface */
+    virtual void myCharacters(int element, const std::string &name,
+                              const std::string &chars);
+
+    /** implementation of the GenericSAXHandler-myEndElement - interface */
+    virtual void myEndElement(int element, const std::string &name);
+
+
+private:
+    /// The container to store the converted polygons into
+    PCPolyContainer &myCont;
+
+    /// The type map to use
+    PCTypeMap &myTypeMap;
+
+    /// Settings to use
+    OptionsCont &myOptions;
+
+};
+
+
+#endif
+
+/****************************************************************************/
+
