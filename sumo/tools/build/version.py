4aac51ebdc1c334d271a90a9fa810927c7eeb030
Michael Behrisch <oss@behrisch.de>
2007-06-05 08:52:13 +0000

    unneeeded libs removed, version info prepared
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4058 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/build/version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os.path, sys
+
+versionFile = os.path.dirname(sys.argv[0]) + "/../../src/version.h"
+svnRevision = "UNKNOWN"
+entries = open(".svn/entries")
+for l in entries:
+    revIndex = l.find('revision="')
+    if revIndex >= 0:
+        revIndex += 10
+	svnRevision = l[revIndex:l.index('"', revIndex)]
+	break
+entries.close()
+
+version = open(versionFile, 'w')
+print >> version, '#define VERSION_STRING "dev-SVN-r%s"' % svnRevision
+version.close()
