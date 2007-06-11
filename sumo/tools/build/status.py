659729c80db7297ba99a9a716ebc9d46141b935d
Michael Behrisch <oss@behrisch.de>
2007-06-11 10:39:40 +0000

    mail if build fails
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4118 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/status.py
+++ sumo/tools/build/status.py
@@ -4,6 +4,7 @@ import os, sys, smtplib
 from os.path import basename, join, expandvars, exists
 from datetime import datetime
 
+failed = ""
 makelog = sys.argv[1]
 makealllog = sys.argv[2]
 print basename(makelog), datetime.now().ctime()
@@ -16,9 +17,11 @@ for l in file(makelog):
         warnings = warnings + 1
     if l.find("error ") > -1:
         errors = errors + 1
+        failed += l
 print warnings, "warnings"
 if errors:
     print errors, "errors"
+    failed += "make failed\n\n"
 print "--"
 if len(sys.argv) > 3:
     texttesttmp = sys.argv[3]
@@ -39,23 +42,33 @@ for l in file(makealllog):
         warnings = warnings + 1
     if l.find("error ") > -1:
         errors = errors + 1
+        failed += l
 print warnings, "warnings"
 if errors:
     print errors, "errors"
+    failed += "make debug failed\n\n"
 print "--"
 
-if errors > 0 and len(sys.argv) > 4 and exists(sys.argv[4]):
+if failed and len(sys.argv) > 4 and exists(sys.argv[4]):
     config = open(sys.argv[4])
     smtp_server = None
-    toaddrs = None        
+    to_addrs = None        
     for line in config:
         option = line.split(":")
         if option[0] == "smtp_server":
-            smtp_server = expandvars(option[1])
+            smtp_server = expandvars(option[1]).strip()
         if option[0] == "batch_recipients":
-            toaddrs = expandvars(option[1])
+            to_addrs = [a.strip() for a in expandvars(option[1]).split(",")]
     config.close()
-    if smtp_server and toaddrs:
-        server = smtplib.SMTP(smtp_server)
-        server.sendmail("msvc8test@TTSTest", toaddrs, "Error occured while building.\n")
+    if smtp_server and to_addrs:
+        from_addr = "michael.behrisch@dlr.de"
+        message = """\
+From: %s
+To: %s
+Subject: Error occured while building.
+
+%s
+""" % (from_addr, ", ".join(to_addrs), failed)
+        server = smtplib.SMTP(smtp_server)        
+        server.sendmail(from_addr, to_addrs, message)
         server.quit()
