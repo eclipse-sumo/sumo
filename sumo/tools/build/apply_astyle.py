5f810ebc78667c8e4911ebf1710f5546f0f33d08
Michael Behrisch <oss@behrisch.de>
2007-06-12 06:21:00 +0000

    astyle call path independent
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4121 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/apply_astyle.py
+++ sumo/tools/build/apply_astyle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-import os
-for root, dirs, files in os.walk("../../src/"):
+import os, os.path, sys
+srcRoot = os.path.join(os.path.dirname(sys.argv[0]), "../../src/")
+for root, dirs, files in os.walk(srcRoot):
     for name in files:
         if name.endswith(".h") or name.endswith(".cpp"):
             os.system("astyle --style=kr -U -l -n " + os.path.join(root, name))
