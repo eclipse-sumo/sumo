54232f9274a433a34ffaf9f3743acbc933cb02c0
Michael Behrisch <oss@behrisch.de>
2007-06-13 12:28:11 +0000

    flexible test script
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4135 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/runTests.sh
+++ sumo/tests/runTests.sh
@@ -6,27 +6,27 @@
 
 OLDDIR=$PWD
 cd `dirname $0`
-export TEXTTEST_HOME=$PWD
+export TEXTTEST_HOME="$PWD"
 if test x"$1" = x; then
   cd ..
-  SUMO_BIN=$PWD/src
+  SUMO_BIN="$PWD/src"
 else
-  SUMO_BIN=$1
+  SUMO_BIN="$1"
 fi
 cd $OLDDIR
-export DFROUTER_BINARY=$SUMO_BIN/sumo-dfrouter
-export DUAROUTER_BINARY=$SUMO_BIN/sumo-duarouter
-export JTRROUTER_BINARY=$SUMO_BIN/sumo-jtrrouter
-export NETCONVERT_BINARY=$SUMO_BIN/sumo-netconvert
-export NETGEN_BINARY=$SUMO_BIN/sumo-netgen
-export OD2TRIPS_BINARY=$SUMO_BIN/sumo-od2trips
-export SUMO_BINARY=$SUMO_BIN/sumo
+export DFROUTER_BINARY="$SUMO_BIN/sumo-dfrouter"
+export DUAROUTER_BINARY="$SUMO_BIN/sumo-duarouter"
+export JTRROUTER_BINARY="$SUMO_BIN/sumo-jtrrouter"
+export NETCONVERT_BINARY="$SUMO_BIN/sumo-netconvert"
+export NETGEN_BINARY="$SUMO_BIN/sumo-netgen"
+export OD2TRIPS_BINARY="$SUMO_BIN/sumo-od2trips"
+export SUMO_BINARY="$SUMO_BIN/sumo"
 
 if test x"$2" = x; then
   TEXTTEST=texttest.py
 else
-  TEXTTEST=$2
+  TEXTTEST="$2"
 fi
 
-$TEXTTEST -gx
+$TEXTTEST
 
