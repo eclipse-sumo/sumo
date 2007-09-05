08f7cd4f7a15a7a9d98e9d588056b601e4352d3d
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-09-05 07:12:39 +0000

    adding switched off traffic lights (tls API changes, in work)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4415 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/traffic_lights/MSOffTrafficLightLogic.cpp
@@ -0,0 +1,159 @@
+/****************************************************************************/
+/// @file    MSOffTrafficLightLogic.cpp
+/// @author  Daniel Krajzewicz
+/// @date    08.05.2007
+/// @version $Id: MSOffTrafficLightLogic.cpp 3486 2007-02-19 13:38:00 +0100 (Mo, 19 Feb 2007) behrisch $
+///
+// A traffic lights logic which represents a tls in an off-mode
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <cassert>
+#include <utility>
+#include <vector>
+#include <bitset>
+#include <sstream>
+#include <microsim/MSEventControl.h>
+#include "MSTrafficLightLogic.h"
+#include "MSOffTrafficLightLogic.h"
+
+#ifdef CHECK_MEMORY_LEAKS
+#include <foreign/nvwa/debug_new.h>
+#endif // CHECK_MEMORY_LEAKS
+
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+MSOffTrafficLightLogic::MSOffTrafficLightLogic(MSTLLogicControl &tlcontrol,
+        const std::string &id)
+        : MSTrafficLightLogic(tlcontrol, id, "off", 0)
+{
+}
+
+
+MSOffTrafficLightLogic::~MSOffTrafficLightLogic()
+{
+}
+
+
+SUMOTime
+MSOffTrafficLightLogic::trySwitch(bool)
+{
+    return 0;
+}
+
+
+size_t
+MSOffTrafficLightLogic::getStepNo() const
+{
+    return 0;
+}
+
+size_t
+MSOffTrafficLightLogic::getCycleTime()
+{
+    return 0;
+}
+
+size_t
+MSOffTrafficLightLogic::getPosition(SUMOTime simStep)
+{
+    return 0;
+}
+
+size_t
+MSOffTrafficLightLogic::getStepFromPos(size_t position)
+{
+    return 0;
+}
+
+size_t
+MSOffTrafficLightLogic::getPosFromStep(size_t step)
+{
+    return 0;
+}
+
+
+const MSOffTrafficLightLogic::Phases &
+MSOffTrafficLightLogic::getPhases() const
+{
+    throw 1;
+}
+
+const MSPhaseDefinition &
+MSOffTrafficLightLogic::getPhaseFromStep(size_t givenStep) const
+{
+    throw 1;
+}
+
+
+void 
+MSOffTrafficLightLogic::setLinkPriorities() const
+{
+}
+
+
+bool 
+MSOffTrafficLightLogic::maskRedLinks() const
+{
+    return true;
+}
+
+
+bool 
+MSOffTrafficLightLogic::maskYellowLinks() const
+{
+    return true;
+}
+
+
+MSPhaseDefinition 
+MSOffTrafficLightLogic::getCurrentPhaseDef() const
+{
+    size_t no = getLinks().size();
+    std::bitset<64> driveMaskArg;
+    std::bitset<64> breakMaskArg;
+    std::bitset<64> yellowMaskArg;
+    for(size_t i=0; i<no; ++i) {
+        driveMaskArg[i] = true;
+        breakMaskArg[i] = false;
+        yellowMaskArg[i] = false;
+    }
+    return MSPhaseDefinition(-1, driveMaskArg, breakMaskArg, yellowMaskArg);
+}
+
+
+std::string
+MSOffTrafficLightLogic::buildStateList() const
+{
+    std::ostringstream strm;
+    size_t no = getLinks().size();
+    for(size_t i=0; i<no; ++i) {
+        strm << 'O';
+    }
+    return strm.str();
+}
+
+
+
+/****************************************************************************/
+
---
+++ sumo/src/microsim/traffic_lights/MSOffTrafficLightLogic.h
@@ -0,0 +1,115 @@
+/****************************************************************************/
+/// @file    MSOffTrafficLightLogic.h
+/// @author  Daniel Krajzewicz
+/// @date    08.05.2007
+/// @version $Id: MSOffTrafficLightLogic.h 3486 2007-02-19 13:38:00 +0100 (Mo, 19 Feb 2007) behrisch $
+///
+// A traffic lights logic which represents a tls in an off-mode
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// copyright : (C) 2001-2007
+//  by DLR (http://www.dlr.de/) and ZAIK (http://www.zaik.uni-koeln.de/AFS)
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef MSOffTrafficLightLogic_h
+#define MSOffTrafficLightLogic_h
+// ===========================================================================
+// compiler pragmas
+// ===========================================================================
+#ifdef _MSC_VER
+#pragma warning(disable: 4786)
+#endif
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef WIN32
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <utility>
+#include <vector>
+#include <bitset>
+#include <microsim/MSEventControl.h>
+#include <microsim/MSNet.h>
+#include "MSTrafficLightLogic.h"
+#include "MSPhaseDefinition.h"
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class MSOffTrafficLightLogic
+ * @brief A traffic lights logic which represents a tls in an off-mode
+ */
+class MSOffTrafficLightLogic : public MSTrafficLightLogic
+{
+public:
+    /// definition of a list of phases, being the junction logic
+    typedef std::vector<MSPhaseDefinition*> Phases;
+
+public:
+    /// constructor
+    MSOffTrafficLightLogic(MSTLLogicControl &tlcontrol,
+                           const std::string &id);
+
+    /// destructor
+    ~MSOffTrafficLightLogic();
+
+    /** @brief Switches to the next phase
+        Returns the time of the next switch */
+    SUMOTime trySwitch(bool isActive);
+
+    /// Returns the priorities for all lanes for the current phase
+    const std::bitset<64> &linkPriorities() const;
+
+    /// Returns a bitset where all links having yellow are set
+    const std::bitset<64> &yellowMask() const;
+
+    const std::bitset<64> &allowed() const;
+
+    /// returns the current step
+    size_t getStepNo() const;
+
+    /// returns the cycletime
+    size_t getCycleTime() ;
+
+    /// returns the position of the logic at the actual step of the simulation
+    size_t getPosition(SUMOTime simStep);
+
+    /// returns the step (the phasenumber) of a given position of the cycle
+    size_t getStepFromPos(size_t position);
+
+    /// returns the position (start of a phase during a cycle) from of a given step
+    size_t getPosFromStep(size_t step);
+
+    /// Returns the phases of this tls
+    const Phases &getPhases() const;
+
+    /// Returns the phase of a given step
+    const MSPhaseDefinition &getPhaseFromStep(size_t givenstep) const;
+
+    void setLinkPriorities() const;
+    bool maskRedLinks() const;
+    bool maskYellowLinks() const;
+    MSPhaseDefinition getCurrentPhaseDef() const;
+    void changeStepAndDuration(MSTLLogicControl &tlcontrol, SUMOTime simStep, int step, SUMOTime stepDuration) {
+    }
+    std::string buildStateList() const;
+};
+
+
+#endif
+
+/****************************************************************************/
+
