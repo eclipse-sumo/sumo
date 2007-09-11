150068d7c8fe366c154b731c56d997c60f50a3d1
Michael Behrisch <oss@behrisch.de>
2007-09-11 15:11:18 +0000

    gcc needs help with typenames
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4462 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/VectorHelper.h
+++ sumo/src/utils/common/VectorHelper.h
@@ -54,7 +54,7 @@ class VectorHelper
 public:
     static T sum(const std::vector<T> &v) {
         T sum = 0;
-        for (std::vector<T>::const_iterator i=v.begin(); i!=v.end(); i++) {
+        for (typename std::vector<T>::const_iterator i=v.begin(); i!=v.end(); i++) {
             sum += *i;
         }
         return sum;
@@ -74,15 +74,15 @@ public:
     }
 
     static void div(std::vector<T> &v, T by) {
-        for (std::vector<T>::iterator i=v.begin(); i!=v.end(); i++) {
+        for (typename std::vector<T>::iterator i=v.begin(); i!=v.end(); i++) {
             *i /= by;
         }
     }
 
     static void removeDouble(std::vector<T> &v) {
-        std::vector<T>::iterator i=v.begin();
+        typename std::vector<T>::iterator i=v.begin();
         while (i!=v.end()) {
-            for (std::vector<T>::iterator j=i+1; j!=v.end();) {
+            for (typename std::vector<T>::iterator j=i+1; j!=v.end();) {
                 if (*i==*j) {
                     j = v.erase(j);
                 } else {
@@ -95,14 +95,14 @@ public:
 
 
     static void set(std::vector<T> &v, T to) {
-        for (std::vector<T>::iterator i=v.begin(); i!=v.end(); i++) {
+        for (typename std::vector<T>::iterator i=v.begin(); i!=v.end(); i++) {
             *i = to;
         }
     }
 
     static T maxValue(const std::vector<T> &v) {
         SUMOReal m = *(v.begin());
-        for (std::vector<T>::const_iterator j=v.begin()+1; j!=v.end(); j++) {
+        for (typename std::vector<T>::const_iterator j=v.begin()+1; j!=v.end(); j++) {
             if ((*j)>m) {
                 m = *j;
             }
@@ -112,7 +112,7 @@ public:
 
     static T minValue(const std::vector<T> &v) {
         SUMOReal m = *(v.begin());
-        for (std::vector<T>::const_iterator j=v.begin()+1; j!=v.end(); j++) {
+        for (typename std::vector<T>::const_iterator j=v.begin()+1; j!=v.end(); j++) {
             if ((*j)<m) {
                 m = *j;
             }
@@ -121,7 +121,7 @@ public:
     }
 
     static void remove_smaller_than(std::vector<T> &v, T swell) {
-        for (std::vector<T>::iterator j=v.begin(); j!=v.end();) {
+        for (typename std::vector<T>::iterator j=v.begin(); j!=v.end();) {
             if ((*j)<swell) {
                 j = v.erase(j);
             } else {
@@ -131,7 +131,7 @@ public:
     }
 
     static void remove_larger_than(std::vector<T> &v, T swell) {
-        for (std::vector<T>::iterator j=v.begin(); j!=v.end();) {
+        for (typename std::vector<T>::iterator j=v.begin(); j!=v.end();) {
             if ((*j)>swell) {
                 j = v.erase(j);
             } else {
@@ -141,14 +141,14 @@ public:
     }
 
     static void add2All(std::vector<T> &v, T what){
-        for (std::vector<T>::iterator j=v.begin(); j!=v.end(); j++) {
+        for (typename std::vector<T>::iterator j=v.begin(); j!=v.end(); j++) {
             (*j) += what;
         }
     }
 
     /// Returns the information whether at least one element is within both vectors
     static bool subSetExists(const std::vector<T> &v1, const std::vector<T> &v2) {
-        for (std::vector<T>::const_iterator i=v1.begin(); i!=v1.end(); i++) {
+        for (typename std::vector<T>::const_iterator i=v1.begin(); i!=v1.end(); i++) {
             int val1 = (*i);
             if (find(v2.begin(), v2.end(), val1)!=v2.end()) {
                 return true;
@@ -163,8 +163,8 @@ public:
 
 template<class T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-    for (std::vector<T>::const_iterator i=v.begin(); i!=v.end(); i++) {
-        if (i!=iv.begin()) {
+    for (typename std::vector<T>::const_iterator i=v.begin(); i!=v.end(); i++) {
+        if (i!=v.begin()) {
             os << ", ";
         }
         os << (*i);
