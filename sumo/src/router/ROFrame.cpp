5dfbbb8c5f8759ecc5821ca75415d6a2b3314c97
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2007-10-12 10:07:12 +0000

    set the end time step to 86400...
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@4570 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router/ROFrame.cpp
+++ sumo/src/router/ROFrame.cpp
@@ -80,7 +80,7 @@ ROFrame::fillOptions(OptionsCont &oc)
     oc.doRegister("begin", 'b', new Option_Integer(0));
     oc.addDescription("begin", "Time", "Defines the begin time; Previous trips will be discarded");
 
-    oc.doRegister("end", 'e', new Option_Integer(864000));
+    oc.doRegister("end", 'e', new Option_Integer(86400));
     oc.addDescription("end", "Time", "Defines the end time; Later trips will be discarded");
 
 
