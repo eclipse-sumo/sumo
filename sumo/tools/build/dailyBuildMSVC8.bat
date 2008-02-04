4ee54f6aed81270a1cd15db256206fe6f7e3ffc1
Michael Behrisch <oss@behrisch.de>
2008-02-04 13:12:19 +0000

    simplifying daily build
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5019 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/dailyBuildMSVC8.bat
+++ sumo/tools/build/dailyBuildMSVC8.bat
@@ -7,27 +7,24 @@ SET MAKEALLLOG=%FILEPREFIX%buildDebug.log
 SET SUMO_BATCH_RESULT=%FILEPREFIX%batch_result
 SET SUMO_REPORT=%FILEPREFIX%report
 SET SMTP_SERVER=129.247.218.247
-SET REMOTE_DIR=D:\Sumo\copytest
 
 cd ..\..
+del %MAKELOG% %MAKEALLLOG%
 svn.exe up > %MAKELOG%
 
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Release|Win32" build\msvc8\prj.sln /out %MAKELOG%
-move /Y %MAKELOG% %REMOTE_DIR%
-
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Debug|Win32" build\msvc8\prj.sln /out %MAKEALLLOG%
-move /Y %MAKEALLLOG% %REMOTE_DIR%
 
 REM run tests
 cd tests
 SET TEXTTEST_TMP=%CD%\texttesttmp
 rmdir /S /Q %TEXTTEST_TMP%
+rmdir /S /Q %SUMO_REPORT%
+mkdir %SUMO_REPORT%
 call testEnv.bat
 texttest.py -b %SUMO_REPORT%
 texttest.py -b %SUMO_REPORT% -coll
-mkdir %REMOTE_DIR%\%SUMO_REPORT%
-xcopy /Y /E %SUMO_REPORT% %REMOTE_DIR%\%SUMO_REPORT%
-cd ..\tools\build
-status.py %REMOTE_DIR%\%MAKELOG% %REMOTE_DIR%\%MAKEALLLOG% %TEXTTEST_TMP% %SMTP_SERVER% > %REMOTE_DIR%\%STATUSLOG%
+cd ..
+tools\build\status.py %MAKELOG% %MAKEALLLOG% %TEXTTEST_TMP% %SMTP_SERVER% > %STATUSLOG%
 
-WinSCP3.com behrisch@shell.sf.net /privatekey=%REMOTE_DIR%\..\key.ppk /command "option batch on" "option confirm off" "cd sumo-www" "cd daily" "put %REMOTE_DIR%\*.*" "exit"
+WinSCP3.com behrisch@shell.sf.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% sumo-www/daily/" "exit"
