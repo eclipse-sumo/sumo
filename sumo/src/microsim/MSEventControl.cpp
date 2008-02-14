64ff18a66a4db155cd40ee49ae5ed9cffce1a12b
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-02-14 07:13:05 +0000

    made behaviour on negative values returned by Command::execute not undefined (warning is generated, command deleted)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5065 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSEventControl.cpp
+++ sumo/src/microsim/MSEventControl.cpp
@@ -98,10 +98,12 @@ MSEventControl::execute(SUMOTime execTime) throw(ProcessError)
 
             // Delete nonrecurring events, reinsert recurring ones
             // with new execution time = execTime + returned offset.
-            if (time == 0) {
+            if (time <= 0) {
+                if(time<0) {
+                    WRITE_WARNING("Command returned negative repeat number; will be deleted.");
+                }
                 delete currEvent.first;
             } else {
-                assert(time > 0);
                 currEvent.second = execTime + time;
                 myEvents.push(currEvent);
             }
