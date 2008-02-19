3a25dd5982c9245367211e91e6ea936265f62170
Michael Behrisch <oss@behrisch.de>
2008-02-19 15:58:31 +0000

    Removed misleading "error" about shapes with a single point
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5090 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netimport/NIXMLEdgesHandler.cpp
+++ sumo/src/netimport/NIXMLEdgesHandler.cpp
@@ -574,10 +574,6 @@ NIXMLEdgesHandler::tryGetShape(const Attributes &attrs)
             GeoConvHelper::x2cartesian(pos);
             shape.push_back(pos);
         }
-
-        if (shape.size()==1) {
-            MsgHandler::getErrorInstance()->inform("The shape of edge '" + myCurrentID + "' has only one entry.");
-        }
         return shape;
     } catch (EmptyData &) {
         MsgHandler::getErrorInstance()->inform("At least one number is missing in shape definition for edge '" + myCurrentID + "'.");
