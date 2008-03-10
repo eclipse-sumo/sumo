549a450d986d3cee052f3feb0aa99b2da6207081
Michael Behrisch <oss@behrisch.de>
2008-03-10 23:36:14 +0000

    invalid directory reference
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5141 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/run.py
+++ sumo/tools/assign/run.py
@@ -1,4 +1,4 @@
-import glob, os, shutil, subprocess
+import glob, os, shutil, subprocess, time
 def makeAndChangeDir(dirName):
     runID = 1
     fullName = "%s%03i" % (dirName, runID)
@@ -23,7 +23,9 @@ duaDir = makeAndChangeDir("../dua")
 duaProcess = subprocess.Popen("dua-iterate.py -n %s -l 20 -t ../input/trips.trips.xml %s" % (netFile, pyAdds), shell=True)
 succDir = makeAndChangeDir("../successive")
 os.system("inc_assignsumo.py -c ../input/districts.xml -m %s -n %s -p ../parameter.txt -u ../CRcurve.txt" % (mtxNamesList, netFile))
-shutil.copy("../%s/trips_0.rou.xml" % duaDir, "../input/routes.rou.xml")
+while not os.path.exists("%s/trips_0.rou.xml" % duaDir):
+    time.sleep(1)
+shutil.copy("%s/trips_0.rou.xml" % duaDir, "../input/routes.rou.xml")
 shotDir = makeAndChangeDir("../oneshot")
 os.system("one-shot.py -n %s -t ../input/routes.rou.xml %s" % (netFile, pyAdds))
 
@@ -33,7 +35,7 @@ os.system("sumo --no-step-log -n %s -e 90000 -r %s/trips_19.rou.xml --vehroute-o
 os.system("GlobalMOE.py -n %s -c ../input/districts.xml -x vehroutes_dua.xml -o Global_MOE_%s.txt" % (netFile, os.path.basename(duaDir)))
 for step in [-1, 3600, 1800, 900, 300, 150, 90, 60, 30, 15]:
     shutil.copy("%s/vehroutes_%s.xml" % (shotDir, step), "vehroutes_oneshot%s.xml" % step)
-    os.system("GlobalMOE.py -n %s -c ../input/districts.xml -x vehroutes_oneshot%s.xml -o Global_MOE_%s_%s.txt" % (netFile, os.path.basename(shotDir), step))
+    os.system("GlobalMOE.py -n %s -c ../input/districts.xml -x vehroutes_oneshot%s.xml -o Global_MOE_%s_%s.txt" % (netFile, step, os.path.basename(shotDir), step))
 os.system("sumo --no-step-log -n %s -e 90000 -r %s/routes.txt --vehroute-output vehroutes_successive.xml %s" % (netFile, succDir, sumoAdds))
 os.system("GlobalMOE.py -n %s -c ../input/districts.xml -x vehroutes_successive.xml -o Global_MOE_%s.txt" % (netFile, os.path.basename(succDir)))
 os.chdir("..")
