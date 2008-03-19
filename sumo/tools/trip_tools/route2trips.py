5cb83965d167155cac4f074119f88f744de3c1ad
Michael Behrisch <oss@behrisch.de>
2008-03-19 17:07:57 +0000

    optional vtype
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5205 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/trip_tools/route2trips.py
+++ sumo/tools/trip_tools/route2trips.py
@@ -12,39 +12,41 @@ class RouteReader(handler.ContentHandler):
     def __init__(self):
         self._vType = ''
         self._vID = ''
+        self._vDepart = 0
         self._routeID = ''
         self._routeString = ''
-	self._routes = {}
+        self._routes = {}
         
     def startElement(self, name, attrs):
         if name == 'vehicle':
-            self._vType = attrs['type']
+            if 'type' in attrs:
+                self._vType = ' type="%s"' % attrs['type']
             self._vID = attrs['id']
             self._vDepart = attrs['depart']
-	    if 'route' in attrs:
-        	self._routeString = self._routes[attrs['route']]
+            if 'route' in attrs:
+                self._routeString = self._routes[attrs['route']]
         elif name == 'route':
             if not self._vID:
                 self._routeID = attrs['id']
             self._routeString = ''
         elif name == 'routes':
-	    print "<tripdefs>"
+            print "<tripdefs>"
 
     def endElement(self, name):
         if name == 'route':
             if not self._vID:
-    		self._routes[self._routeID] = self._routeString
+                self._routes[self._routeID] = self._routeString
                 self._routeString = ''
             self._routeID = ''
         elif name == 'vehicle':
-	    edges = self._routeString.split()
-	    print '    <tripdef depart="%s" from="%s" to="%s" type="%s"/>'\
-	          % (self._vDepart, edges[0], edges[-1], self._vType)
+            edges = self._routeString.split()
+            print '    <tripdef depart="%s" from="%s" to="%s"%s/>'\
+                  % (self._vDepart, edges[0], edges[-1], self._vType)
             self._vType = ''
             self._vID = ''
             self._routeString = ''
         elif name == 'routes':
-	    print "</tripdefs>"
+            print "</tripdefs>"
 
     def characters(self, content):
         self._routeString += content
