e94eb9584a89e05d9b5636ebcfcd9716bb367019
Michael Behrisch <oss@behrisch.de>
2008-03-19 16:56:48 +0000

    correcting names for dua routes
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5204 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/run.py
+++ sumo/tools/assign/run.py
@@ -28,9 +28,9 @@ if addFiles:
     pyAdds = "-+ %s" % addFiles
     sumoAdds = "-a %s" % addFiles
 if options.od2trips:
-    trips = "../input/trips.trips.xml"
+    trips = "trips"
 else:
-    trips = "../input/successive.trips.xml"
+    trips = "successive"
 routes = "../input/routes.rou.xml"
 
 succDir = makeAndChangeDir("../successive")
@@ -39,7 +39,7 @@ if not options.od2trips:
     shutil.copy("%s/routes.txt" % succDir, routes)
     os.system("route2trips.py %s > ../input/successive.trips.xml" % routes)
 duaDir = makeAndChangeDir("../dua")
-duaProcess = subprocess.Popen("dua-iterate.py -C -n %s -t %s %s" % (netFile, trips, pyAdds), shell=True)
+duaProcess = subprocess.Popen("dua-iterate.py -C -n %s -t ../input/%s.trips.xml %s" % (netFile, trips, pyAdds), shell=True)
 clogDir = makeAndChangeDir("../clogit")
 os.system("cLogit.py -d ../input/districts.xml -m %s -n %s -p ../clogit_parameter.txt -u ../CRcurve.txt" % (mtxNamesList, netFile))
 if options.od2trips:
@@ -52,7 +52,7 @@ os.system("one-shot.py -n %s -t %s %s" % (netFile, routes, pyAdds))
 duaProcess.wait()
 makeAndChangeDir("../routes")
 for step in [0, 24, 49]:
-    os.system("sumo --no-step-log -n %s -e 90000 -r %s/trips_%s.rou.xml --vehroute-output vehroutes_dua_%s.xml %s -l sumo_dua_%s.log" % (netFile, duaDir, step, step, sumoAdds, step))
+    os.system("sumo --no-step-log -n %s -e 90000 -r %s/%s_%s.rou.xml --vehroute-output vehroutes_dua_%s.xml %s -l sumo_dua_%s.log" % (netFile, duaDir, trips, step, step, sumoAdds, step))
     os.system("networkStatistics.py -n %s -d ../input/districts.xml -x vehroutes_dua_%s.xml -o networkStatistics_%s_%s.txt" % (netFile, step, os.path.basename(duaDir), step))
 for step in [-1, 1800, 300, 15]:
     shutil.copy("%s/vehroutes_%s.xml" % (shotDir, step), "vehroutes_oneshot%s.xml" % step)
