e207cdf3b7798435ae0f49fd5a36566983b24db2
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-04-08 11:24:01 +0000

    fast addition....
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5341 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/output_tools/netdumpdiv.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# This script reads two network dumps 
+#  and subtracts the values of the second 
+#  from the first writing the results
+#  into the output file
+import os, string, sys
+from optparse import OptionParser
+from xml.sax import saxutils, make_parser, handler
+from datetime import datetime
+
+
+class WeightsReader(handler.ContentHandler):
+    """Reads the dump file"""
+
+    def __init__(self):
+        self._id = ''
+        self._edgeValues = {}
+        self._intervalBegins = []
+        self._intervalEnds = []
+
+    def startElement(self, name, attrs):
+        if name == 'interval':
+            self._beginTime = int(attrs['begin'])
+            self._intervalBegins.append(self._beginTime)
+            self._intervalEnds.append(int(attrs['end']))
+            self._edgeValues[self._beginTime] = {}
+        if name == 'edge':
+            self._id = attrs['id']
+            self._edgeValues[self._beginTime][self._id] = {}
+            for attr in attrs.getQNames():
+                if attr!="id":
+                    self._edgeValues[self._beginTime][self._id][attr] = float(attrs[attr])
+    
+    def sub(self, weights):
+         for t in self._edgeValues:
+             for e in self._edgeValues[t]:
+                 for a in self._edgeValues[t][e]:
+                     self._edgeValues[t][e][a] = self._edgeValues[t][e][a] - weights._edgeValues[t][e][a]
+ 
+    def write(self, options):
+        fd = open(options.output, "w")
+        fd.write("<?xml version=\"1.0\"?>\n\n")
+        fd.write("<!-- generated on %s by netdumpdiv.py \n" % datetime.now())
+        fd.write("   -1 %s\n" % options.dump1)
+        fd.write("   -2 %s\n" % options.dump2)
+        fd.write("   -o %s\n" % options.output)
+        fd.write("-->\n\n")
+        fd.write("<netstats>\n")
+        for i in range(0, len(self._intervalBegins)):
+            fd.write("   <interval begin=\"%s\" end=\"%s\">\n" % (self._intervalBegins[i], self._intervalEnds[i]))
+            t = self._intervalBegins[i]
+            for e in self._edgeValues[t]:
+                fd.write("      <edge id=\"%s\"" % e)
+                for a in self._edgeValues[t][e]:
+                    fd.write(" %s=\"%s\"" % (a, self._edgeValues[t][e][a]))
+                fd.write("/>\n")
+            fd.write("   </interval>\n")
+        fd.write("</netstats>\n")
+
+
+# initialise 
+optParser = OptionParser()
+optParser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                     default=False, help="tell me what you are doing")
+    # i/o
+optParser.add_option("-1", "--dump1", dest="dump1",
+                     help="First dump (mandatory)", metavar="FILE")
+optParser.add_option("-2", "--dump2", dest="dump2",
+                     help="Second  dump (mandatory)", metavar="FILE")
+optParser.add_option("-o", "--output", dest="output",
+                     help="Name for the output", metavar="FILE")
+# parse options
+(options, args) = optParser.parse_args()
+
+
+parser = make_parser()
+# read dump1
+if options.verbose:
+    print "Reading dump1..."
+weights1 = WeightsReader()
+parser.setContentHandler(weights1)
+parser.parse(options.dump1)
+# read dump2
+if options.verbose:
+    print "Reading dump2..."
+weights2 = WeightsReader()
+parser.setContentHandler(weights2)
+parser.parse(options.dump2)
+# process
+if options.verbose:
+    print "Computing diff..."
+weights1.sub(weights2)
+# save
+if options.verbose:
+    print "Writing..."
+weights1.write(options)
+
