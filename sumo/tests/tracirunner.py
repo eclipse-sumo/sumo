7fc58682e4fcc17a02009b96915ed73d473f629f
Friedemann Wesner <wesner@itm.uni-luebeck.de>
2008-05-24 14:51:07 +0000

    adding further tests for traci commands
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5556 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/tracirunner.py
+++ sumo/tests/tracirunner.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 import os,subprocess,sys
 
-#start sumo as server
-serverprocess = subprocess.Popen(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(sys.argv[1:12])),
+numParams = 1
+serverParams = []
+clientParams = []
+
+for i in range(1, len(sys.argv)):
+    if sys.argv[i] == "endsumo":
+        numParams = numParams+1
+        break
+    if sys.argv[i] == "TraCITestClient.exe": 
+        break
+    else:
+        numParams = numParams+1
+        serverParams = serverParams[0:len(serverParams)] + [sys.argv[i]]
+        
+clientParams = sys.argv[numParams:len(sys.argv)]
+
+#start sumo as server    
+serverprocess = subprocess.Popen(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(serverParams)), 
+                shell=True, stdout=sys.stdout, stderr=sys.stderr)       
+#serverprocess = subprocess.Popen(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(sys.argv[1:12])),
+#                shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    
+subprocess.call(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(clientParams)),
                 shell=True, stdout=sys.stdout, stderr=sys.stderr)
-                
-#start a traci client which connects to the server
-subprocess.call(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(sys.argv[12:])),
-                shell=True, stdout=sys.stdout, stderr=sys.stderr)
+#subprocess.call(os.path.join(os.path.dirname(sys.argv[0]), "..", "bin", " ".join(sys.argv[12:])),
+#                shell=True, stdout=sys.stdout, stderr=sys.stderr)
                 
 #wait for the server to finish
 serverprocess.wait()
