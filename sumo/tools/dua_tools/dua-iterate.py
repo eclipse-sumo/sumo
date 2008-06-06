0ef053293797a2bb0e4be60f7f65406242618383
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-06-06 06:25:42 +0000

    added the possibility to assign using the incTime method
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5602 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/dua_tools/dua-iterate.py
+++ sumo/tools/dua_tools/dua-iterate.py
@@ -70,7 +70,11 @@ def writeSUMOConf(step, options, files):
     fd.write("   </files>\n")
     fd.write("   <process>\n")
     fd.write("      <begin>" + str(options.begin) + "</begin>\n")
-    fd.write("      <end>" + str(options.end) + "</end>\n")
+    if not options.timeInc:
+        fd.write("      <end>" + str(options.end) + "</end>\n")
+    else:
+        endTime = int(options.timeInc * (step + 1))
+        fd.write("      <end>" + str(endTime) + "</end>\n")
     fd.write("      <route-steps>" + str(options.routeSteps) + "</route-steps>\n")
     if options.incBase>0:
         fd.write("      <incremental-dua-base>" + str(options.incBase) + "</incremental-dua-base>\n")
@@ -99,6 +103,8 @@ optParser.add_option("-n", "--net-file", dest="net",
                      help="SUMO network (mandatory)", metavar="FILE")
 optParser.add_option("-t", "--trips", dest="trips",
                      help="trips in step 0", metavar="FILE")
+optParser.add_option("-+", "--additional", dest="additional",
+                     default="", help="Additional files")
 
 optParser.add_option("-b", "--begin", dest="begin",
                      type="int", default=0, help="Set simulation/routing begin")
@@ -113,18 +119,18 @@ optParser.add_option("-A", "--gA", dest="gA",
 optParser.add_option("-B", "--gBeta", dest="gBeta",
                      type="float", default=.9, help="Sets Gawron's Beta")
 
-optParser.add_option("-E", "--disable-emissions", dest="noEmissions",
+optParser.add_option("-E", "--disable-emissions", action="store_true", dest="noEmissions",
                      default=False, help="No emissions are written by the simulation")
-optParser.add_option("-T", "--disable-tripinfos", dest="noTripinfo",
+optParser.add_option("-T", "--disable-tripinfos", action="store_true", dest="noTripinfo",
                      default=False, help="No tripinfos are written by the simulation")
-optParser.add_option("-m", "--mesosim", dest="mesosim",
+optParser.add_option("-m", "--mesosim", action="store_true", dest="mesosim",
                      default=False, help="Whether mesosim shall be used")
 optParser.add_option("--inc-base", dest="incBase",
                      type="int", default=-1, help="Give the incrementation base")
 optParser.add_option("--incrementation", dest="incValue",
                      type="int", default=1, help="Give the incrementation")
-optParser.add_option("-+", "--additional", dest="additional",
-                     default="", help="Additional files")
+optParser.add_option("--time-inc", dest="timeInc",
+                     type="int", default=-1, help="Give the time incrementation")
 
 
 optParser.add_option("-f", "--first-step", dest="firstStep",
