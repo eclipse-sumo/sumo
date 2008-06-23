8ea717f4efe6e48b8a319e6307c8d4511209104e
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-06-23 11:52:10 +0000

    documentation added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5639 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router/RONet.h
+++ sumo/src/router/RONet.h
@@ -201,11 +201,23 @@ public:
     /// @name Processing stored vehicle definitions 
     //@{
 
-    /** @brief Computes routes described by their definitions and saves them */
+    /** @brief Computes routes described by their definitions and saves them
+     *
+     * As long a vehicle with a departure time not larger than the given
+     *  exists, its route is computed and it is written and removed from
+     *  the internal container.
+     *
+     * @param[in] options The options used during this process
+     * @param[in] router The router to use for routes computation
+     * @param[in] options The time until which route definitions shall be processed
+     */
     void saveAndRemoveRoutesUntil(OptionsCont &options,
                                   SUMOAbstractRouter<ROEdge,ROVehicle> &router, SUMOTime time);
 
-    /** @brief Removes the route from the net when no further usage is needed */
+
+    /** @brief Removes the route from the net when no further usage is needed 
+     *
+     */
     virtual void removeRouteSecure(const RORouteDef * const route); // !!! was protected before
 
 
