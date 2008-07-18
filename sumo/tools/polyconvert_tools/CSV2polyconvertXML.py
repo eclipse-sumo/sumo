baa5be18e3eac9377bcf9be50a3e3b51d268622c
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-07-18 06:30:33 +0000

    added a tool for converting csv into xml parseable as pois by POLYCONVERT
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@5808 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/polyconvert_tools/CSV2polyconvertXML.py
@@ -0,0 +1,39 @@
+"""
+@file    CSV2polyconvertXML.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2008-07-17
+
+Converts a given CSV-file that contains a list of pois to 
+ an XML-file that may be read by POLYCONVERT.
+
+Copyright (C) 2008 DLR/FS, Germany
+All rights reserved
+"""
+
+import sys
+
+if len(sys.argv)<4:
+    print "Error: Missing argument(s)"
+    print "Call: CSV2polyconvertXML.py <CSV_FILE> <OUTPUT_FILE> <VALUENAME>[,<VALUENAME>]*"
+    print " The values within the csv-file are supposed to be divided by ';'."
+    print " <VALUENAME>s give the attribute names in order of their appearence within the csv-file ."
+    exit()
+
+
+names = sys.argv[3].split(',')
+inpf = open(sys.argv[1])
+outf = open(sys.argv[2], "w")
+outf.write("<pois>\n")
+for line in inpf:
+    if len(line)==0 or line[0]=='#':
+        continue
+    vals = line.strip().split(';')
+    outf.write("    <poi")
+    for i,n in enumerate(names):
+         outf.write(' ' + n + '="' + vals[i] + '"')
+    outf.write("/>\n")
+outf.write("</pois>\n")
+inpf.close()
+outf.close()
+
+        
