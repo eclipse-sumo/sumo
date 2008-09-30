c190dda3f1557bed4b1652cc57c1c4753ed35526
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2008-09-30 07:11:46 +0000

    spell check
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6095 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netbuild/NBConnectionDefs.h
+++ sumo/src/netbuild/NBConnectionDefs.h
@@ -4,7 +4,7 @@
 /// @date    Sept 2002
 /// @version $Id$
 ///
-// Some conatiner for connections
+// Some container for connections
 /****************************************************************************/
 // SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
 // copyright : (C) 2001-2007
