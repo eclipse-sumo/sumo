ce29504e4c23768cda5df4f6b83ac8800ce100d1
Yun-Pang Floetteroed <Yun-Pang.Floetteroed@dlr.de>
2008-11-20 17:20:46 +0000

    debug "del" instead of "remove"
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6371 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/network.py
+++ sumo/tools/assign/network.py
@@ -87,14 +87,14 @@ class Net:
                         node.outEdges.add(link)
                         link.source = node
                     node.outEdges.remove(edge)
-                    self._fullEdges.remove(edge)
+                    del self._fullEdges[edge]
                     toRemove.append(edge.target)   
                 else:
                     for link in edge.source.inEdges:
                         node.inEdges.add(link)
                         link.target = node
                     node.inEdges.remove(edge)
-                    self._fullEdges.remove(edge)
+                    del self._fullEdges[edge]
                     toRemove.append(edge.source)
             
         for node in toRemove:
