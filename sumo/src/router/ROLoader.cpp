e7d4c3da7412928f08eb94438aa239ed500b3ad9
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-01-07 10:47:04 +0000

    debugging problems with dua computation when using networks with internal lanes
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6558 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router/ROLoader.cpp
+++ sumo/src/router/ROLoader.cpp
@@ -89,7 +89,9 @@ ROLoader::EdgeFloatTimeLineRetriever_EdgeWeight::addEdgeWeight(const std::string
     if (e!=0) {
         e->addWeight(val, beg, end);
     } else {
-        MsgHandler::getErrorInstance()->inform("Trying to set a weight for the unknown edge '" + id + "'.");
+        if(id[0]!=':') {
+            MsgHandler::getErrorInstance()->inform("Trying to set a weight for the unknown edge '" + id + "'.");
+        }
     }
 }
 
