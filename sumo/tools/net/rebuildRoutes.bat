85c18181eb36a5e764e9d40cdedd10f47ef02c8e
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-02-11 09:57:38 +0000

    moved to DELPHI-SVN, refs#12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6771 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/net/rebuildRoutes.bat
+++
@@ -1,2 +0,0 @@
-od2trips -n districts.xml -d visum\koeln_based_DB.fma -o koeln_umlegung.trips.xml -v -b 0 -e 86400 --dismiss-loading-errors
-duarouter -n koeln_umlegung.net.xml -t koeln_umlegung.trips.xml -o koeln_umlegung.rou.xml -v -e 3600 --continue-on-unbuild
