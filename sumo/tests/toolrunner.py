5c6bd5fc0fdd9b7915e635707c64a38fdbbf9687
Michael Behrisch <oss@behrisch.de>
2009-02-16 10:00:41 +0000

    fixing tool tests, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6804 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/toolrunner.py
+++ sumo/tests/toolrunner.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 import os,subprocess,sys
-subprocess.call(os.path.join(os.path.dirname(sys.argv[0]), "..", "tools", " ".join(sys.argv[1:])),
+command = ""
+if sys.argv[1].endswith(".jar"):
+    command = "java -jar "
+subprocess.call(command + os.path.join(os.path.dirname(sys.argv[0]), "..", "tools", " ".join(sys.argv[1:])),
                 shell=True, stdout=sys.stdout, stderr=sys.stderr)
