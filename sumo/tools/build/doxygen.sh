13a9fc6f72996e6153b95ba0112443325f448750
Michael Behrisch <oss@behrisch.de>
2009-03-06 13:50:32 +0000

    fixing path of doxygen docu, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6885 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/doxygen.sh
+++ sumo/tools/build/doxygen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PREFIX=$1
 REMOTEDIR="behrisch,sumo@web.sourceforge.net:/home/groups/s/su/sumo/htdocs/daily/"
-DOCDIR=$PREFIX/sumo_doxygen
+DOCDIR=$PREFIX/sumodoku
 
 cd $PREFIX/sumo
 rm -rf $DOCDIR
 nice -19 doxygen sumo.doxyconf
-scp -qr $DOCDIR $REMOTEDIR
+scp -qr $DOCDIR $REMOTEDIR/doxygen
