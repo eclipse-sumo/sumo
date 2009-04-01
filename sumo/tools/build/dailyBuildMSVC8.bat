5e4b8674ed4da2c234fa9707d2dc4db1ca1a6bb8
Michael Behrisch <oss@behrisch.de>
2009-04-01 11:06:33 +0000

    daily binary upload
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6986 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/dailyBuildMSVC8.bat
+++ sumo/tools/build/dailyBuildMSVC8.bat
@@ -10,9 +10,11 @@ SET SMTP_SERVER=129.247.218.247
 
 cd ..\..
 del %MAKELOG% %MAKEALLLOG%
+del bin\*.exe
 svn.exe up > %MAKELOG% 2>&1
 
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Release|Win32" build\msvc8\prj.sln /out %MAKELOG%
+zip -j sumo-%FILEPREFIX%-bin.zip  %PROJ_GDAL%\bin\proj.dll %PROJ_GDAL%\bin\gdal16.dll %XERCES%\lib\xerces-c_2_7.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Debug|Win32" build\msvc8\prj.sln /out %MAKEALLLOG%
 
 REM run tests
@@ -28,4 +30,4 @@ texttest.py -b %SUMO_REPORT% -coll
 cd ..
 tools\build\status.py %MAKELOG% %MAKEALLLOG% %TEXTTEST_TMP% %SMTP_SERVER% > %STATUSLOG%
 
-WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% /home/groups/s/su/sumo/htdocs/daily/" "exit"
+WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% sumo-%FILEPREFIX%-bin.zip /home/groups/s/su/sumo/htdocs/daily/" "exit"
