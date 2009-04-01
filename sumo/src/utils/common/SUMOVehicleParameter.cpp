95d073b2247facfe01d5e75698913f798b8367a6
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-04-01 11:03:19 +0000

    adding missing file (was: adding departure/arrival defaults setting options), refs#48
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@6985 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/utils/common/SUMOVehicleParameter.cpp
@@ -0,0 +1,215 @@
+/****************************************************************************/
+/// @file    SUMOVehicleParameter.cpp
+/// @author  Daniel Krajzewicz
+/// @date    Tue, 31.03.2009
+/// @version $Id: SUMOVehicleParameter.cpp 6907 2009-03-13 12:13:38Z dkrajzew $
+///
+// Structure representing possible vehicle parameter
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2009 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include "SUMOVehicleParameter.h"
+#include <utils/common/ToString.h>
+#include <utils/iodevices/OutputDevice.h>
+#include <utils/options/OptionsCont.h>
+
+#ifdef CHECK_MEMORY_LEAKS
+#include <foreign/nvwa/debug_new.h>
+#endif // CHECK_MEMORY_LEAKS
+
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+SUMOVehicleParameter::SUMOVehicleParameter() throw()
+    : vtypeid(DEFAULT_VTYPE_ID), depart(-1), departLane(0),
+    departLaneProcedure(DEPART_LANE_DEFAULT),
+    departPosProcedure(DEPART_POS_DEFAULT), departSpeedProcedure(DEPART_SPEED_DEFAULT),
+    arrivalLaneProcedure(ARRIVAL_LANE_DEFAULT),
+    arrivalPosProcedure(ARRIVAL_POS_DEFAULT), arrivalSpeedProcedure(ARRIVAL_SPEED_DEFAULT),
+    arrivalPos(0), arrivalSpeed(-1),
+    repetitionNumber(-1), repetitionOffset(-1), setParameter(0), color(RGBColor::DEFAULT_COLOR) {
+}
+
+
+bool 
+SUMOVehicleParameter::defaultOptionOverrides(const OptionsCont &oc, const std::string &optionName) const throw()
+{
+    return oc.isSet(optionName) && oc.getBool("defaults-override");
+}
+
+
+void
+SUMOVehicleParameter::writeAs(const std::string &xmlElem, OutputDevice &dev, 
+                              const OptionsCont &oc) const throw(IOError) {
+    dev << "<vehicle id=\"" << id << "\"";
+    if (wasSet(VEHPARS_VTYPE_SET)) {
+        dev << " type=\"" << vtypeid << "\"";
+    }
+    dev << " depart=\"" << depart << "\"";
+
+    // optional parameter
+    //  departlane
+    if (wasSet(VEHPARS_DEPARTLANE_SET) && !defaultOptionOverrides(oc, "departlane")) {
+        std::string val;
+        switch (departLaneProcedure) {
+        case DEPART_LANE_GIVEN:
+            val = toString(departLane);
+            break;
+        case DEPART_LANE_RANDOM:
+            val = "random";
+            break;
+        case DEPART_LANE_FREE:
+            val = "free";
+            break;
+        case DEPART_LANE_DEPARTLANE:
+            val = "departlane";
+            break;
+        case DEPART_LANE_DEFAULT:
+        default:
+            break;
+        }
+        dev << " departlane=\"" << val << "\"";
+    } else if (oc.isSet("departlane")) {
+        dev << " departlane=\"" << oc.getString("departlane") << "\"";
+    }
+    //  departpos
+    if (wasSet(VEHPARS_DEPARTPOS_SET) && !defaultOptionOverrides(oc, "departpos")) {
+        std::string val;
+        switch (departPosProcedure) {
+        case DEPART_POS_GIVEN:
+            val = toString(departPos);
+            break;
+        case DEPART_POS_RANDOM:
+            val = "random";
+            break;
+        case DEPART_POS_RANDOM_FREE:
+            val = "random_free";
+            break;
+        case DEPART_POS_FREE:
+            val = "free";
+            break;
+        case DEPART_POS_DEFAULT:
+        default:
+            break;
+        }
+        dev << " departpos=\"" << val << "\"";
+    } else if (oc.isSet("departpos")) {
+        dev << " departpos=\"" << oc.getString("departpos") << "\"";
+    }
+    //  departspeed
+    if (wasSet(VEHPARS_DEPARTSPEED_SET) && !defaultOptionOverrides(oc, "departspeed")) {
+        std::string val;
+        switch (departSpeedProcedure) {
+        case DEPART_SPEED_GIVEN:
+            val = toString(departSpeed);
+            break;
+        case DEPART_SPEED_RANDOM:
+            val = "random";
+            break;
+        case DEPART_SPEED_MAX:
+            val = "max";
+            break;
+        case DEPART_SPEED_DEFAULT:
+        default:
+            break;
+        }
+        dev << " departspeed=\"" << val << "\"";
+    } else if (oc.isSet("departspeed")) {
+        dev << " departspeed=\"" << oc.getString("departspeed") << "\"";
+    }
+
+    //  arrivallane
+    if (wasSet(VEHPARS_ARRIVALLANE_SET) && !defaultOptionOverrides(oc, "arrivallane")) {
+        std::string val;
+        switch (arrivalLaneProcedure) {
+        case ARRIVAL_LANE_GIVEN:
+            val = toString(departLane);
+            break;
+        case ARRIVAL_LANE_CURRENT:
+            val = "current";
+            break;
+        case ARRIVAL_LANE_DEFAULT:
+        default:
+            break;
+        }
+        dev << " arrivallane=\"" << val << "\"";
+    } else if (oc.isSet("arrivallane")) {
+        dev << " arrivallane=\"" << oc.getString("arrivallane") << "\"";
+    }
+    //  arrivalpos
+    if (wasSet(VEHPARS_ARRIVALPOS_SET) && !defaultOptionOverrides(oc, "arrivalpos")) {
+        std::string val;
+        switch (arrivalPosProcedure) {
+        case ARRIVAL_POS_GIVEN:
+            val = toString(departPos);
+            break;
+        case ARRIVAL_POS_RANDOM:
+            val = "random";
+            break;
+        case ARRIVAL_POS_MAX:
+            val = "max";
+            break;
+        case ARRIVAL_POS_DEFAULT:
+        default:
+            break;
+        }
+        dev << " arrivalpos=\"" << val << "\"";
+    } else if (oc.isSet("arrivalpos")) {
+        dev << " arrivalpos=\"" << oc.getString("arrivalpos") << "\"";
+    }
+    //  arrivalspeed
+    if (wasSet(VEHPARS_ARRIVALSPEED_SET) && !defaultOptionOverrides(oc, "arrivalspeed")) {
+        std::string val;
+        switch (arrivalSpeedProcedure) {
+        case ARRIVAL_SPEED_GIVEN:
+            val = toString(departSpeed);
+            break;
+        case ARRIVAL_SPEED_CURRENT:
+            val = "current";
+            break;
+        case ARRIVAL_SPEED_DEFAULT:
+        default:
+            break;
+        }
+        dev << " arrivalspeed=\"" << val << "\"";
+    } else if (oc.isSet("arrivalspeed")) {
+        dev << " arrivalspeed=\"" << oc.getString("arrivalspeed") << "\"";
+    }
+
+    // color
+    if (wasSet(VEHPARS_COLOR_SET)) {
+        dev << " color=\"" << color << "\"";
+    }
+    // repetition values
+    if (wasSet(VEHPARS_PERIODNUM_SET)) {
+        dev << " repno=\"" << repetitionNumber << "\"";
+    }
+    if (wasSet(VEHPARS_PERIODFREQ_SET)) {
+        dev << " period=\"" << repetitionOffset << "\"";
+    }
+    dev << ">\n";
+}
+
+
+/****************************************************************************/
+
