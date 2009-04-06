62e81ae5a8e11ee530516949355fffaa9ecfd729
Michael Behrisch <oss@behrisch.de>
2009-04-06 11:06:47 +0000

    nightly build and test for 64bit platform, refs #61
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7003 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/testEnv.bat
+++ sumo/tests/testEnv.bat
@@ -1,9 +1,9 @@
 set TEXTTEST_HOME=%CD%
-set DFROUTER_BINARY=%CD%\..\bin\dfrouter.exe
-set DUAROUTER_BINARY=%CD%\..\bin\duarouter.exe
-set JTRROUTER_BINARY=%CD%\..\bin\jtrrouter.exe
-set NETCONVERT_BINARY=%CD%\..\bin\netconvert.exe
-set NETGEN_BINARY=%CD%\..\bin\netgen.exe
-set OD2TRIPS_BINARY=%CD%\..\bin\od2trips.exe
-set SUMO_BINARY=%CD%\..\bin\sumo.exe
-set POLYCONVERT_BINARY=%CD%\..\bin\polyconvert.exe
+set DFROUTER_BINARY=%CD%\..\bin\dfrouter%1.exe
+set DUAROUTER_BINARY=%CD%\..\bin\duarouter%1.exe
+set JTRROUTER_BINARY=%CD%\..\bin\jtrrouter%1.exe
+set NETCONVERT_BINARY=%CD%\..\bin\netconvert%1.exe
+set NETGEN_BINARY=%CD%\..\bin\netgen%1.exe
+set OD2TRIPS_BINARY=%CD%\..\bin\od2trips%1.exe
+set SUMO_BINARY=%CD%\..\bin\sumo%1.exe
+set POLYCONVERT_BINARY=%CD%\..\bin\polyconvert%1.exe
--- sumo/tools/build/dailyBuildMSVC8.bat
+++ sumo/tools/build/dailyBuildMSVC8.bat
@@ -1,21 +1,25 @@
 @echo off
 
-SET FILEPREFIX=msvc8
+IF "%1" == "" GOTO loop
+
+SET FILEPREFIX=msvc8%1
 SET MAKELOG=%FILEPREFIX%buildRelease.log
 SET STATUSLOG=%FILEPREFIX%status.log
 SET MAKEALLLOG=%FILEPREFIX%buildDebug.log
 SET SUMO_BATCH_RESULT=%FILEPREFIX%batch_result
 SET SUMO_REPORT=%FILEPREFIX%report
 SET SMTP_SERVER=129.247.218.247
+SET BINARY_ZIP=N:\Daten\Sumo\Nightly\sumo-%FILEPREFIX%-bin.zip
 
 cd ..\..
 del %MAKELOG% %MAKEALLLOG%
 del bin\*.exe
 svn.exe up > %MAKELOG% 2>&1
 
-"D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Release|Win32" build\msvc8\prj.sln /out %MAKELOG%
-zip -j sumo-%FILEPREFIX%-bin.zip  %PROJ_GDAL%\bin\proj.dll %PROJ_GDAL%\bin\gdal16.dll %XERCES%\lib\xerces-c_2_7.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
-"D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Debug|Win32" build\msvc8\prj.sln /out %MAKEALLLOG%
+"D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Release|%1" build\msvc8\prj.sln /out %MAKELOG%
+IF "%1" == "Win32" zip -j %BINARY_ZIP% %PROJ_GDAL%\bin\proj.dll %PROJ_GDAL%\bin\gdal16.dll %XERCES%\bin\xerces-c_3_0.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
+IF "%1" == "x64" zip -j %BINARY_ZIP% %PROJ_GDAL_64%\bin\proj.dll %PROJ_GDAL_64%\bin\gdal16.dll %XERCES_64%\bin\xerces-c_3_0.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
+"D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Debug|%1" build\msvc8\prj.sln /out %MAKEALLLOG%
 
 REM run tests
 cd tests
@@ -23,11 +27,20 @@ SET TEXTTEST_TMP=%CD%\texttesttmp
 rmdir /S /Q %TEXTTEST_TMP%
 rmdir /S /Q %SUMO_REPORT%
 mkdir %SUMO_REPORT%
-call testEnv.bat
+IF "%1" == "Win32" call testEnv.bat
+IF "%1" == "x64" call testEnv.bat 64
 runGuisimTests.py -b
 texttest.py -b %SUMO_REPORT%
 texttest.py -b %SUMO_REPORT% -coll
 cd ..
 tools\build\status.py %MAKELOG% %MAKEALLLOG% %TEXTTEST_TMP% %SMTP_SERVER% > %STATUSLOG%
 
-WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% sumo-%FILEPREFIX%-bin.zip /home/groups/s/su/sumo/htdocs/daily/" "exit"
+WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% %BINARY_ZIP% /home/groups/s/su/sumo/htdocs/daily/" "exit"
+
+cd tools\build
+GOTO exit
+
+:loop
+FOR %%i IN (Win32 x64) DO CALL %0 %%i
+
+:exit
