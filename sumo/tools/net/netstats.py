03f953262d2058a14e9d8e6c623e47b798438042
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-04-07 18:55:16 +0000

    a small script for printing statistics added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7006 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/net/netstats.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+@file    netstats.py
+@author  Daniel.Krajzewicz@dlr.de 
+@date    2008-08-13
+@version $Id: netstats.py 6858 2009-02-25 13:27:57Z behrisch $
+
+Prints some information about a given network
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+import os, string, sys, StringIO
+from xml.sax import saxutils, make_parser, handler
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "../lib"))
+import sumonet
+
+
+def renderHTML(values):
+    print "<html><body>"
+    print "<h1>" + values["netname"] + "</h1></br>"
+    # network
+    print "<h2>Network</h2></br>"
+    # edges
+    print "<h2>Edges</h2></br>"
+    print "Edge number: " + str(values["edgeNumber"]) + "</br>"
+    print "Edgelength sum: " + str(values["edgeLengthSum"]) + "</br>"
+    print "Lanelength sum: " + str(values["laneLengthSum"]) + "</br>"
+    # nodes
+    print "<h2>Nodes</h2></br>"
+    print "Node number: " + str(values["nodeNumber"]) + "</br>"
+    print "</body></html>"
+
+
+if len(sys.argv) < 2:
+    print "Usage: " + sys.argv[0] + " <net>"
+    sys.exit()
+print "Reading net..."
+parser = make_parser()
+net = sumonet.NetReader()
+parser.setContentHandler(net)
+parser.parse(sys.argv[1])
+net = net.getNet()
+
+values = {}
+values["netname"] = "hallo"
+values["edgesPerLaneNumber"] = {}
+values["edgeLengthSum"] = 0
+values["laneLengthSum"] = 0
+values["edgeNumber"] = len(net._edges)
+values["nodeNumber"] = len(net._nodes)
+for e in net._edges:
+    values["edgeLengthSum"] = values["edgeLengthSum"] + e._length
+    values["laneLengthSum"] = values["laneLengthSum"] + (e._length * float(len(e._lanes)))
+    if len(e._lanes) not in values["edgesPerLaneNumber"]:
+        values["edgesPerLaneNumber"][len(e._lanes)] = 0
+    values["edgesPerLaneNumber"][len(e._lanes)] = values["edgesPerLaneNumber"][len(e._lanes)] + 1
+
+renderHTML(values)
+
+
