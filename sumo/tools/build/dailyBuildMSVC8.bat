bd4f95c0cb5a4f378021f0e892e3bdf824b3d9e7
Michael Behrisch <oss@behrisch.de>
2009-04-14 11:30:34 +0000

    daily build updated, refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7034 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/dailyBuildMSVC8.bat
+++ sumo/tools/build/dailyBuildMSVC8.bat
@@ -2,14 +2,16 @@
 
 IF "%1" == "" GOTO loop
 
-SET FILEPREFIX=msvc8%1
+SET ROOTDIR=D:\Sumo
+SET FILEPREFIX=%ROOTDIR%\msvc8%1
 SET MAKELOG=%FILEPREFIX%buildRelease.log
 SET STATUSLOG=%FILEPREFIX%status.log
 SET MAKEALLLOG=%FILEPREFIX%buildDebug.log
 SET SUMO_BATCH_RESULT=%FILEPREFIX%batch_result
 SET SUMO_REPORT=%FILEPREFIX%report
 SET SMTP_SERVER=129.247.218.247
-SET BINARY_ZIP=N:\Daten\Sumo\Nightly\sumo-%FILEPREFIX%-bin.zip
+SET NIGHTLY_DIR=N:\Daten\Sumo\Nightly
+SET BINARY_ZIP=%NIGHTLY_DIR%\sumo-%FILEPREFIX%-bin.zip
 
 cd ..\..
 del %MAKELOG% %MAKEALLLOG%
@@ -17,13 +19,21 @@ del bin\*.exe
 svn.exe up > %MAKELOG% 2>&1
 
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Release|%1" build\msvc8\prj.sln /out %MAKELOG%
-IF "%1" == "Win32" zip -j %BINARY_ZIP% %PROJ_GDAL%\bin\proj.dll %PROJ_GDAL%\bin\gdal16.dll %XERCES%\bin\xerces-c_3_0.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
-IF "%1" == "x64" zip -j %BINARY_ZIP% %PROJ_GDAL_64%\bin\proj.dll %PROJ_GDAL_64%\bin\gdal16.dll %XERCES_64%\bin\xerces-c_3_0.dll %FOX14%\lib\FOXDLL-1.4.dll bin\*.exe
+del %BINARY_ZIP%
+IF "%1" == "Win32" (
+    zip -j %BINARY_ZIP% %PROJ_GDAL%\bin\proj.dll %PROJ_GDAL%\bin\gdal16.dll %XERCES%\bin\xerces-c_3_0.dll %FOX16%\lib\FOXDLL-1.6.dll bin\*.exe
+    copy /Y %PROJ_GDAL%\bin\proj.dll %NIGHTLY_DIR%
+    copy /Y %PROJ_GDAL%\bin\gdal16.dll %NIGHTLY_DIR%
+    copy /Y %XERCES%\bin\xerces-c_3_0.dll %NIGHTLY_DIR%
+    copy /Y %FOX16%\lib\FOXDLL-1.6.dll %NIGHTLY_DIR%
+    copy /Y bin\*.exe %NIGHTLY_DIR%
+)
+IF "%1" == "x64" zip -j %BINARY_ZIP% %PROJ_GDAL_64%\bin\proj.dll %PROJ_GDAL_64%\bin\gdal16.dll %XERCES_64%\bin\xerces-c_3_0.dll %FOX16_64%\lib\FOXDLL-1.6.dll bin\*.exe
 "D:\Programme\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" /rebuild "Debug|%1" build\msvc8\prj.sln /out %MAKEALLLOG%
 
 REM run tests
 cd tests
-SET TEXTTEST_TMP=%CD%\texttesttmp
+SET TEXTTEST_TMP=%ROOTDIR%\texttesttmp
 rmdir /S /Q %TEXTTEST_TMP%
 rmdir /S /Q %SUMO_REPORT%
 mkdir %SUMO_REPORT%
@@ -35,7 +45,7 @@ texttest.py -b %SUMO_REPORT% -coll
 cd ..
 tools\build\status.py %MAKELOG% %MAKEALLLOG% %TEXTTEST_TMP% %SMTP_SERVER% > %STATUSLOG%
 
-WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=..\..\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% %BINARY_ZIP% /home/groups/s/su/sumo/htdocs/daily/" "exit"
+WinSCP3.com behrisch,sumo@web.sourceforge.net /privatekey=%ROOTDIR%\key.ppk /command "option batch on" "option confirm off" "put tests\%SUMO_REPORT% %MAKELOG% %MAKEALLLOG% %STATUSLOG% %BINARY_ZIP% /home/groups/s/su/sumo/htdocs/daily/" "exit"
 
 cd tools\build
 GOTO exit
