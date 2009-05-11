e2723f90fc7cd81e825833c4f137c6b6dd09cd4b
Michael Behrisch <oss@behrisch.de>
2009-05-11 08:29:52 +0000

    always update guisim test tree, refs #28
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7142 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/runGuisimTests.py
+++ sumo/tests/runGuisimTests.py
@@ -19,28 +19,29 @@ if len(sys.argv) > 1 and sys.argv[1] == "-b" and os.path.exists(guisimRoot):
     shutil.rmtree(guisimRoot)
 if not os.path.exists(guisimRoot):
     os.mkdir(guisimRoot)
-    for root, dirs, files in os.walk(sumoRoot):
-        for folder in ['.svn', 'meta']:
-            if folder in dirs:
-                dirs.remove(folder)
-        newRoot = root.replace(sumoRoot, guisimRoot)
-        for folder in dirs:
-            newDir = os.path.join(newRoot, folder)
-            if not os.path.exists(newDir):
-                os.mkdir(newDir)
-        for name in files:
-            newName = os.path.join(newRoot, name)
-            if name.endswith('sumo'):
-                newName = newName[:-4] + "guisim"
-            if name == "config.sumo":
-                src = open(os.path.join(root, name))
-                dest = open(newName, "w")
-                for l in src:
-                    dest.write(l.replace('SUMO_BINARY', 'TEXTTEST_HOME/guisimrunner.py'))
-                src.close()
-                dest.close()
-            else:
-                shutil.copy2(os.path.join(root, name), newName)
+for root, dirs, files in os.walk(sumoRoot):
+    for folder in ['.svn', 'meta']:
+        if folder in dirs:
+            dirs.remove(folder)
+    newRoot = root.replace(sumoRoot, guisimRoot)
+    for folder in dirs:
+        newDir = os.path.join(newRoot, folder)
+        if not os.path.exists(newDir):
+            os.mkdir(newDir)
+    for name in files:
+        oldPath = os.path.join(root, name)
+        newPath = os.path.join(newRoot, name)
+        if name.endswith('sumo'):
+            newPath = newPath[:-4] + "guisim"
+        if name == "config.sumo":
+            src = open(oldPath)
+            dest = open(newPath, "w")
+            for l in src:
+                dest.write(l.replace('SUMO_BINARY', 'TEXTTEST_HOME/guisimrunner.py'))
+            src.close()
+            dest.close()
+        elif not os.path.exists(newPath) or os.path.getmtime(oldPath) > os.path.getmtime(newPath):
+            shutil.copy2(oldPath, newPath)
 
 if len(sys.argv) == 1:
     os.environ["TEXTTEST_HOME"] = os.path.dirname(sys.argv[0])
