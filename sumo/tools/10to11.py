0f7f412ae4833cdaa8c553b3cb26fb724edf8576
Michael Behrisch <oss@behrisch.de>
2009-06-09 11:13:45 +0000

    script for configuration update, refs #91
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7285 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/10to11.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+@file    10to11.py
+@author  Michael.Behrisch@dlr.de
+@date    2009-06-09
+@version $Id$
+
+Transfers configuration files from character data to attribute style.
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+import os, sys, re
+
+from xml.sax import make_parser, handler
+from optparse import OptionParser
+
+class ConfigReader(handler.ContentHandler):
+
+    def __init__(self):
+        self._parent = None
+        self._parentWritten = False
+        self._element = None
+        self._string = ''
+
+    def startElement(self, name, attrs):
+        self._parent = self._element
+        self._element = name
+        self._string = ''
+
+    def characters(self, content):
+        self._string += content
+
+    def endElement(self, name):
+        if self._parent == name:
+            print '    />'
+            self._parentWritten = False
+        elif self._element == name:
+            if not self._parentWritten:
+                print '    <' + self._parent
+                self._parentWritten = True
+            print '        %s="%s"' % (self._element, self._string)
+        self._element = self._parent
+
+    def startDocument(self):
+        print '<configuration>'
+
+    def endDocument(self):
+        print '</configuration>'
+
+optParser = OptionParser(usage="usage: %prog <config>+")
+(options, args) = optParser.parse_args()
+if len(args) == 0:
+    optParser.print_help()
+    sys.exit()
+for f in args:
+    parser = make_parser()
+    parser.setContentHandler(ConfigReader())
+    parser.parse(f)
