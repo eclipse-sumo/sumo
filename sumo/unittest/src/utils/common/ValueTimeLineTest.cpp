fca272c1be799c71cbadd450deb092bda1e011f7
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-07-14 08:56:47 +0000

    adding unittests for ValueTimeLine, refs#105
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7430 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/unittest/src/utils/common/ValueTimeLineTest.cpp
@@ -0,0 +1,189 @@
+#include <gtest/gtest.h>
+#include <utils/common/ValueTimeLine.h>
+
+using namespace std;
+
+/*
+Tests ValueTimeLine class from <SUMO>/src/utils/common
+*/
+
+/* Tests what happens if one tries to get a value from an empty ValueTimeLine. */
+/*
+TEST(ValueTimeLine, test_get_from_empty) {
+    ValueTimeLine<int> vtl;
+    EXPECT_EQ(1, vtl.getValue(0) << "Something should happen if nothing was stored.";
+}
+*/
+
+
+// --------------------------------
+// plain retrieval / overwriting tests
+// --------------------------------
+
+/* Tests what happens if one tries to get a stored value (one value stored, fillGaps not called). */
+TEST(ValueTimeLine, test_get_single_nocollect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 2);
+    EXPECT_EQ(2, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one tries to get a stored value (three values stored, fillGaps not called). */
+TEST(ValueTimeLine, test_get_multi_nocollect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 1);
+    vtl.add(100, 200, 2);
+    vtl.add(200, 300, 3);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one tries to get a stored value (one value stored, fillGaps called). */
+TEST(ValueTimeLine, test_get_single_collect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 2);
+    vtl.fillGaps(0);
+    EXPECT_EQ(2, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one tries to get a stored value (three values stored, fillGaps called). */
+TEST(ValueTimeLine, test_get_multi_collect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 1);
+    vtl.add(100, 200, 2);
+    vtl.add(200, 300, 3);
+    vtl.fillGaps(0);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+// --------------------------------
+// overwriting filling tests
+// --------------------------------
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps not called). */
+TEST(ValueTimeLine, test_overwrite_nocollect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 1);
+    vtl.add(200, 300, 3);
+    vtl.add(50, 250, 2);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps called). */
+TEST(ValueTimeLine, test_overwrite_collect) {
+    ValueTimeLine<int> vtl;
+    vtl.add(0, 100, 1);
+    vtl.add(200, 300, 3);
+    vtl.add(50, 250, 2);
+    vtl.fillGaps(0);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps not called, order changed). */
+TEST(ValueTimeLine, test_overwrite_nocollect2) {
+    ValueTimeLine<int> vtl;
+    vtl.add(50, 250, 2);
+    vtl.add(0, 100, 1);
+    vtl.add(200, 300, 3);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps called, order changed). */
+TEST(ValueTimeLine, test_overwrite_collect2) {
+    ValueTimeLine<int> vtl;
+    vtl.add(50, 250, 2);
+    vtl.add(0, 100, 1);
+    vtl.add(200, 300, 3);
+    vtl.fillGaps(0);
+    EXPECT_EQ(1, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(1, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(3, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+
+// --------------------------------
+// gap filling tests
+// --------------------------------
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps called). */
+TEST(ValueTimeLine, test_fill_gaps_withbounds) {
+    ValueTimeLine<int> vtl;
+    vtl.add(50, 250, 2);
+    vtl.fillGaps(4, true);
+    EXPECT_EQ(2, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+
+/* Tests what happens if one overwrites a value (three values stored, fillGaps called). */
+TEST(ValueTimeLine, test_fill_gaps_nobounds) {
+    ValueTimeLine<int> vtl;
+    vtl.add(50, 250, 2);
+    vtl.fillGaps(4, false);
+    EXPECT_EQ(4, vtl.getValue(0)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(99)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(50)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(100)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(199)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(150)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(2, vtl.getValue(200)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(4, vtl.getValue(299)) << "The stored number should be returned when asking within the interval.";
+    EXPECT_EQ(4, vtl.getValue(250)) << "The stored number should be returned when asking within the interval.";
+}
+
+
