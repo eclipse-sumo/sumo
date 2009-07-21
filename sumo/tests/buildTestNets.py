f77536e72541ce2c953d4bbba78aa01b691937d5
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-07-21 10:32:38 +0000

    some work on the network generation script, refs#21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7469 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/buildTestNets.py
+++ sumo/tests/buildTestNets.py
@@ -5,34 +5,37 @@
 import os
 import sys
 
-# get all files from the given folder recursively
-def getStructure(dir):
-	if(dir[-1:]!='/' and dir[-1:]!='\\'):
-		dir = dir + '\\'
-	files = os.listdir(dir)
-	newFiles = []
-	for file in files:
-		if os.path.isdir(dir + file)==1:
-			myNewFiles = getStructure(dir + file + "\\")
-			for tmp in myNewFiles:
-				newFiles.append(tmp)
-		else:
-			newFiles.append( dir + file )
-	return newFiles
-
 # main
-files = getStructure("./dfrouter")
-files.extend(getStructure("./duarouter"))
-for file in files:
-    if(file.endswith(".netc.cfg")):
-        print "----------------------------------"
-        print "Runnning: " + file
-        if(sys.platform=="win32"):
-            (cin, cout) = os.popen4("..\\bin\\netconvert -v -c " + file)
-        else:
-            (cin, cout) = os.popen4("../src/sumo-netconvert -v -c " + file)
-        line = cout.readline()
-        while line:
-             print line[:-1]
-             line = cout.readline()
-        print "----------------------------------\n"
+mRoot = "./"
+if len(sys.argv)>1:
+	mRoot = sys.argv[1]
+for root, dirs, files in os.walk(mRoot):
+    for ignoreDir in ['.svn', 'foreign']:
+        if ignoreDir in dirs:
+            dirs.remove(ignoreDir)
+
+    for file in files:
+        if(file.endswith(".netc.cfg")):
+            print "----------------------------------"
+            print "Runnning: " + file
+            if(sys.platform=="win32"):
+                (cin, cout) = os.popen4("..\\bin\\netconvert -v -c " + os.path.join(root, file))
+            else:
+                (cin, cout) = os.popen4("../src/sumo-netconvert -v -c " + os.path.join(root, file))
+            line = cout.readline()
+            while line:
+                 print line[:-1]
+                 line = cout.readline()
+            print "----------------------------------\n"
+        if(file.endswith(".netg.cfg")):
+            print "----------------------------------"
+            print "Runnning: " + file
+            if(sys.platform=="win32"):
+                (cin, cout) = os.popen4("..\\bin\\netgen -v -c " + os.path.join(root, file))
+            else:
+                (cin, cout) = os.popen4("../src/sumo-netgen -v -c " + os.path.join(root, file))
+            line = cout.readline()
+            while line:
+                 print line[:-1]
+                 line = cout.readline()
+            print "----------------------------------\n"
