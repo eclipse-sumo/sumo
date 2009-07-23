a5a07128d50ce9903f95dae90596a85ca1455357
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-07-23 06:02:58 +0000

    debugging building, refs#3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7476 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSBitSetLogic.h
+++ sumo/src/microsim/MSBitSetLogic.h
@@ -114,7 +114,7 @@ public:
     }
 
     virtual bool isCrossing() const throw() {
-        for(Foes::const_iterator i=myLogic->begin(); i!=myLogic->end(); ++i) {
+        for(typedef std::vector< std::bitset< N > >::const_iterator i=myLogic->begin(); i!=myLogic->end(); ++i) {
             if((*i).any()) {
                 return true;
             }
