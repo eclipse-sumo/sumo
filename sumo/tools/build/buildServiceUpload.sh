b0a0207da77b7ad8ff3881e044e525e6cfdd71bf
Michael Behrisch <oss@behrisch.de>
2009-07-24 19:58:33 +0000

    cleaning empty dirs from data dir, fixing path, refs #63
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7510 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/buildServiceUpload.sh
+++ sumo/tools/build/buildServiceUpload.sh
@@ -2,7 +2,7 @@
 PREFIX=$1
 for f in $PREFIX/sumo/sumo*.tar.bz2; do
   if test -e $PREFIX/sumo/configure -a $f -nt $PREFIX/sumo/configure; then
-    curl -n -X PUT -T $PREFIX/sumo/data/package/sumo.spec https://api.opensuse.org/source/home:behrisch/sumo_nightly/sumo.spec
+    curl -n -X PUT -T $PREFIX/sumo/build/package/sumo.spec https://api.opensuse.org/source/home:behrisch/sumo_nightly/sumo.spec
     curl -n -X PUT -T $PREFIX/sumo/`basename $f` https://api.opensuse.org/source/home:behrisch/sumo_nightly/`basename $f`
   fi
 done
