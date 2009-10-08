fc664ec20df4848adf0f3d9c51f55a8605c9e6bf
Yun-Pang Floetteroed <Yun-Pang.Floetteroed@dlr.de>
2009-10-08 12:41:34 +0000

    reorganized directory for the test with a small network, refs #514
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7839 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/contributed/calibration/smallNetwork/calibrate.bat
@@ -0,0 +1,3 @@
+set PATH=..\..\assign;..\..\trip;%PATH%
+generateTripsXml.py -n input\net.net.xml -m input\mtx0708_histo.fma -d input\districts.xml -s 30
+dua-iterate.py -n input\net.net.xml -t trips.trips.xml -d input\input_weights1.dump.xml -s 0 -c ..\..\assign\calibration\src -a 3600
