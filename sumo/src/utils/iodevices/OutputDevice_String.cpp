eee73c49c0fef38fd4f547f91d53f841cb054346
Michael Behrisch <oss@behrisch.de>
2009-10-10 20:02:17 +0000

    refactoring gui settings saving to xml, refs #113
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7845 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/utils/iodevices/OutputDevice_String.cpp
@@ -0,0 +1,62 @@
+/****************************************************************************/
+/// @file    OutputDevice_String.cpp
+/// @author  Michael Behrisch
+/// @date    2009
+/// @version $Id$
+///
+// An output device that encapsulates a stringstream
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2009 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <sstream>
+#include <string>
+#include "OutputDevice_String.h"
+
+#ifdef CHECK_MEMORY_LEAKS
+#include <foreign/nvwa/debug_new.h>
+#endif // CHECK_MEMORY_LEAKS
+
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+OutputDevice_String::OutputDevice_String() throw(IOError) {
+}
+
+
+OutputDevice_String::~OutputDevice_String() throw() {
+}
+
+
+std::string
+OutputDevice_String::getString() throw() {
+    return myStream.str();
+}
+
+
+std::ostream &
+OutputDevice_String::getOStream() throw() {
+    return myStream;
+}
+
+
+/****************************************************************************/
---
+++ sumo/src/utils/iodevices/OutputDevice_String.h
@@ -0,0 +1,84 @@
+/****************************************************************************/
+/// @file    OutputDevice_String.h
+/// @author  Michael Behrisch
+/// @date    2009
+/// @version $Id$
+///
+// An output device that encapsulates a stringstream
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2009 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef OutputDevice_String_h
+#define OutputDevice_String_h
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <fstream>
+#include "OutputDevice.h"
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class OutputDevice_String
+ * @brief An output device that encapsulates an ofstream
+ *
+ * Please note that the device gots responsible for the stream and deletes
+ *  it (it should not be deleted elsewhere).
+ */
+class OutputDevice_String : public OutputDevice {
+public:
+    /** @brief Constructor
+     * @exception IOError Should not be thrown by this implementation
+     */
+    OutputDevice_String() throw(IOError);
+
+
+    /// @brief Destructor
+    ~OutputDevice_String() throw();
+
+
+    /** @brief Returns the current content as a string
+     * @return The content as string
+     */
+    std::string getString() throw();
+
+protected:
+    /// @name Methods that override/implement OutputDevice-methods
+    /// @{
+
+    /** @brief Returns the associated ostream
+     * @return The used stream
+     */
+    std::ostream &getOStream() throw();
+    /// @}
+
+
+private:
+    /// The wrapped ofstream
+    std::ostringstream myStream;
+
+};
+
+
+#endif
+
+/****************************************************************************/
+
