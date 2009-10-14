524504f82215f7998ccc59b2cfc1429dcfdc0447
Michael Behrisch <oss@behrisch.de>
2009-10-14 09:24:53 +0000

    small test adaptions, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7863 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/guisimrunner.py
+++ sumo/tests/guisimrunner.py
@@ -11,13 +11,12 @@ sumoBinary = os.environ.get("SUMO_BINARY", sumoBinary)
 guisimBinary = os.environ.get("GUISIM_BINARY", guisimBinary)
 
 #start sumo to generate config
-subprocess.call(sumoBinary + " --error-log guisim.stderr --save-configuration guisim.cfg " + " ".join(sys.argv[1:]), 
+subprocess.call(sumoBinary + " --message-log guisim.stdout --error-log guisim.stderr --save-configuration guisim.sumo.cfg " + " ".join(sys.argv[1:]), 
                 shell=True, stdout=sys.stdout, stderr=sys.stderr)
-ret = subprocess.call(guisimBinary + " -Q -c guisim.cfg", 
+ret = subprocess.call(guisimBinary + " -Q -c guisim.sumo.cfg", 
                 shell=True, stdout=sys.stdout, stderr=sys.stderr)
 if os.path.exists("guisim.stderr"):
     f = open("guisim.stderr")
     shutil.copyfileobj(f, sys.stderr)
     f.close()
-    os.remove("guisim.stderr")
 sys.exit(ret)
