1c90b95d3801a68bd58ef838ec9f001a9ffa4f4e
Michael Behrisch <oss@behrisch.de>
2009-11-06 09:23:52 +0000

    no gui tests on build failure, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7953 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/guisimrunner.py
+++ sumo/tests/guisimrunner.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 import os, subprocess, sys, shutil
 
-sumoDir = os.path.join(os.path.dirname(sys.argv[0]), "..")
-sumoBinary = os.path.join(sumoDir, "bin", "sumo.exe")
-guisimBinary = os.path.join(sumoDir, "bin", "guisim.exe")
-if os.name == 'posix':
-    sumoBinary = os.path.join(sumoDir, "src", "sumo")
-    guisimBinary = os.path.join(sumoDir, "src", "sumo-guisim")
+sumoBinary = os.path.join(os.path.dirname(sys.argv[1]), "sumo")
 sumoBinary = os.environ.get("SUMO_BINARY", sumoBinary)
-guisimBinary = os.environ.get("GUISIM_BINARY", guisimBinary)
+guisimBinary = sys.argv[1]
 
 #start sumo to generate config
-subprocess.call(sumoBinary + " --message-log guisim.stdout --error-log guisim.stderr --save-configuration guisim.sumo.cfg " + " ".join(sys.argv[1:]), 
+subprocess.call(sumoBinary + " --message-log guisim.stdout --error-log guisim.stderr --save-configuration guisim.sumo.cfg " + " ".join(sys.argv[2:]), 
                 shell=True, stdout=sys.stdout, stderr=sys.stderr)
 ret = subprocess.call(guisimBinary + " -Q -c guisim.sumo.cfg", 
-                shell=True, stdout=sys.stdout, stderr=sys.stderr)
+                      shell=True, stdout=sys.stdout, stderr=sys.stderr)
 if os.path.exists("guisim.stderr"):
     f = open("guisim.stderr")
     shutil.copyfileobj(f, sys.stderr)
