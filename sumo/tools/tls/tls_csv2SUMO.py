ac383b1f525c58b33670f6e6d2387e666fe04371
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-11-20 14:52:28 +0000

    code beautification, refs#47
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7996 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/tls/tls_csv2SUMO.py
+++ sumo/tools/tls/tls_csv2SUMO.py
@@ -17,9 +17,7 @@ offset;0
 time;<PHASE_LENGTH>[;<PHASE_LENGTH>]+
 (max;<PHASE_LENGTH>[;<PHASE_LENGTH>]+)*
 
-
-
-Copyright (C) 2008 DLR/TS, Germany
+Copyright (C) 2009 DLR/TS, Germany
 All rights reserved
 """
 
@@ -31,8 +29,8 @@ import sumonet
 
 
 if len(sys.argv)<2:
-	print >> sys.stderr, "Call: tls_csv2SUMO.py <TLS_CSV> <NET>"
-	sys.exit()
+    print >> sys.stderr, "Call: tls_csv2SUMO.py <TLS_CSV> <NET>"
+    sys.exit()
 
 
 minTimes = []
@@ -46,28 +44,28 @@ offset = 0
 
 fd = open(sys.argv[1])
 for l in fd:
-	l = l.strip()
-	v = l.split(";")
-	if v[0]=="key":
-		key = v[1]
-	elif v[0]=="subkey":
-		subkey = v[1]
-	elif v[0]=="offset":
-		offset = v[1]
-	elif v[0]=="link":
-		linkNo = int(v[1])
-		fromDef = v[2]
-		toDef = v[3]
-		minor = v[4]=="1"
-		links.append( [ fromDef, toDef, linkNo, minor ] )
-	elif v[0]=="min":
-		minTimes = v[1:]
-	elif v[0]=="max":
-		maxTimes = v[1:]
-	elif v[0]=="time":
-		normTimes = v[1:]
-	else:
-		defs.append(v[1:])
+    l = l.strip()
+    v = l.split(";")
+    if v[0]=="key":
+        key = v[1]
+    elif v[0]=="subkey":
+        subkey = v[1]
+    elif v[0]=="offset":
+        offset = v[1]
+    elif v[0]=="link":
+        linkNo = int(v[1])
+        fromDef = v[2]
+        toDef = v[3]
+        minor = v[4]=="1"
+        links.append( [ fromDef, toDef, linkNo, minor ] )
+    elif v[0]=="min":
+        minTimes = v[1:]
+    elif v[0]=="max":
+        maxTimes = v[1:]
+    elif v[0]=="time":
+        normTimes = v[1:]
+    else:
+        defs.append(v[1:])
 fd.close()
 
 parser = make_parser()
@@ -82,37 +80,37 @@ linkMap = [0]*noConnections
 minorV = [False]*noConnections
 mapCount = [0]*noConnections
 for tl_c in tls._connections:
-	ei = tl_c[0] # incoming lane in our net
-	eo = tl_c[1] # outgoing lane in our net
-	for l in links:
-		valid = True
-		if l[0].find('_')<0:
-			# edge only given
-			if l[0]!=ei._edge._id:
-				valid = False
-		else:
-			# lane given
-			if l[0]!=ei.getID():
-				valid = False
-		if l[1]!="":
-			if l[1].find('_')<0:
-				# edge only given
-				if l[1]!=eo._edge._id:
-					valid = False
-			else:
-				# lane given
-				if l[1]!=eo.getID():
-					valid = False
-		if valid:
-			linkMap[tl_c[2]] = l[2]
-			minorV[tl_c[2]] = l[3]
-			mapCount[tl_c[2]] = mapCount[tl_c[2]] + 1
+    ei = tl_c[0] # incoming lane in our net
+    eo = tl_c[1] # outgoing lane in our net
+    for l in links:
+        valid = True
+        if l[0].find('_')<0:
+            # edge only given
+            if l[0]!=ei._edge._id:
+                valid = False
+        else:
+            # lane given
+            if l[0]!=ei.getID():
+                valid = False
+        if l[1]!="":
+            if l[1].find('_')<0:
+                # edge only given
+                if l[1]!=eo._edge._id:
+                    valid = False
+            else:
+                # lane given
+                if l[1]!=eo.getID():
+                    valid = False
+        if valid:
+            linkMap[tl_c[2]] = l[2]
+            minorV[tl_c[2]] = l[3]
+            mapCount[tl_c[2]] = mapCount[tl_c[2]] + 1
 
 for i in range(0, len(normTimes)):
-	for l in range(0, len(linkMap)):
-		if mapCount[l]==0:
-			print >> sys.stderr, "Error: Link " + str(l) + " is not described."
-			sys.exit()
+    for l in range(0, len(linkMap)):
+        if mapCount[l]==0:
+            print >> sys.stderr, "Error: Link " + str(l) + " is not described."
+            sys.exit()
 
 print "<add>"
 print "    <tl-logic type=\"static\">"
@@ -121,37 +119,34 @@ print "        <subkey>" + subkey + "</subkey>"
 print "        <phaseno>" + str(len(normTimes)) + "</phaseno>"
 print "        <offset>" + offset + "</offset>"
 for i in range(0, len(normTimes)):
-	phase = ""
-	brake = ""
-	yellow = ""
-	for l in range(0, len(linkMap)):
-		d = defs[linkMap[l]-1]
-		if d[i]=='r':
-			phase = "0" + phase
-			brake = "1" + brake
-			yellow = "0" + yellow
-		elif d[i]=='y' or d[i]=='a':
-			phase = "0" + phase
-			brake = "1" + brake
-			yellow = "1" + yellow
-		elif d[i]=='g':
-			phase = "1" + phase
-			brake = "0" + brake
-			yellow = "0" + yellow
-		else:
-			print >> sys.stderr, "missing value; setting to g"
-			phase = "1" + phase
-			brake = "0" + brake
-			yellow = "0" + yellow
-	pd = '        <phase duration="' + normTimes[i] + '" '
-	if len(minTimes)==len(normTimes):
-		pd = pd + 'min_dur="' + minTimes[i] + '" '
-	if len(maxTimes)==len(normTimes):
-		pd = pd + 'max_dur="' + maxTimes[i] + '" '
-	pd = pd + 'phase="' + phase + '" brake="' + brake + '" yellow="' + yellow + '"/>'
-	print pd
+    phase = ""
+    brake = ""
+    yellow = ""
+    for l in range(0, len(linkMap)):
+        d = defs[linkMap[l]-1]
+        if d[i]=='r':
+            phase = "0" + phase
+            brake = "1" + brake
+            yellow = "0" + yellow
+        elif d[i]=='y' or d[i]=='a':
+            phase = "0" + phase
+            brake = "1" + brake
+            yellow = "1" + yellow
+        elif d[i]=='g':
+            phase = "1" + phase
+            brake = "0" + brake
+            yellow = "0" + yellow
+        else:
+            print >> sys.stderr, "missing value; setting to g"
+            phase = "1" + phase
+            brake = "0" + brake
+            yellow = "0" + yellow
+    pd = '        <phase duration="' + normTimes[i] + '" '
+    if len(minTimes)==len(normTimes):
+        pd = pd + 'min_dur="' + minTimes[i] + '" '
+    if len(maxTimes)==len(normTimes):
+        pd = pd + 'max_dur="' + maxTimes[i] + '" '
+    pd = pd + 'phase="' + phase + '" brake="' + brake + '" yellow="' + yellow + '"/>'
+    print pd
 print "    </tl-logic>"
 print "</add>"
-
-	
-	
