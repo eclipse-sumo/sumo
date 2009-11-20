b447eaf81e5235ec2c8b3aeb27d73d74fe72b075
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2009-11-20 14:56:13 +0000

    osm extraction script added
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@7997 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/import/osm/osmGet.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+@file    osmGet.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2009-08-01
+@version $Id: osmGet.py 6858 2009-02-25 13:27:57Z behrisch $
+
+Retrieves an area from OpenStreetMap.
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+
+import os,sys
+
+if len(sys.argv)<3:
+	print >> sys.stderr, "Call: osmGet.py <PREFIX> <BOUNDING_BOX> <TILES_NUMBER>"
+	sys.exit()
+
+prefix = sys.argv[1]#"bs"
+box = sys.argv[2]#"10.38,52.22,10.74,52.34"
+num = int(sys.argv[3])#20
+
+boxV = box.split(",")
+bb = float(boxV[0])
+ee = float(boxV[2])
+
+if bb>ee or boxV[1]>boxV[3]:
+	print >> sys.stderr, "Box minimum is larger than box maximum."
+	sys.exit()
+
+
+b = bb
+for i in range(0,num):
+	e = b + (ee-bb) / float(num)
+	cbox = str(b) + "," + boxV[1] + "," + str(e) + "," + boxV[3]
+	call = "wget.exe http://api.openstreetmap.org/api/0.6/map?bbox=" + cbox + " -O " + prefix + str(i) + "_" + str(num) + ".osm.xml"
+	print call
+	os.system(call)
+	b = e
+
+	
