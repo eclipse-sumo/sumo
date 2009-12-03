2c64252744a8fbd63fce2be08e756cc4fdc54bd3
Michael Behrisch <oss@behrisch.de>
2009-12-03 09:43:05 +0000

    xml check preparation, refs #166
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8056 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/xml/schemaCheck.py
+++ sumo/tools/xml/schemaCheck.py
@@ -3,7 +3,7 @@
 @file    schemaCheck.py
 @author  Daniel.Krajzewicz@dlr.de
 @date    03.12.2009
-@version $Id: schemaCheck.py 7559 2009-07-28 20:29:48Z behrisch $
+@version $Id$
 
 Checks schema for files matching certain file names using SAX2Count.exe.
 
@@ -11,17 +11,23 @@ Copyright (C) 2008 DLR/TS, Germany
 All rights reserved
 """
 
-import os, os.path, sys, shutil
+import os, sys
 
 toCheck = [ "input_edges.edg.xml" ]
-srcRoot = os.path.join(os.path.dirname(sys.argv[0]), sys.argv[1])
+srcRoot = "."
+if len(sys.argv) > 1:
+    srcRoot = sys.argv[1]
+sax2count = "SAX2Count.exe"
+if 'XERCES_64' in os.environ:
+    sax2count = os.path.join(os.environ['XERCES_64'], "bin", sax2count)
+elif 'XERCES' in os.environ:
+    sax2count = os.path.join(os.environ['XERCES'], "bin", sax2count)
 #os.environ['XML_CATALOG_FILES']=os.path.join(os.path.join(os.path.dirname(sys.argv[0]), "./catalog.xml"))
 #print os.environ['XML_CATALOG_FILES']
 
 for root, dirs, files in os.walk(srcRoot):
     for name in files:
         if name in toCheck:
-            os.system("SAX2Count.exe -v=always -f " + os.path.join(root, name) + " > dev0")
-        for ignoreDir in ['.svn', 'foreign']:
-            if ignoreDir in dirs:
-                dirs.remove(ignoreDir)
+            os.system(sax2count + " -v=always -f " + os.path.join(root, name))
+        if '.svn' in dirs:
+            dirs.remove('.svn')
