8a4df3857189e02542a44ce6c377420929214d24
Michael Behrisch <oss@behrisch.de>
2010-01-07 10:13:11 +0000

    creating winbin in nightly build, refs #160
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8115 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/buildServiceUpload.sh
+++ sumo/tools/build/buildServiceUpload.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 PREFIX=$1
+NIGHTDIR=/misc/nas/Daten/Sumo/Nightly
 for f in $PREFIX/sumo/sumo*.tar.bz2; do
   if test -e $PREFIX/sumo/configure -a $f -nt $PREFIX/sumo/configure; then
     curl -n -X PUT -T $PREFIX/sumo/build/package/sumo.spec https://api.opensuse.org/source/home:behrisch/sumo_nightly/sumo.spec
     curl -n -X PUT -T $PREFIX/sumo/`basename $f` https://api.opensuse.org/source/home:behrisch/sumo_nightly/`basename $f`
+    BASE=`basename $f .tar.bz2`
+    NIGHTFILE=$NIGHTDIR/sumo-msvc8Win32-bin.zip
+    if test -e $NIGHTFILE; then
+      cd /tmp
+      rm -rf $BASE
+      tar xjf $f $BASE/examples $BASE/tools
+      mkdir $BASE/bin
+      cd $BASE/bin
+      unzip -q $NIGHTFILE
+      cd ../..
+      zip -rqmT $NIGHTDIR/sumo-winbin${BASE:4}.zip $BASE
+    fi
   fi
 done
 for f in $PREFIX/sumo/sumo-*.tar.* $PREFIX/sumo/sumo-*.zip; do
   if test -e $PREFIX/sumo/configure -a $f -nt $PREFIX/sumo/configure; then
-    cp $f /misc/nas/Daten/Sumo/Nightly
+    cp $f $NIGHTDIR
   fi
 done
--- sumo/tools/build/doxygen.sh
+++ sumo/tools/build/doxygen.sh
@@ -4,6 +4,8 @@ REMOTEDIR="behrisch,sumo@web.sourceforge.net:/home/groups/s/su/sumo/htdocs/daily
 DOCDIR=$PREFIX/sumodoku
 
 cd $PREFIX/sumo
+scp -q docs/internet/daily/index.php $REMOTEDIR
+
 rm -rf $DOCDIR
 nice -19 doxygen sumo.doxyconf &> $DOCDIR.log
 scp -qr $DOCDIR $REMOTEDIR
