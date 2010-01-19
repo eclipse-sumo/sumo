d1a5925148623d6047e734cf6e45b6a94d386b7c
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-01-19 09:34:36 +0000

    tool for network vaidation added, refs#189
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8153 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/tls/tls_check.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+@file    tls_csv2SUMO.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2009-08-01
+@version $Id: tls_csv2SUMO.py 8050 2009-12-03 07:37:52Z behrisch $
+
+Verifies the traffic lights in the given network.
+Currently verified:
+- phase length matches controlled link number
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+
+import sys, os
+from xml.sax import saxutils, make_parser, handler
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "../lib"))
+import sumonet
+
+
+if len(sys.argv)<2:
+    print >> sys.stderr, "Call: tls_csv2SUMO.py <NET>"
+    sys.exit()
+
+parser = make_parser()
+net1 = sumonet.NetReader(withPrograms=True)
+parser.setContentHandler(net1)
+parser.parse(sys.argv[1])
+net1 = net1.getNet()
+
+for tlsID in net1._id2tls:
+    print "Checking tls '%s'" % tlsID 
+    tls = net1._id2tls[tlsID]
+    noConnections = tls._maxConnectionNo + 1
+    for prog in tls._programs:
+        print "   Checking program '%s'" % prog
+        prog = tls._programs[prog]
+        for i, phase in enumerate(prog._phases):
+            if len(phase[0])!=noConnections:
+                print "      Error: phase %s describes %s signals instead of %s." % (i, len(phase[0]), noConnections)
