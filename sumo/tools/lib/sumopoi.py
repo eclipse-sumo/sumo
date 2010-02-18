39eb5bd16664a77897e924eceaa102cc1b2ddd97
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-02-18 11:40:25 +0000

    added poi to induction loops conversion script, refs#141
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8290 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/lib/sumopoi.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+@file    sumopoi.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2010-02-18
+@version $Id: sumopoi.py 7499 2009-07-24 06:40:47Z dkrajzew $
+
+Library for reading and storing POIs.
+
+Copyright (C) 2010 DLR/TS, Germany
+All rights reserved
+"""
+
+import os, sys
+from xml.sax import saxutils, make_parser, handler
+from itertools import *
+
+
+class PoI:
+    def __init__(self, id, type, layer, color, pos, lane):
+        self._id = id
+        self._type = type
+        self._layer = layer
+        self._color = color
+        self._pos = pos
+        self._lane = lane
+
+
+class PoIReader(handler.ContentHandler):
+    def __init__(self):
+        self._id2poi = {}
+        self._pois = []
+
+    def startElement(self, name, attrs):
+        if name == 'poi':
+            if not attrs.has_key('lane'):
+                poi = PoI(attrs['id'], attrs['type'], int(attrs['layer']), attrs['color'], (float(attrs['x']), float(attrs['y'])), None)
+            else:
+                poi = PoI(attrs['id'], attrs['type'], int(attrs['layer']), attrs['color'], float(attrs['pos']), attrs['lane'])
+            self._id2poi[poi._id] = poi
+            self._pois.append(poi)
+
+
+
---
+++ sumo/tools/shapes/pois2inductionLoops.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+@file    pois2inductionLoops.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2010-02-18
+@version $Id: pois2inductionLoops.py 6858 2009-02-25 13:27:57Z behrisch $
+
+
+Converts a given pois located on lanes into induction loop detectors;
+Each poi is replicated to cover all lanes of the road.
+The detectors are named <POINAME>__l<LANE_INDEX>
+
+Call: pois2inductionLoops.py <NET> <POIS> <OUTPUT>
+
+Copyright (C) 2010 DLR/TS, Germany
+All rights reserved
+"""
+
+
+import os, string, sys, StringIO
+from xml.sax import saxutils, make_parser, handler
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "../lib"))
+import sumonet
+import sumopoi
+
+
+if len(sys.argv) < 4:
+    print "Usage: " + sys.argv[0] + " <NET> <POIS> <OUTPUT>"
+    sys.exit()
+parser = make_parser()
+print "Reading net..."
+net = sumonet.NetReader()
+parser.setContentHandler(net)
+parser.parse(sys.argv[1])
+net = net.getNet()
+
+print "Reading PoIs..."
+pois = sumopoi.PoIReader()
+parser.setContentHandler(pois)
+parser.parse(sys.argv[2])
+pois = pois._pois
+
+fdo = open(sys.argv[3], "w")
+print >> fdo, '<additional>'
+for poi in pois:
+    if not poi._lane:
+        print "Error: poi '%s' is not on a lane" % poi._id
+        continue
+    edge = poi._lane[:poi._lane.rfind('_')]
+    edge = net._id2edge[edge]
+    for i, l in enumerate(edge._lanes):
+        print >> fdo, '    <e1-detector id="%s__l%s" lane="%s" pos="%s" freq="60" file="e1_output.xml"/>' % (poi._id, i, l.getID(), poi._pos)
+    print >> fdo, ''
+print >> fdo, '</additional>'
+fdo.close()
+
+
