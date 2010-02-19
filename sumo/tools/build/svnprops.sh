ff65c80f92f1a4ed5f26e44d99debe35c4d448d2
Michael Behrisch <oss@behrisch.de>
2010-02-19 09:50:06 +0000

    fixing svn properties, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8297 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/svnprops.sh
+++ sumo/tools/build/svnprops.sh
@@ -1,4 +1,7 @@
 find tests -regex ".*\.\(xml\|complex\|dfrouter\|duarouter\|jtrrouter\|netconvert\|netgen\|od2trips\|polyconvert\|sumo\|meso\|tools\|traci\)" -print0 | xargs -0 svn ps svn:eol-style LF
+find tests -regex ".*\.\(xml\|complex\|dfrouter\|duarouter\|jtrrouter\|netconvert\|netgen\|od2trips\|polyconvert\|sumo\|meso\|tools\|traci\)" -executable -print0 | xargs -0 svn pd svn:executable
+
 find src tools -regex ".*\.\(cpp\|h\|py\|pl\|java\)" -print0 | xargs -0 svn ps svn:eol-style LF
 find src tools -regex ".*\.\(cpp\|h\|py\|pl\|java\)" -print0 | xargs -0 svn ps svn:keywords "HeadURL Id LastChangedBy LastChangedDate LastChangedRevision"
+find src tools -regex ".*\.\(cpp\|h\|java\)" -executable -print0 | xargs -0 svn pd svn:executable
 
