113700ed518dc58ef2dc1e66a3a1e3e91630cf56
Michael Behrisch <oss@behrisch.de>
2010-02-19 07:52:09 +0000

    fixing guisim path, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8293 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/game/runner.py
+++ sumo/tests/complex/game/runner.py
@@ -4,7 +4,8 @@ import os, subprocess, sys, re
 guisimBinary = "guisim.exe"
 if os.name == "posix":
     guisimBinary="sumo-guisim"
-ret = subprocess.call([guisimBinary, "-Q", "-c", "sumo.sumo.cfg"])
+guisimBinary = os.environ.get("GUISIM_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', 'bin', guisimBinary))
+ret = subprocess.call([guisimBinary, "-G", "-Q", "-c", "sumo.sumo.cfg"])
 totalWait = 0
 for line in open("netstate.xml"):
     m = re.search('waitingTime="(\d+.\d+)"', line)
