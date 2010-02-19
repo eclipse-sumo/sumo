297d432b45d9367e4c958701067ffed48e25ae16
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-02-19 08:08:03 +0000

    added scripts for translating edges and nodes, refs#141
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8294 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/net/xmledges_applyOffset.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+@file    xmledges_applyOffset.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2009-08-01
+@version $Id: xmledges_applyOffset.py 8236 2010-02-10 11:16:41Z behrisch $
+
+Applies a given offset to edges given in an xml-edge-file.
+The results are written into <XMLEDGES>.mod.xml.
+Call: xmledges_applyOffset.py <XMLEDGES> <X-OFFSET> <Y-OFFSET>
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+
+import os, string, sys, StringIO
+from xml.sax import saxutils, make_parser, handler
+
+class XMLNodesReader(handler.ContentHandler):
+    def __init__(self, outFileName, xoff, yoff):
+        self._out = open(outFileName, 'w')
+        self._xoff = xoff
+        self._yoff = yoff
+        
+    def endDocument(self):
+        self._out.close()
+
+    def startElement(self, name, attrs):
+        self._out.write('<' + name)
+        for (key, value) in attrs.items():
+            if key == "shape":
+                shape = value.split(" ")
+                nshape = []
+                for s in shape:
+                    (x,y) = s.split(",")
+                    nshape.append(str(float(x)+self._xoff)+","+str(float(y)+self._yoff))
+                self._out.write(' %s="%s"' % (key, saxutils.escape(" ".join(nshape))))
+            else:
+                self._out.write(' %s="%s"' % (key, saxutils.escape(value)))
+        self._out.write('>')
+
+    def endElement(self, name):
+        self._out.write('</' + name + '>')
+
+    def characters(self, content):
+        self._out.write(saxutils.escape(content))
+                    
+    def ignorableWhitespace(self, content):
+        self._out.write(content)
+        
+    def processingInstruction(self, target, data):
+        self._out.write('<?%s %s?>' % (target, data))
+
+
+            
+if len(sys.argv) < 4:
+    print "Usage: " + sys.argv[0] + " <XMLEDGES> <X-OFFSET> <Y-OFFSET>"
+    sys.exit()
+parser = make_parser()
+reader = XMLNodesReader(sys.argv[1]+".mod.xml", float(sys.argv[2]), float(sys.argv[3]))
+parser.setContentHandler(reader)
+parser.parse(sys.argv[1])
---
+++ sumo/tools/net/xmlnodes_applyOffset.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+@file    xmlnodes_applyOffset.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2009-08-01
+@version $Id: xmledges_applyOffset.py 8236 2010-02-10 11:16:41Z behrisch $
+
+Applies a given offset to edges given in an xml-node-file.
+The results are written into <XMLNODES>.mod.xml.
+Call: xmlnodes_applyOffset.py <XMLNODES> <X-OFFSET> <Y-OFFSET>
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+
+import os, string, sys, StringIO
+from xml.sax import saxutils, make_parser, handler
+
+class XMLNodesReader(handler.ContentHandler):
+    def __init__(self, outFileName, xoff, yoff):
+        self._out = open(outFileName, 'w')
+        self._xoff = xoff
+        self._yoff = yoff
+        
+    def endDocument(self):
+        self._out.close()
+
+    def startElement(self, name, attrs):
+        self._out.write('<' + name)
+        for (key, value) in attrs.items():
+            if key == "x":
+                self._out.write(' %s="%s"' % (key, saxutils.escape(str(float(value)+self._xoff))))
+            elif key == "y":
+                self._out.write(' %s="%s"' % (key, saxutils.escape(str(float(value)+self._yoff))))
+            else:
+                self._out.write(' %s="%s"' % (key, saxutils.escape(value)))
+        self._out.write('>')
+
+    def endElement(self, name):
+        self._out.write('</' + name + '>')
+
+    def characters(self, content):
+        self._out.write(saxutils.escape(content))
+                    
+    def ignorableWhitespace(self, content):
+        self._out.write(content)
+        
+    def processingInstruction(self, target, data):
+        self._out.write('<?%s %s?>' % (target, data))
+
+
+            
+if len(sys.argv) < 4:
+    print "Usage: " + sys.argv[0] + " <XMLNODES> <X-OFFSET> <Y-OFFSET>"
+    sys.exit()
+parser = make_parser()
+reader = XMLNodesReader(sys.argv[1]+".mod.xml", float(sys.argv[2]), float(sys.argv[3]))
+parser.setContentHandler(reader)
+parser.parse(sys.argv[1])
