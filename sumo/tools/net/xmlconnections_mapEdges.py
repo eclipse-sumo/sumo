fa3c30b809304e50e1fa1a4f29c323654e60118c
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-02-19 09:43:05 +0000

    added script for renaming edges in connections, refs#141
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8296 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/net/xmlconnections_mapEdges.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+@file    xmlconnections_mapEdges.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2009-08-01
+@version $Id: xmlconnections_mapEdges.py 8236 2010-02-10 11:16:41Z behrisch $
+
+Reads edge id replacements from "edgemap.txt"; the format of this file is
+ <OLD_EDGE_ID>-><NEW_EDGE_ID>
+Reads the given connections file <CONNECTIONS> and replaces old edge names by new.
+The result is written to <CONNECTIONS>.mod.xml
+
+Call: xmlconnections_mapEdges.py <CONNECTIONS>
+
+Copyright (C) 2009 DLR/TS, Germany
+All rights reserved
+"""
+
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: " + sys.argv[0] + " <CONNECTIONS>"
+    sys.exit()
+
+# read map
+mmap = {}
+fdi = open("edgemap.txt")
+for line in fdi:
+	if line.find("->")<0:
+		continue
+	(orig, dest) = line.strip().split("->")
+	dest = dest.split(",")
+	mmap[orig] = dest
+fdi.close()
+
+fdi = open(sys.argv[1])
+fdo = open(sys.argv[1]+".mod.xml", "w")
+for line in fdi:
+	for orig in mmap:
+		line = line.replace('from="'+orig+'"', 'from="'+mmap[orig][-1]+'"')
+		line = line.replace('to="'+orig+'"', 'to="'+mmap[orig][0]+'"')
+	fdo.write(line)
+fdi.close()
+fdo.close()
+		
