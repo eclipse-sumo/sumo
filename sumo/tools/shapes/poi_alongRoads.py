717691332a718b73000e6a964af34b4b00de3764
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-02-20 19:45:08 +0000

    adding a script for extracting PoIs at TLS positions, refs#141
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8314 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/shapes/poi_alongRoads.py
+++ sumo/tools/shapes/poi_alongRoads.py
@@ -27,7 +27,7 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "../
 import sumonet
 
 if len(sys.argv) < 4:
-	print("Usage: " + sys.argv[0] + " <net> <edge1,edge2,...> <distance between POIs")
+	print >> sys.stderr, "Usage: " + sys.argv[0] + " <NET> <EDGE_ID>[,<EDGE_ID>]* <DISTANCE>"
 	sys.exit()
 edgeList = sys.argv[2].split(',')
 POIdist = float(sys.argv[3])
---
+++ sumo/tools/shapes/poi_atTLS.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+@file    poi_atTLS.py
+@author  Daniel.Krajzewicz@dlr.de
+@date    2010-02-20
+@version $Id: poi_atTLS.py 8236 2010-02-10 11:16:41Z behrisch $
+
+
+Copyright (C) 2010 DLR/TS, Germany
+All rights reserved
+"""
+
+import sys, os
+from xml.sax import saxutils, make_parser, handler
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "../lib"))
+import sumonet
+
+
+if len(sys.argv)<2:
+    print >> sys.stderr, "Usage: " + sys.argv[0] + " <NET>"
+    sys.exit()
+
+print("Reading net...")
+parser = make_parser()
+net1 = sumonet.NetReader(withPrograms=True)
+parser.setContentHandler(net1)
+parser.parse(sys.argv[1])
+net1 = net1.getNet()
+
+
+print("Writing output...")
+fdo = open('pois.add.xml', 'w')
+print >> fdo, '<?xml version="1.0"?>'
+print >> fdo, '<!-- poi_atTLS %s -->' % sys.argv[1]
+print >> fdo, '<additional>'
+for tlsID in net1._id2tls:
+    tls = net1._id2tls[tlsID]
+    nodes = set()
+    for c in tls._connections:
+        iLane = c[0]
+        iEdge = iLane.getEdge()
+        nodes.add(iEdge._to)
+    for n in nodes:
+       print >> fdo, '    <poi id="%s" type="default" color="1,0,0" layer="0" x="%s" y="%s"/>' % (n._id, n._coord[0], n._coord[1])
+print >> fdo, '</additional>'
+fdo.close()
