15cefc1ca84050a67e86579b6e023d43ee5c0bf5
Michael Behrisch <oss@behrisch.de>
2010-02-28 22:21:55 +0000

    first try on tkinter dialog
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8336 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/game/runner.py
+++ sumo/tests/complex/game/runner.py
@@ -1,6 +1,69 @@
 #!/usr/bin/env python
-import os, subprocess, sys, re
+import os, subprocess, sys, re, pickle
 
+_HIGHSCOREFILE = "scores.pkl"
+
+def loadHighscore():
+    try:
+        return pickle.load(open(_HIGHSCOREFILE))
+    except:
+        pass
+    return 10*[("", "", -1.)]
+
+def checkHigh(points):
+    idx = 0
+    while idx < len(high) and high[idx][2] < points and high[idx][2] != -1:
+        idx += 1
+    return idx
+
+def save(name, game, points):
+    idx = checkHigh(points)
+    high.insert(idx, (name, game, points))
+    high.pop()
+    f = open(_HIGHSCOREFILE, 'w')
+    pickle.dump(high, f)
+    f.close()
+
+import Tkinter
+class TKDialog:
+    def __init__(self, game, points):
+        self.root = Tkinter.Tk()
+        self.name = None
+        self.game = game
+        self.points = points
+        haveHigh = False
+        self.root.title("High score")
+
+        idx = 1
+        for n, g, p in high:
+            if not haveHigh and (p == -1 or p < points):
+                self.name = Tkinter.Entry(self.root)
+                self.name.grid(row=idx, sticky=Tkinter.W)
+                p = points
+                haveHigh = True
+                self.root.title("Congratulations")
+            else:
+                Tkinter.Label(self.root, text=n).grid(row=idx, sticky=Tkinter.W)
+            Tkinter.Label(self.root, text=str(p)).grid(row=idx, column=1)
+            idx += 1
+        Tkinter.Button(self.root, text="OK", command=self.ok).grid(row=idx)
+        self.root.grid()
+#        self.root.bind("&lt;Return>", self.ok)
+#        self.root.grab_set()
+        # The following three commands are needed so the window pops
+        # up on top on Windows...
+        self.root.iconify()
+        self.root.update()
+        self.root.deiconify()
+        self.root.mainloop()
+
+    def ok(self):
+        if self.name != None:
+            save(self.name.get(), self.game, self.points)
+        self.root.destroy()
+
+
+high = loadHighscore()
 guisimBinary = "guisim.exe"
 if os.name == "posix":
     guisimBinary="sumo-guisim"
@@ -17,4 +80,5 @@ for line in open("tlsstate.xml"):
     if m:
         switch += [m.group(1)]
 print switch, totalWait
+#TKDialog(switch, totalWait)
 sys.exit(ret)
