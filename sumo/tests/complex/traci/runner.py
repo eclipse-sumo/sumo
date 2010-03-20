15a67f261710d4a63378db87c6b2f5a6d04216f2
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-03-20 20:17:13 +0000

    patching and inserting the repeated execution test, refs#238
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8492 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/runner.py
+++ sumo/tests/complex/traci/runner.py
@@ -16,27 +16,23 @@ def runSingle(sumoEndTime, traciEndTime):
     sumoProcess = subprocess.Popen("%s -c %s -e %s" % (sumoBinary, "sumo.sumo.cfg", str(sumoEndTime)), shell=True, stdout=sys.stdout)
     traciControl.initTraCI(PORT)
     while not step>traciEndTime:
-        try:
-            traciControl.cmdSimulationStep(1)
-            vehs = traciControl.cmdGetVehicleVariable_idList("x")
-            if vehs.index("horiz")<0 or len(vehs)>1:
-                print "Something is false"
-            step += 1
-        except traciControl.FatalTraCIError:
-            print "Closed by SUMO"
-            doClose = False
-            break
-    if doClose:
-        traciControl.cmdClose()
-    print "Print ended at step %s" % step
+        traciControl.cmdSimulationStep(1)
+        vehs = traciControl.cmdGetVehicleVariable_idList("x")
+        if vehs.index("horiz")<0 or len(vehs)>1:
+            print "Something is false"
+        step += 1
+    print "Print ended at step %s" % traciControl.cmdGetSimulationVariable_currentTime()
+    traciControl.cmdClose()
     sys.stdout.flush()
     
-#print "SUMO ends first"
-#for i in range(0, 10):
-#    print " Run %s" % i
-#    runSingle(50, 100)
+print "----------- SUMO ends first -----------"
+sys.stdout.flush()
+for i in range(0, 10):
+    print " Run %s" % i
+    runSingle(50, 99)
 
-print "TraCI ends first"
+print "----------- TraCI ends first -----------"
+sys.stdout.flush()
 for i in range(0, 10):
     print " Run %s" % i
     runSingle(101, 99)
