798a043b487e6077e85361fbb0d801af9be058aa
Michael Behrisch <oss@behrisch.de>
2010-04-01 20:39:05 +0000

    test fixes, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8543 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/state/runner.py
+++ sumo/tests/complex/state/runner.py
@@ -5,7 +5,7 @@ idx = sys.argv.index(":")
 saveParams = sys.argv[1:idx]
 loadParams = sys.argv[idx+1:]
 
-sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', 'bin', 'sumo'))
+sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', 'bin', 'sumo'))
 subprocess.call([sumoBinary]+saveParams,
                 shell=(os.name=="nt"), stdout=sys.stdout, stderr=sys.stderr)
 subprocess.call([sumoBinary]+loadParams,
