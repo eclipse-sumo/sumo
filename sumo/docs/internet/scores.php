f7b0ec32acd40ac028169551e01e07422fbe57df
Michael Behrisch <oss@behrisch.de>
2010-06-04 15:41:13 +0000

    small website improvements, refs #241
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8799 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/docs/internet/scores.php
+++ sumo/docs/internet/scores.php
@@ -1,3 +1,71 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<link rel="stylesheet" type="text/css" href="./css/sumo.css">
+	<head>
+		<title>SUMO - Simulation of Urban MObility  -  Home</title>
+<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+<META NAME="DC.Title" CONTENT="SUMO - Simulation of Urban MObility  -  Home">
+<META NAME="DC.Creator.PersonalName" CONTENT="Krajzewicz, Daniel">
+<META NAME="DC.Subject" CONTENT="road traffic simulation package SUMO (Simulation of Urban MObility)">
+<META NAME="DC.Description" CONTENT="">
+<META NAME="DC.Publisher" CONTENT="Institute of Transport Research at the German Aerospace Centre">
+<META NAME="DC.Contributor" CONTENT="Center for Applied Informatics, Cologne">
+<META NAME="DC.Type" CONTENT="Text">
+<META NAME="DC.Format" SCHEME="IMT" CONTENT="text/html">
+<META NAME="DC.Identifier" CONTENT="http://sumo.sourceforge.net">
+<META NAME="DC.Language" SCHEME="ISO639-1" CONTENT="en">
+<META NAME="DC.Relation" SCHEME="URL" CONTENT="http://sumo.sourceforge.net/index.html">
+<META NAME="DC.Coverage" CONTENT="Introduction, Navigation">
+<META NAME="DC.Rights" CONTENT="(c) IFS/DLR">
+<META NAME="DC.Date.X-MetadataLastModified" SCHEME="ISO8601" CONTENT="2003-07-10">
+
+	</head>
+	<body>
+
+        <table border="0">
+          <tr>
+              <td><img src="./images/64x64_web.gif" width="64" height="64"></td>
+              <td><img src="./images/sumo_logo.gif" width="72" height="17"><br>
+              <img src="./images/sumo_full.gif" width="211" height="16"></td>
+          </tr>
+          </table>
+		<hr>
+
+
+<!-- Navigation bar begin -->
+		<table border="0" ID="Table1">
+		<tr><td valign="top" width="120">
+		<div class="navbar">
+			<a href="index.shtml" class="SUMOMainLink">Home</a><br>
+			<a href="docs/documentation.shtml" class="SUMOMainLink">Documentation</a><br>
+			<a href="downloads/downloads.shtml" class="SUMOMainLink">Downloads</a><br>
+			<a href="contact.shtml" class="SUMOMainLink">Contact</a><br>
+
+			<a href="http://sumo.sourceforge.net/wiki/" class="SUMOMainLink">wiki</a><br/>
+
+			<center><br>
+			<a href="http://www.dlr.de/ts/" class="SUMOMainLink">
+            <img src="./images/logo_dlr.gif" width="40" height="42"></a>
+<br>
+
+			<a href="http://sourceforge.net/projects/sumo/"><img src="http://sourceforge.net/sflogo.php?group_id=45607&type=1" width="88" height="31" border="0" alt="SourceForge.net Logo"></a>
+			</center>
+		</div>
+		</td><td width="20"></td>
+<!-- Navigation bar end -->
+
+
+		<td valign="top">
+		<div class="SUMOMainText">
+<!-- Text area here -->
+<h1>SUMO - <u>S</u>imulation of <u>U</u>rban <u>MO</u>bility</h1>
+<h2>Das Spiel</h2>
+<p>Auf dieser Seite pr&auml;sentieren wir das Ampelspiel zur Langen Nacht der Wissenschaften.</p>
+<p>Sie k&ouml;nnen das vollst&auml;ndige <a href="sumogame.zip">Spiel herunterladen</a> und unten auch die globale Highscoreliste sehen
+</p>
+
+<hr class="thin"/>
+
 <?php
 require_once('./database.php');
 import_request_variables("g", "g_");
@@ -13,14 +81,14 @@ function query($query) {
 $gameIDs = array();
 $res = query("SELECT * FROM games");
 while ($row = mysql_fetch_assoc($res)):
-    $gameIDs[$row["title"]] = $row["gameID"];
+    $gameID = $row["gameID"];
+    $gameIDs[$row["title"]] = $gameID;
     $titles[$row["gameID"]] = $row["fulltitle"];
 endwhile;
 mysql_free_result($res);
 
-if (!isset($gameIDs[$g_game])):
-    print("<span class=\"wichtig\">Fehler:</span> Unbekanntes Spiel.");
-    exit();
+if (!isset($g_game)):
+    $g_game="TLS";
 endif;
 
 $gameID = $gameIDs[$g_game];
@@ -92,3 +160,6 @@ while ($row = mysql_fetch_assoc($res))
 mysql_free_result($res);
 ?>
 </table>
+
+	</body>
+</html>
