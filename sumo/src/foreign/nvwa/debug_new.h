9cc14f38d2baffc012fb215f6bcd508f70636aea
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2010-07-05 13:34:29 +0000

    added memory checker enabling/disabling macros, refs#320
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@8855 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/foreign/nvwa/debug_new.h
+++ sumo/src/foreign/nvwa/debug_new.h
@@ -1,6 +1,6 @@
 // -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 // vim:tabstop=4:shiftwidth=4:expandtab:
-
+#ifdef CHECK_MEMORY_LEAKS // SUMO-internal change
 /*
  * Copyright (C) 2004-2008 Wu Yongwei <adah at users dot sourceforge dot net>
  *
@@ -177,3 +177,7 @@ public:
 static __debug_new_counter __debug_new_count;
 
 #endif // _DEBUG_NEW_H
+
+
+#endif
+
--- sumo/src/foreign/nvwa/fast_mutex.h
+++ sumo/src/foreign/nvwa/fast_mutex.h
@@ -1,6 +1,6 @@
 // -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 // vim:tabstop=4:shiftwidth=4:expandtab:
-
+#ifdef CHECK_MEMORY_LEAKS // SUMO-internal change
 /*
  * Copyright (C) 2004-2008 Wu Yongwei <adah at users dot sourceforge dot net>
  *
@@ -309,3 +309,7 @@ private:
 };
 
 #endif // _FAST_MUTEX_H
+
+
+#endif
+
