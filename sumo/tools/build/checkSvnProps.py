91b1594bdbaa9f1650ec142e4632b066038a01f9
Michael Behrisch <oss@behrisch.de>
2010-08-30 11:42:27 +0000

    prop checking moved to xml, svn props adapted, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9017 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/checkSvnProps.py
+++ sumo/tools/build/checkSvnProps.py
@@ -5,61 +5,66 @@
 @date    2010
 @version $Id: apply_astyle.py 8385 2010-03-05 20:51:30Z behrisch $
 
-Checks svn property settings for all files in src, tests and tools.
+Checks svn property settings for all files.
 
 Copyright (C) 2010 DLR/TS, Germany
 All rights reserved
 """
 
-import os, subprocess, sys
+import os, subprocess, sys, xml.sax
+
+_SOURCE_EXT = [".h", ".cpp", ".py", ".pl", ".java", ".am"]
+_TESTDATA_EXT = [".xml", ".complex", ".dfrouter", ".duarouter", ".jtrrouter", ".netconvert", ".netgen", ".od2trips", ".polyconvert", ".sumo", ".meso", ".tools", ".traci", ".activitygen"]
+_VS_EXT = [".vsprops", ".sln", ".vcproj"]
+_KEYWORDS = "HeadURL Id LastChangedBy LastChangedDate LastChangedRevision"
+
+class PropertyReader(xml.sax.handler.ContentHandler):
+    """Reads a network, storing the edge geometries, lane numbers and max. speeds"""
+
+    def __init__(self, doFix):
+        self._fix = doFix
+        self._file = None
+        self._property = None
+        self._value = ""
+
+    def startElement(self, name, attrs):
+        if name == 'target':
+            self._file = attrs['path']
+        if name == 'property':
+            self._property = attrs['name']
+
+    def characters(self, content):
+        if self._property:
+            self._value += content
+
+
+    def endElement(self, name):
+        if name == 'property':
+            ext = os.path.splitext(self._file)[1]
+            if ext in _SOURCE_EXT or ext in _TESTDATA_EXT:
+                if self._property == "svn:eol-style" and self._value != "LF":
+                    print self._file, self._property, self._value
+                    if self._fix:#it might be necessary to call: sed -i 's/\r$//'
+                        subprocess.call(["svn", "ps", "svn:eol-style", "LF", self._file])
+                if self._property == "svn:executable" and ext not in [".py", ".pl"]:
+                    print self._file, self._property, self._value
+                    if self._fix:
+                        subprocess.call(["svn", "pd", "svn:executable", self._file])
+            if ext in _SOURCE_EXT:
+                if self._property == "svn:keywords" and self._value != _KEYWORDS:
+                    print self._file, self._property, self._value
+                    if self._fix:
+                        subprocess.call(["svn", "ps", "svn:keywords", _KEYWORDS, self._file])
+            if ext in _VS_EXT:
+                if self._property == "svn:eol-style" and self._value != "CRLF":
+                    print self._file, self._property, self._value
+                    if self._fix:
+                        subprocess.call(["svn", "ps", "svn:eol-style", "CRLF", self._file])
+            self._value = ""
+            self._property = None
+
 
 doFix = len(sys.argv) == 2 and sys.argv[1] in ["-f" , "--fix"]
-srcRoot = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../src/"))
-testsRoot = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../tests/"))
-toolsRoot = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../tools/"))
-unittestSrcRoot = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../unittest/src/"))
-for rootDir in [srcRoot, testsRoot, toolsRoot, unittestSrcRoot]:
-    for root, dirs, files in os.walk(rootDir):
-        for name in files:
-            ext = os.path.splitext(name)[1]
-            fullName = os.path.join(root, name)
-            if ext in [".h", ".cpp", ".py", ".pl", ".java"]:
-                output = subprocess.Popen(["svn", "pl", "-v", fullName], stdout=subprocess.PIPE).communicate()[0]
-                for line in output.splitlines():
-                    l = line.split()
-                    if len(l) > 2 and l[0] == "svn:eol-style" and l[2] != "LF":
-                        print fullName, line
-                        if doFix:#it might be necessary to call: sed -i 's/\r$//'
-                            subprocess.call(["svn", "ps", "svn:eol-style", "LF", fullName])
-                    if len(l) > 2 and l[0] == "svn:keywords" and sorted(l[2:]) != ["HeadURL", "Id", "LastChangedBy",
-                                                                    "LastChangedDate", "LastChangedRevision"]:
-                        print fullName, line
-                        if doFix:
-                            subprocess.call(["svn", "ps", "svn:keywords", "HeadURL Id LastChangedBy LastChangedDate LastChangedRevision", fullName])
-                    if l and l[0] == "svn:executable" and ext in [".h", ".cpp", ".java"]:
-                        print fullName, line
-                        if doFix:
-                            subprocess.call(["svn", "pd", "svn:executable", fullName])
-            for ignoreDir in ['.svn', 'foreign']:
-                if ignoreDir in dirs:
-                    dirs.remove(ignoreDir)
-
-for root, dirs, files in os.walk(testsRoot):
-    for name in files:
-        ext = os.path.splitext(name)[1]
-        fullName = os.path.join(root, name)
-        if ext in [".xml", ".complex", ".dfrouter", ".duarouter", ".jtrrouter", ".netconvert", ".netgen", ".od2trips", ".polyconvert", ".sumo", ".meso", ".tools", ".traci", ".activitygen"]:
-            output = subprocess.Popen(["svn", "pl", "-v", fullName], stdout=subprocess.PIPE).communicate()[0]
-            for line in output.splitlines():
-                l = line.split()
-                if len(l) > 2 and l[0] == "svn:eol-style" and l[2] != "LF":
-                    print fullName, line
-                    if doFix:
-                        subprocess.call(["svn", "ps", "svn:eol-style", "LF", fullName])
-                if l and l[0] == "svn:executable":
-                    print fullName, line
-                    if doFix:
-                        subprocess.call(["svn", "pd", "svn:executable", fullName])
-        for ignoreDir in ['.svn', 'foreign']:
-            if ignoreDir in dirs:
-                dirs.remove(ignoreDir)
+root = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "../../"))
+output = subprocess.Popen(["svn", "pl", "-v", "-R", "--xml", root], stdout=subprocess.PIPE).communicate()[0]
+xml.sax.parseString(output, PropertyReader(doFix))
