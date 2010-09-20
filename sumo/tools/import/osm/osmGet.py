9220c1f43c1ccb39f97b9dea0620ac4d3b1ca820
Michael Behrisch <oss@behrisch.de>
2010-09-20 09:03:58 +0000

    improved osm getter, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9098 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/import/osm/osmGet.py
+++ sumo/tools/import/osm/osmGet.py
@@ -11,32 +11,69 @@ Copyright (C) 2009 DLR/TS, Germany
 All rights reserved
 """
 
-import os,sys
+import os, sys, httplib, StringIO, gzip, optparse
 
-if len(sys.argv)<3:
-	print >> sys.stderr, "Call: osmGet.py <PREFIX> <BOUNDING_BOX> <TILES_NUMBER>"
-	sys.exit()
+def readCompressed(response, query, filename):
+    conn.request("POST", "/api/interpreter", """<osm-script timeout="180" element-limit="20000000">
+    <union>
+       %s
+       <recurse type="node-relation" into="rels"/>
+       <recurse type="node-way"/>
+       <recurse type="way-relation"/>
+    </union>
+    <union>
+       <item/>
+       <recurse type="way-node"/>
+    </union>
+    <print mode="body"/>
+    </osm-script>""" % query)
+    response = conn.getresponse()
+    print response.status, response.reason
+    if response.status == 200:
+        stream = StringIO.StringIO(response.read())
+        decompressor = gzip.GzipFile(fileobj=stream)
+        out = open(os.path.join(os.path.dirname(sys.argv[0]), filename), "w")
+        out.write(decompressor.read())
+        out.close()
 
-prefix = sys.argv[1]#"bs"
-box = sys.argv[2]#"10.38,52.22,10.74,52.34"
-num = int(sys.argv[3])#20
+optParser = optparse.OptionParser()
+optParser.add_option("-p", "--prefix", default="osm", help="for output file")
+optParser.add_option("-b", "--bbox", help="bounding box to retrieve in geo coordinates south,west,north,east")
+optParser.add_option("-o", "--oldapi", action="store_true",
+                     default=False, help="use old API for retrieval")
+optParser.add_option("-t", "--tiles", type="int", default=1, help="number of tiles when using old api")
+optParser.add_option("-a", "--area", type="int", help="area id to retrieve")
+(options, args) = optParser.parse_args()
 
-boxV = box.split(",")
-bb = float(boxV[0])
-ee = float(boxV[2])
-
-if bb>ee or boxV[1]>boxV[3]:
-	print >> sys.stderr, "Box minimum is larger than box maximum."
-	sys.exit()
-
-
-b = bb
-for i in range(0,num):
-	e = b + (ee-bb) / float(num)
-	cbox = str(b) + "," + boxV[1] + "," + str(e) + "," + boxV[3]
-	call = "wget.exe http://api.openstreetmap.org/api/0.6/map?bbox=" + cbox + " -O " + prefix + str(i) + "_" + str(num) + ".osm.xml"
-	print call
-	os.system(call)
-	b = e
-
-	
+if not options.bbox and not options.area:
+    optParser.error("At least one of 'bbox' and 'area' has to be set.")
+if options.oldapi and options.area:
+    optParser.error("Only the new API supports 'area'.")
+if options.bbox:
+    south,west,north,east = [float(v) for v in options.bbox.split(',')]
+    if south > north or west > east:
+        optParser.error("Invalid geocoordinates in bbox.")
+if options.oldapi:
+    num = options.tiles
+    b = west
+    conn = httplib.HTTPConnection("api.openstreetmap.org")
+    for i in range(num):
+        e = b + (east-west) / float(num)
+        req = "/api/0.6/map?bbox=%s,%s,%s,%s" % (b, south, e, north)
+        conn.request("GET", req)
+        r = conn.getresponse()
+        print req, r.status, r.reason
+        out = open(os.path.join(os.path.dirname(sys.argv[0]), "%s%s_%s.osm.xml" % (options.prefix, i, num)), "w")
+        out.write(r.read())
+        out.close()
+        b = e
+    conn.close()
+else:
+    conn = httplib.HTTPConnection("78.46.81.38")
+    if options.area:
+        if options.area < 3600000000:
+            options.area += 3600000000
+        readCompressed(conn, '<area-query ref="%s"/>' % options.area, options.prefix + "_city.osm.xml")
+    if options.bbox:
+        readCompressed(conn, '<bbox-query n="%s" s="%s" w="%s" e="%s"/>' % (north, south, west, east), options.prefix + "_bbox.osm.xml")
+    conn.close()
