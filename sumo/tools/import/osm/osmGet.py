849bbe05dec7730321c0eff5dc43cfb169711ad9
Jakob Erdmann <jakob.erdmann@dlr.de>
2010-10-05 13:53:22 +0000

    simplified reuse of the osmGet script
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9190 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/import/osm/osmGet.py
+++ sumo/tools/import/osm/osmGet.py
@@ -13,7 +13,7 @@ All rights reserved
 
 import os, sys, httplib, StringIO, gzip, optparse
 
-def readCompressed(response, query, filename):
+def readCompressed(conn, query, filename):
     conn.request("POST", "/api/interpreter", """<osm-script timeout="180" element-limit="20000000">
     <union>
        %s
@@ -39,41 +39,46 @@ def readCompressed(response, query, filename):
 optParser = optparse.OptionParser()
 optParser.add_option("-p", "--prefix", default="osm", help="for output file")
 optParser.add_option("-b", "--bbox", help="bounding box to retrieve in geo coordinates south,west,north,east")
-optParser.add_option("-o", "--oldapi", action="store_true",
-                     default=False, help="use old API for retrieval")
+optParser.add_option("-o", "--oldapi", action="store_true", default=False, help="use old API for retrieval")
 optParser.add_option("-t", "--tiles", type="int", default=1, help="number of tiles when using old api")
 optParser.add_option("-a", "--area", type="int", help="area id to retrieve")
-(options, args) = optParser.parse_args()
 
-if not options.bbox and not options.area:
-    optParser.error("At least one of 'bbox' and 'area' has to be set.")
-if options.oldapi and options.area:
-    optParser.error("Only the new API supports 'area'.")
-if options.bbox:
-    south,west,north,east = [float(v) for v in options.bbox.split(',')]
-    if south > north or west > east:
-        optParser.error("Invalid geocoordinates in bbox.")
-if options.oldapi:
-    num = options.tiles
-    b = west
-    conn = httplib.HTTPConnection("api.openstreetmap.org")
-    for i in range(num):
-        e = b + (east-west) / float(num)
-        req = "/api/0.6/map?bbox=%s,%s,%s,%s" % (b, south, e, north)
-        conn.request("GET", req)
-        r = conn.getresponse()
-        print req, r.status, r.reason
-        out = open(os.path.join(os.path.dirname(sys.argv[0]), "%s%s_%s.osm.xml" % (options.prefix, i, num)), "w")
-        out.write(r.read())
-        out.close()
-        b = e
-    conn.close()
-else:
-    conn = httplib.HTTPConnection("78.46.81.38")
-    if options.area:
-        if options.area < 3600000000:
-            options.area += 3600000000
-        readCompressed(conn, '<area-query ref="%s"/>' % options.area, options.prefix + "_city.osm.xml")
+def get(args=None):
+    (options, args) = optParser.parse_args(args=args)
+
+    if not options.bbox and not options.area:
+        optParser.error("At least one of 'bbox' and 'area' has to be set.")
+    if options.oldapi and options.area:
+        optParser.error("Only the new API supports 'area'.")
     if options.bbox:
-        readCompressed(conn, '<bbox-query n="%s" s="%s" w="%s" e="%s"/>' % (north, south, west, east), options.prefix + "_bbox.osm.xml")
-    conn.close()
+        south,west,north,east = [float(v) for v in options.bbox.split(',')]
+        if south > north or west > east:
+            optParser.error("Invalid geocoordinates in bbox.")
+
+    if options.oldapi:
+        num = options.tiles
+        b = west
+        conn = httplib.HTTPConnection("api.openstreetmap.org")
+        for i in range(num):
+            e = b + (east-west) / float(num)
+            req = "/api/0.6/map?bbox=%s,%s,%s,%s" % (b, south, e, north)
+            conn.request("GET", req)
+            r = conn.getresponse()
+            print req, r.status, r.reason
+            out = open(os.path.join(os.path.dirname(sys.argv[0]), "%s%s_%s.osm.xml" % (options.prefix, i, num)), "w")
+            out.write(r.read())
+            out.close()
+            b = e
+        conn.close()
+    else:
+        conn = httplib.HTTPConnection("78.46.81.38")
+        if options.area:
+            if options.area < 3600000000:
+                options.area += 3600000000
+            readCompressed(conn, '<area-query ref="%s"/>' % options.area, options.prefix + "_city.osm.xml")
+        if options.bbox:
+            readCompressed(conn, '<bbox-query n="%s" s="%s" w="%s" e="%s"/>' % (north, south, west, east), options.prefix + "_bbox.osm.xml")
+        conn.close()
+
+if __name__ == "__main__":
+    get()
