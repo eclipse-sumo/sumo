43b59c699bcd394ac9ee4af19e6744b85b28a969
Michael Behrisch <oss@behrisch.de>
2010-11-18 13:59:13 +0000

    reworking interface to mesosim, refs #12, #123
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9381 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/microsim/MSBaseVehicle.cpp
@@ -0,0 +1,224 @@
+/****************************************************************************/
+/// @file    MSBaseVehicle.cpp
+/// @author  Michael Behrisch
+/// @date    Mon, 8 Nov 2010
+/// @version $Id$
+///
+// A base class for vehicle implementations
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2010 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <iostream>
+#include <cassert>
+#include <utils/common/StdDefs.h>
+#include <utils/common/MsgHandler.h>
+#include "MSVehicleType.h"
+#include "MSEdge.h"
+#include "MSMoveReminder.h"
+#include <microsim/devices/MSDevice_Vehroutes.h>
+#include <microsim/devices/MSDevice_Tripinfo.h>
+#include <microsim/devices/MSDevice_Routing.h>
+#include <microsim/devices/MSDevice_HBEFA.h>
+#include "MSBaseVehicle.h"
+
+#ifdef CHECK_MEMORY_LEAKS
+#include <foreign/nvwa/debug_new.h>
+#endif // CHECK_MEMORY_LEAKS
+
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+MSBaseVehicle::MSBaseVehicle(SUMOVehicleParameter* pars, const MSRoute* route, const MSVehicleType* type) throw() :
+        myParameter(pars),
+        myRoute(route),
+        myCurrEdge(route->begin()),
+        myMoveReminders(0),
+        myIndividualMaxSpeed(0.0),
+        myHasIndividualMaxSpeed(false),
+        myReferenceSpeed(-1.0),
+        myType(type),
+        myDeparture(-1),
+        myNumberReroutes(0) {
+    // init devices
+    MSDevice_Vehroutes::buildVehicleDevices(*this, myDevices);
+    MSDevice_Tripinfo::buildVehicleDevices(*this, myDevices);
+    MSDevice_Routing::buildVehicleDevices(*this, myDevices);
+    MSDevice_HBEFA::buildVehicleDevices(*this, myDevices);
+    for (std::vector< MSDevice* >::iterator dev=myDevices.begin(); dev != myDevices.end(); ++dev) {
+        myMoveReminders.push_back(std::make_pair(*dev, 0.));
+    }
+    myRoute->addReference();
+}
+
+MSBaseVehicle::~MSBaseVehicle() throw() {
+    myRoute->release();
+    delete myParameter;
+    for (std::vector< MSDevice* >::iterator dev=myDevices.begin(); dev != myDevices.end(); ++dev) {
+        delete(*dev);
+    }
+}
+
+
+const std::string &
+MSBaseVehicle::getID() const throw() {
+    return myParameter->id;
+}
+
+
+const SUMOVehicleParameter &
+MSBaseVehicle::getParameter() const throw() {
+    return *myParameter;
+}
+
+
+const MSRoute &
+MSBaseVehicle::getRoute() const throw() {
+    return *myRoute;
+}
+
+
+const MSVehicleType &
+MSBaseVehicle::getVehicleType() const throw() {
+    return *myType;
+}
+
+
+SUMOReal
+MSBaseVehicle::getMaxSpeed() const {
+    if (myHasIndividualMaxSpeed) {
+        return myIndividualMaxSpeed;
+    }
+    return myType->getMaxSpeed();
+}
+
+
+SUMOReal
+MSBaseVehicle::adaptMaxSpeed(SUMOReal referenceSpeed) {
+    if (myType->hasSpeedDeviation() && referenceSpeed != myReferenceSpeed) {
+        myHasIndividualMaxSpeed = true;
+        myIndividualMaxSpeed = myType->getMaxSpeedWithDeviation(referenceSpeed);
+        myReferenceSpeed = referenceSpeed;
+    }
+    if (myHasIndividualMaxSpeed)
+        return myIndividualMaxSpeed;
+    return MIN2(myType->getMaxSpeed(), referenceSpeed);
+}
+
+
+const MSEdge*
+MSBaseVehicle::succEdge(unsigned int nSuccs) const throw() {
+    if (myCurrEdge + nSuccs < myRoute->end()) {
+        return *(myCurrEdge + nSuccs);
+    } else {
+        return 0;
+    }
+}
+
+
+const MSEdge * const
+MSBaseVehicle::getEdge() const {
+    return *myCurrEdge;
+}
+
+
+void
+MSBaseVehicle::reroute(SUMOTime t, SUMOAbstractRouter<MSEdge, SUMOVehicle> &router, bool withTaz) throw() {
+    // check whether to reroute
+    std::vector<const MSEdge*> edges;
+    if (withTaz && MSEdge::dictionary(myParameter->fromTaz+"-source") && MSEdge::dictionary(myParameter->toTaz+"-sink")) {
+        router.compute(MSEdge::dictionary(myParameter->fromTaz+"-source"), MSEdge::dictionary(myParameter->toTaz+"-sink"), this, t, edges);
+        if (edges.size() >= 2) {
+            edges.erase(edges.begin());
+            edges.pop_back();
+        }
+    } else {
+        router.compute(*myCurrEdge, myRoute->getLastEdge(), this, t, edges);
+    }
+    if (edges.empty()) {
+        WRITE_WARNING("No route for vehicle '" + getID() + "' found.");
+        return;
+    }
+    replaceRouteEdges(edges, withTaz);
+}
+
+
+bool
+MSBaseVehicle::replaceRouteEdges(const MSEdgeVector &edges, bool onInit) throw() {
+    // build a new id, first
+    std::string id = getID();
+    if (id[0]!='!') {
+        id = "!" + id;
+    }
+    if (myRoute->getID().find("!var#")!=std::string::npos) {
+        id = myRoute->getID().substr(0, myRoute->getID().rfind("!var#")+4) + toString(getNumberReroutes() + 1);
+    } else {
+        id = id + "!var#1";
+    }
+    MSRoute *newRoute = new MSRoute(id, edges, 0, myRoute->getColor(), myRoute->getStops());
+    if (!MSRoute::dictionary(id, newRoute)) {
+        delete newRoute;
+        return false;
+    }
+    if (!replaceRoute(newRoute, onInit)) {
+        newRoute->addReference();
+        newRoute->release();
+        return false;
+    }
+    return true;
+}
+
+
+SUMOReal
+MSBaseVehicle::getPreDawdleAcceleration() const throw() {
+    return 0;
+}
+
+
+void
+MSBaseVehicle::onTryEmit() throw() {
+    for (MoveReminderCont::iterator rem=myMoveReminders.begin(); rem!=myMoveReminders.end(); ++rem) {
+        rem->first->onTryEmit();
+    }
+}
+
+
+SUMOReal
+MSBaseVehicle::getSegmentLength() const throw() {
+    return 0;
+}
+
+
+SUMOTime
+MSBaseVehicle::getDeparture() const throw() {
+    return myDeparture;
+}
+
+
+unsigned int
+MSBaseVehicle::getNumberReroutes() const throw() {
+    return myNumberReroutes;
+}
+
+
+/****************************************************************************/
+
---
+++ sumo/src/microsim/MSBaseVehicle.h
@@ -0,0 +1,214 @@
+/****************************************************************************/
+/// @file    MSBaseVehicle.h
+/// @author  Michael Behrisch
+/// @date    Mon, 8 Nov 2010
+/// @version $Id$
+///
+// A base class for vehicle implementations
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2010 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef MSBaseVehicle_h
+#define MSBaseVehicle_h
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <utils/common/SUMOVehicle.h>
+#include <utils/common/StdDefs.h>
+#include "MSRoute.h"
+
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+class SUMOVehicleParameter;
+class MSVehicleType;
+class MSMoveReminder;
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class MSBaseVehicle
+ * @brief A vehicle from the mesoscopic point of view
+ */
+class MSBaseVehicle : public SUMOVehicle {
+public:
+    /** @brief Constructor
+     */
+    MSBaseVehicle(SUMOVehicleParameter* pars, const MSRoute* route, const MSVehicleType* type) throw();
+
+
+    /// @brief Destructor
+    virtual ~MSBaseVehicle() throw();
+
+
+    /// Returns the name of the vehicle
+    const std::string &getID() const throw();
+
+    /** @brief Returns the vehicle's parameter (including departure definition)
+     *
+     * @return The vehicle's parameter
+     */
+    const SUMOVehicleParameter &getParameter() const throw();
+
+
+    /** @brief Returns the current route
+     * @return The route the vehicle uses
+     */
+    const MSRoute &getRoute() const throw();
+
+
+    /** @brief Returns the vehicle's type definition
+     * @return The vehicle's type definition
+     */
+    const MSVehicleType &getVehicleType() const throw();
+
+
+    /** @brief Returns the current maximum speed
+     * @return The vehicle's maximum speed
+     */
+    SUMOReal getMaxSpeed() const;
+
+
+    /** @brief Adapts the current maximum speed using the distribution given in the type
+     * @return The vehicle's new maximum speed
+     */
+    SUMOReal adaptMaxSpeed(SUMOReal referenceSpeed);
+
+
+    /** @brief Returns the nSuccs'th successor of edge the vehicle is currently at
+     *
+     * If the rest of the route (counted from the current edge) than nSuccs,
+     *  0 is returned.
+     * @param[in] nSuccs The number of edge to look forward
+     * @return The nSuccs'th following edge in the vehicle's route
+     */
+    const MSEdge* succEdge(unsigned int nSuccs) const throw();
+
+    /** @brief Returns the edge the vehicle is currently at
+     *
+     * @return The current edge in the vehicle's route
+     */
+    const MSEdge * const getEdge() const;
+
+
+    /** @brief Performs a rerouting using the given router
+     *
+     * Tries to find a new route between the current edge and the destination edge, first.
+     * Tries to replace the current route by the new one using replaceRoute.
+     *
+     * @param[in] t The time for which the route is computed
+     * @param[in] router The router to use
+     * @see replaceRoute
+     */
+    void reroute(SUMOTime t, SUMOAbstractRouter<MSEdge, SUMOVehicle> &router, bool withTaz=false) throw();
+
+
+    /** @brief Replaces the current route by the given edges
+     *
+     * It is possible that the new route is not accepted, if a) it does not
+     *  contain the vehicle's current edge, or b) something fails on insertion
+     *  into the routes container (see in-line comments).
+     *
+     * @param[in] edges The new list of edges to pass
+     * @param[in] simTime The time at which the route was replaced
+     * @return Whether the new route was accepted
+     */
+    bool replaceRouteEdges(const MSEdgeVector &edges, bool onInit=false) throw();
+
+
+    virtual SUMOReal getPreDawdleAcceleration() const throw();
+
+    /** @brief Called when the vehicle tries to get into the network
+     *
+     * Calls the appropriate device function, needed for rerouting.
+     */
+    virtual void onTryEmit() throw();
+
+
+    virtual SUMOReal getSegmentLength() const throw();
+
+
+    SUMOTime getDeparture() const throw();
+
+
+    unsigned int getNumberReroutes() const throw();
+
+
+    /** @brief Returns this vehicle's devices
+     * @return This vehicle's devices
+     */
+    const std::vector<MSDevice*> &getDevices() const {
+        return myDevices;
+    }
+
+protected:
+    /// @brief This Vehicle's parameter.
+    const SUMOVehicleParameter* myParameter;
+
+    /// @brief This Vehicle's route.
+    const MSRoute* myRoute;
+
+    /// @brief This Vehicle's type.
+    const MSVehicleType *myType;
+
+    /// @brief Iterator to current route-edge
+    MSRouteIterator myCurrEdge;
+
+    /// An individual speed for an vehicle that is used (iff set) instead of
+    /// the maximal speed of the vehicle class.
+    /// NOTE: This is just a little workaround for allowing an external
+    ///       influence on the actual speed
+    SUMOReal myIndividualMaxSpeed;
+
+    /// is true if there has an individual speed been set
+    bool myHasIndividualMaxSpeed;
+
+    /// the speed which served as reference when calculating the individual maxspeed
+    SUMOReal myReferenceSpeed;
+
+    /// @name Move reminder structures
+    /// @{
+
+    /// @brief Definition of a move reminder container
+    typedef std::vector< std::pair<MSMoveReminder*, SUMOReal> > MoveReminderCont;
+
+    /// @brief Current lane's move reminder
+    MoveReminderCont myMoveReminders;
+    /// @}
+
+    /// @brief The devices this vehicle has
+    std::vector<MSDevice*> myDevices;
+
+    /// @brief The real departure time
+    SUMOTime myDeparture;
+
+    /// @brief The number of reroutings
+    unsigned int myNumberReroutes;
+
+};
+
+#endif
+
+/****************************************************************************/
