2c7868f82b9613a6f7414d013e74966f7dddb751
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-01-03 13:17:20 +0000

    setting env vars re #412
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9519 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/bin/start-command-line.bat
+++ sumo/bin/start-command-line.bat
@@ -2,4 +2,4 @@
 rem change the following line to set a new default dir:
 set default_dir=%windir%\..
 
-cmd /K "set PATH=%PATH%;%CD% && cd /d %default_dir% && echo info: added location of sumo executables to the search path && echo. && echo use the 'cd /d' command to change directory && echo example usage: && echo cd /d c:\foo\bar 
+cmd /K "set PATH=%PATH%;%CD% && set SUMO_BINDIR=%CD% && set SUMO_HOME=%CD%\.. && cd /d %default_dir% && echo info: added location of sumo executables to the search path && echo info: variables SUMO_HOME, SUMO_BINDIR are set && echo. && echo use the 'cd /d' command to change directory && echo example usage: && echo cd /d c:\foo\bar 
