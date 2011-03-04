0d3be8ebf37fc7391f901b7b032725af9e8a3273
Michael Behrisch <oss@behrisch.de>
2011-03-04 09:49:58 +0000

    new traci api in python, refs #364
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9854 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/traci/__init__.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+@file    __init__.py
+@author  Michael.Behrisch@dlr.de, Lena Kalleske
+@date    2008-10-09
+@version $Id: traciControl.py 9525 2011-01-04 21:22:52Z behrisch $
+
+Python implementation of the TraCI interface.
+
+Copyright (C) 2008-2011 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import socket, time, struct
+
+RESULTS = {0x00: "OK", 0x01: "Not implemented", 0xFF: "Error"}
+
+class FatalTraCIError:
+    def __init__(self, desc):
+        self._desc = desc
+
+class Message:
+    string = ""
+    queue = []
+
+class Storage:
+    def __init__(self, content):
+        self._content = content
+        self._pos = 0
+
+    def read(self, format):
+        oldPos = self._pos
+        self._pos += struct.calcsize(format)
+        return struct.unpack(format, self._content[oldPos:self._pos])
+
+    def readInt(self):
+        return self.read("!i")[0]
+
+    def readFloat(self):
+        return self.read("!f")[0]
+
+    def readDouble(self):
+        return self.read("!d")[0]
+
+    def readLength(self):
+        length = self.read("!B")[0]
+        if length > 0:
+            return length
+        return self.read("!i")[0]
+
+    def readString(self):
+        length = self.read("!i")[0]
+        return self.read("!%ss" % length)[0]
+
+    def readStringList(self):
+        n = self.read("!i")[0]
+        list = []
+        for i in range(n):
+            list.append(self.readString())
+        return list
+
+    def ready(self):
+        return self._pos < len(self._content) 
+
+
+import constants, vehicle
+_socket = None
+_message = Message()
+
+def _recvExact():
+    global _socket
+    try:
+        result = ""
+        while len(result) < 4:
+            t = _socket.recv(4 - len(result))
+            if not t:
+                return None
+            result += t
+        length = struct.unpack("!i", result)[0] - 4
+        result = ""
+        while len(result) < length:
+            t = _socket.recv(length - len(result))
+            if not t:
+                return None
+            result += t
+        return Storage(result)
+    except socket.error:
+        return None
+
+def _sendExact():
+    global _socket
+    length = struct.pack("!i", len(_message.string)+4)
+    _socket.send(length)
+    _socket.send(_message.string)
+    _message.string = ""
+    result = _recvExact()
+    if not result:
+        _socket.close()
+        _socket = None
+        raise FatalTraCIError("connection closed by SUMO")
+    for command in _message.queue:
+        prefix = result.read("!BBB")
+        err = result.readString()
+        if prefix[2] or err:
+            print prefix, RESULTS[prefix[2]], err
+        elif prefix[1] != command:
+            print "Error! Received answer %s for command %s." % (prefix[1], command)
+        elif prefix[1] == constants.CMD_STOP:
+            length = result.read("!B")[0] - 1
+            result.read("!%sx" % length)
+    _message.queue = []
+    return result
+
+def _beginMessage(cmdID, varID, objID, length=0):
+    _message.queue.append(cmdID)
+    length += 1+1+1+4+len(objID)
+    if length<=255:
+        _message.string += struct.pack("!BBBi", length, cmdID, varID, len(objID)) + objID
+    else:
+        _message.string += struct.pack("!BiBBi", 0, length+4, cmdID, varID, len(objID)) + objID
+
+def _sendReadOneStringCmd(cmdID, varID, objID):
+    _beginMessage(cmdID, varID, objID)
+    result = _sendExact()
+    length = result.readLength()
+    response, retVarID = result.read("!BB")
+    objectID = result.readString()
+    if response - cmdID != 16 or retVarID != varID or objectID != objID:
+        print "Error! Received answer %s,%s,%s for command %s,%s,%s."\
+              % (response, retVarID, objectID, cmdID, varID, objID)
+    result.read("!B")     # Return type of the variable
+    return result
+
+def _subscribe(cmdID, begin, end, objID, varIDs):
+    _message.queue.append(cmdID)
+    length = 1+1+4+4+4+len(objID)+1+len(varIDs)
+    if length<=255:
+        _message.string += struct.pack("!B", length)
+    else:
+        _message.string += struct.pack("!Bi", 0, length+4)
+    _message.string += struct.pack("!Biii", cmdID, begin, end, len(objID)) + objID
+    _message.string += struct.pack("!B", len(varIDs))
+    for v in varIDs:
+        _message.string += struct.pack("!B", v)
+    result = _sendExact()
+    length = result.readLength()
+    response = result.read("!B")[0]
+    objectID = result.readString()
+    numVars = result.read("!B")[0]
+    if response - cmdID != 16 or objectID != objID or numVars != len(varIDs):
+        print "Error! Received answer %s,%s,%s for command %s,%s,%s."\
+              % (response, objectID, numVars, cmdID, objID, len(varIDs))
+    for v in varIDs:
+        varID = result.read("!B")[0]
+        status, varType = result.read("!BB")
+        if status:
+            print "Error!", result.readString()
+    while result.ready():
+        print result.read("!B")[0]
+    return result
+
+def init(port, numRetries=10):
+    global _socket
+    _socket = socket.socket()
+    for wait in range(numRetries):
+        try:
+            _socket.connect(("localhost", port))
+            _socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            break
+        except socket.error:
+            time.sleep(wait)
+
+def simulationStep(step):
+    """
+    Make simulation step and simulate up to "step" second in sim time.
+    """
+    _message.queue.append(constants.CMD_SIMSTEP2)
+    _message.string += struct.pack("!BBi", 1+1+4, constants.CMD_SIMSTEP2, step)
+    result = _sendExact()
+    numSubs = result.read("!i")[0]
+    subscriptions = []
+    while result.ready():
+        print result.read("!B")[0]
+    return subscriptions
+
+def close():
+    global _socket
+    if _socket:
+        _message.queue.append(constants.CMD_CLOSE)
+        _message.string += struct.pack("!BB", 1+1, constants.CMD_CLOSE)
+        _sendExact()
+        _socket.close()
+        _socket = None
---
+++ sumo/tools/traci/vehicle.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+@file    vehicle.py
+@author  Michael.Behrisch@dlr.de, Lena Kalleske
+@date    2008-10-09
+@version $Id: traciControl.py 9525 2011-01-04 21:22:52Z behrisch $
+
+Python implementation of the TraCI interface.
+
+Copyright (C) 2008-2011 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import socket, time, struct, traci
+import traci.constants as tc
+
+RETURN_VALUE_FUNC = {tc.ID_LIST:        traci.Storage.readStringList,
+                     tc.VAR_SPEED:      traci.Storage.readFloat,
+                     tc.VAR_POSITION:   lambda(result): result.read("!ff"),
+                     tc.VAR_ANGLE:      traci.Storage.readFloat,
+                     tc.VAR_ROAD_ID:    traci.Storage.readString,
+                     tc.VAR_LANE_ID:    traci.Storage.readString,
+                     tc.VAR_LANE_INDEX: traci.Storage.readInt,
+                     tc.VAR_TYPE:       traci.Storage.readString,
+                     tc.VAR_ROUTE_ID:   traci.Storage.readString,
+                     tc.VAR_LANEPOSITION:      traci.Storage.readFloat,
+                     tc.VAR_COLOR:      lambda(result): result.read("!BBBB")}
+
+def _getUniversal(varID, vehID):
+    result = traci._sendReadOneStringCmd(tc.CMD_GET_VEHICLE_VARIABLE, varID, vehID)
+    return RETURN_VALUE_FUNC[varID](result)
+
+def getIDList():
+    return _getUniversal(tc.ID_LIST, "")
+
+def getSpeed(vehID):
+    return _getUniversal(tc.VAR_SPEED, vehID)
+
+def getPosition(vehID):
+    """
+    Returns the position of the named vehicle within the last step [m,m]
+    """
+    return _getUniversal(tc.VAR_POSITION, vehID)
+
+def getAngle(vehID):
+    return _getUniversal(tc.VAR_ANGLE, vehID)
+
+def getRoadID(vehID):
+    return _getUniversal(tc.VAR_ROAD_ID, vehID)
+
+def getLaneID(vehID):
+    return _getUniversal(tc.VAR_LANE_ID, vehID)
+
+def getLaneIndex(vehID):
+    return _getUniversal(tc.VAR_LANE_INDEX, vehID)
+
+def getTypeID(vehID):
+    return _getUniversal(tc.VAR_TYPE, vehID)
+
+def getRouteID(vehID):
+    return _getUniversal(tc.VAR_ROUTE_ID, vehID)
+
+def getLanePosition(vehID):
+    return _getUniversal(tc.VAR_LANEPOSITION, vehID)
+
+def getColor(vehID):
+    return _getUniversal(tc.VAR_COLOR, vehID)
+
+def getBestLanes(vehID):
+    result = traci._sendReadOneStringCmd(tc.CMD_GET_VEHICLE_VARIABLE, tc.VAR_BEST_LANES, vehID)
+    result.read("!iB")
+    nbLanes = result.read("!i")[0] # Length
+    lanes = []
+    for i in range(nbLanes):
+        result.read("!B")
+        laneID = result.readString()
+        length = result.read("!Bf")[1]
+        occupation = result.read("!Bf")[1]
+        offset = result.read("!Bb")[1]
+        allowsContinuation = result.read("!BB")[1]
+        nextLanesNo = result.read("!Bi")[1]
+        nextLanes = []
+        for j in range(nextLanesNo):
+            nextLanes.append(result.readString())
+        lanes.append( [laneID, length, occupation, offset, allowsContinuation, nextLanes ] )
+    return lanes
+
+
+def subscribe(vehID, varIDs=(tc.VAR_POSITION,), begin=0, end=2**31-1):
+    traci._subscribe(tc.CMD_SUBSCRIBE_VEHICLE_VARIABLE, begin, end, vehID, varIDs)
+
+def resetSubscriptionResults():
+    subscriptionResults = {}
+
+def addSubscriptionResult(vehID, result):
+    if vehID in subscriptionResults:
+        subscriptionResults[vehID].update(result)
+    else:
+        subscriptionResults[vehID] = result
+
+def getSubscriptionResults(vehID):
+    return subscriptionResults.get(vehID, None)
+
+
+def setMaxSpeed(vehID, speed):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_SETMAXSPEED, vehID, 1+4)
+    _message.string += struct.pack("!Bf", tc.TYPE_FLOAT, speed)
+    _sendExact()
+
+def setSpeed(vehID, speed):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.VAR_SPEED, vehID, 1+8)
+    _message.string += struct.pack("!Bd", tc.TYPE_DOUBLE, speed)
+    _sendExact()
+
+def setLanePosition(vehID, position):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.VAR_LANEPOSITION, vehID, 1+8)
+    _message.string += struct.pack("!Bd", tc.TYPE_DOUBLE, position)
+    _sendExact()
+
+def setStop(vehID, edgeID, pos, laneIndex, duration):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_STOP, vehID, 1+4+1+4+len(edgeID)+1+4+1+1+1+4)
+    _message.string += struct.pack("!Bi", tc.TYPE_COMPOUND, 4)
+    _message.string += struct.pack("!B", tc.TYPE_STRING)
+    _message.string += struct.pack("!i", len(edgeID)) + edgeID
+    _message.string += struct.pack("!BfBBBi", tc.TYPE_FLOAT, pos, tc.TYPE_BYTE, laneIndex, tc.TYPE_INTEGER, duration)
+    _sendExact()
+
+def changeLane(vehID, laneIndex, duration):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_CHANGELANE, vehID, 1+4+1+1+1+4)
+    _message.string += struct.pack("!BiBBBi", tc.TYPE_COMPOUND, 2, tc.TYPE_BYTE, laneIndex, tc.TYPE_INTEGER, duration)
+    _sendExact()
+
+def slowDown(vehID, speed, duration):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_SLOWDOWN, vehID, 1+4+1+4+1+4)
+    _message.string += struct.pack("!BiBfBi", tc.TYPE_COMPOUND, 2, tc.TYPE_FLOAT, speed, tc.TYPE_INTEGER, duration)
+    _sendExact()
+
+def changeTarget(vehID, edgeID):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_CHANGETARGET, vehID, 1+4+len(edgeID))
+    _message.string += struct.pack("!B", tc.TYPE_STRING)
+    _message.string += struct.pack("!i", len(edgeID)) + edgeID
+    _sendExact()
+
+def setRoute(vehID, edgeList):
+    """
+    changes the vehicle route to given edges list.
+    The first edge in the list has to be the one that the vehicle is at at the moment.
+    
+    example usasge:
+    setRoute('1', ['1', '2', '4', '6', '7'])
+    
+    this changes route for vehicle id 1 to edges 1-2-4-6-7
+    """
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.VAR_ROUTE, vehID,
+                        1+4+sum(map(len, edgeList))+4*len(edgeList))
+    _message.string += struct.pack("!Bi", tc.TYPE_STRINGLIST, len(edgeList))
+    for edge in edgeList:
+        _message.string += struct.pack("!i", len(edge)) + edge
+    _sendExact()
+
+def moveTo(vehID, laneID, pos):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.VAR_MOVE_TO, vehID, 1+4+1+4+len(laneID)+8)
+    _message.string += struct.pack("!Bi", tc.TYPE_COMPOUND, 2)
+    _message.string += struct.pack("!Bi", tc.TYPE_STRING, len(laneID)) + laneID
+    _message.string += struct.pack("!Bd", tc.TYPE_DOUBLE, pos)
+    _sendExact()
+
+def reroute(vehID):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_REROUTE_TRAVELTIME, vehID, 1+4)
+    _message.string += struct.pack("!Bi", tc.TYPE_COMPOUND, 0)
+    _sendExact()
+
+def setColor(vehID, color):
+    traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.VAR_COLOR, vehID, 1+1+1+1+1)
+    _message.string += struct.pack("!BBBBB", tc.TYPE_COLOR, int(color[0]), int(color[1]), int(color[2]), int(color[3]))
+    _sendExact()
