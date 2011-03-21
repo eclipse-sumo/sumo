8624b8c2782eededc956136a83c68078c973b77b
Michael Behrisch <oss@behrisch.de>
2011-03-21 13:58:06 +0000

    adding gui test, refs #364
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@9936 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/gui/runner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, subprocess, sys, struct, time
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "..", "..", "..", "..", "..", "tools"))
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "..", "..", "..", "..", "..", "tools", "lib"))
+import traci, testUtil
+
+sumoBinary = testUtil.checkBinary('sumo-gui')
+
+sumoProcess = subprocess.Popen("%s -c sumo.sumo.cfg" % (sumoBinary), shell=True, stdout=sys.stdout)
+traci.init(8813)
+for step in range(3):
+    print "step", step
+    traci.simulationStep(step)
+time.sleep(1) # give the gui a chance to draw itself
+print "views", traci.gui.getIDList()
+viewID = traci.gui.DEFAULT_VIEW
+print "examining", viewID
+print "zoom", traci.gui.getZoom(viewID)
+print "offset", traci.gui.getOffset(viewID)
+print "schema", traci.gui.getSchema(viewID)
+print "boundary", traci.gui.getBoundary(viewID)
+print "bgColor", traci.gui.getBackgroundColor(viewID)
+print "netSize", traci.gui.getNetSize(viewID)
+
+traci.gui.subscribe(viewID)
+print traci.gui.getSubscriptionResults(viewID)
+for step in range(3,6):
+    print "step", step
+    traci.simulationStep(step)
+    print traci.gui.getSubscriptionResults(viewID)
+traci.close()
