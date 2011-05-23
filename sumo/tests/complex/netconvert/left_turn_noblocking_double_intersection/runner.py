995b2348a7a4c172251852d87007d43f9546194c
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-05-23 11:10:52 +0000

    added failing tests for blocked intersections refs #384. tests are added under netconvert because we will try to fix the problem with patching the built row-logic
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10426 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/netconvert/left_turn_noblocking_double_intersection/runner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# call jtrrouter twice and check that the output differs
+import sys,os,subprocess,random
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', "tools", "lib"))
+from testUtil import checkBinary
+
+net_output = 'net.net.xml'
+trips_output = 'trips.log'
+
+netconvert = checkBinary('netconvert')
+assert(netconvert)
+sumo = checkBinary('sumo')
+assert(sumo)
+
+args_netc = [netconvert,
+        '--node-files', 'input_nodes.nod.xml',
+        '--edge-files', 'input_edges.edg.xml',
+        '--output', net_output,
+        '--offset.disable-normalization']
+
+args_sumo = [sumo,
+        '--net-file', net_output,
+        '--route-files', 'input_routes.rou.xml',
+        '--end', '50',
+        '--no-step-log',
+        '--no-duration-log',
+        '--tripinfo-output', trips_output]
+
+subprocess.call(args_netc)
+subprocess.call(args_sumo)
+
+# vehicles should have completed their trips
+complete = False
+for line in open(trips_output):
+    if 'veh0' in line:
+        complete = True
+
+if complete:
+    print('test passed. no blocking occured')
+else:
+    print('test failed. vehicles were blocked')
