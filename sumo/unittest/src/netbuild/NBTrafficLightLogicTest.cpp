762890e919d03d0de179b5245222743fdb4419f8
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-05-24 12:41:07 +0000

    fixing build after refactoring refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10434 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/unittest/src/netbuild/NBTrafficLightLogicTest.cpp
+++ sumo/unittest/src/netbuild/NBTrafficLightLogicTest.cpp
@@ -8,17 +8,6 @@ using namespace std;
 Tests the class NBTrafficLightLogic 
 */
 
-/* Test the method 'writeXML'*/
-TEST(NBTrafficLightLogic, test_method_writeXML) {	
-	
-	NBTrafficLightLogic *edge = new NBTrafficLightLogic("1","11",2);
-	edge->addStep(3,"a");
-	OutputDeviceMock *mock = new OutputDeviceMock();
-	edge->writeXML(*mock);
-	string expect = "   <tl-logic id=\"1\" type=\"static\" programID=\"11\" offset=\"0\">\n      <phase duration=\"3\" state=\"a\"/>\n   </tl-logic>\n";
-	EXPECT_EQ(expect, mock->getString());
-}
-
 /* Test the method 'getDuration'*/
 TEST(NBTrafficLightLogic, test_method_getDuration) {	
 	NBTrafficLightLogic *edge = new NBTrafficLightLogic("1","11",2);
