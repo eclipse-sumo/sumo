995721bc167db02afcb59a897776a66441b39b0e
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-05-27 09:03:04 +0000

    added netconvert roundtrip test. fails because one link state is different
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10465 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/netconvert/osm_roundtrip/runner.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# import osm network 
+# then import the built network again and check for idempotency
+
+
+import sys,os,subprocess
+import difflib
+import StringIO
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', "tools", "lib"))
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', "tools", "import", "osm"))
+from testUtil import checkBinary
+
+TEXTTEST_LIB = "/home/erdm_ja/texttest-3.19/source/lib/default"
+sys.path.append(TEXTTEST_LIB)
+import fpdiff 
+
+osm_input = 'osm.xml'
+net_output = 'net.net.xml'
+net_output2 = 'net.out.net.xml'
+
+netconvert = checkBinary('netconvert')
+assert(netconvert)
+
+# filter header and projection clause
+def filter(lines):
+    skippedHeader = False
+    result = []
+    for l in lines:
+        if '<net>' in l:
+            skippedHeader = True
+        if not skippedHeader:
+            continue
+        if '<location' in l:
+            continue
+        if '<projection' in l:
+            continue
+        result.append(l)
+    return result
+
+
+args1 = [netconvert,
+        '--no-internal-links', 
+        '--osm-files',  osm_input, '--proj.utm',
+        '-R', '--ramps.guess', 
+        '--tls.guess', '--tls.join',
+        '--junctions.join',
+        '--output', net_output]
+
+args2 = [netconvert,
+        '--sumo-net-file', net_output,
+        '--no-internal-links', 
+        '--offset.disable-normalization',
+        '--output', net_output2]
+
+subprocess.call(args1)
+subprocess.call(args2)
+
+fromlines = filter(open(net_output, 'U').readlines())
+tolines = filter(open(net_output2, 'U').readlines())
+out = StringIO.StringIO()
+fpdiff.fpfilter(fromlines, tolines, out, 0.0201)
+out.seek(0)
+tolines = out.readlines()
+sys.stderr.writelines(difflib.unified_diff(fromlines, tolines))
