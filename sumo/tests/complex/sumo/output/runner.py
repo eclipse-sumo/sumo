ed9441860f83bf561a41415ba6b9165d6741138c
Michael Behrisch <oss@behrisch.de>
2011-06-17 21:38:27 +0000

    some test runner fallbacks, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10652 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/sumo/output/runner.py
+++ sumo/tests/complex/sumo/output/runner.py
@@ -3,6 +3,12 @@ import os,subprocess,sys
 from xml.sax import parse, handler
 
 class OutputHandler(handler.ContentHandler):
+    e1speed = -1
+    e2speed = -1
+    e3speed = -1
+    edgespeed = -1
+    lanespeed = -1
+
     def startElement(self, name, attrs):
         if name == "interval":
             if "nVehContrib" in attrs:
@@ -21,10 +27,8 @@ class OutputHandler(handler.ContentHandler):
 sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', 'bin', 'sumo'))
 subprocess.call([sumoBinary]+sys.argv[1:], shell=(os.name=="nt"), stdout=sys.stdout, stderr=sys.stderr)
 handler = OutputHandler()
-parse("e1detector.xml", handler)
-parse("e2detector.xml", handler)
-parse("e3detector.xml", handler)
-parse("meandataedge.xml", handler)
-parse("meandatalane.xml", handler)
+for f in ["e1detector.xml", "e2detector.xml", "e3detector.xml", "meandataedge.xml", "meandatalane.xml"]:
+    if os.path.exists(f):
+        parse(f, handler)
 
 print handler.e1speed, handler.e2speed, handler.e3speed, handler.edgespeed, handler.lanespeed
