1d2a92757d27d6fd1a26b1036c19913b21050270
Michael Behrisch <oss@behrisch.de>
2011-06-23 20:39:41 +0000

    improved sumolib, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10707 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/sumolib/output/__init__.py
+++ sumo/tools/sumolib/output/__init__.py
@@ -5,9 +5,9 @@
 @date    2011-06-23
 @version $Id: __init__.py 10125 2011-04-20 21:21:40Z behrisch $
 
-Python interface to SUMO especially for parsing xml input and output files.
+Python interface to SUMO especially for parsing output files.
 
 Copyright (C) 2011-2011 DLR (http://www.dlr.de/) and contributors
 All rights reserved
 """
-import net, poi, output
+import dump, inductionloop
