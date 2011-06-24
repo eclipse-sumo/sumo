15839bef4fdc053385f002252f1ad09af3a40e26
Michael Behrisch <oss@behrisch.de>
2011-06-24 19:13:34 +0000

    more output comparison , refs #254
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10717 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/sumo/output/runner.py
+++ sumo/tests/complex/sumo/output/runner.py
@@ -3,25 +3,29 @@ import os,subprocess,sys
 from xml.sax import parse, handler
 
 class OutputHandler(handler.ContentHandler):
-    e1speed = -1
-    e2speed = -1
-    e3speed = -1
-    edgespeed = -1
-    lanespeed = -1
+    e1speed = {}
+    e2speed = {}
+    e3speed = {}
+    edgespeed = {}
+    lanespeed = {}
+    intervals = set()
 
     def startElement(self, name, attrs):
         if name == "interval":
+            self.interval = (float(attrs["begin"]), float(attrs["end"]))
+            self.intervals.add(self.interval)
             if "nVehContrib" in attrs:
-                self.e1speed = attrs["speed"]
-            if "meanSpeed" in attrs:
-                if "nSamples" in attrs:
-                    self.e2speed = attrs["meanSpeed"]
-                else:
-                    self.e3speed = attrs["meanSpeed"]
+                self.e1speed[self.interval] = float(attrs["speed"])
+            if "meanSpeedWithin" in attrs:
+                self.e3speed[self.interval] = float(attrs["meanSpeedWithin"])
+                if self.e3speed[self.interval] == -1.:
+                    self.e3speed[self.interval] = float(attrs["meanSpeed"])
+            if "nSamples" in attrs:
+                self.e2speed[self.interval] = float(attrs["meanSpeed"])
         if name == "edge" and "id" in attrs and attrs["id"]=="3/1to2/1" and "speed" in attrs:
-            self.edgespeed = attrs["speed"]
+            self.edgespeed[self.interval] = float(attrs["speed"])
         if name == "lane" and "id" in attrs and attrs["id"]=="3/1to2/1_0" and "speed" in attrs:
-            self.lanespeed = attrs["speed"]
+            self.lanespeed[self.interval] = float(attrs["speed"])
 
 
 sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', 'bin', 'sumo'))
@@ -31,4 +35,9 @@ for f in ["e1detector.xml", "e2detector.xml", "e3detector.xml", "meandataedge.xm
     if os.path.exists(f):
         parse(f, handler)
 
-print handler.e1speed, handler.e2speed, handler.e3speed, handler.edgespeed, handler.lanespeed
+for i in sorted(handler.intervals):
+    vals = [handler.e1speed.get(i, -1.), handler.e2speed.get(i, -1.), handler.e3speed.get(i, -1.), handler.edgespeed.get(i, -1.), handler.lanespeed.get(i, -1.)]
+    for v in vals[:-1]:
+        if abs(v - vals[-1]) > 0.001:
+            print i, vals
+            break
