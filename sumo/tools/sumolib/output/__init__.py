5bf5469c13c022e825d830154789e598e5a355da
Michael Behrisch <oss@behrisch.de>
2011-06-25 06:35:18 +0000

    fixing svn props, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10721 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/sumolib/output/__init__.py
+++ sumo/tools/sumolib/output/__init__.py
@@ -3,7 +3,7 @@
 @file    __init__.py
 @author  Michael.Behrisch@dlr.de
 @date    2011-06-23
-@version $Id: __init__.py 10125 2011-04-20 21:21:40Z behrisch $
+@version $Id$
 
 Python interface to SUMO especially for parsing output files.
 
