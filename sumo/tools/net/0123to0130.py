43cdbc7c6933883f629ba7fe1688f2c25f4f976f
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-07-01 12:56:54 +0000

    net upgrade script now performs partial conversion instead of skipping when it encouters succlane=SUMO_NO_DESTINATION refs #480
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10802 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/net/0123to0130.py
+++ sumo/tools/net/0123to0130.py
@@ -22,7 +22,7 @@ a['lane'] = ( 'id', 'index', 'vclasses', 'allow', 'disallow', 'maxspeed', 'lengt
 a['junction'] = ( 'id', 'type', 'x', 'y', 'incLanes', 'intLanes', 'shape' )
 a['logicitem'] = ('response', 'foes', 'cont' )
 a['succlane'] = ('via', 'tl', 'linkno', 'dir', 'state' )
-a['connection'] = ('from', 'to', 'lane', 'via', 'tl', 'linkIndex', 'dir', 'state' )
+a['connection'] = ('from', 'to', 'lane', 'via', 'tl', 'linkIdx', 'dir', 'state' )
 a['row-logic'] = a['ROWLogic'] = ( 'id', 'requestSize' )
 a['tl-logic'] = a['tlLogic'] = ( 'id', 'type', 'programID', 'offset' )
 a['location'] = ( 'netOffset', 'convBoundary', 'origBoundary', 'projParameter' )
@@ -34,8 +34,8 @@ a['taz'] = ( 'id', 'shape', 'edges' )
 a['tazSink'] = ( 'id', 'weight' )
 a['tazSource'] = ( 'id', 'weight' )
 a['roundabout'] = ( 'nodes', 'dummy' )
-a['connection'] = ( 'from', 'to', 'lane', 'via', 'tl', 'linkIdx', 'dir', 'state' )
 a['request'] = ( 'index', 'response', 'foes', 'cont' )
+a['succ'] = ( 'edge', 'lane', 'junction')
 
 # attributes which are optional
 b = {}
@@ -52,10 +52,10 @@ SINGLE = ( 'roundabout', 'logicitem', 'phase', 'succlane', 'dsource', 'dsink', '
         'lane', 'timed_event', 'connection', 'request' )
 
 # remove these
-removed = ( 'lanes', 'logic', 'succ', 'row-logic', 'ROWLogic', 'logicitem')
+REMOVED = ( 'lanes', 'logic', 'succ', 'row-logic', 'ROWLogic', 'logicitem')
 
 # renamed elements
-renamed = {'tl-logic': 'tlLogic', 
+RENAMED_TAGS = {'tl-logic': 'tlLogic', 
         'row-logic': 'ROWLogic', 
         'district':'taz', 
         'dsource':'tazSource', 
@@ -84,7 +84,7 @@ def getBegin(file):
     return ""
 
 class NetConverter(handler.ContentHandler):
-    def __init__(self, outFileName, begin):
+    def __init__(self, outFileName, begin, has_no_destination):
         self._out = open(outFileName, "w")
         self._out.write(begin)
         self._tree = []
@@ -92,6 +92,7 @@ class NetConverter(handler.ContentHandler):
         self._am_parsing_rowlogic = None # parsing old element
         self._logicitems = defaultdict(list) # maps junction ids to items
         self._laneCount = 0
+        self._has_no_destination = has_no_destination
 
     def isSingle(self, name):
         if name in SINGLE:
@@ -99,6 +100,22 @@ class NetConverter(handler.ContentHandler):
         if name == "junction" and self._inner_junction:
             return True
         return False
+
+    def remove(self, name):
+        if name == "succ" and self._has_no_destination:
+            return False
+        if name in REMOVED:
+            return True
+        else:
+            return False
+
+    def rename(self, name):
+        if name == "succlane" and self._has_no_destination:
+            return False
+        if name in RENAMED_TAGS:
+            return True
+        else:
+            return False
 
     def checkWrite(self, what):
         self._out.write(what.encode('iso-8859-1'))
@@ -124,7 +141,7 @@ class NetConverter(handler.ContentHandler):
         if name == "edge":
             self._laneCount = 0
         # skip removed
-        if name in removed:
+        if self.remove(name):
             return
         # compress empty lines
         if self._content.find("\n\n")>=0:
@@ -132,18 +149,23 @@ class NetConverter(handler.ContentHandler):
         self._content = ""
         # open tag
         self.indent()
-        if name in renamed:
-            self.checkWrite("<" + renamed[name])
+        if self.rename(name):
+            self.checkWrite("<" + RENAMED_TAGS[name])
         else:
             self.checkWrite("<" + name)
         # special attribute handling
         if name == "succlane":
-            self.checkWrite(' from="%s"' % self._succ_from)
-            sepIndex = attrs['lane'].rindex('_')
-            toEdge = attrs['lane'][:sepIndex]
-            toIdx = attrs['lane'][sepIndex+1:]
-            self.checkWrite(' to="%s"' % toEdge)
-            self.checkWrite(' lane="%s:%s"' % (self._succ_fromIdx, toIdx))
+            if self._has_no_destination:
+                # keep original
+                self.checkWrite(' lane="%s"' % attrs["lane"])
+            else:
+                # upgrade
+                self.checkWrite(' from="%s"' % self._succ_from)
+                sepIndex = attrs['lane'].rindex('_')
+                toEdge = attrs['lane'][:sepIndex]
+                toIdx = attrs['lane'][sepIndex+1:]
+                self.checkWrite(' to="%s"' % toEdge)
+                self.checkWrite(' lane="%s:%s"' % (self._succ_fromIdx, toIdx))
         # write attributes
         if name in a:
             for key in a[name]:
@@ -183,7 +205,7 @@ class NetConverter(handler.ContentHandler):
         if name == "row-logic" or name == "ROWLogic":
             self._am_parsing_rowlogic = None
         # skip removed
-        if name in removed:
+        if self.remove(name):
             return
         # optionaly write closing tag
         self._tree.pop()
@@ -191,8 +213,8 @@ class NetConverter(handler.ContentHandler):
             self.checkWrite("\n")
         if not self.isSingle(name):
             self.indent()
-            if name in renamed:
-                self.checkWrite("</" + renamed[name] + ">")
+            if self.rename(name):
+                self.checkWrite("</" + RENAMED_TAGS[name] + ">")
             else:
                 self.checkWrite("</" + name + ">")
             if name!="net":
@@ -211,10 +233,11 @@ def changeFile(fname):
             (os.path.join('tools','net','0') in fname)):
         print "Skipping file (looks like intentionally deprecated input): " + fname
         return
+    has_no_destination = False
     if "SUMO_NO_DESTINATION" in open(fname).read():
-        print "Skipping file (cannot convert SUMO_NO_DESTINATION): " + fname
-        return
-    net = NetConverter(fname+".chg", getBegin(fname))
+        print "Partial conversion (cannot convert SUMO_NO_DESTINATION): " + fname
+        has_no_destination = True
+    net = NetConverter(fname+".chg", getBegin(fname), has_no_destination)
     parse(fname, net)
     if options.inplace:
         os.remove(fname)
