9cb28cddb7242c4bb65696d57995d0641ba8d052
Michael Behrisch <oss@behrisch.de>
2011-07-03 22:03:04 +0000

    fixing tests and testrunner, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10818 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/runTests.sh
+++ sumo/tests/runTests.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 #Bash script for the test run.
-#Accepts two parameter:
-#1. Directory for SUMO binaries (defaults to <SUMO_DIST>/bin)
-#2. Path to texttest.py executable (default is simply texttest.py)
+#sets environment variables respecting SUMO_BINDIR and starts texttest
 
 OLDDIR=$PWD
 cd `dirname $0`
 export TEXTTEST_HOME="$PWD"
-if test x"$1" = x; then
+if test x"$SUMO_BINDIR" = x; then
   cd ..
-  SUMO_BIN="$PWD/bin"
-else
-  SUMO_BIN="$1"
+  SUMO_BINDIR="$PWD/bin"
 fi
 cd $OLDDIR
-export ACTIVITYGEN_BINARY="$SUMO_BIN/activitygen"
-export DFROUTER_BINARY="$SUMO_BIN/dfrouter"
-export DUAROUTER_BINARY="$SUMO_BIN/duarouter"
-export JTRROUTER_BINARY="$SUMO_BIN/jtrrouter"
-export NETCONVERT_BINARY="$SUMO_BIN/netconvert"
-export NETGEN_BINARY="$SUMO_BIN/netgen"
-export OD2TRIPS_BINARY="$SUMO_BIN/od2trips"
-export POLYCONVERT_BINARY="$SUMO_BIN/polyconvert"
-export SUMO_BINARY="$SUMO_BIN/sumo"
-export GUISIM_BINARY="$SUMO_BIN/sumo-gui"
+export ACTIVITYGEN_BINARY="$SUMO_BINDIR/activitygen"
+export DFROUTER_BINARY="$SUMO_BINDIR/dfrouter"
+export DUAROUTER_BINARY="$SUMO_BINDIR/duarouter"
+export JTRROUTER_BINARY="$SUMO_BINDIR/jtrrouter"
+export NETCONVERT_BINARY="$SUMO_BINDIR/netconvert"
+export NETGEN_BINARY="$SUMO_BINDIR/netgen"
+export OD2TRIPS_BINARY="$SUMO_BINDIR/od2trips"
+export POLYCONVERT_BINARY="$SUMO_BINDIR/polyconvert"
+export SUMO_BINARY="$SUMO_BINDIR/sumo"
+export GUISIM_BINARY="$SUMO_BINDIR/sumo-gui"
 
-if test x"$2" = x; then
-  TEXTTEST=texttest.py
+if which texttest &> /dev/null; then
+  texttest "$@"
 else
-  TEXTTEST="$2"
+  texttest.py "$@"
 fi
-
-$TEXTTEST
-
