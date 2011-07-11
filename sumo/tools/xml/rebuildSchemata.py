043865854e2948b61b64f0069c75e39fcd468eb3
Michael Behrisch <oss@behrisch.de>
2011-07-11 10:13:59 +0000

    rebuilding config schemata
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10921 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/xml/rebuildSchemata.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+@file    rebuildSchemata.py
+@author  Michael.Behrisch@dlr.de
+@date    2011-07-11
+@version $Id: makeDoc.py 9525 2011-01-04 21:22:52Z behrisch $
+
+Let all SUMO binarie write the schema for their config
+
+Copyright (C) 2011-2011 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import os, sys, subprocess
+homeDir = os.environ.get("SUMO_HOME", os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+binDir = os.environ.get("SUMO_BINDIR", os.path.join(homeDir, "bin"))
+for exe in "activitygen dfrouter duarouter jtrrouter netconvert netgen od2trips polyconvert sumo".split():
+    subprocess.call([os.path.join(binDir, exe), "--save-schema", os.path.join(homeDir, "docs", "internet", "xsd" , exe+"Configuration.xsd")])
