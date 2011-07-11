0a15196c5f80761cb03aa23d5a4a8e02870c7c0f
Michael Behrisch <oss@behrisch.de>
2011-07-11 21:40:56 +0000

    fixing complex traci tests, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@10942 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/busySocket/runner.py
+++ sumo/tests/complex/traci/busySocket/runner.py
@@ -2,9 +2,9 @@
 
 import os, subprocess, sys, time, shutil
 
-sumoHome = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..'))
-sys.path.append(os.path.join(sumoHome, "tools", "traci"))
-import traciControl
+sumoHome = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+sys.path.append(os.path.join(sumoHome, "tools"))
+import traci
 
 if sys.argv[1]=="sumo":
     sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(sumoHome, 'bin', 'sumo'))
@@ -15,20 +15,19 @@ else:
     addOption = "-Q"
     secondConfig = "sumo_log.sumo.cfg"
 PORT = 8813
-DELTA_T = 1000
 
-sumoProcess = subprocess.Popen("%s -c sumo.sumo.cfg %s" % (sumoBinary, addOption), shell=True, stdout=sys.stdout, stderr=sys.stderr)
-traciControl.initTraCI(PORT)
-sumoProcess = subprocess.Popen("%s -c %s %s" % (sumoBinary, secondConfig, addOption), shell=True, stdout=sys.stdout, stderr=sys.stderr)
+subprocess.Popen("%s -c sumo.sumo.cfg %s" % (sumoBinary, addOption), shell=True, stdout=sys.stdout, stderr=sys.stderr)
+traci.init(PORT)
+subprocess.Popen("%s -c %s %s" % (sumoBinary, secondConfig, addOption), shell=True, stdout=sys.stdout, stderr=sys.stderr)
 time.sleep(10)
 step = 0
 while not step>100:
-    traciControl.cmdSimulationStep2(DELTA_T)
-    vehs = traciControl.cmdGetVehicleVariable_idList()
+    traci.simulationStep(step)
+    vehs = traci.vehicle.getIDList()
     if vehs.index("horiz")<0 or len(vehs)>1:
         print "Something is false"
     step += 1
-traciControl.cmdClose()
+traci.close()
 sys.stdout.flush()
 if os.path.exists("lastrun.stderr"):
     f = open("lastrun.stderr")
