4888cc0b3aedf01c0522243a9a6734d6d1a89bd6
Michael Behrisch <oss@behrisch.de>
2011-07-21 10:02:56 +0000

    moving obs src upload into a service
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11049 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/buildServiceUpload.sh
+++ sumo/tools/build/buildServiceUpload.sh
@@ -3,6 +3,5 @@ PREFIX=$1
 for f in $PREFIX/sumo/sumo-src-*.tar.gz; do
   if test -e $PREFIX/sumo/configure -a $f -nt $PREFIX/sumo/configure; then
     curl -n -X PUT -T $PREFIX/sumo/build/package/sumo.spec https://api.opensuse.org/source/home:behrisch/sumo_nightly/sumo.spec
-    curl -n -X PUT -T $PREFIX/sumo/`basename $f` https://api.opensuse.org/source/home:behrisch/sumo_nightly/`basename $f`
   fi
 done
