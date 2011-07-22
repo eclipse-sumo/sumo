8e22e5b464970ade8ed19f2d0046ac4066c6ed03
Michael Behrisch <oss@behrisch.de>
2011-07-22 20:24:09 +0000

    moving b50 test, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11066 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/sumo/b50/runner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os,subprocess,sys,time,shutil
+sumoHome = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+if "SUMO_HOME" in os.environ:
+    sumoHome = os.environ["SUMO_HOME"]
+sys.path.append(os.path.join(sumoHome, "tools"))
+import sumolib
+
+sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(sumoHome, 'bin', 'sumo'))
+netconvertBinary = os.environ.get("NETCONVERT_BINARY", os.path.join(sumoHome, 'bin', 'netconvert'))
+
+subprocess.call([netconvertBinary, "-n", "input_nodes.nod.xml", "-e", "input_edges.edg.xml"], stdout=sys.stdout, stderr=sys.stderr)
+subprocess.call([sumoBinary, "-c", "sumo.sumo.cfg", "-v"], stdout=sys.stdout, stderr=sys.stderr)
