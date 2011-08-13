e1f06653d71389ca4b04491399d60a4c0188cf9f
Michael Behrisch <oss@behrisch.de>
2011-08-13 05:42:32 +0000

    depart checker, refs #462
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11096 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/sumo/max_depart/runner.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os, subprocess, sys
+sumoHome = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+if "SUMO_HOME" in os.environ:
+    sumoHome = os.environ["SUMO_HOME"]
+sys.path.append(os.path.join(sumoHome, "tools"))
+import sumolib
+
+sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(sumoHome, 'bin', 'sumo'))
+
+def call(command):
+    retCode = subprocess.call(command, stdout=sys.stdout, stderr=sys.stderr)
+    if retCode != 0:
+        print >> sys.stderr, "Execution of %s failed." % command
+        sys.exit(retCode) 
+
+PERIOD = 10
+DEPARTSPEED = "max"
+
+fdo = open("results.csv", "w")
+for departPos in "random free random_free base pwagSimple pwagGeneric maxSpeedGap".split():
+    print ">>> Building the routes (for departPos %s)" % departPos
+    fd = open("input_routes.rou.xml", "w")
+    print >> fd, """<routes>
+    <flow id="vright" route="right" departPos="%s" departSpeed="%s" begin="0" end="10000" period="%s"/>
+    <flow id="vleft" route="left" departPos="%s" departSpeed="%s" begin="0" end="10000" period="%s"/>
+    <flow id="vhorizontal" route="horizontal" departPos="%s" departSpeed="%s" begin="0" end="10000" period="%s"/>
+</routes>""" % (3*(departPos, DEPARTSPEED, PERIOD))
+    fd.close()
+
+    print ">>> Simulating ((for departPos %s)" % departPos
+    call([sumoBinary, "-c", "sumo.sumo.cfg", "-v"])
+
+    dump = sumolib.output.dump.readDump("aggregated.xml", ["occupancy"])
+    print >> fdo, "%s;%s" % (departPos, dump.get("occupancy")[-1]["1fi"])
+    os.rename("aggregated.xml", departPos + "_aggregated.xml")
+fdo.close()
