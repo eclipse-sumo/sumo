83935c4400ed62fa89be9abf1f76ef1ef4bf49d6
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-08-29 05:54:15 +0000

    fixing test call after improved error checking. refs 82
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11178 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/unittest/src/netbuild/NBTrafficLightLogicTest.cpp
+++ sumo/unittest/src/netbuild/NBTrafficLightLogicTest.cpp
@@ -13,8 +13,8 @@ TEST(NBTrafficLightLogic, test_method_getDuration) {
 	NBTrafficLightLogic *edge = new NBTrafficLightLogic("1","11",2);
 	EXPECT_EQ(0, edge->getDuration());
 
-	edge->addStep(3,"ab");
-	edge->addStep(5,"cd");
+	edge->addStep(3,"gr");
+	edge->addStep(5,"rG");
 	EXPECT_EQ(8, edge->getDuration());
 	
 }
