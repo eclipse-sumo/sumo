8df2ec6c2e256bd70b46392a02bff344bb35b328
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2011-10-24 10:07:37 +0000

    added parsing links in headers
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11406 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/convertWikiContent.py
+++ sumo/tools/build/convertWikiContent.py
@@ -13,24 +13,7 @@ Copyright (C) 2010-2011 DLR (http://www.dlr.de/) and contributors
 All rights reserved
 """
 
-fd = open("content_user.txt")
-lines = fd.readlines()
-fd.close()
-
-level = 0
-c = "<ul>\n";
-hadHeader = False
-for l in lines:
-    if l[0]=='=':
-        if hadHeader:
-            c = c + "</ul>\n";
-        spc = ' ' * (level+1)
-        c = c + spc + "<li>" + l[1:-2] + "</li>\n";
-        hadHeader = True
-        level = 0
-        continue
-    if l[0].find('*')<0:
-        continue
+def parseLink(l):
     if l.find("[[")>=0:
         b = l.find("[")+2
         e = l.find("]", b)
@@ -54,7 +37,37 @@ for l in lines:
     else:
         text = l[l.find(" ")+1:]
         link = ""
-    
+    return text, link
+
+fd = open("content_user.txt")
+lines = fd.readlines()
+fd.close()
+
+level = 0
+c = "<ul>\n";
+hadHeader = False
+for l in lines:
+    if l[0]=='=':
+        text, link = parseLink(" " + l[1:-2])
+        if hadHeader:
+            c = c + "</ul>\n";
+        spc = ' ' * (level+1)
+        c = c + spc + "<li>";	
+        if link!="":
+            c = c + "<a href=\"" + link + "\">";	
+        c = c + text;
+        if link!="":
+            c = c + "</a>";
+        c = c + "</li>\n";
+
+
+
+        hadHeader = True
+        level = 0
+        continue
+    if l[0].find('*')<0:
+        continue
+    text, link = parseLink(l)
     nLevel = l.count('*')
     if level>nLevel:
         c = c + ("</ul>" * (level-nLevel))
