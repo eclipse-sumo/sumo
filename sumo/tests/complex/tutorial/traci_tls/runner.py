b2a3c629b162e3048211036889a41773edcc260c
Michael Behrisch <oss@behrisch.de>
2011-10-31 09:41:21 +0000

    setup for embedded python test, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11442 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/tutorial/traci_tls/runner.py
+++ sumo/tests/complex/tutorial/traci_tls/runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-@file    cross.py
+@file    runner.py
 @author  Lena Kalleske
 @date    2009-03-26
 @version $Id$
@@ -11,9 +11,9 @@ Copyright (C) 2009-2011 DLR/TS, Germany
 All rights reserved
 """
 
-import os, subprocess, sys, socket, time, struct, random
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', "tools"))
-sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(os.path.dirname(__file__), "..", "..", "..")), "tools"))
+import os, subprocess, sys, random
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'tools'))
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(os.path.dirname(__file__), '..', '..', '..')), 'tools'))
 from sumolib import checkBinary
 import traci
 
@@ -26,52 +26,50 @@ WEYELLOW = "ryry"
 
 PROGRAM = [WEYELLOW,WEYELLOW,WEYELLOW,NSGREEN,NSGREEN,NSGREEN,NSGREEN,NSGREEN,NSGREEN,NSGREEN,NSGREEN,NSYELLOW,NSYELLOW,WEGREEN]
 
-N = 9000
-
-
-pWE = 1./10
-pEW = 1./11
-pNS = 1./30
-
-routes = open("data/cross.rou.xml", "w")
-print >> routes, """<routes>
+if not traci.isEmbedded():
+    N = 9000
+    pWE = 1./10
+    pEW = 1./11
+    pNS = 1./30
+    routes = open("data/cross.rou.xml", "w")
+    print >> routes, """<routes>
     <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
     <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="17" minGap="3" maxSpeed="25" guiShape="bus"/>
 
     <route id="right" edges="51o 1i 2o 52i" />
     <route id="left" edges="52o 2i 1o 51i" />
-    <route id="down" edges="54o 4i 3o 53i" />
-"""
-lastVeh = 0
-vehNr = 0
-for i in range(N):
-    if random.uniform(0,1) < pWE:
-        print >> routes, '    <vehicle id="%i" type="typeWE" route="right" depart="%i" />' % (vehNr, i)
-        vehNr += 1
-        lastVeh = i
-    if random.uniform(0,1) < pEW:
-        print >> routes, '    <vehicle id="%i" type="typeWE" route="left" depart="%i" />' % (vehNr, i)
-        vehNr += 1
-        lastVeh = i
-    if random.uniform(0,1) < pNS:
-        print >> routes, '    <vehicle id="%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (vehNr, i)
-        vehNr += 1
-        lastVeh = i
+    <route id="down" edges="54o 4i 3o 53i" />"""
+    lastVeh = 0
+    vehNr = 0
+    for i in range(N):
+        if random.uniform(0,1) < pWE:
+            print >> routes, '    <vehicle id="%i" type="typeWE" route="right" depart="%i" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+        if random.uniform(0,1) < pEW:
+            print >> routes, '    <vehicle id="%i" type="typeWE" route="left" depart="%i" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+        if random.uniform(0,1) < pNS:
+            print >> routes, '    <vehicle id="%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+    print >> routes, "</routes>"
+    routes.close()
 
-print >> routes, "</routes>"
-routes.close()
-
-sumoBinary = checkBinary('sumo')
-sumoConfig = "data/cross.sumo.cfg"
-sumoProcess = subprocess.Popen("%s -c %s" % (sumoBinary, sumoConfig), shell=True, stdout=sys.stdout)
-
-
-traci.init(PORT)
+    sumoBinary = checkBinary('sumo')
+    sumoConfig = "data/cross.sumo.cfg"
+    if len(sys.argv) > 1:
+        retCode = subprocess.call("%s -c %s --python-script %s" % (sumoBinary, sumoConfig, __file__), shell=True, stdout=sys.stdout)
+        sys.exit(retCode)
+    else:
+        sumoProcess = subprocess.Popen("%s -c %s" % (sumoBinary, sumoConfig), shell=True, stdout=sys.stdout)
+        traci.init(PORT)
 
 programPointer = len(PROGRAM)-1
 step = 0
-while not (step > 10000):
-    traci.simulationStep(1000)
+while step == 0 or traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
     programPointer = min(programPointer+1, len(PROGRAM)-1)
     no = traci.inductionloop.getLastStepVehicleNumber("0")
     if no > 0:
