7244359fb71fc6f139902d180f2066aafcfe6634
Jakob Erdmann <jakob.erdmann@dlr.de>
2011-12-14 14:24:41 +0000

    added universal output parser to module output
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11609 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/sumolib/output/__init__.py
+++ sumo/tools/sumolib/output/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @file    __init__.py
-@author  Michael Behrisch
+@author  Michael Behrisch, Jakob Erdmann
 @date    2011-06-23
 @version $Id$
 
@@ -12,3 +12,46 @@ Copyright (C) 2011-2011 DLR (http://www.dlr.de/) and contributors
 All rights reserved
 """
 import dump, inductionloop
+
+from xml.dom import pulldom
+from collections import namedtuple
+from keyword import iskeyword
+
+
+def parse(xmlfile, element_name):
+    # parses the attributes of all nodes with element_name and returns a list of namedtuples
+    # @note the first node in xmlfile will determine the set of attributes 
+    # @note attribute names which are also python keywords will be prefixed with 'attr_' 
+    elementType = [] # mutable, will be [namedtuple]
+    xml_doc = pulldom.parse(xmlfile)
+    return [get_attrs(parsenode, elementType, element_name) for event, parsenode in xml_doc 
+            if event == pulldom.START_ELEMENT and parsenode.localName == element_name]
+
+def get_attrs(node, elementType, element_name):
+    if not elementType:
+        # why NamedNodeMap does not support iteration is a mystery
+        attrnames = [node.attributes.item(i).localName for i in range(node.attributes.length)]
+        attrnames = [prefix_keyword(a) for a in attrnames]
+        print attrnames
+        elementType.append(namedtuple(element_name, attrnames))
+    return elementType[0](*[node.getAttribute(a) for a in elementType[0]._fields])
+
+
+def prefix_keyword(name):
+    return 'attr_' + name if iskeyword(name) else name
+
+
+def sum(elements, attrname):
+    # for the given elements (as returned by method parse) compute the sum for attrname 
+    # attrname must be the name of a numerical attribute
+    return reduce(lambda x,y: x+y, [float(e.__getattribute__(attrname)) for e in elements])
+
+
+def average(elements, attrname):
+    # for the given elements (as returned by method parse) compute the average for attrname 
+    # attrname must be the name of a numerical attribute
+    if elements:
+        return sum(elements, attrname) / len(elements)
+    else:
+        raise "average of 0 elements is not defined"
+
