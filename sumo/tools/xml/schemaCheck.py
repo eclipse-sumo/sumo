183a589fa168bed116ad5725f2c43a8c7c261f65
Michael Behrisch <oss@behrisch.de>
2012-01-05 13:21:01 +0000

    yet another renaming leftover, refs #627
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11657 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/xml/schemaCheck.py
+++ sumo/tools/xml/schemaCheck.py
@@ -42,7 +42,7 @@ def validate(f):
         traceback.print_exc()
 
 def main(srcRoot, err):
-    toCheck = [ "*.edg.xml", "*.nod.xml", "*.con.xml", "*.typ.xml", "*.net.xml", "*.rou.xml", "*.cfg",
+    toCheck = [ "*.edg.xml", "*.nod.xml", "*.con.xml", "*.typ.xml", "*.net.xml", "*.rou.xml", "*.????cfg",
                 "net.netgen", "net.netconvert", "routes.duarouter", "alts.duarouter", "routes.jtrrouter" ]
     sax2count = "SAX2Count.exe"
     if 'XERCES_64' in os.environ:
