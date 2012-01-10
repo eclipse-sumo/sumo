6f3819fdccc6d2e3a8b474950e77fd8fc95684c2
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-01-10 15:41:04 +0000

    Changed misleading option help refs #2, #641
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11699 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netbuild/NBFrame.cpp
+++ sumo/src/netbuild/NBFrame.cpp
@@ -99,7 +99,7 @@ NBFrame::fillOptions(bool forNetgen) {
 
         oc.doRegister("geometry.remove", 'R', new Option_Bool(false));
         oc.addSynonyme("geometry.remove", "remove-geometry", true);
-        oc.addDescription("geometry.remove", "Processing", "Removes geometry information from edges");
+        oc.addDescription("geometry.remove", "Processing", "Replace nodes which only define edge geometry by geometry points (joins edges)");
 
         oc.doRegister("geometry.max-segment-length", new Option_Float(0));
         oc.addDescription("geometry.max-segment-length", "Processing", "splits geometry to restrict segment length");
