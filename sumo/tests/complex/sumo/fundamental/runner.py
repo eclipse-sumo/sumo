a54f7319c9a693884511e25ea6fedb9569c173e5
Michael Behrisch <oss@behrisch.de>
2012-01-18 16:20:48 +0000

    reducing timeouts, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11735 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/sumo/fundamental/runner.py
+++ sumo/tests/complex/sumo/fundamental/runner.py
@@ -10,30 +10,24 @@ import sumolib
 sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(sumoHome, 'bin', 'sumo'))
 netconvertBinary = os.environ.get("NETCONVERT_BINARY", os.path.join(sumoHome, 'bin', 'netconvert'))
 
-def call(command, log):
-    print >> log, "-" * 79
-    print >> log, command
-    log.flush()
-    print command
+def call(command):
+    sys.stdout.flush()
     retCode = subprocess.call(command, stdout=sys.stdout, stderr=sys.stderr)
     if retCode != 0:
-        print >> sys.stderr, "Execution of %s failed. Look into %s for details." % (command, log.name)
+        print >> sys.stderr, "Execution of %s failed." % (command,)
         sys.exit(retCode) 
 
 USE_ZERO_OFFSET = False
 
-DENSITYSTEP = .01
+DENSITYSTEP = .1 #this should be 0.01, it was made bigger to speed up the test
 VEHICLENUMBER = 1000
 VEHICLELENGTH = 5.0
 VEHICLEGAP = 2.5
 VEHICLESPACE = VEHICLELENGTH + VEHICLEGAP
-RELAXATIONSTEPS = 1000
-MEASURESTEPS = 3600
-log = open("logfile.txt", "w")
 
 density = DENSITYSTEP
 fdo = open("results.csv", "w")
-while density<=1.:
+while density <= 1.:
     print ">>> Building the network (at density %s)" % (density)
     length = VEHICLENUMBER *  VEHICLESPACE / density
     fd = open("input_edges.edg.xml", "w")
@@ -42,14 +36,14 @@ while density<=1.:
     print >> fd, '    <edge id="b" from="b" to="a" numLanes="1" speed="36." length=".1"/>'
     print >> fd, '</edges>'
     fd.close()
-    call([netconvertBinary, "-c", "netconvert.netccfg"], log)
+    call([netconvertBinary, "-c", "netconvert.netccfg"])
 
     print ">>> Building the routes (at density %s)" % (density)
     vehicleOffset = (length - .1) / float(VEHICLENUMBER)
     fd = open("input_routes.rou.xml", "w")
     print >> fd, '<routes>'
     print >> fd, '    <vType id="t1" accel="0.8" decel="4.5" sigma="0.5" length="%s" minGap="%s" maxSpeed="36"/>' % (VEHICLELENGTH, VEHICLEGAP)
-    print >> fd, '    <route id="r1" multi_ref="x" edges="a b a b a b a"/>'
+    print >> fd, '    <route id="r1" edges="a b a b a b a"/>'
     pos = 0
     for i in range(0, VEHICLENUMBER):
         print >> fd, '    <vehicle id="v.%s" depart="0" departSpeed="0" departPos="%s" route="r1" type="t1"/>' % (i, pos)
@@ -62,7 +56,7 @@ while density<=1.:
 
 
     print ">>> Simulating (at density %s)" % (density)
-    call([sumoBinary, "-c", "sumo.sumocfg", "-v"], log)
+    call([sumoBinary, "-c", "sumo.sumocfg", "-v"])
 
     backupdir = "backup/"
     if os.path.isdir(backupdir):
@@ -72,5 +66,5 @@ while density<=1.:
     il = sumolib.output.inductionloop.readInductLoop("detector.xml", ["flow"])
     print >> fdo, "%s;%s;%s" % (density, dump.get("occupancy")[-1]["a_0"], il.get("flow")[-1]["det"])
 
-    density = density + DENSITYSTEP
+    density += DENSITYSTEP
 fdo.close()
