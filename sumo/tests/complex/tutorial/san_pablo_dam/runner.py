1539aa06d89b0f209e2eabc5fc5d7f69bc93c51b
Michael Behrisch <oss@behrisch.de>
2012-01-19 14:26:22 +0000

    simplifying test, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11739 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/tutorial/san_pablo_dam/runner.py
+++ sumo/tests/complex/tutorial/san_pablo_dam/runner.py
@@ -13,12 +13,36 @@ Copyright (C) 2008-2012 DLR (http://www.dlr.de/) and contributors
 All rights reserved
 """
 
-
 import os,subprocess,sys
 import shutil
+import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', "tools"))
 sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(os.path.dirname(__file__), "..", "..", "..")), "tools"))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'data'))
 from sumolib import checkBinary
+
+def buildVSS(obs7file, obs8file, vss):
+    t7Times = []
+    for line in open(obs7file):
+        ll = line.split(':')
+        if ll:
+            t7Times.append(3600*int(ll[0]) + 60*int(ll[1]) + int(float(ll[2])))
+    t8Times = []
+    for line in open(obs8file):
+        ll = line.split(':')
+        if ll:
+            t8Times.append(3600*int(ll[0]) + 60*int(ll[1]) + int(float(ll[2])))
+    print 'data read: ', len(t7Times), len(t8Times)
+
+    fp = open(vss, 'w')
+    lObs8 = 337.5
+    print >> fp, '<vss>'
+    for i, t7 in enumerate(t7Times):
+        v = lObs8/(t8Times[i] - t7)
+        if i != len(t7Times)-1 and t7 != t7Times[i+1]:
+            print >> fp, '    <step time ="%s" speed="%s"/>' % (t7, v)
+    print >> fp, '</vss>'
+    fp.close()
 
 
 netconvertBinary = checkBinary('netconvert')
@@ -29,15 +53,12 @@ except: print "Missing 'spd-road.net.xml'"
 print ">> Netbuilding closed with status %s" % retcode
 sys.stdout.flush()
 # build/check vss
-os.chdir("data")
-exec(compile(open('make-vss-file.py').read(), 'make-vss-file.py', 'exec'))
-try: shutil.copy("spd-road.vss.xml", "../vss.xml")
-except: print "Missing 'spd-road.vss.xml'"
+buildVSS('data/obstimes_1_7.txt', 'data/obstimes_1_8.txt', 'data/spd-road.vss.xml')
+shutil.copy("data/spd-road.vss.xml", "vss.xml")
 sys.stdout.flush()
 # perform calibration
-exec(compile(open('calibrate.py').read(), 'calibrate.py', 'exec'))
-try: shutil.copy("all-the-results.txt", "../result.txt")
+os.chdir("data")
+import calibrate
+try: shutil.copy("all-the-results.txt", "../results.csv")
 except: print "Missing 'all-the-results.txt'"
 sys.stdout.flush()
-
-
