c455f4ceaee1d45331035867c99228123c48e02c
Michael Behrisch <oss@behrisch.de>
2012-01-26 13:38:07 +0000

    creating wiki table from config template, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11793 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/xml/configTemplateToWiki.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+@file    configTemplateToWiki.py
+@author  Michael Behrisch
+@version $Id: guisimrunner.py 11719 2012-01-14 21:45:05Z behrisch $
+
+Generate Wiki table from configuration template.
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2008-2012 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import sys
+from xml.sax import parse, handler
+
+class ConfigReader(handler.ContentHandler):
+
+    def __init__(self):
+        self._level = 0
+
+    def startElement(self, name, attrs):
+        if self._level == 1:
+            # subtopic
+            print """===%s===
+{| cellspacing="0" border="1" width="90%%" align="center"
+|-
+! style="background:#ddffdd;" valign="top" | Option
+! style="background:#ddffdd;" valign="top" | Default Value
+! style="background:#ddffdd;" valign="top" | Description""" % name.replace("_", " ").title()
+        if self._level == 2:
+            # entry
+            print '|-\n| valign="top" |',;
+            a = ""
+            for s in attrs.get('synonymes', '').split():
+                if len(s) == 1:
+                    a = s
+            if a != "":
+                print '{{Option|-%s {{DT_%s}}}}<br/>' % (a, attrs['type']),
+            print '{{Option|--%s {{DT_%s}}}}' % (name, attrs['type'])
+            print '| valign="top" |', attrs['value'];
+            print '| valign="top" |', attrs['help'];
+        self._level += 1
+
+    def endElement(self, name):
+        self._level -= 1
+        if self._level == 1:
+            # subtopic end
+            print "|-\n|}\n"
+
+if __name__ == "__main__":
+    parse(sys.argv[1], ConfigReader())
