2c2a4270e5b42b9fdfe582477a9b4200eba5b517
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-02-17 08:04:52 +0000

    fixing unittest build after refactoring refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11923 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/unittest/src/utils/geom/PositionVectorTest.cpp
+++ sumo/unittest/src/utils/geom/PositionVectorTest.cpp
@@ -2,6 +2,7 @@
 #include <utils/geom/PositionVector.h>
 #include <utils/common/UtilExceptions.h>
 #include <utils/common/MsgHandler.h>
+#include <utils/iodevices/OutputDevice.h>
 
 using namespace std;
 
@@ -189,9 +190,9 @@ TEST_F(PositionVectorTest, test_method_splitAt) {
     vec3.push_back(Position(0,0));
     vec3.push_back(Position(POSITION_EPS,0));
     // supress expected warning
-    MsgHandler::getWarningInstance()->report2cerr(false);
+    MsgHandler::getWarningInstance()->removeRetriever(&OutputDevice::getDevice("stderr"));
     result = vec3.splitAt(smallDiff);
-    MsgHandler::getWarningInstance()->report2cerr(true);
+    MsgHandler::getWarningInstance()->addRetriever(&OutputDevice::getDevice("stderr"));
 
 	EXPECT_FLOAT_EQ(2, result.first.size());
 	EXPECT_FLOAT_EQ(0, result.first[0].x());
