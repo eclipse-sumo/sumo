ef6fa2a8eea4de7bc9106fdf783c4207f569877b
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-03-02 08:55:16 +0000

    fixing netgen after adding netconvert-only options
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@11994 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netbuild/NBNetBuilder.cpp
+++ sumo/src/netbuild/NBNetBuilder.cpp
@@ -190,14 +190,16 @@ NBNetBuilder::compute(OptionsCont& oc,
     myEdgeCont.recomputeLaneShapes();
 
     // APPLY SPEED MODIFICATIONS
-    const SUMOReal speedOffset = oc.getFloat("speed.offset");
-    const SUMOReal speedFactor = oc.getFloat("speed.factor");
-    if (speedOffset != 0 || speedFactor != 1) {
-        PROGRESS_BEGIN_MESSAGE("Applying speed modifications");
-        for (std::map<std::string, NBEdge*>::const_iterator i = myEdgeCont.begin(); i != myEdgeCont.end(); ++i) {
-            (*i).second->setSpeed(-1, (*i).second->getSpeed() * speedFactor + speedOffset);
+    if (oc.exists("speed.offset")) {
+        const SUMOReal speedOffset = oc.getFloat("speed.offset");
+        const SUMOReal speedFactor = oc.getFloat("speed.factor");
+        if (speedOffset != 0 || speedFactor != 1) {
+            PROGRESS_BEGIN_MESSAGE("Applying speed modifications");
+            for (std::map<std::string, NBEdge*>::const_iterator i = myEdgeCont.begin(); i != myEdgeCont.end(); ++i) {
+                (*i).second->setSpeed(-1, (*i).second->getSpeed() * speedFactor + speedOffset);
+            }
+            PROGRESS_DONE_MESSAGE();
         }
-        PROGRESS_DONE_MESSAGE();
     }
 
     // GUESS TLS POSITIONS
