99de3de29228a13728f2f009dccc356b38bc8ef4
Yun-Pang Floetteroed <Yun-Pang.Floetteroed@dlr.de>
2012-03-14 08:30:58 +0000

    corrected the vehrouteReader and the netReader
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12094 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/routeChoices.py
+++ sumo/tools/assign/routeChoices.py
@@ -21,9 +21,6 @@ from optparse import OptionParser
 from xml.sax import saxutils, make_parser, handler
 from xml.sax import parse
 from numpy import *
-#from matplotlib import pyplot as plt
-from math import *
-
 
 class Vehicle:
     def __init__(self,label,depart, departlane, departpos, departspeed):
@@ -88,7 +85,7 @@ class netReader(handler.ContentHandler):
     def __init__(self, edgesList, edgesMap):
         self._edgesList = edgesList
         self._edgesMap = edgesMap
-        self._edgObj = None
+        self._edgeObj = None
         
     def startElement(self, name, attrs):
         if name == 'edge' and not attrs.has_key('function'):
@@ -96,13 +93,14 @@ class netReader(handler.ContentHandler):
                 self._edgeObj = Edge(attrs['id'])
                 self._edgesList.append(self._edgeObj)
                 self._edgesMap[attrs['id']] = self._edgeObj
-            if self._edgeObj and name == 'lane':
-                self._edgeObj.length = float(attrs['length'])
-                self._edgeObj.freespeed = float(attrs['speed'])  #todo when no data in dump. default for fuel consumption?
-                self._edgeObj.freetraveltime = self._edgeObj.length/self._edgeObj.speed
+        if self._edgeObj and name == 'lane':
+            self._edgeObj.length = float(attrs['length'])
+            self._edgeObj.freespeed = float(attrs['speed'])
+            self._edgeObj.freetraveltime = self._edgeObj.length/self._edgeObj.freespeed
+
     def endElement(self, name):
         if name == 'edge':
-            self._edgObj = None
+            self._edgeObj = None
                 
 class addweightsReader(handler.ContentHandler):
     def __init__(self, edgesList, edgesMap):
@@ -139,8 +137,7 @@ class addweightsReader(handler.ContentHandler):
             if attrs.has_key('CO2_abs'):
                 self._edgeObj.CO2_abs_default = float(attrs['CO2_abs'])
             if attrs.has_key('CO_abs'):
-                self._edgeObj.CO_abs_default = float(attrs['CO_abs'])                
-
+                self._edgeObj.CO_abs_default = float(attrs['CO_abs'])
 
 class routeReader(handler.ContentHandler):
     def __init__(self, vehList, vehMap):
@@ -185,9 +182,7 @@ class vehrouteReader(handler.ContentHandler):
 
     def startElement(self, name, attrs):
         if name == 'vehicle':
-            self._vehObj = Vehicle(attrs['id'], attrs['depart'], attrs['departLane'], attrs['departPos'], attrs['departSpeed'])
-            self._vehMap[attrs['id']] = self._vehObj
-            self._vehList.append(self._vehObj)
+            self._vehObj = self._vehMap[attrs['id']]
             
         if self._vehObj and name == 'routeDistribution':
              self._currentSelected = attrs['last']
@@ -204,11 +199,12 @@ class vehrouteReader(handler.ContentHandler):
             if not self._existed:
                 self._routObj = Route(attrs['edges'])
                 self._vehObj.routesList.append(self._routObj)
+
             if attrs.has_key('probability'):
                 self._routObj.ex_probability = float(attrs['probability'])
             if attrs.has_key('cost'):
                 self._routObj.ex_cost = float(attrs['cost'])
-            for e in attrs['edges'].split(' '):
+            for e in self._routObj.edges.split(' '):
                 eObj = self._edgesMap[e]
                 if self._ecoMeasure != 'fuel' and eObj.traveltime == 0.:
                     self._routObj.act_cost += eObj.freetraveltime
@@ -218,16 +214,32 @@ class vehrouteReader(handler.ContentHandler):
                     self._routObj.act_cost += eObj.fuel_perVeh_default
                 elif self._ecoMeasure == 'fuel' and eObj.fuel_perVeh > 0.:
                     self._routObj.act_cost += eObj.fuel_perVeh
+            if self._routObj.ex_cost == 0.:
+                self._routObj.ex_cost = self._routObj.act_cost
 
     def endElement(self, name):
         if name == 'vehicle':
             if len(self._vehObj.routesList) == 1:
                 self._vehObj.routesList[0].ex_probability = 1.
             for r in self._vehObj.routesList:
+                if r.act_cost == 0.:
+                    for e in r.edges.split(' '):
+                        eObj = self._edgesMap[e]
+                        if self._ecoMeasure != 'fuel' and eObj.traveltime == 0.:
+                            r.act_cost += eObj.freetraveltime
+                        elif self._ecoMeasure != 'fuel' and eObj.traveltime > 0.:
+                            r.act_cost += eObj.traveltime
+                        elif self._ecoMeasure == 'fuel' and eObj.fuel_perVeh == 0.:
+                            r.act_cost += eObj.fuel_perVeh_default
+                        elif self._ecoMeasure == 'fuel' and eObj.fuel_perVeh > 0.:
+                            r.act_cost += eObj.fuel_perVeh
+                if r.ex_cost == 0.:
+                    r.ex_cost = r.act_cost
                 if not r.ex_probability:
                     r.ex_probability = 1./float(len(self._vehObj.routesList))
-                    print 'new probability is calculated.'
+                    print 'new probability for route', r.label, 'for veh', self._vehObj.label
                     self._newroutesList.append(r)
+
             if len(self._newroutesList) > 0:
                 addProb = 0.
                 origProbSum  = 0.
@@ -239,7 +251,7 @@ class vehrouteReader(handler.ContentHandler):
                 for r in self._vehObj.routesList:
                     if r not in self._newroutesList:
                         r.ex_probability = r.ex_probability/origProbSum * (1. - addProb)
-                        
+
             for r1 in self._vehObj.routesList:     # todo: add "one used route to all routes"
                 for r2 in self._vehObj.routesList:
                     if r1.label != r2.label:
@@ -250,13 +262,13 @@ class vehrouteReader(handler.ContentHandler):
                 self._vehObj.routesList[0].act_probability = 1.
                 self._selected = 0
             else:
+                cumulatedProbs = 0.
                 for i, r in enumerate(self._vehObj.routesList):
-                    if i == 0 and r.act_probability >= randProb:
+                    cumulatedProbs += r.act_probability
+                    if cumulatedProbs >= randProb:
                         self._selected = i
                         break
-                    elif i > 0 and (self._vehObj.routesList[i-1].act_probability + self._vehObj.routesList[i].act_probability) >= randProb:
-                        self._selected = i
-                        break
+
             # generate the *.rou.xml
             self._foutrout.write('    <vehicle id="%s" depart="%.2f" departLane="%s" departPos="%s" departSpeed="%s">\n' 
                               % (self._vehObj.label, self._vehObj.depart, self._vehObj.departlane, self._vehObj.departpos, self._vehObj.departspeed))
@@ -332,8 +344,8 @@ class dumpsReader(handler.ContentHandler):
         if name == 'edge':
             self._edgeObj = None
 
-def resetEdges(edgesList,edgesMap):
-    for eid in edgesList:
+def resetEdges(edgesMap):
+    for eid in edgesMap:
         e = edgesMap[eid]
         e.traveltime = 0.
         e.CO_abs = 0.
@@ -359,20 +371,19 @@ def getRouteChoices(edgesMap, dumpfile, routeAltfile, netfile, addWeightsfile, a
     outputPath = os.path.abspath(routeAltfile)
     outputPath = os.path.dirname(outputPath)
     prefix = os.path.basename(routeAltfile)
-    prefix = prefix[:prefix.find('.')]
+    #prefix = prefix[:prefix.find('.')]
+    prefix = prefix[:-12]
     print 'outputPath:', outputPath
     print 'prefix:', prefix
     outputAltfile = os.path.join(outputPath, prefix + '.rou.galt.xml')
     outputRoufile = os.path.join(outputPath, prefix + '.grou.xml')
-    print outputAltfile
-    print outputRoufile
 
-    if step == 1:
+    if len(edgesMap) == 0:
         print 'parse network file'
         parse(netfile, netReader(edgesList, edgesMap))
         parse(addWeightsfile, addweightsReader(edgesList,edgesMap))
     else:
-        resetEdges(edgesList, edgesMap)
+        resetEdges(edgesMap)
         
     fout = open(outputAltfile, 'w')
     foutrout = open(outputRoufile, 'w')
@@ -395,89 +406,19 @@ def getRouteChoices(edgesMap, dumpfile, routeAltfile, netfile, addWeightsfile, a
     parse(routeAltfile, routeReader(vehList, vehMap))   # parse routeAltfile from SUMO
     print 'parse routeAltfile from externalGawron'
     ex_outputAltFile = prefix[:prefix.rfind('_')] + '_%03i' %(step-1) + '.rou.galt.xml'
-    ex_outputAltFile = os.path.join(outputPath,ex_outputAltFile)
     print 'ex_outputAltFile:', ex_outputAltFile
-    parse(ex_outputAltFile, vehrouteReader(vehList, vehMap, edgesMap,fout, foutrout,ecoMeasure,alpha, beta))
-    
+    parse(ex_outputAltFile, vehrouteReader(vehList,vehMap,edgesMap,fout,foutrout,ecoMeasure,alpha,beta))
     return outputRoufile, edgesMap
 
 def gawron(r1, r2, alpha, beta,selectedRouteEdges):
     a = alpha
-    #print 'run gawron'
     if r1.edges != selectedRouteEdges:
         r1.act_cost = beta * r1.act_cost + (1. - beta) * r1.ex_cost
     if r2.edges != selectedRouteEdges:
         r2.act_cost = beta * r2.act_cost + (1. - beta) * r2.ex_cost
-    
-    delta = (r2.act_cost - r1.act_cost)/(r2.act_cost + r1.act_cost)
 
+    delta = (r1.act_cost - r2.act_cost)/(r1.act_cost + r2.act_cost)
     g = math.exp(a*delta/(1-delta*delta))
 
     r1.act_probability = (r1.ex_probability*(r1.ex_probability + r2.ex_probability)*g)/(r1.ex_probability*g + r2.ex_probability)  # check together with Eva
     r2.act_probability = r1.ex_probability + r2.ex_probability - r1.act_probability
-    
-#Eva
-# gegeben: 3 Routen
-# fuel consumption
-
-#def get_costs(p, l):
-#    C = 1000
-#    v = 10
-#    F = 5000
-#    Vi = 30
-#    v0 = 16.67
-#    c = 1
-#    k = 0.5
-#    q = 1000
-
-#    Ti =  l/v*(1+(p*F/(2*C))*(p*F/(2*C)))
-#    return c*Ti*(1 + k*p*F/q + 0.5*math.pow(Vi/v0,3)/math.pow(1+k*p*F/q,2))
-
-#def gawron(p1, p2, c1, c2, l1, l2):
-#    a = 0.5
-#    b = 0.9
-#    delta = (c2-c1)/(c2 + c1)
-#    g = math.exp(a*delta/(1-delta*delta))
-#    tmp = p1
-#
-#    p1 = (p1*(p1 + p2)*g)/(p1*g + p2)
-#    p2 = tmp+p2-p1
-#    c1 = get_costs(p1, l1)
-#    c2 = b*get_costs(p2, l2) + (1-b)*c2
-#
-#    return p1, p2, c1, c2
-#
-#anz = 100
-#
-#l1 = 100
-#l2 = 150
-#l3 = 200
-#
-#c1 = get_costs(0,l1) # fuel consumuption
-#c2 = get_costs(0,l2) # fuel consumuption
-#c3 = get_costs(0,l3) # fuel consumuption
-#
-#p1 = 1
-#p2 = 0
-#p3 = 0
-#
-#for i in arange(anz):
-#
-#    if (i==1):
-#        p1 = 0.5
-#        p2 = 0.5
-#
-#    if (i==2):
-#        p1 = p1-(0.33*p1)
-#        p2 = 0.67-p1
-#        p3 = 0.33
-#
-#    p1, p2, c1, c2 = gawron (p1, p2, c1, c2, l1, l2)
-#    p1, p3, c1, c3 = gawron (p1, p3, c1, c3, l1, l3)
-#    # p2, p3, c2, c3 = gawron (p2, p3, c2, c3, l2, l3)
-#    plt.plot(i, p1, ls='', marker='o', mew=0, ms=2, color = 'b')
-#    plt.plot(i, p2, ls='', marker='o', mew=0, ms=2, color = 'r')
-#    plt.plot(i, p3, ls='', marker='o', mew=0, ms=2, color = 'g')
-
-#plt.draw()
-#plt.show()
