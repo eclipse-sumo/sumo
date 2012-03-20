bb4eb3746209e91b16be1c3a64f6c879a51f7087
Michael Behrisch <oss@behrisch.de>
2012-03-20 23:03:58 +0000

    python compressed route parse, refs #661
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12149 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/xml/binary2plain.py
+++ sumo/tools/xml/binary2plain.py
@@ -67,6 +67,35 @@ def readIntListList(content):
             list[-1].append(readInt(content))
     return list
 
+def readRoute(content):
+    n = readInt(content)
+    list = []
+    first = readInt(content)
+    if first < 0:
+        bits = -first
+        numFields = 8 * 4 / bits
+        mask = (1 << bits) - 1
+        edge = readInt(content)
+        list.append(edges[edge])
+        n -= 1
+        field = numFields
+        while n > 0:
+            if field == numFields:
+                data = readInt(content)
+                field = 0
+            followIndex = (data >> ((numFields - field - 1) * bits)) & mask;
+            edge = followers[edge][followIndex]
+            list.append(edges[edge])
+            field += 1
+            n -= 1
+    else:
+        list.append(edges[first])
+        n -= 1
+        while n > 0:
+            list.append(edges[readInt(content)])
+            n -= 1
+    return list
+
 def typedValueStr(content):
     valType = readByte(content)
     if valType == BYTE:
@@ -100,7 +129,7 @@ def typedValueStr(content):
     elif valType == EDGE_FUNCTION:
         return edgeTypes[readByte(content)]
     elif valType == ROUTE:
-        return edgeTypes[readByte(content)]
+        return " ".join(readRoute(content))
 
 out = sys.stdout
 content = open(sys.argv[1], 'rb')
