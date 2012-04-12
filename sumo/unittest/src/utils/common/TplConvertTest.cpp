a4521bc32fa10a5835287f249546e074583d40ed
Michael Behrisch <oss@behrisch.de>
2012-04-12 20:59:27 +0000

    TplConvert unit tests, refs #12, #82, #684
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12224 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/unittest/src/utils/common/TplConvertTest.cpp
@@ -0,0 +1,97 @@
+/****************************************************************************/
+/// @file    TplConvertTest.cpp
+/// @author  Michael Behrisch
+/// @date    Oct 2010
+/// @version $Id$
+///
+// Tests the class TplConvert
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright 2001-2010 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <gtest/gtest.h>
+#include <utils/common/TplConvert.h>
+
+
+// ===========================================================================
+// test definitions
+// ===========================================================================
+/* Test the method '_2int'.*/
+TEST(TplConvert, test_2int) {
+    EXPECT_EQ(0, TplConvert<char>::_2int("0"));
+    EXPECT_EQ(1, TplConvert<char>::_2int("+1"));
+    EXPECT_EQ(-1, TplConvert<char>::_2int("-1"));
+    EXPECT_THROW(TplConvert<char>::_2int("1e0"), NumberFormatException);
+    EXPECT_THROW(TplConvert<char>::_2int("100000000000"), NumberFormatException);
+    EXPECT_THROW(TplConvert<char>::_2int(""), EmptyData);
+}
+
+
+/* Test the method '_2long'.*/
+TEST(TplConvert, test_2long) {
+    EXPECT_EQ(0, TplConvert<char>::_2long("0"));
+    EXPECT_EQ(1, TplConvert<char>::_2long("+1"));
+    EXPECT_EQ(-1, TplConvert<char>::_2long("-1"));
+    EXPECT_THROW(TplConvert<char>::_2long("1e0"), NumberFormatException);
+    EXPECT_EQ(100000000000, TplConvert<char>::_2long("100000000000"));
+    EXPECT_THROW(TplConvert<char>::_2long(""), EmptyData);
+}
+
+
+/* Test the method '_2SUMOReal'.*/
+TEST(TplConvert, test_2SUMOReal) {
+    EXPECT_EQ(0, TplConvert<char>::_2SUMOReal("0"));
+    EXPECT_EQ(1, TplConvert<char>::_2SUMOReal("+1"));
+    EXPECT_EQ(-1, TplConvert<char>::_2SUMOReal("-1"));
+    EXPECT_EQ(1, TplConvert<char>::_2SUMOReal("1e0"));
+    EXPECT_EQ(10, TplConvert<char>::_2SUMOReal("1e1"));
+    EXPECT_EQ(1, TplConvert<char>::_2SUMOReal("1."));
+    EXPECT_EQ(1.1, TplConvert<char>::_2SUMOReal("1.1"));
+    EXPECT_EQ(.1, TplConvert<char>::_2SUMOReal(".1"));
+    EXPECT_EQ(1.1, TplConvert<char>::_2SUMOReal("1,1"));
+    EXPECT_EQ(.1, TplConvert<char>::_2SUMOReal(",1"));
+    EXPECT_EQ(100000000000, TplConvert<char>::_2SUMOReal("100000000000"));
+    EXPECT_THROW(TplConvert<char>::_2SUMOReal(""), EmptyData);
+    EXPECT_THROW(TplConvert<char>::_2SUMOReal("1e0x"), NumberFormatException);
+    EXPECT_THROW(TplConvert<char>::_2SUMOReal("1x"), NumberFormatException);
+}
+
+
+/* Test the method '_2bool'.*/
+TEST(TplConvert, test_2bool) {
+    EXPECT_EQ(true, TplConvert<char>::_2bool("true"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("false"));
+    EXPECT_EQ(true, TplConvert<char>::_2bool("True"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("False"));
+    EXPECT_EQ(true, TplConvert<char>::_2bool("yes"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("no"));
+    EXPECT_EQ(true, TplConvert<char>::_2bool("on"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("off"));
+    EXPECT_EQ(true, TplConvert<char>::_2bool("1"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("0"));
+    EXPECT_EQ(true, TplConvert<char>::_2bool("x"));
+    EXPECT_EQ(false, TplConvert<char>::_2bool("-"));
+    EXPECT_THROW(TplConvert<char>::_2bool(""), EmptyData);
+    EXPECT_THROW(TplConvert<char>::_2bool("1e0"), BoolFormatException);
+    EXPECT_THROW(TplConvert<char>::_2bool("Trari"), BoolFormatException);
+    EXPECT_THROW(TplConvert<char>::_2bool("yessir"), BoolFormatException);
+    EXPECT_THROW(TplConvert<char>::_2bool("ON"), BoolFormatException);
+}
