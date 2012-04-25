c42b8dca0c4c5825aaa46537aefa59065e5dfd89
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-04-25 15:00:35 +0000

    updating version.py so it can deal with newer versions of the SVN client refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12263 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/version.py
+++ sumo/tools/build/version.py
@@ -9,8 +9,10 @@
 
 This script rebuilds "../../src/version.h", the file which
  lets the applications know the version of their build.
-If a version file exists, it is kept, otherwise the current
- SVN revision is used as version information.
+It does this by parsing the SVN revision either from .svn/entries or .svn/wc.db (depending on svn
+version of the working copy).
+If the version file is newer than the svn file or the revision cannot be
+determined any exisitng vershion.h is kept
 
 SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
 Copyright (C) 2008-2012 DLR (http://www.dlr.de/) and contributors
@@ -18,32 +20,96 @@ All rights reserved
 """
 
 import sys
-from os.path import dirname, exists, getmtime, join
+import re
+from os.path import dirname, exists, getmtime, join, isdir
 
-versionDir = entriesDir = join(dirname(__file__), '..', '..', 'src')
-if len(sys.argv) > 1:
-    versionDir = sys.argv[1]
-if len(sys.argv) > 2:
-    entriesDir = sys.argv[2]
-versionFile = join(versionDir, 'version.h')
-entriesFile = join(entriesDir, '.svn', 'entries')
+UNKNOWN_REVISION = "UNKNOWN"
+SVNDIR = '.svn'
+SVN16FILE = 'entries'
+SVN17FILE = 'wc.db'
 
-svnRevision = "UNKNOWN"
-if exists(entriesFile):
-    if exists(versionFile) and getmtime(versionFile) > getmtime(entriesFile):
-        sys.exit()
-    entries = open(entriesFile)
-    for i, l in enumerate(entries):
-        if i == 3 and l.strip().isdigit():
-            svnRevision = l.strip()
-        revIndex = l.find('revision="')
-        if revIndex >= 0:
-            revIndex += 10
-            svnRevision = l[revIndex:l.index('"', revIndex)]
-            break
-    entries.close()
+def find_svnDir(searchRoot):
+    # we need to find the .svn folder
+    # for subversion 1.7 and later, it only exists at the wc root and each externals root
+    candidates = [
+            join(searchRoot, SVNDIR),              # src 
+            join(searchRoot, '..', SVNDIR),        # sumo 
+            join(searchRoot, '..', '..', SVNDIR)]  # trunk
+    for d in candidates:
+        if isdir(d):
+            return d
+    return None
 
-print('generating %s from revision in %s' % (versionFile, entriesFile))
-version = open(versionFile, 'w')
-print >> version, '#define VERSION_STRING "dev-SVN-r%s"' % svnRevision
-version.close()
+
+def find_svnFile(svnDir):
+    candidates = [join(svnDir, SVN17FILE), join(svnDir, SVN16FILE)]
+    for f in candidates:
+        if exists(f):
+            return f
+    return None
+
+
+def parseRevision(svnFile):
+    if SVN17FILE in svnFile:
+        # new style wc.db
+        svnRevision = -1
+        for l in open(svnFile, 'rb'):
+            m = re.search('[!]svn[/]ver[/](\d*)', l)
+            if m:
+                svnRevision = max(svnRevision, int(m.group(1)))
+        if svnRevision >= 0:
+            return svnRevision
+        else:
+            return UNKNOWN_REVISION
+    else:
+        # old style entries file
+        for i, l in open(svnFile):
+            if i == 3 and l.strip().isdigit():
+                svnRevision = l.strip()
+            revIndex = l.find('revision="')
+            if revIndex >= 0:
+                revIndex += 10
+                svnRevision = l[revIndex:l.index('"', revIndex)]
+                return svnRevision
+        return UNKNOWN_REVISION
+
+
+def create_version_file(versionFile, svnRevision, svnFile):
+    print('generating %s from revision in %s' % (versionFile, svnFile))
+    with open(versionFile, 'w') as f:
+        print >>f, '#define VERSION_STRING "dev-SVN-r%s"' % svnRevision
+
+
+def main():
+    sumoSrc = join(dirname(__file__), '..', '..', 'src')
+    # determine output file
+    if len(sys.argv) > 1:
+        versionDir = sys.argv[1]
+    else:
+        versionDir = sumoSrc
+    versionFile = join(versionDir, 'version.h')
+
+    # determine svn dir
+    if len(sys.argv) > 2:
+        svnDir = sys.argv[2]
+    else:
+        svnDir = find_svnDir(sumoSrc)
+    if svnDir == None or not exists(svnDir):
+        print "unknown revision - svn dir '%s' not found" % svnDir
+        if not exists(versionFile):
+            create_version_file(versionFile, UNKNOWN_REVISION, "<None>")
+    else:
+        # determine svn file
+        svnFile = find_svnFile(svnDir)
+        if svnFile == None:
+            print "unknown revision - no svn file found in %s" % svnDir
+            if not exists(versionFile):
+                create_version_file(versionFile, UNKNOWN_REVISION, "<None>")
+        if not exists(versionFile) or getmtime(versionFile) < getmtime(svnFile):
+            # svnFile is newer. lets update the revision number
+            svnRevision = parseRevision(svnFile)
+            create_version_file(versionFile, svnRevision, svnFile)
+
+
+if __name__ == "__main__":
+    main()
