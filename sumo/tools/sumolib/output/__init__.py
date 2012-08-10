d0c65369c982feb702d2e8db35ecfbf37d6c5e42
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2012-08-10 13:41:45 +0000

    adding non-flat parsing as a preparation of rerouter tests reformulation, refs#729
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12519 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/sumolib/output/__init__.py
+++ sumo/tools/sumolib/output/__init__.py
@@ -24,25 +24,44 @@ def parse(xmlfile, element_name):
     # parses the attributes of all nodes with element_name and returns a list of namedtuples
     # @note the first node in xmlfile will determine the set of attributes 
     # @note attribute names which are also python keywords will be prefixed with 'attr_' 
-    elementType = [] # mutable, will be [namedtuple]
+    
+    # @note next line is no longer used; the idea is to give an object (optionally) which describes what shall be parsed (and converted...)
+    #elementType = [] # mutable, will be [namedtuple]
     xml_doc = pulldom.parse(xmlfile)
-    return [_get_attrs(parsenode, elementType, element_name) for event, parsenode in xml_doc 
+    return [_get_attrs(xml_doc, parsenode, [], element_name) for event, parsenode in xml_doc 
             if event == pulldom.START_ELEMENT and parsenode.localName == element_name]
 
-def _get_attrs(node, elementType, element_name):
+def _get_attrs(xml_doc, node, elementType, element_name, first=True):
     # get all attributes defined in elementType from the parsed node
     # if elementType is not yet defined create it as a a named tuple named element_name
+    if first: xml_doc.expandNode(node)
+    # @note Currently, 'elementType' is always empty, see note in parse(...)
     if not elementType:
         # initialized the named tuple type (only once)
         # note: for unfathomable reasons NamedNodeMap does not support pythonic iteration
         attrnames = [node.attributes.item(i).localName for i in range(node.attributes.length)]
-        attrnames = [_prefix_keyword(a) for a in attrnames]
+        attrnames.extend([_prefix_child(attrnames, node.childNodes.item(i).localName) for i in range(node.childNodes.length) if node.childNodes.item(i).localName])
+        attrnames = [_prefix_keyword(a) for a in list(set(attrnames))]
         elementType.append(namedtuple(element_name, attrnames))
-    return elementType[0](*[node.getAttribute(a) for a in elementType[0]._fields])
-
+    vals = {}
+    for v in elementType[0]._fields:
+        if node.hasAttribute(v):
+            vals[v] =  node.getAttribute(v)            
+        else:
+            vals[v] = None
+            c = node.firstChild
+            while c!=None:
+                if c.nodeType==c.ELEMENT_NODE and c.localName==v:
+                    if not vals[v]: vals[v] = []
+                    vals[v].append(_get_attrs(xml_doc, c, [], c.tagName, False))
+                c = c.nextSibling
+    return elementType[0](*[vals[a] for a in elementType[0]._fields])
 
 def _prefix_keyword(name):
     return 'attr_' + name if iskeyword(name) else name
+
+def _prefix_child(attrnames, name):
+    return 'child_' + name if name in attrnames else name
 
 
 def sum(elements, attrname):
