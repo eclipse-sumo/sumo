14b44a07a6f99dffb40cdb4c38e0db541ff5c09f
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-09-10 10:55:22 +0000

    added option for plotting teleports for interesting edges, refactoring
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12703 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/duaIterate_analysis.py
+++ sumo/tools/assign/duaIterate_analysis.py
@@ -30,6 +30,8 @@ def parse_args():
             help="limit length of the plot label to this size")
     optParser.add_option("--limit", type=int,  default=uMax,
             help="only parse the first INT number of iterations")
+    optParser.add_option("--teleports", default="teleplot",
+            help="output prefix for plotting teleport-prone edges")
     options, args = optParser.parse_args()
     if len(args) != 1:
         sys.exit(USAGE)
@@ -39,19 +41,27 @@ def parse_args():
 def parse_dualog(dualog, limit):
     print "Parsing %s" % dualog
     teleStats = Statistics('Teleports')
-    step_values = [['#Emitted', 'Running', 'Waiting', 'Teleports', 'Loaded']] # list of lists
+    header = ['#Emitted', 'Running', 'Waiting', 'Teleports', 'Loaded']
+    step_values = [] # list of lists
+    step_counts = [] # list of edge teleport counters
     reEmitted = re.compile("Emitted: (\d*)")
     reLoaded = re.compile("Loaded: (\d*)")
     reRunning = re.compile("Running: (\d*)")
     reWaiting = re.compile("Waiting: (\d*)")
+    reFrom = re.compile("from '([^']*)'")
     teleports = 0
     emitted = None
     loaded = None
     running = None
     waiting = None
+    counts = defaultdict(lambda:0)
     for line in open(dualog):
         if "Warning: Teleporting vehicle" in line:
             teleports += 1
+            edge = reFrom.search(line).group(1)
+            if ':' in edge: # mesosim output
+                edge = edge.split(':')[0]
+            counts[edge] += 1
         elif "Emitted:" in line:
             emitted = reEmitted.search(line).group(1)
             if "Loaded:" in line: # optional output
@@ -68,9 +78,12 @@ def parse_dualog(dualog, limit):
             teleStats.add(teleports, iteration)
             step_values.append([emitted, running, waiting, teleports, loaded])
             teleports = 0
-    print "  parsed %s steps" % (len(step_values) - 1)
+            step_counts.append(counts)
+            counts = defaultdict(lambda:0)
+
+    print "  parsed %s steps" % len(step_values)
     print teleStats
-    return step_values
+    return [header] + step_values, step_counts
 
 def parse_stdout(step_values, stdout):
     print "Parsing %s" % stdout
@@ -118,16 +131,42 @@ plot \\
 """ % ((xlabel,) + (datafile,) * 5))
 
 
+def create_teleplot(plotfile, step_counts, xlabel):
+    datafile = plotfile + '.data'
+    # an edge is interesting if it has the most teleports in one iteration
+    interesting = set()
+    all_edges = set()
+    for counts in step_counts:
+        interesting.add(max([(c,e) for e,c in counts.iteritems()])[1])
+        all_edges.update(counts.keys())
+    interesting = list(interesting)
+    print "generating teleport plot for %s edges (%s different edges had teleports)" % (
+            len(interesting), len(all_edges))
+    with open(datafile, 'w') as f:
+        print >>f, '#' + ' '.join(interesting)
+        for counts in step_counts:
+            values = [counts[e] for e in interesting]
+            f.write(' '.join(map(str,values)) + '\n')
+    # write plotfile
+    with open(plotfile, 'w') as f:
+        f.write("set xlabel '%s'\nplot \\\n")
+        lines = ["'%s' using 0:%s title '%s' with lines" % (datafile, ii+1, edge) 
+                for ii, edge in enumerate(interesting)]
+        f.write(', \\\n'.join(lines))
+
+
 def main():
     options = parse_args()
     plotfile = options.output
     datafile = plotfile + '.data'
-    step_values = parse_dualog(options.dualog, options.limit)
+    step_values, step_counts = parse_dualog(options.dualog, options.limit)
     if options.stdout is not None:
         parse_stdout(step_values, options.stdout)
     write_data(datafile, step_values)
     duaPath = os.path.dirname(os.path.abspath(options.dualog))[-options.label_size:]
-    write_plotfile(plotfile, datafile, 'Iterations in ' + duaPath)
+    xlabel = 'Iterations in ' + duaPath
+    write_plotfile(plotfile, datafile, xlabel)
+    create_teleplot(options.teleports, step_counts, xlabel)
 
 ##################
 if __name__ == "__main__":
