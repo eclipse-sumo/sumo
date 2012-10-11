bf6474cd310ccad913f54a0fa9853a7b89bd20a6
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-10-11 13:51:16 +0000

    added script which extracts all used edges from a route file and create output suitable for passing to NETCONVERT --keep-edges.input-file. This is usefull when debugging large scenarios
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12831 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/route/extract_route_edges.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+@file    randomTrips.py
+@author  Jakob Erdmann
+@date    2012-10-11
+@version $Id: extract_route_edges.py 12711 2012-09-14 06:44:55Z namdre $
+
+Extract all used edges from routes and person plans and output a file suitable
+for pruning edges with netconvert
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2010-2012 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+
+import os,sys
+from collections import defaultdict
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from sumolib.output import parse, parse_fast
+
+route_file, keep_file = sys.argv[1:]
+edges = set()
+for route in parse_fast(route_file, 'route', ['edges']):
+    edges.update(route.edges.split())
+for walk in parse_fast(route_file, 'walk', ['edges']):
+    edges.update(walk.edges.split())
+
+with open(keep_file, 'w') as outf:
+    outf.write(','.join(edges) + '\n')
