7289a68d8041360c228255b91e00aa564fd2b7d7
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-11-08 08:04:39 +0000

    modified depart speed guard so it ignores vehicle-specific deviations. Otherwise given departSpeeds cause random errors
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12952 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSVehicle.cpp
+++ sumo/src/microsim/MSVehicle.cpp
@@ -304,7 +304,7 @@ MSVehicle::MSVehicle(SUMOVehicleParameter* pars,
     if (depLane == 0) {
         throw ProcessError("Invalid departlane definition for vehicle '" + pars->id + "'.");
     }
-    if (pars->departSpeedProcedure == DEPART_SPEED_GIVEN && pars->departSpeed > depLane->getVehicleMaxSpeed(this)) {
+    if (pars->departSpeedProcedure == DEPART_SPEED_GIVEN && pars->departSpeed > depLane->getSpeedLimit()) {
         throw ProcessError("Departure speed for vehicle '" + pars->id +
                            "' is too high for the departure lane '" + depLane->getID() + "'.");
     }
