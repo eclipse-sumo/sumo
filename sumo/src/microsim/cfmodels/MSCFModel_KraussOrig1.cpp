a95f887c2c930b32cf4e928e3280874fe8100a3a
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-11-09 13:58:19 +0000

    now bounding deceleration refs #779
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@12959 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/cfmodels/MSCFModel_KraussOrig1.cpp
+++ sumo/src/microsim/cfmodels/MSCFModel_KraussOrig1.cpp
@@ -60,11 +60,13 @@ MSCFModel_KraussOrig1::moveHelper(MSVehicle* const veh, SUMOReal vPos) const {
     //  vSafe does not incorporate speed reduction due to interaction
     //  on lane changing
     const SUMOReal vMin = getSpeedAfterMaxDecel(oldV);
-    const SUMOReal vMax = MIN3(veh->getLane()->getVehicleMaxSpeed(veh), maxNextSpeed(oldV), vSafe);
+    // do not exceed max decel even if it is unsafe
+    SUMOReal vMax = MAX2(vMin,
+            MIN3(veh->getLane()->getVehicleMaxSpeed(veh), maxNextSpeed(oldV), vSafe));
 #ifdef _DEBUG
-    if (vMin > vMax) {
-        WRITE_WARNING("Vehicle's '" + veh->getID() + "' maximum speed is lower than the minimum speed (min: " + toString(vMin) + ", max: " + toString(vMax) + ").");
-    }
+    //if (vMin > vMax) {
+    //    WRITE_WARNING("Vehicle's '" + veh->getID() + "' maximum speed is lower than the minimum speed (min: " + toString(vMin) + ", max: " + toString(vMax) + ").");
+    //}
 #endif
     return veh->getLaneChangeModel().patchSpeed(vMin, MAX2(vMin, dawdle(vMax)), vMax, *this);
 }
