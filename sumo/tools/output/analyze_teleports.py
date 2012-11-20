a7e5d9935e9a9489478186f079dff479e3a5ebf1
Jakob Erdmann <jakob.erdmann@dlr.de>
2012-11-20 10:21:28 +0000

    added script for analyzing the warning output of a simluation run
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@13017 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/output/analyze_teleports.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    analyze_teleports.py
+@author  Jakob Erdmann
+@date    2012-11-20
+@version $Id: analyze_teleports.py 12757 2012-09-26 14:01:46Z namdre $
+
+Extract statistics from the warning outputs of a simulation run for plotting.
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2008-2012 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import os,sys
+import re
+from collections import defaultdict
+
+def parse_log(logfile, edges=True, aggregate=3600):
+    print "Parsing %s" % logfile
+    reFrom = re.compile("lane='([^']*)'")
+    reTime = re.compile("time=(\d*)\.")
+    # counts per lane
+    waitingCounts = defaultdict(lambda:0)
+    collisionCounts = defaultdict(lambda:0)
+    # counts per step
+    waitingStepCounts = defaultdict(lambda:0)
+    collisionStepCounts = defaultdict(lambda:0)
+    for line in open(logfile):
+        try:
+            if "Warning: Teleporting vehicle" in line:
+                edge = reFrom.search(line).group(1)
+                time = reTime.search(line).group(1)
+                if edges:
+                    edge = edge[:-2]
+                if "collision" in line:
+                    collisionCounts[edge] += 1
+                    collisionStepCounts[int(time) / aggregate] += 1
+                else:
+                    waitingCounts[edge] += 1
+                    waitingStepCounts[int(time) / aggregate] += 1
+        except:
+            print sys.exc_info()
+            sys.exit("error when parsing line '%s'" % line)
+
+    return (waitingCounts, collisionCounts,
+            waitingStepCounts, collisionStepCounts)
+
+
+def print_counts(countDict, label):
+    counts = [(v,k) for k,v in countDict.items()] 
+    counts.sort()
+    print counts
+    print label, 'total:', sum(countDict.values())
+
+
+def main(logfile):
+    options = parse_args()
+    waitingCounts, collisionCounts, waitingStepCounts, collisionStepCounts = parse_log(logfile)
+    print_counts(waitingCounts, 'waiting')
+    print_counts(collisionCounts, 'collisions')
+    # generate plot
+    min_step = min(min(waitingStepCounts.keys()),
+            min(collisionStepCounts.keys()))
+    max_step = max(max(waitingStepCounts.keys()),
+            max(collisionStepCounts.keys()))
+    with open(logfile + '.plot', 'w') as f:
+        f.write("# plot 'teleplot.data' using 1:2 with lines title 'waiting', 'teleplot.data' using 1:3 with lines title 'collisions'\n")
+        for step in range(min_step, max_step + 1):
+            print >>f, ' '.join(map(str,[step, waitingStepCounts[step], collisionStepCounts[step]]))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
