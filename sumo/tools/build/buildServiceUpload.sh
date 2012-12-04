dbb098206640d87075242914bc47078ab7f79cf3
Michael Behrisch <oss@behrisch.de>
2012-12-04 13:45:59 +0000

    removing script, calling directly
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@13135 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/buildServiceUpload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PREFIX=$1
-for f in $PREFIX/sumo/sumo-src-*.tar.gz; do
-  if test -e $PREFIX/sumo/configure -a $f -nt $PREFIX/sumo/configure; then
-    curl -n -X PUT -T $PREFIX/sumo/build/package/sumo.spec https://api.opensuse.org/source/home:behrisch/sumo_nightly/sumo.spec
-  fi
-done
