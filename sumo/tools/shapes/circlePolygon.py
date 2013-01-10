175de82b5cd3d426fc502470ecbc39e95987fdfe
Michael Behrisch <oss@behrisch.de>
2013-01-10 15:02:20 +0000

    creating polygons from circle descriptions
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@13176 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/shapes/circlePolygon.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+@file    poi_atTLS.py
+@author  Daniel Krajzewicz
+@author  Michael Behrisch
+@date    2010-02-20
+@version $Id$
+
+Approximates a list of circles by polygons.
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2010-2012 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+
+import os, sys, math
+from optparse import OptionParser
+
+optParser = OptionParser()
+optParser.add_option("-r", "--radius", type="float", default=100,
+                     help="default radius")
+optParser.add_option("-p", "--prefix", default="poly",
+                     help="id prefix")
+optParser.add_option("-t", "--type", default="unknown",
+                     help="type string")
+optParser.add_option("-c", "--color", default="1,0,0",
+                     help="color string")
+optParser.add_option("-f", "--fill", action="store_true",
+                     default=False, help="fill the polygons")
+optParser.add_option("-l", "--layer", type="int", default=-1,
+                     help="layer")
+optParser.add_option("-x", "--corners", type="int", default=100,
+                     help="default number of corners")
+(options, args) = optParser.parse_args()
+
+if len(args) == 0:
+    print >> sys.stderr, "Usage: " + sys.argv[0] + " x,y[[,r],c] ..."
+    sys.exit()
+
+print "<additional>"
+for idx, d in enumerate(args):
+    desc = d.split(",")
+    x = float(desc[0])
+    y = float(desc[1])
+    r = float(desc[2]) if len(desc) > 2 else options.radius
+    c = int(desc[3]) if len(desc) > 3 else options.corners
+    angle = 2 * math.pi / c
+    shape = ""
+    for i in range(c):
+        shape += "%s,%s " % (math.cos(i * angle) * r + x, math.sin(i * angle) * r + y)
+    print '    <polygon id="%s%s" type="%s" color="%s"' % (options.prefix, idx, options.type, options.color),
+    print 'fill="%i" layer="%s" shape="%s"/>' % (options.fill, options.layer, shape[:-1])
+print "</additional>"
