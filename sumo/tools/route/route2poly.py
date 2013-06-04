4db9f51f3bf7069eb5df5f41906f52274b940aff
Michael Behrisch <oss@behrisch.de>
2013-06-04 04:24:23 +0000

    small script for repeated scenario execution
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14093 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/route/route2poly.py
+++ sumo/tools/route/route2poly.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 @file    route2poly.py
 @author  Jakob Erdmann
---
+++ sumo/tools/route/vehicle2flow.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+@file    vehicle2flow.py
+@author  Michael Behrisch
+@date    2012-11-15
+@version $Id: route2poly.py 13811 2013-05-01 20:31:43Z behrisch $
+
+This script replaces all vehicle definitions in a route file by
+flow definitions, adding an XML ntity for the repeat interval for
+easy later modification.
+
+SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+Copyright (C) 2013-2013 DLR (http://www.dlr.de/) and contributors
+All rights reserved
+"""
+import sys, os, re
+from optparse import OptionParser
+
+def parse_args():
+    USAGE = "Usage: " + sys.argv[0] + " <routefile> [options]"
+    optParser = OptionParser()
+    optParser.add_option("-o", "--outfile", help="name of output file")
+    optParser.add_option("-r", "--repeat", default=1000, type="float", help="repeater interval")
+    optParser.add_option("-e", "--end", default=2147483, type="float", help="end of the flow")
+    options, args = optParser.parse_args()
+    try:
+        options.routefile = args[0]
+    except:
+        sys.exit(USAGE)
+    if options.outfile is None:
+        options.outfile = options.routefile + ".rou.xml"
+    return options 
+
+def main():
+    options = parse_args()
+    with open(options.routefile) as f:
+        with open(options.outfile, 'w') as outf:
+            headerSeen = False
+            for line in f:
+                if "<routes " in line or "<routes>" in line:
+                    outf.write("""<!DOCTYPE routes [
+    <!ENTITY RepeatInterval "%s">
+    <!ENTITY RepeatEnd "%s">
+]>
+""" % (options.repeat, options.end))
+                line = re.sub(r'<vehicle(.*)depart( ?= ?"[^"]*")', r'<flow\1begin\2 end="&RepeatEnd;" period="&RepeatInterval;"', line)
+                line = re.sub(r'</vehicle>', '</flow>', line)
+                outf.write(line)
+
+if __name__ == "__main__":
+    main()
