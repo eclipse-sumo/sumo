f8cd7faa09a663adff17f06164c6a689db81f622
Michael Behrisch <oss@behrisch.de>
2013-06-28 12:47:25 +0000

    fixing embedded runner, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14233 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/tutorial/traci_tls/embedded.py
+++ sumo/tests/complex/tutorial/traci_tls/embedded.py
@@ -14,33 +14,33 @@ Copyright (C) 2009-2013 DLR/TS, Germany
 All rights reserved
 """
 
-import sys
+import os, sys, subprocess
+# the embedded python does not add the current dir to the python path, so we need to do it
+sys.path.append(os.path.dirname(__file__))
 import runner
 
-# this is the main entry point of this script
-if __name__ == "__main__":
+
+if runner.traci.isEmbedded():
+    # this script has been called from the sumo-internal python interpreter
+    # only execute the main control procedure
+    runner.run()
+else:
     options = runner.get_options()
-
-    if runner.traci.isEmbedded():
-        # this script has been called from the sumo-internal python interpreter
-        # only execute the main control procedure
-        runner.run()
+    # this script has been called from the command line. It will start sumo with
+    # this script as argument
+    if options.nogui:
+        sumoBinary = runner.checkBinary('sumo')
     else:
-        # this script has been called from the command line. It will start sumo as a
-        # server, then connect and run
-        if options.nogui:
-            sumoBinary = runner.checkBinary('sumo')
-        else:
-            sumoBinary = runner.checkBinary('sumo-gui')
+        # gui running probably does not work yet
+        sumoBinary = runner.checkBinary('sumo-gui')
 
-        # first, generate the route file for this simulation
-        runner.generate_routefile()
+    # first, generate the route file for this simulation
+    runner.generate_routefile()
 
-        # now execute sumo
-        sumoConfig = 
-
-        # call sumo with the request to run this very same script again in the internal interpreter
-        # when this happens, the method traci.isEmbedded() in line 23 will evaluate to true
-        # and then the run method will be called
-        retCode = subprocess.call([sumoBinary, "-c", "data/cross.sumocfg", "--python-script", __file__], stdout=sys.stdout)
-        sys.exit(retCode)
+    # call sumo with the request to run this very same script again in the internal interpreter
+    # when this happens, the method traci.isEmbedded() in line 23 will evaluate to true
+    # and then the run method will be called
+    retCode = subprocess.call([sumoBinary, "-c", "data/cross.sumocfg",
+                               "--remote-port", "0", "--python-script", __file__],
+                              stdout=sys.stdout, stderr=sys.stderr)
+    sys.exit(retCode)
