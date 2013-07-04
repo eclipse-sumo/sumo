76d5c62cff0809e72c06cb7ac017a16cbae1c9be
Daniel Krajzewicz <Daniel.Krajzewicz@dlr.de>
2013-07-04 10:12:30 +0000

    debugging
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14266 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/assign/duaIterate.py
+++ sumo/tools/assign/duaIterate.py
@@ -185,6 +185,7 @@ def writeRouteConf(step, options, file, output, routesInfo, initial_type):
         <keep-all-routes value="%s"/>
         <routing-algorithm value="%s"/>%s
         <max-alternatives value="%s"/>
+        <weights.expand value="true"/>
         <logit value="%s"/>
         <logit.beta value="%s"/>
         <logit.gamma value="%s"/>""" % (
@@ -460,7 +461,7 @@ def main(args=None):
         if not (options.skipFirstRouting and step == 0):
             simulation_demands = [get_basename(f) + "_%03i.rou%s" % (step, routesSuffix) for f in input_demands]
         if not ((options.skipFirstRouting and step == 1) or step == 0):
-            router_demands = [get_basename(f) + "_%03i.rou.alt%s" % (step-1, routesSuffix) for fr in input_demands]
+            router_demands = [get_basename(f) + "_%03i.rou.alt%s" % (step-1, routesSuffix) for f in input_demands]
 
         if not (options.skipFirstRouting and step == options.firstStep):
             # call duarouter
