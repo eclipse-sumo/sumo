5f00009a002d95574001a5417bf2099a7f88f240
Michael Behrisch <oss@behrisch.de>
2013-08-19 19:59:11 +0000

    fixing copyright dates, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14443 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/traci/trafficlights.py
+++ sumo/tools/traci/trafficlights.py
@@ -8,7 +8,7 @@
 Python implementation of the TraCI interface.
 
 SUMO, Simulation of Urban MObility; see http://sumo-sim.org/
-Copyright (C) 2011 DLR (http://www.dlr.de/) and contributors
+Copyright (C) 2011-2013 DLR (http://www.dlr.de/) and contributors
 All rights reserved
 """
 import struct, traci
