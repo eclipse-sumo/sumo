7661f3d1da588c36951caf9f8c973f65271682b6
Michael Behrisch <oss@behrisch.de>
2013-08-22 20:03:50 +0000

    fixing links and style, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14475 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/docs/internet/daily/index.php
+++ sumo/docs/internet/daily/index.php
@@ -1,12 +1,49 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-    <head>
-        <title>Status of daily build and test</title>
-    </head>
-    <body>
-        <center>
-            <h2>Status of daily build and test</h2>
-        </center>
+
+<head>
+ <title>SUMO - Status of daily build and tests</title>
+ <link rel="stylesheet" type="text/css" href="../sumo.css"/>
+
+  <link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+  <META NAME="DC.Title" CONTENT="SUMO - Status of daily build and tests">
+  <META NAME="DC.Subject" CONTENT="road traffic simulation package SUMO (Simulation of Urban MObility)">
+  <META NAME="DC.Description" CONTENT="">
+  <META NAME="DC.Publisher" CONTENT="Institute of Transportation Systems at the German Aerospace Center">
+  <META NAME="DC.Type" CONTENT="Text">
+  <META NAME="DC.Format" SCHEME="IMT" CONTENT="text/html">
+  <META NAME="DC.Identifier" CONTENT="http://sumo-sim.org">
+  <META NAME="DC.Language" SCHEME="ISO639-1" CONTENT="en">
+  <META NAME="DC.Relation" SCHEME="URL" CONTENT="http://sumo-sim.org/index.html">
+  <META NAME="DC.Rights" CONTENT="(c) ITS/DLR">
+  <META NAME="DC.Date.X-MetadataLastModified" SCHEME="ISO8601" CONTENT="2011-03-04">
+  <META http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+
+<body id="bmission">
+<div id="container">
+
+
+
+ <div id="logo" align="left"><img src="logo.png" width="128" height="128" align="left"
+     alt="SUMO-Simulation of Urban Mobility" style="margin-left:10px;
+     margin-right:20px;"/>
+     <div id="logo_text1"> SUMO </div>
+     <div id="logo_text2"> Simulation of Urban MObility</div>
+ </div>
+
+ <div id="subsMenu"><ul>
+  <li class="sub"><a href="/">Home</a></li>
+  <li class="sub"><a href="/wiki/Downloads">Download</a></li>
+  <li class="sub"><a href="/userdoc/">Documentation</a></li>
+  <li class="sub"><a href="/wiki/">Wiki</a></li>
+  <li class="sub"><a href="/trac/">Trac</a></li>
+  <li class="sub"><a href="/blog/">Blog</a></li>
+  <li class="sub"><a href="/userdoc/Contact.html">Contact</a></li>
+  <li class="sub"><a href="http://sourceforge.net/projects/sumo/">SF-Project</a></li>
+ </ul></div>
+
+    <h2>Status of daily build and tests</h2>
         <table>
 <?PHP
 $column = 0;
@@ -65,6 +102,14 @@ for ($j = 0; $j < count($cells[0]); $j++) {
 }
 ?> 
         </table>
-    </body>
-</html>
+ </div>
 
+ <div id="footer">
+   <div>(c) 2011-2013, German Aerospace Center, Institute of Transportation Systems</div>
+   <div>Layout based on <a href="http://www.oswd.org/design/preview/id/3365">"Three Quarters"</a> by "SimplyGold"</div>
+ </div>
+                                                       
+ 
+</div></body>
+
+</html>
