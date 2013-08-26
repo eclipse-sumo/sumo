449b0bfbdbc8b40f23bc1d53d5d2ffe10297644b
Michael Behrisch <oss@behrisch.de>
2013-08-26 20:17:38 +0000

    building schemata only for available executables, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14497 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/xml/rebuildSchemata.py
+++ sumo/tools/xml/rebuildSchemata.py
@@ -15,4 +15,7 @@ import os, sys, subprocess
 homeDir = os.environ.get("SUMO_HOME", os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 binDir = os.environ.get("SUMO_BINDIR", os.path.join(homeDir, "bin"))
 for exe in "activitygen dfrouter duarouter marouter jtrrouter netconvert netgenerate od2trips polyconvert sumo".split():
-    subprocess.call([os.path.join(binDir, exe), "--save-schema", os.path.join(homeDir, "docs", "internet", "xsd" , exe+"Configuration.xsd")])
+    if os.path.exists(os.path.join(binDir, exe)):
+        subprocess.call([os.path.join(binDir, exe), "--save-schema", os.path.join(homeDir, "docs", "internet", "xsd" , exe+"Configuration.xsd")])
+    else:
+        print "Warning! %s not found." % exe
