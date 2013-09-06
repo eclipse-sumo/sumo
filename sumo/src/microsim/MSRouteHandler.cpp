1ac3b96ddf8a254ecf5324efb1e8f98cb596e6d6
Michael Behrisch <oss@behrisch.de>
2013-09-06 08:19:01 +0000

    forbidding 0 speed and duration for walks as spec says, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14612 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSRouteHandler.cpp
+++ sumo/src/microsim/MSRouteHandler.cpp
@@ -154,11 +154,14 @@ MSRouteHandler::myStartElement(int element,
             SUMOReal departPos = attrs.getOpt<SUMOReal>(SUMO_ATTR_DEPARTPOS, myVehicleParameter->id.c_str(), ok, 0);
             SUMOReal arrivalPos = attrs.getOpt<SUMOReal>(SUMO_ATTR_ARRIVALPOS, myVehicleParameter->id.c_str(), ok, -1);
             const SUMOTime duration = attrs.getOptSUMOTimeReporting(SUMO_ATTR_DURATION, 0, ok, -1);
+            if (attrs.hasAttribute(SUMO_ATTR_DURATION) && duration <= 0) {
+                throw ProcessError("Non-positive walking duration for  '" + myVehicleParameter->id + "'.");
+            }
             SUMOReal speed = DEFAULT_PERSON_SPEED;
             if (attrs.hasAttribute(SUMO_ATTR_SPEED)) {
                 speed = attrs.getOpt<SUMOReal>(SUMO_ATTR_SPEED, 0, ok, speed);
-                if (speed < 0) {
-                    throw ProcessError("Negative walking speed for  '" + myVehicleParameter->id + "'.");
+                if (speed <= 0) {
+                    throw ProcessError("Non-positive walking speed for  '" + myVehicleParameter->id + "'.");
                 }
             }
             std::string bsID = attrs.getOpt<std::string>(SUMO_ATTR_BUS_STOP, 0, ok, "");
