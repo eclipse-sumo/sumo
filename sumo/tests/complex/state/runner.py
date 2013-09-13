41de8f4951d095461ea37e3007287ad7571fc3e5
Jakob Erdmann <jakob.erdmann@dlr.de>
2013-09-13 12:06:26 +0000

    patching micro state tests refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14702 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/state/runner.py
+++ sumo/tests/complex/state/runner.py
@@ -4,8 +4,18 @@ import os,subprocess,sys
 idx = sys.argv.index(":")
 saveParams = sys.argv[1:idx]
 loadParams = sys.argv[idx+1:]
+if '--mesosim' in loadParams:
+    saveParams.append('--mesosim')
+
+# need to add runner.py again in options.complex.meso to ensure it is the last  entry
+saveParams = [p for p in saveParams if not 'runner.py' in p]
+loadParams = [p for p in loadParams if not 'runner.py' in p]
+
+#print "save:", saveParams
+#print "load:", loadParams
 
 sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', 'bin', 'sumo'))
+#print "sumoBinary", sumoBinary
 subprocess.call([sumoBinary]+saveParams,
                 shell=(os.name=="nt"), stdout=sys.stdout, stderr=sys.stderr)
 subprocess.call([sumoBinary]+loadParams,
