a732f8f38c6cc5568eef938ace286b8546b639c9
Michael Behrisch <oss@behrisch.de>
2013-10-16 09:09:00 +0000

    playing around with python unit tests, refs #82
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@14849 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/unittest/testSuiteTools.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""
+@file    testSuiteDetector.py
+@author  Matthias.Wagner@dlr.de
+@date    2008-02-11
+@version $Id$
+
+Test suite to run all tests in the detector package.
+
+Copyright (C) 2008 DLR/FS, Germany
+All rights reserved
+"""
+import unittest, os, sys
+import tools
+
+class ToolsTestSuite(unittest.TestSuite):
+    '''
+    Test suite class for the package detector.
+    All test have to be added in import and in method addTestCases.
+    Running this class will run all test cases, the results displayed on the 
+    command line.
+    '''
+    passedList = []
+    failedList = []
+    errorList = []
+
+    def __init__(self):
+        #setUp = self.__setUpFunc
+        unittest.TestSuite.__init__(self)
+        #self.addTest(self.suite())
+        self.addTestCases()
+
+    def setUp(self):
+        '''
+        To prepare the test environment.
+        Note: This method does not replace or overwrite setUp() in any TestCase class! 
+        Note: This method is not run when starting this test suite as "Python unit-test" 
+              in the eclipse environment or similar unit test environments! 
+        '''
+        print "Start Test ..."
+
+    def run(self, result):
+        '''
+        Method being called to execute the tests.
+        @param result A result object for the presentation of the results
+        '''
+        self.setUp()
+
+        unittest.TestSuite.run(self, result)
+
+        self.tearDown()
+
+        if len(result.errors) > 0:
+            print "\nErrors:"
+            print "===================="
+            for error in result.errors:
+                print "+++ Test:", error[0], "\n", error[1]
+        if len(result.failures) > 0:
+            print "\nFailures:"
+            print "===================="
+            for failure in result.failures:
+                print "+++ Test:", failure[0], "\n", failure[1]
+
+        print "Summary:", result.testsRun, "Tests run"
+        print "===================="
+        print "Test Passed:", result.testsRun - (len(result.failures) + len(result.errors))
+        print "Test Failures:", len(result.failures)
+        print "Test Errors:", len(result.errors)
+        for test in self.passedList:
+            print test
+
+    def tearDown(self):
+        '''
+        Finishes the test environment.
+        Note: This method does not replace or overwrite tearDown() in any TestCase class!
+        Note: This method is not run when starting this test suite as "Python unit-test" 
+              in the eclipse environment or similar unit test environments!
+        '''
+        print "... Test End."
+
+    def addTestCases(self):
+        '''
+        Method to add all test classes to this suite.
+        Currently each test class has to be added manually here and be imported.
+        TODO: Apply or write a tool to automatically add the test cases.
+        '''
+        self.addTest(unittest.TestLoader().loadTestsFromTestCase(tools.district.testEdgesInDistrict.TestEdgesInDistrict))
+
+class ToolsTestResult(unittest.TestResult):
+    '''
+    Class to collect the results and format the output during the test run.
+    '''
+    shouldStop = False
+    def __init__(self):
+        unittest.TestResult.__init__(self)
+
+    def addError(self, test, err):
+        '''
+        Overwitten from @see unittest.TestResult
+        '''
+        #print "ERROR:  ", test, err
+        print test, "... Error!"
+        unittest.TestResult.addError(self, test, err)
+    def addFailure(self, test, err):
+        '''
+        Overwitten from @see unittest.TestResult
+        '''
+        #print "Failed: ", test, err
+        print test, "... Failed!"
+        unittest.TestResult.addFailure(self, test, err)
+    def addSuccess(self, test):
+        '''
+        Overwitten from @see unittest.TestResult
+        '''
+        print test, "... Passed."
+        unittest.TestResult.addSuccess(self, test)
+
+if __name__ == '__main__':
+    testSuite = ToolsTestSuite()
+    testSuite.run(ToolsTestResult())
+
---
+++ sumo/unittest/tools/district/testEdgesInDistrict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+@file    testPolyfit.py
+@author  Michael.Behrisch@dlr.de
+@date    2007-07-18
+@version $Id$
+
+Simple Unittest to check whether numpy is available and does not have the
+polyfit bug showing up in the 1.0.4 release
+
+Copyright (C) 2007 DLR/FS, Germany
+All rights reserved
+"""
+import os, sys, unittest
+TOOLS_DIR = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'tools', 'district')
+sys.path.append(TOOLS_DIR)
+import edgesInDistricts
+
+class TestEdgesInDistrict(unittest.TestCase):
+    def testPointInPoly(self):
+        convex = [(0,0), (2,0), (2,2), (0,2)]
+        self.assertEqual(edgesInDistricts.isWithin((1,1), convex), True)
+        self.assertEqual(edgesInDistricts.isWithin((-1,-1), convex), False)
+        self.assertEqual(edgesInDistricts.isWithin((3,3), convex), False)
+        self.assertEqual(edgesInDistricts.isWithin((1.5,1.5), convex), True)
+        concave = [(0,0), (2,0), (2,1), (1,1), (1,2), (0,2)]
+        self.assertEqual(edgesInDistricts.isWithin((0.5,0.5), concave), True)
+        self.assertEqual(edgesInDistricts.isWithin((1.5,1.5), concave), False)
+        
+
+if __name__ == '__main__':
+    unittest.main()
