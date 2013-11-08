9b782caa769cec93b4682bfcd6bb468072453cdd
Jakob Erdmann <jakob.erdmann@dlr.de>
2013-11-08 07:48:44 +0000

    (JE2013) introducing new feature 'furtherBlock' this avoids deadlocks when vehicles have to counter-lc across multiple lanes. also relaxing condtions for moving away from the best lane to the right on highways. This fixes the test stops/overtaking_highspeed. refs #1045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15066 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSLCM_JE2013.cpp
+++ sumo/src/microsim/MSLCM_JE2013.cpp
@@ -80,8 +80,10 @@
 
 #define URGENCY (SUMOReal)2.0 
 
-//#define DEBUG_COND (myVehicle.getID() == "1503_25640000" || myVehicle.getID() == "1502_25642222") 
-//#define DEBUG_COND (myVehicle.getID() == "overtaking_right")
+//#define DEBUG_COND (myVehicle.getID() == "1503_46013333" || myVehicle.getID() == "1501_46020000") // fail change to right
+//#define DEBUG_COND (myVehicle.getID() == "1502_46165263" || myVehicle.getID() == "1502_46114285") // fail change to right and change to left, deadlock?
+//#define DEBUG_COND (myVehicle.getID() == "1502_46117142") // fail change to left
+//#define DEBUG_COND (myVehicle.getID() == "overtaking_right") // test stops_overtaking
 #define DEBUG_COND false
 
 // debug function
@@ -528,9 +530,13 @@ MSLCM_JE2013::changed() {
     myLastLaneChangeOffset = 0;
     mySpeedGainProbability = 0;
     myKeepRightProbability = 0;
-    myLeadingBlockerLength = 0;
+    if (myVehicle.getBestLaneOffset() == 0) {
+        // if we are not yet on our best lane there might still be unseen blockers
+        // (during patchSpeed)
+        myLeadingBlockerLength = 0;
+        myLeftSpace = 0;
+    }
     myLookAheadSpeed = LOOK_AHEAD_MIN_SPEED;
-    myLeftSpace = 0;
     myVSafes.clear();
     myDontBrake = false;
 }
@@ -620,6 +626,11 @@ MSLCM_JE2013::_wantsChange(
     }
 
     ret = slowDownForBlocked(lastBlocked, ret);
+    // VARIANT_14 (furtherBlock)
+    if (lastBlocked != firstBlocked) {
+        ret = slowDownForBlocked(firstBlocked, ret);
+    }
+
 
     // we try to estimate the distance which is necessary to get on a lane
     //  we have to get on in order to keep our route
@@ -681,12 +692,12 @@ MSLCM_JE2013::_wantsChange(
             informFollower(msgPass, blocked, myLca, neighFollow, remainingSeconds, plannedSpeed);
         }
 
-        // there might be a vehicle which needs to counter-lane-change one lane further and we cannot see it yet
-        //if (changeToBest && abs(bestLaneOffset) > 1) {
-        //    // save at least his length in myLeadingBlockerLength
-        //    myLeadingBlockerLength = MAX2((SUMOReal)20.0, myLeadingBlockerLength);
-        //    // save the left space
-        //    myLeftSpace = currentDist - myVehicle.getPositionOnLane();
+        // VARIANT_14 (furtherBlock)
+        if (changeToBest && abs(bestLaneOffset) > 1) {
+            // there might be a vehicle which needs to counter-lane-change one lane further and we cannot see it yet
+            if (MSGlobals::gDebugFlag2) std::cout << "  reserving space for unseen blockers\n";
+            myLeadingBlockerLength = MAX2((SUMOReal)(right ? 20.0 : 40.0), myLeadingBlockerLength);
+        }
 
         //}
         // letting vehicles merge in at the end of the lane in case of counter-lane change, step#1
@@ -754,7 +765,7 @@ MSLCM_JE2013::_wantsChange(
     // let's also regard the case where the vehicle is driving on a highway...
     //  in this case, we do not want to get to the dead-end of an on-ramp
     if (right) {
-        if (bestLaneOffset == 0 && myVehicle.getLane()->getVehicleMaxSpeed(&myVehicle) > 80. / 3.6) {
+        if (bestLaneOffset == 0 && myVehicle.getLane()->getVehicleMaxSpeed(&myVehicle) > 80. / 3.6 && myLookAheadSpeed > SUMO_const_haltingSpeed) {
         if (MSGlobals::gDebugFlag2) std::cout << " veh=" << myVehicle.getID() << " does not want to get stranded on the on-ramp of a highway\n";
             return ret | LCA_STAY | LCA_STRATEGIC;
         }
@@ -923,7 +934,7 @@ MSLCM_JE2013::slowDownForBlocked(MSVehicle** blocked, int state) {
                             &myVehicle, myVehicle.getSpeed(), 
                             (SUMOReal)(gap - POSITION_EPS), (*blocked)->getSpeed(), 
                             (*blocked)->getCarFollowModel().getMaxDecel()));
-                (*blocked) = 0;
+                //(*blocked) = 0; // VARIANT_14 (furtherBlock)
             }
         }
     }
