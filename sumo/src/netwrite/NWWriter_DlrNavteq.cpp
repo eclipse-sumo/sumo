148e94cb5ea0d66abb8fbfd30dc5d325f9ecc941
Jakob Erdmann <jakob.erdmann@dlr.de>
2013-11-26 12:50:18 +0000

    fixing name clash between normal nodes and geometry nodes in elmar output
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15215 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netwrite/NWWriter_DlrNavteq.cpp
+++ sumo/src/netwrite/NWWriter_DlrNavteq.cpp
@@ -118,11 +118,13 @@ NWWriter_DlrNavteq::writeNodesUnsplitted(const OptionsCont& oc, NBNodeCont& nc, 
         NBEdge* e = (*i).second;
         const PositionVector& geom = e->getGeometry();
         if (geom.size() > 2) {
-            if (e->getID() == UNDEFINED) {
-                WRITE_WARNING("Edge id '" + UNDEFINED +
-                              "' clashes with the magic value for NO_BETWEEN_NODE. Internal geometry for this edge will be lost.");
+            std::string internalNodeID = e->getID();
+            if (internalNodeID == UNDEFINED ||
+                    (nc.retrieve(internalNodeID) != 0)) {
+                // need to invent a new name to avoid clashing with the id of a 'real' node or a reserved name
+                internalNodeID += "_geometry";
             }
-            device << e->getID() << "\t1\t" << geom.size() - 2;
+            device << internalNodeID << "\t1\t" << geom.size() - 2;
             for (size_t ii = 1; ii < geom.size() - 1; ++ii) {
                 Position pos = geom[(int)ii];
                 gch.cartesian2geo(pos);
