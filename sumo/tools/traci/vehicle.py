8d3197e96f03ea633906a807feefbdaf5bea3457
Laura Bieker <laura.bieker@dlr.de>
2013-12-09 13:59:44 +0000

    Traci: vehicle rerouting with current traveltimes, fixes refs #907
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15289 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/traci/vehicle.py
+++ sumo/tools/traci/vehicle.py
@@ -520,7 +520,16 @@ def setEffort(vehID, begTime, endTime, edgeID, effort):
     traci._message.string += struct.pack("!Bd", tc.TYPE_DOUBLE, effort)
     traci._sendExact()
 
-def rerouteTraveltime(vehID):
+def rerouteTraveltime(vehID, loadTravelTimes = False):
+    """rerouteTraveltime(string, bool) -> None
+    
+    Reroutes a vehicle according to the loaded travel times. If loadTravelTimes is False (default) 
+    then the travel times of a loaded weight file or the minimum travel time is used.
+    If loadTravelTimes is True then the current traveltime of the edges is loaded and used for rerouting.
+    """
+    if loadTravelTimes:
+        for edge in traci.edge.getIDList():
+            traci.edge.adaptTraveltime(edge, traci.edge.getTraveltime(edge)) 
     traci._beginMessage(tc.CMD_SET_VEHICLE_VARIABLE, tc.CMD_REROUTE_TRAVELTIME, vehID, 1+4)
     traci._message.string += struct.pack("!Bi", tc.TYPE_COMPOUND, 0)
     traci._sendExact()
