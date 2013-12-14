aafeeffa7e10b1148b38912a8769ca65d4df8638
Michael Behrisch <oss@behrisch.de>
2013-12-14 23:19:48 +0000

    working on output tests, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15325 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/sumo/output/runner.py
+++ sumo/tests/complex/sumo/output/runner.py
@@ -1,43 +1,73 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os,subprocess,sys
 from xml.sax import parse, handler
+from collections import defaultdict
 
 class OutputHandler(handler.ContentHandler):
-    e1speed = {}
-    e2speed = {}
-    e3speed = {}
-    edgespeed = {}
-    lanespeed = {}
-    intervals = set()
+    
+    def __init__(self, edges):
+        self.edges = edges
+        self.speed = dict([(l, defaultdict(dict)) for l in edges.itervalues()])
+        self.intervals = set()
 
     def startElement(self, name, attrs):
+        if "id" in attrs:
+            lane = attrs["id"][3:]
         if name == "interval":
             self.interval = (float(attrs["begin"]), float(attrs["end"]))
             self.intervals.add(self.interval)
             if "nVehContrib" in attrs:
-                self.e1speed[self.interval] = float(attrs["speed"])
-            if "meanSpeedWithin" in attrs:
-                self.e3speed[self.interval] = float(attrs["meanSpeedWithin"])
-                if self.e3speed[self.interval] == -1.:
-                    self.e3speed[self.interval] = float(attrs["meanSpeed"])
+                self.speed[lane]["e1"][self.interval] = float(attrs["speed"])
             if "nSamples" in attrs:
-                self.e2speed[self.interval] = float(attrs["meanSpeed"])
-        if name == "edge" and "id" in attrs and attrs["id"]=="3/1to2/1" and "speed" in attrs:
-            self.edgespeed[self.interval] = float(attrs["speed"])
-        if name == "lane" and "id" in attrs and attrs["id"]=="3/1to2/1_0" and "speed" in attrs:
-            self.lanespeed[self.interval] = float(attrs["speed"])
+                self.speed[lane]["e2"][self.interval] = float(attrs["meanSpeed"])
+            if "meanSpeedWithin" in attrs:
+                self.speed[lane]["e3"][self.interval] = float(attrs["meanSpeedWithin"])
+                if self.speed[lane]["e3"][self.interval] == -1.:
+                    self.speed[lane]["e3"][self.interval] = float(attrs["meanSpeed"])
+        if name == "edge" and "id" in attrs and attrs["id"] in self.edges and "speed" in attrs:
+            self.speed[edges[attrs["id"]]]["edge"][self.interval] = float(attrs["speed"])
+        if name == "lane" and "id" in attrs and attrs["id"] in self.edges.values() and "speed" in attrs:
+            self.speed[attrs["id"]]["lane"][self.interval] = float(attrs["speed"])
 
+def generateDetectorDef(out, freq, enableLoop, laneIDs):
+    print('<additional xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo-sim.org/xsd/additional_file.xsd">', file=out)
+    for laneId in laneIDs:
+        if enableLoop:
+            print('    <e1Detector id="e1_%s" lane="%s" pos="200" freq="%s" file="detector.xml"/>' % (laneId, laneId, freq), file=out)
+        print("""    <e2Detector id="e2_%s" lane="%s" pos="0" length="30000" friendlyPos="true" freq="%s" file="detector.xml"/>
+        <e3Detector id="e3_%s" freq="%s" file="detector.xml">
+            <detEntry lane="%s" pos="0"/>
+            <detExit lane="%s" pos="30000" friendlyPos="true"/>
+        </e3Detector>""" % (laneId, laneId, freq, laneId, freq, laneId, laneId), file=out)
+    print("""    <edgeData id="dump_15" freq="%s" file="meandataedge.xml" excludeEmpty="true"/>
+    <laneData id="dump_15" freq="%s" file="meandatalane.xml" excludeEmpty="true"/>
+</additional>""" % (freq, freq), file=out)
 
+withLoop = float(sys.argv[1]) > 50
+sumoArgStart = len(sys.argv)
+for idx, arg in enumerate(sys.argv):
+    if arg[0] == "-":
+        sumoArgStart = idx
+        break
+lanes = sys.argv[2:sumoArgStart]
+edges = dict([(l[:-2], l) for l in lanes])
+with open("input_additional.add.xml", 'w') as out:
+    generateDetectorDef(out, sys.argv[1], withLoop, lanes)
 sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', 'bin', 'sumo'))
-subprocess.call([sumoBinary]+sys.argv[1:], shell=(os.name=="nt"), stdout=sys.stdout, stderr=sys.stderr)
-handler = OutputHandler()
-for f in ["e1detector.xml", "e2detector.xml", "e3detector.xml", "meandataedge.xml", "meandatalane.xml"]:
+subprocess.call([sumoBinary, "-c", "sumo.sumocfg"] + sys.argv[sumoArgStart:], shell=(os.name=="nt"), stdout=sys.stdout, stderr=sys.stderr)
+handler = OutputHandler(edges)
+for f in ["detector.xml", "meandataedge.xml", "meandatalane.xml"]:
     if os.path.exists(f):
         parse(f, handler)
 
 for i in sorted(handler.intervals):
-    vals = [handler.e1speed.get(i, -1.), handler.e2speed.get(i, -1.), handler.e3speed.get(i, -1.), handler.edgespeed.get(i, -1.), handler.lanespeed.get(i, -1.)]
-    for v in vals[:-1]:
-        if abs(v - vals[-1]) > 0.001:
-            print i, vals
-            break
+    for lane in lanes:
+        if withLoop:
+            vals = [handler.speed[lane][type].get(i, -1.) for type in ["e1", "e2", "e3", "edge", "lane"]]
+        else:
+            vals = [handler.speed[lane][type].get(i, -1.) for type in ["e2", "e3", "edge", "lane"]]
+        for v in vals[:-1]:
+            if abs(v - vals[-1]) > 0.001:
+                print(lane, i, vals)
+                break
