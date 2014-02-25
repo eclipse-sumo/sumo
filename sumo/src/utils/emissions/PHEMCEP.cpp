5e5e0afec32b79c07fb0758e7f5238cd87e35b52
Michael Behrisch <oss@behrisch.de>
2014-02-25 08:47:10 +0000

    somehow an outdated PHEMCEP smuggled in, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15728 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/emissions/PHEMCEP.cpp
+++ sumo/src/utils/emissions/PHEMCEP.cpp
@@ -101,220 +101,206 @@ PHEMCEP::PHEMCEP(bool heavyVehicel, SUMOEmissionClass emissionClass,
             throw InvalidArgument("Error loading vehicle file for: " + SumoEmissionClassStrings.getString(emissionClass));
         }
 
-        // looping through matrix and assigning values for Fuel consumption
-        _cepCurveFC.clear();
-        for (int i = 0; i < matrixFC.size(); i++) {
-            if (matrixFC[i].size() != 2) {
-                throw InvalidArgument("Error loading vehicle file for: " + SumoEmissionClassStrings.getString(emissionClass));
+        _powerPatternFC.push_back(matrixFC[i][0] * _ratedPower);
+        _cepCurveFC.push_back(matrixFC[i][1]);
+
+    } // end for
+
+
+    // looping through matrix and assigning values for pollutants
+    double normalizingPower = 0;
+
+    if (heavyVehicel) {
+        normalizingPower = _ratedPower;
+    } else {
+        normalizingPower = PHEMCEP::CalcPower(NORMALIZING_SPEED, NORMALIZING_ACCELARATION, 0);
+    } // end if
+
+    int headerCount = headerLinePollutants.size();
+    for (int i = 0; i < (int)matrixPollutants.size(); i++) {
+        for (int j = 0; j < (int)matrixPollutants[i].size(); j++) {
+            if (matrixPollutants[i].size() != headerCount + 1) {
+                return;
             }
 
-            _powerPatternFC.push_back(matrixFC[i][0] * _ratedPower);
-            _cepCurveFC.push_back(matrixFC[i][1]);
+            if (j == 0) {
+                _powerPatternPollutants.push_back(matrixPollutants[i][j] * normalizingPower);
+            } else {
+                pollutantMeasures[j - 1].push_back(matrixPollutants[i][j]);
+            } // end if
+
 
         } // end for
+    } // end for
 
+    for (int i = 0; i < (int)headerLinePollutants.size(); i++) {
+        _cepCurvePollutants.insert(pollutantIdentifier[i], pollutantMeasures[i]);
+    } // end for
 
-        int headerCount = headerLinePollutants.size();
-        for (int i = 0; i < (int)matrixPollutants.size(); i++) {
-            for (int j = 0; j < (int)matrixPollutants[i].size(); j++) {
-                if (matrixPollutants[i].size() != headerCount + 1) {
-                    return;
-                }
-
-                if (heavyVehicel) {
-                    normalizingPower = _ratedPower;
-                } else {
-                    normalizingPower = PHEMCEP::CalcPower(NORMALIZING_SPEED, NORMALIZING_ACCELARATION, 0);
-                } // end if
-
-                int headerCount = headerLinePollutants.size();
-                for (int i = 0; i < matrixPollutants.size(); i++) {
-                    for (int j = 0; j < matrixPollutants[i].size(); j++) {
-                        if (matrixPollutants[i].size() != headerCount + 1) {
-                            return;
-                        }
-
-                        if (j == 0) {
-                            _powerPatternPollutants.push_back(matrixPollutants[i][j] * normalizingPower);
-                        } else {
-                            pollutantMeasures[j - 1].push_back(matrixPollutants[i][j]);
-                        } // end if
-
-                        for (int i = 0; i < (int)headerLinePollutants.size(); i++) {
-                            _cepCurvePollutants.insert(pollutantIdentifier[i], pollutantMeasures[i]);
-                        } // end for
-
-                    } // end for
-                } // end for
-
-                for (int i = 0; i < headerLinePollutants.size(); i++) {
-                    _cepCurvePollutants.insert(pollutantIdentifier[i], pollutantMeasures[i]);
-                } // end for
-
-            } // end of Cep
+} // end of Cep
 
 
 
-            PHEMCEP::~PHEMCEP() {
-                // free power pattern
-                _powerPatternFC.clear();
-                _powerPatternPollutants.clear();
-                _cepCurveFC.clear();
-                _speedCurveRotational.clear();
-                _speedPatternRotational.clear();
-            } // end of ~Cep
+PHEMCEP::~PHEMCEP() {
+    // free power pattern
+    _powerPatternFC.clear();
+    _powerPatternPollutants.clear();
+    _cepCurveFC.clear();
+    _speedCurveRotational.clear();
+    _speedPatternRotational.clear();
+} // end of ~Cep
 
-            double
-            PHEMCEP::CalcPower(double v, double a, double slope) const {
-                double power = 0;
+double
+PHEMCEP::CalcPower(double v, double a, double slope) const {
+    double power = 0;
 
-                double rotFactor = PHEMCEP::GetRotationalCoeffecient(v);
-                power += (_massVehicle + _vehicleLoading) * GRAVITY_CONST * (_resistanceF0 + _resistanceF1 * v + _resistanceF4 * pow(v, 4)) * v;
-                power += (_crossSectionalArea * _cwValue * AIR_DENSITY_CONST / 2) * pow(v, 3);
-                power += (_massVehicle * rotFactor + _massRot + _vehicleLoading) * a * v;
-                power += (_massVehicle + _vehicleLoading) * slope * 0.01 * v;
+    double rotFactor = PHEMCEP::GetRotationalCoeffecient(v);
+    power += (_massVehicle + _vehicleLoading) * GRAVITY_CONST * (_resistanceF0 + _resistanceF1 * v + _resistanceF4 * pow(v, 4)) * v;
+    power += (_crossSectionalArea * _cwValue * AIR_DENSITY_CONST / 2) * pow(v, 3);
+    power += (_massVehicle * rotFactor + _massRot + _vehicleLoading) * a * v;
+    power += (_massVehicle + _vehicleLoading) * slope * 0.01 * v;
 
-                return power / 950.;
-            }
+    return power / 950.;
+}
 
-            double
-            PHEMCEP::GetMaxAccel(double v, double a, double gradient) const {
-                double rotFactor = GetRotationalCoeffecient(v);
-                double pMaxNorm = GetPMaxNorm(v);
-                double pMaxForAcc = GetPMaxNorm(v) * _ratedPower - PHEMCEP::CalcPower(v, 0, gradient);
+double
+PHEMCEP::GetMaxAccel(double v, double a, double gradient) const {
+    double rotFactor = GetRotationalCoeffecient(v);
+    double pMaxNorm = GetPMaxNorm(v);
+    double pMaxForAcc = GetPMaxNorm(v) * _ratedPower - PHEMCEP::CalcPower(v, 0, gradient);
 
-                return (pMaxForAcc * 1000) / ((_massVehicle * rotFactor + _massRot + _vehicleLoading) * v);
+    return (pMaxForAcc * 1000) / ((_massVehicle * rotFactor + _massRot + _vehicleLoading) * v);
 
-            } // end of GetMaxAccel
+} // end of GetMaxAccel
 
-            double
-            PHEMCEP::GetEmission(const std::string & pollutant, double power) const {
-                std::vector<double> emissionCurve;
-                std::vector<double> powerPattern;
+double
+PHEMCEP::GetEmission(const std::string& pollutant, double power) const {
+    std::vector<double> emissionCurve;
+    std::vector<double> powerPattern;
 
-                if (pollutant == "FC") {
-                    emissionCurve = _cepCurveFC;
-                    powerPattern = _powerPatternFC;
-                } else {
-                    if (!_cepCurvePollutants.hasString(pollutant)) {
-                        throw InvalidArgument("Emission pollutant " + pollutant + " not found!");
-                    }
+    if (pollutant == "FC") {
+        emissionCurve = _cepCurveFC;
+        powerPattern = _powerPatternFC;
+    } else {
+        if (!_cepCurvePollutants.hasString(pollutant)) {
+            throw InvalidArgument("Emission pollutant " + pollutant + " not found!");
+        }
 
-                    emissionCurve = _cepCurvePollutants.get(pollutant);
-                    powerPattern = _powerPatternPollutants;
-                } // end if
-
-
-
-                if (emissionCurve.size() == 0) {
-                    throw InvalidArgument("Empty emission curve for " + pollutant + " found!");
-                }
-
-                if (emissionCurve.size() == 1) {
-                    return emissionCurve[0];
-                }
-
-                // in case that the demanded power is smaller than the first entry (smallest) in the power pattern the first two entries are extrapolated
-                if (power <= powerPattern.front()) {
-                    double calcEmission =  PHEMCEP::Interpolate(power, powerPattern[0], powerPattern[1], emissionCurve[0], emissionCurve[1]);
-
-                    if (calcEmission < 0) {
-                        return 0;
-                    } else {
-                        return calcEmission;
-                    }
-
-                } // end if
-
-                // if power bigger than all entries in power pattern the last two values are linearly extrapolated
-                if (power >= powerPattern.back()) {
-                    return PHEMCEP::Interpolate(power, powerPattern[powerPattern.size() - 2], powerPattern.back(), emissionCurve[emissionCurve.size() - 2], emissionCurve.back());
-                } // end if
-
-                // bisection search to find correct position in power pattern
-                int upperIndex;
-                int lowerIndex;
-
-                PHEMCEP::FindLowerUpperInPattern(lowerIndex, upperIndex, powerPattern, power);
-
-                return PHEMCEP::Interpolate(power, powerPattern[lowerIndex], powerPattern[upperIndex], emissionCurve[lowerIndex], emissionCurve[upperIndex]);
-
-            } // end of GetEmission
+        emissionCurve = _cepCurvePollutants.get(pollutant);
+        powerPattern = _powerPatternPollutants;
+    } // end if
 
 
-            double
-            PHEMCEP::Interpolate(double px, double p1, double p2, double e1, double e2) const {
-                if (p2 == p1) {
-                    return e1;
-                }
-                return e1 + (px - p1) / (p2 - p1) * (e2 - e1);
-            } // end of Interpolate
 
-            double
-            PHEMCEP::GetRotationalCoeffecient(double speed) const {
-                int upperIndex;
-                int lowerIndex;
+    if (emissionCurve.size() == 0) {
+        throw InvalidArgument("Empty emission curve for " + pollutant + " found!");
+    }
 
-                PHEMCEP::FindLowerUpperInPattern(lowerIndex, upperIndex, _speedPatternRotational, speed);
+    if (emissionCurve.size() == 1) {
+        return emissionCurve[0];
+    }
 
-                return PHEMCEP::Interpolate(speed,
-                                            _speedPatternRotational[lowerIndex],
-                                            _speedPatternRotational[upperIndex],
-                                            _speedCurveRotational[lowerIndex],
-                                            _speedCurveRotational[upperIndex]);
-            } // end of GetRotationalCoeffecient
+    // in case that the demanded power is smaller than the first entry (smallest) in the power pattern the first two entries are extrapolated
+    if (power <= powerPattern.front()) {
+        double calcEmission =  PHEMCEP::Interpolate(power, powerPattern[0], powerPattern[1], emissionCurve[0], emissionCurve[1]);
 
-            void
-            PHEMCEP::FindLowerUpperInPattern(int & lowerIndex, int & upperIndex, std::vector<double> pattern, double value) const {
-                if (value <= pattern.front()) {
-                    lowerIndex = 0;
-                    upperIndex = 0;
-                    return;
+        if (calcEmission < 0) {
+            return 0;
+        } else {
+            return calcEmission;
+        }
 
-                } // end if
+    } // end if
 
-                if (value >= pattern.back()) {
-                    lowerIndex = pattern.size() - 1;
-                    upperIndex = pattern.size() - 1;
-                    return;
-                } // end if
+    // if power bigger than all entries in power pattern the last two values are linearly extrapolated
+    if (power >= powerPattern.back()) {
+        return PHEMCEP::Interpolate(power, powerPattern[powerPattern.size() - 2], powerPattern.back(), emissionCurve[emissionCurve.size() - 2], emissionCurve.back());
+    } // end if
 
-                // bisection search to find correct position in power pattern
-                int middleIndex = (pattern.size() - 1) / 2;
-                upperIndex = pattern.size() - 1;
-                lowerIndex = 0;
+    // bisection search to find correct position in power pattern
+    int upperIndex;
+    int lowerIndex;
 
-                while (upperIndex - lowerIndex > 1) {
-                    if (pattern[middleIndex] == value) {
-                        lowerIndex = middleIndex;
-                        upperIndex = middleIndex;
-                        return;
-                    } else if (pattern[middleIndex] < value) {
-                        lowerIndex = middleIndex;
-                        middleIndex = (upperIndex - lowerIndex) / 2 + lowerIndex;
-                    } else {
-                        upperIndex = middleIndex;
-                        middleIndex = (upperIndex - lowerIndex) / 2 + lowerIndex;
-                    } // end if
-                } // end while
+    PHEMCEP::FindLowerUpperInPattern(lowerIndex, upperIndex, powerPattern, power);
 
-                if (pattern[lowerIndex] <= value && value < pattern[upperIndex]) {
-                    return;
-                } else {
-                    throw ProcessError("Error during calculation of position in pattern!");
-                }
-            } // end of FindLowerUpperInPattern
+    return PHEMCEP::Interpolate(power, powerPattern[lowerIndex], powerPattern[upperIndex], emissionCurve[lowerIndex], emissionCurve[upperIndex]);
+
+} // end of GetEmission
 
 
-            double PHEMCEP::GetPMaxNorm(double speed) const {
-                // Linear function between v0 and v1, constant elsewhere
-                if (speed <= _pNormV0) {
-                    return _pNormP0;
-                } else if (speed >= _pNormV1) {
-                    return _pNormP1;
-                } else {
-                    return PHEMCEP::Interpolate(speed, _pNormV0, _pNormV1, _pNormP0, _pNormP1);
-                }
-            } // end of GetPMaxNorm
+double
+PHEMCEP::Interpolate(double px, double p1, double p2, double e1, double e2) const {
+    if (p2 == p1) {
+        return e1;
+    }
+    return e1 + (px - p1) / (p2 - p1) * (e2 - e1);
+} // end of Interpolate
 
-            /****************************************************************************/
+double
+PHEMCEP::GetRotationalCoeffecient(double speed) const {
+    int upperIndex;
+    int lowerIndex;
+
+    PHEMCEP::FindLowerUpperInPattern(lowerIndex, upperIndex, _speedPatternRotational, speed);
+
+    return PHEMCEP::Interpolate(speed,
+                                _speedPatternRotational[lowerIndex],
+                                _speedPatternRotational[upperIndex],
+                                _speedCurveRotational[lowerIndex],
+                                _speedCurveRotational[upperIndex]);
+} // end of GetRotationalCoeffecient
+
+void
+PHEMCEP::FindLowerUpperInPattern(int& lowerIndex, int& upperIndex, std::vector<double> pattern, double value) const {
+    if (value <= pattern.front()) {
+        lowerIndex = 0;
+        upperIndex = 0;
+        return;
+
+    } // end if
+
+    if (value >= pattern.back()) {
+        lowerIndex = pattern.size() - 1;
+        upperIndex = pattern.size() - 1;
+        return;
+    } // end if
+
+    // bisection search to find correct position in power pattern
+    int middleIndex = (pattern.size() - 1) / 2;
+    upperIndex = pattern.size() - 1;
+    lowerIndex = 0;
+
+    while (upperIndex - lowerIndex > 1) {
+        if (pattern[middleIndex] == value) {
+            lowerIndex = middleIndex;
+            upperIndex = middleIndex;
+            return;
+        } else if (pattern[middleIndex] < value) {
+            lowerIndex = middleIndex;
+            middleIndex = (upperIndex - lowerIndex) / 2 + lowerIndex;
+        } else {
+            upperIndex = middleIndex;
+            middleIndex = (upperIndex - lowerIndex) / 2 + lowerIndex;
+        } // end if
+    } // end while
+
+    if (pattern[lowerIndex] <= value && value < pattern[upperIndex]) {
+        return;
+    } else {
+        throw ProcessError("Error during calculation of position in pattern!");
+    }
+} // end of FindLowerUpperInPattern
+
+
+double PHEMCEP::GetPMaxNorm(double speed) const {
+    // Linear function between v0 and v1, constant elsewhere
+    if (speed <= _pNormV0) {
+        return _pNormP0;
+    } else if (speed >= _pNormV1) {
+        return _pNormP1;
+    } else {
+        return PHEMCEP::Interpolate(speed, _pNormV0, _pNormV1, _pNormP0, _pNormP1);
+    }
+} // end of GetPMaxNorm
+
+/****************************************************************************/
