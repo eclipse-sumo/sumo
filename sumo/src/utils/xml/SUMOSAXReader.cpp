c00f31ac4a30331016def923fdbbdda782e753ee
Michael Behrisch <oss@behrisch.de>
2014-03-02 12:33:43 +0000

    checking for old website references when doing xml schema lookups, refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15781 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/xml/SUMOSAXReader.cpp
+++ sumo/src/utils/xml/SUMOSAXReader.cpp
@@ -217,7 +217,7 @@ SUMOSAXReader::LocalSchemaResolver::resolveEntity(const XMLCh* const /* publicId
     const std::string::size_type pos = url.rfind("/");
     if (pos != std::string::npos) {
         const std::string dir = url.substr(0, pos);
-        if (dir == "http://sumo-sim.org/xsd") {
+        if (dir == "http://sumo-sim.org/xsd" || dir == "http://sumo.sf.net/xsd") {
             const char* sumoPath = std::getenv("SUMO_HOME");
             if (sumoPath == 0) {
                 WRITE_WARNING("Environment variable SUMO_HOME is not set, schema resolution will use slow website lookups.");
