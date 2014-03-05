ab9fd5c8d4cab5b876a91917e35d68dc9c3f1dd5
Michael Behrisch <oss@behrisch.de>
2014-03-05 22:17:06 +0000

    new xsd location, refs #563
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15839 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/xml/rebuildSchemata.py
+++ sumo/tools/xml/rebuildSchemata.py
@@ -21,6 +21,6 @@ homeDir = os.environ.get("SUMO_HOME", os.path.dirname(os.path.dirname(os.path.di
 binDir = os.environ.get("SUMO_BINDIR", os.path.join(homeDir, "bin"))
 for exe in "activitygen dfrouter duarouter marouter jtrrouter netconvert netgenerate od2trips polyconvert sumo".split():
     if os.path.exists(os.path.join(binDir, exe)):
-        subprocess.call([os.path.join(binDir, exe), "--save-schema", os.path.join(homeDir, "docs", "internet", "xsd" , exe+"Configuration.xsd")])
+        subprocess.call([os.path.join(binDir, exe), "--save-schema", os.path.join(homeDir, "data", "xsd" , exe+"Configuration.xsd")])
     else:
         print "Warning! %s not found." % exe
