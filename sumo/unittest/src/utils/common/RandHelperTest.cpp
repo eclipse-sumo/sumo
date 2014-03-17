1973f7cff04ac171f0c2b731fc744f8569714396
Christian Schmidt <ch.schmidt@dlr.de>
2014-03-17 12:15:55 +0000

    fixing file headers, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15953 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/unittest/src/utils/common/RandHelperTest.cpp
+++ sumo/unittest/src/utils/common/RandHelperTest.cpp
@@ -6,13 +6,14 @@
 ///
 // Tests the class RandHelper
 /****************************************************************************/
-// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
-// Copyright 2001-2014 DLR (http://www.dlr.de/) and contributors
+// SUMO, Simulation of Urban MObility; see http://sumo-sim.org/
+// Copyright (C) 2001-2014 DLR (http://www.dlr.de/) and contributors
 /****************************************************************************/
 //
-//   This program is free software; you can redistribute it and/or modify
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 2 of the License, or
+//   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
 //
 /****************************************************************************/
