ab38a013bf3d18bfbea8e3b59170d52291e699db
Christian Schmidt <ch.schmidt@dlr.de>
2014-03-18 12:07:14 +0000

    fixing file headers #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@15964 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/sumolib/miscutils.py
+++ sumo/tools/sumolib/miscutils.py
@@ -7,15 +7,14 @@
 
 Common utility functions
 
-Copyright (C) 2007-2014 DLR/FS, Germany
+SUMO, Simulation of Urban MObility; see http://sumo-sim.org/
+Copyright (C) 2008-2014 DLR (http://www.dlr.de/) and contributors
 
 This file is part of SUMO.
 SUMO is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
-
-This is a duplicate of tools/util/miscutils.py from the VABENE repository
 """
 import sys
 import time
