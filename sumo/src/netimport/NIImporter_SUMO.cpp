bbacd091f87599e28066a150e909b9a750a585af
Jakob Erdmann <jakob.erdmann@dlr.de>
2014-05-06 06:18:02 +0000

    fixing debug build refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16294 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netimport/NIImporter_SUMO.cpp
+++ sumo/src/netimport/NIImporter_SUMO.cpp
@@ -341,7 +341,7 @@ NIImporter_SUMO::myEndElement(int element) {
         case SUMO_TAG_JUNCTION:
             if (myCurrentJunction.node != 0) {
                 assert(myCurrentJunction.intLanes.size() > 0);
-                assert(myCurrentJunction.intLanes.size() == myCurrentJunction.foes.size());
+                assert(myCurrentJunction.intLanes.size() == myCurrentJunction.response.size());
                 std::vector<Crossing>& crossings = myPedestrianCrossings[myCurrentJunction.node->getID()];
                 for (std::vector<Crossing>::iterator it = crossings.begin(); it != crossings.end(); ++it) {
                     for (int i = 0; i < myCurrentJunction.intLanes.size(); ++i) {
