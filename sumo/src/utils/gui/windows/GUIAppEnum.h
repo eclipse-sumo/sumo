72f419a8c927fb143854ccb9d82bb61f4825a6ea
Jakob Erdmann <jakob.erdmann@dlr.de>
2014-06-10 06:34:18 +0000

    adding message id refs #457
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16564 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/gui/windows/GUIAppEnum.h
+++ sumo/src/utils/gui/windows/GUIAppEnum.h
@@ -417,6 +417,8 @@ enum {
     MID_GNE_SPLIT_EDGE_BIDI,
     /** reverse an edge */
     MID_GNE_REVERSE_EDGE,
+    /** edit junction shape */
+    MID_GNE_NODE_SHAPE,
     /** set non-default geometry endpoint */
     MID_GNE_SET_EDGE_ENDPOINT,
     /** restore geometry endpoint to node position */
