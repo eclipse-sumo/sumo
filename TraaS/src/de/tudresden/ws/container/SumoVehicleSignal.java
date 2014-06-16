792909b0aa7f35e11d1ce02338b3bf1181c12874
Mario Krumnow <mario.krumnow.de@gmail.com>
2014-06-16 18:09:24 +0000

    added some additional classes for the signal states of a vehicle

---
+++ TraaS/src/de/tudresden/ws/container/SumoVehicleSignal.java
@@ -0,0 +1,49 @@
+package de.tudresden.ws.container;
+
+import java.util.LinkedList;
+
+public class SumoVehicleSignal {
+
+	
+	/**
+	 * 
+	 * @author Mario Krumnow
+	 * @author Anja Liebscher
+	 *
+	 */
+	
+	LinkedList<Integer> ll_states;
+	
+	public SumoVehicleSignal(int code){
+		
+		String s1 = this.getDual(code);
+		String[] tmp = s1.split("");
+		
+		//init
+		this.ll_states = new LinkedList<Integer>();
+		for(int i=0; i<14; i++){this.ll_states.add(0);}
+
+		for(int i = tmp.length-1; i>0; i--){
+			int pos = tmp.length-i-1;
+			this.ll_states.set(pos, Integer.valueOf(tmp[i]));		
+		}
+		
+	}
+	
+	public boolean getState(SumoVehicleSignalState s){
+		
+		boolean out = false;
+		if(this.ll_states.get(s.getPos()) == 1){out = true;}
+		return out;
+		
+	}
+	
+	
+	private String getDual(int code) {
+		if (code < 2) {return "" + code;} 
+		else {return getDual(code / 2) + code % 2;
+	
+		}
+	}
+	
+}
