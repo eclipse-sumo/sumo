839901a0ba2c0bfa060c0d2c4428503410c11797
Michael Behrisch <oss@behrisch.de>
2014-06-21 11:37:34 +0000

    building history, refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16650 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/build/history.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+@file    history.py
+@author  Michael Behrisch
+@date    2014-06-21
+@version $Id: pythonPropsMSVC.py 15692 2014-02-22 09:17:02Z behrisch $
+
+This script builds all sumo versions in a certain revision range
+and tries to eliminate duplicates afterwards.
+
+SUMO, Simulation of Urban MObility; see http://sumo-sim.org/
+Copyright (C) 2011-2014 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+import subprocess, optparse, shutil, os
+
+optParser = optparse.OptionParser()
+optParser.add_option("-b", "--begin", type="int",
+                     default=10000, help="start revision")
+optParser.add_option("-e", "--end", type="int",
+                     default=100000, help="end revision")
+optParser.add_option("-s", "--step", type="int",
+                     default=1, help="increment")
+options, args = optParser.parse_args()
+
+for rev in range(options.begin, options.end, options.step):
+    if not os.path.exists('bin%s' % rev):
+        ret = subprocess.call('svn up --ignore-externals -r %s sumo' % rev, shell=True)
+        if ret != 0:
+            break
+        ret = subprocess.call('cd sumo; make -j 16; cd ..', shell=True)
+        shutil.copytree('sumo/bin', 'bin%s' % rev)
+for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
+    dups = line.split()
+    for d in dups[1:]:
+        subprocess.call('ln -sf ../%s %s' % (dups[0], d), shell=True)
