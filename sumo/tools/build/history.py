266b523fe693df93900ae392d7aa59bf1e1aa36b
Michael Behrisch <oss@behrisch.de>
2014-06-24 13:20:15 +0000

    history script refinement, refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16657 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/history.py
+++ sumo/tools/build/history.py
@@ -22,7 +22,7 @@ import subprocess, optparse, shutil, os
 
 optParser = optparse.OptionParser()
 optParser.add_option("-b", "--begin", type="int",
-                     default=10000, help="start revision")
+                     default=16000, help="start revision")
 optParser.add_option("-e", "--end", type="int",
                      default=100000, help="end revision")
 optParser.add_option("-s", "--step", type="int",
@@ -34,7 +34,9 @@ for rev in range(options.begin, options.end, options.step):
         ret = subprocess.call('svn up --ignore-externals -r %s sumo' % rev, shell=True)
         if ret != 0:
             break
-        ret = subprocess.call('cd sumo; make -j 16; cd ..', shell=True)
+        subprocess.call('cd sumo; make clean; make -j 16; cd ..', shell=True)
+        subprocess.call('strip -R .note.gnu.build-id sumo/bin/*', shell=True)
+        subprocess.call("sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' sumo/bin/*" % rev, shell=True)
         shutil.copytree('sumo/bin', 'bin%s' % rev)
 for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
     dups = line.split()
