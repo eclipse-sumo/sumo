b6fee430158d12479fdc208b782362d823b3e055
Michael Behrisch <oss@behrisch.de>
2014-06-24 15:32:53 +0000

    less error messages from build history
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16660 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/history.py
+++ sumo/tools/build/history.py
@@ -22,14 +22,19 @@ import subprocess, optparse, shutil, os
 
 optParser = optparse.OptionParser()
 optParser.add_option("-b", "--begin", type="int",
-                     default=16000, help="start revision")
+                     default=16000, help="first revision to build")
 optParser.add_option("-e", "--end", type="int",
-                     default=100000, help="end revision")
+                     help="last revision to build")
 optParser.add_option("-s", "--step", type="int",
                      default=1, help="increment")
 options, args = optParser.parse_args()
 
-for rev in range(options.begin, options.end, options.step):
+if not options.end:
+    for line in subprocess.check_output('svn info http://svn.code.sf.net/p/sumo/code/trunk/sumo', shell=True).splitlines():
+        l = line.split()
+        if len(l) == 2 and l[0] == "Revision:":
+            options.end = int(l[1])
+for rev in range(options.begin, options.end+1, options.step):
     if not os.path.exists('bin%s' % rev):
         ret = subprocess.call('svn up --ignore-externals -r %s sumo' % rev, shell=True)
         if ret != 0:
@@ -37,7 +42,7 @@ for rev in range(options.begin, options.end, options.step):
         subprocess.call('cd sumo; make clean; make -j 16; cd ..', shell=True)
         subprocess.call('strip -R .note.gnu.build-id sumo/bin/*', shell=True)
         subprocess.call("sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' sumo/bin/*" % rev, shell=True)
-        shutil.copytree('sumo/bin', 'bin%s' % rev)
+        shutil.copytree('sumo/bin', 'bin%s' % rev, ignore=shutil.ignore_patterns('Makefile*', '*.bat'))
 for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
     dups = line.split()
     for d in dups[1:]:
