6bfe9dea07907a9605f182da62a00fa022b052ac
Jakob Erdmann <jakob.erdmann@dlr.de>
2014-08-01 07:05:18 +0000

    fixed angle of parking vehicles refs #36
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16810 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSVehicle.cpp
+++ sumo/src/microsim/MSVehicle.cpp
@@ -659,6 +659,9 @@ SUMOReal
 MSVehicle::getAngle() const {
     Position p1;
     Position p2;
+    if (isParking()) {
+        return -myLane->getShape().rotationDegreeAtOffset(myLane->interpolateLanePosToGeometryPos(getPositionOnLane()));
+    }
     if (getLaneChangeModel().isChangingLanes()) {
         // cannot use getPosition() because it already includes the offset to the side and thus messes up the angle
         p1 = myLane->geometryPositionAtOffset(myState.myPos);
