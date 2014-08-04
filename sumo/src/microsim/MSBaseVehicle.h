759b6e54e7b396c28927bedca63aad5c8fdaab8e
Jakob Erdmann <jakob.erdmann@dlr.de>
2014-08-04 11:41:50 +0000

    fixed docstring refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16829 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSBaseVehicle.h
+++ sumo/src/microsim/MSBaseVehicle.h
@@ -172,7 +172,7 @@ public:
     /** @brief Returns the slope of the road at vehicle's position
      *
      * This default implementation returns always 0.
-     * @return The acceleration
+     * @return The slope
      */
     virtual SUMOReal getSlope() const;
 
