972c74030f36845fe5eed1aba0c206406469cd48
Michael Behrisch <oss@behrisch.de>
2014-08-28 12:29:15 +0000

    adding resume test, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@16955 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/resume/runner.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+@file    sumoWhisperResume.py
+@author  Marek Heinrich
+@date    2011-08-28
+
+Python resume test.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+import subprocess
+import os, sys
+
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path += [tools, os.path.join(tools, 'assign')]
+    import sumolib, traci
+    import traci.constants as tc
+    
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+    
+# the port used for communicating with your sumo instance
+PORT_TRACI = 8873
+
+
+
+
+def init_traci():
+
+    sumoBinary = sumolib.checkBinary('sumo')
+#    sumoBinary = sumolib.checkBinary('sumo-gui')
+    
+    sumo_call = [sumoBinary, "-c", "data/hello.sumocfg",
+                "--remote-port", str(PORT_TRACI),
+                "--netstate-dump", "rawdump.xml",
+                "--no-step-log",
+                "-v",
+    ]
+    
+    sumoProcess = subprocess.Popen(sumo_call, stdout=sys.stdout, stderr=sys.stderr)
+    
+    traci.init(PORT_TRACI)
+
+    
+def main(args):
+    init_traci()
+    step = -1
+    while True:
+        step += 1
+
+        traci.simulationStep()
+
+        if step == 140:
+            traci.vehicle.resume('Stapler_00')
+                                        
+        if step == 160:
+            traci.close()
+            break
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
