2916726de90866f6aae35ca92578dc42f17958c3
Michael Behrisch <oss@behrisch.de>
2014-09-17 10:17:52 +0000

    undoing assumed astar speedup, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@17074 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/vehicle/AStarRouter.h
+++ sumo/src/utils/vehicle/AStarRouter.h
@@ -202,6 +202,8 @@ public:
             }
             minimumInfo->visited = true;
             const SUMOReal traveltime = minimumInfo->traveltime + this->getEffort(minEdge, vehicle, time + minimumInfo->traveltime);
+            // admissible A* heuristic: straight line distance at maximum speed 
+            const SUMOReal heuristic_remaining = myLookupTable == 0 ? minEdge->getDistanceTo(to) / vehicle->getMaxSpeed() : (*myLookupTable)[minEdge->getNumericalID()][to->getNumericalID()] / vehicle->getChosenSpeedFactor();
             // check all ways from the node with the minimal length
             unsigned int i = 0;
             const unsigned int length_size = minEdge->getNumSuccessors();
@@ -215,6 +217,9 @@ public:
                 const SUMOReal oldEffort = followerInfo->traveltime;
                 if (!followerInfo->visited && traveltime < oldEffort) {
                     followerInfo->traveltime = traveltime;
+                    followerInfo->heuristicTime = traveltime + heuristic_remaining;
+                    /* the code below results in fewer edges being looked up but is more costly due to the effort
+                       calculations. Overall it resulted in a slowdown in the Berlin tests but could be made configurable someday.
                     followerInfo->heuristicTime = traveltime;
                     if (follower != to) {
                         if (myLookupTable == 0) {
@@ -223,7 +228,7 @@ public:
                         } else {
                             followerInfo->heuristicTime += this->getEffort(follower, vehicle, time + traveltime) + (*myLookupTable)[follower->getNumericalID()][to->getNumericalID()] / vehicle->getChosenSpeedFactor();
                         }
-                    }
+                    }*/
                     followerInfo->prev = minimumInfo;
                     if (oldEffort == std::numeric_limits<SUMOReal>::max()) {
                         myFrontierList.push_back(followerInfo);
