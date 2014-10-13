a537512041c1d19528e11a66ded0ccdbf9b47907
Michael Behrisch <oss@behrisch.de>
2014-10-13 04:49:48 +0000

    improving emission plot
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@17183 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/emissions/evaluateHBEFA3vsPHEM.py
+++ sumo/tests/complex/emissions/evaluateHBEFA3vsPHEM.py
@@ -23,8 +23,8 @@ import matplotlib.pyplot as plt
 sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', "tools"))
 
 ec = None
-hbefa = collections.defaultdict(dict)
-phem = collections.defaultdict(dict)
+hbefa = collections.defaultdict(lambda:collections.defaultdict(list))
+phem = collections.defaultdict(lambda:collections.defaultdict(list))
 maxVal = collections.defaultdict(float)
 for line in open('nefz_hbefa3/output.complex'):
     l = line.split(":")
@@ -32,7 +32,7 @@ for line in open('nefz_hbefa3/output.complex'):
         ec = line.split('/')[1][:-2]
     elif len(l) > 1:
         val = float(l[1])
-        hbefa[ec][l[0]] = val
+        hbefa[ec][l[0]].append(val)
         maxVal[l[0]] = max(maxVal[l[0]], val)
 for line in open('nefz_phemlightV3/output.complex'):
     l = line.split(":")
@@ -40,26 +40,38 @@ for line in open('nefz_phemlightV3/output.complex'):
         ec = line.split('/')[1][:-2]
         if ec[:3] == "PKW":
             ec = "PC" + ec[3:]
+        if ec[:3] == "LNF":
+            ec = "LDV" + ec[3:ec.rfind("_")]
     elif len(l) > 1:
         val = float(l[1])
-        phem[ec][l[0]] = val
+        phem[ec][l[0]].append(val)
+        if ec in hbefa and len(hbefa[ec][l[0]]) < len(phem[ec][l[0]]):
+            hbefa[ec][l[0]].append(hbefa[ec][l[0]][0])
         maxVal[l[0]] = max(maxVal[l[0]], val)
 
-marker = {"PC_G": "s", "PC_D": "^"}
+marker = {"PC_G_": "s", "PC_D_": "^", "LDV_G": "o", "LDV_D": "v"}
 color = {"0": "k", "1": "r", "2": "y", "3": "g", "4": "m", "5": "b", "6": "c"}
-limit = {"CO": 100000, "CO2": 5e6}
-        
-for idx, emission in enumerate(["CO", "CO2"]):
+limit = {"CO": 100000, "CO2": 5e6, "HC": 30000, "PMx": 6000, "fuel": 2000}
+
+for emission in ["CO", "CO2", "HC", "PMx", "fuel"]:
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    plt.xlim(0, limit[emission])
-    plt.ylim(0, limit[emission])
+    if emission in limit:
+        m = limit[emission]
+    else:
+        m = maxVal[emission]
+    plt.xlim(0, m)
+    plt.ylim(0, m)
     for ec in sorted(phem.iterkeys()):
-        if ec in hbefa and hbefa[ec][emission] > 0:
-            print(ec, [phem[ec][emission]], [hbefa[ec][emission]])
-            ax1.scatter([phem[ec][emission]], [hbefa[ec][emission]], s=30, c=color[ec[-1]], marker=marker[ec[:4]], label=ec)
-    ax1.plot((0, limit[emission]), (0, limit[emission]))
+        if ec in hbefa and hbefa[ec][emission][0] > 0:
+            print(ec, phem[ec][emission], hbefa[ec][emission])
+            ax1.scatter(phem[ec][emission], hbefa[ec][emission], s=30, c=color[ec[-1]], marker=marker[ec[:5]], label=ec)
+    ax1.plot((0, m), (0, m))
+    plt.title(emission)
     plt.xlabel("PHEMlight")
     plt.ylabel("HBEFA3")
-    plt.legend(scatterpoints=1, fontsize=8)
-    plt.savefig(emission+".png")
+    plt.legend(scatterpoints=1, fontsize=8, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig(emission+".png", bbox_inches="tight")
+    plt.xlim(0, m/5)
+    plt.ylim(0, m/5)
+    plt.savefig(emission+"Zoom.png", bbox_inches="tight")
