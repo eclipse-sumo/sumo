79f0734e75c124a13bec616d8c7f8a28e3fbb055
Michael Behrisch <oss@behrisch.de>
2015-02-03 15:38:01 +0000

    make it easier to run single instances, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@17786 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/routes/runner.py
+++ sumo/tests/complex/routes/runner.py
@@ -20,47 +20,78 @@ the Free Software Foundation; either version 3 of the License, or
 
 from __future__ import print_function
 import os, subprocess, sys
+from optparse import OptionParser, BadOptionError, AmbiguousOptionError
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', "tools"))
 from sumolib import checkBinary
 
-if sys.argv[1] == "sumo":
+class PassThroughOptionParser(OptionParser):
+    """
+    An unknown option pass-through implementation of OptionParser.
+    see http://stackoverflow.com/questions/1885161/how-can-i-get-optparses-optionparser-to-ignore-invalid-options
+
+    When unknown arguments are encountered, bundle with largs and try again,
+    until rargs is depleted.  
+
+    sys.exit(status) will still be called if a known argument is passed
+    incorrectly (e.g. missing arguments or bad argument types, etc.)        
+    """
+    def _process_args(self, largs, rargs, values):
+        while rargs:
+            try:
+                OptionParser._process_args(self,largs,rargs,values)
+            except (BadOptionError,AmbiguousOptionError), e:
+                largs.append(e.opt_str)
+
+def runInstance(elem, attrSet, childSet, depart):
+    print(elem, attrSet, childSet)
+    sys.stdout.flush()
+    with open("routes.xml", "w") as routes:
+        routes.write('<routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">\n    <route id="a" edges="1fi 1si 2o 2fi 2si"/>\n    <%s id="v" %s' % (elem, depart))
+        for idx, attr in enumerate(attrs):
+            if attrSet & idx:
+                routes.write(attr)
+        routes.write('>\n')
+        for idx, child in enumerate(childs):
+            if childSet & idx:
+                routes.write(child)
+        routes.write('    </%s>\n</routes>\n' % elem)
+    retCode = subprocess.call(call + ["-n", "input_net.net.xml", "-r", routes.name], stdout=sys.stdout, stderr=sys.stdout)
+    if retCode < 0:
+        sys.stdout.write(open(routes.name).read())
+        sys.exit()
+
+optParser = PassThroughOptionParser()
+optParser.add_option("-e", "--element", help="xml element to choose")
+optParser.add_option("-a", "--attr", type="int", default=0, help="attribute set to use")
+optParser.add_option("-c", "--child", type="int", default=0, help="child set to use")
+options, args = optParser.parse_args()
+
+if len(args) == 0 or args[0] == "sumo":
     call = [checkBinary('sumo'), "--no-step-log", "--no-duration-log",
             "-a", "input_additional.add.xml"]
-elif sys.argv[1] == "dfrouter":
+elif args[0] == "dfrouter":
     call = [checkBinary('dfrouter'),
             "--detector-files", "input_additional.add.xml"]
-elif sys.argv[1] == "duarouter":
+elif args[0] == "duarouter":
     call = [checkBinary('duarouter'), "--no-step-log",
             "-o", "dummy.xml", "-d", "input_additional.add.xml"]
-elif sys.argv[1] == "jtrrouter":
+elif args[0] == "jtrrouter":
     call = [checkBinary('jtrrouter'), "--no-step-log",
             "-o", "dummy.xml", "-a", "input_additional.add.xml"]
 else:
     print >> sys.stderr, "Unsupported application defined"
-call += sys.argv[2:]
+call += args[1:]
 
-elements = [('vehicle', 'depart="0"'), ('flow', 'begin="0" end="1" number="1"'), ('trip', 'depart="0"')]
+elements = {'vehicle': 'depart="0"', 'flow': 'begin="0" end="1" number="1"', 'trip': 'depart="0"'}
 attrs = [' from="1fi"', ' to="2si"', ' fromTaz="1"', ' toTaz="2"', ' route="a"']
 childs = ['<route edges="1fi 1si 2o 2fi 2si"/>\n', '<stop lane="1fi_0" duration="10"/>\n',
           '<stop lane="1si_0" duration="10"/>\n', '<stop lane="2si_0" duration="10"/>\n']
 
 # check route processing
-for elem, depart in elements:
-    for attrSet in range(2 ** len(attrs)):
-        for childSet in range(2 ** len(childs)):
-            print(elem, attrSet, childSet)
-            sys.stdout.flush()
-            with open("routes.xml", "w") as routes:
-                routes.write('<routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">\n    <route id="a" edges="1fi 1si 2o 2fi 2si"/>\n    <%s id="v" %s' % (elem, depart))
-                for idx, attr in enumerate(attrs):
-                    if attrSet & idx:
-                        routes.write(attr)
-                routes.write('>\n')
-                for idx, child in enumerate(childs):
-                    if childSet & idx:
-                        routes.write(child)
-                routes.write('    </%s>\n</routes>\n' % elem)
-            retCode = subprocess.call(call + ["-n", "input_net.net.xml", "-r", routes.name], stdout=sys.stdout, stderr=sys.stdout)
-            if retCode < 0:
-                sys.stdout.write(open(routes.name).read())
-                sys.exit()
+if options.element:
+    runInstance(options.element, options.attr, options.child, elements[options.element])
+else:
+    for elem, depart in sorted(elements.iteritems()):
+        for attrSet in range(2 ** len(attrs)):
+            for childSet in range(2 ** len(childs)):
+                runInstance(elem, attrSet, childSet, depart)
