466bcb27d30ffdabf012131e9d40e439604b561f
Jakob Erdmann <jakob.erdmann@dlr.de>
2015-02-06 11:54:49 +0000

    added test for person api refs #1596
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@17804 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/person/runner.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Jakob Erdmann
+@date    2015-02-06
+@version $Id: runner.py 17235 2014-11-03 10:53:02Z behrisch $
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2014 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+import os, subprocess, sys, random
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "..", "..", "..", "..", "..", "tools"))
+import traci, sumolib
+
+sumoBinary = sumolib.checkBinary('sumo')
+
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen("%s -c sumo.sumocfg --remote-port %s" % (sumoBinary, PORT), shell=True, stdout=sys.stdout)
+traci.init(PORT)
+
+def step():
+    s = traci.simulation.getCurrentTime() / 1000
+    traci.simulationStep()
+    return s
+
+for i in range(3):
+    print "step", step()
+
+def check(personID):
+    print "persons", traci.person.getIDList()
+    print "person count", traci.person.getIDCount()
+    print "examining", personID
+    print "speed", traci.person.getSpeed(personID)
+    print "pos", traci.person.getPosition(personID)
+    print "angle", traci.person.getAngle(personID)
+    print "road", traci.person.getRoadID(personID)
+    print "type", traci.person.getTypeID(personID)
+    print "lanePos", traci.person.getLanePosition(personID)
+    print "color", traci.person.getColor(personID)
+    print "length", traci.person.getLength(personID)
+    print "MinGap", traci.person.getMinGap(personID)
+    print "width", traci.person.getWidth(personID)
+    print "waiting time", traci.person.getWaitingTime(personID)
+
+personID = "horiz"
+check(personID)
+traci.person.subscribe(personID)
+print traci.person.getSubscriptionResults(personID)
+for i in range(3):
+    print "step", step()
+    print traci.person.getSubscriptionResults(personID)
+check(personID)
+try:
+    check("bla")
+except traci.TraCIException:
+    print "recovering from exception after asking for unknown person"
+print "step", step()
+print "getIDList", traci.person.getIDList()
+for i in range(6):
+    print "step", step()
+    print traci.person.getSubscriptionResults(personID)
+traci.close()
