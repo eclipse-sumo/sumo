311eca1180e4f0e433ac278dea7cb2fee93ea42a
Jakob Erdmann <jakob.erdmann@dlr.de>
2015-03-02 15:51:37 +0000

    fix #1644
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@17992 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSLink.cpp
+++ sumo/src/microsim/MSLink.cpp
@@ -159,9 +159,17 @@ MSLink::setRequestInformation(int index, bool hasFoes, bool isCont,
                 // also length/geometry factor
                 intersections1.back() = lane->interpolateGeometryPosToLanePos(intersections1.back());
                 intersections2.back() = (*it_lane)->interpolateGeometryPosToLanePos(intersections2.back());
+
+                if (internalLaneBefore->getLogicalPredecessorLane()->getEdge().isInternal() && !(*it_lane)->getEdge().isCrossing())  {
+                    // wait at the internal junction 
+                    // (except for foes that are crossings since there is no internal junction)
+                    intersections1.back() = 0;
+                }
+
                 myLengthsBehindCrossing.push_back(std::make_pair(
                                                       lane->getLength() - intersections1.back(),
                                                       (*it_lane)->getLength() - intersections2.back()));
+                
 #ifdef MSLink_DEBUG_CROSSING_POINTS
                 std::cout
                         << " intersection of " << lane->getID()
