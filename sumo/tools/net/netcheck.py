bb269381234a192ad645d0561f18b205451caf37
Michael Behrisch <oss@behrisch.de>
2015-04-23 11:18:47 +0000

    improving netcheck to respect vclasses, write component selection files and calculate edges which can reach a fixed destination
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18283 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/net/netcheck.py
+++ sumo/tools/net/netcheck.py
@@ -33,8 +33,11 @@ def parse_args():
     optParser = OptionParser()
     optParser.add_option("-s", "--source",
                          default=False, help="List edges reachable from the source")
-    optParser.add_option("--selection-output",
-                         help="When used with --source, write output to file as a loadable selection")
+    optParser.add_option("-d", "--destination",
+                         default=False, help="List edges which can reach the destination")
+    optParser.add_option("-o", "--selection-output",
+                         help="Write output to file(s) as a loadable selection")
+    optParser.add_option("-l", "--vclass", help="Include only edges allowing VCLASS")
 
     options, args = optParser.parse_args()
     if len(args) != 1:
@@ -43,7 +46,7 @@ def parse_args():
     return options
 
 
-def getWeaklyConnected(net):
+def getWeaklyConnected(net, vclass=None):
     components = []
     edgesLeft = set(net.getEdges())
     queue = list()
@@ -52,37 +55,47 @@ def getWeaklyConnected(net):
         queue.append(edgesLeft.pop())
         while not len(queue) == 0:
             edge = queue.pop(0)
-            component.add(edge.getID())
-            for n in edge.getOutgoing().iterkeys():
-                if n in edgesLeft:
-                    queue.append(n)
-                    edgesLeft.remove(n)
-            for n in edge.getIncoming().iterkeys():
-                if n in edgesLeft:
-                    queue.append(n)
-                    edgesLeft.remove(n)
-        components.append(component)
+            if vclass is None or edge.allows(vclass): 
+                component.add(edge.getID())
+                for n in edge.getOutgoing().iterkeys():
+                    if n in edgesLeft:
+                        queue.append(n)
+                        edgesLeft.remove(n)
+                for n in edge.getIncoming().iterkeys():
+                    if n in edgesLeft:
+                        queue.append(n)
+                        edgesLeft.remove(n)
+        if component:
+            components.append(component)
     return components
 
 
-def getReachable(net, source_id, options):
+def getReachable(net, source_id, options, useIncoming=False):
     if not net.hasEdge(source_id):
         sys.exit("'%s' is not a valid edge id" % source_id)
     source = net.getEdge(source_id)
+    if options.vclass is not None and not source.allows(options.vclass): 
+        sys.exit("'%s' does not allow %s" % (source_id, options.vclass))
     fringe = [source]
     found = set()
     found.add(source)
     while len(fringe) > 0:
         new_fringe = []
         for edge in fringe:
-            for reachable in edge.getOutgoing().iterkeys():
-                if not reachable in found:
-                    found.add(reachable)
-                    new_fringe.append(reachable)
+            cands = edge.getIncoming() if useIncoming else edge.getOutgoing()
+            for reachable in cands.iterkeys():
+                if options.vclass is None or reachable.allows(options.vclass): 
+                    if not reachable in found:
+                        found.add(reachable)
+                        new_fringe.append(reachable)
         fringe = new_fringe
 
-    print "%s of %s edges are reachable from edge '%s':" % (
-        len(found), len(net.getEdges()), source_id)
+    if useIncoming:
+        print "%s of %s edges can reach edge '%s':" % (
+            len(found), len(net.getEdges()), source_id)
+    else:
+        print "%s of %s edges are reachable from edge '%s':" % (
+            len(found), len(net.getEdges()), source_id)
 
     if options.selection_output:
         with open(options.selection_output, 'w') as f:
@@ -98,11 +111,18 @@ if __name__ == "__main__":
     net = sumolib.net.readNet(options.net)
     if options.source:
         getReachable(net, options.source, options)
+    elif options.destination:
+        getReachable(net, options.destination, options, True)
     else:
-        components = getWeaklyConnected(net)
+        components = getWeaklyConnected(net, options.vclass)
         if len(components) != 1:
             print "Warning! Net is not connected."
             for idx, comp in enumerate(sorted(components, key=lambda c: iter(c).next())):
-                print "Component", idx
-                print " ".join(comp)
-                print
+                if options.selection_output:
+                    with open(options.selection_output + "comp%s.txt" % idx, 'w') as f:
+                        for e in comp:
+                            f.write("edge:%s\n" % e)
+                else:
+                    print "Component", idx
+                    print " ".join(comp)
+                    print
