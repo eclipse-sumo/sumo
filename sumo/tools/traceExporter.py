bbb750a080ac954baaa81eb780a52f5a9ce3d78b
Michael Behrisch <oss@behrisch.de>
2015-05-20 07:28:40 +0000

    adding boundary option, fix #1799
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18413 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/traceExporter.py
+++ sumo/tools/traceExporter.py
@@ -83,6 +83,8 @@ def procFCDStream(fcdstream, options):
     ft = -1  # "first" time step
     lastExported = -1
     chosen = {}
+    if options.boundary:
+        xmin,ymin,xmax,ymax = [float(e) for e in options.boundary.split(",")]
     for i, q in enumerate(fcdstream):
         pt = lt
         lt = float(q.time.encode("latin1"))
@@ -107,8 +109,9 @@ def procFCDStream(fcdstream, options):
                         z = v.z
                     else:
                         z = 0
-                    e.vehicle.append(
-                        FCDVehicleEntry(v.id, x, y, z, v.speed, v.type, v.lane, v.slope))
+                    if not options.boundary or (x >= xmin and x <= xmax and y >=ymin and y <= ymax):
+                        e.vehicle.append(
+                            FCDVehicleEntry(v.id, x, y, z, v.speed, v.type, v.lane, v.slope))
         yield e
     t = lt - pt + lt
     yield FCDTimeEntry(t)
@@ -120,7 +123,7 @@ def runMethod(inputFile, outputFile, writer, options, further={}):
     if options.base >= 0:
         further["base-date"] = datetime.datetime.fromtimestamp(options.base)
     else:
-        further["base-date"] = datetime.datetime.now()
+        further["base-date"] = datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0)
     o = _getOutputStream(outputFile)
     fcdStream = sumolib.output.parse(inputFile, "timestep")
     ret = writer(procFCDStream(fcdStream, options), o, further)
@@ -152,6 +155,7 @@ output format. Optionally the output can be sampled, filtered and distorted.
                          type="float", help="Defines the export step length")
     optParser.add_option("--gps-blur", dest="blur", default=0,
                          type="float", help="Defines the GPS blur")
+    optParser.add_option("--boundary", help="Defines the bounding box as 'xmin,ymin,xmax,ymax'")
     optParser.add_option("-s", "--seed", dest="seed", default=42,
                          type="float", help="Defines the randomizer seed")
     optParser.add_option(
