ec4302562dfbd6503d7f5029605a00017d912533
Jakob Erdmann <jakob.erdmann@dlr.de>
2015-05-20 15:16:08 +0000

    improving warning message refs #2
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18422 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSVehicle.cpp
+++ sumo/src/microsim/MSVehicle.cpp
@@ -1874,7 +1874,8 @@ MSVehicle::leaveLane(const MSMoveReminder::Notification reason) {
     }
     if (reason != MSMoveReminder::NOTIFICATION_PARKING && reason != MSMoveReminder::NOTIFICATION_LANE_CHANGE) {
         while (!myStops.empty() && myStops.front().edge == myCurrEdge) {
-            WRITE_WARNING("Vehicle '" + getID() + "' skips stop on lane '" + myStops.front().lane->getID() + "'.");
+            WRITE_WARNING("Vehicle '" + getID() + "' skips stop on lane '" + myStops.front().lane->getID() 
+                    + "' time=" + time2string(MSNet::getInstance()->getCurrentTimeStep()) + ".\n")
             myStops.pop_front();
         }
     }
