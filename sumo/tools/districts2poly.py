67538eedf2c8fe48c12d79a19f2b2150db9ea3dd
Jakob Erdmann <jakob.erdmann@dlr.de>
2015-08-03 07:15:11 +0000

    added tool for visualizing districts
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18589 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tools/districts2poly.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""
+@file    districts2poly.py
+@author  Jakob Erdmann
+@date    2015-07-31
+@version $Id: route2poly.py 18096 2015-03-17 09:50:59Z behrisch $
+
+From a sumo network and a taz (district) file, this script colors each district
+with a unique color (by creating a colored polygon for each edge in that
+district)
+These polygons can then be visualized in sumo-gui
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2012-2015 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+import sys
+import os
+import itertools
+import random
+from optparse import OptionParser
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from sumolib.output import parse
+from sumolib.net import readNet
+from sumolib.miscutils import Colorgen
+
+
+def parse_args():
+    USAGE = "Usage: " + sys.argv[0] + " <netfile> <routefile> [options]"
+    optParser = OptionParser()
+    optParser.add_option("-o", "--outfile", help="name of output file")
+    optParser.add_option("-u", "--hue", default="random",
+                         help="hue for polygons (float from [0,1] or 'random')")
+    optParser.add_option("-s", "--saturation", default=1,
+                         help="saturation for polygons (float from [0,1] or 'random')")
+    optParser.add_option("-b", "--brightness", default=1,
+                         help="brightness for polygons (float from [0,1] or 'random')")
+    optParser.add_option(
+        "-l", "--layer", default=100, help="layer for generated polygons")
+    options, args = optParser.parse_args()
+    try:
+        options.net, options.routefile = args
+        options.colorgen = Colorgen(
+            (options.hue, options.saturation, options.brightness))
+    except:
+        sys.exit(USAGE)
+    if options.outfile is None:
+        options.outfile = options.routefile + ".poly.xml"
+    return options
+
+
+def generate_poly(net, id, color, layer, edges, outf):
+    for edge in edges:
+        shape = net.getEdge(edge).getLane(0).getShape()
+        shapeString = ' '.join('%s,%s' % (x, y) for x, y in shape)
+        outf.write('<poly id="%s_%s" color="%s" layer="%s" type="taz_edge" shape="%s"/>\n' % (
+            id, net.getEdge(edge).getID(), color, layer, shapeString))
+
+
+def main():
+    random.seed(42)
+    options = parse_args()
+    net = readNet(options.net)
+    with open(options.outfile, 'w') as outf:
+        outf.write('<polygons>\n')
+        for taz in parse(options.routefile, 'taz'):
+            generate_poly(net, taz.id, options.colorgen(),
+                          options.layer, taz.edges.split(), outf)
+        outf.write('</polygons>\n')
+
+if __name__ == "__main__":
+    main()
