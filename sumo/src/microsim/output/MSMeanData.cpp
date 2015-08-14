be4155408dd4036b9312bbb6165a26478d8b54b1
Jakob Erdmann <jakob.erdmann@dlr.de>
2015-08-14 10:05:26 +0000

    excluding pedestrian lanes from meandata output (as they collect no data anyway)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18632 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/output/MSMeanData.cpp
+++ sumo/src/microsim/output/MSMeanData.cpp
@@ -276,7 +276,9 @@ void
 MSMeanData::init() {
     const MSEdgeVector& edges = MSNet::getInstance()->getEdgeControl().getEdges();
     for (MSEdgeVector::const_iterator e = edges.begin(); e != edges.end(); ++e) {
-        if (myDumpInternal || (*e)->getPurpose() != MSEdge::EDGEFUNCTION_INTERNAL) {
+        const MSEdge::EdgeBasicFunction efun = (*e)->getPurpose();
+        if ((myDumpInternal || efun != MSEdge::EDGEFUNCTION_INTERNAL) 
+           && efun != MSEdge::EDGEFUNCTION_CROSSING && efun != MSEdge::EDGEFUNCTION_WALKINGAREA) {
             myEdges.push_back(*e);
             myMeasures.push_back(std::vector<MeanDataValues*>());
             const std::vector<MSLane*>& lanes = (*e)->getLanes();
