2d76e069484992248fbebaec0dfa73a7da8d6d16
Michael Behrisch <oss@behrisch.de>
2015-08-25 21:17:47 +0000

    adding test for many screenshots, refs #1341, #1862
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18726 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/b50/runner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Michael Behrisch
+@author  Laura Bieker
+@date    2011-07-22
+@version $Id$
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2015 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+import os
+import subprocess
+import sys
+import time
+import shutil
+sumoHome = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+if "SUMO_HOME" in os.environ:
+    sumoHome = os.environ["SUMO_HOME"]
+sys.path.append(os.path.join(sumoHome, "tools"))
+import sumolib
+import traci
+PORT= 8765
+
+sumoBinary = os.environ.get(
+    "GUISIM_BINARY", os.path.join(sumoHome, 'bin', 'sumo-gui'))
+netconvertBinary = os.environ.get(
+    "NETCONVERT_BINARY", os.path.join(sumoHome, 'bin', 'netconvert'))
+
+subprocess.call([netconvertBinary, "-n", "input_nodes.nod.xml",
+                 "-e", "input_edges.edg.xml"], stdout=sys.stdout, stderr=sys.stderr)
+p = subprocess.Popen(
+    [sumoBinary, "-c", "sumo.sumocfg", "-v", "-S", "-Q", "--remote-port", str(PORT)], stdout=sys.stdout, stderr=sys.stderr)
+traci.init(PORT)
+traci.simulationStep(200000)
+for i in range(10):
+    for j in range(10):
+        traci.gui.screenshot('View #0', "test_%s_%s.png" % (i,j))
+    traci.simulationStep()
+traci.close()
+p.wait()
