579987270f88e0b55281007d2c8944e4e70a2b9f
Michael Behrisch <oss@behrisch.de>
2015-09-11 08:57:34 +0000

    fixing file headers, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@18830 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netedit/GNEInspector.h
+++ sumo/src/netedit/GNEInspector.h
@@ -6,13 +6,14 @@
 ///
 // The Widget for modifying network-element attributes (i.e. lane speed)
 /****************************************************************************/
-// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
-// Copyright (C) 2001-2014 DLR (http://www.dlr.de/) and contributors
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2015 DLR (http://www.dlr.de/) and contributors
 /****************************************************************************/
 //
-//   This program is free software; you can redistribute it and/or modify
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 2 of the License, or
+//   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
 //
 /****************************************************************************/
