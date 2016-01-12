35b63a1b74358f4b49005ab56c7beeb61d760710
Jakob Erdmann <jakob.erdmann@dlr.de>
2016-01-12 08:29:09 +0000

    affecting a more equitable split when importing looped ways from OSM. refs #2088
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@19710 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netimport/NIImporter_OpenStreetMap.cpp
+++ sumo/src/netimport/NIImporter_OpenStreetMap.cpp
@@ -306,12 +306,12 @@ NIImporter_OpenStreetMap::insertEdge(Edge* e, int index, NBNode* from, NBNode* t
     if (from == to) {
         // in the special case of a looped way split again using passed
         assert(passed.size() >= 2);
-        std::vector<long long int> geom(passed);
-        geom.pop_back(); // remove to-node
-        NBNode* intermediate = insertNodeChecking(geom.back(), nc, tlsc);
-        index = insertEdge(e, index, from, intermediate, geom, nb);
-        geom.clear();
-        return insertEdge(e, index, intermediate, to, geom, nb);
+        size_t intermediateIndex = passed.size() / 2;
+        NBNode* intermediate = insertNodeChecking(passed[intermediateIndex], nc, tlsc);
+        std::vector<long long int> part1(passed.begin(), passed.begin() + intermediateIndex);
+        std::vector<long long int> part2(passed.begin() + intermediateIndex + 1, passed.end());
+        index = insertEdge(e, index, from, intermediate, part1, nb);
+        return insertEdge(e, index, intermediate, to, part2, nb);
     }
     const int newIndex = index + 1;
 
