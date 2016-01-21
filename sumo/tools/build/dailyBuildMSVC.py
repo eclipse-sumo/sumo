97f1ce7a465dce45c05f45f98ba88aaf9c7d2fd1
Michael Behrisch <oss@behrisch.de>
2016-01-21 11:24:58 +0000

    trying to simplify nightly build, refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@19770 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/dailyBuildMSVC.py
+++ sumo/tools/build/dailyBuildMSVC.py
@@ -9,7 +9,7 @@
 
 Does the nightly svn update on the windows server and the visual
 studio build. The script is also used for the meso build.
-Some paths especially for the temp dir and the compiler are
+Some paths especially for the names of the texttest output dirs are
 hard coded into this script.
 
 SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
@@ -34,8 +34,72 @@ import zipfile
 import shutil
 import datetime
 import sys
+
 import status
 import wix
+
+def repositoryUpdate(options, repoLogFile):
+    if options.no_update:
+        return ""
+    with open(repoLogFile, 'w') as log:
+        subprocess.call(
+            "svn.exe up %s\\trunk" % options.rootDir, stdout=log, stderr=subprocess.STDOUT)
+    update_log = open(repoLogFile).read()
+    match_rev = re.search('At revision (\d*)\.', update_log)
+    if match_rev:
+        svnrev = match_rev.group(1)
+    else:
+        open(repoLogFile, 'a').write("Error parsing svn revision\n")
+        sys.exit()
+    end_marker = 'Fetching external'
+    if end_marker in update_log:
+        update_lines = len(
+            update_log[:update_log.index(end_marker)].splitlines())
+    else:
+        open(repoLogFile, 'a').write("Error parsing svn output\n")
+        sys.exit()
+
+    if update_lines < 3 and not options.force:
+        open(repoLogFile, 'a').write(
+            "No changes since last update, skipping build and test\n")
+        print("No changes since last update, skipping build and test")
+        sys.exit()
+    return svnrev
+
+def runTests(options, env, testLog, svnrev):
+    if options.no_tests:
+        return
+    env["TEXTTEST_TMP"] = os.path.join(
+        options.rootDir, env["FILEPREFIX"] + "texttesttmp")
+    env["TEXTTEST_HOME"] = os.path.join(options.rootDir, options.testsDir)
+    shutil.rmtree(env["TEXTTEST_TMP"], True)
+    if not os.path.exists(env["SUMO_REPORT"]):
+        os.makedirs(env["SUMO_REPORT"])
+    for name in ["dfrouter", "duarouter", "jtrrouter", "marouter", "netconvert", "netgenerate",
+                 "od2trips", "sumo", "polyconvert", "sumo-gui", "activitygen",
+                 "emissionsDrivingCycle", "emissionsMap"]:
+        binary = os.path.join(options.rootDir, options.binDir, name + ".exe")
+        if name == "sumo-gui":
+            if os.path.exists(binary):
+                env["GUISIM_BINARY"] = binary
+        elif os.path.exists(binary):
+            env[name.upper() + "_BINARY"] = binary
+    log = open(testLog, 'w')
+    # provide more information than just the date:
+    fullOpt = ["-b", env["FILEPREFIX"], "-name", "%sr%s" %
+               (date.today().strftime("%d%b%y"), svnrev)]
+    ttBin = "texttestc.py"
+    if options.sumoExe == "meso":
+        runInternalTests.runInternal("", fullOpt, log, console=True)
+    else:
+        subprocess.call([ttBin] + fullOpt, env=env,
+                        stdout=log, stderr=subprocess.STDOUT, shell=True)
+    subprocess.call([ttBin, "-a", "sumo.gui"] + fullOpt, env=env,
+                    stdout=log, stderr=subprocess.STDOUT, shell=True)
+    subprocess.call([ttBin, "-b", env["FILEPREFIX"], "-coll"], env=env,
+                    stdout=log, stderr=subprocess.STDOUT, shell=True)
+    log.close()
+
 
 optParser = optparse.OptionParser()
 optParser.add_option("-r", "--root-dir", dest="rootDir",
@@ -48,8 +112,6 @@ optParser.add_option("-b", "--bin-dir", dest="binDir", default=r"trunk\sumo\bin"
                      help="directory containg the binaries, relative to the root dir")
 optParser.add_option("-t", "--tests-dir", dest="testsDir", default=r"trunk\sumo\tests",
                      help="directory containg the tests, relative to the root dir")
-optParser.add_option("-e", "--sumo-exe", dest="sumoExe", default="sumo",
-                     help="name of the sumo executable")
 optParser.add_option("-m", "--remote-dir", dest="remoteDir",
                      default=r"O:\Daten\Sumo\daily",
                      help="directory to move the results to")
@@ -63,6 +125,10 @@ optParser.add_option("-d", "--dll-dirs", dest="dllDirs",
                      help="path to dependency dlls for the relevant platforms")
 optParser.add_option("-f", "--force", action="store_true",
                      default=False, help="force rebuild even if no source changed")
+optParser.add_option("-u", "--no-update", action="store_true",
+                     default=False, help="skip repository update")
+optParser.add_option("-n", "--no-tests", action="store_true",
+                     default=False, help="skip tests")
 (options, args) = optParser.parse_args()
 
 sys.path.append(os.path.join(options.rootDir, options.testsDir))
@@ -81,7 +147,7 @@ if "VS100COMNTOOLS" in env:
 if "VS120COMNTOOLS" in env:
     compiler = os.path.join(env["VS120COMNTOOLS"], "..", "IDE", "devenv.exe")
     msvcVersion = "msvc12"
-svnrev = ""
+svnrev = repositoryUpdate(options, os.path.join(options.remoteDir, msvcVersion + options.suffix + "Update.log"))
 
 maxTime = 0
 sumoAllZip = None
@@ -107,31 +173,6 @@ for platform, dllDir in platformDlls:
             os.remove(f)
         except WindowsError:
             pass
-    if platform == "Win32":
-        with open(makeLog, 'w') as log:
-            subprocess.call(
-                "svn.exe up %s\\trunk" % options.rootDir, stdout=log, stderr=subprocess.STDOUT)
-        update_log = open(makeLog).read()
-        match_rev = re.search('At revision (\d*)\.', update_log)
-        if match_rev:
-            svnrev = match_rev.group(1)
-        else:
-            open(makeLog, 'a').write("Error parsing svn revision\n")
-            sys.exit()
-        end_marker = 'Fetching external'
-        if end_marker in update_log:
-            update_lines = len(
-                update_log[:update_log.index(end_marker)].splitlines())
-        else:
-            open(makeLog, 'a').write("Error parsing svn output\n")
-            sys.exit()
-
-        if update_lines < 3 and not options.force:
-            open(makeLog, 'a').write(
-                "No changes since last update, skipping build and test\n")
-            print("No changes since last update, skipping build and test")
-            sys.exit()
-
     subprocess.call(compiler + " /rebuild Release|%s %s\\%s /out %s" %
                     (platform, options.rootDir, options.project, makeLog))
     if options.addConf:
@@ -179,13 +220,12 @@ for platform, dllDir in platformDlls:
             (errno, strerror) = ziperr.args
             print("Warning: Could not zip to %s!" % binaryZip, file=log)
             print("I/O error(%s): %s" % (errno, strerror), file=log)
-    if (platform == "Win32" and options.sumoExe == "sumo") or (platform == "x64" and options.sumoExe == "meso"):
-        try:
-            setup = os.path.join(env["SUMO_HOME"], 'tools', 'game', 'setup.py')
-            subprocess.call(
-                ['python', setup, binaryZip], stdout=log, stderr=subprocess.STDOUT)
-        except Exception as e:
-            print("Warning: Could not create nightly sumo-game.zip! (%s)" % e, file=log)
+    try:
+        setup = os.path.join(env["SUMO_HOME"], 'tools', 'game', 'setup.py')
+        subprocess.call(
+            ['python', setup, binaryZip], stdout=log, stderr=subprocess.STDOUT)
+    except Exception as e:
+        print("Warning: Could not create nightly sumo-game.zip! (%s)" % e, file=log)
     log.close()
     subprocess.call(compiler + " /rebuild Debug|%s %s\\%s /out %s" %
                     (platform, options.rootDir, options.project, makeAllLog))
@@ -195,38 +235,7 @@ for platform, dllDir in platformDlls:
     if options.addSln:
         subprocess.call(compiler + " /rebuild Debug|%s %s\\%s /out %s" %
                         (platform, options.rootDir, options.addSln, makeAllLog))
-
-# run tests
-    env["TEXTTEST_TMP"] = os.path.join(
-        options.rootDir, env["FILEPREFIX"] + "texttesttmp")
-    env["TEXTTEST_HOME"] = os.path.join(options.rootDir, options.testsDir)
-    shutil.rmtree(env["TEXTTEST_TMP"], True)
-    if not os.path.exists(env["SUMO_REPORT"]):
-        os.makedirs(env["SUMO_REPORT"])
-    for name in ["dfrouter", "duarouter", "jtrrouter", "marouter", "netconvert", "netgenerate",
-                 "od2trips", "sumo", "polyconvert", "sumo-gui", "activitygen",
-                 "emissionsDrivingCycle", "emissionsMap"]:
-        binary = os.path.join(options.rootDir, options.binDir, name + ".exe")
-        if name == "sumo-gui":
-            if os.path.exists(binary):
-                env["GUISIM_BINARY"] = binary
-        elif os.path.exists(binary):
-            env[name.upper() + "_BINARY"] = binary
-    log = open(testLog, 'w')
-    # provide more information than just the date:
-    fullOpt = ["-b", env["FILEPREFIX"], "-name", "%sr%s" %
-               (date.today().strftime("%d%b%y"), svnrev)]
-    ttBin = "texttestc.py"
-    if options.sumoExe == "meso":
-        runInternalTests.runInternal("", fullOpt, log, console=True)
-    else:
-        subprocess.call([ttBin] + fullOpt, env=os.environ,
-                        stdout=log, stderr=subprocess.STDOUT, shell=True)
-    subprocess.call([ttBin, "-a", "sumo.gui"] + fullOpt, env=os.environ,
-                    stdout=log, stderr=subprocess.STDOUT, shell=True)
-    subprocess.call([ttBin, "-b", env["FILEPREFIX"], "-coll"], env=os.environ,
-                    stdout=log, stderr=subprocess.STDOUT, shell=True)
-    log.close()
+    runTests(options, env, testLog, svnrev)
     log = open(statusLog, 'w')
     status.printStatus(makeLog, makeAllLog, env["SMTP_SERVER"], log)
     log.close()
