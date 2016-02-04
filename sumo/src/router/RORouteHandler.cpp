41793d171fd284924defcbfd6277ba4dc3f87fc9
Michael Behrisch <oss@behrisch.de>
2016-02-04 08:58:22 +0000

    deleting vehicle parameters but replacing unknown vehicle type, refs #2132
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@19888 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/router/RORouteHandler.cpp
+++ sumo/src/router/RORouteHandler.cpp
@@ -514,9 +514,7 @@ RORouteHandler::closeFlow() {
     }
     if (myNet.getVehicleTypeSecure(myVehicleParameter->vtypeid) == 0) {
         myErrorOutput->inform("The vehicle type '" + myVehicleParameter->vtypeid + "' for flow '" + myVehicleParameter->id + "' is not known.");
-        delete myVehicleParameter;
-        myVehicleParameter = 0;
-        return;
+        myVehicleParameter->vtypeid = DEFAULT_VTYPE_ID;
     }
     if (myVehicleParameter->routeid[0] == '!' && myNet.getRouteDef(myVehicleParameter->routeid) == 0) {
         closeRoute(true);
@@ -534,6 +532,8 @@ RORouteHandler::closeFlow() {
         } else {
             myErrorOutput->inform("Another flow with the id '" + myVehicleParameter->id + "' exists.");
         }
+    } else {
+        delete myVehicleParameter;
     }
     myVehicleParameter = 0;
     myInsertStopEdgesAt = -1;
