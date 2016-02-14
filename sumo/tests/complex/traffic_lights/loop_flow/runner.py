84755a7ec4f45e60a6d2b74e8d77b5e346ea438c
Michael Behrisch <oss@behrisch.de>
2016-02-14 15:58:13 +0000

    removing generated files, refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@19977 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traffic_lights/loop_flow/runner.py
+++ sumo/tests/complex/traffic_lights/loop_flow/runner.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Daniel Krajzewicz
+@date    2007-07-26
+@version $Id$
+
+test different traffic_light types
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2007-2015 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
 from __future__ import absolute_import
 from __future__ import print_function
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# test different traffic_light types
 import sys
 import os
 import subprocess
