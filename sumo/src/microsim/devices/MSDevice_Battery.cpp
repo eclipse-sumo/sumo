d2e90746215bdace766ac188d3a25899e8614048
Michael Behrisch <oss@behrisch.de>
2016-02-25 11:08:40 +0000

    removing unused define, refs #12, #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@20071 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/devices/MSDevice_Battery.cpp
+++ sumo/src/microsim/devices/MSDevice_Battery.cpp
@@ -40,8 +40,6 @@
 #include "MSDevice_Tripinfo.h"
 #include "MSDevice_Battery.h"
 
-#define PI 3.141592654
-
 #ifdef CHECK_MEMORY_LEAKS
 #include <foreign/nvwa/debug_new.h>
 #endif // CHECK_MEMORY_LEAKS
