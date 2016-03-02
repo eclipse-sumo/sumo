8bed31f24994fb5ea58d2805d88af5f5b9a2c9c8
Martin Taraz <martin@taraz.de>
2016-03-02 09:44:13 +0000

    Fixing Fileheaders ref: #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@20117 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/emissions/tls_emissions/sumodetectors.py
+++ sumo/tests/complex/emissions/tls_emissions/sumodetectors.py
@@ -1,3 +1,19 @@
+"""
+@file    sumodetectors.py
+@author  Daniel Krajzewicz
+@date    2013-10-19
+@version $Id$
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2012-2015 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
 from __future__ import absolute_import
 
 
