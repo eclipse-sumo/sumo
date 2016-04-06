37435f60c48369040651c55e5b417e2052c82214
Michael Behrisch <oss@behrisch.de>
2016-04-06 11:51:09 +0000

    using the new model files, refs #2206
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@20359 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/emissions/HelpersPHEMlight.cpp
+++ sumo/src/utils/emissions/HelpersPHEMlight.cpp
@@ -62,17 +62,17 @@ HelpersPHEMlight::getClassByName(const std::string& eClass, const SUMOVehicleCla
     if (myEmissionClassStrings.hasString(eClass)) {
         return myEmissionClassStrings.get(eClass);
     }
-    if (eClass.size() < 8 || (eClass.find("_D_EU") == std::string::npos && eClass.find("_G_EU") == std::string::npos)) {
-        return false;
+    if (eClass.size() < 6) {
+        throw InvalidArgument("Unknown emission class '" + eClass + "'.");
     }
     int index = myIndex++;
     const std::string type = eClass.substr(0, 3);
-    if (type == "LB_" || type == "RB_" || type == "LSZ" || eClass.find("LKW") != std::string::npos) {
+    if (type == "HDV" || type == "LB_" || type == "RB_" || type == "LSZ" || eClass.find("LKW") != std::string::npos) {
         index |= PollutantsInterface::HEAVY_BIT;
     }
     myEmissionClassStrings.insert(eClass, index);
 #ifdef INTERNAL_PHEM
-    if (!PHEMCEPHandler::getHandlerInstance().Load(index, eClass)) {
+    if (type == "HDV" || type == "LCV" || type == "PC_" || !PHEMCEPHandler::getHandlerInstance().Load(index, eClass)) {
 #endif
         std::vector<std::string> phemPath;
         phemPath.push_back(OptionsCont::getOptions().getString("phemlight-path") + "/");
