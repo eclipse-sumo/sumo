d839b40cdfc0fdb28ffb6a3f8de0c1f116e272a3
Michael Behrisch <oss@behrisch.de>
2016-07-06 13:50:16 +0000

    trying to fix build for old school debian users, refs #3
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21124 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/osgview/GUIOSGBoundingBoxCalculator.h
+++ sumo/src/osgview/GUIOSGBoundingBoxCalculator.h
@@ -62,7 +62,11 @@ public:
     void apply(osg::Geode& geode) {
         osg::BoundingBox bbox;
         for (unsigned int i = 0; i < geode.getNumDrawables(); ++i) {
+#if OSG_MIN_VERSION_REQUIRED(3,4,0)
             bbox.expandBy(geode.getDrawable(i)->getBoundingBox());
+#else
+            bbox.expandBy(geode.getDrawable(i)->getBound());
+#endif
         }
         osg::BoundingBox bboxTrans;
         for (unsigned int i = 0; i < 8; ++i) {
