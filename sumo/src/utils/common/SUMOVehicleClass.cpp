7722814eafe9e72e6797c00e98b97c0afbada8e9
Robert Hilbrich <robert.hilbrich@dlr.de>
2016-08-02 11:37:16 +0000

    fix #2148
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21275 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/common/SUMOVehicleClass.cpp
+++ sumo/src/utils/common/SUMOVehicleClass.cpp
@@ -204,12 +204,17 @@ parseVehicleClasses(const std::string& allowedS) {
     StringTokenizer sta(allowedS, " ");
     while (sta.hasNext()) {
         const std::string s = sta.next();
-        const SUMOVehicleClass vc = getVehicleClassID(s);
-        const std::string& realName = SumoVehicleClassStrings.getString(vc);
-        if (realName != s) {
-            deprecatedVehicleClassesSeen.insert(s);
+        if (!SumoVehicleClassStrings.hasString(s)) {
+	    WRITE_ERROR("Unknown vehicle class '" + s + "' encountered. It will be ignored.");
         }
-        result |= vc;
+	else {
+	    const SUMOVehicleClass vc = getVehicleClassID(s);
+	    const std::string& realName = SumoVehicleClassStrings.getString(vc);
+	    if (realName != s) {
+		deprecatedVehicleClassesSeen.insert(s);
+	    }
+	    result |= vc;
+	}
     }
     return result;
 }
