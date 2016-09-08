337ffda5e98bfa446f24b11a8762a082e9d9331a
Michael Behrisch <oss@behrisch.de>
2016-09-08 13:16:16 +0000

    adding test, refs #2465
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21459 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/distance_after_reroute/runner.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Laura Bieker
+@author  Michael Behrisch
+@date    2014-08-28
+@version $Id: runner.py 21122 2016-07-06 11:06:52Z behrisch $
+
+Python resume test.
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2016 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+import subprocess
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path += [tools]
+    import sumolib
+    import traci
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+# the port used for communicating with your sumo instance
+PORT_TRACI = sumolib.miscutils.getFreeSocketPort()
+
+def main(args):
+    sumoBinary = sumolib.checkBinary('sumo')
+    sumo_call = [sumoBinary, "-c", "data/hello.sumocfg",
+                 "--remote-port", str(PORT_TRACI),
+                 "--netstate-dump", "rawdump.xml",
+                 "--no-step-log"]
+    sumoProcess = subprocess.Popen(sumo_call, stdout=sys.stdout, stderr=sys.stderr)
+    traci.init(PORT_TRACI)
+
+    for step in range(161):
+        traci.simulationStep()
+        if step == 120:
+            print(traci.vehicle.getDistance('Stapler_00'))
+            traci.vehicle.setRoute('Stapler_00', ('ed1', 'ed5'))
+            print(traci.vehicle.getRoute('Stapler_00'))
+            assert(traci.vehicle.getRoute('Stapler_00') == ['ed0', 'ed1', 'ed5'])
+            print(traci.vehicle.getDistance('Stapler_00'))
+        if step == 122:
+            assert(traci.vehicle.getRoute('Stapler_00') == ['ed0', 'ed1', 'ed5'])
+            print(traci.vehicle.getDistance('Stapler_00'))
+            traci.vehicle.setRouteID('Stapler_00', "short")
+            print(traci.vehicle.getRoute('Stapler_00'))
+            print(traci.vehicle.getDistance('Stapler_00'))
+    traci.close()
+    sumoProcess.wait()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
