e95262bf3b0bb4180c8c9fff1614002f6abd1f27
Jakob Erdmann <jakob.erdmann@dlr.de>
2016-09-19 11:17:18 +0000

    fixing crash when writing opendrive-files
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21499 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/geom/PositionVector.cpp
+++ sumo/src/utils/geom/PositionVector.cpp
@@ -905,6 +905,9 @@ PositionVector::move2side(SUMOReal amount) {
     if (size() < 2) {
         return;
     }
+    if (length2D() == 0) {
+        throw InvalidArgument("2D-length is 0");
+    }
     PositionVector shape;
     for (int i = 0; i < static_cast<int>(size()); i++) {
         if (i == 0) {
