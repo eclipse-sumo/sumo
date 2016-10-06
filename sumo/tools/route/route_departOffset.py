8765c8dd7ed0bee874df6fb35d314789cbc3f54a
Jakob Erdmann <jakob.erdmann@dlr.de>
2016-10-06 13:53:59 +0000

    added new option, fix #2568
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21629 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/route/route_departOffset.py
+++ sumo/tools/route/route_departOffset.py
@@ -41,7 +41,9 @@ def get_options(args=None):
     optParser = optparse.OptionParser()
     optParser.add_option("-r", "--input-file", dest="infile", help="the input route file (mandatory)")
     optParser.add_option("-o", "--output-file", dest="outfile", help="the output route file (mandatory)")
-    optParser.add_option("-d", "--depart-offset", dest="offset", type="float", help="the depart offset to apply (mandatory)")
+    optParser.add_option("-d", "--depart-offset", dest="offset", type="float", help="the depart offset to apply")
+    optParser.add_option("-i", "--depart-interval", dest="interval", 
+            help="time intervals a,b,c,d where all vehicles departing in the interval [a,b[ are mapped to the interval [c,d[")
     optParser.add_option("--modify-ids", dest="modify_ids", action="store_true", default=False, help="whether ids should be modified as well")
     optParser.add_option("--heterogeneous", dest="heterogeneous",
             action="store_true", default=False, help="whether heterogeneous objects shall be parsed (i.e. vehicles with embeded and referenced routes)")
@@ -51,11 +53,20 @@ def get_options(args=None):
             help="only modify departure times of vehicles departing on edges or lanes in the given selection file")
 
     (options, args) = optParser.parse_args(args=args)
-    if options.infile is None or options.outfile is None or options.offset is None:
+    if options.infile is None or options.outfile is None:
         optParser.print_help()
         sys.exit()
 
-    options.offset = intIfPossible(options.offset)
+    if ((options.offset is None and options.interval is None) 
+            or (options.offset is not None and options.interval is not None)):
+        print("Either one of the options --depart-offset or --depart-interval must be given")
+        sys.exit()
+
+    if options.offset is not None:
+        options.name_suffix = "_%s" % intIfPossible(options.offset)
+    else:
+        options.interval = tuple(map(float, options.interval.split(',')))
+        options.name_suffix = "_%s_%s_%s_%s" % options.interval
 
     if options.depart_edges is not None:
         options.depart_edges = options.depart_edges.split(',')
@@ -73,6 +84,13 @@ def get_options(args=None):
                 options.depart_edges.append(line)
 
     return options
+
+
+def shiftInterval(val, interval):
+    val = float(val)
+    if interval[0] <= val < interval[1]:
+        val = (val - interval[0]) / (interval[1] - interval[0]) * (interval[3] - interval[2]) + interval[2]
+    return str(intIfPossible(val))
 
 
 def main(options):
@@ -93,7 +111,7 @@ def main(options):
                 pass
             else:
                 if options.modify_ids:
-                    obj.id += "_%s" % options.offset
+                    obj.id += options.name_suffix
                 departEdge = None
                 if options.depart_edges is not None:
                     # determine the departEdge of the current vehicle
@@ -114,11 +132,20 @@ def main(options):
                             # route child element
                             departEdge = obj.route[0].edges.split()[0]
                 if departEdge is None or departEdge in options.depart_edges:
-                    if obj.name in ['trip', 'vehicle']:
-                        obj.depart = str(intIfPossible(float(obj.depart) + options.offset))
+                    if options.offset is not None:
+                        # shift by offset
+                        if obj.name in ['trip', 'vehicle']:
+                            obj.depart = str(intIfPossible(float(obj.depart) + options.offset))
+                        else:
+                            obj.begin = str(intIfPossible(float(obj.begin) + options.offset))
+                            obj.end = str(intIfPossible(float(obj.end) + options.offset))
                     else:
-                        obj.begin = str(intIfPossible(float(obj.begin) + options.offset))
-                        obj.end = str(intIfPossible(float(obj.end) + options.offset))
+                        # shift by interval
+                        if obj.name in ['trip', 'vehicle']:
+                            obj.depart = shiftInterval(obj.depart, options.interval)
+                        else:
+                            obj.begin = shiftInterval(obj.begin, options.interval)
+                            obj.end = shiftInterval(obj.end, options.interval)
 
             out.write(obj.toXML('    '))
         out.write("</routes>\n")
