b9082303a7e7c5b04ad5767b5c2bb5bca4b70919
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-11-11 09:24:14 +0000

    Updated netedit test busstop/create and basic/new_net_1edge
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@21966 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionals/busstop/create/test.sikuli/test.py
+++ sumo/tests/netedit/additionals/busstop/create/test.sikuli/test.py
@@ -10,165 +10,74 @@ Settings.DelayAfterDrag = 0.1
 # SUMO Folder
 SUMOFolder = os.environ.get('SUMO_HOME', '.')
 # Current environment
-currentEnvironmentFile = open(
-    SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
+currentEnvironmentFile = open(SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
 # Get path to netEdit app
 neteditApp = currentEnvironmentFile.readline().replace("\n", "")
 # Get SandBox folder
 textTestSandBox = currentEnvironmentFile.readline().replace("\n", "")
 # Get resources depending of the current Operating system
 currentOS = currentEnvironmentFile.readline().replace("\n", "")
-netEditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
+neteditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
+neteditReference = SUMOFolder + "/tests/netedit/imageResources/reference.png"
 currentEnvironmentFile.close()
 #****#
 
 # Open netedit
-netEditProcess = subprocess.Popen([neteditApp,
-                                   '--window-size', '800,600',
+neteditProcess = subprocess.Popen([neteditApp,
+                                   '--gui-testing',
+                                   '--window-size', '700,500',
                                    '--new',
                                    '--additionals-output', textTestSandBox + "/additionals.xml"],
-                                  env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
+                                   env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
 
-# Wait 10 seconds to netedit main windows
+# Wait to netedit and focus
 try:
-    match = wait(netEditResources + "neteditToolbar.png", 10)
+    match = wait(neteditReference, 20)
 except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'neteditToolbar.png' not found")
+    neteditProcess.kill()
+    sys.exit("Killed netedit process. 'reference.png' not found")
 
-# focus netEdit window
-click(match.getTarget().offset(0, -20))
-
+# Focus netedid window
+click(match.getTarget().offset(0, -105))
+	
 # Change to create mode
 type("e")
 
 # Create two nodes
-click(match.getTarget().offset(-250, 400))
-click(match.getTarget().offset(250, 400))
+click(match.getTarget().offset(100, 300))
+click(match.getTarget().offset(500, 300))
 
 # Change to create additional
 type("a")
 
 # by default, additional is busstop, then isn't needed to select "busstop"
 
-# Check that default parameters are correct
-try:
-    find(netEditResources + "additionals/busStop-defaultParameters.png")
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'busStop-defaultParameters.png' not found")
-
 # Add three extra lines
-click(match.getTarget().offset(-310, 200))
-click(match.getTarget().offset(-310, 225))
-click(match.getTarget().offset(-310, 250))
+click(match.getTarget().offset(-145, 130))
+click(match.getTarget().offset(-145, 155))
+click(match.getTarget().offset(-145, 180))
 
 # Set name of lines
-doubleClick(match.getTarget().offset(-280, 175))
+doubleClick(match.getTarget().offset(-135, 100))
 type("Line1" + Key.ENTER)
-doubleClick(match.getTarget().offset(-280, 200))
+doubleClick(match.getTarget().offset(-135, 125))
 type("Line2" + Key.ENTER)
-doubleClick(match.getTarget().offset(-280, 225))
+doubleClick(match.getTarget().offset(-135, 150))
 type("Line3" + Key.ENTER)
-doubleClick(match.getTarget().offset(-280, 250))
+doubleClick(match.getTarget().offset(-135, 175))
 type("Line4" + Key.ENTER)
 
 # Remove last line
-click(match.getTarget().offset(-280, 275))
+click(match.getTarget().offset(-110, 195))
 
 # create busstop in mode "reference left"
-click(match.getTarget().offset(200, 400))
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference right
-click(match.getTarget().offset(-300, 390))
-
-# create busstop in mode "reference right"
-click(match.getTarget().offset(100, 400))
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference center
-click(match.getTarget().offset(-300, 410))
-
-# create busstop in mode "reference center
-click(match.getTarget().offset(0, 400))
-
-# change default lenght to 20
-doubleClick(match.getTarget().offset(-270, 370))
-type("100" + Key.ENTER)
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference right
-click(match.getTarget().offset(-300, 390))
-
-# try to create busstop with a different lenght WITHOUT forcing position.
-# BusStop musn't be created
-click(match.getTarget().offset(-100, 400))
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference left
-click(match.getTarget().offset(-300, 365))
-
-# try to create busstop with a different lenght WITHOUT forcing position.
-# BusStop musn't be created
-click(match.getTarget().offset(300, 400))
-
-# Enable force position
-click(match.getTarget().offset(-350, 400))
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference right
-click(match.getTarget().offset(-300, 390))
-
-# Create busstop with a different lenght forcing position.
-click(match.getTarget().offset(-100, 400))
-
-# Go to reference mode
-click(match.getTarget().offset(-300, 340))
-
-# Change to reference left
-click(match.getTarget().offset(-300, 365))
-
-# Create busstop with a different lenght forcing position.
-click(match.getTarget().offset(300, 400))
-
-# Check Undo (5 busStops)
-for x in range(0, 5):
-    try:
-        click(match.getTarget().offset(-325, 5))
-        click(netEditResources + "undoredo/edit-undo.png")
-    except:
-        netEditProcess.kill()
-        sys.exit("Killed netedit process. 'edit-undo.png' not found")
-
-# Check Redo (5 busStops)
-for x in range(0, 5):
-    try:
-        click(match.getTarget().offset(-325, 5))
-        click(netEditResources + "undoredo/edit-redo.png")
-    except:
-        netEditProcess.kill()
-        sys.exit("Killed netedit process. 'edit-redo.png' not found")
+click(match.getTarget().offset(400, 300))
 
 # save additionals
-click(match.getTarget().offset(-375, 5))
-click(match.getTarget().offset(-350, 265))
+click(match.getTarget().offset(-200, -80))
+click(match.getTarget().offset(-200, 180))
 
 # quit
 type("q", Key.CTRL)
-try:
-    find(netEditResources + "confirmClosingNetworkDialog.png")
-    type("y", Key.ALT)
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'confirmClosingNetworkDialog.png' not found")
+
+type("y", Key.ALT)
--- sumo/tests/netedit/basic/new_net_1edge/test.sikuli/test.py
+++ sumo/tests/netedit/basic/new_net_1edge/test.sikuli/test.py
@@ -10,41 +10,42 @@ Settings.DelayAfterDrag = 0.1
 # SUMO Folder
 SUMOFolder = os.environ.get('SUMO_HOME', '.')
 # Current environment
-currentEnvironmentFile = open(
-    SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
+currentEnvironmentFile = open(SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
 # Get path to netEdit app
 neteditApp = currentEnvironmentFile.readline().replace("\n", "")
 # Get SandBox folder
 textTestSandBox = currentEnvironmentFile.readline().replace("\n", "")
 # Get resources depending of the current Operating system
 currentOS = currentEnvironmentFile.readline().replace("\n", "")
-netEditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
+neteditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
+neteditReference = SUMOFolder + "/tests/netedit/imageResources/reference.png"
 currentEnvironmentFile.close()
 #****#
 
 # Open netedit
-netEditProcess = subprocess.Popen([neteditApp,
-                                   '--window-size', '800,600',
+neteditProcess = subprocess.Popen([neteditApp,
+                                   '--gui-testing',
+                                   '--window-size', '700,500',
                                    '--new',
                                    '-o', textTestSandBox + "/net.net.xml"],
-                                  env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
+								   env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
 
-# Wait to netedit
+# Wait to netedit and focus
 try:
-    match = wait(netEditResources + "neteditToolbar.png", 20)
+    match = wait(neteditReference, 20)
 except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'neteditToolbar.png' not found")
+    neteditProcess.kill()
+    sys.exit("Killed netedit process. 'reference.png' not found")
 
-# focusa
-click(match.getTarget().offset(0, -20))
+# Focus netedid window
+click(match.getTarget().offset(0, -105))
 
 # Change to create mode
 type("e")
 
 # Create two nodes
-click(match.getTarget().offset(-200, 300))
-click(match.getTarget().offset(200, 300))
+click(match.getTarget().offset(100, 300))
+click(match.getTarget().offset(500, 300))
 
 # save network and quit
 type("s", Key.CTRL)
