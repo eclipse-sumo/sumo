356ae960e7563184336b0ba5a91643e94b65480c
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-11-17 12:30:34 +0000

    Updated netedit tests. Now use images to find elements.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22048 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionals/busstop/create/test.sikuli/test.py
+++ sumo/tests/netedit/additionals/busstop/create/test.sikuli/test.py
@@ -37,8 +37,8 @@ except:
     neteditProcess.kill()
     sys.exit("Killed netedit process. 'reference.png' not found")
 
-# Focus netedid window
-click(match.getTarget().offset(0, -105))
+# Focus netedit window
+click(match)
 	
 # Change to create mode
 type("e")
@@ -53,25 +53,76 @@ type("a")
 # by default, additional is busstop, then isn't needed to select "busstop"
 
 # Add three extra lines
-click(match.getTarget().offset(-145, 130))
-click(match.getTarget().offset(-145, 155))
-click(match.getTarget().offset(-145, 180))
-
-# Set name of lines
-doubleClick(match.getTarget().offset(-135, 100))
+doubleClick(find(neteditResources + "attributes/attributeLines0.png").offset(75, 0))
 paste("Line1")
-doubleClick(match.getTarget().offset(-135, 125))
+
+click(find(neteditResources + "additionalMode/add.png"))
+doubleClick(find(neteditResources + "attributes/attributeLines1.png").offset(75, 0))
 paste("Line2")
-doubleClick(match.getTarget().offset(-135, 150))
+
+click(find(neteditResources + "additionalMode/add.png"))
+doubleClick(find(neteditResources + "attributes/attributeLines2.png").offset(75, 0))
 paste("Line3")
-doubleClick(match.getTarget().offset(-135, 175))
+
+click(find(neteditResources + "additionalMode/add.png"))
+doubleClick(find(neteditResources + "attributes/attributeLines3.png").offset(75, 0))
 paste("Line4")
 
-# Remove last line
-click(match.getTarget().offset(-110, 195))
+# remove last line
+click(find(neteditResources + "additionalMode/remove.png"))
 
 # create busstop in mode "reference left"
-click(match.getTarget().offset(400, 300))
+click(match.getTarget().offset(450, 300))
+
+# change reference to right
+click(find(neteditResources + "additionalMode/referenceLeft.png"))
+type(Key.DOWN)
+
+# create busstop in mode "reference right"
+click(match.getTarget().offset(300, 300))
+
+# change reference to center
+click(find(neteditResources + "additionalMode/referenceRight.png"))
+type(Key.DOWN)
+
+# create busstop in mode "reference center"
+click(match.getTarget().offset(350, 300))
+
+# return to mode "reference left"
+click(find(neteditResources + "additionalMode/referenceCenter.png"))
+type(Key.UP)
+type(Key.UP)
+
+# Change length
+doubleClick(find(neteditResources + "attributes/attributeLength.png").offset(75, 0))
+paste("30")
+
+# try to create a busstop (Warning)
+click(match.getTarget().offset(500, 300))
+
+# change reference to right
+click(find(neteditResources + "additionalMode/referenceLeft.png"))
+type(Key.DOWN)
+
+# try busstop in mode "reference right" (Warning)
+click(match.getTarget().offset(250, 300))
+
+# return to mode "reference left"
+click(find(neteditResources + "additionalMode/referenceCenter.png"))
+type(Key.UP)
+
+# enable force position
+click(find(neteditResources + "additionalMode/attributeForcePosition.png"))
+
+# create a busstop forcing position
+click(match.getTarget().offset(500, 300))
+
+# change reference to right
+click(find(neteditResources + "additionalMode/referenceLeft.png"))
+type(Key.DOWN)
+
+# create a busstop forcing position
+click(match.getTarget().offset(250, 300))
 
 # save additionals
 # XXX add a keyboard hotkey
