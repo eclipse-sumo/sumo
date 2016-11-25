a0031807c26d37fe0026d54fd7ff009ba4bbeefd
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-11-25 09:49:47 +0000

    Updated test busstop/load. Founded bug with the saving default directory of additionals.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22098 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionals/busstop/load/test.sikuli/test.py
+++ sumo/tests/netedit/additionals/busstop/load/test.sikuli/test.py
@@ -1,54 +1,16 @@
-# Import libraries
+# import common functions for netedit tests
 import os
-import sys
-import subprocess
-
-#** Common parameters **#
-Settings.MoveMouseDelay = 0.1
-Settings.DelayBeforeDrop = 0.1
-Settings.DelayAfterDrag = 0.1
-# SUMO Folder
-SUMOFolder = os.environ.get('SUMO_HOME', '.')
-# Current environment
-currentEnvironmentFile = open(
-    SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
-# Get path to netEdit app
-neteditApp = currentEnvironmentFile.readline().replace("\n", "")
-# Get SandBox folder
-textTestSandBox = currentEnvironmentFile.readline().replace("\n", "")
-# Get resources depending of the current Operating system
-currentOS = currentEnvironmentFile.readline().replace("\n", "")
-netEditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
-currentEnvironmentFile.close()
-#****#
+execfile(os.environ.get('SUMO_HOME', '.') + "/tests/netedit/neteditTestFunctions.py")
 
 # Open netedit
-netEditProcess = subprocess.Popen([neteditApp,
-                                   '--window-size', '800,600',
-                                   '--sumo-net-file', textTestSandBox + "/input_net.net.xml",
-                                   '--sumo-additionals-file', textTestSandBox + "/input_additionals.add.xml",
-                                   '--additionals-output', textTestSandBox + "/additionals.xml"],
-                                  env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
+neteditProcess = openNetedit(False)
 
-# Wait 10 seconds to netedit main windows
-try:
-    match = wait(netEditResources + "neteditToolbar.png", 10)
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'neteditToolbar.png' not found")
-
-# focus netEdit window
-click(match.getTarget().offset(0, -20))
+# Wait to netedit reference
+match = getNeteditMatch(neteditProcess)
 
 # save additionals
-click(match.getTarget().offset(-375, 5))
-click(match.getTarget().offset(-350, 265))
+neteditSaveAdditionals(match)
 
-# quit
-type("q", Key.CTRL)
-try:
-    find(netEditResources + "confirmClosingNetworkDialog.png")
-    type("y", Key.ALT)
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'confirmClosingNetworkDialog.png' not found")
+#quit without saving
+neteditQuit(False, False)
+
