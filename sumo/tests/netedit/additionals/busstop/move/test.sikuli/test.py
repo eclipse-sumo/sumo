383dfb24b772b77525dadc83dfd1d9431a91909d
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-11-26 11:52:24 +0000

    Updated test busstop/move
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22102 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionals/busstop/move/test.sikuli/test.py
+++ sumo/tests/netedit/additionals/busstop/move/test.sikuli/test.py
@@ -1,150 +1,84 @@
-# Import libraries
+# import common functions for netedit tests
 import os
-import sys
-import subprocess
-
-#** Common parameters **#
-Settings.MoveMouseDelay = 0.1
-Settings.DelayBeforeDrop = 0.1
-Settings.DelayAfterDrag = 0.1
-# SUMO Folder
-SUMOFolder = os.environ.get('SUMO_HOME', '.')
-# Current environment
-currentEnvironmentFile = open(
-    SUMOFolder + "/tests/netedit/currentEnvironment.tmp", "r")
-# Get path to netEdit app
-neteditApp = currentEnvironmentFile.readline().replace("\n", "")
-# Get SandBox folder
-textTestSandBox = currentEnvironmentFile.readline().replace("\n", "")
-# Get resources depending of the current Operating system
-currentOS = currentEnvironmentFile.readline().replace("\n", "")
-netEditResources = SUMOFolder + "/tests/netedit/imageResources/" + currentOS + "/"
-currentEnvironmentFile.close()
-#****#
+execfile(os.environ.get('SUMO_HOME', '.') + "/tests/netedit/neteditTestFunctions.py")
 
 # Open netedit
-netEditProcess = subprocess.Popen([neteditApp,
-                                   '--window-size', '800,600',
-                                   '--new',
-                                   '--additionals-output', textTestSandBox + "/additionals.xml"],
-                                  env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
+neteditProcess = openNetedit(True)
 
-# Wait 10 seconds to netedit main windows
-try:
-    match = wait(netEditResources + "neteditToolbar.png", 10)
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'neteditToolbar.png' not found")
-
-# focus netEdit window
-click(match.getTarget().offset(0, -20))
+# Wait to netedit reference
+match = getNeteditMatch(neteditProcess)
 
 # Change to create mode
 type("e")
 
 # Create two nodes
-click(match.getTarget().offset(-250, 400))
-click(match.getTarget().offset(250, 400))
+click(match.getTarget().offset(100, 300))
+click(match.getTarget().offset(300, 300))
 
 # Change to create additional
 type("a")
 
+# go to additionalsComboBox
+click(additionalsComboBox)
+
 # by default, additional is busstop, then isn't needed to select "busstop"
 
-# Go to reference mode
-click(match.getTarget().offset(-300, 300))
+# obtain match for additionalsComboBox
+additionalsComboBox = match.getTarget().offset(-75, 50)
 
-# Change to reference center
-click(match.getTarget().offset(-300, 360))
+# obtain reference for parameters (In this case, is the same as the additionalsComboBox)
+parametersReference = additionalsComboBox
+
+# change reference to center
+modifyStoppingPlaceReference(parametersReference, 6, 2)
 
 # create busstop in mode "reference center"
-click(match.getTarget().offset(100, 400))
+click(match.getTarget().offset(300, 300))
 
-# Change to movement mode
+# center edge
+rightClick(match.getTarget().offset(305, 305))
+click(match.getTarget().offset(330, 330))
+
+# change to move mode
 type("m")
 
-# Change mouse move delay
-Settings.MoveMouseDelay = 1
+# change mouse move delay
+Settings.MoveMouseDelay = 0.5
 
-# Check all possible movement combinations
-# center->Left
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(-100, 405))
-dragDrop(match.getTarget().offset(-100, 405),
-         match.getTarget().offset(100, 405))
+# move bus stop to left
+dragDrop(match.getTarget().offset(300, 235), match.getTarget().offset(200, 235))
 
-# center->Right
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(300, 405))
-dragDrop(match.getTarget().offset(300, 405),
-         match.getTarget().offset(100, 405))
+# move back
+dragDrop(match.getTarget().offset(200, 235), match.getTarget().offset(300, 235))
 
-# center->Left top
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(-100, 300))
-dragDrop(match.getTarget().offset(-100, 405),
-         match.getTarget().offset(100, 300))
+# move bus stop to right
+dragDrop(match.getTarget().offset(300, 235), match.getTarget().offset(400, 235))
 
-# center->Right top
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(300, 300))
-dragDrop(match.getTarget().offset(300, 405),
-         match.getTarget().offset(100, 300))
+# move back
+dragDrop(match.getTarget().offset(400, 235), match.getTarget().offset(300, 235))
 
-# center->Left bot
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(-100, 450))
-dragDrop(match.getTarget().offset(-100, 405),
-         match.getTarget().offset(100, 450))
+# move bus stop to left overpassing lane
+dragDrop(match.getTarget().offset(300, 235), match.getTarget().offset(50, 235))
 
-# center->Right bot
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(300, 450))
-dragDrop(match.getTarget().offset(300, 405),
-         match.getTarget().offset(100, 450))
+# move back
+dragDrop(match.getTarget().offset(100, 235), match.getTarget().offset(300, 235))
 
-# center->Left corner
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(-300, 405))
-dragDrop(match.getTarget().offset(-120, 405),
-         match.getTarget().offset(100, 405))
+# move bus stop to right overpassing lane
+dragDrop(match.getTarget().offset(300, 235), match.getTarget().offset(550, 235))
 
-# center->Right corner
-dragDrop(match.getTarget().offset(100, 405),
-         match.getTarget().offset(390, 405))
-dragDrop(match.getTarget().offset(320, 405),
-         match.getTarget().offset(100, 405))
+# move back
+dragDrop(match.getTarget().offset(500, 235), match.getTarget().offset(300, 235))
 
-# Change mouse move delay
+# set back mouse
 Settings.MoveMouseDelay = 0.1
 
-# Check Undo (16 movements)
-for x in range(0, 16):
-    try:
-        click(match.getTarget().offset(-325, 5))
-        click(netEditResources + "undoredo/edit-undo.png")
-    except:
-        netEditProcess.kill()
-        sys.exit("Killed netedit process. 'edit-undo.png' not found")
-
-# Check Redo (16 movements)
-for x in range(0, 16):
-    try:
-        click(match.getTarget().offset(-325, 5))
-        click(netEditResources + "undoredo/edit-redo.png")
-    except:
-        netEditProcess.kill()
-        sys.exit("Killed netedit process. 'edit-redo.png' not found")
+# Check undos and redos
+neteditUndo(neteditProcess, match, 10)
+neteditRedo(neteditProcess, match, 10)
 
 # save additionals
-click(match.getTarget().offset(-375, 5))
-click(match.getTarget().offset(-350, 265))
+neteditSaveAdditionals(match)
 
-# quit
-type("q", Key.CTRL)
-try:
-    find(netEditResources + "confirmClosingNetworkDialog.png")
-    type("y", Key.ALT)
-except:
-    netEditProcess.kill()
-    sys.exit("Killed netedit process. 'confirmClosingNetworkDialog.png' not found")
+# quit netedit without saving
+neteditQuit(True, True)
+
