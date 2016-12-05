31948bc1b703a3178344ca1f554468a911c1be47
Michael Behrisch <oss@behrisch.de>
2016-12-05 12:05:55 +0000

    failsafe lock removal for history script
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22192 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/history.py
+++ sumo/tools/build/history.py
@@ -24,6 +24,7 @@ import optparse
 import shutil
 import os
 import sys
+import traceback
 
 optParser = optparse.OptionParser()
 optParser.add_option("-b", "--begin", type="int",
@@ -38,27 +39,29 @@ LOCK = "history.lock"
 if os.path.exists(LOCK):
     sys.exit("History building is still locked!")
 open(LOCK, 'w').close()
-
-if not options.end:
-    for line in subprocess.check_output('svn info http://svn.code.sf.net/p/sumo/code/trunk/sumo', shell=True).splitlines():
-        l = line.split()
-        if len(l) == 2 and l[0] == "Revision:":
-            options.end = int(l[1])
-for rev in range(options.begin, options.end + 1, options.step):
-    if not os.path.exists('bin%s' % rev):
-        ret = subprocess.call(
-            'svn up --ignore-externals -r %s sumo' % rev, shell=True)
-        if ret != 0:
-            break
-        subprocess.call('cd sumo; make clean; make -j 16; cd ..', shell=True)
-        shutil.copytree('sumo/bin', 'bin%s' % rev,
-                        ignore=shutil.ignore_patterns('Makefile*', '*.bat', '*.jar'))
-        subprocess.call('strip -R .note.gnu.build-id bin%s/*' %
-                        rev, shell=True)
-        subprocess.call(
-            "sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' bin%s/*" % (rev, rev), shell=True)
-for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
-    dups = line.split()
-    for d in dups[1:]:
-        subprocess.call('ln -sf ../%s %s' % (dups[0], d), shell=True)
+try:
+    if not options.end:
+        for line in subprocess.check_output('svn info http://svn.code.sf.net/p/sumo/code/trunk/sumo', shell=True).splitlines():
+            l = line.split()
+            if len(l) == 2 and l[0] == "Revision:":
+                options.end = int(l[1])
+    for rev in range(options.begin, options.end + 1, options.step):
+        if not os.path.exists('bin%s' % rev):
+            ret = subprocess.call(
+                'svn up --ignore-externals -r %s sumo' % rev, shell=True)
+            if ret != 0:
+                break
+            subprocess.call('cd sumo; make clean; make -j 16; cd ..', shell=True)
+            shutil.copytree('sumo/bin', 'bin%s' % rev,
+                            ignore=shutil.ignore_patterns('Makefile*', '*.bat', '*.jar'))
+            subprocess.call('strip -R .note.gnu.build-id bin%s/*' %
+                            rev, shell=True)
+            subprocess.call(
+                "sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' bin%s/*" % (rev, rev), shell=True)
+    for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
+        dups = line.split()
+        for d in dups[1:]:
+            subprocess.call('ln -sf ../%s %s' % (dups[0], d), shell=True)
+except:
+    traceback.print_exc()
 os.remove(LOCK)
