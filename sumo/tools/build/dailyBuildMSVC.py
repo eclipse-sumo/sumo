d0ea5185fdac3c0bfef4ac739c0eae82404e6b31
Michael Behrisch <oss@behrisch.de>
2016-12-06 09:23:14 +0000

    disabling sikulix server start to make tests (hopefully) more reliable, refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22201 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tools/build/dailyBuildMSVC.py
+++ sumo/tools/build/dailyBuildMSVC.py
@@ -108,8 +108,8 @@ def runTests(options, env, svnrev, debugSuffix=""):
         subprocess.call([ttBin, "-a", "sumo.gui"] + fullOpt, env=env,
                         stdout=log, stderr=subprocess.STDOUT, shell=True)
         # Check if sikulixServer is already opened
-        if runSikulixServer.checkStatus() == False :
-            runSikulixServer.startSikulixServer()
+        #if runSikulixServer.checkStatus() == False :
+        #    runSikulixServer.startSikulixServer()
         subprocess.call([ttBin, "-a", "netedit.daily"] + fullOpt, env=env,
                         stdout=log, stderr=subprocess.STDOUT, shell=True)
     subprocess.call([ttBin, "-b", env["FILEPREFIX"], "-coll"], env=env,
