90ed93d90dcf66259b694b0af82e3cbd21746533
Jakob Erdmann <jakob.erdmann@dlr.de>
2016-12-09 14:14:18 +0000

    added test fix #2738 (was already implemented)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22249 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/tools/sumolib/net_roundabout/runner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Jakob Erdmann
+@version $Id: runner.py 22026 2016-11-17 08:15:22Z behrisch $
+
+This script tests sumolib functions
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2016 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+import os
+import sys
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import sumolib.net
+
+net = sumolib.net.readNet(sys.argv[1])
+for r in net.getRoundabouts():
+    print("nodes=%s edges=%s" % (
+        [n for n in r.getNodes()],
+        [e for e in r.getEdges()]))
