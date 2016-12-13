e968df89952ba30c1c21855a6ee9335921bf5508
Leonhard Luecken <leonhard.luecken@dlr.de>
2016-12-13 07:53:11 +0000

    added tutorial circles (in tests/complex).
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22278 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/tutorial/circles/runner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Daniel Krajzewicz
+@author  Michael Behrisch
+@date    2007-10-25
+@version $Id: runner.py 20433 2016-04-13 08:00:14Z behrisch $
+
+This script is a test runner for the "Driving in Circles" Tutorial.
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2016 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+import os
+import subprocess
+import sys
+import shutil
+try:
+    sys.path.append(os.path.join(os.path.dirname(
+        __file__), '..', '..', '..', '..', "tools"))  # tutorial in tests
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
+        os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+    from sumolib import checkBinary
+except ImportError:
+    sys.exit(
+        "please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
+sumoBinary = checkBinary('sumo')
+# run simulation
+retcode = subprocess.call(
+    [sumoBinary, "-c", "data/circles.sumocfg", "--no-step-log"], stdout=sys.stdout, stderr=sys.stderr)
+print(">> Simulation closed with status %s" % retcode)
+sys.stdout.flush()
