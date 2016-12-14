be8def2886de9455c799a6edb6b185d07ddbd65e
Leonhard Luecken <leonhard.luecken@dlr.de>
2016-12-14 20:48:50 +0000

    added sumolympic tutorial files
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22300 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/tutorial/sumolympics/data/makeSumolympicWalkers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+outfile = "sumolympicWalks.rou.xml"
+startEdge = "beg"
+endEdge   = "end"
+# Startzeit
+departTime = 0.
+# Startposition
+departPos = -30.
+# Anzahl Fussgaenger
+numberTrips = 200
+# Generiere XML Datei
+xml_string = "<routes>\n"
+for i in range(numberTrips):
+	xml_string += '    <person depart="%f" id="p%d">\n'%(departTime, i)
+	xml_string += '        <walk departPos="%f" edges="%s %s"/>\n'%(departPos, startEdge, endEdge)
+	xml_string += '    </person>\n'
+xml_string += "</routes>\n"
+with open(outfile, "w") as f:
+    f.write(xml_string)
