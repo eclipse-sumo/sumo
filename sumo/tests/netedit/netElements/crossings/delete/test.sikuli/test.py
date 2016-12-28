44d0f754fb51dbda4f4bd5b4c48105d81a6b31f0
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-12-28 16:08:24 +0000

    Added test netedit/netElements/crossings/delete. the output net has still traces of crossings (:A_w0 and :B_w0). Ref #2758
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22377 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/netElements/crossings/delete/test.sikuli/test.py
@@ -0,0 +1,36 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, True)
+
+# Rebuild network
+netedit.rebuildNetwork()
+
+# zoom in central node
+netedit.zoomIn(match.getTarget().offset(325, 225), 10)
+
+# set crossing mode
+type("d")
+
+# delete first crossing
+click(match.getTarget().offset(250, 225))
+
+# delete second crossing
+click(match.getTarget().offset(385, 225))
+
+# check undo redo
+netedit.undo(match, 2)
+netedit.redo(match, 2)
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+#netedit.quit(neteditProcess, False, False)
