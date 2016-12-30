4001924f20bb02be3d1b12188353a0afb9b3ba54
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-12-30 12:11:30 +0000

    Added netedit test additionalElements/containerstop/lock_unlock.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22401 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/containerstop/lock_unlock/test.sikuli/test.py
@@ -0,0 +1,81 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select containerStop
+netedit.changeAdditional(comboboxAdditional, 3)
+
+# change reference to center
+netedit.modifyStoppingPlaceReference(comboboxAdditional, 8, 2)
+
+# create containerStop in mode "reference center"
+netedit.leftClick(match, 250, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move containerStop to right
+netedit.moveElement(match, 150, 275, 250, 275)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect containerStop
+netedit.leftClick(match, 300, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# block additional
+netedit.changeBlockAdditional(comboboxAdditional, 5)
+
+# change to move mode
+netedit.moveMode()
+
+# try to move containerStop to right (must be blocked)
+netedit.moveElement(match, 250, 275, 350, 275)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect containerStop
+netedit.leftClick(match, 300, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# unblock additional
+netedit.changeBlockAdditional(comboboxAdditional, 5)
+
+# change to move mode
+netedit.moveMode()
+
+# move containerStop to right (must be allowed)
+netedit.moveElement(match, 250, 275, 350, 275)
+
+# Check undos and redos
+netedit.undo(match, 5)
+netedit.redo(match, 5)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
