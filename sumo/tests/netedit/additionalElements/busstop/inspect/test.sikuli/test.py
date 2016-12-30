62612a9c42889271ae4a65866a17fde3529cc089
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-12-30 10:49:37 +0000

    updated netedit tests: additionalElements/busstop/inspect
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22388 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionalElements/busstop/inspect/test.sikuli/test.py
+++ sumo/tests/netedit/additionalElements/busstop/inspect/test.sikuli/test.py
@@ -1,3 +1,4 @@
+# import common functions for netedit tests
 import os
 import sys
 
@@ -7,57 +8,37 @@ sys.path.append(neteditTestRoot)
 import neteditTestFunctions as netedit
 
 # Open netedit
-neteditProcess, match = netedit.setupAndStart(neteditTestRoot, True)
-
-# obtain match for additionalsComboBox
-additionalsComboBox = match.getTarget().offset(-75, 50)
-
-# Focus netedit window
-click(match)
-
-# Change to create mode
-type("e")
-
-# Create first two nodes
-click(match.getTarget().offset(100, 150))
-click(match.getTarget().offset(500, 150))
-
-# Create second two nodes
-click(match.getTarget().offset(100, 300))
-click(match.getTarget().offset(500, 300))
-
-# Change to inspect
-type("i")
-
-# get menu of lane and duplicate it
-rightClick(match.getTarget().offset(150, 160))
-click(match.getTarget().offset(160, 460))
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
 
 # Change to create additional
-type("a")
+netedit.additionalMode()
 
-# by default, additional is busstop, then isn't needed to select "busstop"
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
 
-# obtain reference for parameters (In this case, is the same as the additionalsComboBox)
-parametersReference = additionalsComboBox
+# select busstop
+netedit.changeAdditional(comboboxAdditional, 0)
 
 # change reference to center
-netedit.modifyStoppingPlaceReference(parametersReference, 6, 2)
+netedit.modifyStoppingPlaceReference(comboboxAdditional, 8, 2)
 
-# create first busstop in mode "reference center"
-click(match.getTarget().offset(400, 150))
+# create busstop 1 in mode "reference center left"
+netedit.leftClick(match, 250, 150)
 
-# create second busstop in mode "reference center"
-click(match.getTarget().offset(400, 300))
+# create busstop 2 in mode "reference center left"
+netedit.leftClick(match, 450, 150)
 
 # Change to inspect mode
 type("i")
 
 # inspect first busstop
-click(match.getTarget().offset(400, 170))
+netedit.leftClick(match, 250, 170)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
 
 # Change parameter 0 with a non valid value (Duplicated ID)
-netedit.modifyAttribute(parametersReference, 0, "busStop_gneE1_0_1")
+netedit.modifyAttribute(parametersReference, 0, "busStop_gneE2_1_1")
 
 # Change parameter 0 with a valid value
 netedit.modifyAttribute(parametersReference, 0, "correct ID")
@@ -68,8 +49,11 @@ netedit.modifyAttribute(parametersReference, 1, "dummy lane")
 # Change parameter 1 with a valid value (different edge)
 netedit.modifyAttribute(parametersReference, 1, "gneE0_0")
 
-# Change parameter 1 with a valid value (same edge, different lane)
-netedit.modifyAttribute(parametersReference, 1, "gneE0_1")
+# Change parameter 1 with a valid value (original edge, same lane)
+netedit.modifyAttribute(parametersReference, 1, "gneE2_1")
+
+# Change parameter 1 with a valid value (original edge, different lane)
+netedit.modifyAttribute(parametersReference, 1, "gneE2_0")
 
 # Change parameter 2 with a non valid value (negative)
 netedit.modifyAttribute(parametersReference, 2, "-5")
@@ -111,8 +95,11 @@ click(match);
 netedit.undo(match, 13)
 netedit.redo(match, 13)
 
-# Save additionals
-netedit.saveAdditionals(match)
+# save additionals
+netedit.saveAdditionals()
 
-# quit netedit saving net
-netedit.quit(neteditProcess, True, True)
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
