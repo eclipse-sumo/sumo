2e3f10a333b492920d42264b155cfc9000b4dd2f
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-12-30 12:12:01 +0000

    Added netedit test additionalElements/containerstop/move.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22402 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/containerstop/move/test.sikuli/test.py
@@ -0,0 +1,66 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select containerStop
+netedit.changeAdditional(comboboxAdditional, 3)
+
+# change reference to center
+netedit.modifyStoppingPlaceReference(comboboxAdditional, 8, 2)
+
+# create containerStop in mode "reference center"
+netedit.leftClick(match, 250, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move containerStop to left
+netedit.moveElement(match, 150, 275, 50, 275)
+
+# move back
+netedit.moveElement(match, 50, 275, 150, 275)
+
+# move containerStop to right
+netedit.moveElement(match, 150, 275, 250, 275)
+
+# move back
+netedit.moveElement(match, 250, 275, 150, 275)
+
+# move containerStop to left overpassing lane
+netedit.moveElement(match, 150, 275, -100, 275)
+
+# move back
+netedit.moveElement(match, 0, 275, 150, 275)
+
+# move containerStop to right overpassing lane
+netedit.moveElement(match, 150, 275, 550, 275)
+
+# move back to another different position of initial
+netedit.moveElement(match, 500, 275, 300, 275)
+
+# Check undos and redos
+netedit.undo(match, 10)
+netedit.redo(match, 10)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
