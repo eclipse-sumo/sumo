b168508a46e3c7a16ab6cd87c58467dc9c32e472
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2016-12-30 12:10:21 +0000

    Added netedit test additionalElements/containerstop/delete.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22398 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/containerstop/delete/test.sikuli/test.py
@@ -0,0 +1,45 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select containerStop
+netedit.changeAdditional(comboboxAdditional, 3)
+
+# create containerStop in mode "reference left"
+netedit.leftClick(match, 250, 250)
+
+# Change to delete
+netedit.deleteMode()
+
+# delete created containerStop
+netedit.leftClick(match, 260, 250)
+
+# delete loaded containerStop
+netedit.leftClick(match, 450, 250)
+
+# Check undo redo
+netedit.undo(match, 2)
+netedit.redo(match, 2)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
