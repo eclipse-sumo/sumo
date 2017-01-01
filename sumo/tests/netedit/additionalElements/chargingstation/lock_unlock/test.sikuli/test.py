ea5c7cc2bf7664493241de217d4d5e88199436a1
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-01 18:04:36 +0000

    Added netedit test additionalElements/chargingstation/lock_unlock.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22405 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/chargingstation/lock_unlock/test.sikuli/test.py
@@ -0,0 +1,81 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select chargingStation
+netedit.changeAdditional(comboboxAdditional, 2)
+
+# change reference to center
+netedit.modifyStoppingPlaceReference(comboboxAdditional, 7, 2)
+
+# create chargingStation in mode "reference center"
+netedit.leftClick(match, 250, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move chargingStation to right
+netedit.moveElement(match, 150, 250, 250, 250)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect chargingStation
+netedit.leftClick(match, 300, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# block additional
+netedit.changeBlockAdditional(comboboxAdditional, 8)
+
+# change to move mode
+netedit.moveMode()
+
+# try to move chargingStation to right (must be blocked)
+netedit.moveElement(match, 250, 250, 350, 250)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect chargingStation
+netedit.leftClick(match, 300, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# unblock additional
+netedit.changeBlockAdditional(comboboxAdditional, 8)
+
+# change to move mode
+netedit.moveMode()
+
+# move chargingStation to right (must be allowed)
+netedit.moveElement(match, 250, 250, 350, 250)
+
+# Check undos and redos
+netedit.undo(match, 5)
+netedit.redo(match, 5)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
