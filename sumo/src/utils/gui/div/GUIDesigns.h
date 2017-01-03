a1983fd169f7eedafc59c46a51bcb6e20abb4980
Michael Behrisch <oss@behrisch.de>
2017-01-03 15:35:54 +0000

    cleaning file headers, removing unused file, refs #2, #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22432 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/utils/gui/div/GUIDesigns.h
+++ sumo/src/utils/gui/div/GUIDesigns.h
@@ -6,14 +6,14 @@
 ///
 /// File with the definitions of standard style of FXObjects in SUMO
 /****************************************************************************/
-// SUMO, Simulation of Urban MObility; see http://sumo-sim.org/
-// Copyright (C) 2001-2013 DLR (http://www.dlr.de/) and contributors
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2016 DLR (http://www.dlr.de/) and contributors
 /****************************************************************************/
 //
 //   This file is part of SUMO.
-//   SUMO is free software; you can redistribute it and/or modify
+//   SUMO is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 3 of the License, or
+//   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
 //
 /****************************************************************************/
