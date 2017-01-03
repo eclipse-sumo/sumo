11ff0aa8960057273a7e0af3a2057d0296a0d22b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-03 10:45:07 +0000

    Added netedit test additionalElements/e1detector/delete.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22427 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/e1detector/delete/test.sikuli/test.py
@@ -0,0 +1,45 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select E1
+netedit.changeAdditional(comboboxAdditional, 6)
+
+# create E1
+netedit.leftClick(match, 250, 250)
+
+# Change to delete
+netedit.deleteMode()
+
+# delete created E1
+netedit.leftClick(match, 250, 250)
+
+# delete loaded E1
+netedit.leftClick(match, 450, 250)
+
+# Check undo redo
+netedit.undo(match, 2)
+netedit.redo(match, 2)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
