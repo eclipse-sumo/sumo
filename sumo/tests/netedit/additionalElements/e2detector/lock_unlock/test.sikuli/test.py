cdb0b5136da2838731fff854653189daab146311
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-04 09:59:33 +0000

    Added netedit test additionalElements/e2detector/lock_unlock.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22442 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/e2detector/lock_unlock/test.sikuli/test.py
@@ -0,0 +1,78 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select E2
+netedit.changeAdditional(comboboxAdditional, 7)
+
+# create E2
+netedit.leftClick(match, 240, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move E2 to right
+netedit.moveElement(match, 120, 250, 250, 250)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect E2
+netedit.leftClick(match, 350, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# block additional
+netedit.changeBlockAdditional(comboboxAdditional, 10)
+
+# change to move mode
+netedit.moveMode()
+
+# try to move E2 to right (must be blocked)
+netedit.moveElement(match, 250, 250, 350, 250)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect E2
+netedit.leftClick(match, 350, 250)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# unblock additional
+netedit.changeBlockAdditional(comboboxAdditional, 10)
+
+# change to move mode
+netedit.moveMode()
+
+# move E2 to right (must be allowed)
+netedit.moveElement(match, 250, 250, 350, 250)
+
+# Check undos and redos
+netedit.undo(match, 5)
+netedit.redo(match, 5)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
