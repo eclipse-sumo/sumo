30f72d9fd22d8babeb114adba0c5fd268efb5c7d
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-04 09:06:13 +0000

    Added netedit test additionalElements/e1detector/move.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22438 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/e1detector/move/test.sikuli/test.py
@@ -0,0 +1,63 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select E1
+netedit.changeAdditional(comboboxAdditional, 6)
+
+# create E1
+netedit.leftClick(match, 250, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move E1 to left
+netedit.moveElement(match, 120, 250, 50, 250)
+
+# move back
+netedit.moveElement(match, 50, 250, 120, 250)
+
+# move E1 to right
+netedit.moveElement(match, 120, 250, 250, 250)
+
+# move back
+netedit.moveElement(match, 250, 250, 120, 250)
+
+# move E1 to left overpassing lane
+netedit.moveElement(match, 120, 250, -150, 250)
+
+# move back
+netedit.moveElement(match, -100, 250, 120, 250)
+
+# move E1 to right overpassing lane
+netedit.moveElement(match, 120, 250, 580, 250)
+
+# move back to another different position of initial
+netedit.moveElement(match, 520, 250, 300, 250)
+
+# Check undos and redos
+netedit.undo(match, 10)
+netedit.redo(match, 10)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
