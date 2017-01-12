5a9568eca3252fc63f311c2c36ad6c0e40afa0e5
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-12 10:18:22 +0000

    Added netedit test additionalElements/e3detector/delete.
    Found Problem removing childs of Additionals
    
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22573 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/e3detector/delete/test.sikuli/test.py
@@ -0,0 +1,64 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# apply zoom 
+netedit.zoomIn(match.getTarget().offset(325, 200), 10)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select E3
+netedit.changeAdditional(comboboxAdditional, 8)
+
+# create E3 1
+netedit.leftClick(match, 100, 100)
+
+# create E3 2
+netedit.leftClick(match, 300, 100)
+
+# select entry detector
+netedit.changeAdditional(comboboxAdditional, -4)
+
+# Create Entry detectors for E3 2
+netedit.selectChild(comboboxAdditional, 4, 3)
+netedit.leftClick(match, 150, 250)
+
+# Change to delete
+netedit.deleteMode()
+
+# delete created E3 1
+netedit.leftClick(match, 100, 100)
+
+# delete created E3 2
+netedit.leftClick(match, 300, 100)
+
+# delete loaded E3 1
+netedit.leftClick(match, 400, 100)
+
+# delete loaded E3 2
+netedit.leftClick(match, 500, 100)
+
+# Check undo redo
+netedit.undo(match, 4)
+netedit.redo(match, 4)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
