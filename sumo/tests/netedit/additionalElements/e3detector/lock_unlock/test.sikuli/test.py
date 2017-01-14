860daa58181dadd497a825af3f9a9b8e7589b43e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-01-14 11:56:33 +0000

    Added netedit test additionalElements/e3detector/lock_unlock.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22587 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/e3detector/lock_unlock/test.sikuli/test.py
@@ -0,0 +1,88 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# apply zoom 
+netedit.zoomIn(match.getTarget().offset(325, 200), 10)
+
+# Change to create additional
+netedit.additionalMode()
+
+# obtain match for comboboxAdditional
+comboboxAdditional = netedit.getComboBoxAdditional(match)
+
+# select E3
+netedit.changeAdditional(comboboxAdditional, 8)
+
+# create E3
+netedit.leftClick(match, 250, 250)
+
+# select entry detector
+netedit.changeAdditional(comboboxAdditional, -4)
+
+# Create Entry detector E3 (for saving)
+netedit.selectChild(comboboxAdditional, 4, 1)
+netedit.leftClick(match, 400, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move E3 to up
+netedit.moveElement(match, 120, 250, 120, 50)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect E3
+netedit.leftClick(match, 250, 100)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# block additional
+netedit.changeBlockAdditional(comboboxAdditional, 7)
+
+# change to move mode
+netedit.moveMode()
+
+# try to move E3 to right (must be blocked)
+netedit.moveElement(match, 120, 50, 300, 50)
+
+# Change to inspect mode
+netedit.inspectMode()
+
+# inspect E3
+netedit.leftClick(match, 250, 100)
+
+# obtain parameters reference
+parametersReference = netedit.getParametersReference(match)
+
+# unblock additional
+netedit.changeBlockAdditional(comboboxAdditional, 7)
+
+# change to move mode
+netedit.moveMode()
+
+# move E3 to right (must be allowed)
+netedit.moveElement(match, 120, 50, 300, 50)
+
+# Check undos and redos
+netedit.undo(match, 6)
+netedit.redo(match, 6)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
