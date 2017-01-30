1697c24df056e00a8802f30ed2b01faec63d7fc3
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-01-30 13:17:03 +0000

    added test refs #2808. seems to be working
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22772 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/rerouting/rerouteTraveltime_adaptEdgeWeight/runner.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Jakob Erdmann
+@date    2017-01-23
+@version $Id: runner.py 22698 2017-01-25 13:24:38Z behrisch $
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2017 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import subprocess
+import sys
+import random
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci
+import sumolib
+
+sumoBinary = os.environ["SUMO_BINARY"]
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen([sumoBinary,
+                                '-c', 'sumo.sumocfg',
+                                '-S', '-Q',
+                                '--remote-port', str(PORT)], stdout=sys.stdout)
+
+def checkMiddleEdges():
+    now = traci.simulation.getCurrentTime() / 1000
+    for edge in ["middle", "middle2", "middle3"]:
+        print("edge=%s current=%s adapted=%s" % (edge,
+            traci.edge.getTraveltime(edge),
+            traci.edge.getAdaptedTraveltime(edge, now)))
+
+vehID = "ego"
+traci.init(PORT)
+traci.simulationStep()
+checkMiddleEdges()
+traci.edge.adaptTraveltime("middle", 20)
+traci.edge.adaptTraveltime("middle3", 14)
+checkMiddleEdges()
+traci.vehicle.rerouteTraveltime(vehID, False)
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+traci.close()
+sumoProcess.wait()
