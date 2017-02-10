33bc9932fb71dbb4af481103ab780973407cccc8
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-02-10 11:58:47 +0000

    Added netedit test netElements/crossings/delete_TLS.
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@22907 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/netElements/crossings/delete_TLS/test.sikuli/test.py
@@ -0,0 +1,32 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# Rebuild network
+netedit.rebuildNetwork()
+
+# zoom in central node
+netedit.zoomIn(match.getTarget().offset(325, 225), 10)
+
+# set delete mode
+type("d")
+
+# delete junction 
+click(match.getTarget().offset(300, 250))
+
+# check undo
+netedit.undo(match, 1)
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit 
+netedit.quit(neteditProcess, False, False)
