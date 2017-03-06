b7489dd36da947639fb78832abaef6d30d926703
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-06 11:51:02 +0000

    Added and enabled netedit test selection/select_setoperators
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23224 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/selection/select_setoperators/test.sikuli/test.py
@@ -0,0 +1,67 @@
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# first rebuild network
+netedit.rebuildNetwork()
+
+# go to select mode
+netedit.selectMode()
+
+# netedit selection reference
+selectType = netedit.getSelectReference(match)
+
+# first check that invert works
+click(selectType)
+for x in range(0, 10):
+	type(Key.TAB)
+type(Key.SPACE)
+
+# invert again and delete selected items (all must be unselected)
+click(selectType)
+for x in range(0, 10):
+	type(Key.TAB)
+type(Key.SPACE)
+netedit.deleteSelectedItems()
+
+# invert again and clear selection
+click(selectType)
+for x in range(0, 10):
+	type(Key.TAB)
+type(Key.SPACE)
+click(selectType)
+for x in range(0, 9):
+	type(Key.TAB)
+type(Key.SPACE)
+
+# use a rectangle to select central elements 
+netedit.selectionRectangle(match, 250, 150, 400, 300)
+
+# invert selection to select only extern nodes and delete it
+click(selectType)
+for x in range(0, 10):
+	type(Key.TAB)
+type(Key.SPACE)
+netedit.deleteSelectedItems()
+
+# check undo and redo
+netedit.undo(match, 1)
+netedit.redo(match, 2)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess, False, False)
