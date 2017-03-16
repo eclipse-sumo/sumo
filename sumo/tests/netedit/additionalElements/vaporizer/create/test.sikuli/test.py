499bdbc890d19fb3152d67a423259acc47157477
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-16 13:58:38 +0000

    added netedit test aadditionalElements/vaporizer/create
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23485 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/vaporizer/create/test.sikuli/test.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 23331 2017-03-12 11:09:06Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select vaporizer
+netedit.changeAdditional("vaporizer")
+
+# create vaporizer (camera will be moved)
+netedit.leftClick(match, 250, 220)
+
+# create another vaporizer with the same default attributes (camera will be moved)
+netedit.leftClick(match, 250, 220)
+
+# change default start
+netedit.modifyAdditionalDefaultValue(2, "10")
+
+# create vaporizer (camera will be moved)
+netedit.leftClick(match, 250, 220)
+
+# change default end
+netedit.modifyAdditionalDefaultValue(3, "20")
+
+# create vaporizer
+netedit.leftClick(match, 250, 220)
+
+# change default start (Invalid, end > startTime)
+netedit.modifyAdditionalDefaultValue(2, "50")
+
+# try to create invalid vaporizer (show warning
+netedit.leftClick(match, 250, 220)
+
+# change default end (valid))
+netedit.modifyAdditionalDefaultValue(2, "100")
+
+# Check undo redo
+netedit.undo(match, 4)
+netedit.redo(match, 4)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess, False, False)
