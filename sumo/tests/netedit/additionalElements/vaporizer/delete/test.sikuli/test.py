95b7dcfa13f94d9729d56bbbea81218b01f1e722
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-17 09:22:23 +0000

    added netedit test additionalElements/vaporizer/delete
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23496 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/vaporizer/delete/test.sikuli/test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 23331 2017-03-12 11:09:06Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select vaporizer
+netedit.changeAdditional("vaporizer")
+
+# create vaporizer
+netedit.leftClick(match, 350, 200)
+
+# Change to delete
+netedit.deleteMode()
+
+# delete loaded vaporizer
+netedit.leftClick(match, 310, 200)
+
+# delete created vaporizer (using stack)
+netedit.leftClick(match, 310, 200)
+
+# delete lane with the second loaded vaporizer
+netedit.leftClick(match, 310, 250)
+
+# Check undo
+netedit.undo(match, 3)
+
+# Change to delete
+netedit.deleteMode()
+
+# disable 'Automatically delete additionals'
+netedit.changeAutomaticallyDeleteAdditionals(match)
+
+# try to delete lane with the second loaded vaporizer (doesn't allowed)
+netedit.leftClick(match, 310, 250)
+
+# wait warning
+netedit.waitAutomaticallyDeleteAdditionalsWarning()
+
+# check redo
+netedit.redo(match, 3)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess, False, False)
