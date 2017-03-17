b15300e831a16a22b0e5ba9988397ba4090e13bc
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-17 14:40:17 +0000

    Updated GNECalibratorFlowDialog
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23521 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/netedit/GNECalibratorFlowDialog.h
@@ -0,0 +1,98 @@
+/****************************************************************************/
+/// @file    GNECalibratorFlowDialog.h
+/// @author  Pablo Alvarez Lopez
+/// @date    March 2017
+/// @version $Id$
+///
+/// Dialog for edit calibrator flows
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef GNECalibratorFlowDialog_h
+#define GNECalibratorFlowDialog_h
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include "GNEAdditionalDialog.h"
+#include "GNECalibratorFlow.h"
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+
+class GNECalibratorDialog;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+/**
+ * @class GNECalibratorFlowDialog
+ * @brief Dialog for edit rerouter intervals
+ */
+
+class GNECalibratorFlowDialog : public GNEAdditionalDialog {
+    /// @brief FOX-declaration
+    FXDECLARE(GNECalibratorFlowDialog)
+
+public:
+    /// @brief constructor
+    GNECalibratorFlowDialog(GNECalibratorDialog* calibratorDialog, GNECalibratorFlow &calibratorFlow);
+
+    /// @brief destructor
+    ~GNECalibratorFlowDialog();
+
+    /// @name FOX-callbacks
+    /// @{
+    /// @brief event after press accept button
+    long onCmdAccept(FXObject*, FXSelector, void*);
+
+    /// @brief event after press cancel button
+    long onCmdCancel(FXObject*, FXSelector, void*);
+
+    /// @brief event after press reset button
+    long onCmdReset(FXObject*, FXSelector, void*);
+    /// @}
+
+protected:
+    /// @brief FOX needs this
+    GNECalibratorFlowDialog() {}
+
+    /// @brief pointer to GNECalibratorDialog parent
+    GNECalibratorDialog* myCalibratorDialogParent;
+
+    /// @brief pointer to rerouter interval
+    GNECalibratorFlow* myCalibratorFlow;
+
+    /// @brief flag to check if current calibrator flow is valid
+    bool myCalibratorFlowValid;
+
+    /// @brief update data fields
+    void updateCalibratorFlowValues();
+
+private:
+    /// @brief Invalidated copy constructor.
+    GNECalibratorFlowDialog(const GNECalibratorFlowDialog&);
+
+    /// @brief Invalidated assignment operator.
+    GNECalibratorFlowDialog& operator=(const GNECalibratorFlowDialog&);
+};
+
+#endif
