60ca8f76bda96abfa580f596d3e49d4557ba6b0a
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-17 09:44:59 +0000

    added netedit test additionalElements/vaporizer/inspect
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23500 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/vaporizer/inspect/test.sikuli/test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 23331 2017-03-12 11:09:06Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select vaporizer
+netedit.changeAdditional("vaporizer")
+
+# create vaporizer
+netedit.leftClick(match, 250, 120)
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect first vaporizer
+netedit.leftClick(match, 310, 200)
+
+# Change parameter 0 with a non valid value (dummy edge)
+netedit.modifyAttribute(0, "dummyEdge")
+
+# Change parameter 0 with a valid value (different edge)
+netedit.modifyAttribute(0, "gneE0")
+
+# Change parameter 1 with a non valid value (dummy)
+netedit.modifyAttribute(1, "dummy")
+
+# Change parameter 1 with a non valid value (negative)
+netedit.modifyAttribute(1, "-10")
+
+# Change parameter 1 with a non valid value (greather than end)
+netedit.modifyAttribute(1, "50")
+
+# Change parameter 1 with a valid value
+netedit.modifyAttribute(1, "5")
+
+# Change parameter 2 with a non valid value (dummy)
+netedit.modifyAttribute(2, "dummy")
+
+# Change parameter 2 with a non valid value (negative)
+netedit.modifyAttribute(2, "-10")
+
+# Change parameter 2 with a non valid value (minor than startTime)
+netedit.modifyAttribute(2, "2")
+
+# Change parameter 2 with a valid value
+netedit.modifyAttribute(2, "20")
+
+# go to a empty area
+click(match)
+
+# Check undos and redos
+netedit.undo(match, 7)
+netedit.redo(match, 7)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess, False, False)
