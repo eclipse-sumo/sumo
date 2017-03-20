562f1c63a35443c186878fc717dc2781236e870b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-03-20 09:03:20 +0000

    Finished GNECalibratorVehicleType
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23541 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netedit/GNECalibratorVehicleType.cpp
+++ sumo/src/netedit/GNECalibratorVehicleType.cpp
@@ -88,6 +88,155 @@ GNECalibratorVehicleType::getVehicleTypeID() const {
 }
 
 
+double 
+GNECalibratorVehicleType::getAccel() const {
+    return myAccel;
+}
+
+
+double 
+GNECalibratorVehicleType::getDecel() const {
+    return myDecel;
+}
+
+
+double 
+GNECalibratorVehicleType::getSigma() const {
+    return mySigma;
+}
+
+
+double 
+GNECalibratorVehicleType::getTau() const {
+    return myTau;
+}
+
+
+double 
+GNECalibratorVehicleType::getLength() const {
+    return myLength;
+}
+
+
+double
+GNECalibratorVehicleType::getMinGap() const {
+    return myMinGap;
+}
+
+
+double 
+GNECalibratorVehicleType::getMaxSpeed() const {
+    return myMaxSpeed;
+}
+
+
+double 
+GNECalibratorVehicleType::getSpeedFactor() const {
+    return mySpeedFactor;
+}
+
+
+double 
+GNECalibratorVehicleType::getSpeedDev() const {
+    return mySpeedDev;
+}
+
+
+std::string 
+GNECalibratorVehicleType::getColor() const {
+    return myColor;
+}
+
+
+SUMOVehicleClass
+GNECalibratorVehicleType::getVClass() const {
+    return myVClass;
+}
+
+
+std::string 
+GNECalibratorVehicleType::getEmissionClass() const {
+    return myEmissionClass;
+}
+
+
+SUMOVehicleShape
+GNECalibratorVehicleType::getShape() const {
+    return myShape;
+}
+
+
+double 
+GNECalibratorVehicleType::getWidth() const {
+    return myWidth;
+}
+
+
+std::string
+GNECalibratorVehicleType::getFilename() const {
+    return myFilename;
+}
+
+
+double
+GNECalibratorVehicleType::getImpatience() const {
+    return myImpatience;
+}
+
+
+std::string
+GNECalibratorVehicleType::getLaneChangeModel() const {
+    return myLaneChangeModel;
+}
+
+
+std::string
+GNECalibratorVehicleType::getCarFollowModel() const {
+    return myCarFollowModel;
+}
+
+
+int
+GNECalibratorVehicleType::getPersonCapacity() const {
+    return myPersonCapacity;
+}
+
+
+int
+GNECalibratorVehicleType::getContainerCapacity() const {
+    return myContainerCapacity;
+}
+
+
+double
+GNECalibratorVehicleType::getBoardingDuration() const {
+    return myBoardingDuration;
+}
+
+
+double
+GNECalibratorVehicleType::getLoadingDuration() const {
+    return myLoadingDuration;
+}
+
+
+std::string
+GNECalibratorVehicleType::getLatAlignment() const {
+    return myLatAlignment;
+}
+
+
+double
+GNECalibratorVehicleType::getMinGapLat() const {
+    return myMinGapLat;
+}
+
+
+double
+GNECalibratorVehicleType::getMaxSpeedLat() const {
+    return myMaxSpeedLat;
+}
+
 bool 
 GNECalibratorVehicleType::setVehicleTypeID(std::string vehicleTypeID) {
     if (vehicleTypeID.empty()) {
@@ -99,5 +248,456 @@ GNECalibratorVehicleType::setVehicleTypeID(std::string vehicleTypeID) {
         return true;
     }
 }
+
+
+bool 
+GNECalibratorVehicleType::setAccel(double accel) {
+    if(accel < 0) {
+        return false;
+    } else {
+        myAccel = accel;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setAccel(std::string accel) {
+    if (GNEAttributeCarrier::canParse<double>(accel)) {
+        return setAccel(GNEAttributeCarrier::parse<double>(accel));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setDecel(double decel) {
+    if(decel < 0) {
+        return false;
+    } else {
+        myDecel = decel;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setDecel(std::string decel) {
+    if (GNEAttributeCarrier::canParse<double>(decel)) {
+        return setDecel(GNEAttributeCarrier::parse<double>(decel));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setSigma(double sigma) {
+    if(sigma < 0) {
+        return false;
+    } else {
+        mySigma = sigma;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setSigma(std::string sigma) {
+    if (GNEAttributeCarrier::canParse<double>(sigma)) {
+        return setSigma(GNEAttributeCarrier::parse<double>(sigma));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setTau(double tau) {
+    if(tau < 0) {
+        return false;
+    } else {
+        myTau = tau;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setTau(std::string tau) {
+    if (GNEAttributeCarrier::canParse<double>(tau)) {
+        return setTau(GNEAttributeCarrier::parse<double>(tau));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setLength(double lenght) {
+    if(lenght < 0) {
+        return false;
+    } else {
+        myLength = lenght;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setLength(std::string length) {
+    if (GNEAttributeCarrier::canParse<double>(length)) {
+        return setLength(GNEAttributeCarrier::parse<double>(length));
+    } else {
+        return false;
+    }
+}
+
+ 
+bool
+GNECalibratorVehicleType::setMinGap(double minGap) {
+    if(minGap < 0) {
+        return false;
+    } else {
+        myMinGap = minGap;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMinGap(std::string minGap) {
+    if (GNEAttributeCarrier::canParse<double>(minGap)) {
+        return setMinGap(GNEAttributeCarrier::parse<double>(minGap));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMaxSpeed(double maxSpeed) {
+    if(maxSpeed < 0) {
+        return false;
+    } else {
+        myMaxSpeed = maxSpeed;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMaxSpeed(std::string maxSpeed) {
+    if (GNEAttributeCarrier::canParse<double>(maxSpeed)) {
+        return setMaxSpeed(GNEAttributeCarrier::parse<double>(maxSpeed));
+    } else {
+        return false;
+    }
+}
+
+  
+bool 
+GNECalibratorVehicleType::setSpeedFactor(double speedFactor) {
+    if(speedFactor < 0) {
+        return false;
+    } else {
+        mySpeedFactor = speedFactor;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setSpeedFactor(std::string speedFactor) {
+    if (GNEAttributeCarrier::canParse<double>(speedFactor)) {
+        return setSpeedFactor(GNEAttributeCarrier::parse<double>(speedFactor));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setSpeedDev(double speedDev) {
+    if(speedDev < 0) {
+        return false;
+    } else {
+        mySpeedDev = speedDev;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setSpeedDev(std::string speedDev) {
+    if (GNEAttributeCarrier::canParse<double>(speedDev)) {
+        return setSpeedDev(GNEAttributeCarrier::parse<double>(speedDev));
+    } else {
+        return false;
+    }
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setColor(std::string color) {
+    myColor = color;
+    return true;
+}
+
+
+bool 
+GNECalibratorVehicleType::setVClass(SUMOVehicleClass vClass) {
+    myVClass = vClass;
+    return true;
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setVClass(std::string vClass) {
+    if(canParseVehicleClasses(vClass) == true) {
+        return setVClass(getVehicleClassID(vClass));
+    } else {
+        return false;
+    }
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setEmissionClass(std::string emissionClass) {
+    myEmissionClass = emissionClass;
+    return true;
+}
+
+
+bool 
+GNECalibratorVehicleType::setShape(SUMOVehicleShape shape) {
+    myShape = shape;
+    return true;
+}
+
+
+bool 
+GNECalibratorVehicleType::setShape(std::string shape) {
+    if(canParseVehicleShape(shape)) {
+        return setShape(getVehicleShapeID(shape));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setWidth(double width) {
+    if(width < 0) {
+        return false;
+    } else {
+        myWidth = width;
+        return true;
+    }
+}
+
+   
+bool 
+GNECalibratorVehicleType::setWidth(std::string width) {
+    if (GNEAttributeCarrier::canParse<double>(width)) {
+        return setWidth(GNEAttributeCarrier::parse<double>(width));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setFilename(std::string filename) {
+    myFilename = filename;
+    return true;
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setImpatience(double impatience) {
+    if(impatience < 0) {
+        return false;
+    } else {
+        myImpatience = impatience;
+        return true;
+    }
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setImpatience(std::string impatience) {
+    if (GNEAttributeCarrier::canParse<double>(impatience)) {
+        return setImpatience(GNEAttributeCarrier::parse<double>(impatience));
+    } else {
+        return false;
+    }
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setLaneChangeModel(std::string laneChangeModel) {
+    myLaneChangeModel = laneChangeModel;
+    return true;
+}
+
+
+bool 
+GNECalibratorVehicleType::setCarFollowModel(std::string carFollowModel) {
+    myCarFollowModel = carFollowModel;
+    return true;
+}
+
+ 
+bool 
+GNECalibratorVehicleType::setPersonCapacity(int personCapacity) {
+    if(personCapacity < 0) {
+        return false;
+    } else {
+        myPersonCapacity = personCapacity;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setPersonCapacity(std::string personCapacity) {
+    if (GNEAttributeCarrier::canParse<double>(personCapacity)) {
+        double impatienceD = GNEAttributeCarrier::canParse<double>(personCapacity);
+        // Check that double doesn't have decimals
+        if(fmod(impatienceD, 1) == 0) {
+            return setImpatience((int)impatienceD);
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setContainerCapacity(int containerCapacity) {
+    if(containerCapacity < 0) {
+        return false;
+    } else {
+        myContainerCapacity = containerCapacity;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setContainerCapacity(std::string containerCapacity) {
+    if (GNEAttributeCarrier::canParse<double>(containerCapacity)) {
+        double containerCapacityD = GNEAttributeCarrier::canParse<double>(containerCapacity);
+        // Check that double doesn't have decimals
+        if(fmod(containerCapacityD, 1) == 0) {
+            return setImpatience((int)containerCapacityD);
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setBoardingDuration(double boardingDuration) {
+    if(boardingDuration < 0) {
+        return false;
+    } else {
+        myBoardingDuration = boardingDuration;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setBoardingDuration(std::string boardingDuration) {
+    if (GNEAttributeCarrier::canParse<double>(boardingDuration)) {
+        return setBoardingDuration(GNEAttributeCarrier::parse<double>(boardingDuration));
+    } else {
+        return false;
+    }
+}
+
+
+bool
+GNECalibratorVehicleType::setLoadingDuration(double loadingDuration) {
+    if(loadingDuration < 0) {
+        return false;
+    } else {
+        myLoadingDuration = loadingDuration;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setLoadingDuration(std::string loadingDuration) {
+    if (GNEAttributeCarrier::canParse<double>(loadingDuration)) {
+        return setLoadingDuration(GNEAttributeCarrier::parse<double>(loadingDuration));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setLatAlignment(std::string latAlignment) {
+    if((latAlignment == "left") && (latAlignment == "right") && (latAlignment == "center") &&
+       (latAlignment == "compact") && (latAlignment == "nice") && (latAlignment == "arbitrary")) {
+           myLatAlignment = latAlignment;
+           return true;
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMinGapLat(double minGapLat) {
+    if(minGapLat < 0) {
+        return false;
+    } else {
+        myMinGapLat = minGapLat;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMinGapLat(std::string minGapLat) {
+    if (GNEAttributeCarrier::canParse<double>(minGapLat)) {
+        return setMinGapLat(GNEAttributeCarrier::parse<double>(minGapLat));
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMaxSpeedLat(double maxSpeedLat) {
+    if(maxSpeedLat < 0) {
+        return false;
+    } else {
+        myMaxSpeedLat = maxSpeedLat;
+        return true;
+    }
+}
+
+
+bool 
+GNECalibratorVehicleType::setMaxSpeedLat(std::string maxSpeedLat) {
+    if (GNEAttributeCarrier::canParse<double>(maxSpeedLat)) {
+        return setMaxSpeedLat(GNEAttributeCarrier::parse<double>(maxSpeedLat));
+    } else {
+        return false;
+    }
+}
+
 
 /****************************************************************************/
--- sumo/src/netedit/GNECalibratorVehicleType.h
+++ sumo/src/netedit/GNECalibratorVehicleType.h
@@ -32,6 +32,7 @@
 
 #include <utils/common/UtilExceptions.h>
 #include <utils/xml/SUMOXMLDefinitions.h>
+#include <utils/common/SUMOVehicleClass.h>
 
 // ===========================================================================
 // class declaration
@@ -48,8 +49,6 @@ class GNECalibrator;
  */
 class GNECalibratorVehicleType {
 public:
-
-
     /// @brief constructor
     GNECalibratorVehicleType(GNECalibrator* calibratorParent);
 
@@ -68,10 +67,305 @@ public:
     /// @brief get VehicleTypeID
     std::string getVehicleTypeID() const;
 
+    /// @brief get accel
+    double getAccel() const;
+
+    /// @brief get decel
+    double getDecel() const;
+
+    /// @brief get sigma
+    double getSigma() const;
+
+    /// @brief get tau
+    double getTau() const;
+
+    /// @brief get lenght
+    double getLength() const;
+
+    /// @brief get min gap
+    double getMinGap() const;
+
+    /// @brief get max speed
+    double getMaxSpeed() const;
+
+    /// @brief get speed factor
+    double getSpeedFactor() const;
+
+    /// @brief get speed dev
+    double getSpeedDev() const;
+
+    /// @brief get color
+    std::string getColor() const;
+
+    /// @brief get VClass
+    SUMOVehicleClass getVClass() const;
+
+    /// @brief get emission class
+    std::string getEmissionClass() const;
+
+    /// @brief get shape
+    SUMOVehicleShape getShape() const;
+
+    /// @brief get width
+    double getWidth() const;
+
+    /// @brief get filename
+    std::string getFilename() const;
+
+    /// @brief get impatience
+    double getImpatience() const;
+
+    /// @brief get lane change model
+    std::string getLaneChangeModel() const;
+
+    /// @brief get car follow model
+    std::string getCarFollowModel() const;
+
+    /// @brief get person capacity
+    int getPersonCapacity() const;
+
+    /// @brief get container capacity
+    int getContainerCapacity() const;
+
+    /// @brief get boarding duration
+    double getBoardingDuration() const;
+
+    /// @brief get loading duration
+    double getLoadingDuration() const;
+
+    /// @brief get lateral lat
+    std::string getLatAlignment() const;
+
+    /// @brief get min gap lat
+    double getMinGapLat() const;
+
+    /// @brief get max speed lat
+    double getMaxSpeedLat() const;
+
     /**@brief set vehicleType ID
-    * @return true if was sucesfully set, or false if value isn't valid
-    */
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
     bool setVehicleTypeID(std::string vehicleTypeID);
+
+    /**@brief set accel
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setAccel(double accel = 2.6);
+
+    /**@brief set accel (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setAccel(std::string accel);
+
+    /**@brief set decel
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setDecel(double decel = 4.5);
+
+    /**@brief set decel (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setDecel(std::string decel);
+
+    /**@brief set sigma
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSigma(double sigma = 0.5);
+
+    /**@brief set sigma (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSigma(std::string sigma);
+
+    /**@brief set tau
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setTau(double tau = 1.0);
+
+    /**@brief set tau (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setTau(std::string tau);
+
+    /**@brief set lenght
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLength(double lenght = 5.0);
+
+    /**@brief set lenght (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLength(std::string lenght);
+
+    /**@brief set min gap
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMinGap(double minGap = 2.5);
+
+    /**@brief set min gap (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMinGap(std::string minGap);
+
+    /**@brief set max speed
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMaxSpeed(double maxSpeed = 70.0);
+
+    /**@brief set max speed (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMaxSpeed(std::string maxSpeed);
+
+    /**@brief set speed factor
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSpeedFactor(double speedFactor = 1.0);
+
+    /**@brief set speed factor (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSpeedFactor(std::string speedFactor);
+
+    /**@brief set speed dev
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSpeedDev(double speedDev = 0.0);
+
+    /**@brief set speed dev (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setSpeedDev(std::string speedDev);
+
+    /**@brief set color
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setColor(std::string color = "1,1,0");
+
+    /**@brief set VClass
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setVClass(SUMOVehicleClass vClass = SVC_IGNORING);
+
+    /**@brief set VClass (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setVClass(std::string vClass);
+
+    /**@brief set emission class
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setEmissionClass(std::string emissionClass = "P_7_7");
+
+    /**@brief set shape (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setShape(std::string shape);
+
+    /**@brief set shape
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setShape(SUMOVehicleShape shape = SVS_UNKNOWN);
+
+    /**@brief set width
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setWidth(double width = 2.0);
+
+    /**@brief set width (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setWidth(std::string width);
+
+    /**@brief set filename
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setFilename(std::string filename = "");
+
+    /**@brief set impatience
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setImpatience(double impatience = 0.0);
+
+    /**@brief set impatience (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setImpatience(std::string impatience);
+
+    /**@brief set lane change model
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLaneChangeModel(std::string laneChangeModel = "LC2013");
+
+    /**@brief set car follow model
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setCarFollowModel(std::string carFollowModel = "Krauss");
+
+    /**@brief set person capacity
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setPersonCapacity(int personCapacity = 4);
+
+    /**@brief set person capacity (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setPersonCapacity(std::string personCapacity);
+
+    /**@brief set container capacity
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setContainerCapacity(int containerCapacity = 0);
+
+    /**@brief set container capacity (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setContainerCapacity(std::string containerCapacity);
+
+    /**@brief set boarding duration
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setBoardingDuration(double boardingDuration = 0.5);
+
+    /**@brief set boarding duration (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setBoardingDuration(std::string boardingDuration);
+
+    /**@brief set loading duration
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLoadingDuration(double loadingDuration = 90.0);
+
+    /**@brief set loading duration (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLoadingDuration(std::string loadingDuration);
+
+    /**@brief set lateral lat
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setLatAlignment(std::string latAlignment = "center");
+
+    /**@brief set min gap lat
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMinGapLat(double minGapLat = 0.12);
+
+    /**@brief set min gap lat (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMinGapLat(std::string minGapLat);
+
+    /**@brief set max speed lat
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMaxSpeedLat(double maxSpeedLat = 1.0);
+
+    /**@brief set max speed lat (string version)
+     * @return true if was sucesfully set, or false if value isn't valid
+     */
+    bool setMaxSpeedLat(std::string maxSpeedLat);
 
 private:
     /// @brief pointer to calibrator parent
@@ -79,6 +373,81 @@ private:
 
     /// @brief vehicleType ID
     std::string myVehicleTypeID;
+
+    /// @brief The acceleration ability of vehicles of this type (in m/s^2)
+    double myAccel;
+
+    /// @brief The deceleration ability of vehicles of this type (in m/s^2)
+    double myDecel;
+
+    /// @brief Car-following model parameter
+    double mySigma;
+
+    /// @brief Car-following model parameter
+    double myTau;
+
+    /// @brief The vehicle's netto-length (length) (in m)
+    double myLength;
+
+    /// @brief Empty space after leader [m]
+    double myMinGap;
+
+    /// @brief The vehicle's maximum velocity (in m/s)
+    double myMaxSpeed;
+
+    /// @brief The vehicles expected multiplicator for lane speed limits
+    double mySpeedFactor;
+
+    /// @brief The deviation of the speedFactor; see below for details
+    double mySpeedDev;
+
+    /// @brief This vehicle type's color
+    std::string myColor;
+
+    /// @brief An abstract vehicle class
+    SUMOVehicleClass myVClass;
+
+    /// @brief An abstract emission class 
+    std::string myEmissionClass;
+
+    /// @brief How this vehicle is rendered
+    SUMOVehicleShape myShape;
+
+    /// @brief The vehicle's width [m] (only used for drawing)
+    double myWidth;
+
+    /// @brief Image file for rendering vehicles of this type (should be grayscale to allow functional coloring)
+    std::string myFilename;
+
+    /// @brief Willingess of drivers to impede vehicles with higher priority. See below for semantics.
+    double myImpatience;
+
+    /// @brief The model used for changing lanes
+    std::string myLaneChangeModel;
+
+    /// @brief The model used for car following
+    std::string myCarFollowModel;
+
+    /// @brief The number of persons (excluding an autonomous driver) the vehicle can transport.
+    int myPersonCapacity;
+
+    /// @brief The number of containers the vehicle can transport.
+    int myContainerCapacity;
+
+    /// @brief The time required by a person to board the vehicle.
+    double myBoardingDuration;
+
+    /// @brief The time required to load a container onto the vehicle.
+    double myLoadingDuration;
+
+    /// @brief The preferred lateral alignment when using the sublane-model. One of (left, right, center, compact, nice, arbitrary).
+    std::string myLatAlignment;
+
+    /// @brief The minimum lateral gap at a speed difference of 100km/h when using the sublane-model
+    double myMinGapLat;
+
+    /// @brief The maximum lateral speed when using the sublane-model
+    double myMaxSpeedLat;
 };
 
 #endif
