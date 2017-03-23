efb980b64c57f5f5ee8300184f236262a298d557
Michael Behrisch <oss@behrisch.de>
2017-03-23 14:14:59 +0000

    loading test, refs #2972
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23633 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/load/runner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Michael Behrisch
+@author  Daniel Krajzewicz
+@author  Jakob Erdmann
+@date    2011-03-04
+@version $Id$
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2017 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import subprocess
+import sys
+sys.path.append(os.path.join(
+    os.path.dirname(sys.argv[0]), "..", "..", "..", "..", "..", "tools"))
+import traci
+import sumolib
+
+sumoBinary = sumolib.checkBinary('sumo')
+
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen(
+    "%s -c sumo.sumocfg --remote-port %s" % (sumoBinary, PORT), shell=True, stdout=sys.stdout)
+traci.init(PORT)
+traci.simulationStep()
+traci.load(["sumo.sumocfg"])
+traci.simulationStep()
+traci.close()
+sumoProcess.wait()
