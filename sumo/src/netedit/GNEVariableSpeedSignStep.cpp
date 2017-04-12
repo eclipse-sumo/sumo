912660b1dd2776312f8f11b6790402d4fbf36b17
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-04-12 08:15:10 +0000

    implemented GNEVariableSpeedSignSteps
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@23923 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/netedit/GNEVariableSpeedSignStep.cpp
@@ -0,0 +1,143 @@
+/****************************************************************************/
+/// @file    GNEVariableSpeedSignStep.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Apr 2017
+/// @version $Id: GNEVariableSpeedSignStep.cpp 23529 2017-03-18 10:22:36Z behrisch $
+///
+///
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <utils/common/ToString.h>
+#include <utils/common/MsgHandler.h>
+
+#include "GNEVariableSpeedSignStep.h"
+#include "GNEVariableSpeedSign.h"
+
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+
+GNEVariableSpeedSignStep::GNEVariableSpeedSignStep(GNEVariableSpeedSign* variableSpeedSignParent) :
+    myTime(0),
+    mySpeed(0),
+    myVariableSpeedSignParent(variableSpeedSignParent),
+    myTag(SUMO_TAG_STEP) {
+}
+
+
+GNEVariableSpeedSignStep::GNEVariableSpeedSignStep(GNEVariableSpeedSign* variableSpeedSignParent, double time, double speed) :
+    myTime(0),
+    mySpeed(0),
+    myVariableSpeedSignParent(variableSpeedSignParent),
+    myTag(SUMO_TAG_STEP) {
+    // use set functions to avid non valid intervals
+    setTime(time);
+    setSpeed(speed);
+}
+
+
+GNEVariableSpeedSignStep::~GNEVariableSpeedSignStep() {}
+
+
+GNEVariableSpeedSign*
+GNEVariableSpeedSignStep::getVariableSpeedSignParent() const {
+    return myVariableSpeedSignParent;
+}
+
+
+SumoXMLTag
+GNEVariableSpeedSignStep::getTag() const {
+    return myTag;
+}
+
+
+double
+GNEVariableSpeedSignStep::getTime() const {
+    return myTime;
+}
+
+
+double
+GNEVariableSpeedSignStep::getSpeed() const {
+    return mySpeed;
+}
+
+
+bool
+GNEVariableSpeedSignStep::setTime(double time) {
+    if(time >= 0) {
+        myTime = time;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+bool
+GNEVariableSpeedSignStep::setSpeed(double speed) {
+    if(speed >= 0) {
+        mySpeed = speed;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator==(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime == variableSpeedSignStep.myTime);
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator!=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime != variableSpeedSignStep.myTime);
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator>(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime > variableSpeedSignStep.myTime);
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator<(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime < variableSpeedSignStep.myTime);
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator>=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime >= variableSpeedSignStep.myTime);
+}
+
+
+bool 
+GNEVariableSpeedSignStep::operator<=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const {
+    return (myTime <= variableSpeedSignStep.myTime);
+}
+/****************************************************************************/
---
+++ sumo/src/netedit/GNEVariableSpeedSignStep.h
@@ -0,0 +1,117 @@
+/****************************************************************************/
+/// @file    GNEVariableSpeedSignStep.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Apr 2017
+/// @version $Id: GNEVariableSpeedSignStep.h 23150 2017-02-27 12:08:30Z behrisch $
+///
+///
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef GNEVariableSpeedSignStep_h
+#define GNEVariableSpeedSignStep_h
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <vector>
+#include <utils/common/UtilExceptions.h>
+#include <utils/xml/SUMOXMLDefinitions.h>
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+
+class GNEVariableSpeedSign;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class GNEVariableSpeedSignStep
+ * class used to represent a interval used in variable speed sign
+ */
+class GNEVariableSpeedSignStep {
+public:
+    /// @brief default constructor
+    GNEVariableSpeedSignStep(GNEVariableSpeedSign* variableSpeedSignParent);
+
+    /// @brief constructor
+    GNEVariableSpeedSignStep(GNEVariableSpeedSign* variableSpeedSignParent, double time, double speed);
+
+    /// @brief destructor
+    ~GNEVariableSpeedSignStep();
+
+    /// @brief get variable speed sign parent
+    GNEVariableSpeedSign* getVariableSpeedSignParent() const;
+
+    /// @brief get tag
+    SumoXMLTag getTag() const;
+
+    /// @brief get time
+    double getTime() const;
+
+    /// @brief get speed
+    double getSpeed() const;
+
+    /**@brief set time
+     * @brief return true if was sucesfully set, false in other case
+     */
+    bool setTime(double time);
+
+    /**@brief set speed time
+     * @brief return true if was sucesfully set, false in other case
+     */
+    bool setSpeed(double speed);
+
+    /// @brief overload operator ==
+    bool operator==(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+    /// @brief overload operator !=
+    bool operator!=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+    /// @brief overload operator >
+    bool operator>(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+    /// @brief overload operator <
+    bool operator<(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+    /// @brief overload operator >=
+    bool operator>=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+    /// @brief overload operator <=
+    bool operator<=(const GNEVariableSpeedSignStep& variableSpeedSignStep) const;
+
+protected:
+    /// @brief pointer to variable speed sign parent
+    GNEVariableSpeedSign* myVariableSpeedSignParent;
+
+    /// @brief XML Tag of a variable speed sign interval
+    SumoXMLTag myTag;
+
+    /// @brief timeStep
+    double myTime;
+
+    /// @brief speed in this timeStep
+    double mySpeed;
+};
+
+#endif
+
+/****************************************************************************/
