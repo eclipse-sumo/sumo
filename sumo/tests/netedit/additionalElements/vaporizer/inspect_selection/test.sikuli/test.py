20d011f4200732e59fbd5986786c45a74c131734
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-04-24 08:56:22 +0000

    added netedit test netedit/additionalElements/vaporizer/inspect_selection
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24036 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/vaporizer/inspect_selection/test.sikuli/test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 23331 2017-03-12 11:09:06Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvert()
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect vaporizers
+netedit.leftClick(match, 310, 200)
+
+# Set invalid begin 
+netedit.modifyAttribute(0, "-30")
+
+# Set valid begin 
+netedit.modifyAttribute(0, "5")
+
+# Set invalid end 1 
+netedit.modifyAttribute(1, "-5")
+
+# Set invalid end 2
+netedit.modifyAttribute(1, "1")
+
+# Set valid begin
+netedit.modifyAttribute(1, "15.8")
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
