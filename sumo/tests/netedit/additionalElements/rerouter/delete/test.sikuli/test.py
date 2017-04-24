744bb7817fb4fdf2c82e0d82ada5abfc24a1a6fe
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-04-24 15:15:28 +0000

    added netedit test netedit/additionalElements/rerouter/delete
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24050 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/additionalElements/rerouter/delete/test.sikuli/test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 24005 2017-04-21 12:54:13Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# apply zoom
+netedit.setZoom("25", "0", "25")
+
+# go to additional mode
+netedit.additionalMode()
+
+# select rerouter
+netedit.changeAdditional("rerouter")
+
+# select edge child
+netedit.selectAdditionalChild(8, 0)
+
+# create rerouter with default parameters
+netedit.leftClick(match, 200, 100)
+
+# go to delete mode
+netedit.deleteMode()
+
+# remove created rerouter
+netedit.leftClick(match, 200, 100)
+
+# remove loaded rerouter
+netedit.leftClick(match, 300, 100)
+
+# remove edge of rerouter
+netedit.leftClick(match, 300, 200)
+
+# Check undo
+netedit.undo(match, 3)
+
+# Change to delete
+netedit.deleteMode()
+
+# disble 'Automatically delete additionals'
+netedit.changeAutomaticallyDeleteAdditionals(match)
+
+# try to delete edge related to the second loaded rerouter (musn't be allowed)
+netedit.leftClick(match, 300, 200)
+
+# wait warning
+netedit.waitAutomaticallyDeleteAdditionalsWarning()
+
+# check redo
+netedit.redo(match, 3)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
