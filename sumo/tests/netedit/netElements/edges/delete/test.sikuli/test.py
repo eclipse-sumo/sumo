fe786fa29e38daafa588a35651f37795db9a5cf3
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-04-28 09:18:43 +0000

    Added netedit test netElements/edges/delete
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24117 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/netElements/edges/delete/test.sikuli/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 24005 2017-04-21 12:54:13Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# zoom in central node
+netedit.setZoom("50", "50", "100")
+
+# go to delete mode
+netedit.deleteMode()
+
+# delete edge
+netedit.leftClick(match, 200, 200)
+
+# delete node 
+netedit.leftClick(match, 575, 225)
+
+# Check undos
+netedit.undo(match, 2)
+
+# Check redos
+netedit.redo(match, 2)
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
