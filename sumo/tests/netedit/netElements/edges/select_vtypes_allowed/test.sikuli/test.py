03315bdbc7b3677bbc8e6ac854ca0291d021aff7
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-05-09 08:36:54 +0000

    Added netedit test netElements/edges/select_vtypes_allowed due #3084. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24242 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/netElements/edges/select_vtypes_allowed/test.sikuli/test.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 24005 2017-04-21 12:54:13Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, False)
+
+# go to select mode
+netedit.selectMode()
+
+# select all edges with allow = "all" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=all")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "passenger" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=passenger")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "taxi" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=taxi")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "bus" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=bus")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "coach" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=coach")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "delivery" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=delivery")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "truck" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=truck")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "trailer" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=trailer")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "emergency" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=emergency")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "motorcycle" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=motorcycle")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "moped" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=moped")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "bicycle" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=bicycle")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "pedestrian" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=pedestrian")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "tram" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=tram")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "rail_urban" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=rail_urban")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "rail" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=rail")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "rail_electric" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=rail_electric")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "ship" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=ship")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "evehicle" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=evehicle")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "private" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=private")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "army" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=army")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "authority" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=authority")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "vip" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=vip")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "hov" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=hov")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "custom1" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=custom1")
+netedit.deleteSelectedItems()
+
+# select all edges with allow = "custom2" and remove it
+netedit.selectItems("Net Element", "edge", "allow", "=custom2")
+netedit.deleteSelectedItems()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
