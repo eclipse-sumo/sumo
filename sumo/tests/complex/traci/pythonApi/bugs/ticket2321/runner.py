e4b9073bba6c320797e8e62482b31e7dfd2f0a8e
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-05-19 09:18:23 +0000

    added test refs #2321
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24359 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/bugs/ticket2321/runner.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Jakob Erdmann
+@date    2017-01-23
+@version $Id: runner.py 22929 2017-02-13 14:38:39Z behrisch $
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2017 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import subprocess
+import sys
+import random
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci
+import sumolib
+
+sumoBinary = os.environ["SUMO_BINARY"]
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen([sumoBinary,
+                                '-c', 'sumo.sumocfg',
+                                '-S', '-Q',
+                                '--remote-port', str(PORT)], stdout=sys.stdout)
+
+
+
+
+vehID = "v0"
+traci.init(PORT)
+traci.simulationStep()
+traci.route.add("r0", ["SC", "CN"])
+traci.vehicle.add(vehID, "r0")
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+    try:
+        print("step=%s lane=%s dist=%s" % (
+            traci.simulation.getCurrentTime() / 1000,
+            traci.vehicle.getLaneID(vehID),
+            traci.vehicle.getDistance(vehID)))
+    except traci.TraCIException:
+        pass
+
+traci.close()
+sumoProcess.wait()
