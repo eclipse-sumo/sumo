b95807fc92a7d31729e9e2aef02130f40022e522
Laura Bieker <laura.bieker@dlr.de>
2017-05-24 08:06:44 +0000

    added failing test for bug with the usage of sublanes with setSpeedMode, refs #3140
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24451 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/bugs/ticket3140/runner.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Laura Bieker
+@date    2017-05-23
+@version $Id: runner.py 22929 2017-02-13 14:38:39Z behrisch $
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2017 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import subprocess
+import sys
+import random
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci
+import sumolib
+
+sumoBinary = os.environ["SUMO_BINARY"]
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen([sumoBinary,
+                                '-c', 'sumo.sumocfg',
+                                '--fcd-output', 'fcd.xml',
+                                '-S', '-Q',
+                                '--remote-port', str(PORT)], stdout=sys.stdout)
+
+traci.init(PORT)
+traci.simulationStep()
+for i in range(45):
+    traci.simulationStep()
+traci.vehicle.setSpeedMode("rescue", 7)
+traci.vehicle.setSpeed("rescue", 13.9)
+traci.trafficlights.setRedYellowGreenState("C","rrrrrrrrrrrrrrrrrr")
+for i in range(20):
+    traci.simulationStep()
+traci.close()
+sumoProcess.wait()
+
+
+
+
