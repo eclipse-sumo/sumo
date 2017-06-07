b509c738b659b7a11a1444bd88f5a2a1f4b1b9b3
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-06-07 14:35:25 +0000

    added test refs #3168
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24577 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/selection/select_allow_all/test.sikuli/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 24264 2017-05-12 10:40:18Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot)
+
+# select edges that allow 'passenger'
+netedit.selectMode()
+netedit.focusOnFrame()
+for x in range(0, 9):
+    netedit.typeTab()
+netedit.pasteIntoTextField("allow")
+netedit.typeTab()
+netedit.typeTab()
+netedit.pasteIntoTextField("passenger")
+netedit.typeEnter()
+
+# save selection
+netedit.focusOnFrame()
+for x in range(0, 16):
+    netedit.typeTab()
+netedit.typeSpace()
+# jump to filename textfield
+netedit.typeTwoKeys("f", Key.ALT)
+filename = os.path.join(netedit.textTestSandBox, "selection.txt")
+netedit.pasteIntoTextField(filename)
+netedit.typeEnter()
+
+# quit netedit
+netedit.quit(neteditProcess, True)
