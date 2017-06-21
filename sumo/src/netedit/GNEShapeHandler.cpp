1d64521de098eed37b234f48c70186a34287bfaf
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-06-21 08:46:50 +0000

    Added GNEShapeHandler.h and GNEShapeHandler.cpp. Refs #2765
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@24808 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/netedit/GNEShapeHandler.cpp
@@ -0,0 +1,210 @@
+
+/****************************************************************************/
+/// @file    GNEShapeHandler.cpp
+/// @author  Jakob Erdmann
+/// @author  Pablo Alvarez Lopez
+/// @date    Jun 2017
+/// @version $Id: GNEShapeHandler.cpp 23529 2017-03-18 10:22:36Z behrisch $
+///
+// The XML-Handler for loading Polys/POIs in netedit
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <utils/xml/SUMOXMLDefinitions.h>
+#include <utils/xml/SUMOSAXHandler.h>
+#include <utils/xml/XMLSubSys.h>
+#include <utils/common/MsgHandler.h>
+#include <utils/common/TplConvert.h>
+#include <utils/common/StringTokenizer.h>
+#include <utils/common/RGBColor.h>
+#include <utils/geom/GeomConvHelper.h>
+#include <utils/iodevices/OutputDevice.h>
+#include <utils/common/UtilExceptions.h>
+#include <utils/geom/GeoConvHelper.h>
+#include <utils/gui/globjects/GUIGlObjectTypes.h>
+#include <utils/shapes/shape.h>
+#include <utils/shapes/ShapeContainer.h>
+
+#include "GNEShapeHandler.h"
+
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+GNEShapeHandler::GNEShapeHandler(const std::string& file, ShapeContainer& sc) :
+    SUMOSAXHandler(file), myShapeContainer(sc),
+    myPrefix(""), myDefaultColor(RGBColor::RED), myDefaultLayer(), myDefaultFill(false) {}
+
+
+GNEShapeHandler::~GNEShapeHandler() {}
+
+
+void
+GNEShapeHandler::myStartElement(int element,
+                             const SUMOSAXAttributes& attrs) {
+    try {
+        switch (element) {
+            case SUMO_TAG_POLY:
+                myDefaultLayer = Shape::DEFAULT_LAYER;
+                addPoly(attrs, false, false);
+                break;
+            case SUMO_TAG_POI:
+                myDefaultLayer = (double)GLO_POI;
+                addPOI(attrs, false, false);
+                break;
+            default:
+                break;
+        }
+    } catch (InvalidArgument& e) {
+        WRITE_ERROR(e.what());
+    }
+}
+
+
+
+void
+GNEShapeHandler::addPOI(const SUMOSAXAttributes& attrs, const bool ignorePruning, const bool useProcessing) {
+    bool ok = true;
+    const double INVALID_POSITION(-1000000);
+    const std::string id = myPrefix + attrs.get<std::string>(SUMO_ATTR_ID, 0, ok);
+    double x = attrs.getOpt<double>(SUMO_ATTR_X, id.c_str(), ok, INVALID_POSITION);
+    const double y = attrs.getOpt<double>(SUMO_ATTR_Y, id.c_str(), ok, INVALID_POSITION);
+    double lon = attrs.getOpt<double>(SUMO_ATTR_LON, id.c_str(), ok, INVALID_POSITION);
+    double lat = attrs.getOpt<double>(SUMO_ATTR_LAT, id.c_str(), ok, INVALID_POSITION);
+    const double lanePos = attrs.getOpt<double>(SUMO_ATTR_POSITION, id.c_str(), ok, INVALID_POSITION);
+    const double layer = attrs.getOpt<double>(SUMO_ATTR_LAYER, id.c_str(), ok, myDefaultLayer);
+    const std::string type = attrs.getOpt<std::string>(SUMO_ATTR_TYPE, id.c_str(), ok, "");
+    const std::string laneID = attrs.getOpt<std::string>(SUMO_ATTR_LANE, id.c_str(), ok, "");
+    const RGBColor color = attrs.hasAttribute(SUMO_ATTR_COLOR) ? attrs.get<RGBColor>(SUMO_ATTR_COLOR, id.c_str(), ok) : myDefaultColor;
+    const double angle = attrs.getOpt<double>(SUMO_ATTR_ANGLE, id.c_str(), ok, Shape::DEFAULT_ANGLE);
+    std::string imgFile = attrs.getOpt<std::string>(SUMO_ATTR_IMGFILE, id.c_str(), ok, Shape::DEFAULT_IMG_FILE);
+    if (imgFile != "" && !FileHelpers::isAbsolute(imgFile)) {
+        imgFile = FileHelpers::getConfigurationRelative(getFileName(), imgFile);
+    }
+    const double width = attrs.getOpt<double>(SUMO_ATTR_WIDTH, id.c_str(), ok, Shape::DEFAULT_IMG_WIDTH);
+    const double height = attrs.getOpt<double>(SUMO_ATTR_HEIGHT, id.c_str(), ok, Shape::DEFAULT_IMG_HEIGHT);
+    if (!ok) {
+        return;
+    }
+    const GeoConvHelper& gch = useProcessing ? GeoConvHelper::getProcessing() : GeoConvHelper::getFinal();
+    if (useProcessing && gch.usingGeoProjection()) {
+        if (lat == INVALID_POSITION || lon == INVALID_POSITION) {
+            lon = x;
+            lat = y;
+            x = INVALID_POSITION;
+        }
+    }
+    Position pos(x, y);
+    if (x == INVALID_POSITION || y == INVALID_POSITION) {
+        // try computing x,y from lane,pos
+        if (laneID != "") {
+            pos = getLanePos(id, laneID, lanePos);
+        } else {
+            // try computing x,y from lon,lat
+            if (lat == INVALID_POSITION || lon == INVALID_POSITION) {
+                WRITE_ERROR("Either (x, y), (lon, lat) or (lane, pos) must be specified for PoI '" + id + "'.");
+                return;
+            } else if (!gch.usingGeoProjection()) {
+                WRITE_ERROR("(lon, lat) is specified for PoI '" + id + "' but no geo-conversion is specified for the network.");
+                return;
+            }
+            pos.set(lon, lat);
+            bool success = true;
+            if (useProcessing) {
+                success = GeoConvHelper::getProcessing().x2cartesian(pos);
+            } else {
+                success = GeoConvHelper::getFinal().x2cartesian_const(pos);
+            }
+            if (!success) {
+                WRITE_ERROR("Unable to project coordinates for PoI '" + id + "'.");
+                return;
+            }
+        }
+    }
+    if (!myShapeContainer.addPOI(id, type, color, layer, angle, imgFile, pos, width, height, ignorePruning)) {
+        WRITE_ERROR("PoI '" + id + "' already exists.");
+    }
+}
+
+
+void
+GNEShapeHandler::addPoly(const SUMOSAXAttributes& attrs, const bool ignorePruning, const bool useProcessing) {
+    bool ok = true;
+    const std::string id = myPrefix + attrs.get<std::string>(SUMO_ATTR_ID, 0, ok);
+    // get the id, report an error if not given or empty...
+    if (!ok) {
+        return;
+    }
+    const double layer = attrs.getOpt<double>(SUMO_ATTR_LAYER, id.c_str(), ok, myDefaultLayer);
+    const bool fill = attrs.getOpt<bool>(SUMO_ATTR_FILL, id.c_str(), ok, myDefaultFill);
+    const std::string type = attrs.getOpt<std::string>(SUMO_ATTR_TYPE, id.c_str(), ok, Shape::DEFAULT_TYPE);
+    const RGBColor color = attrs.hasAttribute(SUMO_ATTR_COLOR) ? attrs.get<RGBColor>(SUMO_ATTR_COLOR, id.c_str(), ok) : myDefaultColor;
+    PositionVector shape = attrs.get<PositionVector>(SUMO_ATTR_SHAPE, id.c_str(), ok);
+    if (attrs.getOpt<bool>(SUMO_ATTR_GEO, id.c_str(), ok, false)) {
+        bool success = true;
+        for (int i = 0; i < (int)shape.size(); i++) {
+            if (useProcessing) {
+                success &= GeoConvHelper::getProcessing().x2cartesian(shape[i]);
+            } else {
+                success &= GeoConvHelper::getFinal().x2cartesian_const(shape[i]);
+            }
+        }
+        if (!success) {
+            WRITE_WARNING("Unable to project coordinates for polygon '" + id + "'.");
+            return;
+        }
+    }
+    const double angle = attrs.getOpt<double>(SUMO_ATTR_ANGLE, id.c_str(), ok, Shape::DEFAULT_ANGLE);
+    std::string imgFile = attrs.getOpt<std::string>(SUMO_ATTR_IMGFILE, id.c_str(), ok, Shape::DEFAULT_IMG_FILE);
+    if (imgFile != "" && !FileHelpers::isAbsolute(imgFile)) {
+        imgFile = FileHelpers::getConfigurationRelative(getFileName(), imgFile);
+    }
+    if (!myShapeContainer.addPolygon(id, type, color, layer, angle, imgFile, shape, fill, ignorePruning)) {
+        WRITE_ERROR("Polygon '" + id + "' already exists.");
+    }
+}
+
+
+
+bool
+GNEShapeHandler::loadFiles(const std::vector<std::string>& files, GNEShapeHandler& sh) {
+    for (std::vector<std::string>::const_iterator fileIt = files.begin(); fileIt != files.end(); ++fileIt) {
+        if (!XMLSubSys::runParser(sh, *fileIt, false)) {
+            WRITE_MESSAGE("Loading of shapes from " + *fileIt + " failed.");
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void
+GNEShapeHandler::setDefaults(const std::string& prefix, const RGBColor& color, const double layer, const bool fill) {
+    myPrefix = prefix;
+    myDefaultColor = color;
+    myDefaultLayer = layer;
+    myDefaultFill = fill;
+}
+
+
+
+/****************************************************************************/
---
+++ sumo/src/netedit/GNEShapeHandler.h
@@ -0,0 +1,136 @@
+
+/****************************************************************************/
+/// @file    GNEShapeHandler.h
+/// @author  Jakob Erdmann
+/// @author  Pablo Alvarez Lopez
+/// @date    Jun 2017
+/// @version $Id: GNEShapeHandler.h 23150 2017-02-27 12:08:30Z behrisch $
+///
+// The XML-Handler for Polys/POIs in netedit
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2001-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef GNEShapeHandler_h
+#define GNEShapeHandler_h
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <utils/common/RGBColor.h>
+#include <utils/geom/Position.h>
+#include <utils/xml/SUMOSAXHandler.h>
+
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+class ShapeContainer;
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class GNEShapeHandler
+ * @brief The XML-Handler for network loading
+ *
+ * The SAX2-handler responsible for parsing networks and routes to load.
+ * This is an extension of the MSRouteHandler as routes and vehicles may also
+ *  be loaded from network descriptions.
+ */
+class GNEShapeHandler : public SUMOSAXHandler {
+public:
+
+public:
+    /** @brief Constructor
+     *
+     * @param[in] file Name of the parsed file
+     * @param[in, out] net The network to fill
+     * @param[in] detBuilder The detector builder to use
+     * @param[in] triggerBuilder The trigger builder to use
+     * @param[in] edgeBuilder The builder of edges to use
+     * @param[in] junctionBuilder The builder of junctions to use
+     */
+    GNEShapeHandler(const std::string& file, ShapeContainer& sc);
+
+    /// @brief Destructor
+    virtual ~GNEShapeHandler();
+
+    /// @brief loads all of the given files
+    static bool loadFiles(const std::vector<std::string>& files, GNEShapeHandler& sh);
+
+
+protected:
+    /// @name inherited from GenericSAXHandler
+    //@{
+
+    /** @brief Called on the opening of a tag;
+     *
+     * @param[in] element ID of the currently opened element
+     * @param[in] attrs Attributes within the currently opened element
+     * @exception ProcessError If something fails
+     * @see GenericSAXHandler::myStartElement
+     * @todo Refactor/describe
+     */
+    virtual void myStartElement(int element,
+                                const SUMOSAXAttributes& attrs);
+
+
+    //@}
+
+
+    /// @brief get position for a given laneID
+    virtual Position getLanePos(const std::string& poiID, const std::string& laneID, double lanePos) = 0;
+
+protected:
+    void setDefaults(const std::string& prefix, const RGBColor& color, const double layer, const bool fill = false);
+
+    /// adds a POI
+    void addPOI(const SUMOSAXAttributes& attrs, const bool ignorePruning, const bool useProcessing);
+
+    /// adds a polygon
+    void addPoly(const SUMOSAXAttributes& attrs, const bool ignorePruning, const bool useProcessing);
+
+protected:
+    ShapeContainer& myShapeContainer;
+
+private:
+    /// @brief The prefix to use
+    std::string myPrefix;
+    /// @brief The default color to use
+    RGBColor myDefaultColor;
+    /// @brief The default layer to use
+    double myDefaultLayer;
+    /// @brief Information whether polygons should be filled
+    bool myDefaultFill;
+
+private:
+    /// @brief invalid copy constructor
+    GNEShapeHandler(const GNEShapeHandler& s);
+
+    /// @brief invalid assignment operator
+    GNEShapeHandler& operator=(const GNEShapeHandler& s);
+
+};
+
+
+#endif
+
+/****************************************************************************/
+
