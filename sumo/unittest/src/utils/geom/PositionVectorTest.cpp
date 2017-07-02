3f84407b2ca3a35f3f7a5c18d57dcff88230ba4a
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-07-02 06:12:46 +0000

    patching unittest expected results after [25035] (symmetrical overlap) refs #82
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25039 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/unittest/src/utils/geom/PositionVectorTest.cpp
+++ sumo/unittest/src/utils/geom/PositionVectorTest.cpp
@@ -567,7 +567,9 @@ TEST_F(PositionVectorTest, test_method_overlapsWith) {
     EXPECT_TRUE(vec1.overlapsWith(vec3));
     EXPECT_TRUE(vec1.overlapsWith(vec4));
     EXPECT_FALSE(vec1.overlapsWith(vec5, 0));
-    EXPECT_FALSE(vec1.overlapsWith(vec5, 5)); // growth is from centroid and thus different from Boundary behavior
+    // growth is from centroid and thus different from Boundary behavior
+    EXPECT_FALSE(vec1.overlapsWith(vec5, 1)); 
+    EXPECT_TRUE(vec1.overlapsWith(vec5, 2));
     EXPECT_TRUE(vec1.overlapsWith(vec5, 6));
     EXPECT_FALSE(vec1.overlapsWith(empty));
 }
