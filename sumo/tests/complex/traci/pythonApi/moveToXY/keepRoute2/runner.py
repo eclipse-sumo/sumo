4cabc2452a718965d64bda9e2fbf7d6c896b87c1
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-07-03 09:21:50 +0000

    added failing test refs #3263
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25047 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/complex/traci/pythonApi/moveToXY/keepRoute2/runner.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file    runner.py
+@author  Jakob Erdmann
+@date    2017-01-23
+@version $Id: runner.py 24854 2017-06-22 13:21:09Z behrisch $
+
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2008-2017 DLR (http://www.dlr.de/) and contributors
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import subprocess
+import sys
+import random
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci
+import sumolib  # noqa
+
+sumoBinary = os.environ["SUMO_BINARY"]
+PORT = sumolib.miscutils.getFreeSocketPort()
+sumoProcess = subprocess.Popen([sumoBinary,
+                                '-n', 'input_net.net.xml',
+                                '--no-step-log',
+                                '-S', '-Q',
+                                '--remote-port', str(PORT)], stdout=sys.stdout)
+
+vehID = "v0"
+def check(x, y, comment):
+    traci.vehicle.moveToXY(vehID, "", 0, x, y, keepRoute=2)
+    traci.simulationStep()
+    x2, y2 = traci.vehicle.getPosition(vehID)
+    if x == x2 and y == y2:
+        #print(comment, "success")
+        pass
+    else:
+        print(comment, "failed: x=%s, x2=%s, y=%s, y2=%s" % (x, x2, y, y2))
+
+
+traci.init(PORT)
+traci.simulationStep()
+traci.route.add("beg", ["beg"])
+traci.vehicle.add(vehID, "beg")
+check(-1, 0, "before the start of the edge")
+traci.close()
+sumoProcess.wait()
