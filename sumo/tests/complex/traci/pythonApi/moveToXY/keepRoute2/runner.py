90e7435dc7156de4dd85ce61c09c61dbddbb2ded
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-07-03 11:40:39 +0000

    added check refs #3263
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25052 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/pythonApi/moveToXY/keepRoute2/runner.py
+++ sumo/tests/complex/traci/pythonApi/moveToXY/keepRoute2/runner.py
@@ -35,23 +35,34 @@ sumoProcess = subprocess.Popen([sumoBinary,
                                 '-S', '-Q',
                                 '--remote-port', str(PORT)], stdout=sys.stdout)
 
+ANGLE_UNDEF = traci.constants.INVALID_DOUBLE_VALUE
+
 vehID = "v0"
-def check(x, y, comment):
-    traci.vehicle.moveToXY(vehID, "", 0, x, y, keepRoute=2)
+def check(x, y, angle, exLane, exPos, exPosLat, comment):
+    traci.vehicle.moveToXY(vehID, "", angle, x, y, keepRoute=2)
     traci.simulationStep()
     x2, y2 = traci.vehicle.getPosition(vehID)
-    if x == x2 and y == y2:
+    lane2 = traci.vehicle.getLaneID(vehID)
+    pos2 = traci.vehicle.getLanePosition(vehID)
+    posLat2 = traci.vehicle.getLateralLanePosition(vehID)
+    if (abs(x - x2) > 0.1 or abs(y - y2) > 0.1 or exLane != lane2 
+            or (exPos is not None and abs(exPos - pos2) > 0.1)
+            or (exPosLat is not None and abs(exPosLat - posLat2) > 0.1)
+            ):
+        print(comment, "failed: x=%s, x2=%s,   y=%s, y2=%s,   lane=%s, lane2=%s, pos=%s, pos2=%s   posLat=%s posLat2=%s" % (
+            x, x2, y, y2, exLane, lane2, exPos, pos2, exPosLat, posLat2))
+    else:
         #print(comment, "success")
         pass
-    else:
-        print(comment, "failed: x=%s, x2=%s, y=%s, y2=%s" % (x, x2, y, y2))
 
 
 traci.init(PORT)
 traci.simulationStep()
 traci.route.add("beg", ["beg"])
 traci.vehicle.add(vehID, "beg")
-check(-1, 0, "1m before the start of the edge")
-check(-5, 0, "5m before the start of the edge")
+check(-1,  0,   ANGLE_UNDEF, "",         None, None, "1m before the start of the edge")
+check(-5,  0,   ANGLE_UNDEF, "",         None, None, "5m before the start of the edge")
+check(198, 1.9, ANGLE_UNDEF, "middle_0", 98.0, 1.9,  "internal corner (inside)")
+#check(198, 1.9, 0,           "middle_0", 101.9, 2.0,  "internal corner (inside)")
 traci.close()
 sumoProcess.wait()
