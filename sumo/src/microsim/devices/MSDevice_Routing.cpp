bc797f993308bd359592a1e9c935a8e3f922c34f
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-07-06 10:41:30 +0000

    improve speed when computing missing landmark-cache values refs #3047
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25123 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/devices/MSDevice_Routing.cpp
+++ sumo/src/microsim/devices/MSDevice_Routing.cpp
@@ -384,7 +384,10 @@ MSDevice_Routing::reroute(const SUMOTime currentTime, const bool onInit) {
                     const double speedFactor = myHolder.getChosenSpeedFactor();
                     // we need an exemplary vehicle with speedFactor 1
                     myHolder.setChosenSpeedFactor(1);
-                    lookup = new AStar::LMLT(oc.getString("astar.landmark-distances"), MSEdge::getAllEdges(), &MSNet::getInstance()->getRouterTT(), &myHolder);
+                    CHRouterWrapper<MSEdge, SUMOVehicle, prohibited_withPermissions<MSEdge, SUMOVehicle> > router(
+                            MSEdge::getAllEdges(), true, &MSNet::getTravelTime,
+                            string2time(oc.getString("begin")), string2time(oc.getString("end")), std::numeric_limits<int>::max(), 1);
+                    lookup = new AStar::LMLT(oc.getString("astar.landmark-distances"), MSEdge::getAllEdges(), &router, &myHolder);
                     myHolder.setChosenSpeedFactor(speedFactor);
                 }
                 myRouter = new AStar(MSEdge::getAllEdges(), true, &MSDevice_Routing::getEffort, lookup);
@@ -397,7 +400,10 @@ MSDevice_Routing::reroute(const SUMOTime currentTime, const bool onInit) {
                     const double speedFactor = myHolder.getChosenSpeedFactor();
                     // we need an exemplary vehicle with speedFactor 1
                     myHolder.setChosenSpeedFactor(1);
-                    lookup = new AStar::LMLT(oc.getString("astar.landmark-distances"), MSEdge::getAllEdges(), &MSNet::getInstance()->getRouterTT(), &myHolder);
+                    CHRouterWrapper<MSEdge, SUMOVehicle, prohibited_withPermissions<MSEdge, SUMOVehicle> > router(
+                            MSEdge::getAllEdges(), true, &MSNet::getTravelTime,
+                            string2time(oc.getString("begin")), string2time(oc.getString("end")), std::numeric_limits<int>::max(), 1);
+                    lookup = new AStar::LMLT(oc.getString("astar.landmark-distances"), MSEdge::getAllEdges(), &router, &myHolder);
                     myHolder.setChosenSpeedFactor(speedFactor);
                 }
                 myRouter = new AStar(MSEdge::getAllEdges(), true, &MSDevice_Routing::getEffort, lookup);
