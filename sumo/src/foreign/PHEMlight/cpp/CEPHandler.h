4336d6e7ebe09e07027a58903345b8455b320b12
Michael Behrisch <oss@behrisch.de>
2017-07-23 04:47:18 +0000

    license header for PHEMlight, refs #22
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25297 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/foreign/PHEMlight/cpp/CEPHandler.h
+++ sumo/src/foreign/PHEMlight/cpp/CEPHandler.h
@@ -1,3 +1,27 @@
+/****************************************************************************/
+/// @file    CEPHandler.h
+/// @author  Martin Dippold
+/// @author  Michael Behrisch
+/// @date    July 2016
+/// @version $Id$
+///
+//
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2016-2017 DLR (http://www.dlr.de/) and contributors
+// PHEMlight module
+// Copyright 2016 Technische Universitaet Graz, https://www.tugraz.at/
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
 #ifndef PHEMlightCEPHANDLER
 #define PHEMlightCEPHANDLER
 
