3bfef47ea5bb24a3cf453b17c7a2a83829c82561
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-07-24 13:20:06 +0000

    Partial fix of [25314]. Refs #3281
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25315 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netedit/GNEEdge.cpp
+++ sumo/src/netedit/GNEEdge.cpp
@@ -302,8 +302,12 @@ Position
 GNEEdge::moveGeometry(const Position& oldPos, const Position& newPos, bool relative) {
     PositionVector geom = myNBEdge.getGeometry();
     bool changed = changeGeometry(geom, getMicrosimID(), oldPos, newPos, relative);
+    if (changed) {
         setGeometry(geom, false);
         return newPos;
+    } else {
+        return oldPos;
+    }
 }
 
 
