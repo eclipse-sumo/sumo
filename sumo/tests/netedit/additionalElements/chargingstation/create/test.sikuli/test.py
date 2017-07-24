cb31b2e6195736bac65793269a421255ab9ca419
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-07-24 10:23:55 +0000

    Updated netedit/additionalElements/chargingstation test. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25307 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionalElements/chargingstation/create/test.sikuli/test.py
+++ sumo/tests/netedit/additionalElements/chargingstation/create/test.sikuli/test.py
@@ -98,18 +98,37 @@ netedit.leftClick(match, 500, 250)
 # change reference to "reference right"
 netedit.modifyAdditionalDefaultValue(9, "reference right")
 
-# create a chargingStation in mode "reference right" forcing position
+# create a chargingStation in mode "reference right"
 netedit.leftClick(match, 110, 250)
 
+# disable friendlyPos
+netedit.modifyAdditionalDefaultBoolValue(3)
+
+# change reference to "reference left"
+netedit.modifyAdditionalDefaultValue(9, "reference left")
+
+# create a chargingStation in mode "reference left" without friendlyPos
+netedit.leftClick(match, 120, 215)
+
+# change reference to "reference right"
+netedit.modifyAdditionalDefaultValue(9, "reference right")
+
+# create a chargingStation in mode "reference right" without friendlyPos
+netedit.leftClick(match, 500, 215)
+
 # Check undo redo
-netedit.undo(match, 6)
-netedit.redo(match, 6)
+netedit.undo(match, 8)
+netedit.redo(match, 8)
 
 # save additionals
 netedit.saveAdditionals()
+
+# Fix stopping places position
+netedit.fixStoppingPlace("fixPositions")
 
 # save newtork
 netedit.saveNetwork()
 
 # quit netedit
 netedit.quit(neteditProcess)
+
