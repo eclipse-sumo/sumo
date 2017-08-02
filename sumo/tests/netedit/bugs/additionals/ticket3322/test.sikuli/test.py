97fffb3f71eaf08693e2c92f01d9c8a122f0df54
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-08-02 11:17:23 +0000

    Added netedit test for #3322. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25448 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/bugs/additionals/ticket3322/test.sikuli/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 25267 2017-07-19 10:41:16Z behrisch $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot)
+
+# go to delete mode
+netedit.deleteMode()
+
+# delete junction
+netedit.leftClick(match, 330, 300)
+
+# Undo
+netedit.undo(match, 1)
+
+# Change to delete
+netedit.deleteMode()
+
+# disable 'Automatically delete additionals'
+netedit.changeAutomaticallyDeleteAdditionals(match)
+
+# try to delete junction
+netedit.leftClick(match, 330, 300)
+
+# wait warning
+netedit.waitAutomaticallyDeleteAdditionalsWarning()
+
+# recompute
+netedit.rebuildNetwork()
+
+# save additionals
+netedit.saveAdditionals()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
