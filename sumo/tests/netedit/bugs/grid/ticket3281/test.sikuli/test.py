7b95f644b686c9406b2eb595f8f6bf1bddc2afb4
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-08-07 10:10:25 +0000

    Added netedit test for Ticket #3281. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25496 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/bugs/grid/ticket3281/test.sikuli/test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 25267 2017-07-19 10:41:16Z behrisch $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot, ['--new'])
+
+# zoom in central node
+netedit.setZoom("100", "0", "200")
+
+# Change to create edge mode
+netedit.createEdgeMode()
+
+# Create one way edge
+netedit.leftClick(match, -30, 230)
+netedit.leftClick(match, 430, 230)
+
+# change to move mode
+netedit.moveMode()
+
+# Try to move to origin position
+netedit.moveElement(match, 215, 232, -40, 232)
+
+# Now move to top
+netedit.moveElement(match, 215, 232, 215, 432)
+
+# rebuild network
+netedit.rebuildNetwork()
+
+# Check undo and redo
+netedit.undo(match, 3)
+netedit.redo(match, 3)
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
