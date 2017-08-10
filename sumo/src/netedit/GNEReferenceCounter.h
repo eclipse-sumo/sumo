65b54c59ec94eaa40c9a14cf34abbcead1b7547e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-08-10 10:48:53 +0000

    Fixed warnings in Visual Studio 15/17. Refs #12
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25532 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netedit/GNEReferenceCounter.h
+++ sumo/src/netedit/GNEReferenceCounter.h
@@ -32,7 +32,7 @@
 #include <config.h>
 #endif
 
-#include <utils/common/UtilExceptions.h>
+#include <utils/common/MsgHandler.h>
 #include <utils/common/ToString.h>
 
 
@@ -51,43 +51,50 @@ public:
 
     //// @brief destructor
     ~GNEReferenceCounter() {
-        if (myCount) {
+        // If myCount is different of 0, means that references weren't removed correctly
+        if (myCount != 0) {
             // cannot print id here, it already got destructed
-            throw ProcessError("Attempt to delete instance of GNEReferenceCounter with count " + toString(myCount));
+            WRITE_ERROR("Attempt to delete instance of GNEReferenceCounter with count " + toString(myCount));
         }
     }
 
-
+    /// @brief Decrease reference
     void decRef(const std::string& debugMsg = "") {
+        // debugMsg only used for print debugging
 #ifdef _DEBUG_REFERENCECOUNTER
         std::cout << "decRef (" + toString(myCount) + ") for " + getID() + ": " << debugMsg << "\n";
+#else
+        UNUSED_PARAMETER(debugMsg); 
 #endif
-        UNUSED_PARAMETER(debugMsg); // only used for print debugging
+        // write error if decrement results into a negative count
         if (myCount < 1) {
-            throw ProcessError("Attempt to decrement references below zero for instance of GNEReferenceCounter");
+            WRITE_ERROR("Attempt to decrement references below zero for instance of GNEReferenceCounter");
         }
         myCount--;
     }
 
-
+    /// @brief Increarse reference
     void incRef(const std::string& debugMsg = "") {
+        // debugMsg only used for print debugging
 #ifdef _DEBUG_REFERENCECOUNTER
         std::cout << "incRef (" + toString(myCount) + ") for " + getID() + ": " << debugMsg << "\n";
+#else
+        UNUSED_PARAMETER(debugMsg);
 #endif
-        UNUSED_PARAMETER(debugMsg); // only used for print debugging
         myCount++;
     }
 
-
+    /// @brief check if object ins't referenced
     bool unreferenced() {
         return myCount == 0;
     }
 
-
+    /// @brief return ID of object
     virtual const std::string getID() const = 0;
 
 
 private:
+    /// @brief reference counter
     int myCount;
 
 };
