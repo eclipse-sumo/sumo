1774745f8b873a7afc4aac4e39ba1ce2e002e60e
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-08-15 19:24:53 +0000

    extended test for adding trip-like person walk
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25623 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/pythonApi/person/runner.py
+++ sumo/tests/complex/traci/pythonApi/person/runner.py
@@ -53,9 +53,18 @@ for i in range(3):
     print("step", step())
 
 
+# test rerouting
 print("detour edges", traci.person.getEdges("detour"))
 traci.person.rerouteTraveltime("detour")
 print("detour edges after routing", traci.person.getEdges("detour"))
+
+
+# trip-like rerouting
+traci.person.add("tripTest", "2fi", -10)
+traci.person.appendWalkingStage("tripTest", ["2fi", "1fi"], -20)
+traci.person.removeStage("tripTest", 0) # proceed from initial waiting-to-depart stage
+traci.person.rerouteTraveltime("tripTest")
+print("tripTest edges after routing", traci.person.getEdges("tripTest"))
 
 personID = "horiz"
 traci.person.setType(personID, "pType2")
