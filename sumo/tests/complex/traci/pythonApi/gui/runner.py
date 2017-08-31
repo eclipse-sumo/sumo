a335bf276bb73a62387d68ad7ff6f965362ce54b
Jakob Erdmann <jakob.erdmann@dlr.de>
2017-08-31 06:07:30 +0000

    trying to make screenshot test repeatable. refs #21
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25754 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/pythonApi/gui/runner.py
+++ sumo/tests/complex/traci/pythonApi/gui/runner.py
@@ -56,7 +56,7 @@ for step in range(3, 6):
     print("step", step)
     traci.simulationStep()
     print(traci.gui.getSubscriptionResults(viewID))
-traci.gui.screenshot(viewID, "out.png")
+traci.gui.screenshot(viewID, "out.ps")
 traci.gui.screenshot(viewID, "test.blub")
 traci.close()
 sumoProcess.wait()
