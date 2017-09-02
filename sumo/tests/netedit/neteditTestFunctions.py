ee1baef1edfe7cb5ebe9670d534b0aeded925701
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-09-02 13:16:09 +0000

    Now netedit test support shapes. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25795 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/neteditTestFunctions.py
+++ sumo/tests/netedit/neteditTestFunctions.py
@@ -42,71 +42,78 @@ referenceImage = os.path.join("imageResources", "reference.png")
 # interaction functions
 #################################################
 
-# type escape key
-
-
+""" 
+@brief type escape key
+"""
 def typeEscape():
     # wait before every operation
     wait(DELAY)
     # type ESC key (Sikulix Function)
     type(Key.ESC)
 
-# type enter  key
 
-
+"""
+@brief type enter key
+"""
 def typeEnter():
     # wait before every operation
     wait(DELAY)
     # type enter key (Sikulix Function)
     type(Key.ENTER)
 
-# type space key
 
-
+"""
+@brief type space key
+"""
 def typeSpace():
     wait(DELAY)
     # type space key (Sikulix Function)
     type(Key.SPACE)
 
-# type tab key
 
-
+"""
+@brief type tab key
+"""
 def typeTab():
     # wait before every operation
     wait(DELAY)
     # type tab key (Sikulix Function)
     type(Key.TAB)
 
-# type Shift + Tab keys
 
-
+"""
+@brief type Shift + Tab keys
+"""
 def typeInvertTab():
     # wait before every operation
     wait(DELAY)
     # type Tab and Shift at the same time (Sikulix Function)
     type(Key.TAB, Key.SHIFT)
 
-# type single key
 
-
+"""
+@brief type single key
+"""
 def typeKey(key):
     # wait before every operation
     wait(DELAY)
     # type keys (Sikulix Function)
     type(key)
 
-# type two keys at the same time
 
-
+"""
+@brief type two keys at the same time
+"""
 def typeTwoKeys(key1, key2):
     # wait before every operation
     wait(DELAY)
     # type two keys at the same time (Sikulix Function)
     type(key1, key2)
 
-# paste value into current text field
 
-
+"""
+@brief paste value into current text field
+"""
 def pasteIntoTextField(value, removePreviousContents=True):
     # wait before every operation
     wait(DELAY)
@@ -117,9 +124,10 @@ def pasteIntoTextField(value, removePreviousContents=True):
     # paste string (Sikulix Function)
     paste(value)
 
-# do left click over a position relative to match (pink square)
 
-
+"""
+@brief do left click over a position relative to match (pink square)
+"""
 def leftClick(match, positionx, positiony):
     # wait before every operation
     wait(DELAY)
@@ -127,6 +135,9 @@ def leftClick(match, positionx, positiony):
     click(match.getTarget().offset(positionx, positiony))
 
 
+"""
+@brief drag and drop from position 1 to position 2
+"""
 def dragDrop(match, x1, y1, x2, y2):
     # wait before every operation
     wait(DELAY)
@@ -138,7 +149,9 @@ def dragDrop(match, x1, y1, x2, y2):
 # basic functions
 #################################################
 
-
+"""
+@brief setup netedit
+"""
 def setup(neteditTests):
     # Open current environment file to obtain path to the netedit app,
     # textTestSandBox
@@ -156,6 +169,9 @@ def setup(neteditTests):
         neteditTests, "imageResources", "reference.png")
 
 
+"""
+@brief open netedit
+"""
 def Popen(extraParameters, debugInformation):
     # set the default parameters of netedit
     neteditCall = [neteditApp, '--gui-testing', '--window-pos', '50,50',
@@ -175,6 +191,11 @@ def Popen(extraParameters, debugInformation):
     if os.path.exists(os.path.join(textTestSandBox, "input_additionals.add.xml")):
         neteditCall += ['--sumo-additionals-file',
                         os.path.join(textTestSandBox, "input_additionals.add.xml")]
+                        
+    # Check if shapes must be loaded
+    if os.path.exists(os.path.join(textTestSandBox, "input_shapes.add.xml")):
+        neteditCall += ['--sumo-shapes-file',
+                        os.path.join(textTestSandBox, "input_shapes.add.xml")]
 
     # check if a gui settings file has to be load
     if os.path.exists(os.path.join(textTestSandBox, "gui-settings.xml")):
@@ -189,15 +210,21 @@ def Popen(extraParameters, debugInformation):
     neteditCall += ['--additionals-output',
                     os.path.join(textTestSandBox, "additionals.xml")]
 
+    # set output for shapes
+    neteditCall += ['--shapes-output',
+                    os.path.join(textTestSandBox, "shapes.xml")]
+
+                    
     # add extra parameters
     neteditCall += extraParameters
 
     # return a subprocess with netedit
     return subprocess.Popen(neteditCall, env=os.environ, stdout=sys.stdout, stderr=sys.stderr)
 
-# obtain match
 
-
+"""
+@brief obtain reference match (pink square)
+"""
 def getReferenceMatch(neProcess, waitTime):
     try:
         referenceMatch = wait(referenceImage, waitTime)
@@ -210,9 +237,10 @@ def getReferenceMatch(neProcess, waitTime):
         # print debug information
         sys.exit("TestFunctions: Killed netedit process. 'reference.png' not found")
 
-# setup and start netedit
 
-
+"""
+@brief setup and start netedit
+"""
 def setupAndStart(testRoot, extraParameters=[], debugInformation=True, searchReference=True, waitTime=DELAY_REFERENCE):
     setup(testRoot)
     # Open netedit
@@ -231,15 +259,16 @@ def setupAndStart(testRoot, extraParameters=[], debugInformation=True, searchRef
         wait(1)
         return neteditProcess
 
-# rebuild network
-
-
+"""
+@brief rebuild network
+"""
 def rebuildNetwork():
     typeKey(Key.F5)
 
 
-# rebuild network with volatile options
-
+"""
+@brief rebuild network with volatile options
+"""
 def rebuildNetworkWithVolatileOptions(question=True):
     typeTwoKeys(Key.F5, Key.SHIFT)
     # confirm recompute
@@ -249,26 +278,30 @@ def rebuildNetworkWithVolatileOptions(question=True):
         waitQuestion('n')
 
 
-# clean junction
-
+"""
+@brief clean junction
+"""
 def cleanJunction():
     typeKey(Key.F6)
 
-# join selected junctions
 
-
+"""
+@brief join selected junctions
+"""
 def joinSelectedJunctions():
     typeKey(Key.F7)
 
-# select focus on upper element of current frame
 
-
+"""
+@brief select focus on upper element of current frame
+"""
 def focusOnFrame():
     typeKey(Key.F12)
 
-# undo last operation
 
-
+"""
+@brief undo last operation
+"""
 def undo(match, number):
     # needed to avoid errors with undo/redo (Provisionally)
     typeKey("i")
@@ -277,9 +310,10 @@ def undo(match, number):
     for x in range(0, number):
         typeTwoKeys("z", Key.CTRL)
 
-# undo last operation
 
-
+"""
+@brief undo last operation
+"""
 def redo(match, number):
     # needed to avoid errors with undo/redo (Provisionally)
     typeKey("i")
@@ -288,9 +322,10 @@ def redo(match, number):
     for x in range(0, number):
         typeTwoKeys("y", Key.CTRL)
 
-# set Zoom
 
-
+"""
+@brief set Zoom
+"""
 def setZoom(positionX, positionY, zoomLevel):
     # open edit viewport dialog
     typeKey("v")
@@ -307,18 +342,20 @@ def setZoom(positionX, positionY, zoomLevel):
     # press OK Button using shortcut
     typeTwoKeys('o', Key.ALT)
 
-# netedit wait question
 
-
+"""
+@brief wait question of netedit and select a yes/no answer
+"""
 def waitQuestion(answer):
     # wait 0.5 second to question dialog
     wait(DELAY_QUESTION)
     # Answer can be "y" or "n"
     typeTwoKeys(answer, Key.ALT)
 
-# netedit quit
 
-
+"""
+@brief quit netedit quit
+"""
 def quit(neteditProcess, openNonSavedNetDialog=False, saveNet=False, openedAdditionalsNonSavedDialog=False, saveAdditionals=False):
     # check if netedit is already closed
     if neteditProcess.poll() is not None:
@@ -352,22 +389,34 @@ def quit(neteditProcess, openNonSavedNetDialog=False, saveNet=False, openedAddit
             # print debug information
             print("TestFunctions: Error closing Netedit")
 
-# save network
 
-
+"""
+@brief save network
+"""
 def saveNetwork():
     # save newtork using hotkey
     typeTwoKeys("s", Key.CTRL)
 
-# save additionals
 
-
+"""
+@brief save additionals
+"""
 def saveAdditionals():
     # save additionals using hotkey
     typeTwoKeys("d", Key.CTRL + Key.SHIFT)
 
 
-# open and close about dialog
+"""
+@brief save shapes
+"""
+def saveShapes():
+    # save additionals using hotkey
+    typeTwoKeys("p", Key.CTRL + Key.SHIFT)
+
+
+"""
+@brief open and close about dialog
+"""
 def openAboutDialog(waitingTime=DELAY_QUESTION):
     # type F2 to open about dialog
     typeKey(Key.F2)
@@ -376,27 +425,28 @@ def openAboutDialog(waitingTime=DELAY_QUESTION):
     # press enter to close dialog (Ok must be focused)
     typeSpace()
 
-
 #################################################
-# Create edge
+# Create nodes and edges
 #################################################
 
-# Change to create edge mode
-
-
+""" 
+@brief Change to create edge mode
+"""
 def createEdgeMode():
     typeKey("e")
 
-# Cancel current created edge (used in chain mode)
 
-
+"""
+@brief Cancel current created edge (used in chain mode)
+"""
 def cancelEdge():
     # type ESC to cancel current edge
     typeEscape()
 
-# Change chain option
 
-
+"""
+@brief Change chain option
+"""
 def changeChainOption():
     # cancel current created edge
     cancelEdge()
@@ -406,9 +456,10 @@ def changeChainOption():
     # change chain mode
     typeSpace()
 
-# Change two-way mode
 
-
+"""
+@brief Change two-way mode
+"""
 def changeTwoWayOption():
     # cancel current created edge
     cancelEdge()
@@ -422,15 +473,15 @@ def changeTwoWayOption():
 # Inspect mode
 #################################################
 
-# go to inspect mode
-
-
+"""
+@brief go to inspect mode
+"""
 def inspectMode():
     typeKey("i")
 
-# netedit modify int/float/string
-
-
+"""
+@brief modify attribute of type int/float/string
+"""
 def modifyAttribute(attributeNumber, value):
     # focus current frame
     focusOnFrame()
@@ -442,9 +493,10 @@ def modifyAttribute(attributeNumber, value):
     # type Enter to commit change
     typeEnter()
 
-# netedit modify bool attribute
 
-
+"""
+@brief modify boolean attribute
+"""
 def modifyBoolAttribute(attributeNumber):
     # focus current frame
     focusOnFrame()
@@ -458,15 +510,16 @@ def modifyBoolAttribute(attributeNumber):
 # Move mode
 #################################################
 
-# set move mode
-
-
+"""
+@brief set move mode
+"""
 def moveMode():
     typeKey("m")
 
-# move element
 
-
+"""    
+@brief move element
+"""
 def moveElement(match, startX, startY, endX, endY):
     # change mouse move delay
     Settings.MoveMouseDelay = 0.5
@@ -479,15 +532,16 @@ def moveElement(match, startX, startY, endX, endY):
 # crossings
 #################################################
 
-# Change to crossing mode
-
-
+"""
+@brief Change to crossing mode
+"""
 def crossingMode():
     typeKey("r")
 
-# create crossing
 
-
+"""
+@brief create crossing
+"""
 def createCrossing():
     # focus current frame
     focusOnFrame()
@@ -498,9 +552,9 @@ def createCrossing():
     typeSpace()
 
 
-# change default int/real/string crossing default value
-
-
+"""
+@brief change default int/real/string crossing default value
+"""
 def modifyCrossingDefaultValue(numtabs, value):
     # focus current frame
     focusOnFrame()
@@ -512,9 +566,10 @@ def modifyCrossingDefaultValue(numtabs, value):
     # type enter to save change
     typeEnter()
 
-# change default boolean crossing default value
 
-
+"""
+@brief change default boolean crossing default value
+"""
 def modifyCrossingDefaultBoolValue(numtabs):
     # focus current frame
     focusOnFrame()
@@ -524,9 +579,10 @@ def modifyCrossingDefaultBoolValue(numtabs):
     # type space to change value
     typeSpace()
 
-# clear crossing
 
-
+"""
+@brief clear crossing
+"""
 def crossingClearEdges(useSelectedEdges=False):
     # focus current frame
     focusOnFrame()
@@ -541,9 +597,10 @@ def crossingClearEdges(useSelectedEdges=False):
     # type space to activate button
     typeSpace()
 
-# invert crossing
 
-
+"""
+@brief invert crossing
+"""
 def crossingInvertEdges(useSelectedEdges=False):
     # focus current frame
     focusOnFrame()
@@ -562,15 +619,16 @@ def crossingInvertEdges(useSelectedEdges=False):
 # additionals
 #################################################
 
-# change to additional mode
-
-
+"""
+@brief change to additional mode
+"""
 def additionalMode():
     typeKey('a')
 
-# change additional
 
-
+"""
+@brief change additional
+"""
 def changeAdditional(additional):
     # focus current frame
     focusOnFrame()
@@ -581,9 +639,10 @@ def changeAdditional(additional):
     # type enter to save change
     typeEnter()
 
-# modify default int/double/string value of an additional
 
-
+"""
+@brief modify default int/double/string value of an additional
+"""
 def modifyAdditionalDefaultValue(numTabs, length):
     # focus current frame
     focusOnFrame()
@@ -596,9 +655,9 @@ def modifyAdditionalDefaultValue(numTabs, length):
     typeEnter()
 
 
-# modify default boolean value of an additional
-
-
+"""
+@brief modify default boolean value of an additional
+"""
 def modifyAdditionalDefaultBoolValue(numTabs):
     # focus current frame
     focusOnFrame()
@@ -608,9 +667,10 @@ def modifyAdditionalDefaultBoolValue(numTabs):
     # Change current value
     typeSpace()
 
-# modify number of stopping place lines
 
-
+"""
+@brief modify number of stopping place lines
+"""
 def modifyStoppingPlaceLines(numTabs, numLines):
     # focus current frame
     focusOnFrame()
@@ -621,9 +681,10 @@ def modifyStoppingPlaceLines(numTabs, numLines):
     for x in range(0, numLines):
         typeSpace()
 
-# fill lines to stopping places
 
-
+"""
+@brief fill lines to stopping places
+"""
 def fillStoppingPlaceLines(numTabs, numLines):
     # focus current frame
     focusOnFrame()
@@ -637,9 +698,10 @@ def fillStoppingPlaceLines(numTabs, numLines):
         # go to next field
         typeTab()
 
-# select child of additional
 
-
+"""
+@brief select child of additional
+"""
 def selectAdditionalChild(numTabs, childNumber):
     # focus current frame
     focusOnFrame()
@@ -655,9 +717,10 @@ def selectAdditionalChild(numTabs, childNumber):
     # use TAB to select additional child
     typeTab()
 
-# fix stoppingPlaces
 
-
+"""
+@brief fix stoppingPlaces
+"""
 def fixStoppingPlace(solution, cancel=False):
     # select bullet depending of solution
     if (solution == "saveInvalids"):
@@ -680,18 +743,20 @@ def fixStoppingPlace(solution, cancel=False):
     else:
         typeTwoKeys("a", Key.ALT)
 
-
 #################################################
 # delete
 #################################################
 
-# Change to delete mode
+"""
+@brief Change to delete mode
+"""
 def deleteMode():
     typeKey("d")
 
-# Enable or disable 'automatically delete Additionals'
 
-
+"""
+@brief Enable or disable 'automatically delete Additionals'
+"""
 def changeAutomaticallyDeleteAdditionals(match):
     # select delete mode again to set mode
     deleteMode()
@@ -700,9 +765,10 @@ def changeAutomaticallyDeleteAdditionals(match):
     # type SPACE to change value
     typeSpace()
 
-# close warning about automatically delete additionals
 
-
+"""
+@brief close warning about automatically delete additionals
+"""
 def waitAutomaticallyDeleteAdditionalsWarning():
     # wait 0.5 second to question dialog
     wait(DELAY_QUESTION)
@@ -713,31 +779,34 @@ def waitAutomaticallyDeleteAdditionalsWarning():
 # select mode
 #################################################
 
-# Change to select mode
-
-
+"""
+@brief Change to select mode
+"""
 def selectMode():
     typeKey("s")
 
-# abort current selection
 
-
+"""
+@brief abort current selection
+"""
 def abortSelection():
     # type ESC to abort current selection
     typeEscape()
 
-# select elements with default frame values
 
-
+"""
+@brief select elements with default frame values
+"""
 def selectDefault():
     for x in range(0, 11):
         typeTab()
     # type enter to select it
     typeEnter()
 
-# select items
 
-
+"""
+@brief select items
+"""
 def selectItems(elementClass, elementType, attribute, value):
     # focus current frame
     focusOnFrame()
@@ -764,15 +833,17 @@ def selectItems(elementClass, elementType, attribute, value):
     # type enter to select it
     typeEnter()
 
-# delete selected items
 
-
+"""
+@brief delete selected items
+"""
 def deleteSelectedItems():
     typeKey(Key.DELETE)
 
-# set modification mode "add"
 
-
+"""
+@brief set modification mode "add"
+"""
 def modificationModeAdd():
     # focus current frame
     focusOnFrame()
@@ -781,9 +852,10 @@ def modificationModeAdd():
     # select it
     typeSpace()
 
-# set modification mode "remove"
 
-
+"""
+@brief set modification mode "remove"
+"""
 def modificationModeRemove():
     # focus current frame
     focusOnFrame()
@@ -793,9 +865,10 @@ def modificationModeRemove():
     # select it
     typeSpace()
 
-# set modification mode "keep"
 
-
+"""
+@brief set modification mode "keep"
+"""
 def modificationModeKeep():
     # focus current frame
     focusOnFrame()
@@ -805,9 +878,10 @@ def modificationModeKeep():
     # select it
     typeSpace()
 
-# set modification mode "replace"
 
-
+"""
+@brief set modification mode "replace"
+"""
 def modificationModeReplace():
     # focus current frame
     focusOnFrame()
@@ -817,9 +891,10 @@ def modificationModeReplace():
     # select it
     typeSpace()
 
-# select using an rectangle
 
-
+"""
+@brief select using an rectangle
+"""
 def selectionRectangle(match, startX, startY, endX, endY):
     # Leave Shift key pressed (Sikulix function)
     keyDown(Key.SHIFT)
@@ -832,9 +907,10 @@ def selectionRectangle(match, startX, startY, endX, endY):
     # Release Shift key (Sikulix function)
     keyUp(Key.SHIFT)
 
-# clear selection
 
-
+"""
+@brief clear selection
+"""
 def selectionClear(previouslyInserted=False):
     # focus current frame
     focusOnFrame()
@@ -843,9 +919,10 @@ def selectionClear(previouslyInserted=False):
     # type space to select clear option
     typeSpace()
 
-# invert selection
 
-
+"""
+@brief invert selection
+"""
 def selectionInvert():
     # focus current frame
     focusOnFrame()
@@ -855,7 +932,9 @@ def selectionInvert():
     typeSpace()
 
 
-# Toogle select edges and lanes
+"""
+@brief Toogle select edges and lanes
+"""
 def selectionToogleEdges():
     # go to check box "select edges"
     for x in range(0, 3):
@@ -863,19 +942,20 @@ def selectionToogleEdges():
     # type space to enable or disable edge selection
     typeSpace()
 
-
 #################################################
 # traffic light
 #################################################
 
-# Change to traffic light mode
-
-
+"""
+@brief Change to traffic light mode
+"""
 def selectTLSMode():
     typeKey("t")
 
 
-# Create TLS
+"""
+@brief Create TLS
+"""
 def createTLS():
     # focus current frame
     focusOnFrame()
