f0cb16aad28b8811f3d5746ce6b95772e6ce3d42
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-09-05 13:13:44 +0000

    Added netedit test shapeElements/poi/delete. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25857 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/shapeElements/poi/delete/test.sikuli/test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2016-11-25
+@version $Id: test.py 25301 2017-07-24 09:18:25Z palcraft $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot)
+
+# go to shape mode
+netedit.shapeMode()
+
+# go to additional mode
+netedit.changeShape("poi")
+
+# create POI
+netedit.leftClick(match, 150, 50)
+
+# go to delete mode
+netedit.deleteMode()
+
+# delete created poi
+netedit.leftClick(match, 150, 50)
+
+# delete first stacked loaded POI (Second stacked must stay)
+netedit.leftClick(match, 200, 50)
+
+# save shapes
+netedit.saveShapes()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
