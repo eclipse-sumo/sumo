087784c89ba12a6638a7a7e1418993ffea7b8a8c
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-09-05 12:23:33 +0000

    Added netedit test shapeElements/basic/shape_invalid. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@25853 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/tests/netedit/shapeElements/basic/shape_invalid/test.sikuli/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+@file    test.py
+@author  Pablo Alvarez Lopez
+@date    2017-03-12
+@version $Id: test.py 25267 2017-07-19 10:41:16Z behrisch $
+
+python script used by sikulix for testing netedit
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, match = netedit.setupAndStart(neteditTestRoot)
+
+# go to shape mode
+netedit.shapeMode()
+
+# select invalid shape (dummy)
+netedit.changeShape("dummyShape")
+
+# try to create an dummy shape
+netedit.leftClick(match, 150, 50)
+
+# select valid additional (POI)
+netedit.changeShape("poi")
+
+# create POI
+netedit.leftClick(match, 150, 50)
+
+# save additionals
+netedit.saveShapes()
+
+# save newtork
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
