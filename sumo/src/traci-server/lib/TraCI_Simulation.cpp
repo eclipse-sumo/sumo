fd3ffbfb5d053de89d9a421149b03009103b4756
Laura Bieker <laura.bieker@dlr.de>
2017-09-15 08:59:53 +0000

    Header for traci lib simulation ref #2942
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26029 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/traci-server/lib/TraCI_Simulation.cpp
@@ -0,0 +1,48 @@
+/****************************************************************************/
+/// @file    TraCI_Simulation.cpp
+/// @author  Laura Bieker-Walz
+/// @author  Robert Hilbrich
+/// @date    15.09.2017
+/// @version $Id: TraCI_POI.cpp 25277 2017-07-20 11:15:41Z behrisch $
+///
+// C++ TraCI client API implementation
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2017-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#ifndef NO_TRACI
+
+#include <utils/common/StdDefs.h>
+#include <utils/geom/GeoConvHelper.h>
+#include <microsim/MSNet.h>
+#include <microsim/MSEdgeControl.h>
+#include <microsim/MSInsertionControl.h>
+#include <microsim/MSEdge.h>
+#include <microsim/MSLane.h>
+#include <microsim/MSVehicle.h>
+#include <microsim/MSVehicleControl.h>
+#include <microsim/MSStateHandler.h>
+#include <microsim/MSStoppingPlace.h>
+#include <traci-server/lib/TraCI.h>
+#include "TraCIConstants.h"
+#include "TraCI_Simulation.h"
+
+#endif
---
+++ sumo/src/traci-server/lib/TraCI_Simulation.h
@@ -0,0 +1,23 @@
+/****************************************************************************/
+/// @file    TraCI_Simulation.h
+/// @author  Robert Hilbrich
+/// @date    15.09.2017
+/// @version $Id: TraCI_POI.h 23594 2017-03-22 07:37:54Z namdre $
+///
+// C++ TraCI client API implementation
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+// Copyright (C) 2012-2017 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This file is part of SUMO.
+//   SUMO is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef TraCI_Simulation_h
+#define TraCI_Simulation_h
+
+#endif
