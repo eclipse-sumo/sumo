4e6f25b88c40ebf34e697cd5d8b08fe96031954b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-09-18 12:19:09 +0000

    Fixed netedit test additionalElements/calibrator_lane. Refs #3045
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26085 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/netedit/additionalElements/calibrator_lane/create/test.sikuli/test.py
+++ sumo/tests/netedit/additionalElements/calibrator_lane/create/test.sikuli/test.py
@@ -38,27 +38,75 @@ netedit.changeAdditional("calibrator (lane)")
 # create calibrator
 netedit.leftClick(match, 240, 250)
 
-# change frequency with an invalid value (dummy)
+# change position with an invalid value (dummy)
 netedit.modifyAdditionalDefaultValue(2, "dummyValue")
 
 # create calibrator with an invalid parameter (Default value will be used)
 netedit.leftClick(match, 400, 215)
 
-# change frequency with an invalid value (negative)
-netedit.modifyAdditionalDefaultValue(2, "-30")
+# change position with an invalid value (negative)
+netedit.modifyAdditionalDefaultValue(2, "-5")
 
 # create calibrator with an invalid parameter (Default value will be used)
 netedit.leftClick(match, 400, 215)
 
+# change position with n valid value
+netedit.modifyAdditionalDefaultValue(2, "10")
+
+# create calibrator with an invalid parameter (Default value will be used)
+netedit.leftClick(match, 400, 215)
+
+# change frequency with an invalid value (dummy)
+netedit.modifyAdditionalDefaultValue(3, "dummyValue")
+
+# create calibrator with an invalid parameter (Default value will be used)
+netedit.leftClick(match, 400, 180)
+
+# change frequency with an invalid value (negative)
+netedit.modifyAdditionalDefaultValue(3, "-30")
+
+# create calibrator with an invalid parameter (Default value will be used)
+netedit.leftClick(match, 400, 180)
+
 # change frequency with a valid value
-netedit.modifyAdditionalDefaultValue(2, "250")
+netedit.modifyAdditionalDefaultValue(3, "250")
 
 # create calibrator with a valid parameter in other lane
 netedit.leftClick(match, 400, 180)
 
+# change routeprobe with a invalid routeProbe
+netedit.modifyAdditionalDefaultValue(4, "%%$···$%")
+
+# create calibrator with a different routeProbe in other lane
+netedit.leftClick(match, 200, 230)
+
+# change routeprobe with a different routeProbe (valid, empty)
+netedit.modifyAdditionalDefaultValue(4, "")
+
+# create calibrator with a valid parameter in other lane
+netedit.leftClick(match, 200, 230)
+
+# change routeprobe with a different routeProbe
+netedit.modifyAdditionalDefaultValue(4, "routeProbe")
+
+# create calibrator with a valid parameter in other lane
+netedit.leftClick(match, 200, 230)
+
+# change output with an invalid value 
+netedit.modifyAdditionalDefaultValue(5, "%%%%###!!!")
+
+# create calibrator with a valid parameter in other lane
+netedit.leftClick(match, 200, 230)
+
+# change output with an valid value 
+netedit.modifyAdditionalDefaultValue(5, "myOwnOutput")
+
+# create calibrator with a valid parameter in other lane
+netedit.leftClick(match, 200, 230)
+
 # Check undo redo
-netedit.undo(match, 4)
-netedit.redo(match, 4)
+netedit.undo(match, 7)
+netedit.redo(match, 7)
 
 # save additionals
 netedit.saveAdditionals()
