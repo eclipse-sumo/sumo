de4f82f186375df1281edd851a9ba1f124e0907b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-10-04 12:03:29 +0000

    Fixed #2330
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26320 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/netedit/GNELane.cpp
+++ sumo/src/netedit/GNELane.cpp
@@ -468,19 +468,17 @@ GNELane::getPopUpMenu(GUIMainWindow& app, GUISUMOAbstractView& parent) {
         FXIcon* bikeIcon = GUIIconSubSys::getIcon(ICON_LANEBIKE);
         FXIcon* busIcon = GUIIconSubSys::getIcon(ICON_LANEBUS);
         // Create basic commands
+        std::string plural = myNet->retrieveEdges(true).size() > 1? "s" : "";
         new FXMenuCommand(ret, ("Split " + toString(SUMO_TAG_EDGE) + " here").c_str(), 0, &parent, MID_GNE_EDGE_SPLIT);
         new FXMenuCommand(ret, ("Split " + toString(SUMO_TAG_EDGE) + "s in both direction here").c_str(), 0, &parent, MID_GNE_EDGE_SPLIT_BIDI);
         new FXMenuCommand(ret, ("Reverse " + toString(SUMO_TAG_EDGE)).c_str(), 0, &parent, MID_GNE_EDGE_REVERSE);
         new FXMenuCommand(ret, "Add reverse direction", 0, &parent, MID_GNE_EDGE_ADD_REVERSE);
         new FXMenuCommand(ret, "Set geometry endpoint here", 0, &parent, MID_GNE_EDGE_SET_ENDPOINT);
         new FXMenuCommand(ret, "Restore geometry endpoint", 0, &parent, MID_GNE_EDGE_RESET_ENDPOINT);
+        new FXMenuCommand(ret, ("Straighten " + toString(SUMO_TAG_EDGE) + plural).c_str(), 0, &parent, MID_GNE_EDGE_STRAIGHTEN);
         if (gSelected.isSelected(GLO_LANE, getGlID())) {
-            new FXMenuCommand(ret, ("Straighten selected " + toString(SUMO_TAG_EDGE) + "s").c_str(), 0, &parent, MID_GNE_EDGE_STRAIGHTEN);
-        } else {
-            new FXMenuCommand(ret, ("Straighten " + toString(SUMO_TAG_EDGE)).c_str(), 0, &parent, MID_GNE_EDGE_STRAIGHTEN);
-        }
-        if (gSelected.isSelected(GLO_LANE, getGlID())) {
-            new FXMenuCommand(ret, ("Duplicate selected " + toString(SUMO_TAG_LANE) + "s").c_str(), 0, &parent, MID_GNE_LANE_DUPLICATE);
+            std::string plural = myNet->retrieveLanes(true).size() > 1? "s" : "";
+            new FXMenuCommand(ret, ("Duplicate selected " + toString(SUMO_TAG_LANE) + plural).c_str(), 0, &parent, MID_GNE_LANE_DUPLICATE);
             // Create panel for lane operations
             FXMenuPane* addSpecialLanes = new FXMenuPane(ret);
             ret->insertMenuPaneChild(addSpecialLanes);
