b9773d100b8798dd8c9914e05267d238df83583b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2017-10-11 08:58:32 +0000

    Updated GNEPOILane. Refs #1667
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26417 afbd958f-9f77-42d5-a016-97a22340ccf4

---
+++ sumo/src/netedit/GNEPOILane.cpp
@@ -0,0 +1,313 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2017 German Aerospace Center (DLR) and others.
+/****************************************************************************/
+//
+//   This program and the accompanying materials
+//   are made available under the terms of the Eclipse Public License v2.0
+//   which accompanies this distribution, and is available at
+//   http://www.eclipse.org/legal/epl-v20.html
+//
+/****************************************************************************/
+/// @file    GNEPOI.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2017
+/// @version $Id: GNEPOI.cpp 26411 2017-10-10 13:21:07Z palcraft $
+///
+// A class for visualizing and editing POIS in netedit (adapted from
+// GUIPointOfInterest and NLHandler)
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <utility>
+#include <utils/foxtools/MFXImageHelper.h>
+#include <utils/geom/Position.h>
+#include <utils/geom/GeomConvHelper.h>
+#include <utils/geom/GeoConvHelper.h>
+#include <utils/common/MsgHandler.h>
+#include <utils/xml/XMLSubSys.h>
+#include <utils/gui/windows/GUIAppEnum.h>
+#include <utils/gui/windows/GUISUMOAbstractView.h>
+#include <utils/gui/globjects/GUIGLObjectPopupMenu.h>
+#include <utils/gui/div/GUIGlobalSelection.h>
+#include <utils/gui/div/GUIParameterTableWindow.h>
+#include <utils/gui/div/GLHelper.h>
+#include <utils/gui/images/GUITexturesHelper.h>
+#include <utils/gui/images/GUIIconSubSys.h>
+#include <utils/gui/globjects/GUIGlObjectStorage.h>
+#include <utils/gui/globjects/GUIGLObjectPopupMenu.h>
+#include <netimport/NIImporter_SUMO.h>
+#include <netwrite/NWWriter_SUMO.h>
+#include "GNENet.h"
+#include "GNEEdge.h"
+#include "GNEUndoList.h"
+#include "GNEViewNet.h"
+#include "GNEChange_Attribute.h"
+#include "GNEPOILane.h"
+#include "GNELane.h"
+
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+
+GNEPOILane::GNEPOILane(GNENet* net, const std::string& id, const std::string& type, const RGBColor& color,
+    double layer, double angle, const std::string& imgFile, GNELane *lane, double posOverLane,
+    double width, double height, bool movementBlocked) :
+    GUIPointOfInterest(id, type, color, Position(), layer, angle, imgFile, width, height),
+    GNEShape(net, SUMO_TAG_POI, ICON_LOCATEPOI, movementBlocked, false),
+    myLane(lane),
+    myPositionOverLane(posOverLane) {
+}
+
+
+GNEPOILane::~GNEPOILane() {}
+
+
+void 
+GNEPOILane::writeShape(OutputDevice& device) {
+    //writeXML(device, false, 0, myLane->getID()
+}
+
+
+void
+GNEPOILane::moveGeometry(const Position& oldPos, const Position &offset) {
+    if (!myBlockMovement) {
+        // restore old position, apply offset and refresh element
+        set(oldPos);
+        add(offset);
+        myNet->refreshElement(this);
+    }
+}
+
+
+void
+GNEPOILane::commitGeometryMoving(const Position& oldPos, GNEUndoList* undoList) {
+    if (!myBlockMovement) {
+        undoList->p_begin("position of " + toString(getTag()));
+        undoList->p_add(new GNEChange_Attribute(this, SUMO_ATTR_POSITION, toString(getPositionInView()), true, toString(oldPos)));
+        undoList->p_end();
+        // Refresh element
+        //myNet->refreshPOI(this);
+    }
+}
+
+
+Position
+GNEPOILane::getPositionInView() const {
+    return Position(x(), y());
+}
+
+
+const std::string&
+GNEPOILane::getParentName() const {
+    return myNet->getMicrosimID();
+}
+
+
+GUIGLObjectPopupMenu*
+GNEPOILane::getPopUpMenu(GUIMainWindow& app, GUISUMOAbstractView& parent) {
+    return GUIPointOfInterest::getPopUpMenu(app, parent);
+}
+
+
+GUIParameterTableWindow*
+GNEPOILane::getParameterWindow(GUIMainWindow& app, GUISUMOAbstractView& parent) {
+    return GUIPointOfInterest::getParameterWindow(app, parent);
+}
+
+
+Boundary
+GNEPOILane::getCenteringBoundary() const {
+    return GUIPointOfInterest::getCenteringBoundary();
+}
+
+
+void
+GNEPOILane::drawGL(const GUIVisualizationSettings& s) const {
+    GUIPointOfInterest::drawGL(s);
+    // draw lock icon
+    drawLockIcon(*this, GLO_POI);
+}
+
+
+std::string
+GNEPOILane::getAttribute(SumoXMLAttr key) const {
+    switch (key) {
+    case SUMO_ATTR_ID:
+        return myID;
+    case SUMO_ATTR_COLOR:
+        return toString(myColor);
+    case SUMO_ATTR_POSITION:
+        return toString(myPositionOverLane);
+    case SUMO_ATTR_LANE:
+        return myLane->getID();
+    case SUMO_ATTR_TYPE:
+        return myType;
+    case SUMO_ATTR_LAYER:
+        return toString(myLayer);
+    case SUMO_ATTR_IMGFILE:
+        return myImgFile;
+    case SUMO_ATTR_WIDTH:
+        return toString(getWidth());
+    case SUMO_ATTR_HEIGHT:
+        return toString(getHeight());
+    case SUMO_ATTR_ANGLE:
+        return toString(getNaviDegree());
+    case GNE_ATTR_BLOCK_MOVEMENT:
+        return toString(myBlockMovement);
+    default:
+        throw InvalidArgument(toString(getTag()) + " attribute '" + toString(key) + "' not allowed");
+    }
+}
+
+
+void
+GNEPOILane::setAttribute(SumoXMLAttr key, const std::string& value, GNEUndoList* undoList) {
+    if (value == getAttribute(key)) {
+        return; //avoid needless changes, later logic relies on the fact that attributes have changed
+    }
+    switch (key) {
+    case SUMO_ATTR_ID:
+    case SUMO_ATTR_COLOR:
+    case SUMO_ATTR_POSITION:
+    case SUMO_ATTR_LANE:
+    case SUMO_ATTR_TYPE:
+    case SUMO_ATTR_LAYER:
+    case SUMO_ATTR_IMGFILE:
+    case SUMO_ATTR_WIDTH:
+    case SUMO_ATTR_HEIGHT:
+    case SUMO_ATTR_ANGLE:
+    case GNE_ATTR_BLOCK_MOVEMENT:
+        undoList->p_add(new GNEChange_Attribute(this, key, value));
+        break;
+    default:
+        throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+bool
+GNEPOILane::isValid(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+    case SUMO_ATTR_ID:
+        return isValidID(value) && (myNet->retrievePOI(value, false) == 0);
+    case SUMO_ATTR_COLOR:
+        return canParse<RGBColor>(value);
+        /*
+    case SUMO_ATTR_POSITION: {
+        bool ok;
+        return GeomConvHelper::parseShapeReporting(value, "user-supplied position", 0, ok, false).size() == 1;
+    }
+    case SUMO_ATTR_GEOPOSITION: {
+        bool ok;
+        return GeomConvHelper::parseShapeReporting(value, "user-supplied GEO position", 0, ok, false).size() == 1;
+    }
+    case SUMO_ATTR_GEO:
+        return canParse<bool>(value);
+    */
+    case SUMO_ATTR_TYPE:
+        return true;
+    case SUMO_ATTR_LAYER:
+        return canParse<double>(value);
+    case SUMO_ATTR_IMGFILE:
+        if (value == "") {
+            return true;
+        } else if (isValidFilename(value)) {
+            // check that image can be loaded
+            return GUITexturesHelper::getTextureID(value) != -1;
+        } else {
+            return false;
+        }
+    case SUMO_ATTR_WIDTH:
+        return canParse<double>(value);
+    case SUMO_ATTR_HEIGHT:
+        return canParse<double>(value);
+    case SUMO_ATTR_ANGLE:
+        return canParse<double>(value);
+    case GNE_ATTR_BLOCK_MOVEMENT:
+        return canParse<bool>(value);
+    default:
+        throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+// ===========================================================================
+// private
+// ===========================================================================
+
+void
+GNEPOILane::setAttribute(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+    case SUMO_ATTR_ID: {
+        std::string oldID = myID;
+        myID = value;
+        //myNet->changePOIID(this, oldID);
+        break;
+    }
+    case SUMO_ATTR_COLOR:
+        myColor = parse<RGBColor>(value);
+        break;
+    /*
+    case SUMO_ATTR_POSITION: {
+        bool ok = true;
+        set(GeomConvHelper::parseShapeReporting(value, "netedit-given", 0, ok, false)[0]);
+        // set GEO Position
+        myGEOPosition = *this;
+        GeoConvHelper::getFinal().cartesian2geo(myGEOPosition);
+        myNet->refreshPOI(this);
+        break;
+    }
+    case SUMO_ATTR_GEOPOSITION: {
+        bool ok = true;
+        myGEOPosition = GeomConvHelper::parseShapeReporting(value, "netedit-given", 0, ok, false)[0];
+        // set cartesian Position
+        set(myGEOPosition);
+        GeoConvHelper::getFinal().x2cartesian_const(*this);
+        myNet->refreshPOI(this);
+        break;
+    }
+    case SUMO_ATTR_GEO:
+        myUseGEO = parse<bool>(value);
+        break;
+    */
+    case SUMO_ATTR_TYPE:
+        myType = value;
+        break;
+    case SUMO_ATTR_LAYER:
+        myLayer = parse<double>(value);
+        break;
+    case SUMO_ATTR_IMGFILE:
+        myImgFile = value;
+        break;
+    case SUMO_ATTR_WIDTH:
+        setWidth(parse<double>(value));
+        break;
+    case SUMO_ATTR_HEIGHT:
+        setHeight(parse<double>(value));
+        break;
+    case SUMO_ATTR_ANGLE:
+        setNaviDegree(parse<double>(value));
+        break;
+    case GNE_ATTR_BLOCK_MOVEMENT:
+        myBlockMovement = parse<bool>(value);
+        break;
+    default:
+        throw InvalidArgument(toString(getTag()) + " attribute '" + toString(key) + "' not allowed");
+    }
+    // update view after every change
+    myNet->getViewNet()->update();
+}
+
+/****************************************************************************/
---
+++ sumo/src/netedit/GNEPOILane.h
@@ -0,0 +1,179 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2017 German Aerospace Center (DLR) and others.
+/****************************************************************************/
+//
+//   This program and the accompanying materials
+//   are made available under the terms of the Eclipse Public License v2.0
+//   which accompanies this distribution, and is available at
+//   http://www.eclipse.org/legal/epl-v20.html
+//
+/****************************************************************************/
+/// @file    GNEPOI.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2017
+/// @version $Id: GNEPOI.h 26372 2017-10-07 12:33:09Z palcraft $
+///
+// A class for visualizing and editing POIS in netedit over lanes (adapted from
+// GUIPointOfInterest and NLHandler)
+/****************************************************************************/
+#ifndef GNEPOILane_h
+#define GNEPOILane_h
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include "GNEShape.h"
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+class GNELane;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+* @class GNEPOI
+*
+* In the case the represented junction's shape is empty, the boundary
+*  is computed using the junction's position to which an offset of 1m to each
+*  side is added.
+*/
+class GNEPOILane : public GUIPointOfInterest, public GNEShape {
+
+public:
+    /// @brief needed to avoid diamond Problem between GUIPolygon and GNEShape
+    using GNEShape::getID;
+
+    /** @brief Constructor
+    * @param[in] net net in which this polygon is placed
+    * @param[in] id The name of the POI
+    * @param[in] type The (abstract) type of the POI
+    * @param[in] color The color of the POI
+    * @param[in] layer The layer of the POI
+    * @param[in] angle The rotation of the POI
+    * @param[in] imgFile The raster image of the shape
+    * @param[in] lane lane in which tis POILane is placed
+    * @param[in] posOverLane Position over lane in which this POILane is placed
+    * @param[in] width The width of the POI image
+    * @param[in] height The height of the POI image
+    * @param[in] movementBlocked if movement of POI is blocked
+    */
+    GNEPOILane(GNENet* net, const std::string& id, const std::string& type, const RGBColor& color, double layer, double angle,
+           const std::string& imgFile, GNELane *lane, double posOverLane, double width, double height, bool movementBlocked);
+
+    /// @brief Destructor
+    ~GNEPOILane();
+
+    /**@brief change the position of the element geometry without saving in undoList
+    * @param[in] newPosition new position of geometry
+    * @note should't be called in drawGL(...) functions to avoid smoothness issues
+    */
+    void moveGeometry(const Position& oldPos, const Position &offset);
+
+    /**@brief commit geometry changes in the attributes of an element after use of moveGeometry(...)
+    * @param[in] oldPos the old position of additional
+    * @param[in] undoList The undoList on which to register changes
+    */
+    void commitGeometryMoving(const Position& oldPos, GNEUndoList* undoList);
+
+    /// @name inherited from GNEShape
+    /// @{
+    /**@brief writte shape element into a xml file
+    * @param[in] device device in which write parameters of additional element
+    */
+    void writeShape(OutputDevice& device);
+
+    /// @brief Returns position of additional in view
+    Position getPositionInView() const;
+    /// @}
+
+    /// @name inherited from GUIGlObject
+    /// @{
+    /**@brief Returns the name of the parent object
+    * @return This object's parent id
+    */
+    const std::string& getParentName() const;
+
+    /**@brief Returns an own popup-menu
+    *
+    * @param[in] app The application needed to build the popup-menu
+    * @param[in] parent The parent window needed to build the popup-menu
+    * @return The built popup-menu
+    * @see GUIGlObject::getPopUpMenu
+    */
+    GUIGLObjectPopupMenu* getPopUpMenu(GUIMainWindow& app, GUISUMOAbstractView& parent);
+
+    /**@brief Returns an own parameter window
+    *
+    * @param[in] app The application needed to build the parameter window
+    * @param[in] parent The parent window needed to build the parameter window
+    * @return The built parameter window
+    * @see GUIGlObject::getParameterWindow
+    */
+    GUIParameterTableWindow* getParameterWindow(GUIMainWindow& app, GUISUMOAbstractView& parent);
+
+    /// @brief Returns the boundary to which the view shall be centered in order to show the object
+    Boundary getCenteringBoundary() const;
+
+    /**@brief Draws the object
+    * @param[in] s The settings for the current view (may influence drawing)
+    * @see GUIGlObject::drawGL
+    */
+    void drawGL(const GUIVisualizationSettings& s) const;
+    /// @}
+
+    /// @name inherited from GNEAttributeCarrier
+    /// @{
+    /**@brief method for getting the Attribute of an XML key
+    * @param[in] key The attribute key
+    * @return string with the value associated to key
+    */
+    std::string getAttribute(SumoXMLAttr key) const;
+
+    /**@brief method for setting the attribute and letting the object perform additional changes
+    * @param[in] key The attribute key
+    * @param[in] value The new value
+    * @param[in] undoList The undoList on which to register changes
+    */
+    void setAttribute(SumoXMLAttr key, const std::string& value, GNEUndoList* undoList);
+
+    /**@brief method for checking if the key and their correspond attribute are valids
+    * @param[in] key The attribute key
+    * @param[in] value The value asociated to key key
+    * @return true if the value is valid, false in other case
+    */
+    bool isValid(SumoXMLAttr key, const std::string& value);
+    /// @}
+
+protected:
+    /// @brief lane in which this POI can be placed
+    GNELane* myLane;
+
+    /// @brief position over lane
+    double myPositionOverLane;
+
+private:
+    /// @brief set attribute after validation
+    void setAttribute(SumoXMLAttr key, const std::string& value);
+
+    /// @brief Invalidated copy constructor.
+    GNEPOILane(const GNEPOILane&) = delete;
+
+    /// @brief Invalidated assignment operator.
+    GNEPOILane& operator=(const GNEPOILane&) = delete;
+};
+
+
+#endif
+
+/****************************************************************************/
+
