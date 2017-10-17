f0779652be61ffdf3124749c30a0d589c96d07fc
Leonhard Luecken <leonhard.luecken@dlr.de>
2017-10-17 13:02:28 +0000

    adapted #3505 test (wrong commit order, sorry)
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26535 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/tests/complex/traci/pythonApi/bugs/ticket3505/runner.py
+++ sumo/tests/complex/traci/pythonApi/bugs/ticket3505/runner.py
@@ -26,11 +26,12 @@ sumoBinary = os.environ.get("SUMO_BINARY", os.path.join(
     os.path.dirname(sys.argv[0]), '..', '..', '..', 'bin', 'sumo'))
             
 ## LOAD
+PORT = traci.getFreeSocketPort()
 loadParams = ["-c", "sumo.sumocfg"]
 print("Starting...")
 print("loadParams: %s"%str(loadParams))
 v=traci.start([sumoBinary] + loadParams)
-print("Version%s"%v)
-traci.simulationStep()
+while traci.simulation.getCurrentTime() < 300*1000:
+    traci.simulationStep()
+print(traci.simulation.getCurrentTime())
 traci.close()
-sumoProcess.wait()
