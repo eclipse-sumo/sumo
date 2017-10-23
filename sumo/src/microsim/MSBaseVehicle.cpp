29d482f4659369df9a37bcb046f0ac67d92e3eab
Michael Behrisch <oss@behrisch.de>
2017-10-23 10:11:31 +0000

    fixing order of route addition and until offsets, refs #3526
    git-svn-id: file:///home/behr_mi/git/sumo_synched/trunk@26664 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/src/microsim/MSBaseVehicle.cpp
+++ sumo/src/microsim/MSBaseVehicle.cpp
@@ -471,17 +471,17 @@ MSBaseVehicle::saveState(OutputDevice& out) {
 
 void
 MSBaseVehicle::addStops(const bool ignoreStopErrors) {
-    const SUMOTime untilOffset = myParameter->repetitionOffset > 0 ? myParameter->repetitionsDone * myParameter->repetitionOffset : 0;
-    for (std::vector<SUMOVehicleParameter::Stop>::const_iterator i = myParameter->stops.begin(); i != myParameter->stops.end(); ++i) {
+    for (std::vector<SUMOVehicleParameter::Stop>::const_iterator i = myRoute->getStops().begin(); i != myRoute->getStops().end(); ++i) {
         std::string errorMsg;
-        if (!addStop(*i, errorMsg, untilOffset) && !ignoreStopErrors) {
+        if (!addStop(*i, errorMsg, myParameter->depart) && !ignoreStopErrors) {
             throw ProcessError(errorMsg);
         }
         if (errorMsg != "") {
             WRITE_WARNING(errorMsg);
         }
     }
-    for (std::vector<SUMOVehicleParameter::Stop>::const_iterator i = myRoute->getStops().begin(); i != myRoute->getStops().end(); ++i) {
+    const SUMOTime untilOffset = myParameter->repetitionOffset > 0 ? myParameter->repetitionsDone * myParameter->repetitionOffset : 0;
+    for (std::vector<SUMOVehicleParameter::Stop>::const_iterator i = myParameter->stops.begin(); i != myParameter->stops.end(); ++i) {
         std::string errorMsg;
         if (!addStop(*i, errorMsg, untilOffset) && !ignoreStopErrors) {
             throw ProcessError(errorMsg);
