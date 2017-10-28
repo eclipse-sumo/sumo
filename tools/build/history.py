5a92bc6938937baac1e5e17a5433684001222ca8
Michael Behrisch <oss@behrisch.de>
2017-10-28 23:19:48 +0200

    build history for git, refs #3546

--- tools/build/history.py
+++ tools/build/history.py
@@ -25,39 +25,35 @@ import sys
 import traceback
 
 optParser = optparse.OptionParser()
-optParser.add_option("-b", "--begin", type="int",
-                     default=16000, help="first revision to build")
-optParser.add_option("-e", "--end", type="int",
-                     help="last revision to build")
+optParser.add_option("-b", "--begin", default="ebef3bdbdf", help="first revision to build")
+optParser.add_option("-e", "--end", default="HEAD", help="last revision to build")
 optParser.add_option("-s", "--step", type="int",
                      default=1, help="increment")
 options, args = optParser.parse_args()
 
-LOCK = "history.lock"
+LOCK = "../history.lock"
 if os.path.exists(LOCK):
     sys.exit("History building is still locked!")
 open(LOCK, 'w').close()
 try:
-    if not options.end:
-        for line in subprocess.check_output(
-                'svn info http://svn.code.sf.net/p/sumo/code/trunk/sumo', shell=True).splitlines():
-            l = line.split()
-            if len(l) == 2 and l[0] == "Revision:":
-                options.end = int(l[1])
-    for rev in range(options.begin, options.end + 1, options.step):
-        if not os.path.exists('bin%s' % rev):
-            ret = subprocess.call(
-                'svn up --ignore-externals -r %s sumo' % rev, shell=True)
+    subprocess.call(["git", "checkout", "master"])
+    subprocess.call(["git", "pull"])
+    commits = {}
+    for line in subprocess.check_output(["git", "log", "%s..%s" % (options.begin, options.end)]).splitlines():
+        if line.startswith("commit "):
+            h = line.split()[1]
+            commits[h] = subprocess.check_output(["git", "describe", h])
+    for h, desc in commits.items():
+        if not os.path.exists('bin%s' % desc):
+            ret = subprocess.call(["git", "checkout", h])
             if ret != 0:
                 break
-            subprocess.call(
-                'cd sumo; make clean; make -j 16; cd ..', shell=True)
-            shutil.copytree('sumo/bin', 'bin%s' % rev,
+            subprocess.call('make clean; make -j 16', shell=True)
+            shutil.copytree('bin', '../bin%s' % desc,
                             ignore=shutil.ignore_patterns('Makefile*', '*.bat', '*.jar'))
-            subprocess.call('strip -R .note.gnu.build-id bin%s/*' %
-                            rev, shell=True)
+            subprocess.call('strip -R .note.gnu.build-id bin%s/*' % desc, shell=True)
             subprocess.call(
-                "sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' bin%s/*" % (rev, rev), shell=True)
+                "sed -i 's/dev-SVN-r%s/dev-SVN-r00000/' bin%s/*" % (desc, desc), shell=True)
     for line in subprocess.check_output('fdupes -1 -q bin*', shell=True).splitlines():
         dups = line.split()
         for d in dups[1:]:
