eclipse-sumo/sumo

commit 8bbe9abed96de4382e053b7518d498d748a56d74

fix #3634

src/microsim/MSVehicle.cpp
@@ -362,7 +362,9 @@ MSVehicle::Influencer::influenceChangeDecision(const SUMOTime currentTime, const
     if ((state & LCA_WANTS_LANECHANGE_OR_STAY) != 0) {
         // flags for the current reason
         LaneChangeMode mode = LC_NEVER;
-        if ((state & LCA_STRATEGIC) != 0) {
+        if ((state & LCA_TRACI) != 0) {
+            mode = LC_ALWAYS;
+        } else if ((state & LCA_STRATEGIC) != 0) {
             mode = myStrategicLC;
         } else if ((state & LCA_COOPERATIVE) != 0) {
             mode = myCooperativeLC;
@@ -372,8 +374,6 @@ MSVehicle::Influencer::influenceChangeDecision(const SUMOTime currentTime, const
             mode = myRightDriveLC;
         } else if ((state & LCA_SUBLANE) != 0) {
             mode = mySublaneLC;
-        } else if ((state & LCA_TRACI) != 0) {
-            mode = LC_NEVER;
         } else {
             WRITE_WARNING("Lane change model did not provide a reason for changing (state=" + toString(state) + ", time=" + time2string(currentTime) + "\n");
         }

src/microsim/MSLaneChangerSublane.cpp
@@ -300,6 +300,12 @@ MSLaneChangerSublane::startChangeSublane(MSVehicle* vehicle, ChangerIt& from, do
                 << "\n";
     vehicle->setAngle(laneAngle + changeAngle,
             vehicle->getLaneChangeModel().sublaneChangeCompleted(latDist));
+
+    // check if a traci manoeuvre must continue
+    if ((vehicle->getLaneChangeModel().getOwnState() & LCA_TRACI) != 0) {
+        if (vehicle->getLaneChangeModel().debugVehicle()) std::cout << SIMTIME << " continue TraCI-manoeuvre remainingLatDist=" << vehicle->getLaneChangeModel().getManeuverDist() - latDist << "\n";
+        vehicle->getInfluencer().setSublaneChange(vehicle->getLaneChangeModel().getManeuverDist() - latDist);
+    }
     return changedToNewLane;
 }
 
@@ -465,13 +471,13 @@ MSLaneChangerSublane::checkChangeSublane(
     // ensure that a continuous lane change manoeuvre can be completed
     // before the next turning movement
 
-    const int oldstate = state;
 #ifndef NO_TRACI
     // let TraCI influence the wish to change lanes and the security to take
+    const int oldstate = state;
     state = vehicle->influenceChangeDecision(state);
-    //if (vehicle->getID() == "150_2_36000000") {
-    //    std::cout << STEPS2TIME(MSNet::getInstance()->getCurrentTimeStep()) << " veh=" << vehicle->getID() << " oldstate=" << oldstate << " newstate=" << state << "\n";
-    //}
+    if (DEBUG_COND && state != oldstate) {
+        std::cout << SIMTIME << " veh=" << vehicle->getID() << " stateAfterTraCI=" << toString((LaneChangeAction)state) << " original=" << toString((LaneChangeAction)oldstate) << "\n";
+    }
 #endif
     vehicle->getLaneChangeModel().saveState(laneOffset, oldstate, state);
     return state;

src/microsim/lcmodels/MSAbstractLaneChangeModel.cpp
@@ -112,10 +112,6 @@ MSAbstractLaneChangeModel::setOwnState(const int state) {
     myPreviousState2 = myPreviousState;
     myOwnState = state;
     myPreviousState = state; // myOwnState is modified in prepareStep so we make a backup
-    // reset lateral influence after step is completed
-    if (myVehicle.hasInfluencer()) {
-        myVehicle.getInfluencer().setSublaneChange(0);
-    }
 }
 
 void

src/microsim/lcmodels/MSLCM_SL2015.cpp
@@ -2321,10 +2321,16 @@ MSLCM_SL2015::checkStrategicChange(int ret,
     if (gDebugFlag2) {
         std::cout << SIMTIME << " veh=" << myVehicle.getID() << " ret=" << ret;
     }
-    ret = myVehicle.influenceChangeDecision(ret);
-    if ((ret & lcaCounter) != 0) {
-        // we are not interested in traci requests for the opposite direction here
-        ret &= ~(LCA_TRACI | lcaCounter | LCA_URGENT);
+    int retTraCI = myVehicle.influenceChangeDecision(ret);
+    if ((retTraCI & LCA_TRACI) != 0) {
+        if ((retTraCI & LCA_STAY) != 0) {
+            ret = retTraCI;
+            latDist = 0;
+        } else if (((retTraCI & LCA_RIGHT) != 0 && laneOffset < 0)
+                || ((retTraCI & LCA_LEFT) != 0 && laneOffset > 0)) {
+            ret = retTraCI;
+            latDist = latLaneDist;
+        }
     }
     if (gDebugFlag2) {
         std::cout << " reqAfterInfluence=" << ret << " ret=" << ret << "\n";
@@ -2477,6 +2483,8 @@ MSLCM_SL2015::keepLatGap(int state,
     if (myVehicle.hasInfluencer() && myVehicle.getInfluencer().getLatDist() != 0) {
         // @note: the influence is reset in MSAbstractLaneChangeModel::setOwnState at the end of the lane-changing code for this vehicle
         latDist = myVehicle.getInfluencer().getLatDist();
+        state |= LCA_TRACI;
+        if (gDebugFlag2) std::cout << "     traci influenced latDist=" << latDist << "\n";
     }
     // if we cannot move in the desired direction, consider the maneuver blocked anyway
     bool nonSublaneChange = (state & (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT)) != 0;
@@ -2495,14 +2503,14 @@ MSLCM_SL2015::keepLatGap(int state,
             blocked = LCA_OVERLAPPING | LCA_BLOCKED_RIGHT;
         }
     }
-    // if we move, even though we wish to stay, update the change reason
+    // if we move, even though we wish to stay, update the change reason (except for TraCI)
     if (fabs(latDist) > NUMERICAL_EPS * myVehicle.getActionStepLengthSecs() && oldLatDist == 0) {
-        state &= ~(LCA_CHANGE_REASONS | LCA_STAY);
+        state &= (~(LCA_CHANGE_REASONS | LCA_STAY) | LCA_TRACI);
     }
     // update blocked status
     if (fabs(latDist - oldLatDist) > NUMERICAL_EPS * myVehicle.getActionStepLengthSecs()) {
         if (gDebugFlag2) {
-            std::cout << "     latDistUpdated=" << latDist << "oldLatDist=" << oldLatDist << "\n";
+            std::cout << "     latDistUpdated=" << latDist << " oldLatDist=" << oldLatDist << "\n";
         }
         blocked = checkBlocking(neighLane, latDist, targetDistLat, laneOffset, leaders, followers, blockers, neighLeaders, neighFollowers, neighBlockers, 0, 0, nonSublaneChange);
     }

src/utils/xml/SUMOXMLDefinitions.h
@@ -1064,7 +1064,7 @@ enum LaneChangeAction {
     /// @brief blocked in all directions
     LCA_BLOCKED = LCA_BLOCKED_LEFT | LCA_BLOCKED_RIGHT | LCA_INSUFFICIENT_SPACE,
     /// @brief reasons of lane change
-    LCA_CHANGE_REASONS = (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT | LCA_SUBLANE)
+    LCA_CHANGE_REASONS = (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT | LCA_SUBLANE | LCA_TRACI)
                          // LCA_BLOCKED_BY_CURRENT_LEADER = 1 << 28
                          // LCA_BLOCKED_BY_CURRENT_FOLLOWER = 1 << 29
                          /// @}

tests/complex/traci/pythonApi/bugs/ticket3634/runner.py
@@ -43,5 +43,6 @@ traci.simulationStep()
 vehID = "v0"
 traci.vehicle.setLaneChangeMode(vehID, 0b0100000000)
 traci.vehicle.changeLane(vehID, 1, 0)
-traci.simulationStep()
+for i in range(20):
+    traci.simulationStep()
 traci.close()

commit 403b1df1d61009aafb5be0af7d2e62902bce0fdd

fixed regression from [8bbe9abe] refs #3634

src/microsim/MSVehicle.cpp
@@ -363,8 +363,9 @@ MSVehicle::Influencer::influenceChangeDecision(const SUMOTime currentTime, const
     if ((state & LCA_WANTS_LANECHANGE_OR_STAY) != 0) {
         // flags for the current reason
         LaneChangeMode mode = LC_NEVER;
-        if ((state & LCA_TRACI) != 0) {
-            mode = LC_ALWAYS;
+        if ((state & LCA_TRACI) != 0 && myLatDist != 0) {
+            // continue sublane change manoeuvre
+            return state;
         } else if ((state & LCA_STRATEGIC) != 0) {
             mode = myStrategicLC;
         } else if ((state & LCA_COOPERATIVE) != 0) {
@@ -375,6 +376,8 @@ MSVehicle::Influencer::influenceChangeDecision(const SUMOTime currentTime, const
             mode = myRightDriveLC;
         } else if ((state & LCA_SUBLANE) != 0) {
             mode = mySublaneLC;
+        } else if ((state & LCA_TRACI) != 0) {
+            mode = LC_NEVER;
         } else {
             WRITE_WARNING("Lane change model did not provide a reason for changing (state=" + toString(state) + ", time=" + time2string(currentTime) + "\n");
         }