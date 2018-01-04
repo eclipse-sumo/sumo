45b239f71c417434a795271108cd16100b638b07
Michael Behrisch <oss@behrisch.de>
2018-01-04 21:53:06 +0100

    Happy New Year, refs #2

--- src/utils/options/OptionsCont.cpp
+++ src/utils/options/OptionsCont.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************/
 // Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-// Copyright (C) 2001-2017 German Aerospace Center (DLR) and others.
+// Copyright (C) 2001-2018 German Aerospace Center (DLR) and others.
 // This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v2.0
 // which accompanies this distribution, and is available at
@@ -65,7 +65,7 @@ OptionsCont::getOptions() {
 
 OptionsCont::OptionsCont()
     : myAddresses(), myValues(), myDeprecatedSynonymes(), myHaveInformedAboutDeprecatedDivider(false) {
-    myCopyrightNotices.push_back("Copyright (C) 2001-2017 German Aerospace Center (DLR) and others; http://sumo.dlr.de");
+    myCopyrightNotices.push_back("Copyright (C) 2001-2018 German Aerospace Center (DLR) and others; http://sumo.dlr.de");
 }
 
 
