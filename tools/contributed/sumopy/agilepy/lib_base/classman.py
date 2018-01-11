7de0683501f247665bbea78645c2775249cd7dbd
Michael Behrisch <oss@behrisch.de>
2018-01-11 12:40:22 +0100

    adding SPDX to license header, refs #3476

--- tools/contributed/sumopy/agilepy/lib_base/classman.py
+++ tools/contributed/sumopy/agilepy/lib_base/classman.py
@@ -6,6 +6,7 @@
 # are made available under the terms of the Eclipse Public License v2.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
 
 # @file    classman.py
 # @author  Joerg Schweizer
--- tools/import/opendrive/signal_POIs_from_xodr.py
+++ tools/import/opendrive/signal_POIs_from_xodr.py
@@ -6,6 +6,7 @@
 # are made available under the terms of the Eclipse Public License v2.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
 
 # @file    signal_POIs_from_xodr.py
 # @author  Gerald Richter; gerald.richter@ait.ac.at
