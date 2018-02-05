d3eb7ee3dc2fd3c29d879418dca457964464de14
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-02-05 16:29:25 +0100

    undo teporal solution for bug #2780. Refs #3798

--- src/netedit/GNEViewNet.cpp
+++ src/netedit/GNEViewNet.cpp
@@ -592,9 +592,6 @@ GNEViewNet::doPaintGL(int mode, const Boundary& bound) {
             glVertex2d(size, 0);
             glEnd();
             glPopMatrix();
-
-            // Reset textures due bug #2780. This solution is only provisional, and must be updated in the future
-            GUITextureSubSys::resetTextures();
         }
     }
 
