ba417dabcf9d99278d4447f7915c3b46cbfc5426
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-02-14 13:56:15 +0100

    Updated netedit test shapeElements/poilane/create. Refs #3045

--- tests/netedit/shapeElements/poilane/create/test.sikuli/test.py
+++ tests/netedit/shapeElements/poilane/create/test.sikuli/test.py
@@ -31,153 +31,183 @@ netedit.shapeMode()
 # go to shape mode
 netedit.changeShape("poiLane")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 140, 215)
 
 # change offset (invalid)
-netedit.modifyShapeDefaultValue(1, "dummyOffset")
+netedit.modifyShapeDefaultValue(2, "dummyOffset")
 
-# try to create POILaneLane
+# try to create POILane
 netedit.leftClick(match, 160, 215)
 
 # change offset (empty)
-netedit.modifyShapeDefaultValue(1, "")
+netedit.modifyShapeDefaultValue(2, "")
 
-# try to create POILaneLane
+# try to create POILane
 netedit.leftClick(match, 160, 215)
 
 # change offset (valid, negative)
-netedit.modifyShapeDefaultValue(1, "-4")
+netedit.modifyShapeDefaultValue(2, "-4")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 160, 215)
 
 # change offset (valid, positive)
-netedit.modifyShapeDefaultValue(1, "6")
+netedit.modifyShapeDefaultValue(2, "6")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 180, 215)
 
 # change color using dialog
-netedit.changeColorUsingDialog(2, 5)
+netedit.changeColorUsingDialog(3, 5)
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 200, 215)
 
 # change color manually (invalid)
-netedit.modifyShapeDefaultValue(3, "Vlue")
+netedit.modifyShapeDefaultValue(4, "Vlue")
 
-# try to create POILaneLane
+# try to create POILane
 netedit.leftClick(match, 220, 215)
 
 # change color manually (valid)
-netedit.modifyShapeDefaultValue(3, "blue")
+netedit.modifyShapeDefaultValue(4, "blue")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 220, 215)
 
 # change type
-netedit.modifyShapeDefaultValue(4, "poi_test")
+netedit.modifyShapeDefaultValue(5, "poi_test")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 240, 215)
 
 # change layer (invalid)
-netedit.modifyShapeDefaultValue(5, "dummyLayer")
+netedit.modifyShapeDefaultValue(6, "dummyLayer")
 
-# try to create POILaneLane
+# try to create POILane
+netedit.leftClick(match, 260, 215)
+
+# change layer (valid, negative)
+netedit.modifyShapeDefaultValue(6, "-2")
+
+# create POILane
 netedit.leftClick(match, 260, 215)
 
 # change layer (valid)
-netedit.modifyShapeDefaultValue(5, "2")
+netedit.modifyShapeDefaultValue(6, "3")
 
-# create POILaneLane
-netedit.leftClick(match, 260, 215)
+# create POILane
+netedit.leftClick(match, 280, 215)
+
+# change width (invalid, dummy)
+netedit.modifyShapeDefaultValue(7, "dummyWidth")
+
+# try to create POILane
+netedit.leftClick(match, 300, 215)
 
 # change width (invalid, negative)
-netedit.modifyShapeDefaultValue(6, "-2")
+netedit.modifyShapeDefaultValue(7, "-2")
 
-# try to create POILaneLane
-netedit.leftClick(match, 280, 215)
+# try to create POILane
+netedit.leftClick(match, 300, 215)
 
 # change width (valid)
-netedit.modifyShapeDefaultValue(6, "2")
-
-# create POILaneLane
-netedit.leftClick(match, 280, 215)
-
-# change height (invalid, negative)
-netedit.modifyShapeDefaultValue(7, "-3")
-
-# try to create POILaneLane
-netedit.leftClick(match, 300, 215)
-
-# change height (valid)
 netedit.modifyShapeDefaultValue(7, "2")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 300, 215)
 
+# change height (invalid, dummy)
+netedit.modifyShapeDefaultValue(8, "dummyHeight")
+
+# try to create POILane
+netedit.leftClick(match, 320, 215)
+
+# change height (invalid, negative)
+netedit.modifyShapeDefaultValue(8, "-3")
+
+# try to create POILane
+netedit.leftClick(match, 320, 215)
+
+# change height (valid)
+netedit.modifyShapeDefaultValue(8, "2")
+
+# create POILane
+netedit.leftClick(match, 320, 215)
+
 # change color to white (To show image)
-netedit.modifyShapeDefaultValue(3, "white")
+netedit.modifyShapeDefaultValue(4, "white")
 
 # change imgfile (invalid)
-netedit.modifyShapeDefaultValue(8, "%%$%$&$%$%$")
+netedit.modifyShapeDefaultValue(9, "%%$%$&$%$%$")
 
-# try to create POILaneLane
-netedit.leftClick(match, 320, 215)
-
-# change imgfile (non exist)
-netedit.modifyShapeDefaultValue(8, "paris.ico")
-
-# try to create POILaneLane
-netedit.leftClick(match, 320, 215)
-
-# change imgfile (valid)
-netedit.modifyShapeDefaultValue(8, "berlin_icon.ico")
-
-# create POILaneLane
-netedit.leftClick(match, 320, 215)
-
-# change save relative path
-netedit.modifyShapeDefaultBoolValue(9)
-
-# create POILaneLane
+# try to create POILane
 netedit.leftClick(match, 340, 215)
 
+# change imgfile (non exist)
+netedit.modifyShapeDefaultValue(9, "paris.ico")
+
+# try to create POILane
+netedit.leftClick(match, 340, 215)
+
+# change imgfile (valid)
+netedit.modifyShapeDefaultValue(9, "berlin_icon.ico")
+
+# create POILane
+netedit.leftClick(match, 340, 215)
+
+# change save relative path
+netedit.modifyShapeDefaultBoolValue(10)
+
+# create POILane
+netedit.leftClick(match, 360, 215)
+
 # change angle (invalid)
-netedit.modifyShapeDefaultValue(10, "dummyAngle")
+netedit.modifyShapeDefaultValue(11, "dummyAngle")
 
-# try to create POILaneLane
-netedit.leftClick(match, 360, 215)
+# try to create POILane
+netedit.leftClick(match, 380, 215)
 
-# change angle (valid, but > 360)
-netedit.modifyShapeDefaultValue(10, "500")
+# change angle (invalid, dummy)
+netedit.modifyShapeDefaultValue(11, "dummy")
 
-# create POILaneLane
-netedit.leftClick(match, 360, 215)
+# try to create POILane
+netedit.leftClick(match, 380, 215)
 
-# change angle (valid, < 0)
-netedit.modifyShapeDefaultValue(10, "-27")
+# change angle (invalid, empty)
+netedit.modifyShapeDefaultValue(11, "")
 
-# create POILaneLane
+# try to create POILane
+netedit.leftClick(match, 380, 215)
+
+# change angle (valid, > 360)
+netedit.modifyShapeDefaultValue(11, "500")
+
+# create POILane
 netedit.leftClick(match, 380, 215)
 
 # change angle (valid, < 0)
-netedit.modifyShapeDefaultValue(10, "45")
+netedit.modifyShapeDefaultValue(11, "-27")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 400, 215)
 
-# change block
-netedit.modifyShapeDefaultBoolValue(12)
+# change angle (valid, < 0)
+netedit.modifyShapeDefaultValue(11, "45")
 
-# create POILaneLane
+# create POILane
 netedit.leftClick(match, 420, 215)
 
+# change block
+netedit.modifyShapeDefaultBoolValue(13)
+
+# create POILane
+netedit.leftClick(match, 440, 215)
+
 # Check undo redo
-netedit.undo(match, 12)
-netedit.redo(match, 12)
+netedit.undo(match, 13)
+netedit.redo(match, 13)
 
 # save shapes
 netedit.saveShapes()
