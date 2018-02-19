eclipse-sumo/sumo

commit b682832bfec79e5c94dde2fb411055af8488e03a

Implemented getAssumedDecelForLaneChangeDuration(), modified estimateLCDuration(), introduced LC_RESOLUTION_SPEED_LAT and LC_ASSUMED_DECEL. Fixes #3771.

src/microsim/lcmodels/MSLCM_LC2013.h
@@ -132,6 +132,10 @@ public:
     /// @brief decides the next lateral speed (for continuous lane changing)
     double computeSpeedLat(double latDist, double& maneuverDist);
 
+    /// @brief Returns a deceleration value which is used for the estimation of the duration of a lane change.
+    /// @note  Effective only for continuous lane-changing when using attributes myMaxSpeedLatFactor and myMaxSpeedLatStanding. See #3771
+    double getAssumedDecelForLaneChangeDuration() const;
+
 protected:
 
     /// @brief helper function for doing the actual work

src/utils/xml/SUMOXMLDefinitions.h
@@ -1051,6 +1051,8 @@ enum LaneChangeAction {
     LCA_INSUFFICIENT_SPACE = 1 << 14,
     /// @brief used by the sublane model
     LCA_SUBLANE = 1 << 15,
+    /// @brief Vehicle is too slow to guarantee success of lane change (used for continuous lane changing in case that maxSpeedLatStanding==0)
+    LCA_INSUFFICIENT_SPEED = 1 << 16,
     /// @brief lane can change
     LCA_WANTS_LANECHANGE = LCA_LEFT | LCA_RIGHT,
     /// @brief lane can change or stay
@@ -1064,7 +1066,7 @@ enum LaneChangeAction {
     /// @brief blocker by follower
     LCA_BLOCKED_BY_FOLLOWER = LCA_BLOCKED_BY_LEFT_FOLLOWER | LCA_BLOCKED_BY_RIGHT_FOLLOWER,
     /// @brief blocked in all directions
-    LCA_BLOCKED = LCA_BLOCKED_LEFT | LCA_BLOCKED_RIGHT | LCA_INSUFFICIENT_SPACE,
+    LCA_BLOCKED = LCA_BLOCKED_LEFT | LCA_BLOCKED_RIGHT | LCA_INSUFFICIENT_SPACE | LCA_INSUFFICIENT_SPEED,
     /// @brief reasons of lane change
     LCA_CHANGE_REASONS = (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT | LCA_SUBLANE | LCA_TRACI)
                          // LCA_BLOCKED_BY_CURRENT_LEADER = 1 << 28

src/microsim/MSLaneChanger.cpp
@@ -696,7 +696,6 @@ MSLaneChanger::checkChange(
 
     MSVehicle* vehicle = veh(myCandi);
 
-    // Debug (Leo)
 #ifdef DEBUG_CHECK_CHANGE
     if (DEBUG_COND) {
         std::cout
@@ -704,7 +703,6 @@ MSLaneChanger::checkChange(
                 << std::endl;
     }
 #endif
-
 
     int blocked = 0;
     int blockedByLeader = (laneOffset == -1 ? LCA_BLOCKED_BY_RIGHT_LEADER : LCA_BLOCKED_BY_LEFT_LEADER);
@@ -726,7 +724,6 @@ MSLaneChanger::checkChange(
     if (neighLead.first != 0 && neighLead.second < 0) {
         blocked |= (blockedByLeader | LCA_OVERLAPPING);
 
-        // Debug (Leo)
 #ifdef DEBUG_CHECK_CHANGE
         if (DEBUG_COND) {
             std::cout << SIMTIME
@@ -841,75 +838,112 @@ MSLaneChanger::checkChange(
     }
 
     if ((state & LCA_BLOCKED) == 0 && (state & LCA_WANTS_LANECHANGE) != 0 && MSGlobals::gLaneChangeDuration > DELTA_T) {
-        // ensure that a continuous lane change manoeuvre can be completed
-        // before the next turning movement
-        double seen = myCandi->lane->getLength() - vehicle->getPositionOnLane();
-        const double decel = vehicle->getCarFollowModel().getMaxDecel() * STEPS2TIME(MSGlobals::gLaneChangeDuration);
-        const double avgSpeed = 0.5 * (
-                                    MAX2(0., vehicle->getSpeed() - ACCEL2SPEED(vehicle->getCarFollowModel().getMaxDecel())) +
-                                    MAX2(0., vehicle->getSpeed() - decel));
-        const double space2change = avgSpeed * STEPS2TIME(MSGlobals::gLaneChangeDuration);
-        // for finding turns it doesn't matter whether we look along the current lane or the target lane
-        const std::vector<MSLane*>& bestLaneConts = vehicle->getBestLanesContinuation();
-        int view = 1;
-        MSLane* nextLane = vehicle->getLane();
-        MSLinkCont::const_iterator link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
-        while (!nextLane->isLinkEnd(link) && seen <= space2change) {
-            if ((*link)->getDirection() == LINKDIR_LEFT || (*link)->getDirection() == LINKDIR_RIGHT
-                    // the lanes after an internal junction are on different
-                    // edges and do not allow lane-changing
-                    || (nextLane->getEdge().isInternal() && (*link)->getViaLaneOrLane()->getEdge().isInternal())
-               ) {
-                state |= LCA_INSUFFICIENT_SPACE;
-                break;
-            }
-            if ((*link)->getViaLane() == 0) {
-                view++;
-            }
-            nextLane = (*link)->getViaLaneOrLane();
-            seen += nextLane->getLength();
-            // get the next link used
-            link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
-        }
-        if (nextLane->isLinkEnd(link) && seen < space2change) {
+        // Ensure that a continuous lane change manoeuvre can be completed before the next turning movement.
+        // Assume lateral position == 0. (If this should change in the future add + laneOffset*vehicle->getLateralPositionOnLane() to distToNeighLane)
+        const double distToNeighLane = 0.5*(vehicle->getLane()->getWidth() + targetLane->getWidth());
+        // Extrapolate the LC duration if operating with speed dependent lateral speed.
+        const MSAbstractLaneChangeModel& lcm = vehicle->getLaneChangeModel();
+        const double assumedDecel = lcm.getAssumedDecelForLaneChangeDuration();
+        const double estimatedLCDuration = lcm.estimateLCDuration(vehicle->getSpeed(), distToNeighLane, assumedDecel);
+        if (estimatedLCDuration==-1) {
+            // Can't guarantee that LC will succeed if vehicle is braking -> assert(lcm.myMaxSpeedLatStanding==0)
 #ifdef DEBUG_CHECK_CHANGE
-            if (DEBUG_COND) {
-                std::cout << SIMTIME << " checkChange insufficientSpace: seen=" << seen << " space2change=" << space2change << "\n";
+            if DEBUG_COND {
+                std::cout << SIMTIME << " checkChange() too slow to guarantee completion of continuous lane change."
+                        <<"\nestimatedLCDuration="<<estimatedLCDuration
+                        <<"\ndistToNeighLane="<<distToNeighLane
+                        << std::endl;
             }
 #endif
-            state |= LCA_INSUFFICIENT_SPACE;
-        }
+            state |= LCA_INSUFFICIENT_SPEED;
+        } else {
+            // Compute covered distance, when braking for the whole lc duration
+            const double decel = vehicle->getCarFollowModel().getMaxDecel() * estimatedLCDuration;
+            const double avgSpeed = 0.5 * (
+                    MAX2(0., vehicle->getSpeed() - ACCEL2SPEED(vehicle->getCarFollowModel().getMaxDecel())) +
+                    MAX2(0., vehicle->getSpeed() - decel));
+            // Distance required for lane change.
+            const double space2change = avgSpeed * estimatedLCDuration;
+            // Available distance for LC maneuver (distance till next turn)
+            double seen = myCandi->lane->getLength() - vehicle->getPositionOnLane();
+#ifdef DEBUG_CHECK_CHANGE
+            if DEBUG_COND {
+                std::cout << SIMTIME << " checkChange() checking continuous lane change..."
+                        << "\ndistToNeighLane=" << distToNeighLane
+                        << " estimatedLCDuration=" << estimatedLCDuration
+                        << " space2change=" << space2change
+                        << " avgSpeed=" << avgSpeed
+                        << std::endl;
+            }
+#endif
 
-        if ((state & LCA_BLOCKED) == 0) {
-            // check for dangerous leaders in case the target lane changes laterally between
-            // now and the lane-changing midpoint
-            const double speed = vehicle->getSpeed();
-            seen = myCandi->lane->getLength() - vehicle->getPositionOnLane();
-            nextLane = vehicle->getLane();
-            view = 1;
-            const double dist = vehicle->getCarFollowModel().brakeGap(speed) + vehicle->getVehicleType().getMinGap();
+            // for finding turns it doesn't matter whether we look along the current lane or the target lane
+            const std::vector<MSLane*>& bestLaneConts = vehicle->getBestLanesContinuation();
+            int view = 1;
+            MSLane* nextLane = vehicle->getLane();
             MSLinkCont::const_iterator link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
-            while (!nextLane->isLinkEnd(link) && seen <= space2change && seen <= dist) {
-                nextLane = (*link)->getViaLaneOrLane();
-                MSLane* targetLane = nextLane->getParallelLane(laneOffset);
-                if (targetLane == 0) {
+            while (!nextLane->isLinkEnd(link) && seen <= space2change) {
+                if ((*link)->getDirection() == LINKDIR_LEFT || (*link)->getDirection() == LINKDIR_RIGHT
+                        // the lanes after an internal junction are on different
+                        // edges and do not allow lane-changing
+                        || (nextLane->getEdge().isInternal() && (*link)->getViaLaneOrLane()->getEdge().isInternal())
+                ) {
                     state |= LCA_INSUFFICIENT_SPACE;
                     break;
-                } else {
-                    std::pair<MSVehicle* const, double> neighLead2 = targetLane->getLeader(vehicle, -seen, std::vector<MSLane*>());
-                    if (neighLead2.first != 0 && neighLead2.first != neighLead.first
-                            && (neighLead2.second < vehicle->getCarFollowModel().getSecureGap(
-                                    vehicle->getSpeed(), neighLead2.first->getSpeed(), neighLead2.first->getCarFollowModel().getMaxDecel()))) {
-                        state |= blockedByLeader;
-                        break;
-                    }
                 }
                 if ((*link)->getViaLane() == 0) {
                     view++;
                 }
+                nextLane = (*link)->getViaLaneOrLane();
                 seen += nextLane->getLength();
                 // get the next link used
                 link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
+            }
+#ifdef DEBUG_CHECK_CHANGE
+            if (DEBUG_COND) {
+                std::cout << " available distance=" << seen << std::endl;
+            }
+#endif
+            if (nextLane->isLinkEnd(link) && seen < space2change) {
+#ifdef DEBUG_CHECK_CHANGE
+                if (DEBUG_COND) {
+                    std::cout << SIMTIME << " checkChange insufficientSpace: seen=" << seen << " space2change=" << space2change << "\n";
+                }
+#endif
+                state |= LCA_INSUFFICIENT_SPACE;
+            }
+
+            if ((state & LCA_BLOCKED) == 0) {
+                // check for dangerous leaders in case the target lane changes laterally between
+                // now and the lane-changing midpoint
+                const double speed = vehicle->getSpeed();
+                seen = myCandi->lane->getLength() - vehicle->getPositionOnLane();
+                nextLane = vehicle->getLane();
+                view = 1;
+                const double dist = vehicle->getCarFollowModel().brakeGap(speed) + vehicle->getVehicleType().getMinGap();
+                MSLinkCont::const_iterator link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
+                while (!nextLane->isLinkEnd(link) && seen <= space2change && seen <= dist) {
+                    nextLane = (*link)->getViaLaneOrLane();
+                    MSLane* targetLane = nextLane->getParallelLane(laneOffset);
+                    if (targetLane == 0) {
+                        state |= LCA_INSUFFICIENT_SPACE;
+                        break;
+                    } else {
+                        std::pair<MSVehicle* const, double> neighLead2 = targetLane->getLeader(vehicle, -seen, std::vector<MSLane*>());
+                        if (neighLead2.first != 0 && neighLead2.first != neighLead.first
+                                && (neighLead2.second < vehicle->getCarFollowModel().getSecureGap(
+                                        vehicle->getSpeed(), neighLead2.first->getSpeed(), neighLead2.first->getCarFollowModel().getMaxDecel()))) {
+                            state |= blockedByLeader;
+                            break;
+                        }
+                    }
+                    if ((*link)->getViaLane() == 0) {
+                        view++;
+                    }
+                    seen += nextLane->getLength();
+                    // get the next link used
+                    link = MSLane::succLinkSec(*vehicle, view, *nextLane, bestLaneConts);
+                }
             }
         }
     }

src/microsim/lcmodels/MSAbstractLaneChangeModel.cpp
@@ -263,10 +263,16 @@ MSAbstractLaneChangeModel::computeSpeedLat(double /*latDist*/, double& maneuverD
 }
 
 
+double
+MSAbstractLaneChangeModel::getAssumedDecelForLaneChangeDuration() const {
+    throw ProcessError("Method getAssumedDecelForLaneChangeDuration() not implemented by model " + toString(myModel));
+}
+
+
 bool
 MSAbstractLaneChangeModel::updateCompletion() {
     const bool pastBefore = pastMidpoint();
-    // maneuverDist is not updated in the context of continous lane changing but represents the full LC distance
+    // maneuverDist is not updated in the context of continuous lane changing but represents the full LC distance
     double maneuverDist = getManeuverDist();
     mySpeedLat = computeSpeedLat(0, maneuverDist);
     myLaneChangeCompletion += (SPEED2DIST(mySpeedLat) / myManeuverDist);
@@ -525,9 +531,24 @@ MSAbstractLaneChangeModel::getAngleOffset() const {
 
 double
 MSAbstractLaneChangeModel::estimateLCDuration(const double speed, const double remainingManeuverDist, const double decel) const {
+
+    const SUMOVTypeParameter::SubParams& lcParams=myVehicle.getVehicleType().getParameter().getLCParams();
+    if (lcParams.find(SUMO_ATTR_LCA_MAXSPEEDLATSTANDING) == lcParams.end() && lcParams.find(SUMO_ATTR_LCA_MAXSPEEDLATFACTOR) == lcParams.end()) {
+        if (!myVehicle.getVehicleType().wasSet(VTYPEPARS_MAXSPEED_LAT_SET)){
+            // no dependency of lateral speed on longitudinal speed. (Only called prior to LC initialization to determine whether it could be completed)
+            return STEPS2TIME(MSGlobals::gLaneChangeDuration);
+        } else {
+            return remainingManeuverDist/myVehicle.getVehicleType().getMaxSpeedLat();
+        }
+    }
+
+    if(remainingManeuverDist==0){
+        return 0;
+    }
+
     // Check argument assumptions
     assert(speed>=0);
-    assert(remainingManeuverDist>0);
+    assert(remainingManeuverDist>=0);
     assert(decel>0);
     assert(myVehicle.getVehicleType().getMaxSpeedLat()>0);
     assert(myMaxSpeedLatStanding <= myVehicle.getVehicleType().getMaxSpeedLat());
@@ -539,7 +560,7 @@ MSAbstractLaneChangeModel::estimateLCDuration(const double speed, const double r
     const double b=decel;
     const double wmin=myMaxSpeedLatStanding;
     const double f=myMaxSpeedLatFactor;
-    const double wmax=myVehicle.getVehicleType().getMaxSpeedLat();
+    const double wmax= myVehicle.getVehicleType().getMaxSpeedLat();
 
     /* Here's the approach for the calculation of the required time for the LC:
      * To obtain the maximal LC-duration, for v(t) we assume that v(t)=max(0, v0-b*t),
@@ -608,7 +629,17 @@ MSAbstractLaneChangeModel::estimateLCDuration(const double speed, const double r
 
 SUMOTime
 MSAbstractLaneChangeModel::remainingTime() const {
-    return (SUMOTime)((1. - myLaneChangeCompletion) * MSGlobals::gLaneChangeDuration);
+    assert(isChangingLanes()); // Only to be called during ongoing lane change
+    const SUMOVTypeParameter::SubParams& lcParams=myVehicle.getVehicleType().getParameter().getLCParams();
+    if (lcParams.find(SUMO_ATTR_LCA_MAXSPEEDLATSTANDING) == lcParams.end() && lcParams.find(SUMO_ATTR_LCA_MAXSPEEDLATFACTOR) == lcParams.end()) {
+        if (myVehicle.getVehicleType().wasSet(VTYPEPARS_MAXSPEED_LAT_SET)) {
+            return TIME2STEPS((1. - myLaneChangeCompletion) * myManeuverDist/myVehicle.getVehicleType().getMaxSpeedLat());
+        } else {
+            return (SUMOTime)((1. - myLaneChangeCompletion) * MSGlobals::gLaneChangeDuration);
+        }
+    }
+    // Using maxSpeedLat(Factor/Standing)
+    return TIME2STEPS(estimateLCDuration(myVehicle.getSpeed(), fabs(myManeuverDist*(1-myLaneChangeCompletion)), myVehicle.getCarFollowModel().getMaxDecel()));
 }
 
 

src/microsim/lcmodels/MSAbstractLaneChangeModel.h
@@ -389,13 +389,20 @@ public:
     /// @param[in] remainingManeuverDist dist which is still to be covered until LC is completed
     /// @param[in] decel Maximal assumed deceleration rate applied during the LC.
     /// @return maximal LC duration (or -1) if it is possible that it can't be completed.
-    /// @note For the calculation it is assumed that the vehicle starts breaking with decel (>=0) immediately.
+    /// @note 1) For the calculation it is assumed that the vehicle starts breaking with decel (>=0) immediately.
     ///       If lcMaxSpeedStanding==0 the completion may be impossible, and -1 is returned.
+    ///       2) In case that no maxSpeedLat is used to control lane changing, this is only called prior to a lane change,
+    ///          and the duration is MSGlobals::gLaneChangeDuration.
     virtual double estimateLCDuration(const double speed, const double remainingManeuverDist, const double decel) const;
 
     /// @brief return true if the vehicle currently performs a lane change maneuver
     inline bool isChangingLanes() const {
         return myLaneChangeCompletion < (1 - NUMERICAL_EPS);
+    }
+
+    /// @brief Get the current lane change completion ratio
+    inline double getLaneChangeCompletion() const {
+        return myLaneChangeCompletion;
     }
 
     /// @brief return the direction of the current lane change maneuver
@@ -499,6 +506,10 @@ public:
     /// @brief decides the next lateral speed depending on the remaining lane change distance to be covered
     ///        and updates maneuverDist according to lateral safety constraints.
     virtual double computeSpeedLat(double latDist, double& maneuverDist); 
+
+    /// @brief Returns a deceleration value which is used for the estimation of the duration of a lane change.
+    /// @note  Effective only for continuous lane-changing when using attributes myMaxSpeedLatFactor and myMaxSpeedLatStanding. See #3771
+    virtual double getAssumedDecelForLaneChangeDuration() const;
 
     /// @brief try to retrieve the given parameter from this laneChangeModel. Throw exception for unsupported key
     virtual std::string getParameter(const std::string& key) const {

src/microsim/lcmodels/MSLCM_LC2013.cpp
@@ -77,6 +77,9 @@
 #define MAX_ONRAMP_LENGTH (double)200.
 #define TURN_LANE_DIST (double)200.0 // the distance at which a lane leading elsewhere is considered to be a turn-lane that must be avoided
 
+#define LC_RESOLUTION_SPEED_LAT (double)0.5 // the lateral speed (in m/s) for a standing vehicle which was unable to finish a continuous LC in time (in case mySpeedLatStanding==0), see #3771
+#define LC_ASSUMED_DECEL (double)1.0 // the minimal constant deceleration assumed to estimate the duration of a continuous lane-change at its initiation.
+
 // ===========================================================================
 // debug defines
 // ===========================================================================
@@ -2050,11 +2053,19 @@ void MSLCM_LC2013::addLCSpeedAdvice(const double vSafe) {
 
 double
 MSLCM_LC2013::computeSpeedLat(double latDist, double& maneuverDist) {
-    const double speedBound = myMaxSpeedLatStanding + myMaxSpeedLatFactor * myVehicle.getSpeed();
+    double speedBound = myMaxSpeedLatStanding + myMaxSpeedLatFactor * myVehicle.getSpeed();
+    if (isChangingLanes()) {
+        // Don't stay caught in the middle of a lane change while vehicle is standing, workaround for #3771
+        speedBound = MAX2(LC_RESOLUTION_SPEED_LAT, speedBound);
+    }
     return MAX2(-speedBound, MIN2(speedBound, 
                 MSAbstractLaneChangeModel::computeSpeedLat(latDist, maneuverDist)));
 }
 
+double
+MSLCM_LC2013::getAssumedDecelForLaneChangeDuration() const {
+    return MAX2(LC_ASSUMED_DECEL, -myVehicle.getAcceleration());
+}
 
 std::string
 MSLCM_LC2013::getParameter(const std::string& key) const {

src/utils/vehicle/SUMOVTypeParameter.cpp
@@ -333,6 +333,11 @@ SUMOVTypeParameter::getLCParam(const SumoXMLAttr attr, const double defaultValue
     }
 }
 
+const SUMOVTypeParameter::SubParams&
+SUMOVTypeParameter::getLCParams() const {
+    return lcParameter;
+}
+
 
 double
 SUMOVTypeParameter::getJMParam(const SumoXMLAttr attr, const double defaultValue) const {

src/utils/vehicle/SUMOVTypeParameter.h
@@ -133,6 +133,12 @@ public:
      */
     double getLCParam(const SumoXMLAttr attr, const double defaultValue) const;
 
+    /// @brief sub-model parameters
+    typedef std::map<SumoXMLAttr, std::string> SubParams;
+
+    /// @brief Returns the LC parameter
+    const SubParams& getLCParams() const;
+
     /** @brief Returns the named value from the map, or the default if it is not contained there
      * @param[in] attr The corresponding xml attribute
      * @param[in] defaultValue The value to return if the given map does not contain the named variable
@@ -197,8 +203,6 @@ public:
     /// @brief The enum-representation of the car-following model to use
     SumoXMLTag cfModel;
 
-    /// @brief sub-model parameters
-    typedef std::map<SumoXMLAttr, std::string> SubParams;
     /// @brief Car-following parameter
     SubParams cfParameter;
     /// @brief Lane-changing parameter

commit d28dd5f4b9e0596eb4dd4cefe258133d20ebd930

Merged MSLCM_XXX::myLCAEnums into LaneChangeAction. Fixes issues caused by b682832 (due to new element LCA_INSUFFICIENT_SPEED in SUMOXMLDefinitions.h (LaneChangeAction enum) being overridden in model's MSLC_XXX::myLCAEnum).

src/microsim/lcmodels/MSLCM_DK2008.h
@@ -44,21 +44,6 @@
 class MSLCM_DK2008 : public MSAbstractLaneChangeModel {
 public:
 
-    enum MyLCAEnum {
-        LCA_AMBLOCKINGLEADER = 1 << 16,
-        LCA_AMBLOCKINGFOLLOWER = 1 << 17,
-        LCA_MRIGHT = 1 << 18,
-        LCA_MLEFT = 1 << 19,
-        // !!! never set LCA_UNBLOCK = 1 << 20,
-        LCA_AMBLOCKINGFOLLOWER_DONTBRAKE = 1 << 21,
-        // !!! never used LCA_AMBLOCKINGSECONDFOLLOWER = 1 << 22,
-
-        // !!! never read LCA_KEEP1 = 1 << 24,
-        // !!! never used LCA_KEEP2 = 1 << 25,
-        LCA_AMBACKBLOCKER = 1 << 26,
-        LCA_AMBACKBLOCKER_STANDING = 1 << 27
-    };
-
     MSLCM_DK2008(MSVehicle& v);
 
     virtual ~MSLCM_DK2008();

src/microsim/lcmodels/MSLCM_LC2013.h
@@ -52,22 +52,6 @@
 class MSLCM_LC2013 : public MSAbstractLaneChangeModel {
 public:
 
-    enum MyLCAEnum {
-        LCA_AMBLOCKINGLEADER = 1 << 16,
-        LCA_AMBLOCKINGFOLLOWER = 1 << 17,
-        LCA_MRIGHT = 1 << 18,
-        LCA_MLEFT = 1 << 19,
-        // !!! never set LCA_UNBLOCK = 1 << 20,
-        LCA_AMBLOCKINGFOLLOWER_DONTBRAKE = 1 << 21,
-        // !!! never used LCA_AMBLOCKINGSECONDFOLLOWER = 1 << 22,
-        LCA_CHANGE_TO_HELP = 1 << 23,
-        // !!! never read LCA_KEEP1 = 1 << 24,
-        // !!! never used LCA_KEEP2 = 1 << 25,
-        LCA_AMBACKBLOCKER = 1 << 26,
-        LCA_AMBACKBLOCKER_STANDING = 1 << 27
-    };
-
-
     MSLCM_LC2013(MSVehicle& v);
 
     virtual ~MSLCM_LC2013();

src/microsim/lcmodels/MSLCM_SL2015.h
@@ -42,23 +42,6 @@
 class MSLCM_SL2015 : public MSAbstractLaneChangeModel {
 public:
 
-    enum MyLCAEnum {
-        LCA_MNone = 0,
-        LCA_AMBLOCKINGLEADER = 1 << 16,
-        LCA_AMBLOCKINGFOLLOWER = 1 << 17,
-        LCA_MRIGHT = 1 << 18,
-        LCA_MLEFT = 1 << 19,
-        // !!! never set LCA_UNBLOCK = 1 << 20,
-        LCA_AMBLOCKINGFOLLOWER_DONTBRAKE = 1 << 21,
-        // !!! never used LCA_AMBLOCKINGSECONDFOLLOWER = 1 << 22,
-        LCA_CHANGE_TO_HELP = 1 << 23,
-        // !!! never read LCA_KEEP1 = 1 << 24,
-        // !!! never used LCA_KEEP2 = 1 << 25,
-        LCA_AMBACKBLOCKER = 1 << 26,
-        LCA_AMBACKBLOCKER_STANDING = 1 << 27
-    };
-
-
     MSLCM_SL2015(MSVehicle& v);
 
     virtual ~MSLCM_SL2015();

src/utils/xml/SUMOXMLDefinitions.h
@@ -1057,7 +1057,7 @@ enum LaneChangeAction {
     /// @brief used by the sublane model
     LCA_SUBLANE = 1 << 15,
     /// @brief Vehicle is too slow to guarantee success of lane change (used for continuous lane changing in case that maxSpeedLatStanding==0)
-    LCA_INSUFFICIENT_SPEED = 1 << 16,
+    LCA_INSUFFICIENT_SPEED = 1 << 28,
     /// @brief lane can change
     LCA_WANTS_LANECHANGE = LCA_LEFT | LCA_RIGHT,
     /// @brief lane can change or stay
@@ -1073,10 +1073,27 @@ enum LaneChangeAction {
     /// @brief blocked in all directions
     LCA_BLOCKED = LCA_BLOCKED_LEFT | LCA_BLOCKED_RIGHT | LCA_INSUFFICIENT_SPACE | LCA_INSUFFICIENT_SPEED,
     /// @brief reasons of lane change
-    LCA_CHANGE_REASONS = (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT | LCA_SUBLANE | LCA_TRACI)
+    LCA_CHANGE_REASONS = (LCA_STRATEGIC | LCA_COOPERATIVE | LCA_SPEEDGAIN | LCA_KEEPRIGHT | LCA_SUBLANE | LCA_TRACI),
                          // LCA_BLOCKED_BY_CURRENT_LEADER = 1 << 28
                          // LCA_BLOCKED_BY_CURRENT_FOLLOWER = 1 << 29
                          /// @}
+
+    /// @name originally model specific states (migrated here since
+    ///       they were duplicated in all current models)
+    /// @{
+    LCA_AMBLOCKINGLEADER = 1 << 16,
+    LCA_AMBLOCKINGFOLLOWER = 1 << 17,
+    LCA_MRIGHT = 1 << 18,
+    LCA_MLEFT = 1 << 19,
+    // !!! never set LCA_UNBLOCK = 1 << 20,
+    LCA_AMBLOCKINGFOLLOWER_DONTBRAKE = 1 << 21,
+    // !!! never used LCA_AMBLOCKINGSECONDFOLLOWER = 1 << 22,
+    LCA_CHANGE_TO_HELP = 1 << 23,
+    // !!! never read LCA_KEEP1 = 1 << 24,
+    // !!! never used LCA_KEEP2 = 1 << 25,
+    LCA_AMBACKBLOCKER = 1 << 26,
+    LCA_AMBACKBLOCKER_STANDING = 1 << 27
+    /// @}
 };
 
 