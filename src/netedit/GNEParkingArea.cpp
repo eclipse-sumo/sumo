6807cf57ba88f64c691eb6514a0482e8f0849830
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-02-20 16:28:34 +0100

    Updated GNEParkingAreas

---
+++ src/netedit/GNEParkingArea.cpp
@@ -0,0 +1,364 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2018 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    GNEParkingArea.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Feb 2018
+/// @version $Id$
+///
+// A lane area vehicles can park at (GNE version)
+/****************************************************************************/
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <utility>
+#include <foreign/fontstash/fontstash.h>
+#include <utils/geom/PositionVector.h>
+#include <utils/common/RandHelper.h>
+#include <utils/common/SUMOVehicleClass.h>
+#include <utils/common/ToString.h>
+#include <utils/geom/GeomHelper.h>
+#include <utils/gui/windows/GUISUMOAbstractView.h>
+#include <utils/gui/windows/GUIAppEnum.h>
+#include <utils/gui/images/GUIIconSubSys.h>
+#include <utils/gui/globjects/GUIGLObjectPopupMenu.h>
+#include <utils/gui/div/GUIGlobalSelection.h>
+#include <utils/gui/div/GLHelper.h>
+#include <utils/gui/windows/GUIAppEnum.h>
+#include <utils/gui/images/GUITexturesHelper.h>
+#include <utils/xml/SUMOSAXHandler.h>
+#include <utils/common/MsgHandler.h>
+
+#include "GNEParkingArea.h"
+#include "GNELane.h"
+#include "GNEEdge.h"
+#include "GNEJunction.h"
+#include "GNEUndoList.h"
+#include "GNENet.h"
+#include "GNEChange_Attribute.h"
+#include "GNEViewNet.h"
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+
+GNEParkingArea::GNEParkingArea(const std::string& id, GNELane* lane, GNEViewNet* viewNet, double startPos, double endPos, const std::string& name, const std::vector<std::string>& lines, bool friendlyPosition) :
+    GNEStoppingPlace(id, viewNet, SUMO_TAG_PARKING_AREA, ICON_PARKINGAREA, lane, startPos, endPos, name, friendlyPosition),
+    myLines(lines) {
+}
+
+
+GNEParkingArea::~GNEParkingArea() {}
+
+
+void
+GNEParkingArea::updateGeometry() {
+    // Get value of option "lefthand"
+    double offsetSign = OptionsCont::getOptions().getBool("lefthand") ? -1 : 1;
+
+    // Update common geometry of stopping place
+    setStoppingPlaceGeometry();
+
+    // Obtain a copy of the shape
+    PositionVector tmpShape = myShape;
+
+    // Move shape to side
+    tmpShape.move2side(1.5 * offsetSign);
+
+    // Get position of the sign
+    mySignPos = tmpShape.getLineCenter();
+
+    // Set block icon position
+    myBlockIconPosition = myShape.getLineCenter();
+
+    // Set block icon rotation, and using their rotation for sign
+    setBlockIconRotation(myLane);
+
+    // Refresh element (neccesary to avoid grabbing problems)
+    myViewNet->getNet()->refreshElement(this);
+}
+
+
+void
+GNEParkingArea::writeAdditional(OutputDevice& device) const {
+    // Write parameters
+    device.openTag(getTag());
+    device.writeAttr(SUMO_ATTR_ID, getID());
+    device.writeAttr(SUMO_ATTR_LANE, myLane->getID());
+    device.writeAttr(SUMO_ATTR_STARTPOS, getAbsoluteStartPosition());
+    device.writeAttr(SUMO_ATTR_ENDPOS, getAbsoluteEndPosition());
+    if (myName.empty() == false) {
+        device.writeAttr(SUMO_ATTR_NAME, myName);
+    }
+    device.writeAttr(SUMO_ATTR_FRIENDLY_POS, myFriendlyPosition);
+    if (myLines.size() > 0) {
+        device.writeAttr(SUMO_ATTR_LINES, getAttribute(SUMO_ATTR_LINES));
+    }
+    // Close tag
+    device.closeTag();
+}
+
+
+const std::vector<std::string>&
+GNEParkingArea::getLines() const {
+    return myLines;
+}
+
+
+void
+GNEParkingArea::drawGL(const GUIVisualizationSettings& s) const {
+    // Start drawing adding an gl identificator
+    glPushName(getGlID());
+
+    // Add a draw matrix
+    glPushMatrix();
+
+    // Start with the drawing of the area traslating matrix to origin
+    glTranslated(0, 0, getType());
+
+    // Set color of the base
+    if (isAdditionalSelected()) {
+        GLHelper::setColor(myViewNet->getNet()->selectedAdditionalColor);
+    } else {
+        GLHelper::setColor(RGBColor(76, 170, 50));
+    }
+
+    // Obtain exaggeration of the draw
+    const double exaggeration = s.addSize.getExaggeration(s);
+
+    // Draw the area using shape, shapeRotations, shapeLengths and value of exaggeration
+    GLHelper::drawBoxLines(myShape, myShapeRotations, myShapeLengths, exaggeration);
+
+    // Check if the distance is enought to draw details
+    if (s.scale * exaggeration >= 10) {
+
+        // Add a draw matrix for details
+        glPushMatrix();
+
+        // Iterate over every line
+        for (int i = 0; i < (int)myLines.size(); ++i) {
+            // push a new matrix for every line
+            glPushMatrix();
+
+            // Rotate and traslaste
+            glTranslated(mySignPos.x(), mySignPos.y(), 0);
+            glRotated(-1 * myBlockIconRotation, 0, 0, 1);
+
+            // draw line with a color depending of the selection status
+            if (isAdditionalSelected()) {
+                GLHelper::drawText(myLines[i].c_str(), Position(1.2, (double)i), .1, 1.f, myViewNet->getNet()->selectionColor, 0, FONS_ALIGN_LEFT);
+            } else {
+                GLHelper::drawText(myLines[i].c_str(), Position(1.2, (double)i), .1, 1.f, RGBColor(76, 170, 50), 0, FONS_ALIGN_LEFT);
+            }
+
+            // pop matrix for every line
+            glPopMatrix();
+        }
+
+        // Start drawing sign traslating matrix to signal position
+        glTranslated(mySignPos.x(), mySignPos.y(), 0);
+
+        // Define number of points (for efficiency)
+        int noPoints = 9;
+
+        // If the scale * exaggeration is more than 25, recalculate number of points
+        if (s.scale * exaggeration > 25) {
+            noPoints = MIN2((int)(9.0 + (s.scale * exaggeration) / 10.0), 36);
+        }
+
+        // scale matrix depending of the exaggeration
+        glScaled(exaggeration, exaggeration, 1);
+
+        // Set color of the externe circle
+        if (isAdditionalSelected()) {
+            GLHelper::setColor(myViewNet->getNet()->selectedAdditionalColor);
+        } else {
+            GLHelper::setColor(RGBColor(76, 170, 50));
+        }
+
+        // Draw circle
+        GLHelper::drawFilledCircle((double) 1.1, noPoints);
+
+        // Traslate to front
+        glTranslated(0, 0, .1);
+
+        // Set color of the interne circle
+        if (isAdditionalSelected()) {
+            GLHelper::setColor(myViewNet->getNet()->selectionColor);
+        } else {
+            GLHelper::setColor(RGBColor(255, 235, 0));
+        }
+
+        // draw another circle in the same position, but a little bit more small
+        GLHelper::drawFilledCircle((double) 0.9, noPoints);
+
+        // If the scale * exageration is equal or more than 4.5, draw H
+        if (s.scale * exaggeration >= 4.5) {
+            if (isAdditionalSelected()) {
+                GLHelper::drawText("H", Position(), .1, 1.6, myViewNet->getNet()->selectedAdditionalColor, myBlockIconRotation);
+            } else {
+                GLHelper::drawText("H", Position(), .1, 1.6, RGBColor(76, 170, 50, 255), myBlockIconRotation);
+            }
+        }
+
+        // pop draw matrix
+        glPopMatrix();
+
+        // Show Lock icon depending of the Edit mode
+        drawLockIcon();
+    }
+
+    // pop draw matrix
+    glPopMatrix();
+
+    // Pop name
+    glPopName();
+
+    // Draw name
+    drawName(getCenteringBoundary().getCenter(), s.scale, s.addName);
+    if (s.addFullName.show && myName != "") {
+        GLHelper::drawText(myName, mySignPos, GLO_MAX - getType(), s.addFullName.size / s.scale, s.addFullName.color, myBlockIconRotation);
+    }
+}
+
+
+std::string
+GNEParkingArea::getAttribute(SumoXMLAttr key) const {
+    switch (key) {
+        case SUMO_ATTR_ID:
+            return getAdditionalID();
+        case SUMO_ATTR_LANE:
+            return myLane->getID();
+        case SUMO_ATTR_STARTPOS:
+            return toString(getAbsoluteStartPosition());
+        case SUMO_ATTR_ENDPOS:
+            return toString(getAbsoluteEndPosition());
+        case SUMO_ATTR_NAME:
+            return myName;
+        case SUMO_ATTR_FRIENDLY_POS:
+            return toString(myFriendlyPosition);
+        case SUMO_ATTR_LINES:
+            return joinToString(myLines, " ");
+        case GNE_ATTR_BLOCK_MOVEMENT:
+            return toString(myBlocked);
+        default:
+            throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+void
+GNEParkingArea::setAttribute(SumoXMLAttr key, const std::string& value, GNEUndoList* undoList) {
+    if (value == getAttribute(key)) {
+        return; //avoid needless changes, later logic relies on the fact that attributes have changed
+    }
+    switch (key) {
+        case SUMO_ATTR_ID:
+        case SUMO_ATTR_LANE:
+        case SUMO_ATTR_STARTPOS:
+        case SUMO_ATTR_ENDPOS:
+        case SUMO_ATTR_NAME:
+        case SUMO_ATTR_FRIENDLY_POS:
+        case SUMO_ATTR_LINES:
+        case GNE_ATTR_BLOCK_MOVEMENT:
+            undoList->p_add(new GNEChange_Attribute(this, key, value));
+            break;
+        default:
+            throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+bool
+GNEParkingArea::isValid(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+        case SUMO_ATTR_ID:
+            return isValidAdditionalID(value);
+        case SUMO_ATTR_LANE:
+            if (myViewNet->getNet()->retrieveLane(value, false) != NULL) {
+                return true;
+            } else {
+                return false;
+            }
+        case SUMO_ATTR_STARTPOS:
+            if (canParse<double>(value)) {
+                // Check that new start Position is smaller that end position
+                return ((parse<double>(value) / myLane->getLaneParametricLength()) < myEndPosRelative);
+            } else {
+                return false;
+            }
+        case SUMO_ATTR_ENDPOS:
+            if (canParse<double>(value)) {
+                // Check that new end Position is larger that end position
+                return ((parse<double>(value) / myLane->getLaneParametricLength()) > myStartPosRelative);
+            } else {
+                return false;
+            }
+        case SUMO_ATTR_NAME:
+            return true;
+        case SUMO_ATTR_FRIENDLY_POS:
+            return canParse<bool>(value);
+        case SUMO_ATTR_LINES:
+            return canParse<std::vector<std::string> >(value);
+        case GNE_ATTR_BLOCK_MOVEMENT:
+            return canParse<bool>(value);
+        default:
+            throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+// ===========================================================================
+// private
+// ===========================================================================
+
+void
+GNEParkingArea::setAttribute(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+        case SUMO_ATTR_ID:
+            changeAdditionalID(value);
+            break;
+        case SUMO_ATTR_LANE:
+            myLane = changeLane(myLane, value);
+            break;
+        case SUMO_ATTR_STARTPOS:
+            myStartPosRelative = parse<double>(value) / myLane->getLaneParametricLength();
+            break;
+        case SUMO_ATTR_ENDPOS:
+            myEndPosRelative = parse<double>(value) / myLane->getLaneParametricLength();
+            break;
+        case SUMO_ATTR_NAME:
+            myName = value;
+            break;
+        case SUMO_ATTR_FRIENDLY_POS:
+            myFriendlyPosition = parse<bool>(value);
+            break;
+        case SUMO_ATTR_LINES:
+            myLines = GNEAttributeCarrier::parse<std::vector<std::string> >(value);
+            break;
+        case GNE_ATTR_BLOCK_MOVEMENT:
+            myBlocked = parse<bool>(value);
+            break;
+        default:
+            throw InvalidArgument(toString(getTag()) + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+    // After setting attribute always update Geometry
+    updateGeometry();
+}
+
+
+/****************************************************************************/
