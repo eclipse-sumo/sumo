bc58d116c669ec448db1cac84edbc405ef1976b8
namdre <namdre.sumo@gmail.com>
2018-03-17 00:38:26 +0100

    remove-loops no longer ignores arrival and departure attributes. refs #335

--- src/router/ROVehicle.cpp
+++ src/router/ROVehicle.cpp
@@ -145,7 +145,11 @@ ROVehicle::computeRoute(const RORouterProvider& provider,
     }
     // check whether we have to evaluate the route for not containing loops
     if (removeLoops) {
-        current->recheckForLoops(getMandatoryEdges(0, 0));
+        const ROEdge* requiredStart = (getParameter().departPosProcedure == DEPART_POS_GIVEN 
+                || getParameter().departLaneProcedure == DEPART_LANE_GIVEN ? current->getEdgeVector().front() : 0);
+        const ROEdge* requiredEnd = (getParameter().arrivalPosProcedure == ARRIVAL_POS_GIVEN
+                || getParameter().arrivalLaneProcedure == ARRIVAL_LANE_GIVEN ? current->getEdgeVector().back() : 0);
+        current->recheckForLoops(getMandatoryEdges(requiredStart, requiredEnd));
         // check whether the route is still valid
         if (current->size() == 0) {
             delete current;
