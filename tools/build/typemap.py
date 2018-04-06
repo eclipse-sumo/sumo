a542e6ca3afb01f19d93e3f787b3e41b77420ec2
Michael Behrisch <michael.behrisch@dlr.de>
2018-04-06 16:16:32 +0200

    always rebuilding both typemaps, refactoring build script, refs #12, fix #3969

--- tools/build/typemap.py
+++ tools/build/typemap.py
@@ -13,7 +13,7 @@
 # @version $Id$
 
 """
-This script rebuilds "../../src/netimport/typemap.h", the file
+This script rebuilds "src/netimport/typemap.h" and "src/polyconvert/pc_typemap.h", the files
 representing the default typemaps.
 It does this by parsing the data from the sumo data dir.
 """
@@ -33,24 +33,13 @@ def writeTypeMap(typemapFile, typemap):
                       line.replace('"', r'\"').replace('\n', r'\n'), file=f)
             print(";", file=f)
 
-
-def main():
-    typemapDir = join(dirname(__file__), '..', '..', 'data', 'typemap')
-    if len(sys.argv) == 1:
-        typemapFile = join(
-            dirname(__file__), '..', '..', 'src', 'netimport', 'typemap.h')
-        formats = ("opendrive", "osm")
-        suffix = "Netconvert.typ.xml"
-    else:
-        typemapFile = join(
-            dirname(__file__), '..', '..', 'src', 'polyconvert', 'pc_typemap.h')
-        formats = ("navteq", "osm", "visum")
-        suffix = "Polyconvert.typ.xml"
-    # determine output file
+def generateTypeMap(relPath, formats, suffix):
+    typemapDataDir = join(dirname(__file__), '..', '..', 'data', 'typemap')
+    typemapFile = join(dirname(__file__), '..', '..', 'src', relPath)
     typemap = {}
     maxTime = 0
     for format in formats:
-        typemap[format] = join(typemapDir, format + suffix)
+        typemap[format] = join(typemapDataDir, format + suffix)
         if exists(typemap[format]):
             maxTime = max(maxTime, getmtime(typemap[format]))
     if not exists(typemapFile) or maxTime > getmtime(typemapFile):
@@ -58,4 +47,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    generateTypeMap(join('netimport', 'typemap.h'), ("opendrive", "osm"), "Netconvert.typ.xml")
+    generateTypeMap(join('polyconvert', 'pc_typemap.h'), ("navteq", "osm", "visum"), "Polyconvert.typ.xml")
