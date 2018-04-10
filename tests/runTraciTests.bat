0876ac16984b51a09ba92f4b416c54088f1f437c
Michael Behrisch <michael.behrisch@dlr.de>
2018-04-10 10:35:50 +0200

    removing everything for a clean openHub history

--- tests/runTraciTests.bat
+++
@@ -1,9 +0,0 @@
-REM Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-REM Copyright (C) 2008-2018 German Aerospace Center (DLR) and others.
-REM This program and the accompanying materials
-REM are made available under the terms of the Eclipse Public License v2.0
-REM which accompanies this distribution, and is available at
-REM http://www.eclipse.org/legal/epl-v20.html
-REM SPDX-License-Identifier: EPL-2.0
-call %~dp0\testenv.bat %1
-start %TEXTTESTPY% -a traci
--- tools/build/buildMSVC17ProjectWin64.bat
+++
@@ -1,1 +0,0 @@
-buildCMakeProject.py --generator "Visual Studio 15 2017 Win64"
