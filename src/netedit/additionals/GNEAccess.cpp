ef4867a34c6fd42de0e4889db95dd2c018546aa4
Pablo Alvarez Lopez <p.alvarezlopez@dlr.de>
2018-05-24 15:41:16 +0200

    Updating GNEAccess.h and GNEAccess.cpp. Refs #4018

---
+++ src/netedit/additionals/GNEAccess.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2018 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    GNEAccess.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Nov 2015
+/// @version $Id$
+///
+// A lane area vehicles can halt at (GNE version)
+/****************************************************************************/
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <utility>
+#include <foreign/fontstash/fontstash.h>
+#include <utils/geom/PositionVector.h>
+#include <utils/common/RandHelper.h>
+#include <utils/common/SUMOVehicleClass.h>
+#include <utils/common/ToString.h>
+#include <utils/geom/GeomHelper.h>
+#include <utils/gui/windows/GUISUMOAbstractView.h>
+#include <utils/gui/windows/GUIAppEnum.h>
+#include <utils/gui/images/GUIIconSubSys.h>
+#include <utils/gui/globjects/GUIGLObjectPopupMenu.h>
+#include <utils/gui/div/GLHelper.h>
+#include <utils/gui/windows/GUIAppEnum.h>
+#include <utils/gui/images/GUITexturesHelper.h>
+#include <utils/xml/SUMOSAXHandler.h>
+#include <utils/common/MsgHandler.h>
+#include <netedit/netelements/GNELane.h>
+#include <netedit/netelements/GNEEdge.h>
+#include <netedit/netelements/GNEJunction.h>
+#include <netedit/GNEUndoList.h>
+#include <netedit/GNENet.h>
+#include <netedit/changes/GNEChange_Attribute.h>
+#include <netedit/GNEViewNet.h>
+#include <netedit/GNEViewParent.h>
+
+#include "GNEBusStop.h"
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+
+
+/****************************************************************************/
---
+++ src/netedit/additionals/GNEAccess.h
@@ -0,0 +1,40 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2018 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    GNEAccess.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Nov 2015
+/// @version $Id$
+///
+// A class for visualizing busStop geometry (adapted from GUILaneWrapper)
+/****************************************************************************/
+#ifndef GNEAccess_h
+#define GNEAccess_h
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <string>
+#include <vector>
+#include <utils/gui/globjects/GUIGlObject.h>
+#include <utils/gui/settings/GUIPropertySchemeStorage.h>
+#include <utils/geom/PositionVector.h>
+#include <netedit/GNEAttributeCarrier.h>
+
+#include "GNEStoppingPlace.h"
+
+
+#endif
