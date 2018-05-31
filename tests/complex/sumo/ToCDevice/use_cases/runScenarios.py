7b9bf7a8a4ba9be1c20661b145cc7a3b51d849b9
yunpangfloetteroed <yun-pang.floetteroed@dlr.de>
2018-05-31 21:36:45 +0200

    [TransAID] ToCRequest at information signs;added the 2. VType for automated vehicles; set the fixed number of vehicles

--- tests/complex/sumo/ToCDevice/use_cases/runScenarios.py
+++ tests/complex/sumo/ToCDevice/use_cases/runScenarios.py
@@ -24,12 +24,16 @@ runnerPy = os.path.join(ROOT_DIR,"runner.py")
 # parameters
 timeUntilMRM =[10]#, 8, 6]
 initialAwareness= [0.95]#, 0.75, 0.5]
-responseTime= [5] # [[1, 5], [3, 9], [3,15]] ? give a range or a certain value?
+responseTime= [2]#, 5, 9, 15] # [[1, 5], [3, 9], [3,15]] ? give a range or a certain value?
 recoveryRate= [0.05]#, 0.05, 0.05]
 mrmDecel= [1.1]#, 1.3, 1.5]
 routeMap= {}
 routeMap['UC1_1'] = ['r0', 'start approach1 approach2 safetyzone1_1 safetyzone1_2 workzone safetyzone2_1 safetyzone2_2 leave end']
 routeMap['UC5_1'] = ['r0', 'e0']
+# TODO: set up the planed 45 scenarios in regard to the demands, vehicular compositions and 5 parameter sets
+#demandMap = {"UC1_1":[525, 825, 1155], "UC5_1":[665, 1015, 1463]}
+#compositionMap = {}
+#componentMap = {"UC1_1":[[0.7, 0.3]], "UC5_1":[665, 1015, 1463]}
 
 def checkDir(dir):
     if not os.path.exists(dir):
@@ -51,33 +55,41 @@ def generateRouteFile(routefile,i,j,k,l,rList):
         end = edges
     fd = open(routefile, 'w')
     print("""<?xml version="1.0" encoding="UTF-8"?>
-    <routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">
+    <routes >
         <!-- vType definitions -->
         <!-- vClass custom1 disallows vehicles to enter bus lane at construction site -->
         <!-- default vClass is passenger -->
-        <vType id="automated" sigma="0." speedFactor="1" vClass="custom1"/>
-        <vType id="manual" sigma="0.5" speedFactor="normc(0.8,0.1,0.5,1.5)" emergencyDecel="9" guiShape="passenger/van" carFollowModel="TCI"/>
+        <vType id="toc" sigma="0." speedFactor="1" vClass="custom1" emergencyDecel="9" />
+        <vType id="automated" sigma="0." speedFactor="1" vClass="custom1" emergencyDecel="9" />
+        <vType id="manual" sigma="0.5" speedFactor="normc(0.8,0.1,0.5,1.5)" emergencyDecel="9" guiShape="passenger/van" driverState="true"/>
         <route id="%s" edges="%s"/>
         <!-- one  hour automated vehicle flow -->
-        <flow id="AVflow" type="automated" route="%s" begin="0" end="3600" probability="0.01" color="red">
+        <flow id="AVflowToC" type="toc" route="%s" begin="0" end="3600" number="150" color="red">
             <param key="has.toc.device" value="true"/>
             <param key="device.toc.manualType" value="manual"/>
-            <param key="device.toc.automatedType" value="automated"/>
+            <param key="device.toc.automatedType" value="toc"/>
             <param key="device.toc.initialAwareness" value="%s"/>
             <param key="device.toc.responseTime" value="%s"/>
             <param key="device.toc.recoveryRate" value="%s"/>
             <param key="device.toc.mrmDecel" value="%s"/>
+            <param key="device.toc.colorScheme" value="true"/>
+        </flow>
+        <flow id="AVflow" type="automated" route="%s" begin="0" end="3600" number="150" color="white">
+            <param key="has.toc.device" value="false"/>
+            <param key="device.toc.manualType" value="manual"/>
+            <param key="device.toc.automatedType" value="automated"/>
+            <param key="device.toc.colorScheme" value="true"/>
         </flow>
         <!-- one  hour manually driven vehicle flow -->
-        <flow id="LVflow" type="manual" from="%s" to="%s" begin="0" end="3600" probability="0.01"/>
-    </routes>""" %(routeID, edges, routeID, i, j, k, l, start, end), file=fd)
+        <flow id="LVflow" type="manual" from="%s" to="%s" begin="0" end="3600" number="200"/>
+    </routes>""" %(routeID, edges, routeID, i, j, k, l, routeID, start, end), file=fd)
     fd.close()
     
-def generateAddFile(addfile, frequency, outputdir):
+def generateAddFile(addfile, frequency, outputdir, i, j, k, l, t):
     freq = frequency
     fd = open(addfile, 'w')
-    edgeFile = os.path.join(outputdir, "edges_%s.xml" %freq)
-    laneFile = os.path.join(outputdir, "lanes_%s.xml" %freq)
+    edgeFile = os.path.join('OUTPUT', "edges_%s_%s_%s_%s_%s_%s.xml" %(freq, i, j, k, l, t))
+    laneFile = os.path.join('OUTPUT', "lanes_%s_%s_%s_%s_%s_%s.xml" %(freq, i, j, k, l, t))
     print("""
     <additional xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/additional_file.xsd">
         <edgeData id="edge_%s" freq="%s" file="%s" excludeEmpty="true"/>
@@ -85,10 +97,17 @@ def generateAddFile(addfile, frequency, outputdir):
     </additional>""" %(freq,freq,edgeFile,freq,freq,laneFile), file=fd)
     fd.close()
 
+def printParameterSet(i,j,k,l,t):
+    print ("initialAwareness: %s" %(i))
+    print ("responseTime: %s" %(j))
+    print ("recoveryRate: %s" %(k))
+    print ("mrmDecel: %s" %(l))
+    print ("timeUntilMRM: %s" %(t))
+
 if __name__ == "__main__":
 
     optParser = OptionParser()
-    optParser.add_option("--codes", help="scenario code", default="UC1_1 UC5_1")
+    optParser.add_option("--codes", help="scenario code", default="UC1_1")# UC5_1")
     optParser.add_option("--frequency", help="output frequency", default= 300)
     optParser.add_option("-v", "--verbose", action="store_true", dest="verbose", 
                          default=False, help="tell me what you are doing")
@@ -97,47 +116,54 @@ if __name__ == "__main__":
     options.codes = options.codes.split()
     if options.verbose:
         print ("output frequency: %s" %(options.frequency))
+    # two use cases
     for code in options.codes:
-    # generate input files
+        # generate input files
         OUT_DIR = os.path.join(ROOT_DIR, code)
         outputdir = checkDir(OUT_DIR)
-        # ? do not need an additional file for rerouting?
-        addfile = os.path.join(ROOT_DIR, "input_additional.add.xml")
-        generateAddFile(addfile, options.frequency, outputdir)
+        netfile = glob.glob(os.path.join(OUT_DIR, '*.net.xml'))[0]
+        closedLaneFile = glob.glob(os.path.join(OUT_DIR, 'close*.add.xml'))
+        shapefile = glob.glob(os.path.join(OUT_DIR, 'shape*.add.xml'))
+        viewfile = glob.glob(os.path.join(OUT_DIR, 'view*.add.xml'))[0]
+        print ("%s: %s; %s" %(OUT_DIR, closedLaneFile, shapefile))
         
         for i in initialAwareness:
-            if options.verbose:
-                print ("initialAwareness: %s" %(i))
             for j in responseTime:
-                if options.verbose:
-                    print ("responseTime: %s" %(j))
                 for k in recoveryRate:
-                    if options.verbose:
-                        print ("recoveryRate: %s" %(k))
                     for l in mrmDecel:
-                        if options.verbose:
-                            print ("mrmDecel: %s" %(l))
                         filename =  'input_routes_' + str(i) + '_' + str(j) + '_' + str(k) + '_' + str(l) + '.rou.xml'
                         routefile = os.path.join(OUT_DIR, filename)
+
                         if routeMap[code]:
                             generateRouteFile(routefile,i,j,k,l,routeMap[code])
                         else:
                             print("Error: no route information exists.")
                         
                         for t in timeUntilMRM:
+                            addfile = os.path.join(OUT_DIR, "input_additional_%s_%s_%s_%s_%s.add.xml" %(i,j,k,l,t))
+                            generateAddFile(addfile, options.frequency, outputdir, i, j, k, l, t)
                             if options.verbose:
-                                print ("timeUntilMRM: %s" %(t))
+                                printParameterSet(i,j,k,l,t)
                             fcdname = 'fcd_' + str(i) + '_' + str(j) + '_' + str(k) + '_' + str(l) + '_' + str(t) + '.xml'
                             fcdfile = os.path.join(outputdir, fcdname)
+                            addfiles = addfile
+                            if closedLaneFile:
+                                addfiles = addfiles + ',' + closedLaneFile[0]
+                            if shapefile:
+                                addfiles = addfiles + ',' + shapefile[0]
+                            
                             cmd = [
                                 'python',
                                 runnerPy,
-                                '--net-file', glob.glob(os.path.join(OUT_DIR, '*.net.xml'))[0],
+                                '--net-file', netfile,
                                 '--route-file', routefile,
-                                '--add-file', addfile,
+                                '--add-file', addfiles,
                                 '--output-file', fcdfile,
+                                '--view-file', viewfile,
                                 '--time-MRM', str(t),
-                                '--verbose',
+                                '--code', code,
+                                '--nogui', "false",
+                                #'--verbose',
                                 ]
                             print("calling %s" % ' '.join(cmd))
                             subprocess.call(cmd, stdout=sys.stdout, stderr=sys.stderr)
--- tests/complex/sumo/ToCDevice/use_cases/runner.py
+++ tests/complex/sumo/ToCDevice/use_cases/runner.py
@@ -44,21 +44,54 @@ def run():
     """execute the TraCI control loop"""
     step = 0
     AVsOnRoad = set(traci.vehicle.getIDList())
-    while step < 500/DT:
+    onlyUpward = set()
+    while traci.simulation.getMinExpectedNumber() > 0: #while step < 3600/DT:
         traci.simulationStep()
         # Keep book in a set AVsOnRoad
         AVsOnRoad = AVsOnRoad.difference([vehID for vehID in traci.simulation.getArrivedIDList() if vehID.startswith(ToC_vehicles_identifier)])
         AVsOnRoad.update([vehID for vehID in traci.simulation.getDepartedIDList() if vehID.startswith(ToC_vehicles_identifier)])
-        if step == int(200/DT):
-            t = traci.simulation.getCurrentTime()/1000.
-            for vehID in AVsOnRoad:
-                requestToC(vehID, timeTillMRM)
-                print("Requested ToC of veh0 at t=%s (until t=%s)"%(t,t + timeTillMRM))
-                printToCParams(vehID, True)
+        toRemove=set()
+        # provide the ToCService at the traffic sign for informing the lane closure
+        for vehID in AVsOnRoad:
+                distToTOR = traci.vehicle.getDrivingDistance(vehID, "approach2", 245.)
+                print ("distToTOR = %s"%distToTOR)
+                if distToTOR < 0:
+                    toRemove.add(vehID)
+                    t = traci.simulation.getCurrentTime()/1000.
+                    if traci.vehicle.getTypeID(vehID).startswith("toc"):
+                        requestToC(vehID, timeUntilMRM)
+                        if options.verbose:
+                            print("Requested ToC of %s at t=%s (until t=%s)"%(vehID, t,t + float(timeUntilMRM)))
+                            printToCParams(vehID, True)
+                    elif traci.vehicle.getTypeID(vehID).startswith("auto"):
+                        traci.vehicle.setVehicleClass(vehID, "passenger")
+            # todo: for UC5_1
+            #if traci.vehicle.getDrivingDistance(vehID, "e0", 250.) == 250.:
+            #    traci.vehicle.setType(vehID, "manual")
+        AVsOnRoad = AVsOnRoad.difference(toRemove)
+        onlyUpward.update(toRemove)
+        print ("onlyUpward:%s" %onlyUpward)
+        toRemove=set()
+        # provide ToCService at the traffic sign for informing the lane closure
+        for vehID in onlyUpward:
+            distToTOR = traci.vehicle.getDrivingDistance(vehID, "approach2", 245.)
+            print ("distToTOR = %s"%distToTOR)
+            if distToTOR < 0:
+                toRemove.add(vehID)
+                t = traci.simulation.getCurrentTime()/1000.
+                if traci.vehicle.getTypeID(vehID).startswith("toc"):
+                    requestToC(vehID, timeUntilMRM)
+                    if options.verbose:
+                        print("Requested ToC of %s at t=%s (until t=%s)"%(vehID, t,t + float(timeUntilMRM)))
+                        printToCParams(vehID, True)
+                elif traci.vehicle.getTypeID(vehID).startswith("auto"):
+                    traci.vehicle.setVehicleClass(vehID, "passenger")
+        onlyUpward.difference(toRemove)
+        
         step += 1
     
-def requestToC(vehID, timeTillMRM):
-    traci.vehicle.setParameter(vehID, "device.toc.requestToC", str(timeTillMRM))
+def requestToC(vehID, timeUntilMRM):
+    traci.vehicle.setParameter(vehID, "device.toc.requestToC", str(timeUntilMRM))
 
 def printToCParams(vehID, only_dynamic = False):
     holder = traci.vehicle.getParameter(vehID, "device.toc.holder")
@@ -88,15 +121,15 @@ def printToCParams(vehID, only_dynamic = False):
     print("  currentSpeed = %s"%speed)
     print("  state = %s"%state)
     
-    
-
 def get_options():
     optParser = optparse.OptionParser()
+    optParser.add_option("-c", "--code", dest="code", help="scenario code")
     optParser.add_option("-n", "--net-file", dest="netfile", help="network file")
     optParser.add_option("-r", "--route-file", dest="routefile", help="route file")
     optParser.add_option("-a", "--add-file", dest="addfile", help="additional file")
     optParser.add_option("-o", "--output-file", dest="outputfile", help="fcd output filename")
-    optParser.add_option("-t", "--time-MRM", dest="timeTillMRM", default= 8, help="time until MRM")
+    optParser.add_option("-g", "--view-file", dest="viewfile", help="sumo-gui view setting file")
+    optParser.add_option("-t", "--time-MRM", dest="timeUntilMRM", default= 8, help="time until MRM")
     optParser.add_option("--nogui", action="store_false",
                          default=True, help="run the commandline version of sumo")
     optParser.add_option("-v", "--verbose", action="store_true", dest="verbose", 
@@ -108,9 +141,10 @@ def get_options():
 # this is the main entry point of this script
 if __name__ == "__main__":
     options = get_options()
-    timeTillMRM = options.timeTillMRM
+    timeUntilMRM = float(options.timeUntilMRM)
+    code = options.code
     if options.verbose:
-        print ("time until MRM: %s" %(timeTillMRM))
+        print ("time to MRM: %s" %(timeUntilMRM))
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
     if options.nogui:
@@ -120,9 +154,12 @@ if __name__ == "__main__":
 
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
-    traci.start([sumoBinary, "-n", options.netfile, "-r", options.routefile, "-a", options.addfile, "--fcd-output", options.outputfile, "--no-step-log", "true"])
+    traci.start([sumoBinary, "-n", options.netfile, "-r", options.routefile, "-a", options.addfile, "--ignore-route-errors", "--step-method.ballistic", "--fcd-output", options.outputfile, "--gui-settings-file", options.viewfile, "--no-step-log", "true"])
      
-    run()
+    if code=="UC1_1":
+        run()
+    else:
+        pass
     
     traci.close()
     sys.stdout.flush()
