f2b2d6b4dc2e12f327fb31b958ae64fe9ab36289
namdre <jakob.erdmann@dlr.de>
2018-06-01 20:09:46 +0200

    python3 compatibility. fix #4149

--- tools/game/racing.py
+++ tools/game/racing.py
@@ -21,7 +21,14 @@ from __future__ import absolute_import
 from __future__ import print_function
 import os
 import sys
-import Queue
+
+if sys.version_info.major == 3:
+    import queue as Queue
+    from tkinter import *
+else:
+    import Queue
+    from Tkinter import *
+
 import threading
 import math
 
@@ -35,14 +42,12 @@ except ImportError:
     sys.exit(
         "please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
 
-
 try:
     import autopy
 except ImportError:
     sys.stderr.write("autopy not installed. Can only use keyboard control.")
     autopy = None
 
-from Tkinter import *
 
 eventQueue = Queue.Queue()
 TS = 0.05
