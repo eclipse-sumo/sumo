76ff541a3ac5ebb0809502b32800ea048a0f2c23
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-07-26 12:54:19 +0200

    Fixed compilation with C++ 11. Refs #4355

--- src/utils/foxtools/FXLinkLabel.cpp
+++ src/utils/foxtools/FXLinkLabel.cpp
@@ -34,7 +34,7 @@
 FXint
 FXLinkLabel::fxexecute(FXString link) {
 #ifdef WIN32
-    return (intptr_t)ShellExecute(NULL, "open", link.text(), NULL, NULL, SW_SHOWNORMAL) > 32;
+    return (int)ShellExecute(NULL, "open", link.text(), NULL, NULL, SW_SHOWNORMAL) > 32;
 #else
     FXString ext = FXPath::extension(link);
     FXString list;
