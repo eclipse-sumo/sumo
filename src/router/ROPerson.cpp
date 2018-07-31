eclipse-sumo/sumo

commit 294eea65b438d094c47f613be5d46f8fac26de40

Updated code of Parameterised. Refs #4361

src/router/ROPerson.cpp
@@ -263,13 +263,8 @@ ROPerson::saveAsXML(OutputDevice& os, OutputDevice* const typeos, bool asAlterna
         (*it)->saveAsXML(os, asAlternatives);
     }
 
-    for (std::map<std::string, std::string>::const_iterator j = getParameter().getMap().begin(); j != getParameter().getMap().end(); ++j) {
-        os.openTag(SUMO_TAG_PARAM);
-        os.writeAttr(SUMO_ATTR_KEY, (*j).first);
-        os.writeAttr(SUMO_ATTR_VALUE, (*j).second);
-        os.closeTag();
-    }
-    os.closeTag();
+    // write params
+    getParameter().writeParams(os);
 }
 
 

src/guisim/GUIContainer.cpp
@@ -125,7 +125,7 @@ GUIParameterTableWindow*
 GUIContainer::getParameterWindow(GUIMainWindow& app,
                                  GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 12 + (int)getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 12 + (int)getParameter().getParametersMap().size());
     // add items
     ret->mkItem("stage", false, getCurrentStageDescription());
     ret->mkItem("start edge [id]", false, getFromEdge()->getID());
@@ -147,7 +147,7 @@ GUIParameterTableWindow*
 GUIContainer::getTypeParameterWindow(GUIMainWindow& app,
                                      GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 8 + (int)myVType->getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 8 + (int)myVType->getParameter().getParametersMap().size());
     // add items
     ret->mkItem("Type Information:", false, "");
     ret->mkItem("type [id]", false, myVType->getID());

src/guisim/GUIJunctionWrapper.cpp
@@ -99,7 +99,7 @@ GUIJunctionWrapper::getPopUpMenu(GUIMainWindow& app,
 GUIParameterTableWindow*
 GUIJunctionWrapper::getParameterWindow(GUIMainWindow& app, GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 12 + (int)myJunction.getMap().size());
+        new GUIParameterTableWindow(app, *this, 12 + (int)myJunction.getParametersMap().size());
     // add items
     ret->mkItem("type", false, toString(myJunction.getType()));
     // close building

src/guisim/GUIPerson.cpp
@@ -222,7 +222,7 @@ GUIParameterTableWindow*
 GUIPerson::getParameterWindow(GUIMainWindow& app,
                               GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 12 + (int)getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 12 + (int)getParameter().getParametersMap().size());
     // add items
     ret->mkItem("stage", false, getCurrentStageDescription());
     // there is always the "start" stage which we do not count here because it is not strictly part of the plan
@@ -247,7 +247,7 @@ GUIParameterTableWindow*
 GUIPerson::getTypeParameterWindow(GUIMainWindow& app,
                                   GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 8 + (int)myVType->getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 8 + (int)myVType->getParameter().getParametersMap().size());
     // add items
     ret->mkItem("Type Information:", false, "");
     ret->mkItem("type [id]", false, myVType->getID());

src/guisim/GUITrafficLightLogicWrapper.cpp
@@ -189,7 +189,7 @@ GUIParameterTableWindow*
 GUITrafficLightLogicWrapper::getParameterWindow(GUIMainWindow& app,
         GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 3 + (int)myTLLogic.getMap().size());
+        new GUIParameterTableWindow(app, *this, 3 + (int)myTLLogic.getParametersMap().size());
     ret->mkItem("tlLogic [id]", false, myTLLogic.getID());
     ret->mkItem("program", false, myTLLogic.getProgramID());
     // close building

src/guisim/GUIVehicle.cpp
@@ -93,7 +93,7 @@ GUIVehicle::getParameterWindow(GUIMainWindow& app,
                                GUISUMOAbstractView&) {
     const int sublaneParams = MSGlobals::gLateralResolution > 0 ? 4 : 0;
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 37 + sublaneParams + (int)getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 37 + sublaneParams + (int)getParameter().getParametersMap().size());
     // add items
     ret->mkItem("lane [id]", false, Named::getIDSecure(myLane, "n/a"));
     if (MSAbstractLaneChangeModel::haveLateralDynamics()) {
@@ -190,7 +190,7 @@ GUIVehicle::getTypeParameterWindow(GUIMainWindow& app,
                                    GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
         new GUIParameterTableWindow(app, *this, 25 
-                + (int)myType->getParameter().getMap().size()
+                + (int)myType->getParameter().getParametersMap().size()
                 + (int)myType->getParameter().lcParameter.size()
                 + (int)myType->getParameter().jmParameter.size());
     // add items

src/libsumo/TrafficLight.cpp
@@ -59,7 +59,7 @@ TrafficLight::getCompleteRedYellowGreenDefinition(const std::string& tlsID) {
     const std::vector<MSTrafficLightLogic*> logics = getTLS(tlsID).getAllLogics();
     for (MSTrafficLightLogic* logic : logics) {
         TraCILogic l(logic->getProgramID(), 0, logic->getCurrentPhaseIndex());
-        l.subParameter = logic->getMap();
+        l.subParameter = logic->getParametersMap();
         for (int j = 0; j < logic->getPhaseNumber(); ++j) {
             MSPhaseDefinition phase = logic->getPhase(j);
             l.phases.emplace_back(TraCIPhase(phase.duration, phase.minDuration, phase.maxDuration, phase.getState()));

src/mesogui/GUIMEVehicle.cpp
@@ -59,7 +59,7 @@ GUIParameterTableWindow*
 GUIMEVehicle::getParameterWindow(GUIMainWindow& app,
                                  GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 21 + (int)getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 21 + (int)getParameter().getParametersMap().size());
     // add items
     ret->mkItem("edge [id]", false, getEdge()->getID());
     ret->mkItem("segment [#]", false, getSegment()->getIndex());
@@ -132,7 +132,7 @@ GUIParameterTableWindow*
 GUIMEVehicle::getTypeParameterWindow(GUIMainWindow& app,
                                      GUISUMOAbstractView&) {
     GUIParameterTableWindow* ret =
-        new GUIParameterTableWindow(app, *this, 9 + (int)myType->getParameter().getMap().size());
+        new GUIParameterTableWindow(app, *this, 9 + (int)myType->getParameter().getParametersMap().size());
     // add items
     ret->mkItem("Type Information:", false, "");
     ret->mkItem("type [id]", false, myType->getID());

commit 0b4b86eadeb1289d36ab35de0d98a20c5d531b90

fixed writing of persons broken in [294eea65b438d094c47f613be5d46f8fac26de40]

src/router/ROPerson.cpp
@@ -265,6 +265,7 @@ ROPerson::saveAsXML(OutputDevice& os, OutputDevice* const typeos, bool asAlterna
 
     // write params
     getParameter().writeParams(os);
+    os.closeTag();
 }
 
 