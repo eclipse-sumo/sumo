eclipse-sumo/sumo

commit 7d8cd284aac397251e2152163e55e3904b86f463

Fixed #4408

src/foreign/rtree/SUMORTree.h
@@ -27,6 +27,7 @@
 #include <utils/gui/settings/GUIVisualizationSettings.h>
 #include <utils/geom/Boundary.h>
 #include <utils/foxtools/MFXMutex.h>
+#include <utils/options/OptionsCont.h>
 
 #include "RTree.h"
 
@@ -122,6 +123,19 @@ public:
     void addAdditionalGLObject(GUIGlObject *o) {
         AbstractMutex::ScopedLocker locker(myLock);
         Boundary b = o->getCenteringBoundary();
+
+        // show information in gui testing debug gl mode
+        if (OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
+            if(myTreeDebug.count(o) != 0) {
+                throw ProcessError("GUIGlObject " + o->getMicrosimID() + " was already inserted");
+            } else if ((b.getWidth() == 0) || (b.getHeight() == 0)) {
+                throw ProcessError("boundary of GUIGlObject " + o->getMicrosimID() + " has an invalid size");
+            } else {
+                myTreeDebug[o] = b;
+                std::cout << "Inserted object " << o->getMicrosimID() << " into tree with boundary " << toString(b) << std::endl;
+            }
+        }
+
         const float cmin[2] = {(float) b.xmin(), (float) b.ymin()};
         const float cmax[2] = {(float) b.xmax(), (float) b.ymax()};
         Insert(cmin, cmax, o);
@@ -134,6 +148,19 @@ public:
     void removeAdditionalGLObject(GUIGlObject *o) {
         AbstractMutex::ScopedLocker locker(myLock);
         Boundary b = o->getCenteringBoundary();
+
+        // show information in gui testing debug gl mode
+        if (OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
+            if(myTreeDebug.count(o) == 0) {
+                throw ProcessError("GUIGlObject " + o->getMicrosimID() + " wasn't inserted");
+            } else if (b != myTreeDebug.at(o)) {
+                throw ProcessError("add boundary of GUIGlObject " + o->getMicrosimID() + " is different of remove boundary");
+            } else {
+                myTreeDebug.erase(o);
+                std::cout << "removed object " << o->getMicrosimID() << " from tree " << std::endl;
+            }
+        }
+
         const float cmin[2] = {(float) b.xmin(), (float) b.ymin()};
         const float cmax[2] = {(float) b.xmax(), (float) b.ymax()};
         Remove(cmin, cmax, o);
@@ -144,6 +171,12 @@ protected:
     /// @brief A mutex avoiding parallel change and traversal of the tree
     mutable MFXMutex myLock;
 
+private:
+    
+    /**@brief Map only used for check that SUMORTree works as expected, only is used if option "gui-testing-debug-gl" is enabled.
+     * @note Warning: DO NOT USE in release mode and use it in debug mode carefully, due it produces a slowdown.
+     */
+    std::map<GUIGlObject*, Boundary> myTreeDebug;
 };
 
 

src/netedit/GNELoadThread.cpp
@@ -250,6 +250,9 @@ GNELoadThread::fillOptions(OptionsCont& oc) {
     oc.doRegister("gui-testing-debug", new Option_Bool(false));
     oc.addDescription("gui-testing-debug", "Visualisation", "Enable output messages during GUI-Testing");
 
+    oc.doRegister("gui-testing-debug-gl", new Option_Bool(false));
+    oc.addDescription("gui-testing-debug-gl", "Visualisation", "Enable output messages during GUI-Testing specific of gl functions");
+
     SystemFrame::addReportOptions(oc); // this subtopic is filled here, too
 
     NIFrame::fillOptions();

src/netedit/GNENet.cpp
@@ -1174,7 +1174,6 @@ GNENet::addGLObjectIntoNet(GUIGlObject* o) {
 void 
 GNENet::removeGLObjectFromNet(GUIGlObject* o) {
     myGrid.removeAdditionalGLObject(o);
-
 }
 
 

src/utils/geom/Boundary.cpp
@@ -344,6 +344,12 @@ Boundary::operator==(const Boundary& b) const {
 }
 
 
+bool 
+Boundary::operator!=(const Boundary& b) const {
+    return !(*this == b);
+}
+
+
 void
 Boundary::set(double xmin, double ymin, double xmax, double ymax) {
     myXmin = xmin;

src/utils/geom/Boundary.h
@@ -136,8 +136,11 @@ public:
     /// Output operator
     friend std::ostream& operator<<(std::ostream& os, const Boundary& b);
 
-    /// @brief Comparison operator
+    /// @brief Comparison operator equal
     bool operator==(const Boundary& b) const;
+
+    /// @brief Comparison operator not equal
+    bool operator!=(const Boundary& b) const;
 
 private:
     /// The boundaries

commit 7b91ed460e009b274c8a49c382c85abc17cb8111

quick fix: sumo-gui was broken by [7d8cd284aac397251e2152163e55e3904b86f463]. refs #4408

src/foreign/rtree/SUMORTree.h
@@ -125,7 +125,7 @@ public:
         Boundary b = o->getCenteringBoundary();
 
         // show information in gui testing debug gl mode
-        if (OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
+        if (OptionsCont::getOptions().exists("gui-testing-debug-gl") && OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
             if(myTreeDebug.count(o) != 0) {
                 throw ProcessError("GUIGlObject " + o->getMicrosimID() + " was already inserted");
             } else if ((b.getWidth() == 0) || (b.getHeight() == 0)) {
@@ -150,7 +150,7 @@ public:
         Boundary b = o->getCenteringBoundary();
 
         // show information in gui testing debug gl mode
-        if (OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
+        if (OptionsCont::getOptions().exists("gui-testing-debug-gl") && OptionsCont::getOptions().getBool("gui-testing-debug-gl")) {
             if(myTreeDebug.count(o) == 0) {
                 throw ProcessError("GUIGlObject " + o->getMicrosimID() + " wasn't inserted");
             } else if (b != myTreeDebug.at(o)) {