cd8e2779bd2a3558a35a3b8d7bd018f78897d7d0
namdre <jakob.erdmann@dlr.de>
2018-08-15 08:25:34 +0200

    renamed option for consistency (old option name added as synonym). refs #12

--- src/microsim/MSFrame.cpp
+++ src/microsim/MSFrame.cpp
@@ -246,10 +246,6 @@ MSFrame::fillOptions() {
     oc.doRegister("lateral-resolution", new Option_Float(-1));
     oc.addDescription("lateral-resolution", "Processing", "Defines the resolution in m when handling lateral positioning within a lane (with -1 all vehicles drive at the center of their lane");
 
-    oc.doRegister("carfollow.model", new Option_String("Krauss"));
-    oc.addDescription("carfollow.model", "Processing", "Select default car following model (Krauss, IDM, ...)");
-    oc.addSynonyme("carfollow.model", "carfollowing.model", false);
-
     // register the processing options
     oc.doRegister("route-steps", 's', new Option_String("200", "TIME"));
     oc.addDescription("route-steps", "Processing", "Load routes for the next number of seconds ahead");
@@ -322,6 +318,10 @@ MSFrame::fillOptions() {
 
     oc.doRegister("default.action-step-length", new Option_Float(0.0));
     oc.addDescription("default.action-step-length", "Processing", "Length of the default interval length between action points for the car-following and lane-change models (in seconds). If not specified, the simulation step-length is used per default. Vehicle- or VType-specific settings override the default. Must be a multiple of the simulation step-length.");
+
+    oc.doRegister("default.carfollowmodel", new Option_String("Krauss"));
+    oc.addDescription("default.carfollowmodel", "Processing", "Select default car following model (Krauss, IDM, ...)");
+    oc.addSynonyme("default.carfollowmodel", "carfollow.model", false);
 
     // pedestrian model
     oc.doRegister("pedestrian.model", new Option_String("striping"));
