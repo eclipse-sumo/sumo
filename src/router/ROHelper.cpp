e53fdc98e8601da98196f962a8c08cacb069ac84
Michael Behrisch <michael.behrisch@dlr.de>
2018-08-16 10:26:06 +0200

    fixing warning, refs #3

--- src/router/ROHelper.cpp
+++ src/router/ROHelper.cpp
@@ -69,14 +69,11 @@ recheckForLoops(ConstROEdgeVector& edges, const ConstROEdgeVector& mandatory) {
     // remove loops at the route's end
     //  (vehicle makes a turnaround to get into the right direction at an already passed node)
     const RONode* end = edges.back()->getToJunction();
-    int firstEnd = (int)edges.size() - 1;
-    for (int i = 0; i < firstEnd; i++) {
-        if (edges[i]->getToJunction() == end) {
-            firstEnd = i;
+    for (int i = 0; i < (int)edges.size() - 1; i++) {
+        if (edges[i]->getToJunction() == end && noMandatory(mandatory, edges.begin() + i + 2, edges.end())) {
+            edges.erase(edges.begin() + i + 2, edges.end());
+            break;
         }
-    }
-    if (firstEnd < (int)edges.size() - 1 && noMandatory(mandatory, edges.begin() + firstEnd + 2, edges.end())) {
-        edges.erase(edges.begin() + firstEnd + 2, edges.end());
     }
 
     // removal of node loops (node occurs twice) is not done because these may occur legitimately
