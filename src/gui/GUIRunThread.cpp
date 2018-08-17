eclipse-sumo/sumo

commit 079d1c13abcbfdcb22b1734c675f8cde938903f9

now traci.load flushes output files

src/sumo_main.cpp
@@ -35,6 +35,7 @@
 #include <netload/NLBuilder.h>
 #include <utils/options/OptionsCont.h>
 #include <utils/options/OptionsIO.h>
+#include <utils/iodevices/OutputDevice.h>
 #include <utils/common/MsgHandler.h>
 #include <utils/common/SystemFrame.h>
 #include <utils/common/UtilExceptions.h>
@@ -68,6 +69,7 @@ main(int argc, char** argv) {
             MSNet* net = NLBuilder::init();
             if (net != nullptr) {
                 state = net->simulate(string2time(oc.getString("begin")), string2time(oc.getString("end")));
+                OutputDevice::closeAll();
                 delete net;
             } else {
                 break;

commit 33ae46d0d83558addd780bef8c3aff40b8018e9a

fixing crash introduced in [079d1c13abcbfdcb22b1734c675f8cde938903f9]

src/gui/GUIRunThread.cpp
@@ -193,7 +193,8 @@ GUIRunThread::makeStep() {
                     WRITE_MESSAGE("Reason: " + MSNet::getStateMessage(state));
                     e = new GUIEvent_SimulationEnded(state, myNet->getCurrentTimeStep() - DELTA_T);
                     // ensure that files are closed (deleteSim is called a bit later by the gui thread)
-                    OutputDevice::closeAll();
+                    // MSNet destructor may trigger MsgHandler (via routing device cleanup). Closing output devices here is not safe
+                    // OutputDevice::closeAll();
                     myHaveSignaledEnd = true;
                 }
                 break;

src/sumo_main.cpp
@@ -69,8 +69,8 @@ main(int argc, char** argv) {
             MSNet* net = NLBuilder::init();
             if (net != nullptr) {
                 state = net->simulate(string2time(oc.getString("begin")), string2time(oc.getString("end")));
-                OutputDevice::closeAll();
                 delete net;
+                OutputDevice::closeAll();
             } else {
                 break;
             }