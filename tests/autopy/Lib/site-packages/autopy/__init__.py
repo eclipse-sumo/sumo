02b49cb18a88cae2275dd256dfb08365a3fc019d
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-09-10 08:50:04 +0200

    Added autopy as executable. Refs #4515

---
+++ tests/autopy/Lib/site-packages/autopy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+AutoPy is a simple, cross-platform GUI automation library for Python.
+"""
+
+from . import alert, bitmap, color, key, mouse, screen
+
+__author__ = "Michael Sanders"
+__version__ = "1.0.1"
+__all__ = ["alert", "bitmap", "color", "key", "mouse", "screen"]
