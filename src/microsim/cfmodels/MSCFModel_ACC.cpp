eclipse-sumo/sumo

commit 457180afc59f0e04db72c8fd130bfb0c2adc8bbb

adding emergency speed override refs #4551

src/microsim/cfmodels/MSCFModel_ACC.cpp
@@ -58,6 +58,9 @@
 #define DEFAULT_CA_GAIN_SPACE 0.8
 #define DEFAULT_CA_GAIN_SPEED 0.23
 
+// override followSpeed when deemed unsafe by the given margin (the value was selected to reduce the number of necessary interventions)
+#define DEFAULT_EMERGENCY_OVERRIDE_THRESHOLD 2.0
+
 /// @todo: add attributes for myCollisionAvoidanceGainSpeed and myCollisionAvoidanceGainSpace
 
 // ===========================================================================
@@ -81,9 +84,16 @@ MSCFModel_ACC::~MSCFModel_ACC() {}
 
 
 double
-MSCFModel_ACC::followSpeed(const MSVehicle* const veh, double speed, double gap2pred, double predSpeed, double /* predMaxDecel */, const MSVehicle* const /* pred */) const {
+MSCFModel_ACC::followSpeed(const MSVehicle* const veh, double speed, double gap2pred, double predSpeed, double /* predMaxDecel */, const MSVehicle* const pred) const {
     const double desSpeed = MIN2(veh->getLane()->getSpeedLimit(), veh->getMaxSpeed());
-    return _v(veh, gap2pred, speed, predSpeed, desSpeed, true);
+    const double vACC = _v(veh, gap2pred, speed, predSpeed, desSpeed, true);
+    const double vSafe = maximumSafeFollowSpeed(gap2pred, speed, predSpeed, pred->getCarFollowModel().getMaxDecel());
+    if (vSafe + DEFAULT_EMERGENCY_OVERRIDE_THRESHOLD < vACC) {
+        //ACCVehicleVariables* vars = (ACCVehicleVariables*)veh->getCarFollowVariables();
+        //std::cout << SIMTIME << " veh=" << veh->getID() << " v=" << speed << " vL=" << predSpeed << " gap=" << gap2pred << " vACC=" << vACC << " vSafe=" << vSafe << " cm=" << vars->ACC_ControlMode << "\n";
+        return vSafe + DEFAULT_EMERGENCY_OVERRIDE_THRESHOLD;
+    }
+    return vACC;
 }
 
 

commit a947ab79f3e0c35d825df6234e6de7d60bc5e900

fixing crash, introduced in [457180afc59f0e04]. refs #4551

src/microsim/cfmodels/MSCFModel_ACC.cpp
@@ -83,10 +83,10 @@ MSCFModel_ACC::~MSCFModel_ACC() {}
 
 
 double
-MSCFModel_ACC::followSpeed(const MSVehicle* const veh, double speed, double gap2pred, double predSpeed, double /* predMaxDecel */, const MSVehicle* const pred) const {
+MSCFModel_ACC::followSpeed(const MSVehicle* const veh, double speed, double gap2pred, double predSpeed, double predMaxDecel, const MSVehicle* const pred) const {
     const double desSpeed = MIN2(veh->getLane()->getSpeedLimit(), veh->getMaxSpeed());
     const double vACC = _v(veh, gap2pred, speed, predSpeed, desSpeed, true);
-    const double vSafe = maximumSafeFollowSpeed(gap2pred, speed, predSpeed, pred->getCarFollowModel().getMaxDecel());
+    const double vSafe = maximumSafeFollowSpeed(gap2pred, speed, predSpeed, predMaxDecel);
     if (vSafe + DEFAULT_EMERGENCY_OVERRIDE_THRESHOLD < vACC) {
         //ACCVehicleVariables* vars = (ACCVehicleVariables*)veh->getCarFollowVariables();
         //std::cout << SIMTIME << " veh=" << veh->getID() << " v=" << speed << " vL=" << predSpeed << " gap=" << gap2pred << " vACC=" << vACC << " vSafe=" << vSafe << " cm=" << vars->ACC_ControlMode << "\n";