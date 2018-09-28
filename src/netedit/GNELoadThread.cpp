eclipse-sumo/sumo

commit 00822c50b78ce5934eab5d491245af7d67c49adc

fix #4634

src/netedit/GNELoadThread.cpp
@@ -86,10 +86,12 @@ GNELoadThread::run() {
 
     // try to load the given configuration
     OptionsCont& oc = OptionsCont::getOptions();
-    oc.clear();
-    if (!initOptions()) {
-        submitEndAndCleanup(net);
-        return 0;
+    if (myFile != "") {
+        oc.clear();
+        if (!initOptions()) {
+            submitEndAndCleanup(net);
+            return 0;
+        }
     }
     MsgHandler::initOutputOptions();
     if (!(NIFrame::checkOptions() &&

src/netedit/GNEApplicationWindow.cpp
@@ -806,7 +806,7 @@ GNEApplicationWindow::onCmdOpenForeign(FXObject*, FXSelector, void*) {
 
         if (wizard->execute()) {
             NIFrame::checkOptions(); // needed to set projection parameters
-            loadConfigOrNet(file, false, false, false);
+            loadConfigOrNet("", false, false, false);
         }
     }
     return 1;

commit 5f1342a0b646911fa079cc1f1b5a5dfc50ddd548

fixed output file when loading network via commandline option (broken in [00822c50b78ce]. refs 4634

src/netedit/GNELoadThread.cpp
@@ -86,7 +86,7 @@ GNELoadThread::run() {
 
     // try to load the given configuration
     OptionsCont& oc = OptionsCont::getOptions();
-    if (myFile != "") {
+    if (myFile != "" || oc.getString("sumo-net-file") != "") {
         oc.clear();
         if (!initOptions()) {
             submitEndAndCleanup(net);