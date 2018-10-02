f3cfd534a9fed353b119a3d0fdc6556d8e4459d3
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-10-02 08:58:52 +0200

    Added files for netedit E2 multilane detectors. Refs #3045 and #2909

---
+++ tests/netedit/additionalElements/e2MultiLaneDetector/delete/test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to additional mode
+netedit.additionalMode()
+
+# select E2
+netedit.changeAdditional("e2Detector")
+
+# create E2
+netedit.leftClick(referencePosition, 250, 250)
+
+# Change to delete
+netedit.deleteMode()
+
+# delete created E2
+netedit.leftClick(referencePosition, 250, 250)
+
+# delete loaded E2
+netedit.leftClick(referencePosition, 450, 250)
+
+# delete lane with the second loaded E2
+netedit.leftClick(referencePosition, 200, 200)
+
+# Check undo
+netedit.undo(referencePosition, 3)
+
+# Change to delete
+netedit.deleteMode()
+
+# disble 'Automatically delete additionals'
+netedit.changeAutomaticallyDeleteAdditionals(referencePosition)
+
+# try to delete lane with the second loaded E2 (doesn't allowed)
+netedit.leftClick(referencePosition, 200, 200)
+
+# wait warning
+netedit.waitAutomaticallyDeleteAdditionalsWarning()
+
+# check redo
+netedit.redo(referencePosition, 3)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
---
+++ tests/netedit/additionalElements/e2MultiLaneDetector/move/test.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select E2
+netedit.changeAdditional("e2Detector")
+
+# create E2
+netedit.leftClick(referencePosition, 210, 250)
+
+# change to move mode
+netedit.moveMode()
+
+# move E2 to left
+netedit.moveElement(referencePosition, 120, 250, 30, 250)
+
+# move back
+netedit.moveElement(referencePosition, 50, 250, 120, 250)
+
+# move E2 to right
+netedit.moveElement(referencePosition, 130, 250, 250, 250)
+
+# move back
+netedit.moveElement(referencePosition, 260, 250, 120, 250)
+
+# move E2 to left overpassing lane
+netedit.moveElement(referencePosition, 130, 250, -150, 250)
+
+# move back
+netedit.moveElement(referencePosition, -80, 250, 120, 250)
+
+# move E2 to right overpassing lane
+netedit.moveElement(referencePosition, 130, 250, 480, 250)
+
+# move back to another different position of initial
+netedit.moveElement(referencePosition, 500, 250, 300, 250)
+
+# Check undos and redos
+netedit.undo(referencePosition, 10)
+netedit.redo(referencePosition, 10)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
