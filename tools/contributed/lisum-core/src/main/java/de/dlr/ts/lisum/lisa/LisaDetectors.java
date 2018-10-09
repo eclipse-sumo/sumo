3b6ea652277b9857f408c9165783716dd74ad7ee
namdre <jakob.erdmann@dlr.de>
2018-10-09 13:14:05 +0200

    Java 7 compatibility

--- tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/lisa/LisaDetectors.java
+++ tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/lisa/LisaDetectors.java
@@ -1,4 +1,4 @@
-/****************************************************************************/
+/** ************************************************************************* */
 // Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
 // Copyright (C) 2016-2018 German Aerospace Center (DLR) and others.
 // This program and the accompanying materials
@@ -6,140 +6,130 @@
 // which accompanies this distribution, and is available at
 // http://www.eclipse.org/legal/epl-v20.html
 // SPDX-License-Identifier: EPL-2.0
-/****************************************************************************/
+/** ************************************************************************* */
 /// @file    Constants.java
 /// @author  Maximiliano Bottazzi
 /// @date    2016
 /// @version $Id$
 ///
 //
-/****************************************************************************/
+/** ************************************************************************* */
 package de.dlr.ts.lisum.lisa;
 
 import de.dlr.ts.commons.logger.DLRLogger;
 import de.dlr.ts.lisum.interfaces.DetectorInterface;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  *
- * @author @author <a href="mailto:maximiliano.bottazzi@dlr.de">Maximiliano Bottazzi</a>
+ * @author @author <a href="mailto:maximiliano.bottazzi@dlr.de">Maximiliano
+ * Bottazzi</a>
  */
-class LisaDetectors
-{
+class LisaDetectors {
+
     private final List<LisaDetector> detectors = new ArrayList<>();
 
-    
     /**
-     * 
+     *
      */
-    public LisaDetectors()
-    {
+    public LisaDetectors() {
     }
 
     /**
-     * 
+     *
      */
-    public void sort()
-    {
+    public void sort() {
         Collections.sort(detectors);
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder("Detectors").append("\n");
-        
-        for (LisaDetector det : detectors)
+
+        for (LisaDetector det : detectors) {
             sb.append(det).append("\n");
-                
+        }
+
         return sb.toString();
     }
 
     /**
-     * 
-     * @param cu 
+     *
+     * @param cu
      */
-    void load(LisaConfigurationFiles.ControlUnit cu)
-    {
-        for (LisaConfigurationFiles.ControlUnit.Detector de : cu.detectors)
-        {
+    void load(LisaConfigurationFiles.ControlUnit cu) {
+        for (LisaConfigurationFiles.ControlUnit.Detector de : cu.detectors) {
             DLRLogger.info(this, "Adding detector: " + de.fullName + " (" + de.objNr + ")");
-            
+
             LisaDetector lisaDetector = new LisaDetector(de.objNr, de.bezeichnung);
-            detectors.add(lisaDetector);        
+            detectors.add(lisaDetector);
         }
     }
-    
+
     /**
-     * 
+     *
      * @param detectorId
-     * @return 
+     * @return
      */
-    public DetectorInterface getDetector(String detectorId)
-    {
-        for (LisaDetector detector : detectors)
-            if(detector.getId().equals(detectorId))
+    public DetectorInterface getDetector(String detectorId) {
+        for (LisaDetector detector : detectors) {
+            if (detector.getId().equals(detectorId)) {
                 return detector;
-        
+            }
+        }
+
         return null;
     }
-    
-    
+
     /**
-     * 
+     *
      */
-    public void clear()
-    {
-        detectors.stream().forEach((detector) ->
-        {
+    public void clear() {
+        for (LisaDetector detector : detectors) {
             detector.clear();
-        });
+        }
     }
-    
-    
+
     /**
-     * 
-     * 
-     * {71/22/(4)14!-75/9!-49}
-     * 71/22/(4)14!-75/9!49
-     * @return 
+     *
+     *
+     * {71/22/(4)14!-75/9!-49} 71/22/(4)14!-75/9!49
+     *
+     * @return
      */
-    public String getLisaString()
-    {
+    public String getLisaString() {
         StringBuilder sb = new StringBuilder();
-    
+
         boolean addIndex = false;
-        for (int i = 0; i < detectors.size(); i++)
-        {
+        for (int i = 0; i < detectors.size(); i++) {
             LisaDetector detector = detectors.get(i);
-            
-            if(detector.isEmpty())
+
+            if (detector.isEmpty()) {
                 addIndex = true;
-            else
-            {
+            } else {
                 sb.append(detector.getLisaString(addIndex)).append("/");
                 addIndex = false;
             }
         }
-        
+
         String ts = sb.toString();
-        
-        if(ts.isEmpty())
+
+        if (ts.isEmpty()) {
             return "";
-        
-        return ts.substring(0, ts.length()-1);
+        }
+
+        return ts.substring(0, ts.length() - 1);
     }
-    
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
-    public int detectorsCount()
-    {
+    public int detectorsCount() {
         return this.detectors.size();
     }
-    
+
 }
