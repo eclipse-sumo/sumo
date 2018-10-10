bbc550702e2fbcd3857f1f047a7825dc1b8d28d4
Maximiliano Bottazzi <maximiliano.bottazzi@dlr.de>
2018-10-10 13:42:23 +0200

    New Setter for configFile in Main

--- tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/Main.java
+++ tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/Main.java
@@ -22,13 +22,14 @@ import java.util.logging.Logger;
  */
 public class Main 
 {   
-    String sumoExec = "sumo";
-    String sumoConfig = null;
-    int sumoPort = 9100;
-    String lisaRestFulServerDir = "localhost";
-    int lisaPort = 9091;    
-    String lisumFile = "";
+    private String sumoExec = "sumo";
+    private String sumoConfig = null;
+    private int sumoPort = 9100;
+    private String lisaRestFulServerDir = "localhost";
+    private int lisaPort = 9091;    
+    private String lisumFile = "";
     private String loggingLevel = "INFO";        
+    
     
     /**
      * 
@@ -47,18 +48,7 @@ public class Main
         sc.addRuntimeOption("-S or -sumocfg  ", "Sumo configuration", "");
         sc.addRuntimeOption("-l or -lisa     ", "lisa RESTful server address", "localhost");
         sc.showSplashScreen();
-        
-        /*
-        if(args.length == 0) {            
-            
-            String aa = "-f D:\\Tmp\\Lisum\\Lisum_Victor_20181002\\lisum.xml -c culo.txt";
-            //String aa = "-c culo.txt";
-            new Main().start(aa.split(" "));
-            
-            return;
-        }
-        */
-        
+                
         new Main().start(args);
     }    
     
@@ -166,7 +156,8 @@ public class Main
             return;
         }
         
-        LisumSimulation ls = new LisumSimulation(sumoExec, sumoConfig, sumoPort, lisaRestFulServerDir, lisaPort);        
+        LisumSimulation ls = new LisumSimulation(sumoExec, sumoPort, lisaRestFulServerDir, lisaPort);        
+        ls.setSumoConfig(sumoConfig);
         ls.load(new File(lisumFile));
      
         ls.initBeforePlay();
