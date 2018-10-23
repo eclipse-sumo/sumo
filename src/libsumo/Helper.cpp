9b7a58290e72c0d3950067c38a9a4760c58facd3
Leonhard Luecken <leonhard.luecken@dlr.de>
2018-10-23 14:07:06 +0200

    [ZL2] More debug output and turn filter corrections

--- src/libsumo/Helper.cpp
+++ src/libsumo/Helper.cpp
@@ -550,19 +550,41 @@ Helper::applySubscriptionFilters(const Subscription& s, std::set<std::string>& o
                 // Get upcoming junctions and vialanes within downstream distance, where foe links exist or at least the link direction is not straight
                 MSLane* lane = v->getLane();
                 std::vector<const MSLink*> links = lane->getUpcomingLinks(v->getPositionOnLane(), downstreamDist, v->getBestLanesContinuation());
-
+#ifdef DEBUG_SURROUNDING
+                std::cout << "Applying turn filter for vehicle '" << v->getID() << "'\n Gathering foes ..." << std::endl;
+#endif
                 // Iterate through junctions and find approaching foes within upstreamDist.
                 for (auto& l : links) {
-                    for(auto& foeLink : l->getFoeLinks()) {
+#ifdef DEBUG_SURROUNDING
+                    std::cout << "  On junction '" << l->getJunction()->getID() << "' (no. foe links = " << l->getFoeLinks().size() << "):" << std::endl;
+#endif
+                    for(auto& foeLane : l->getFoeLanes()) {
+                        // Check vehicles approaching the entry link corresponding to this lane
+                        const MSLink* foeLink = foeLane->getEntryLink();
                         for (auto& vi : foeLink->getApproaching()) {
                             if (vi.second.dist <= upstreamDist) {
+#ifdef DEBUG_SURROUNDING
+                                std::cout << "    Approaching from foe-lane '" << vi.first->getID() << "'" << std::endl;
+#endif
                                 vehs.insert(vehs.end(), dynamic_cast<const MSVehicle*>(vi.first));
                             }
                         }
+                        // add vehicles currently on the junction
+                        for (const MSVehicle* foe : foeLane->getVehiclesSecure()) {
+                            vehs.insert(vehs.end(), foe);
+                        }
+                        foeLane->releaseVehicles();
                     }
                 }
             }
-
+#ifdef DEBUG_SURROUNDING
+                std::cout << SIMTIME << " applySubscriptionFilters() for veh '" << v->getID() << "'. Found the following vehicles:\n";
+                for (auto veh : vehs) {
+                    if (veh != nullptr) {
+                        std::cout << "  '" << veh->getID() << "' on lane '" << veh->getLane()->getID() << "'\n";
+                    }
+                }
+#endif
         } else {
             // No maneuver filters requested, but only lanes filter (directly, or indirectly by specifying downstream or upstream distance)
             assert(filterLanes.size() > 0);
@@ -642,15 +664,15 @@ Helper::applySubscriptionFilters(const Subscription& s, std::set<std::string>& o
                         }
                     }
                 }
-
-            }
-
 #ifdef DEBUG_SURROUNDING
-            std::cout << SIMTIME << " applySubscriptionFilters() for veh '" << v->getID() << "'. Found the following vehicles:\n";
-            for (auto veh : vehs) {
-                std::cout << "  '" << veh->getID() << "' on lane '" << veh->getLane()->getID() << "'\n";
-            }
+                std::cout << SIMTIME << " applySubscriptionFilters() for veh '" << v->getID() << "'. Found the following vehicles:\n";
+                for (auto veh : vehs) {
+                    if (veh != nullptr) {
+                        std::cout << "  '" << veh->getID() << "' on lane '" << veh->getLane()->getID() << "'\n";
+                    }
+                }
 #endif
+            }
 
             // filter vehicles in vehs by class and/or type if requested
             if (s.activeFilters & SUBS_FILTER_VCLASS) {
