98c3dd2e50b913a4d7df8b0e53be45e4fce3d835
Leonhard Luecken <leonhard.luecken@dlr.de>
2018-11-01 15:43:30 +0100

    Added ToC Device output tests, refs #4750

---
+++ tests/complex/sumo/ToCDevice/output/CF_situation1/runner.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Lena Kalleske
+# @author  Daniel Krajzewicz
+# @author  Michael Behrisch
+# @author  Jakob Erdmann
+# @author  Leonhard Luecken
+# @date    2009-03-26
+# @version $Id$
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+
+# we need to import python modules from the $SUMO_HOME/tools directory
+try:
+    sys.path.append(os.path.join(os.path.dirname(
+        __file__), '..', '..', '..', '..', '..', "tools"))  # tutorial in tests
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
+        os.path.dirname(__file__), "..", "..", "..", "..")), "tools"))  # tutorial in docs
+    from sumolib import checkBinary  # noqa
+except ImportError:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+import traci  # noqa
+
+ToC_vehicle = "ToC_veh"
+timeTillMRM = 1
+
+
+def run():
+    """execute the TraCI control loop"""
+    # Keep follower behind the ToC_vehicle
+    traci.vehicle.changeLane("follower", 0, 200)
+
+    step = 0
+    while step < 500:
+        traci.simulationStep()
+        if step == 10:
+            requestToC(ToC_vehicle, timeTillMRM)
+            t = traci.simulation.getTime()
+            print("Requested ToC of veh0 at t=%s (until t=%s)" % (t, t + timeTillMRM))
+        printToCParams(ToC_vehicle, True)
+        step += 1
+
+
+def requestToC(vehID, timeTillMRM):
+    traci.vehicle.setParameter(vehID, "device.toc.requestToC", str(timeTillMRM))
+
+
+def printToCParams(vehID, only_dynamic=False):
+    holder = traci.vehicle.getParameter(vehID, "device.toc.holder")
+    manualType = traci.vehicle.getParameter(vehID, "device.toc.manualType")
+    automatedType = traci.vehicle.getParameter(vehID, "device.toc.automatedType")
+    responseTime = traci.vehicle.getParameter(vehID, "device.toc.responseTime")
+    recoveryRate = traci.vehicle.getParameter(vehID, "device.toc.recoveryRate")
+    initialAwareness = traci.vehicle.getParameter(vehID, "device.toc.initialAwareness")
+    mrmDecel = traci.vehicle.getParameter(vehID, "device.toc.mrmDecel")
+    currentAwareness = traci.vehicle.getParameter(vehID, "device.toc.currentAwareness")
+    state = traci.vehicle.getParameter(vehID, "device.toc.state")
+    speed = traci.vehicle.getSpeed(vehID)
+
+    print("time step %s" % traci.simulation.getCurrentTime())
+    print("ToC device infos for vehicle '%s'" % vehID)
+    if not only_dynamic:
+        print("Static parameters:")
+        print("  holder = %s" % holder)
+        print("  manualType = %s" % manualType)
+        print("  automatedType = %s" % automatedType)
+        print("  responseTime = %s" % responseTime)
+        print("  recoveryRate = %s" % recoveryRate)
+        print("  initialAwareness = %s" % initialAwareness)
+        print("  mrmDecel = %s" % mrmDecel)
+        print("Dynamic parameters:")
+    print("  currentAwareness = %s" % currentAwareness)
+    print("  currentSpeed = %s" % speed)
+    print("  state = %s" % state)
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_false",
+                         default=True, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-n", "input_net.net.xml", "-r", "input_routes.rou.xml", "-a",
+                 "input_additional.add.xml", "--fcd-output", "fcd.xml",
+                 "--no-step-log", "true",
+                 "--default.speeddev", "0"])
+
+    # Wait until the vehicle enters
+    while ToC_vehicle not in traci.vehicle.getIDList():
+        traci.simulationStep()
+    printToCParams(ToC_vehicle)
+
+    run()
+
+    traci.close()
+    sys.stdout.flush()
