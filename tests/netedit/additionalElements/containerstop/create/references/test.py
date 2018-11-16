c3ef53f1da405403b8c13321e6985ffc7bf25435
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-11-16 13:46:23 +0100

    Updated netedit containerstop test. Refs #3045, #4535 and #4536

---
+++ tests/netedit/additionalElements/containerstop/create/references/test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to additional mode
+netedit.additionalMode()
+
+# select containerStop
+netedit.changeAdditional("containerStop")
+
+# create containerStop in mode "reference left"
+netedit.leftClick(referencePosition, 240, 250)
+
+# change reference to right
+netedit.modifyAdditionalDefaultValue(6, "reference right")
+
+# create containerStop in mode "reference right"
+netedit.leftClick(referencePosition, 230, 250)
+
+# change reference to center
+netedit.modifyAdditionalDefaultValue(6, "reference center")
+
+# create containerStop in mode "reference center"
+netedit.leftClick(referencePosition, 425, 250)
+
+# Check undo redo
+netedit.undo(referencePosition, 3)
+netedit.redo(referencePosition, 3)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
