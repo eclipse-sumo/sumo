02be78154eb752f37f810506220a1fbbe32c7903
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-11-20 15:20:37 +0100

    Updated netedit e2Detector tests. Refs #3045, #4535 and #4536

---
+++ tests/netedit/additionalElements/e2detector/inspect/file/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select E2
+netedit.changeAdditional("e2Detector")
+
+# create E2
+netedit.leftClick(referencePosition, 250, 150)
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect first E2
+netedit.leftClick(referencePosition, 260, 150)
+
+# Change parameter file with an non valid value
+netedit.modifyAttribute(9, "%;;%%&%%%&&")
+
+# Change parameter file with an empty value
+netedit.modifyAttribute(9, "")
+
+# Change parameter file with a valid value
+netedit.modifyAttribute(9, "myOwnOutput.txt")
+
+# Check undos and redos
+netedit.undo(referencePosition, 3)
+netedit.redo(referencePosition, 3)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
---
+++ tests/netedit/additionalElements/e2detector/inspect_selection/length/test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvert()
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect E2s
+netedit.leftClick(referencePosition, 150, 250)
+
+# Change parameter lenght with a non valid value(dummy)
+netedit.modifyAttribute(3, "dummyLength")
+
+# Change parameter lenght with a non valid value (negative)
+netedit.modifyAttribute(3, "-5")
+
+# Change parameter lenght with a non valid value (0)
+netedit.modifyAttribute(3, "0")
+
+# Change parameter lenght with a valid value (> lane lenght)
+netedit.modifyAttribute(3, "500")
+
+# Change parameter lenght with a valid value
+netedit.modifyAttribute(3, "5")
+
+# Check undos and redos
+netedit.undo(referencePosition, 4)
+netedit.redo(referencePosition, 4)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
