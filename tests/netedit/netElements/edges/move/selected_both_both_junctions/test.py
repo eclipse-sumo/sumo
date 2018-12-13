c0d3e36738df926ced390e57d3057106c316f680
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2018-12-13 12:12:27 +0100

    Added netedit test edge/move. Refs #3045

---
+++ tests/netedit/netElements/edges/move/selected_both_both_junctions/test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to select mode
+netedit.selectMode()
+
+# select edge
+netedit.leftClick(referencePosition, 220, 250)
+
+# select second 
+netedit.leftClick(referencePosition, 220, 220)
+
+# select both junctions
+netedit.leftClick(referencePosition, 75, 220)
+netedit.leftClick(referencePosition, 570, 220)
+
+# change to move mode
+netedit.moveMode()
+
+# move both edges
+netedit.moveElement(referencePosition, 220, 250, 220, 350)
+
+# Check undos and redos
+netedit.undo(referencePosition, 2)
+netedit.redo(referencePosition, 2)
+
+# save additionals
+netedit.saveAdditionals()
+
+# save network
+netedit.saveNetwork()
+
+# quit netedit
+netedit.quit(neteditProcess)
