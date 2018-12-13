f366da5de1f680f37ffdefc601444963e2fdef55
namdre <jakob.erdmann@dlr.de>
2018-12-13 14:47:04 +0100

    added test refs #4920

---
+++ tests/complex/traci/pythonApi/bugs/ticket4920/runner.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2018 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Mirko Barthauer (Technische Universitaet Braunschweig)
+# @date    2018-09-27
+# @version $Id$
+
+import os
+import sys
+
+SUMO_HOME = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..")
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", SUMO_HOME), "tools"))
+
+import traci
+import sumolib
+
+sumoBinary = sumolib.checkBinary('sumo')
+traci.start([sumoBinary,
+             "-n", "input_net.net.xml", 
+             "-r", "input_routes.rou.xml",
+             "-b", "2147400", #  ~ 2**31 / 1000
+             "--no-step-log",
+             ])
+
+for i in range(100):
+    traci.simulationStep()
+    traci.vehicle.add(str(i), "r0")
+
+traci.close()
+
