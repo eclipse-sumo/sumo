11881ecb5f9d4d84177b11af04186039478bb6bc
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-01-10 09:36:42 +0100

    Added modul RouteModeSelector. Refs #4976

--- src/netedit/frames/GNERouteFrame.cpp
+++ src/netedit/frames/GNERouteFrame.cpp
@@ -23,26 +23,129 @@
 
 #include <utils/gui/div/GUIDesigns.h>
 #include <utils/gui/windows/GUIAppEnum.h>
-#include <netedit/netelements/GNEConnection.h>
-#include <netedit/netelements/GNECrossing.h>
-#include <netedit/netelements/GNEEdge.h>
-#include <netedit/netelements/GNEJunction.h>
-#include <netedit/netelements/GNELane.h>
-#include <netedit/additionals/GNETAZ.h>
 #include <netedit/GNENet.h>
-#include <netedit/additionals/GNEPoly.h>
 #include <netedit/GNEUndoList.h>
 #include <netedit/GNEViewParent.h>
 
 #include "GNERouteFrame.h"
-#include "GNEAdditionalFrame.h"
+
+// ===========================================================================
+// FOX callback mapping
+// ===========================================================================
+
+FXDEFMAP(GNERouteFrame::RouteModeSelector) RouteModeSelectorMap[] = {
+    FXMAPFUNC(SEL_COMMAND, MID_GNE_SET_TYPE,    GNERouteFrame::RouteModeSelector::onCmdSelectRouteMode),
+};
+
+// Object implementation
+FXIMPLEMENT(GNERouteFrame::RouteModeSelector,             FXGroupBox,         RouteModeSelectorMap,                ARRAYNUMBER(RouteModeSelectorMap))
+
 
 // ===========================================================================
 // method definitions
 // ===========================================================================
 
+// ---------------------------------------------------------------------------
+// GNERouteFrame::RouteModeSelector - methods
+// ---------------------------------------------------------------------------
+
+GNERouteFrame::RouteModeSelector::RouteModeSelector(GNERouteFrame* routeFrameParent) :
+    FXGroupBox(routeFrameParent->myContentFrame, "Route mode", GUIDesignGroupBoxFrame),
+    myRouteFrameParent(routeFrameParent),
+    myCurrentRouteMode(ROUTEMODE_EDGETOEDGE) {
+    // first fill myRouteModesStrings
+    myRouteModesStrings.push_back(std::make_pair(ROUTEMODE_EDGETOEDGE, "edge to edge"));
+    myRouteModesStrings.push_back(std::make_pair(ROUTEMODE_MAXVELOCITY, "maximum velocity"));
+    myRouteModesStrings.push_back(std::make_pair(ROUTEMODE_MINIMUMLENGHT, "minimum lenght"));
+    // Create FXComboBox
+    myTypeMatchBox = new FXComboBox(this, GUIDesignComboBoxNCol, this, MID_GNE_SET_TYPE, GUIDesignComboBox);
+    // fill myTypeMatchBox with route modes
+    for (const auto &i : myRouteModesStrings) {
+        myTypeMatchBox->appendItem(i.second.c_str());
+    }
+    // Set visible items
+    myTypeMatchBox->setNumVisible((int)myTypeMatchBox->getNumItems());
+    // RouteModeSelector is always shown
+    show();
+}
+
+
+GNERouteFrame::RouteModeSelector::~RouteModeSelector() {}
+
+
+const GNERouteFrame::RouteMode&
+GNERouteFrame::RouteModeSelector::getCurrenRouteMode() const {
+    return myCurrentRouteMode;
+}
+
+
+void
+GNERouteFrame::RouteModeSelector::setCurrentRouteMode(RouteMode routemode) {
+    // make sure that route isn't invalid
+    if(routemode != ROUTEMODE_INVALID) {
+        // restore color
+        myTypeMatchBox->setTextColor(FXRGB(0, 0, 0));
+        // set current route mode
+        myCurrentRouteMode = routemode;
+        // set item in myTypeMatchBox
+        for (int i = 0; i < (int)myRouteModesStrings.size(); i++) {
+            if (myRouteModesStrings.at(i).first == myCurrentRouteMode) {
+                myTypeMatchBox->setCurrentItem(i);
+            }
+        }
+        // enable moduls
+        /**
+            enable moduls
+        **/
+    } else {
+        // hide all moduls if route mode isnt' valid
+        /**
+            enable moduls
+        **/
+    }
+}
+
+
+long
+GNERouteFrame::RouteModeSelector::onCmdSelectRouteMode(FXObject*, FXSelector, void*) {
+    // Check if value of myTypeMatchBox correspond of an allowed additional tags
+    for (const auto &i : myRouteModesStrings) {
+        if (i.second == myTypeMatchBox->getText().text()) {
+            // set color of myTypeMatchBox to black (valid)
+            myTypeMatchBox->setTextColor(FXRGB(0, 0, 0));
+            // Set new current type
+            myCurrentRouteMode = i.first;
+            // enable moduls
+            /**
+                enable moduls
+            **/
+            // Write Warning in console if we're in testing mode
+            WRITE_DEBUG(("Selected item '" + myTypeMatchBox->getText() + "' in RouteModeSelector").text());
+            return 1;
+        }
+    }
+    // if additional name isn't correct, set SUMO_TAG_NOTHING as current type
+    myCurrentRouteMode = ROUTEMODE_INVALID;
+    // hide all moduls if route mode isnt' valid
+    /**
+        enable moduls
+    **/
+    // set color of myTypeMatchBox to red (invalid)
+    myTypeMatchBox->setTextColor(FXRGB(255, 0, 0));
+    // Write Warning in console if we're in testing mode
+    WRITE_DEBUG("Selected invalid item in RouteModeSelector");
+    return 1;
+}
+
+// ---------------------------------------------------------------------------
+// GNERouteFrame - methods
+// ---------------------------------------------------------------------------
+
 GNERouteFrame::GNERouteFrame(FXHorizontalFrame* horizontalFrameParent, GNEViewNet* viewNet) :
     GNEFrame(horizontalFrameParent, viewNet, "Routes") {
+
+    // create route mode Selector modul for additionals
+    myRouteModeSelector = new RouteModeSelector(this);
 }
 
 
--- src/netedit/frames/GNERouteFrame.h
+++ src/netedit/frames/GNERouteFrame.h
@@ -33,6 +33,59 @@ class GNERouteFrame : public GNEFrame {
 
 public:
 
+    /// @brief route creation modes
+    enum RouteMode {
+        ROUTEMODE_INVALID,          // invalid RouteMode
+        ROUTEMODE_EDGETOEDGE,       // Create route clicking over consecutive edges
+        ROUTEMODE_MAXVELOCITY,      // Give start and end edges, and calculate route with maximum velocity
+        ROUTEMODE_MINIMUMLENGHT,    // Give start and end edges, and calculate route with minimum edge lenghts
+    };
+
+    // ===========================================================================
+    // class RouteModeSelector
+    // ===========================================================================
+
+    class RouteModeSelector : protected FXGroupBox {
+        /// @brief FOX-declaration
+        FXDECLARE(GNERouteFrame::RouteModeSelector)
+
+    public:
+        /// @brief constructor
+        RouteModeSelector(GNERouteFrame* routeFrameParent);
+
+        /// @brief destructor
+        ~RouteModeSelector();
+
+        /// @brief get current route mode
+        const RouteMode &getCurrenRouteMode() const;
+
+        /// @brief set current route mode type manually
+        void setCurrentRouteMode(RouteMode routemode);
+
+        /// @name FOX-callbacks
+        /// @{
+        /// @brief Called when the user select another route mode in ComboBox
+        long onCmdSelectRouteMode(FXObject*, FXSelector, void*);
+        /// @}
+
+    protected:
+        /// @brief FOX needs this
+        RouteModeSelector() {}
+
+    private:
+        /// @brief pointer to Frame Parent
+        GNERouteFrame* myRouteFrameParent;
+
+        /// @brief comboBox with the list of route modes
+        FXComboBox* myTypeMatchBox;
+
+        /// @brief current selected route mode
+        RouteMode myCurrentRouteMode;
+
+        /// @brief list of Route modes that will be shown in Match Box
+        std::vector<std::pair<RouteMode, std::string> > myRouteModesStrings;
+    };
+
     /**@brief Constructor
      * @brief parent FXHorizontalFrame in which this GNEFrame is placed
      * @brief viewNet viewNet that uses this GNEFrame
@@ -49,6 +102,9 @@ public:
     void hide();
 
 private:
+
+    /// @brief item selector
+    RouteModeSelector* myRouteModeSelector;
 
 };
 
