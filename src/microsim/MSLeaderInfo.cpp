eclipse-sumo/sumo

commit f8609593f747188b6d1204ac36208c959d8697c6

Fixed #5170

src/microsim/MSLeaderInfo.cpp
@@ -41,12 +41,14 @@
 // MSLeaderInfo member method definitions
 // ===========================================================================
 MSLeaderInfo::MSLeaderInfo(const MSLane* lane, const MSVehicle* ego, double latOffset) :
-    myWidth(lane->getWidth()),
     myVehicles(MAX2(1, int(ceil(myWidth / MSGlobals::gLateralResolution))), (MSVehicle*)nullptr),
     myFreeSublanes((int)myVehicles.size()),
     egoRightMost(-1),
     egoLeftMost(-1),
     myHasVehicles(false) {
+    if (lane != nullptr) {
+        myWidth = lane->getWidth();
+    }
     if (ego != nullptr) {
         getSubLanes(ego, latOffset, egoRightMost, egoLeftMost);
         // filter out sublanes not of interest to ego
@@ -204,6 +206,7 @@ MSLeaderDistanceInfo::MSLeaderDistanceInfo(const CLeaderDist& cLeaderDist, const
     myDistances(1, cLeaderDist.second) {
     assert(myVehicles.size() == 1);
     myVehicles[0] = cLeaderDist.first;
+    myHasVehicles = true;
 }
 
 MSLeaderDistanceInfo::~MSLeaderDistanceInfo() { }

src/libsumo/Vehicle.cpp
@@ -632,6 +632,94 @@ Vehicle::getParameter(const std::string& vehicleID, const std::string& key) {
 }
 
 
+std::map<const MSVehicle*, double>
+Vehicle::getNeighbors(const std::string& vehicleID, const int mode) {
+    int dir = (1 & mode) ? -1 : 1;
+    bool queryLeaders = (2 & mode);
+    bool blockersOnly = (4 & mode);
+
+    MSVehicle* veh = getVehicle(vehicleID);
+    std::map<const MSVehicle*, double> neighs;
+    auto& lcm = veh->getLaneChangeModel();
+
+    if (blockersOnly) {
+        // Check if a blocking neigh exists in the given direction
+        bool blocked = false;
+        if (dir == 1) {
+            if (queryLeaders) {
+                blocked = (lcm.getOwnState() & LCA_BLOCKED_BY_RIGHT_LEADER);
+            } else {
+                blocked = (lcm.getOwnState() & LCA_BLOCKED_BY_RIGHT_FOLLOWER);
+            }
+        } else {
+            if (queryLeaders) {
+                blocked = (lcm.getOwnState() & LCA_BLOCKED_BY_LEFT_LEADER);
+            } else {
+                blocked = (lcm.getOwnState() & LCA_BLOCKED_BY_LEFT_FOLLOWER);
+            }
+        }
+        if (!blocked) {
+            // Not blocked => return empty vector
+            return neighs;
+        }
+    }
+
+    const std::shared_ptr<MSLeaderDistanceInfo> res = queryLeaders ? lcm.getLeaders(dir) : lcm.getFollowers(dir);
+    if (res != nullptr && res->hasVehicles()) {
+        auto distIt = begin(res->getDistances());
+        auto vehIt = begin(res->getVehicles());
+        while (distIt != end(res->getDistances())) {
+            if (*vehIt != nullptr) {
+                neighs[*vehIt] = *distIt;
+            }
+            ++vehIt;
+            ++distIt;
+        }
+    }
+    return neighs;
+}
+
+
+std::map<const MSVehicle*, double>
+Vehicle::getRightFollowers(const std::string& vehicleID, bool blockingOnly) {
+    if (blockingOnly) {
+        return getNeighbors(vehicleID, 5);
+    } else {
+        return getNeighbors(vehicleID, 1);
+    }
+}
+
+
+std::map<const MSVehicle*, double>
+Vehicle::getRightLeaders(const std::string& vehicleID, bool blockingOnly) {
+    if (blockingOnly) {
+        return getNeighbors(vehicleID, 7);
+    } else {
+        return getNeighbors(vehicleID, 3);
+    }
+}
+
+
+std::map<const MSVehicle*, double>
+Vehicle::getLeftFollowers(const std::string& vehicleID, bool blockingOnly) {
+    if (blockingOnly) {
+        return getNeighbors(vehicleID, 4);
+    } else {
+        return getNeighbors(vehicleID, 0);
+    }
+}
+
+
+std::map<const MSVehicle*, double>
+Vehicle::getLeftLeaders(const std::string& vehicleID, bool blockingOnly) {
+    if (blockingOnly) {
+        return getNeighbors(vehicleID, 6);
+    } else {
+        return getNeighbors(vehicleID, 2);
+    }
+}
+
+
 const MSVehicleType&
 Vehicle::getVehicleType(const std::string& vehicleID) {
     return getVehicle(vehicleID)->getVehicleType();

src/libsumo/Vehicle.h
@@ -106,6 +106,11 @@ public:
     static std::pair<int, int> getLaneChangeState(const std::string& vehicleID, int direction);
     static double getLastActionTime(const std::string& vehicleID);
     static std::string getParameter(const std::string& vehicleID, const std::string& key);
+    static std::map<const MSVehicle*, double> getNeighbors(const std::string& vehicleID, const int mode);
+    static std::map<const MSVehicle*, double> getRightFollowers(const std::string& vehicleID, bool blockingOnly=false);
+    static std::map<const MSVehicle*, double> getRightLeaders(const std::string& vehicleID, bool blockingOnly=false);
+    static std::map<const MSVehicle*, double> getLeftFollowers(const std::string& vehicleID, bool blockingOnly=false);
+    static std::map<const MSVehicle*, double> getLeftLeaders(const std::string& vehicleID, bool blockingOnly=false);
     static const MSVehicleType& getVehicleType(const std::string& vehicleID);
     /// @}
 

src/microsim/MSLaneChanger.cpp
@@ -265,6 +265,7 @@ MSLaneChanger::change() {
 
     myCandi = findCandidate();
     MSVehicle* vehicle = veh(myCandi);
+    vehicle->getLaneChangeModel().clearNeighbors();
 
     if (vehicle->getLaneChangeModel().isChangingLanes()) {
         return continueChange(vehicle, myCandi);
@@ -1005,6 +1006,9 @@ MSLaneChanger::checkChange(
         vehicle->getLaneChangeModel().setFollowerGaps(neighFollow, secureBackGap);
         vehicle->getLaneChangeModel().setLeaderGaps(neighLead, secureFrontGap);
         vehicle->getLaneChangeModel().setOrigLeaderGaps(leader, secureOrigFrontGap);
+    }
+    if (laneOffset != 0) {
+        vehicle->getLaneChangeModel().saveNeighbors(laneOffset, neighFollow, neighLead);
     }
     return state;
 }

src/microsim/MSLaneChangerSublane.cpp
@@ -108,6 +108,7 @@ MSLaneChangerSublane::change() {
     // variant of change() for the sublane case
     myCandi = findCandidate();
     MSVehicle* vehicle = veh(myCandi);
+    vehicle->getLaneChangeModel().clearNeighbors();
 #ifdef DEBUG_ACTIONSTEPS
     if DEBUG_COND {
     std::cout << "\nCHANGE" << std::endl;
@@ -574,6 +575,9 @@ MSLaneChangerSublane::checkChangeSublane(
     }
 #endif
     vehicle->getLaneChangeModel().saveLCState(laneOffset, oldstate, state);
+    if (laneOffset != 0) {
+        vehicle->getLaneChangeModel().saveNeighbors(laneOffset, neighFollowers, neighLeaders);
+    }
     return state;
 }
 

src/microsim/MSLeaderInfo.h
@@ -97,6 +97,10 @@ public:
         return myHasVehicles;
     }
 
+    const std::vector<const MSVehicle*>& getVehicles() const {
+        return myVehicles;
+    }
+
     /// @brief whether a stopped vehicle is leader
     bool hasStoppedVehicle() const;
 
@@ -162,6 +166,10 @@ public:
 
     /// @brief print a debugging representation
     virtual std::string toString() const;
+
+    const std::vector<double>& getDistances() const {
+        return myDistances;
+    }
 
 protected:
 

src/microsim/lcmodels/MSAbstractLaneChangeModel.cpp
@@ -162,6 +162,72 @@ MSAbstractLaneChangeModel::getManeuverDist() const {
 }
 
 
+void
+MSAbstractLaneChangeModel::saveNeighbors(const int dir, const MSLeaderDistanceInfo& followers, const MSLeaderDistanceInfo& leaders) {
+    if (dir == -1) {
+        myLeftFollowers = std::make_shared<MSLeaderDistanceInfo>(followers);
+        myLeftLeaders = std::make_shared<MSLeaderDistanceInfo>(leaders);
+    } else if (dir == 1) {
+        myRightFollowers = std::make_shared<MSLeaderDistanceInfo>(followers);
+        myRightLeaders = std::make_shared<MSLeaderDistanceInfo>(leaders);
+    } else {
+        // dir \in {-1,1} !
+        assert(false);
+    }
+}
+
+
+void
+MSAbstractLaneChangeModel::saveNeighbors(const int dir, const std::pair<MSVehicle* const, double>& follower, const std::pair<MSVehicle* const, double>& leader) {
+    if (dir == -1) {
+        myLeftFollowers = std::make_shared<MSLeaderDistanceInfo>(follower, myVehicle.getLane());
+        myLeftLeaders = std::make_shared<MSLeaderDistanceInfo>(leader, myVehicle.getLane());
+    } else if (dir == 1) {
+        myRightFollowers = std::make_shared<MSLeaderDistanceInfo>(follower, myVehicle.getLane());
+        myRightLeaders = std::make_shared<MSLeaderDistanceInfo>(leader, myVehicle.getLane());
+    } else {
+        // dir \in {-1,1} !
+        assert(false);
+    }
+}
+
+
+void
+MSAbstractLaneChangeModel::clearNeighbors() {
+    myLeftFollowers = nullptr;
+    myLeftLeaders = nullptr;
+    myRightFollowers = nullptr;
+    myRightLeaders = nullptr;
+}
+
+
+const std::shared_ptr<MSLeaderDistanceInfo>
+MSAbstractLaneChangeModel::getFollowers(const int dir) {
+    if (dir == -1) {
+        return myLeftFollowers;
+    } else if (dir == 1) {
+        return myRightFollowers;
+    } else {
+        // dir \in {-1,1} !
+        assert(false);
+    }
+    return nullptr;
+}
+
+const std::shared_ptr<MSLeaderDistanceInfo>
+MSAbstractLaneChangeModel::getLeaders(const int dir) {
+    if (dir == -1) {
+        return myLeftLeaders;
+    } else if (dir == 1) {
+        return myRightLeaders;
+    } else {
+        // dir \in {-1,1} !
+        assert(false);
+    }
+    return nullptr;
+}
+
+
 bool
 MSAbstractLaneChangeModel::congested(const MSVehicle* const neighLeader) {
     if (neighLeader == nullptr) {

src/microsim/lcmodels/MSAbstractLaneChangeModel.h
@@ -212,6 +212,23 @@ public:
         }
     }
 
+    /// @brief Saves the lane change relevant vehicles, which are currently on neighboring lanes in the given direction
+    ///        (as detected in wantsChangeSublane()). -> SL2015 case
+    void saveNeighbors(const int dir, const MSLeaderDistanceInfo& followers, const MSLeaderDistanceInfo& leaders);
+
+    /// @brief Saves the lane change relevant vehicles, which are currently on neighboring lanes in the given direction
+    ///        (as detected in wantsChange()). -> LC2013 case
+    void saveNeighbors(const int dir, const std::pair<MSVehicle* const, double>& follower, const std::pair<MSVehicle* const, double>& leader);
+
+    /// @brief Clear info on neighboring vehicle from previous step
+    void clearNeighbors();
+
+    /// @brief Returns the neighboring, lc-relevant followers for the last step in the requested direction
+    const std::shared_ptr<MSLeaderDistanceInfo> getFollowers(const int dir);
+
+    /// @brief Returns the neighboring, lc-relevant leaders for the last step in the requested direction
+    const std::shared_ptr<MSLeaderDistanceInfo> getLeaders(const int dir);
+
     int& getCanceledState(const int dir) {
         if (dir == -1) {
             return myCanceledStateRight;
@@ -602,6 +619,14 @@ protected:
     int myCanceledStateRight;
     int myCanceledStateCenter;
     int myCanceledStateLeft;
+
+    /// @brief Cached info on lc-relevant neighboring vehicles
+    /// @{
+    std::shared_ptr<MSLeaderDistanceInfo> myLeftFollowers;
+    std::shared_ptr<MSLeaderDistanceInfo> myLeftLeaders;
+    std::shared_ptr<MSLeaderDistanceInfo> myRightFollowers;
+    std::shared_ptr<MSLeaderDistanceInfo> myRightLeaders;
+    /// @}
 
     /// @brief the current lateral speed
     double mySpeedLat;

commit be08527511129e63c360a21a4f39e5b1cd070839

fix #5180, broken in [f8609593f747188b6d1204ac36208c959d8697c6]

src/microsim/MSLeaderInfo.cpp
@@ -41,14 +41,12 @@
 // MSLeaderInfo member method definitions
 // ===========================================================================
 MSLeaderInfo::MSLeaderInfo(const MSLane* lane, const MSVehicle* ego, double latOffset) :
+    myWidth(lane != nullptr ? lane->getWidth() : 0),
     myVehicles(MAX2(1, int(ceil(myWidth / MSGlobals::gLateralResolution))), (MSVehicle*)nullptr),
     myFreeSublanes((int)myVehicles.size()),
     egoRightMost(-1),
     egoLeftMost(-1),
     myHasVehicles(false) {
-    if (lane != nullptr) {
-        myWidth = lane->getWidth();
-    }
     if (ego != nullptr) {
         getSubLanes(ego, latOffset, egoRightMost, egoLeftMost);
         // filter out sublanes not of interest to ego
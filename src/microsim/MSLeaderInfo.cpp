be08527511129e63c360a21a4f39e5b1cd070839
namdre <jakob.erdmann@dlr.de>
2019-02-12 23:57:24 +0100

    fix #5180, broken in [f8609593f747188b6d1204ac36208c959d8697c6]

--- src/microsim/MSLeaderInfo.cpp
+++ src/microsim/MSLeaderInfo.cpp
@@ -41,14 +41,12 @@
 // MSLeaderInfo member method definitions
 // ===========================================================================
 MSLeaderInfo::MSLeaderInfo(const MSLane* lane, const MSVehicle* ego, double latOffset) :
+    myWidth(lane != nullptr ? lane->getWidth() : 0),
     myVehicles(MAX2(1, int(ceil(myWidth / MSGlobals::gLateralResolution))), (MSVehicle*)nullptr),
     myFreeSublanes((int)myVehicles.size()),
     egoRightMost(-1),
     egoLeftMost(-1),
     myHasVehicles(false) {
-    if (lane != nullptr) {
-        myWidth = lane->getWidth();
-    }
     if (ego != nullptr) {
         getSubLanes(ego, latOffset, egoRightMost, egoLeftMost);
         // filter out sublanes not of interest to ego
