71390da6b2450b7788132c367fa74044220e28c0
namdre <jakob.erdmann@dlr.de>
2019-03-04 14:08:52 +0100

    fixing race condition. refs #5233

--- src/gui/GUIViewTraffic.cpp
+++ src/gui/GUIViewTraffic.cpp
@@ -418,8 +418,11 @@ GUIViewTraffic::onGamingClick(Position pos) {
                 GUIGlID id = *sel.begin();
                 GUIVehicle* veh = dynamic_cast<GUIVehicle*>(GUIGlObjectStorage::gIDStorage.getObjectBlocking(id));
                 assert(veh != 0);
+                MSLane* lane = veh->getLane();
+                lane->getVehiclesSecure();
                 veh->rerouteDRTStop(closestStop);
                 GUIGlObjectStorage::gIDStorage.unblockObject(id);
+                lane->releaseVehicles();
             }
         }
     }
