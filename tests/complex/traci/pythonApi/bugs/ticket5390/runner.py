1709438e38a37b817f4b6e9b78153a5aeaab69a4
namdre <jakob.erdmann@dlr.de>
2019-03-31 07:43:00 +0200

    added test refs #5390

---
+++ tests/complex/traci/pythonApi/bugs/ticket5390/runner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Jakob Erdmann
+# @date    2017-01-23
+# @version $Id$
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci  # noqa
+import sumolib  # noqa
+
+
+sumoBinary = os.environ["SUMO_BINARY"]
+cmd = [sumoBinary,
+       '-n', 'input_net2.net.xml',
+       '--no-step-log',
+       ]
+
+traci.start(cmd)
+traci.vehicle.add("ego", "")
+traci.vehicle.setRoute("ego", [":C_3"])
+
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+
+traci.close()
