2a947141e5e30bef8ce29b4414d0ee0ff08cf7a9
Leonhard Luecken <leonhard.luecken@dlr.de>
2019-04-17 13:43:04 +0200

    Switch off gui for ToC device test, refs #21

--- tests/complex/sumo/ToCDevice/LC_during_MRM/runner.py
+++ tests/complex/sumo/ToCDevice/LC_during_MRM/runner.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
     if options.nogui:
-        sumoBinary = checkBinary('sumo-gui')
+        sumoBinary = checkBinary('sumo')
     else:
         sumoBinary = checkBinary('sumo-gui')
 
