c156cc349d3a3f5154913f0a150b12bc228c9c3c
Michael Behrisch <michael.behrisch@dlr.de>
2019-05-01 18:57:21 +0200

    moving traas test into regular test suite, refs #21

---
+++ tests/complex/traas/api/runner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2019-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @date    2019-05-01
+# @version $Id$
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+import os
+import subprocess
+import sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+from sumolib import checkBinary  # noqa
+
+traasJar  = os.path.join(os.environ['SUMO_HOME'], "bin", "TraaS.jar")
+assert(os.path.exists(traasJar))
+
+subprocess.call(["javac", "-cp", traasJar, "data/APITest.java"])
+retcode = subprocess.call(["java", "-cp", traasJar + ":data", "APITest", checkBinary('sumo'), "data/config.sumocfg"])
+print(">> Simulation closed with status %s" % retcode)
