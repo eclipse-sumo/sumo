2ab9f4d22c0fa2bdfbeb66a641e7a544ac5dd1d9
Michael Behrisch <michael.behrisch@dlr.de>
2019-05-01 18:00:47 +0200

    adding new pom for lisum gui build, refs #5534

---
+++ tools/contributed/lisum-gui/src/main/java/de/dlr/ts/lisum/gui/MainAppWrapper.java
@@ -0,0 +1,34 @@
+/** ************************************************************************* */
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2016-2018 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/** ************************************************************************* */
+/// @file    Constants.java
+/// @author  Maximiliano Bottazzi
+/// @date    2016
+/// @version $Id$
+///
+//
+/** ************************************************************************* */
+package de.dlr.ts.lisum.gui;
+
+/**
+ *
+ * @author @author <a href="mailto:maximiliano.bottazzi@dlr.de">Maximiliano
+ * Bottazzi</a>
+ */
+public class MainAppWrapper {
+
+    /**
+     *
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
