c5f64448e5f2d655461a456c16a9a766ed48316e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-05-03 14:21:52 +0200

    Fixed #5566

--- src/netedit/netelements/GNEEdge.cpp
+++ src/netedit/netelements/GNEEdge.cpp
@@ -537,6 +537,10 @@ GNEEdge::drawGL(const GUIVisualizationSettings& s) const {
     for (const auto &i : getAdditionalChilds()) {
         i->drawGL(s);
     }
+    // draw demand element childs
+    for (const auto &i : getDemandElementChilds()) {
+        i->drawGL(s);
+    }
     // draw edge chidls
     if (myNet->getViewNet()->getViewOptions().showDemandElements()) {
         for (const auto &i : getDemandElementChilds()) {
