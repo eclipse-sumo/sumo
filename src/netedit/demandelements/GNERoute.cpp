657f04aa2bb73f6c37ab43d60b9ce12b13043e17
namdre <jakob.erdmann@dlr.de>
2019-05-09 16:19:05 +0200

    fixing warnings, possibly also compile error for stricter compilers

--- src/netedit/demandelements/GNERoute.cpp
+++ src/netedit/demandelements/GNERoute.cpp
@@ -321,9 +321,9 @@ GNERoute::isValid(SumoXMLAttr key, const std::string& value) {
             if (value.empty()) {
                 return true;
             } else if(isValidDemandElementID(value)) {
-                return (myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_VEHICLE, false) || 
-                        myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_FLOW, false) || 
-                        myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_TRIP, false));
+                return (myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_VEHICLE, value, false) != nullptr || 
+                        myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_FLOW, value, false) != nullptr || 
+                        myViewNet->getNet()->retrieveDemandElement(SUMO_TAG_TRIP, value, false) != nullptr );
             } else {
                 return false;
             }
