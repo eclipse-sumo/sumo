692e8aded0da096ac732c45f6a6564eb0d58ff7a
Michael Behrisch <michael.behrisch@dlr.de>
2019-05-18 12:10:17 +0200

    cleaning lisum headers, refs #22

--- tools/contributed/lisum-core/src/main/java/de/dlr/ts/commons/logger/DateTools.java
+++ tools/contributed/lisum-core/src/main/java/de/dlr/ts/commons/logger/DateTools.java
@@ -1,12 +1,19 @@
-/*
- * Copyright (C) 2014
- * Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
- * Institut fuer Verkehrssystemtechnik
- *
- * German Aerospace Center
- * Institute of Transportation Systems
- *
- */
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2014-2019 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    DateTools.java
+/// @author  Maximiliano Bottazzi
+/// @date    2014
+/// @version $Id$
+///
+//
+/****************************************************************************/
 package de.dlr.ts.commons.logger;
 
 import java.text.DateFormat;
--- tools/contributed/lisum-core/src/main/java/de/dlr/ts/commons/logger/LogLevel.java
+++ tools/contributed/lisum-core/src/main/java/de/dlr/ts/commons/logger/LogLevel.java
@@ -1,9 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2016-2019 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    LogLevel.java
+/// @author  Maximiliano Bottazzi
+/// @date    2016
+/// @version $Id$
+///
+//
+/****************************************************************************/
 package de.dlr.ts.commons.logger;
 
 
