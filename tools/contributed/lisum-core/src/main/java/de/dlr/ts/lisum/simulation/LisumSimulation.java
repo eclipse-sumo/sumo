e8ec6317a7f1249df9ad93f7aa869257fa6e98d5
Michael Behrisch <michael.behrisch@dlr.de>
2019-05-18 11:40:26 +0200

    fixing some lisum headers, refs #22

--- tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/simulation/LisumSimulation.java
+++ tools/contributed/lisum-core/src/main/java/de/dlr/ts/lisum/simulation/LisumSimulation.java
@@ -1,19 +1,19 @@
-/** ************************************************************************* */
-// Eclipse SUMO, LisumSimulation of Urban MObility; see https://eclipse.org/sumo
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
 // Copyright (C) 2016-2019 German Aerospace Center (DLR) and others.
 // This program and the accompanying materials
 // are made available under the terms of the Eclipse Public License v2.0
 // which accompanies this distribution, and is available at
 // http://www.eclipse.org/legal/epl-v20.html
 // SPDX-License-Identifier: EPL-2.0
-/** ************************************************************************* */
+/****************************************************************************/
 /// @file    Constants.java
 /// @author  Maximiliano Bottazzi
 /// @date    2016
 /// @version $Id$
 ///
 //
-/** ************************************************************************* */
+/****************************************************************************/
 package de.dlr.ts.lisum.simulation;
 
 /*
--- tools/contributed/lisum-gui/src/main/java/de/dlr/ts/lisum/gui/Tools.java
+++ tools/contributed/lisum-gui/src/main/java/de/dlr/ts/lisum/gui/Tools.java
@@ -1,4 +1,4 @@
-/** ************************************************************************* */
+/****************************************************************************/
 // Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
 // Copyright (C) 2016-2019 German Aerospace Center (DLR) and others.
 // This program and the accompanying materials
@@ -6,14 +6,14 @@
 // which accompanies this distribution, and is available at
 // http://www.eclipse.org/legal/epl-v20.html
 // SPDX-License-Identifier: EPL-2.0
-/** ************************************************************************* */
+/****************************************************************************/
 /// @file    Constants.java
 /// @author  Maximiliano Bottazzi
 /// @date    2016
 /// @version $Id$
 ///
 //
-/** ************************************************************************* */
+/****************************************************************************/
 package de.dlr.ts.lisum.gui;
 
 import de.dlr.ts.commons.logger.DLRLogger;
