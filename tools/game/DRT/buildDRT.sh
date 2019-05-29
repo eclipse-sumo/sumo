eb98eba2d903679641ac61f7c952113c8ecd3bce
namdre <jakob.erdmann@dlr.de>
2019-05-29 13:17:59 +0200

    updated build script for DRT demo

--- tools/game/DRT/buildDRT.sh
+++ tools/game/DRT/buildDRT.sh
@@ -2,3 +2,4 @@
 $SUMO_HOME/tools/game/DRT/randomRides.py -n osm.net.xml -a drtstops.xml -o rides.xml  --poi-output drt.pois.xml --prefix a -b 0   -e 1600 -p 80 --poi-offset 32 --initial-duration 3
 $SUMO_HOME/tools/game/DRT/randomRides.py -n osm.net.xml -a drtstops.xml -o rides2.xml --poi-output drt.pois.xml --prefix a -b 0   -e 1600 -p 60  --poi-offset 32 --initial-duration 3
 $SUMO_HOME/tools/game/DRT/randomRides.py -n osm.net.xml -a drtstops.xml -o rides3.xml --poi-output drt.pois.xml --prefix b -b 600 -e 1600 -p 80  --poi-offset 32 --initial-duration 3
+$SUMO_HOME/tools/game/DRT/randomRides.py -n osm.net.xml -a drtstops.xml -o rides_demo.xml --poi-output drt.pois.xml --prefix d -b 0 -e 20000 -p 200  --poi-offset 32 --initial-duration 3
