0e940b50f748790b365fe84cd80cb0900190845d
tkchouaki <tkchouaki@icloud.com>
2019-06-05 11:46:19 +0200

    Ability to retrieve chargingStations's lane id. (#5717)
    Signed-off-by: tarek chouaki <tarek.chouaki@irt-systemx.fr>

--- src/libsumo/Simulation.cpp
+++ src/libsumo/Simulation.cpp
@@ -616,6 +616,8 @@ Simulation::getParameter(const std::string& objectID, const std::string& key) {
             return toString(cs->getTotalCharged());
         } else if (attrName == toString(SUMO_ATTR_NAME)) {
             return toString(cs->getMyName());
+        } else if (attrName == "lane") {
+            return cs->getLane().getID();
         } else {
             throw TraCIException("Invalid chargingStation parameter '" + attrName + "'");
         }
