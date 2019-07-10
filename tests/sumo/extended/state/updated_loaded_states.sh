abf740102842ccf25691a2e779b836296dbab9dd
namdre <jakob.erdmann@dlr.de>
2019-07-10 13:49:25 +0200

    addendum to [c1df9172823c68b29f9d5b61459cafbc34168bda]. patching inputs and expected results refs #21

--- tests/sumo/extended/state/updated_loaded_states.sh
+++ tests/sumo/extended/state/updated_loaded_states.sh
@@ -8,17 +8,17 @@ cp save2/state2.sumo.meso load_scaled/input_meso_state.xml
 cp save2/state2.sumo load_remove/input_state.xml
 cp save2/state2.sumo.meso load_remove/input_meso_state.xml
 
-cp save2/state2.sumo load_late_insertion/input_state.xml
-cp save2/state2.sumo.meso load_late_insertion/input_meso_state.xml
+cp save2/state.sumo load_late_insertion/input_state.xml
+cp save2/state.sumo.meso load_late_insertion/input_meso_state.xml
 
-cp save2/state2.sumo load_and_save/input_state.xml
-cp save2/state2.sumo.meso load_and_save/input_meso_state.xml
+cp save2/state.sumo load_and_save/input_state.xml
+cp save2/state.sumo.meso load_and_save/input_meso_state.xml
 
-cp save2/state2.sumo load_and_save_including_actionStepLength/input_state.xml
-cp save2/state2.sumo.meso load_and_save_including_actionStepLength/input_meso_state.xml
+#cp save2/state.sumo load_and_save_including_actionStepLength/input_state.xml
+cp save2/state.sumo.meso load_and_save_including_actionStepLength/input_meso_state.xml
 
-cp save2/state2.sumo load_and_save_including_actionStepLength2/input_state.xml
-cp save2/state2.sumo.meso load_and_save_including_actionStepLength2/input_meso_state.xml
+#cp save2/state.sumo load_and_save_including_actionStepLength2/input_state.xml
+cp save2/state.sumo.meso load_and_save_including_actionStepLength2/input_meso_state.xml
 
 cp save_flow/state.sumo load_flow/input_state.xml
 cp save_flow/state.sumo.meso load_flow/input_meso_state.xml
