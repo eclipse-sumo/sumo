9eb4ec69f674b56d0210f22c0452f0cae12538e5
Danny Behnecke <danny.behnecke@dlr.de>
2019-07-26 13:38:45 +0200

    adjusted CPackConfig to use SUMO_HOME environment variable for paths.

--- build/cmake_modules/CPackConfig.cmake
+++ build/cmake_modules/CPackConfig.cmake
@@ -3,10 +3,13 @@
 # set(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS "${INCLUDE_DIRECTORIES}")
 # include(InstallRequiredSystemLibraries)
 
+if(NOT DEFINED SUMO_HOME)
+    set(SUMO_HOME ${CMAKE_CURRENT_SOURCE_DIR})
+endif(NOT DEFINED SUMO_HOME)
+
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Eclipse Simulation of Urban Mobility - A Microscopic Traffic Simulation")
-#${MISSING_LINK} is a variable only used when integrating SUMO via "add subdirectory" into other projects. If not used that way, it just is empty
-set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}${MISSING_LINK}/LICENSE")
-set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}${MISSING_LINK}/README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${SUMO_HOME}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${SUMO_HOME}/README.md")
 set(CPACK_PACKAGE_VERSION "${PACKAGE_VERSION}")
 
 message(STATUS "CPACK_GENERATOR: " ${CPACK_GENERATOR})
