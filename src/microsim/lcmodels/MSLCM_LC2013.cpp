8413a4dab3311d2b88eb553cc265512378804054
theDiv <div@thoda.uk>
2019-07-27 11:07:04 +0100

    Initial updates to introduce manoeuvering behaviour into vehicle parking
    Signed-off-by: theDiv <div@thoda.uk>

--- src/microsim/lcmodels/MSLCM_LC2013.cpp
+++ src/microsim/lcmodels/MSLCM_LC2013.cpp
@@ -1114,7 +1114,8 @@ MSLCM_LC2013::_wantsChange(
         currentDist = curr.length;
     }
     double driveToNextStop = -std::numeric_limits<double>::max();
-    if (myVehicle.nextStopDist() < std::numeric_limits<double>::max()
+    // check for hasStops added because there is a corner case, whilst exiting parking place where there are no stops and this exceptions
+    if (myVehicle.nextStopDist() < std::numeric_limits<double>::max()  && myVehicle.hasStops()
             && &myVehicle.getNextStop().lane->getEdge() == &myVehicle.getLane()->getEdge()) {
         // vehicle can always drive up to stop distance
         // @note this information is dynamic and thus not available in updateBestLanes()
