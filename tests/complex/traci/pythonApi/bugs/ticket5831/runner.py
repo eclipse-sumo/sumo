8dc4e1c28dbd0d67308e288ec4358d57f6d53c59
Michael Behrisch <michael.behrisch@dlr.de>
2019-08-02 11:55:08 +0200

    adding failing test, refs #5831

---
+++ tests/complex/traci/pythonApi/bugs/ticket5831/runner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @date    2017-01-23
+# @version $Id$
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os, sys
+sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+import traci  # noqa
+import sumolib  # noqa
+
+
+traci.start([sumolib.checkBinary("sumo"), '-c', 'sumo.sumocfg'])
+vehID = "egoVehicle.0"
+for _ in range(250):
+   traci.simulationStep()
+   appeared = vehID in traci.vehicle.getIDList()
+   if appeared:
+      traci.vehicle.setLength(vehID, 3.5)
+traci.close()
