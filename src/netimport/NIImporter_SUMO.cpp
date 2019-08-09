eclipse-sumo/sumo

commit 02bdd0e2d3ab1e4e993b566082ae4d9b32ade789

fix #5842

src/netimport/NIImporter_SUMO.cpp
@@ -204,7 +204,9 @@ NIImporter_SUMO::_loadNetwork(OptionsCont& oc) {
                 nbe->addLane2LaneConnection(
                     fromLaneIndex, toEdge, c.toLaneIdx, NBEdge::L2L_VALIDATED,
                     true, c.mayDefinitelyPass, c.keepClear, c.contPos, c.visibility, c.speed, c.customShape, uncontrolled);
-
+                if (c.getParametersMap().size() > 0) {
+                    nbe->getConnectionRef(fromLaneIndex, toEdge, c.toLaneIdx).updateParameter(c.getParametersMap());
+                }
                 // maybe we have a tls-controlled connection
                 if (c.tlID != "" && myRailSignals.count(c.tlID) == 0) {
                     const std::map<std::string, NBTrafficLightDefinition*>& programs = myTLLCont.getPrograms(c.tlID);
@@ -516,6 +518,7 @@ NIImporter_SUMO::myEndElement(int element) {
             }
             break;
         case SUMO_TAG_CONNECTION:
+            myLastParameterised.pop_back();
             break;
         default:
             break;
@@ -765,6 +768,7 @@ NIImporter_SUMO::addConnection(const SUMOSAXAttributes& attrs) {
         return;
     }
     from->lanes[fromLaneIdx]->connections.push_back(conn);
+    myLastParameterised.push_back(&from->lanes[fromLaneIdx]->connections.back());
 
     // determine crossing priority and tlIndex
     if (myPedestrianCrossings.size() > 0) {

src/netwrite/NWWriter_SUMO.cpp
@@ -712,6 +712,7 @@ NWWriter_SUMO::writeConnection(OutputDevice& into, const NBEdge& from, const NBE
             into.writeAttr(SUMO_ATTR_STATE, linkState);
         }
     }
+    c.writeParams(into);
     into.closeTag();
 }
 

commit a15ac48aa56cadacf3dc1c368afdd5eec114b599

fixing crash on broken network from 02bdd0e2d3ab1e4e9

src/netimport/NIImporter_SUMO.cpp
@@ -518,7 +518,10 @@ NIImporter_SUMO::myEndElement(int element) {
             }
             break;
         case SUMO_TAG_CONNECTION:
-            myLastParameterised.pop_back();
+            // !!! this just avoids a crash but is not a real check that it was a connection
+            if (!myLastParameterised.empty()) {
+                myLastParameterised.pop_back();
+            }
             break;
         default:
             break;