4fb3655afb96111457ea83e265d54c0602a6bfdd
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-08-15 12:18:06 +0200

    Added Netedit routeFlow inspect_selection test. Refs #3045 and #5086

---
+++ tests/netedit/demandElements/routeflow/inspect_selection/departSpeed/speedLimit/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2019-07-16
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to demand mode
+netedit.supermodeDemand()
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvertDemand()
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect vehicle
+netedit.leftClick(referencePosition, 91, 413)
+
+# change departSpeed with an invalid value
+netedit.modifyAttribute(4, "", True)
+
+# change departSpeed with an invalid value
+netedit.modifyAttribute(4, "dummySpeed", True)
+
+# change departSpeed with a valid value
+netedit.modifyAttribute(4, "speedLimit", True)
+
+# Check undo redo
+netedit.undo(referencePosition, 5)
+netedit.redo(referencePosition, 5)
+
+# save routes
+netedit.saveRoutes(referencePosition)
+
+# save network
+netedit.saveNetwork(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
