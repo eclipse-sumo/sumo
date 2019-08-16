767ee601ebb798c9f87496bed87c59e8a6ff4788
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-08-16 11:02:03 +0200

    Added Netedit delete person test. Refs #5369 and #3045

---
+++ tests/netedit/demandElements/personFlow/delete/test.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2019-07-16
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to demand mode
+netedit.supermodeDemand()
+
+# go to person mode
+netedit.personMode()
+
+# select personFlwo
+netedit.changeElement("personFlow")
+
+# create person using three edges
+netedit.leftClick(referencePosition, 274, 414)
+netedit.leftClick(referencePosition, 570, 250)
+netedit.leftClick(referencePosition, 180, 60)
+
+# press enter to create person
+netedit.typeEnter()
+
+# go to delete mode
+netedit.deleteMode()
+
+# change zoom
+netedit.setZoom("15", "20", "20")
+
+# delete person
+netedit.leftClick(referencePosition, 150, 295)
+
+# Check undo
+netedit.undo(referencePosition, 1)
+
+# Change to network mode
+netedit.supermodeNetwork()
+
+# go to delete mode
+netedit.deleteMode()
+
+# try to delete an edge with demand elements
+netedit.leftClick(referencePosition, 400, 295)
+
+# wait warning
+netedit.waitDeleteWarning()
+
+# disable protect demand elemnts
+netedit.changeProtectDemandElements(referencePosition)
+
+# now delete edge with their person
+netedit.leftClick(referencePosition, 400, 295)
+
+# Check undo
+netedit.undo(referencePosition, 2)
+netedit.redo(referencePosition, 2)
+
+# save network
+netedit.saveNetwork(referencePosition)
+
+# save persons
+netedit.saveRoutes(referencePosition)
+
+# save additionals
+netedit.saveAdditionals(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
