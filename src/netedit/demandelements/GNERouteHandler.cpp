410aaf0c61d7b85ac1b88839d4fd05bb5306c12b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-08-22 13:35:58 +0200

    Fixed #5967

--- src/netedit/demandelements/GNERouteHandler.cpp
+++ src/netedit/demandelements/GNERouteHandler.cpp
@@ -408,6 +408,12 @@ GNERouteHandler::buildFlow(GNEViewNet* viewNet, bool undoDemandElements, const S
             }
             // obtain route between edges
             std::vector<GNEEdge*> routeEdges = GNEDemandElement::getRouteCalculatorInstance()->calculateDijkstraRoute(vType->getVClass(), edges);
+            // check if obtained routes correspond to a valid route
+            if (routeEdges.size() == 0) {
+                routeEdges = edges;
+            } else if (routeEdges.size() == 1) {
+                routeEdges.push_back(routeEdges.front());
+            }
             // create trip or flow using tripParameters
             GNEVehicle* flow = new GNEVehicle(viewNet, vType, routeEdges, vehicleParameters);
             if (undoDemandElements) {
