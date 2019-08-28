4483d8451fa7ab0772f842e1792ed7f316b35ff4
Michael Behrisch <michael.behrisch@dlr.de>
2019-08-28 15:28:42 +0200

    fixing style in traas, refs #22

--- tools/contributed/traas/src/main/java/de/tudresden/sumo/util/Observable.java
+++ tools/contributed/traas/src/main/java/de/tudresden/sumo/util/Observable.java
@@ -1,3 +1,21 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2017-2019 German Aerospace Center (DLR) and others.
+// TraaS module
+// Copyright (C) 2016-2017 Dresden University of Technology
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    Observable.java
+/// @author  Mario Krumnow
+/// @date    2019
+/// @version $Id$
+///
+//
+/****************************************************************************/
 package de.tudresden.sumo.util;
 
 import java.util.ArrayList;
@@ -6,15 +24,17 @@ import java.util.List;
 import de.tudresden.sumo.subscription.SubscriptionObject;
 
 public class Observable {
-	
-   private List<Observer> observers = new ArrayList<Observer>();
 
-   public void addObserver(Observer observer){
-      observers.add(observer);		
-   }
+    private List<Observer> observers = new ArrayList<Observer>();
 
-   public void notifyObservers(SubscriptionObject so){
-      for (Observer observer : observers) {observer.update(this, so);}
-   } 	
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(SubscriptionObject so) {
+        for (Observer observer : observers) {
+            observer.update(this, so);
+        }
+    }
 
 }
--- tools/contributed/traas/src/main/java/de/tudresden/sumo/util/Observer.java
+++ tools/contributed/traas/src/main/java/de/tudresden/sumo/util/Observer.java
@@ -1,9 +1,27 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2017-2019 German Aerospace Center (DLR) and others.
+// TraaS module
+// Copyright (C) 2016-2017 Dresden University of Technology
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    Observer.java
+/// @author  Mario Krumnow
+/// @date    2019
+/// @version $Id$
+///
+//
+/****************************************************************************/
 package de.tudresden.sumo.util;
 
 import de.tudresden.sumo.subscription.SubscriptionObject;
 
 public interface Observer {
-	
-   public void update(Observable ob, SubscriptionObject so);
+
+    public void update(Observable ob, SubscriptionObject so);
 
 }
