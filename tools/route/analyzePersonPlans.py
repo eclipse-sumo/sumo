07c5c5bf5aa0ce18e3ffa273bf0f2cf06f1d5663
namdre <jakob.erdmann@dlr.de>
2019-09-13 15:19:11 +0200

    can now print examplary person ids. refs #6083

--- tools/route/analyzePersonPlans.py
+++ tools/route/analyzePersonPlans.py
@@ -34,6 +34,7 @@ def get_options(args=None):
     parser.add_argument("-r", "--route-files", dest="routeFiles", help="Input route files")
     parser.add_argument("-w", "--merge-walks", dest="mergeWalks", action="store_true", help="merge subsequent walks")
     parser.add_argument("-p", "--public-prefixes", dest="public", help="Distinguis public transport modes by prefix")
+    parser.add_argument("-i", "--ids", dest="ids", default=0, type=int, help="List the given number of person ids for each type of plan")
     options = parser.parse_args(args=args)
     if options.routeFiles is None:
         parser.print_help()
@@ -59,7 +60,7 @@ def stageName(options, person, stage):
 
 
 def main(options):
-    counts = defaultdict(lambda : 0)
+    counts = defaultdict(lambda : list())
     for routeFile in options.routeFiles.split(','):
         for person in parse(routeFile, 'person'):
             stages = tuple([stageName(options, person, s) for s in person.getChildList()])
@@ -69,11 +70,21 @@ def main(options):
                     if len(filtered) == 0 or s != filtered[-1]:
                         filtered.append(s)
                 stages = tuple(filtered)
-            counts[stages] += 1
+            counts[stages].append(person.id)
 
-    reverseCounts = [(count, k) for k,count in counts.items()]
+    numPersons = sum(map(len, counts.values()))
+    print("Loaded %s persons" % numPersons)
+    maxPadding = max(map(len, [' '.join(k) for k,v in counts.items()]))
+    countSize = len(str(max([len(p) for k,p in counts.items()])))
+    formatStr = "%" + str(countSize) + "s: %s%s"
+    reverseCounts = [(len(p), k) for k,p in counts.items()]
     for count, k in sorted(reverseCounts):
-        print("%s: %s" % (count, ' '.join(k)))
+        stages = ' '.join(k)
+        examples = ""
+        if options.ids > 0:
+            padding = " " * (maxPadding - len(stages))
+            examples = padding + " - " + ','.join(counts[k][:options.ids])
+        print(formatStr % (count, stages, examples))
 
 if __name__ == "__main__":
     main(get_options())
