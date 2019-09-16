878e2df37c4e4adbfaee2f7edbc9ae47fc2d1f34
Michael Behrisch <michael.behrisch@dlr.de>
2019-09-16 08:52:55 +0200

    adding script for netedit debug tests, refs #21

---
+++ tests/runNeteditDebugTests.bat
@@ -0,0 +1,9 @@
+REM Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+REM Copyright (C) 2008-2018 German Aerospace Center (DLR) and others.
+REM This program and the accompanying materials
+REM are made available under the terms of the Eclipse Public License v2.0
+REM which accompanies this distribution, and is available at
+REM http://www.eclipse.org/legal/epl-v20.html
+REM SPDX-License-Identifier: EPL-2.0
+call %~dp0\testEnv.bat D
+start %TEXTTESTPY% -a netedit.gui -gui -v debug
