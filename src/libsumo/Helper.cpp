eclipse-sumo/sumo

commit e791d00fb262f7b5e69b31bed74bb1755cb23d91

fixing vs 2019 warnings, refs #3

src/libsumo/Helper.cpp
@@ -698,12 +698,11 @@ Helper::applySubscriptionFilters(const Subscription& s, std::set<std::string>& o
                     // Collect vehicles from opposite lanes
                     if (nOpp > 0) {
                         for (auto& laneCov : *checkedLanesInDrivingDir) {
-                            const MSLane* lane = laneCov.first;
-                            if (lane == nullptr || lane->getEdge().getOppositeEdge() == nullptr) {
+                            const MSLane* const l = laneCov.first;
+                            if (l == nullptr || l->getEdge().getOppositeEdge() == nullptr) {
                                 continue;
                             }
-                            const MSEdge* edge = &(lane->getEdge());
-                            const MSEdge* opposite = edge->getOppositeEdge();
+                            const MSEdge* opposite = l->getEdge().getOppositeEdge();
                             const std::pair<double, double>& range = laneCov.second;
                             auto leftMostOppositeLaneIt = opposite->getLanes().rbegin();
                             for (auto oppositeLaneIt = leftMostOppositeLaneIt;

src/config.h.cmake
@@ -8,6 +8,8 @@
 #pragma warning(disable: 4290)
 /* Disable DLL-Interface warnings */
 #pragma warning(disable: 4251)
+/* Disable integer overflow in arithmetics warnings */
+#pragma warning(disable: 26451)
 
 /* Disable "unsafe" warnings for crt functions in VC++ 2005. */
 #if _MSC_VER >= 1400

src/gui/GUIViewTraffic.cpp
@@ -361,12 +361,9 @@ void
 GUIViewTraffic::onGamingClick(Position pos) {
     if (myTLSGame) {
         MSTLLogicControl& tlsControl = MSNet::getInstance()->getTLSControl();
-        const std::vector<MSTrafficLightLogic*>& logics = tlsControl.getAllLogics();
-        MSTrafficLightLogic* minTll = nullptr;
+        const MSTrafficLightLogic* minTll = nullptr;
         double minDist = std::numeric_limits<double>::infinity();
-        for (std::vector<MSTrafficLightLogic*>::const_iterator i = logics.begin(); i != logics.end(); ++i) {
-            // get the logic
-            MSTrafficLightLogic* tll = (*i);
+        for (const MSTrafficLightLogic* const tll : tlsControl.getAllLogics()) {
             if (tlsControl.isActive(tll) && tll->getProgramID() != "off") {
                 // get the links
                 const MSTrafficLightLogic::LaneVector& lanes = tll->getLanesAt(0);

src/gui/TraCIServerAPI_GUI.cpp
@@ -139,7 +139,7 @@ TraCIServerAPI_GUI::processSet(TraCIServer& server, tcpip::Storage& inputStorage
         return server.writeErrorStatusCmd(libsumo::CMD_SET_GUI_VARIABLE, "Change GUI State: unsupported variable " + toHex(variable, 2) + " specified", outputStorage);
     }
     // id
-    std::string id = inputStorage.readString();
+    const std::string id = inputStorage.readString();
     GUISUMOAbstractView* v = getNamedView(id);
     if (v == nullptr) {
         return server.writeErrorStatusCmd(libsumo::CMD_SET_GUI_VARIABLE, "View '" + id + "' is not known", outputStorage);
@@ -211,16 +211,16 @@ TraCIServerAPI_GUI::processSet(TraCIServer& server, tcpip::Storage& inputStorage
         }
         break;
         case libsumo::VAR_TRACK_VEHICLE: {
-            std::string id;
-            if (!server.readTypeCheckingString(inputStorage, id)) {
+            std::string vehID;
+            if (!server.readTypeCheckingString(inputStorage, vehID)) {
                 return server.writeErrorStatusCmd(libsumo::CMD_SET_GUI_VARIABLE, "Tracking requires a string vehicle ID.", outputStorage);
             }
             if (id == "") {
                 v->stopTrack();
             } else {
-                SUMOVehicle* veh = MSNet::getInstance()->getVehicleControl().getVehicle(id);
+                SUMOVehicle* veh = MSNet::getInstance()->getVehicleControl().getVehicle(vehID);
                 if (veh == nullptr) {
-                    return server.writeErrorStatusCmd(libsumo::CMD_SET_GUI_VARIABLE, "Could not find vehicle '" + id + "'.", outputStorage);
+                    return server.writeErrorStatusCmd(libsumo::CMD_SET_GUI_VARIABLE, "Could not find vehicle '" + vehID + "'.", outputStorage);
                 }
                 if (v->getTrackedID() != static_cast<GUIVehicle*>(veh)->getGlID()) {
                     v->startTrack(static_cast<GUIVehicle*>(veh)->getGlID());

src/libsumo/Vehicle.cpp
@@ -377,26 +377,26 @@ Vehicle::getNextTLS(const std::string& vehicleID) {
     if (veh->isOnRoad()) {
         const MSLane* lane = veh->getLane();
         const std::vector<MSLane*>& bestLaneConts = veh->getBestLanesContinuation(lane);
-        double seen = veh->getLane()->getLength() - veh->getPositionOnLane();
+        double seen = lane->getLength() - veh->getPositionOnLane();
         int view = 1;
-        MSLinkCont::const_iterator link = MSLane::succLinkSec(*veh, view, *lane, bestLaneConts);
-        while (!lane->isLinkEnd(link)) {
+        MSLinkCont::const_iterator linkIt = MSLane::succLinkSec(*veh, view, *lane, bestLaneConts);
+        while (!lane->isLinkEnd(linkIt)) {
             if (!lane->getEdge().isInternal()) {
-                if ((*link)->isTLSControlled()) {
+                if ((*linkIt)->isTLSControlled()) {
                     TraCINextTLSData ntd;
-                    ntd.id = (*link)->getTLLogic()->getID();
-                    ntd.tlIndex = (*link)->getTLIndex();
+                    ntd.id = (*linkIt)->getTLLogic()->getID();
+                    ntd.tlIndex = (*linkIt)->getTLIndex();
                     ntd.dist = seen;
-                    ntd.state = (char)(*link)->getState();
+                    ntd.state = (char)(*linkIt)->getState();
                     result.push_back(ntd);
                 }
             }
-            lane = (*link)->getViaLaneOrLane();
+            lane = (*linkIt)->getViaLaneOrLane();
             if (!lane->getEdge().isInternal()) {
                 view++;
             }
             seen += lane->getLength();
-            link = MSLane::succLinkSec(*veh, view, *lane, bestLaneConts);
+            linkIt = MSLane::succLinkSec(*veh, view, *lane, bestLaneConts);
         }
         // consider edges beyond bestLanes
         const int remainingEdges = (int)(veh->getRoute().end() - veh->getCurrentRouteEdge()) - view;
@@ -406,7 +406,7 @@ Vehicle::getNextTLS(const std::string& vehicleID) {
             const MSEdge* next = *(veh->getCurrentRouteEdge() + view + i);
             const std::vector<MSLane*>* allowed = prev->allowedLanes(*next, veh->getVClass());
             if (allowed != nullptr && allowed->size() != 0) {
-                for (MSLink* link : allowed->front()->getLinkCont()) {
+                for (const MSLink* const link : allowed->front()->getLinkCont()) {
                     if (&link->getLane()->getEdge() == next) {
                         if (link->isTLSControlled()) {
                             TraCINextTLSData ntd;

src/mesosim/MESegment.cpp
@@ -382,19 +382,17 @@ MESegment::getLink(const MEVehicle* veh, bool penalty) const {
         }
         // try to find any link leading to our next edge, start with the lane pointed to by the que index
         const MSLane* const bestLane = myEdge.getLanes()[veh->getQueIndex()];
-        const MSLinkCont& links = bestLane->getLinkCont();
-        for (std::vector<MSLink*>::const_iterator j = links.begin(); j != links.end(); ++j) {
-            if (&(*j)->getLane()->getEdge() == nextEdge) {
-                return *j;
+        for (MSLink* const link : bestLane->getLinkCont()) {
+            if (&link->getLane()->getEdge() == nextEdge) {
+                return link;
             }
         }
         // this is for the non-multique case, maybe we should use caching here !!!
-        for (std::vector<MSLane*>::const_iterator l = myEdge.getLanes().begin(); l != myEdge.getLanes().end(); ++l) {
-            if ((*l) != bestLane) {
-                const MSLinkCont& links = (*l)->getLinkCont();
-                for (std::vector<MSLink*>::const_iterator j = links.begin(); j != links.end(); ++j) {
-                    if (&(*j)->getLane()->getEdge() == nextEdge) {
-                        return *j;
+        for (const MSLane* const lane : myEdge.getLanes()) {
+            if (lane != bestLane) {
+                for (MSLink* const link : lane->getLinkCont()) {
+                    if (&link->getLane()->getEdge() == nextEdge) {
+                        return link;
                     }
                 }
             }

src/microsim/MSEdge.cpp
@@ -704,7 +704,6 @@ MSEdge::changeLanes(SUMOTime t) {
 }
 
 
-
 const MSEdge*
 MSEdge::getInternalFollowingEdge(const MSEdge* followerAfterInternal) const {
     //@todo to be optimized
@@ -722,6 +721,7 @@ MSEdge::getInternalFollowingEdge(const MSEdge* followerAfterInternal) const {
     return nullptr;
 }
 
+
 double
 MSEdge::getInternalFollowingLengthTo(const MSEdge* followerAfterInternal) const {
     assert(followerAfterInternal != 0);
@@ -735,6 +735,7 @@ MSEdge::getInternalFollowingLengthTo(const MSEdge* followerAfterInternal) const 
     }
     return dist;
 }
+
 
 const MSEdge*
 MSEdge::getNormalBefore() const {
@@ -797,7 +798,6 @@ double
 MSEdge::getRoutingSpeed() const {
     return MSRoutingEngine::getAssumedSpeed(this);
 }
-
 
 
 bool

src/netedit/dialogs/GNECalibratorDialog.cpp
@@ -175,9 +175,9 @@ GNECalibratorDialog::onCmdClickedRoute(FXObject*, FXSelector, void*) {
                 // write warning if netedit is running in testing mode
                 WRITE_DEBUG("Opening FXMessageBox of type 'question'");
                 // open question dialog box
-                std::string message = ("Deletio n of " + toString(SUMO_TAG_ROUTE) + " '" + myRouteList->getItem(i, 0)->getText().text() + "' will remove " +
-                                       toString(calibratorFlowsToErase.size()) + " " + toString(SUMO_TAG_FLOW_CALIBRATOR) + (calibratorFlowsToErase.size() > 1 ? ("s") : ("")) + ". Continue?");
-                FXuint answer = FXMessageBox::question(getApp(), MBOX_YES_NO, ("Remove " + toString(SUMO_TAG_FLOW_CALIBRATOR) + "s").c_str(), "%s",  message.c_str());
+                const std::string msg = ("Deletion of " + toString(SUMO_TAG_ROUTE) + " '" + myRouteList->getItem(i, 0)->getText().text() + "' will remove " +
+                                         toString(calibratorFlowsToErase.size()) + " " + toString(SUMO_TAG_FLOW_CALIBRATOR) + (calibratorFlowsToErase.size() > 1 ? ("s") : ("")) + ". Continue?");
+                FXuint answer = FXMessageBox::question(getApp(), MBOX_YES_NO, ("Remove " + toString(SUMO_TAG_FLOW_CALIBRATOR) + "s").c_str(), "%s", msg.c_str());
                 if (answer != 1) { //1:yes, 2:no, 4:esc
                     // write warning if netedit is running in testing mode
                     if (answer == 2) {
@@ -293,9 +293,9 @@ GNECalibratorDialog::onCmdClickedVehicleType(FXObject*, FXSelector, void*) {
             }
             // if there are flows that has vehicle type to remove as "vehicle type" parameter
             if (calibratorFlowsToErase.size() > 0) {
-                std::string message = ("Deletion of " + toString(SUMO_TAG_VTYPE) + " '" + myVehicleTypeList->getItem(i, 0)->getText().text() + "' will remove " +
-                                       toString(calibratorFlowsToErase.size()) + " " + toString(SUMO_TAG_FLOW_CALIBRATOR) + (calibratorFlowsToErase.size() > 1 ? ("s") : ("")) + ". Continue?");
-                FXuint answer = FXMessageBox::question(getApp(), MBOX_YES_NO, ("Remove " + toString(SUMO_TAG_FLOW_CALIBRATOR) + "s").c_str(), "%s", message.c_str());
+                const std::string msg = ("Deletion of " + toString(SUMO_TAG_VTYPE) + " '" + myVehicleTypeList->getItem(i, 0)->getText().text() + "' will remove " +
+                                         toString(calibratorFlowsToErase.size()) + " " + toString(SUMO_TAG_FLOW_CALIBRATOR) + (calibratorFlowsToErase.size() > 1 ? ("s") : ("")) + ". Continue?");
+                FXuint answer = FXMessageBox::question(getApp(), MBOX_YES_NO, ("Remove " + toString(SUMO_TAG_FLOW_CALIBRATOR) + "s").c_str(), "%s", msg.c_str());
                 if (answer != 1) { //1:yes, 2:no, 4:esc
                     // write warning if netedit is running in testing mode
                     if (answer == 2) {

commit c2b3dba6ad172332214ea36319a76f2c05d185da

fixing crash introduced in e791d00fb262f7b5e69b31bed74bb1755cb23d91

src/libsumo/Helper.cpp
@@ -712,7 +712,7 @@ Helper::applySubscriptionFilters(const Subscription& s, std::set<std::string>& o
                                 }
                                 // Add vehicles from corresponding range on opposite direction
                                 const MSLane* oppositeLane = *oppositeLaneIt;
-                                auto new_vehs = oppositeLane->getVehiclesInRange(lane->getLength() - range.second, lane->getLength() - range.first);
+                                auto new_vehs = oppositeLane->getVehiclesInRange(l->getLength() - range.second, l->getLength() - range.first);
                                 vehs.insert(new_vehs.begin(), new_vehs.end());
                             }
                         }