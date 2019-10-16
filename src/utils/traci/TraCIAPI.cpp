f0d9418b13fdfe5bb8f7b94376837f032b5401eb
CG <timpcg76@yahoo.com.sg>
2019-10-16 10:09:10 +0800

    Fixed not a double bug when using EdgeScope::setMaxSpeed function
    Signed-off-by: CG <timpcg76@yahoo.com.sg>

--- src/utils/traci/TraCIAPI.cpp
+++ src/utils/traci/TraCIAPI.cpp
@@ -817,6 +817,7 @@ TraCIAPI::EdgeScope::setEffort(const std::string& edgeID, double effort, double 
 void
 TraCIAPI::EdgeScope::setMaxSpeed(const std::string& edgeID, double speed) const {
     tcpip::Storage content;
+	content.writeUnsignedByte(libsumo::TYPE_DOUBLE);
     content.writeDouble(speed);
     myParent.createCommand(libsumo::CMD_SET_EDGE_VARIABLE, libsumo::VAR_MAXSPEED, edgeID, &content);
     myParent.processSet(libsumo::CMD_SET_EDGE_VARIABLE);
