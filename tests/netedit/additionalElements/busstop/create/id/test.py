dc494f3eeb6c244383983ec71232ad9022a40717
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-10-21 16:01:46 +0200

    Patching Netedit expected results. Refs #3045

---
+++ tests/netedit/additionalElements/busstop/create/id/test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+# @version $Id$
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# go to additional mode
+netedit.additionalMode()
+
+# select busStop
+netedit.changeElement("busStop")
+
+# create busStop in mode "reference left"
+netedit.leftClick(referencePosition, 250, 250)
+
+# enable ID
+netedit.changeDefaultBoolValue(2)
+
+# set invalid ID (duplicated
+netedit.changeDefaultValue(3, ";;;;;;;;;;;;;;;;")
+
+# create busStop in mode "reference left"
+netedit.leftClick(referencePosition, 250, 250)
+
+# set invalid ID (duplicated
+netedit.changeDefaultValue(3, "busStop_gneE0_0_0")
+
+# create busStop in mode "reference left"
+netedit.leftClick(referencePosition, 250, 250)
+
+# set invalid ID (duplicated
+netedit.changeDefaultValue(3, "custom_ID")
+
+# create busStop in mode "reference left"
+netedit.leftClick(referencePosition, 250, 250)
+
+# Check undo redo
+netedit.undo(referencePosition, 2)
+netedit.redo(referencePosition, 2)
+
+# save additionals
+netedit.saveAdditionals(referencePosition)
+
+# save network
+netedit.saveNetwork(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
+
