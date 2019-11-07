e877546cb5b1df3798a14f03dbd5d8e95f49d964
namdre <jakob.erdmann@dlr.de>
2019-11-07 05:45:34 -0600

    adding new tool. refs #6229

---
+++ tools/jtcrouter.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2012-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    jtcrouter.py
+# @author  Jakob Erdmann
+# @date    2019-11-07
+# @version $Id$
+
+"""
+Generate routes like jtrrouter but bas on count parameters at connections
+The counts are used to derive the turn file and flow file for calling jtrrouter
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+from argparse import ArgumentParser
+from collections import defaultdict
+import subprocess
+
+if 'SUMO_HOME' in os.environ:
+    sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import sumolib  # noqa
+
+
+def get_options(args=None):
+    parser = ArgumentParser(description="Analyze person plans")
+    parser.add_argument("-n", "--net-file", dest="net", help="Input net file")
+    parser.add_argument("-o", "--output-file", dest="out", default="out.rou.xml",
+            help="Output route file")
+    parser.add_argument("--turn-file", dest="turnFile", default="turns.xml",
+            help="Intermediate turn-ration-file")
+    parser.add_argument("--flow-file", dest="flowFile", default="flows.xml",
+            help="Intermediate flow file")
+    parser.add_argument("-b", "--begin",  default=0, help="begin time")
+    parser.add_argument("-e", "--end",  default=3600, help="end time (default 3600)")
+    parser.add_argument("-p", "--count-param", dest="countParam", default="count", 
+            help="the connection parameter to use as count")
+    options = parser.parse_args(args=args)
+    if options.net is None:
+        parser.print_help()
+        sys.exit()
+    return options
+
+
+def main(options):
+    net = sumolib.net.readNet(options.net)
+    with open(options.turnFile, 'w') as tf, open(options.flowFile, 'w') as ff:
+        tf.write('<turns>\n')
+        ff.write('<routes>\n')
+        tf.write('    <interval begin="%s" end="%s">\n' % (options.begin, options.end))
+        for edge in net.getEdges():
+            counts = defaultdict(lambda : 0)
+            for toEdge, cons in edge.getOutgoing().items():
+                for con in cons:
+                    value = con.getParam(options.countParam)
+                    if value is not None:
+                        counts[con.getTo().getID()] += float(value)
+            if counts:
+                tf.write('        <fromEdge id="%s">\n' % (edge.getID()))
+                for toEdge, count in counts.items():
+                    tf.write('            <toEdge id="%s" probability="%s"/>\n' % (toEdge, count))
+                tf.write('        </fromEdge>\n')
+
+                if edge.is_fringe(edge._incoming):
+                    totalCount = int(sum(counts.values()))
+                    ff.write('    <flow id="%s" from="%s" begin="%s" end="%s" number="%s"/>\n' % (
+                        edge.getID(), edge.getID(), options.begin, options.end, totalCount))
+        tf.write('    </interval>\n')
+        tf.write('</turns>\n')
+        ff.write('</routes>\n')
+
+    JTRROUTER = sumolib.checkBinary('jtrrouter')
+    subprocess.call([JTRROUTER, 
+        '-n', options.net,
+        '--turn-ratio-files', options.turnFile,
+        '--route-files', options.flowFile,
+        '--accept-all-destinations',
+        '-o', options.out])
+
+
+if __name__ == "__main__":
+    main(get_options())
