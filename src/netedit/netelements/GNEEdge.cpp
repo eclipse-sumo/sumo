eclipse-sumo/sumo

commit 110070c1f8199dcd7951026769d7b1f4505d099b

fix #6231

src/netedit/netelements/GNEEdge.cpp
@@ -2127,8 +2127,12 @@ GNEEdge::drawEdgeName(const GUIVisualizationSettings& s) const {
             GLHelper::drawTextSettings(s.streetName, myNBEdge.getStreetName(), p, s.scale, angle);
         }
         if (s.edgeValue.show) {
-            double value = lane2->getColorValue(s, s.laneColorer.getActive());
-            GLHelper::drawTextSettings(s.edgeValue, toString(value), p, s.scale, angle);
+            std::string value = (s.laneColorer.getActive() == 13
+                    ? getNBEdge()->getLaneStruct(lane2->getIndex()).getParameter(s.laneParam, "")
+                    : getNBEdge()->getParameter(s.edgeParam, ""));
+            if (value != "") {
+                GLHelper::drawTextSettings(s.edgeValue, value, p, s.scale, angle);
+            }
         }
         glPopName();
     }

commit 232b628eef9c32cec2b58f99bd6f13f8d896e0aa

fixed regression in [110070c1f8199dcd7951026769d7b1f4505d099b]. refs #6231

src/netedit/netelements/GNEEdge.cpp
@@ -2097,9 +2097,20 @@ GNEEdge::drawEdgeName(const GUIVisualizationSettings& s) const {
             GLHelper::drawTextSettings(s.streetName, myNBEdge.getStreetName(), p, s.scale, angle);
         }
         if (s.edgeValue.show) {
-            std::string value = (s.laneColorer.getActive() == 13
-                    ? getNBEdge()->getLaneStruct(lane2->getIndex()).getParameter(s.laneParam, "")
-                    : getNBEdge()->getParameter(s.edgeParam, ""));
+            const int activeScheme = s.laneColorer.getActive();
+            std::string value;
+            if (activeScheme == 12) {
+                // edge param, could be non-numerical
+                value = getNBEdge()->getParameter(s.edgeParam, "");
+            } else if (activeScheme == 13) {
+                // lane param, could be non-numerical
+                value = getNBEdge()->getLaneStruct(lane2->getIndex()).getParameter(s.laneParam, "");
+            } else {
+                // use numerical value value of leftmost lane to hopefully avoid sidewalks, bikelanes etc
+                const double doubleValue = lane2->getColorValue(s, activeScheme);
+                const RGBColor color = s.laneColorer.getScheme().getColor(doubleValue);
+                value = color.alpha() == 0 ? "" : toString(doubleValue);
+            }
             if (value != "") {
                 GLHelper::drawTextSettings(s.edgeValue, value, p, s.scale, angle);
             }