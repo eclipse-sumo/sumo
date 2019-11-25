eclipse-sumo/sumo

commit 6ce8106ce1048de55a69383317988b069443be5f

fix #6304

src/guisim/GUIVehicle.cpp
@@ -306,10 +306,10 @@ GUIVehicle::drawAction_drawCarriageClass(const GUIVisualizationSettings& s, bool
     const int seatsPerCarriage = (int)ceil(totalSeats / (numCarriages - firstPassengerCarriage));
     // lane on which the carriage front is situated
     MSLane* lane = myLane;
-    int routeIndex = getRoutePosition();
+    int furtherIndex = 0;
     // lane on which the carriage back is situated
     MSLane* backLane = myLane;
-    int backRouteIndex = routeIndex;
+    int backFurtherIndex = furtherIndex;
     // offsets of front and back
     double carriageOffset = myState.pos();
     double carriageBackOffset = myState.pos() - firstCarriageLength;
@@ -328,7 +328,7 @@ GUIVehicle::drawAction_drawCarriageClass(const GUIVisualizationSettings& s, bool
             curCLength = carriageLength;
         }
         while (carriageOffset < 0) {
-            MSLane* prev = getPreviousLane(lane, routeIndex);
+            MSLane* prev = getPreviousLane(lane, furtherIndex);
             if (prev != lane) {
                 carriageOffset += prev->getLength();
             } else {
@@ -338,7 +338,7 @@ GUIVehicle::drawAction_drawCarriageClass(const GUIVisualizationSettings& s, bool
             lane = prev;
         }
         while (carriageBackOffset < 0) {
-            MSLane* prev = getPreviousLane(backLane, backRouteIndex);
+            MSLane* prev = getPreviousLane(backLane, backFurtherIndex);
             if (prev != backLane) {
                 carriageBackOffset += prev->getLength();
             } else {
@@ -666,24 +666,9 @@ GUIVehicle::drawRouteHelper(const GUIVisualizationSettings& s, const MSRoute& r,
 
 
 MSLane*
-GUIVehicle::getPreviousLane(MSLane* current, int& routeIndex) const {
-    if (current->isInternal()) {
-        return current->getIncomingLanes().front().lane;
-    }
-    if (routeIndex > 0) {
-        routeIndex--;
-        const MSEdge* prevNormal = myRoute->getEdges()[routeIndex];
-        for (MSLane* cand : prevNormal->getLanes()) {
-            for (MSLink* link : cand->getLinkCont()) {
-                if (link->getLane() == current) {
-                    if (link->getViaLane() != nullptr) {
-                        return link->getViaLane();
-                    } else {
-                        return const_cast<MSLane*>(link->getLaneBefore());
-                    }
-                }
-            }
-        }
+GUIVehicle::getPreviousLane(MSLane* current, int& furtherIndex) const {
+    if (furtherIndex < (int)myFurtherLanes.size()) {
+        return myFurtherLanes[furtherIndex++];
     }
     return current;
 }

src/guisim/GUIVehicle.h
@@ -174,7 +174,7 @@ private:
      * @param[in] current The lane of which the predecessor should be returned
      * @param[in,out] routeIndex The index of the current or previous non-internal edge in the route
      */
-    MSLane* getPreviousLane(MSLane* current, int& routeIndex) const;
+    MSLane* getPreviousLane(MSLane* current, int& furtherIndex) const;
 
     /// @brief retrieve information about the current stop state
     std::string getStopInfo() const;

commit b3a03ca935f951b89d7dd734ab475668b8b57181

fixed regression in [6ce8106ce1048de55a69383317988b069443be5f]. refs #6304

src/guisim/GUIVehicle.cpp
@@ -669,6 +669,51 @@ MSLane*
 GUIVehicle::getPreviousLane(MSLane* current, int& furtherIndex) const {
     if (furtherIndex < (int)myFurtherLanes.size()) {
         return myFurtherLanes[furtherIndex++];
+    } else {
+        // try to use route information
+        int routeIndex = getRoutePosition();
+        bool resultInternal;
+        if (MSGlobals::gUsingInternalLanes && MSNet::getInstance()->hasInternalLinks()) {
+            if (myLane->isInternal()) {
+                if (furtherIndex % 2 == 0) {
+                    routeIndex -= (furtherIndex + 0) / 2;
+                    resultInternal = false;
+                } else {
+                    routeIndex -= (furtherIndex + 1) / 2;
+                    resultInternal = false;
+                }
+            } else {
+                if (furtherIndex % 2 != 0) {
+                    routeIndex -= (furtherIndex + 1) / 2;
+                    resultInternal = false;
+                } else {
+                    routeIndex -= (furtherIndex + 2) / 2;
+                    resultInternal = true;
+                }
+            }
+        } else {
+            routeIndex -= furtherIndex;
+            resultInternal = false;
+        }
+        furtherIndex++;
+        if (routeIndex >= 0) {
+            if (resultInternal) {
+                const MSEdge* prevNormal = myRoute->getEdges()[routeIndex];
+                for (MSLane* cand : prevNormal->getLanes()) {
+                    for (MSLink* link : cand->getLinkCont()) {
+                        if (link->getLane() == current) {
+                            if (link->getViaLane() != nullptr) {
+                                return link->getViaLane();
+                            } else {
+                                return const_cast<MSLane*>(link->getLaneBefore());
+                            }
+                        }
+                    }
+                }
+            } else {
+                return myRoute->getEdges()[routeIndex]->getLanes()[0];
+            }
+        }
     }
     return current;
 }