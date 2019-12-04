02d794578e2069002485bc5140cf8f4abe3dae3f
namdre <jakob.erdmann@dlr.de>
2019-12-04 08:55:34 +0100

    added test for lane foe retrieval in intermodal network

---
+++ tests/complex/traci/pythonApi/lane_intermodal/runner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @author  Daniel Krajzewicz
+# @date    2011-03-04
+# @version $Id$
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+
+SUMO_HOME = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..")
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", SUMO_HOME), "tools"))
+import traci  # noqa
+import sumolib  # noqa
+
+traci.start([sumolib.checkBinary('sumo'), "-n", "input_net3.net.xml",
+    "--no-step-log"])
+traci.simulationStep()
+
+print("foes", traci.lane.getFoes("SC_3", "CN_3"))
+print("internal foes", traci.lane.getInternalFoes(":C_15_0"))
+
+traci.close()
