6be815f51ed0ef88665ffe1f015eecf386870333
namdre <jakob.erdmann@dlr.de>
2020-01-06 14:11:05 +0100

    addendum to [939ab08ccb98dd1293e01f6984875c8a525630c2]. refs #36

--- src/guisim/GUIPerson.cpp
+++ src/guisim/GUIPerson.cpp
@@ -412,12 +412,12 @@ GUIPerson::setFunctionalColor(int activeScheme) const {
             }
             return false;
         }
-        case 8: { // color by angle
+        case 9: { // color by angle
             double hue = GeomHelper::naviDegree(getAngle());
             GLHelper::setColor(RGBColor::fromHSV(hue, 1., 1.));
             return true;
         }
-        case 9: { // color randomly (by pointer)
+        case 10: { // color randomly (by pointer)
             const double hue = (long)this % 360; // [0-360]
             const double sat = (((long)this / 360) % 67) / 100.0 + 0.33; // [0.33-1]
             GLHelper::setColor(RGBColor::fromHSV(hue, sat, 1.));
