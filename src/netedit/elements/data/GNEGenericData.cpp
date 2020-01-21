6803c02e9d0290f2fa074e77db87c810f8bff610
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2020-01-21 08:36:10 +0100

    Added class GNEGenericData. Refs #6461

---
+++ src/netedit/elements/data/GNEGenericData.cpp
@@ -0,0 +1,90 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2019 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+// SPDX-License-Identifier: EPL-2.0
+/****************************************************************************/
+/// @file    GNEGenericData.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Jan 2020
+///
+// A abstract class for data elements
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#include <config.h>
+
+#include <netedit/GNEViewNet.h>
+
+#include "GNEGenericData.h"
+
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+// GNEGenericData - methods
+// ---------------------------------------------------------------------------
+
+GNEGenericData::GNEGenericData(const std::string& id, GNEViewNet* viewNet) :
+    GNEAttributeCarrier(SUMO_TAG_NOTHING),
+    myViewNet(viewNet) {
+}
+
+
+GNEGenericData::GNEGenericData(GNEGenericData* dataElementParent, GNEViewNet* viewNet) :
+    GNEAttributeCarrier(SUMO_TAG_NOTHING),
+    myViewNet(viewNet) {
+}
+
+
+GNEGenericData::~GNEGenericData() {}
+
+
+bool
+GNEGenericData::isGenericDataValid() const {
+    return true;
+}
+
+
+std::string
+GNEGenericData::getGenericDataProblem() const {
+    return "";
+}
+
+
+void
+GNEGenericData::fixGenericDataProblem() {
+    throw InvalidArgument(getTagStr() + " cannot fix any problem");
+}
+
+
+GNEViewNet*
+GNEGenericData::getViewNet() const {
+    return myViewNet;
+}
+
+
+bool
+GNEGenericData::isAttributeCarrierSelected() const {
+    return mySelected;
+}
+
+
+bool
+GNEGenericData::drawUsingSelectColor() const {
+    if (mySelected && (myViewNet->getEditModes().currentSupermode == GNE_SUPERMODE_DEMAND)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/****************************************************************************/
