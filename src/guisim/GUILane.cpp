59e5674f56733cf2c665b8a1d870f3106f2ee83b
namdre <jakob.erdmann@dlr.de>
2020-01-29 20:44:45 +0100

    fix #6575

--- src/guisim/GUILane.cpp
+++ src/guisim/GUILane.cpp
@@ -603,7 +603,7 @@ GUILane::drawGL(const GUIVisualizationSettings& s) const {
                 const double halfWidth = isInternal ? myQuarterLaneWidth : (myHalfLaneWidth - SUMO_const_laneMarkWidth / 2);
                 mustDrawMarkings = !isInternal && myPermissions != 0 && myPermissions != SVC_PEDESTRIAN && exaggeration == 1.0 && !isWaterway(myPermissions);
                 const int cornerDetail = drawDetails && !isInternal ? (int)(s.scale * exaggeration) : 0;
-                const double offset = halfWidth * MAX2(0., (exaggeration - 1));
+                const double offset = halfWidth * MAX2(0., (exaggeration - 1)) * (MSGlobals::gLefthand ? -1 : 1);
                 if (myShapeColors.size() > 0) {
                     GLHelper::drawBoxLines(myShape, myShapeRotations, myShapeLengths, myShapeColors, halfWidth * exaggeration, cornerDetail, offset);
                 } else {
