fa0d70f9e75f937b4409f0646681b3cd8c5a0229
namdre <jakob.erdmann@dlr.de>
2020-02-27 10:21:37 +0100

    now writing sorted plans

--- tools/import/matsim/matsim_importPlans.py
+++ tools/import/matsim/matsim_importPlans.py
@@ -25,6 +25,10 @@ from __future__ import print_function
 import os
 import sys
 import optparse
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 if 'SUMO_HOME' in os.environ:
     sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
@@ -66,61 +70,68 @@ def writeLeg(outf, options, idveh, leg, startLink, endLink):
 
 
 def main(options):
-    with open(options.outfile, 'w') as outf:
-        sumolib.writeXMLHeader(outf, "$Id: matsim_importPlans.py v1_3_1+0713-63b241ac79 michael.behrisch@dlr.de 2019-10-20 15:41:56 +0200 $", "routes")  # noqa
-        for person in sumolib.output.parse(options.planfile, 'person'):
+    persons = [] # (depart, xmlsnippet)
+    for person in sumolib.output.parse(options.planfile, 'person'):
+        outf = StringIO()
+        vehIndex = 0
+        plan = person.plan[0]
+        firstAct = plan.getChildList()[0]
+        depart = firstAct.start_time
+        if depart is None:
+            depart = options.defaultStart
+        attributes = person.attributes[0] if person.attributes else None
+        # write vehicles
+        vehicleslist = []
+        untillist = []
+        lastAct = None
+        lastLeg = None
+        for item in plan.getChildList():
+            leg = None
+            idveh = "%s_%s" % (person.id, vehIndex)
+            if "act" in item.name: # act or activity
+                if lastLeg is not None:
+                    leg = lastLeg
+                    leg.dep_time = lastAct.end_time
+                    writeLeg(outf, options, idveh, leg, lastAct.link, item.link)
+                    lastLeg = None
+                lastAct = item
+            if item.name == "leg":
+                if item.route is None:
+                    lastLeg = item
+                else:
+                    leg = item
+                    writeLeg(outf, options, idveh, leg, leg.route[0].start_link, leg.route[0].end_link)
+            if leg:
+                untillist.append(leg.dep_time)
+                vehicleslist.append(idveh)
+                vehIndex = vehIndex+1
+        untillist.append(lastAct.end_time if lastAct.end_time else options.defaultEnd)
+        # write person
+        if not options.carsOnly:
             vehIndex = 0
-            plan = person.plan[0]
-            attributes = person.attributes[0] if person.attributes else None
-            # write vehicles
-            vehicleslist = []
-            untillist = []
-            lastAct = None
+            outf.write('   <person id="%s" depart="%s">\n' % (person.id, depart))
+            if attributes is not None:
+                for attr in attributes.attribute:
+                    outf.write('       <param key="%s" value="%s"/>\n' % (attr.attr_name, attr.getText()))
+
             lastLeg = None
             for item in plan.getChildList():
-                leg = None
-                idveh = "%s_%s" % (person.id, vehIndex)
                 if "act" in item.name: # act or activity
                     if lastLeg is not None:
-                        leg = lastLeg
-                        leg.dep_time = lastAct.end_time
-                        writeLeg(outf, options, idveh, leg, lastAct.link, item.link)
-                        lastLeg = None
-                    lastAct = item
+                        outf.write('       <ride lines="%s" to="%s"  />\n' % (vehicleslist[vehIndex], item.link))
+                        vehIndex = vehIndex+1
+                    outf.write('       <stop lane="%s_0" until="%s" actType="%s" />\n' %
+                               (item.link, untillist[vehIndex], item.type))
                 if item.name == "leg":
-                    if item.route is None:
-                        lastLeg = item
-                    else:
-                        leg = item
-                        writeLeg(outf, options, idveh, leg, leg.route[0].start_link, leg.route[0].end_link)
-                if leg:
-                    untillist.append(leg.dep_time)
-                    vehicleslist.append(idveh)
-                    vehIndex = vehIndex+1
-            untillist.append(lastAct.end_time if lastAct.end_time else options.defaultEnd)
-            # write person
-            if not options.carsOnly:
-                vehIndex = 0
-                firstAct = plan.getChildList()[0]
-                depart = firstAct.start_time
-                if depart is None:
-                    depart = options.defaultStart
-                outf.write('   <person id="%s" depart="%s">\n' % (person.id, depart))
-                if attributes is not None:
-                    for attr in attributes.attribute:
-                        outf.write('       <param key="%s" value="%s"/>\n' % (attr.attr_name, attr.getText()))
+                    lastLeg = item
+            outf.write('   </person>\n')
+        persons.append((sumolib.miscutils.parseTime(depart), outf.getvalue()))
 
-                lastLeg = None
-                for item in plan.getChildList():
-                    if "act" in item.name: # act or activity
-                        if lastLeg is not None:
-                            outf.write('       <ride lines="%s" to="%s"  />\n' % (vehicleslist[vehIndex], item.link))
-                            vehIndex = vehIndex+1
-                        outf.write('       <stop lane="%s_0" until="%s" actType="%s" />\n' %
-                                   (item.link, untillist[vehIndex], item.type))
-                    if item.name == "leg":
-                        lastLeg = item
-                outf.write('   </person>\n')
+    persons.sort()
+    with open(options.outfile, 'w') as outf:
+        sumolib.writeXMLHeader(outf, "$Id: matsim_importPlans.py v1_3_1+0713-63b241ac79 michael.behrisch@dlr.de 2019-10-20 15:41:56 +0200 $", "routes")  # noqa
+        for depart, xml in persons:
+            outf.write(xml)
         outf.write('</routes>\n')
     outf.close()
 
