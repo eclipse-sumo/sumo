679e88fc07d71b29bc4efaaf2f213f9c630a7fd3
Robert Hilbrich <robert.hilbrich@dlr.de>
2020-02-28 12:55:17 +0100

    Added stubs for creation and destruction, refs #6533

--- src/fmi/fmi2Functions.c
+++ src/fmi/fmi2Functions.c
@@ -1,16 +1,32 @@
 #include "fmi2Functions.h"
 
+
+/* ***********************************************************************************************
+   * CREATION AND DESTRUCTION OF AN FMU
+   ***********************************************************************************************/
+
 /* The function returns a new instance of an FMU. If a null pointer is returned, then instantiation
    failed.*/
-
 fmi2Component 
 fmi2Instantiate(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID,
                               fmi2String fmuResourceLocation, const fmi2CallbackFunctions *functions,
                               fmi2Boolean visible, fmi2Boolean loggingOn)
 {
-
    /* For co-simulation, this function call has to perform all actions of a slave which are necessary
    before a simulation run starts */
-   functions->logger(functions->componentEnvironment, instanceName, fmi2Error, "error", "Not yet implemented");
+   if (loggingOn) 
+      functions->logger(functions->componentEnvironment, instanceName, fmi2Error, "error", "Not yet implemented");
+   
    return NULL;
+}
+
+/* Disposes the given instance, unloads the loaded model, and frees all the allocated memory
+and other resources that have been allocated by the functions of the FMU interface. */
+void 
+fmi2FreeInstance(fmi2Component c) {
+   if (!c) return;
+   
+   /* Frees the model instance */
+   // ModelInstance *comp = (ModelInstance *)c;
+   // c->freeMemory(c->componentEnvironment, 
 }
