563cdca0f0f93c5e91caff635154af95001e57ae
Robert Hilbrich <robert.hilbrich@dlr.de>
2020-02-28 12:40:15 +0100

    Adding a first logger call, refs #6533

--- src/fmi/fmi2Functions.c
+++ src/fmi/fmi2Functions.c
@@ -1,15 +1,16 @@
 #include "fmi2Functions.h"
 
-
 /* The function returns a new instance of an FMU. If a null pointer is returned, then instantiation
    failed.*/
 
-fmi2Component fmi2Instantiate(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID,
-                            fmi2String fmuResourceLocation, const fmi2CallbackFunctions *functions,
-                            fmi2Boolean visible, fmi2Boolean loggingOn) {
+fmi2Component 
+fmi2Instantiate(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID,
+                              fmi2String fmuResourceLocation, const fmi2CallbackFunctions *functions,
+                              fmi2Boolean visible, fmi2Boolean loggingOn)
+{
 
-/* For co-simulation, this function call has to perform all actions of a slave which are necessary
-before a simulation run starts */
-
-	return NULL;
+   /* For co-simulation, this function call has to perform all actions of a slave which are necessary
+   before a simulation run starts */
+   functions->logger(functions->componentEnvironment, instanceName, fmi2Error, "error", "Not yet implemented");
+   return NULL;
 }
