f21e668a2344847fc17a7555a336a1f6e15cd649
Robert Hilbrich <robert.hilbrich@dlr.de>
2020-02-28 12:08:38 +0100

    Adding fmi2 headers and first steps for fmi, refs #6533

---
+++ src/fmi/fmi2Functions.c
@@ -0,0 +1,15 @@
+#include "fmi2Functions.h"
+
+
+/* The function returns a new instance of an FMU. If a null pointer is returned, then instantiation
+   failed.*/
+
+fmi2Component fmi2Instantiate(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID,
+                            fmi2String fmuResourceLocation, const fmi2CallbackFunctions *functions,
+                            fmi2Boolean visible, fmi2Boolean loggingOn) {
+
+/* For co-simulation, this function call has to perform all actions of a slave which are necessary
+before a simulation run starts */
+
+	return NULL;
+}
---
+++ src/fmi/main.c
@@ -0,0 +1,4 @@
+#define FMI_VERSION 2
+
+#include "fmi2Functions.h"
+
