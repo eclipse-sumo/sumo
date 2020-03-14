192931613fd4030e5a23ea0cb1fa6b179f2985a0
namdre <jakob.erdmann@dlr.de>
2020-03-14 19:06:21 +0100

    added test refs #3361

--- tests/complex/traci/vehicle/vehicle/runner.py
+++ tests/complex/traci/vehicle/vehicle/runner.py
@@ -233,6 +233,23 @@ print("speedmode", traci.vehicle.getSpeedMode(vehID))
 print("lanechangemode", traci.vehicle.getLaneChangeMode(vehID))
 print("slope", traci.vehicle.getSlope(vehID))
 print("leader", traci.vehicle.getLeader("2"))
+leaderID, dist = traci.vehicle.getLeader("2")
+
+print("followSpeed", traci.vehicle.getFollowSpeed("2",
+    traci.vehicle.getSpeed("2"),
+    dist,
+    traci.vehicle.getSpeed(leaderID),
+    traci.vehicle.getDecel(leaderID),
+    leaderID))
+
+print("secureGap", traci.vehicle.getSecureGap("2",
+    traci.vehicle.getSpeed("2") * 3, # return something other than 0
+    traci.vehicle.getSpeed(leaderID),
+    traci.vehicle.getDecel(leaderID),
+    leaderID))
+
+print("stopSpeed", traci.vehicle.getStopSpeed("2", 15, 20))
+
 traci.vehicle.subscribeLeader("2")
 for i in range(6):
     print("step", step())
