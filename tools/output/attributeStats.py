9f6f2cf5b34959befc60b53709b2763412b11d56
namdre <jakob.erdmann@dlr.de>
2020-03-27 13:01:59 +0100

    added fast parsing refs #6839

--- tools/output/attributeStats.py
+++ tools/output/attributeStats.py
@@ -30,14 +30,14 @@ if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(os.path.join(tools))
     import sumolib
-    from sumolib.xml import parse  # noqa
+    from sumolib.xml import parse,parse_fast  # noqa
     from sumolib.miscutils import Statistics  # noqa
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
 
 def get_options():
-    USAGE = """Usage %prog [options] <tripinfos..xml>"""
+    USAGE = """Usage %prog [options] <tripinfos.xml>"""
     optParser = OptionParser(usage=USAGE)
     optParser.add_option("-v", "--verbose", action="store_true",
                          default=False, help="Give more output")
@@ -53,6 +53,8 @@ def get_options():
                          default=None, help="output file for histogram (gnuplot compatible)")
     optParser.add_option("--full-output", type="string",
                          default=None, help="output file for full data dump")
+    optParser.add_option("--fast", action="store_true",
+                         default=False, help="use fast parser (does not track missing data)")
     options, args = optParser.parse_args()
 
     if len(args) != 1:
@@ -69,13 +71,24 @@ def main():
     stats = Statistics("%s %ss" % (options.element, options.attribute), histogram=True, scale=options.binwidth)
     missingAttr = set()
     invalidType = set()
-    for element in parse(options.datafile, options.element, heterogeneous=True):
-        if element.hasAttribute(options.idAttr):
-            elementID = element.getAttribute(options.idAttr)
-        else:
-            elementID = None
-        if element.hasAttribute(options.attribute):
-            stringVal = element.getAttribute(options.attribute)
+
+    if options.fast:
+        def elements():
+            for element in parse_fast(options.datafile, options.element, [options.idAttr, options.attribute]):
+                yield getattr(element, options.idAttr), getattr(element, options.attribute)
+    else:
+        def elements():
+            for element in parse(options.datafile, options.element, heterogeneous=True):
+                elementID = None
+                if element.hasAttribute(options.idAttr):
+                    elementID = element.getAttribute(options.idAttr)
+                stringVal = None
+                if element.hasAttribute(options.attribute):
+                    stringVal = element.getAttribute(options.attribute)
+                yield elementID, stringVal
+
+    for elementID, stringVal in elements():
+        if stringVal is not None:
             try:
                 val = sumolib.miscutils.parseTime(stringVal)
                 vals[elementID].append(val)
