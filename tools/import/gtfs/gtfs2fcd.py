90db249114de37f639356de2f273790ab5e1361c
Michael Behrisch <michael.behrisch@dlr.de>
2020-04-30 19:48:56 +0200

    preliminary version of gtfs import, refs #4596

---
+++ tools/import/gtfs/gtfs2fcd.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file     gtfs2fcd.py
+@author   Michael Behrisch
+@author   Robert Hilbrich
+@date     2018-06-13
+@version  $Id: gtfs2fcd.py 7695 2019-04-10 12:49:04Z erdm_ja $
+
+Converts GTFS data into separate fcd traces for every distinct trip
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+import pandas as pd
+import zipfile
+import datetime
+import subprocess
+import io
+
+sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+import sumolib
+import traceExporter
+
+
+URMO = "berlin_rail"
+
+
+def add_options():
+    argParser = sumolib.options.ArgumentParser()
+    argParser.add_argument("-r", "--region", default=URMO,
+                           help="define the region to process")
+    argParser.add_argument("--gtfs", help="define gtfs zip file to load")
+    argParser.add_argument("--date", type=int, help="define the day to import")
+    argParser.add_argument("--fcd", help="directory to write / read the generated FCD files to / from")
+    argParser.add_argument("--gpsdat", help="directory to write / read the generated gpsdat files to / from")
+    argParser.add_argument("--encoding", default="latin1", help="input encoding")
+    argParser.add_argument("--verbose", action="store_true", default=False, help="tell me what you are doing")
+    return argParser
+
+
+def check_options(options):
+    if options.region == "moin":
+        options.gtfs = 'resources/moin/dataMDV2019TestSet.zip'
+        options.date = 20190522
+    elif options.gtfs is None:
+        options.gtfs = '../public-transit-berlin/GTFS_VBB_Juni-Dezember-2016.zip'
+        options.date = 20160713
+    if options.fcd is None:
+        options.fcd = os.path.join('fcd', options.region)
+    if options.gpsdat is None:
+        options.gpsdat = os.path.join('input', options.region)
+    return options
+
+
+def main(options):
+    if options.verbose:
+        print('Loading GTFS data "%s"' % options.gtfs)
+    gtfsZip = zipfile.ZipFile(options.gtfs)
+    routes = pd.read_csv(gtfsZip.open('routes.txt'))
+    stops = pd.read_csv(gtfsZip.open('stops.txt'))
+    stop_times = pd.read_csv(gtfsZip.open('stop_times.txt'))
+    trips = pd.read_csv(gtfsZip.open('trips.txt'))
+    calendar_dates = pd.read_csv(gtfsZip.open('calendar_dates.txt'))
+
+    # currently not used:
+    # agency = pd.read_csv(gtfsZip.open('agency.txt'))
+    # calendar = pd.read_csv(gtfsZip.open('calendar.txt'))
+    # transfers = pd.read_csv(gtfsZip.open('transfers.txt'))
+
+    # Merging the tables
+    tmp = pd.merge(trips, calendar_dates, on='service_id')
+    trips_on_day = tmp[tmp['date'] == options.date]
+    if options.region == "moin":
+        zones = pd.read_csv(gtfsZip.open('fare_stops.txt'))
+        stops_merged = pd.merge(pd.merge(stops, stop_times, on='stop_id'), zones, on='stop_id')
+    else:
+        stops_merged = pd.merge(stops, stop_times, on='stop_id')
+        stops_merged['fare_zone'] = ''
+        stops_merged['fare_token'] = ''
+        stops_merged['start_char'] = ''
+
+    trips_routes_merged = pd.merge(trips_on_day, routes, on='route_id')
+    full_data_merged = pd.merge(stops_merged,
+                                trips_routes_merged,
+                                on='trip_id')[['trip_id', 'route_id', 'route_short_name', 'route_type',
+                                               'stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'stop_sequence',
+                                               'fare_zone', 'fare_token', 'start_char', 'arrival_time', 'departure_time']].drop_duplicates()
+
+    gtfs_modes = {
+    # modes according to https://developers.google.com/transit/gtfs/reference/#routestxt
+        0:  'tram',
+        1:  'subway',
+        2:  'rail',
+        3:  'bus',
+        4:  'ship',
+        #5:  'cableTram',
+        #6:  'aerialLift',
+        #7:  'funicular',
+    # modes used in Berlin and MDV see https://developers.google.com/transit/gtfs/reference/extended-route-types
+        100:  'rail',        # DB
+        109:  'light_rail',  # S-Bahn
+        400:  'subway',      # U-Bahn
+        700:  'bus',         # Bus
+        714:  'bus',         # SEV
+        715:  'bus',         # Rufbus
+        900:  'tram',        # Tram
+        1000: 'ship',        # Faehre
+    # modes used by hafas
+        's'  : 'light_rail',
+        'RE' : 'rail',
+        'RB' : 'rail',
+        'IXB': 'rail',        # tbd
+        'ICE': 'rail_electric',
+        'IC' : 'rail_electric',
+        'IRX': 'rail',        # tbd
+        'EC' : 'rail',
+        'NJ' : 'rail',        # tbd
+        'RHI': 'rail',        # tbd
+        'DPN': 'rail',        # tbd
+        'SCH': 'rail',        # tbd
+        'Bsv': 'rail',        # tbd
+        'KAT': 'rail',        # tbd
+        'AIR': 'rail',        # tbd
+        'DPS': 'rail',        # tbd
+        'lt' : 'light_rail', #tbd
+        'BUS': 'bus',        # tbd
+        'Str': 'tram',        # tbd
+        'DPF': 'rail',        # tbd
+        '3'  : 'bus',        # tbd
+    }
+    fcdFile = {}
+    tripFile = {}
+    if not os.path.exists(options.fcd):
+        os.makedirs(options.fcd)
+    for mode in set(gtfs_modes.values()):
+        filePrefix = os.path.join(options.fcd, mode)
+        fcdFile[mode] = io.open(filePrefix + '.fcd.xml', 'w', encoding="utf8")
+        sumolib.writeXMLHeader(fcdFile[mode], "gtfs2fcd.py")
+        fcdFile[mode].write('<fcd-export>\n')
+        print('Writing fcd file "%s"' % fcdFile[mode].name)
+        tripFile[mode] = open(filePrefix + '.rou.xml', 'w')
+        tripFile[mode].write("<routes>\n")
+    timeIndex = 0
+    for _, trip_data in full_data_merged.groupby(['route_id']):
+        seqs = {}
+        for trip_id, data in trip_data.groupby(['trip_id']):
+            stopSeq = []
+            buf = ""
+            offset = 0
+            firstDep = None
+            for __, d in data.sort_values(by=['stop_sequence']).iterrows():
+                arrival = list(map(int, d.arrival_time.split(":")))
+                arrivalSec = arrival[0] * 3600 + arrival[1] * 60 + arrival[2] + timeIndex
+                stopSeq.append(d.stop_id)
+                departure = list(map(int, d.departure_time.split(":")))
+                departureSec = departure[0] * 3600 + departure[1] * 60 + departure[2] + timeIndex
+                until = 0 if firstDep is None else departureSec - timeIndex - firstDep
+                buf += ('    <timestep time="%s"><vehicle id="%s_%s" x="%s" y="%s" until="%s" name=%s fareZone="%s" fareSymbol="%s" startFare="%s" speed="20"/></timestep>\n' %
+                        (arrivalSec - offset, d.route_short_name, trip_id, d.stop_lon, d.stop_lat, until, 
+                            #sumolib.xml.quoteattr(d.stop_name).decode('latin1').encode('utf8'),
+                            sumolib.xml.quoteattr(d.stop_name),
+                            d.fare_zone, d.fare_token, d.start_char))
+                if firstDep is None:
+                    firstDep = departureSec - timeIndex
+                offset += departureSec - arrivalSec
+            mode = gtfs_modes[d.route_type]
+            s = tuple(stopSeq)
+            if s not in seqs:
+                seqs[s] = trip_id
+                fcdFile[mode].write(buf)
+                timeIndex = arrivalSec
+            tripFile[mode].write('    <vehicle id="%s" route="%s" type="%s" depart="%s" line="%s_%s"/>\n' %
+                                 (trip_id, seqs[s], mode, firstDep, d.route_short_name, seqs[s]))
+    if options.gpsdat:
+        if not os.path.exists(options.gpsdat):
+            os.makedirs(options.gpsdat)
+        for mode in set(gtfs_modes.values()):
+            fcdFile[mode].write('</fcd-export>\n')
+            fcdFile[mode].close()
+            tripFile[mode].write("</routes>\n")
+            tripFile[mode].close()
+            f = "gpsdat_%s.csv" % mode
+            traceExporter.main(['', '--base-date', '0', '-i', fcdFile[mode].name, '--gpsdat-output', f])
+            with open(f) as inp, open(os.path.join(options.gpsdat, f), "w") as outp:
+                for l in inp:
+                    outp.write(l[l.index("_")+1:])
+            os.remove(f)
+
+if __name__ == "__main__":
+    main(check_options(add_options().parse_args()))
---
+++ tools/import/gtfs/gtfs2pt.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+from __future__ import absolute_import
+import os,sys,glob,shutil, subprocess
+from collections import defaultdict
+
+sys.path += [os.path.join(os.environ["SUMO_HOME"], "tools"), os.path.join(os.environ['SUMO_HOME'], 'tools', 'route')] 
+import route2poly
+import sumolib
+
+import gtfs2fcd
+
+
+def get_options(args=None):
+    argParser = gtfs2fcd.add_options()
+    argParser.add_argument("--mapperlib", default="lib/fcd-process-chain-2.2.2.jar",
+                           help="mapping library to use")
+    argParser.add_argument("--map-output", help="directory to write the generated mapping files to")
+    argParser.add_argument("--map-output-config", default="conf/output_configuration_template.xml",
+                           help="output configuration template for the mapper library")
+    argParser.add_argument("--map-input-config", default="conf/input_configuration_template.xml",
+                           help="input configuration template for the mapper library")
+    argParser.add_argument("--map-parameter", default="conf/parameters_template.xml",
+                           help="parameter template for the mapper library")
+    argParser.add_argument("--poly-output", help="file to write the generated polygon files to")
+    argParser.add_argument("--route-output", default="publictransport.add.xml",
+                           help="file to write the generated public transport stops and routes to")
+    argParser.add_argument("--vehicle-output", default="publictransport.rou.xml",
+                           help="file to write the generated public transport vehicles to")
+    argParser.add_argument("-n", "--network", help="sumo network to use")
+    argParser.add_argument("--network-split", help="directory to write generated networks to")
+    argParser.add_argument("-b", "--begin", default=0,
+                           type=int, help="Defines the begin time to export")
+    argParser.add_argument("-e", "--end", default=86400,
+                           type=int, help="Defines the end time for the export")
+    argParser.add_argument("--skip-fcd", action="store_true", default=False, help="skip generation fcd data")
+
+    options = gtfs2fcd.check_options(argParser.parse_args(args=args))
+    if options.map_output is None:
+        options.map_output = os.path.join('output', options.region)
+    if options.network_split is None:
+        options.network_split = os.path.join('resources', options.region)
+    return options
+
+
+def splitNet(options):
+    netcCall = [sumolib.checkBinary("netconvert"), "--no-internal-links", "--numerical-ids", "--no-turnarounds", 
+                "--offset.disable-normalization", "--output.original-names",
+                "--junctions.corner-detail", "0", "--dlr-navteq.precision", "0", "--geometry.avoid-overlap", "false"]
+    if not os.path.exists(options.network_split):
+        os.makedirs(options.network_split)
+    numIdNet = os.path.join(options.network_split, "all.net.xml")
+    subprocess.call(netcCall + ["-s", options.network, "-o", numIdNet])
+    for inp in glob.glob(os.path.join(options.gpsdat, "gpsdat_*.csv")):
+        railType = os.path.basename(inp)[7:-4]
+        netPrefix = os.path.join(options.network_split, railType)
+        if railType == "bus":
+            ret = subprocess.call(netcCall + ["-s", numIdNet, "-o", os.path.join(options.network_split, railType + ".net.xml"),
+                                              "--dlr-navteq-output", netPrefix])
+        else:
+            edgeType = railType
+            if "rail" in railType:
+                edgeType = "railway." + railType
+            if railType == "tram":
+                edgeType = "railway.tram,highway.residential\|railway.tram"
+            ret = subprocess.call(netcCall + ["-s", numIdNet, "-o", os.path.join(options.network_split, railType + ".net.xml"),
+                                              "--dlr-navteq-output", netPrefix, "--dismiss-vclasses", "--keep-edges.by-type", edgeType])
+        if ret == 0:
+            yield inp, railType, netPrefix
+
+
+def mapFCD(options):
+    for o in glob.glob(os.path.join(options.map_output, "*.dat")):
+        os.remove(o)
+    outConf = os.path.join(os.path.dirname(options.map_output_config), "output_configuration.xml")
+    with open(options.map_output_config) as inp, open(outConf, "w") as outp:
+        outp.write(inp.read() % {"output": options.map_output})
+    for gpsdat, railType, netPrefix in splitNet(options):
+        conf = os.path.join(os.path.dirname(options.map_input_config), "input_configuration_%s.xml") % railType
+        with open(options.map_input_config) as inp, open(conf, "w") as outp:
+            outp.write(inp.read() % {"input": gpsdat, "net_prefix": netPrefix})
+        param = os.path.join(os.path.dirname(options.map_parameter), "parameters_%s.xml") % railType
+        with open(options.map_parameter) as inp, open(param, "w") as outp:
+            outp.write(inp.read() % {"radius": 100 if railType in ("bus", "tram") else 1000})
+        call = "java -mx16000m -jar %s %s %s %s" % (options.mapperlib, conf, outConf, param)
+        if options.verbose:
+            print(call)
+        sys.stdout.flush()
+        subprocess.call(call, shell=True)
+
+
+def traceMap(options):
+    for o in glob.glob("output/%s/*.dat" % options.region):
+        os.remove(o)
+
+
+def generate_polygons(net, routes, outfile):
+    colorgen = sumolib.miscutils.Colorgen(('random', 1, 1))
+    class PolyOptions:
+        internal = False
+        spread = 0.2
+        blur = 0
+        geo = True
+        layer = 100
+        
+    with open(outfile, 'w') as outf:
+        outf.write('<polygons>\n')
+        for vehID, edges in routes.items():
+            route2poly.generate_poly(PolyOptions, net, vehID, colorgen(), edges, outf)
+        outf.write('</polygons>\n')
+
+def map_stops(options, net, routes, rout):
+    stops = defaultdict(lambda:[])
+    stopDef = set()
+    rid = None
+    for inp in glob.glob(os.path.join(options.fcd, "*.fcd.xml")):
+        railType = os.path.basename(inp)[:-8]
+        typedNetFile = os.path.join(options.network_split, railType + ".net.xml")
+        if not os.path.exists(typedNetFile):
+            print("Warning! No net", typedNetFile)
+            continue
+        if options.verbose:
+            print("Reading", typedNetFile)
+        typedNet = sumolib.net.readNet(typedNetFile)
+        seen = set()
+        fixed = set()
+        for veh in sumolib.xml.parse_fast(inp, "vehicle", ("id", "x", "y", "until", "name", "fareZone", "fareSymbol", "startFare")):
+            addAttrs = ' friendlyPos="true" name="%s"' % veh.attr_name
+            params = ""
+            if options.region == "moin":
+                params = "".join(['        <param key="%s" value="%s"/>\n' % p for p in (('fareZone', veh.fareZone), ('fareSymbol', veh.fareSymbol), ('startFare', veh.startFare))])
+            if rid != veh.id.split("_")[1]:
+                lastIndex = 0
+                rid = veh.id.split("_")[1]
+            if rid not in routes:
+                if rid not in seen:
+                    print("Warning! Not mapped", rid)
+                    seen.add(rid)
+                continue
+            route = routes[rid]
+            if rid not in fixed:
+                routeFixed = [route[0]]
+                for routeEdgeID in route[1:]:
+                    path, _ = typedNet.getShortestPath(typedNet.getEdge(routeFixed[-1]), typedNet.getEdge(routeEdgeID))
+                    if path is None:
+                        print("Warning! Not connected", rid)
+                        routeFixed.append(routeEdgeID)
+                    elif 2 < len(path) < 10:
+                        print("Warning! Fixed connection", rid, len(path))
+                        routeFixed += [e.getID() for e in path[1:]]
+                    else:
+                        routeFixed.append(routeEdgeID)
+                route = routes[rid] = routeFixed
+                fixed.add(rid)
+            p = typedNet.convertLonLat2XY(float(veh.x), float(veh.y))
+            found = False
+            for edge, dist in sorted(typedNet.getNeighboringEdges(*p, r=200), key=lambda i:i[1]):
+                if edge.getID() in route[lastIndex:]:
+                    lastIndex = route.index(edge.getID(), lastIndex)
+                    pos = edge.getClosestLanePosDist(p)[1]
+                    stop = "%s:%.2f" % (edge.getID(), pos)
+                    if not stop in stopDef:
+                        stopDef.add(stop)
+                        if railType == "bus":
+                            for l in edge.getLanes():
+                                if l.allows(railType):
+                                    break
+                            startPos = max(0, pos - 10)
+                            rout.write('    <busStop id="%s" lane="%s" startPos="%s" endPos="%s"%s>\n%s    </busStop>\n' % (stop, l.getID(), startPos, pos + 10, addAttrs, params))
+                        else:
+                            startPos = max(0, pos - 60)
+                            rout.write('    <trainStop id="%s" lane="%s_0" startPos="%s" endPos="%s"%s>\n%s' % (stop, edge.getID(), startPos, pos + 60, addAttrs, params))
+                            ap = net.convertLonLat2XY(float(veh.x), float(veh.y))
+                            numAccess = 0
+                            for accessEdge, _ in sorted(net.getNeighboringEdges(*ap, r=100), key=lambda i:i[1]):
+                                if accessEdge.getID() != edge.getID() and accessEdge.allows("passenger") and numAccess < 10:
+                                    _, accessPos, accessDist = accessEdge.getClosestLanePosDist(ap)
+                                    rout.write('        <access friendlyPos="true" lane="%s_0" pos="%s" length="%s"/>\n' % (accessEdge.getID(), accessPos, 1.5 * accessDist))
+                                    numAccess += 1
+                            rout.write('    </trainStop>\n')
+                    stops[rid].append((stop, veh.until))
+                    found = True
+                    break
+            if not found:
+                print("Warning! No stop for", p, "on", veh)
+    return stops
+
+def filter_trips(options, routes, stops, outfile, begin, end):
+    numDays = end // 86400
+    if end % 86400 != 0:
+        numDays += 1
+    with open(outfile, 'w') as outf:
+        sumolib.xml.writeHeader(outf, os.path.basename(__file__), "routes")
+        for inp in glob.glob(os.path.join(options.fcd, "*.rou.xml")):
+            for veh in sumolib.xml.parse_fast(inp, "vehicle", ("id", "route", "type", "depart", "line")):
+                if veh.route in routes and len(routes[veh.route]) > 0 and veh.route in stops and len(stops[veh.route]) > 1:
+                    for d in range(numDays):
+                         depart = d * 86400 + int(veh.depart)
+                         if begin <= depart < end:
+                             outf.write('    <vehicle id="%s.%s" route="%s" type="%s" depart="%s" line="%s"/>\n' % (veh.id, d, veh.route, veh.type, depart, veh.line))
+        outf.write('</routes>\n')
+
+def main(options):
+    if not options.skip_fcd:
+        gtfs2fcd.main(options)
+    if os.path.exists(options.mapperlib):
+        mapFCD(options)
+    else:
+        print("Warning! No mapping library found, falling back to tracemapper.")
+        traceMap(options)
+    net = sumolib.net.readNet(options.network)
+    routes = defaultdict(lambda:[])
+    for o in glob.glob(os.path.join(options.map_output, "*.dat")):
+        for line in open(o):
+            time, edge, speed, coverage, id, minute_of_week = line.split('\t')[:6]
+            routes[id].append(edge)
+    if options.poly_output:
+        generate_polygons(net, routes, options.poly_output)
+    with open(options.route_output, 'w') as rout:
+        sumolib.xml.writeHeader(rout, os.path.basename(__file__), "additional")
+        stops = map_stops(options, net, routes, rout)
+        for vehID, edges in routes.items():
+            if edges:
+                rout.write('    <route id="%s" edges="%s">\n' % (vehID, " ".join(edges)))
+                for stop in stops[vehID]:
+                    rout.write('        <stop busStop="%s" duration="10" until="%s"/>\n' % stop)
+                rout.write('    </route>\n')
+            else:
+                print("Warning! Empty route", vehID)
+        rout.write('</additional>\n')
+    if options.region == gtfs2fcd.URMO:
+        filter_trips(options, routes, stops, options.vehicle_output, 23 * 3600, 49 * 3600)
+    else:
+        filter_trips(options, routes, stops, options.vehicle_output, options.begin, options.end)
+
+if __name__ == "__main__":
+    main(get_options())
