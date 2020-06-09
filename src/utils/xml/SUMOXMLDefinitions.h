1bf21e1a52fa4f132492b9da1b4e552c25969eef
namdre <jakob.erdmann@dlr.de>
2020-06-09 10:10:33 +0200

    refactoring refs #12

--- src/utils/xml/SUMOXMLDefinitions.h
+++ src/utils/xml/SUMOXMLDefinitions.h
@@ -123,16 +123,12 @@ enum SumoXMLTag {
     SUMO_TAG_ROUTES,
     /// @brief description of a vehicle
     SUMO_TAG_VEHICLE,
-    /// @brief description of a vehicle with an embedded route (used in NETEDIT)
-    GNE_TAG_VEHICLEWITHROUTE,
     /// @brief description of a vehicle type
     SUMO_TAG_VTYPE,
     /// @brief description of a person type (used in NETEDIT)
     SUMO_TAG_PTYPE,
     /// @brief begin/end of the description of a route
     SUMO_TAG_ROUTE,
-    /// @brief embedded route (used in NETEDIT)
-    GNE_TAG_ROUTE_EMBEDDED,
     /// @brief description of a logic request within the junction
     SUMO_TAG_REQUEST,
     /// @brief a source
@@ -155,10 +151,6 @@ enum SumoXMLTag {
     SUMO_TAG_TRIP_TAZ,
     /// @brief a flow definitio nusing a from-to edges instead of a route (used by router)
     SUMO_TAG_FLOW,
-    /// @brief a flow definition nusing a route instead of a from-to edges route (used in NETEDIT)
-    GNE_TAG_FLOW_ROUTE,
-    /// @brief description of a vehicle with an embedded route (used in NETEDIT)
-    GNE_TAG_FLOW_WITHROUTE,
     /// @brief a flow definition within in Calibrator (used in NETEDIT)
     SUMO_TAG_FLOW_CALIBRATOR,
     /// @brief a flow state definition (used when saving and loading simulatino state)
@@ -390,11 +382,13 @@ enum SumoXMLTag {
     GNE_TAG_PERSONSTOP_BUSSTOP,
     GNE_TAG_PERSONSTOP_EDGE,
     /// @brief description of a vehicle with an embedded route (used in NETEDIT)
-    GNE_TAG_VEHICLE_EMBEDDED,
+    GNE_TAG_VEHICLEWITHROUTE,
     /// @brief embedded route (used in NETEDIT)
-    GNE_TAG_EMBEDDEDROUTE,
+    GNE_TAG_ROUTE_EMBEDDED,
+    /// @brief a flow definition nusing a route instead of a from-to edges route (used in NETEDIT)
+    GNE_TAG_FLOW_ROUTE,
     /// @brief description of a vehicle with an embedded route (used in NETEDIT)
-    GNE_TAG_FLOW_EMBEDDED,
+    GNE_TAG_FLOW_WITHROUTE,
     /// @brief description of a person with an embedded route (used in NETEDIT)
     GNE_TAG_PERSON_EMBEDDED
     /// @}
