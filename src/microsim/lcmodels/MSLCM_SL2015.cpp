3d9467bb529477a253e30e4539c0efddb8c7ad31
namdre <jakob.erdmann@dlr.de>
2020-06-10 23:42:59 +0200

    disabled debug code

--- src/microsim/lcmodels/MSLCM_SL2015.cpp
+++ src/microsim/lcmodels/MSLCM_SL2015.cpp
@@ -86,23 +86,23 @@
 // ===========================================================================
 // Debug flags
 // ===========================================================================
-#define DEBUG_ACTIONSTEPS
-#define DEBUG_STATE
-#define DEBUG_SURROUNDING
-#define DEBUG_MANEUVER
-#define DEBUG_COMMITTED_SPEED
-#define DEBUG_PATCHSPEED
-#define DEBUG_INFORM
-#define DEBUG_ROUNDABOUTS
-#define DEBUG_WANTSCHANGE
-#define DEBUG_COOPERATE
-#define DEBUG_SLOWDOWN
-#define DEBUG_SAVE_BLOCKER_LENGTH
-#define DEBUG_BLOCKING
-#define DEBUG_TRACI
-#define DEBUG_STRATEGIC_CHANGE
-#define DEBUG_KEEP_LATGAP
-#define DEBUG_EXPECTED_SLSPEED
+//#define DEBUG_ACTIONSTEPS
+//#define DEBUG_STATE
+//#define DEBUG_SURROUNDING
+//#define DEBUG_MANEUVER
+//#define DEBUG_COMMITTED_SPEED
+//#define DEBUG_PATCHSPEED
+//#define DEBUG_INFORM
+//#define DEBUG_ROUNDABOUTS
+//#define DEBUG_WANTSCHANGE
+//#define DEBUG_COOPERATE
+//#define DEBUG_SLOWDOWN
+//#define DEBUG_SAVE_BLOCKER_LENGTH
+//#define DEBUG_BLOCKING
+//#define DEBUG_TRACI
+//#define DEBUG_STRATEGIC_CHANGE
+//#define DEBUG_KEEP_LATGAP
+//#define DEBUG_EXPECTED_SLSPEED
 //#define DEBUG_COND (myVehicle.getID() == "moped.18" || myVehicle.getID() == "moped.16")
 //#define DEBUG_COND (myVehicle.getID() == "Togliatti_71_0")
 #define DEBUG_COND (myVehicle.isSelected())
