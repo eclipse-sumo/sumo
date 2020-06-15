8365a744bbe34b240c6e5625b7e4c22e3d2af7b2
namdre <jakob.erdmann@dlr.de>
2020-06-15 22:34:59 +0200

    added test refs #7161

---
+++ tests/complex/traci/polygon/add_large/runner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2020 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @author  Daniel Krajzewicz
+# @date    2011-03-04
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+
+SUMO_HOME = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..")
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", SUMO_HOME), "tools"))
+import traci  # noqa
+import sumolib  # noqa
+
+traci.start([sumolib.checkBinary('sumo'), "-c", "sumo.sumocfg"])
+polygonID = "0"
+print("adding", polygonID)
+shape = [(x, x) for x in range(300)]
+traci.polygon.add(polygonID, shape, (255, 0, 0), True, "test")
+readShape = traci.polygon.getShape(polygonID)
+print("shapeLen=%s" % len(readShape))
+
+traci.close()
