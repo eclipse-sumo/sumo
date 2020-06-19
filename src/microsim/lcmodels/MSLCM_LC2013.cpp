79baa9eb18241b233204859a3ce2146130ea99b8
namdre <jakob.erdmann@dlr.de>
2020-06-19 23:28:19 +0200

    fix #6558

--- src/microsim/lcmodels/MSLCM_LC2013.cpp
+++ src/microsim/lcmodels/MSLCM_LC2013.cpp
@@ -35,6 +35,7 @@
 #include <microsim/MSLane.h>
 #include <microsim/MSDriverState.h>
 #include <microsim/MSNet.h>
+#include "MSLCHelper.h"
 #include "MSLCM_LC2013.h"
 
 
@@ -1236,7 +1237,7 @@ MSLCM_LC2013::_wantsChange(
 
     // Next we assign to roundabout edges a larger distance than to normal edges
     // in order to decrease sense of lc urgency and induce higher usage of inner roundabout lanes.
-    const double roundaboutBonus = getRoundaboutDistBonus(curr, neigh, best);
+    const double roundaboutBonus = MSLCHelper::getRoundaboutDistBonus(myVehicle, myRoundaboutBonus, curr, neigh, best);
     currentDist += roundaboutBonus;
     neighDist += roundaboutBonus;
 
@@ -1799,158 +1800,6 @@ MSLCM_LC2013::anticipateFollowSpeed(const std::pair<MSVehicle*, double>& leaderD
         }
     }
     return futureSpeed;
-}
-
-
-double
-MSLCM_LC2013::getRoundaboutDistBonus(const MSVehicle::LaneQ& curr, const MSVehicle::LaneQ& neigh, const MSVehicle::LaneQ& best) {
-    if (isOpposite()) {
-        return 0;
-    }
-    const MSVehicle::LaneQ& inner = curr.lane == best.lane ? neigh : curr;
-
-    int roundaboutJunctionsAhead = 0;
-    bool enteredRoundabout = false;
-    double seen = -myVehicle.getPositionOnLane();
-
-    // first check using only normal lanes
-    for (int i = 0; i < (int)best.bestContinuations.size(); i++) {
-        MSLane* lane = best.bestContinuations[i];
-        if (lane == nullptr) {
-            lane = myVehicle.getLane();
-        }
-        if ((!enteredRoundabout || lane->getEdge().isRoundabout()) && i >= (int)inner.bestContinuations.size()) {
-            // no bonus if we cannot continue on the inner lane until leaving the roundabout
-#ifdef DEBUG_WANTS_CHANGE
-            if (debugVehicle()) {
-                std::cout << "   noBonus: inner does not continue (lane=" << lane->getID() << ")\n";
-            }
-#endif
-            return 0;
-        }
-        if (seen > 300) {
-            // avoid long look-ahead
-#ifdef DEBUG_WANTS_CHANGE
-            if (debugVehicle()) {
-                std::cout << "   noBonus: seen=" << seen << " (lane=" << lane->getID() << ")\n";
-            }
-#endif
-            return 0;
-        }
-        const MSJunction* junction = lane->getEdge().getToJunction();
-        if (lane->getEdge().isRoundabout()) {
-            enteredRoundabout = true;
-            if (junction->getIncoming().size() + junction->getOutgoing().size() > 2) {
-                roundaboutJunctionsAhead++;
-            }
-        } else if (enteredRoundabout) {
-            // only check the first roundabout
-            break;
-        }
-        seen += lane->getLength();
-    }
-    // no bonus if we want to take the next exit
-    if (roundaboutJunctionsAhead < 2) {
-        return 0;
-    }
-
-    // compute bonus value based on jamming and exact distances (taking into
-    // account internal lanes)
-    double occupancyOuter = 0;
-    double occupancyInner = 0;
-    double distanceInRoundabout = 0;
-    MSLane* prevNormal = nullptr;
-    MSLane* prevInner = nullptr;
-    enteredRoundabout = false;
-    for (int i = 0; i < (int)best.bestContinuations.size(); i++) {
-        MSLane* lane = best.bestContinuations[i];
-        if (lane == nullptr) {
-            continue;
-        }
-        if (lane->getEdge().isRoundabout()) {
-            enteredRoundabout = true;
-        } else if (enteredRoundabout) {
-            // only check the first roundabout
-            break;
-        }
-        MSLane* via = nullptr;
-        if (prevNormal != nullptr) {
-            for (MSLink* link : prevNormal->getLinkCont()) {
-                if (link->getLane() == lane) {
-                    via = link->getViaLane();
-                }
-            }
-        }
-        if (enteredRoundabout) {
-            distanceInRoundabout += lane->getLength();
-            if (via != nullptr) {
-                distanceInRoundabout += via->getLength();
-            }
-        }
-        // discount vehicles that are upstream from ego
-        const double upstreamDiscount = &lane->getEdge() == &myVehicle.getLane()->getEdge()
-                                        ? (lane->getLength() - myVehicle.getPositionOnLane()) / lane->getLength() : 1;
-        prevNormal = lane;
-        occupancyOuter += upstreamDiscount * lane->getBruttoVehLenSum();
-        if (debugVehicle()) {
-            std::cout << " lane=" << lane->getID() << " occ=" << lane->getBruttoVehLenSum() << " discount=" << upstreamDiscount << " outer=" << occupancyOuter << "\n";
-        }
-        if (via != nullptr) {
-            occupancyOuter += via->getBruttoVehLenSum();
-            if (debugVehicle()) {
-                std::cout << " via=" << via->getID() << " occ=" << via->getBruttoVehLenSum() << " outer=" << occupancyOuter << "\n";
-            }
-        }
-        if (i < (int)inner.bestContinuations.size()) {
-            MSLane* innerLane = inner.bestContinuations[i];
-            occupancyInner += upstreamDiscount * innerLane->getBruttoVehLenSum();
-            if (debugVehicle()) {
-                std::cout << " inner=" << innerLane->getID() << " occ=" << innerLane->getBruttoVehLenSum() << " discount=" << upstreamDiscount << " inner=" << occupancyInner << "\n";
-            }
-            if (prevInner != nullptr) {
-                for (MSLink* link : prevInner->getLinkCont()) {
-                    if (link->getLane() == innerLane && link->getViaLane() != nullptr) {
-                        occupancyInner += link->getViaLane()->getBruttoVehLenSum();
-                        if (debugVehicle()) {
-                            std::cout << " innerVia=" << link->getViaLane()->getID() << " occ=" << link->getViaLane()->getBruttoVehLenSum() << " inner=" << occupancyInner << "\n";
-                        }
-                    }
-                }
-            }
-            prevInner = innerLane;
-        }
-    }
-
-#ifdef DEBUG_WANTS_CHANGE
-    if (debugVehicle()) {
-        std::cout << "   distanceInRoundabout=" << distanceInRoundabout
-                  << " roundaboutJunctionsAhead=" << roundaboutJunctionsAhead
-                  << " occupancyInner=" << occupancyInner
-                  << " occupancyOuter=" << occupancyOuter
-                  << "\n";
-    }
-#endif
-
-    const double maxOccupancy = MAX2(occupancyInner, occupancyOuter);
-    if (maxOccupancy == 0) {
-        // no bonues if the roundabout is empty
-        return 0;
-    }
-    // give some bonus for using the inside lane at equal occupancy
-    const double bonus = roundaboutJunctionsAhead * 7.5;
-    const double relativeJam = (occupancyOuter - occupancyInner + bonus) / (maxOccupancy + bonus);
-    // no bonus if the inner lane or the left lane entering the roundabout is jammed
-    const double jamFactor = MAX2(0.0, relativeJam);
-    const double result = distanceInRoundabout * jamFactor * myRoundaboutBonus * 9; // the 9 is abitrary and only there for backward compatibility
-#ifdef DEBUG_WANTS_CHANGE
-    if (debugVehicle()) {
-        std::cout << "   relativeJam=" << relativeJam
-                  << " jamFactor=" << jamFactor
-                  << " distanceBonus=" << result
-                  << "\n";
-    }
-#endif
-    return result;
 }
 
 
