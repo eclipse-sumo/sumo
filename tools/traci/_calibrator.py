f50ab83d4b39241b44de17f26cddb1264fa6de8f
Yun-Pang Flötteröd <yun-pang.floetteroed@dlr.de>
2020-06-23 10:53:15 +0200

    use getCmd instead of struct and connection #7160

--- tools/traci/_calibrator.py
+++ tools/traci/_calibrator.py
@@ -113,15 +113,4 @@ class CalibratorDomain(Domain):
         """setFlow(string, double, double, double, double, string, string, string, string) -> None
         Update or add a calibrator interval
         """
-        messageString = struct.pack("!Bi", tc.TYPE_COMPOUND, 8)
-        messageString += struct.pack("!Bd", tc.TYPE_DOUBLE, begin)
-        messageString += struct.pack("!Bd", tc.TYPE_DOUBLE, end)
-        messageString += struct.pack("!Bd", tc.TYPE_DOUBLE, vehsPerHour)
-        messageString += struct.pack("!Bd", tc.TYPE_DOUBLE, speed)
-        for val in (typeID, routeID, departLane, departSpeed):
-            val = str(val)
-            messageString += struct.pack("!Bi", tc.TYPE_STRING, len(val)) + val.encode("latin1")
-        self._connection._beginMessage(
-            tc.CMD_SET_CALIBRATOR_VARIABLE, tc.CMD_SET_FLOW, calibratorID, len(messageString))
-        self._connection._string += messageString
-        self._connection._sendExact()
+        self._setCmd(tc.CMD_SET_FLOW, calibratorID, "tddddssss",8, begin, end, vehsPerHour, speed, typeID, routeID, departLane, departSpeed)
