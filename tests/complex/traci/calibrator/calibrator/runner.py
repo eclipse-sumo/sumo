9c2c742cdd90dcf0d0eab01c5563a8573fb665cc
Yun-Pang Flötteröd <yun-pang.floetteroed@dlr.de>
2020-06-23 13:52:47 +0200

    change the print format, #7160

--- tests/complex/traci/calibrator/calibrator/runner.py
+++ tests/complex/traci/calibrator/calibrator/runner.py
@@ -54,7 +54,11 @@ for cali in traci.calibrator.getIDList():
 
 traci.calibrator.setFlow("cali_WE", 100, 200, 1800, 15, "DEFAULT_VEHTYPE", "WS")
 traci.calibrator.setFlow("cali_SN", 0, 200, 300, -1, "DEFAULT_VEHTYPE", "SN")
-print("calibrator: %s, period: %s - %s, setFlow: %s" %("cali_SN", traci.calibrator.getBegin("cali_SN"), traci.calibrator.getEnd("cali_SN"), traci.calibrator.getVehsPerHour("cali_SN")))
+print("calibrator: %s, period: %s - %s, setFlow: %s" % (
+    "cali_SN",
+    traci.calibrator.getBegin("cali_SN"),
+    traci.calibrator.getEnd("cali_SN"),
+    traci.calibrator.getVehsPerHour("cali_SN")))
 
 for step in range(250):
     traci.simulationStep()
