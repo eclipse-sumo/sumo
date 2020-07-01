5defde34e35e56ca2c737de4d93d8ad2f1ebb0da
namdre <jakob.erdmann@dlr.de>
2020-07-01 20:04:56 +0200

    fix #7256

--- src/microsim/devices/MSRoutingEngine.cpp
+++ src/microsim/devices/MSRoutingEngine.cpp
@@ -318,7 +318,7 @@ MSRoutingEngine::initRouter(SUMOVehicle* vehicle) {
     } else if (routingAlgorithm == "CH") {
         const SUMOTime weightPeriod = myAdaptationInterval > 0 ? myAdaptationInterval : SUMOTime_MAX;
         router = new CHRouter<MSEdge, SUMOVehicle>(
-            MSEdge::getAllEdges(), true, myEffortFunc, vehicle->getVClass(), weightPeriod, true, false);
+            MSEdge::getAllEdges(), true, myEffortFunc, vehicle == nullptr ? SVC_PASSENGER : vehicle->getVClass(), weightPeriod, true, false);
     } else if (routingAlgorithm == "CHWrapper") {
         const SUMOTime weightPeriod = myAdaptationInterval > 0 ? myAdaptationInterval : SUMOTime_MAX;
         router = new CHRouterWrapper<MSEdge, SUMOVehicle>(
