158c9717c3cac9290d33c7390735db08a14b482a
namdre <jakob.erdmann@dlr.de>
2020-07-02 13:16:34 +0200

    simplified test definition refs #7265

--- tests/complex/traci/person/person/runner.py
+++ tests/complex/traci/person/person/runner.py
@@ -226,32 +226,16 @@ for i in range(5):
 traci.person.add("p3", "1fi", -10)
 stage = traci.simulation.Stage(
     type=traci.constants.STAGE_WALKING,
-    vType="", line="", destStop="",
-    edges=["1fi", "1si"],
-    travelTime=-1, cost=-1, length=-1,
-    intended="", depart=-1, departPos=-20, arrivalPos=10,
-    description="foo")
+    edges=["1fi", "1si"], departPos=-20, arrivalPos=10, description="foo")
 stage2 = traci.simulation.Stage(
     type=traci.constants.STAGE_WALKING,
-    vType="car", line="", destStop="",
-    edges=["1fi", "1o"],
-    travelTime=-1, cost=-1, length=-1,
-    intended="", depart=-1, departPos=-20, arrivalPos=10,
-    description="foo")
+    vType="car", edges=["1fi", "1o"], departPos=-20, arrivalPos=10, description="foo")
 stage3 = traci.simulation.Stage(
     type=traci.constants.STAGE_WALKING,
-    vType="car", line="", destStop="",
-    edges=["1o", "3o"],
-    travelTime=-1, cost=-1, length=-1,
-    intended="", depart=-1, departPos=-20, arrivalPos=10,
-    description="foo")
+    vType="car", edges=["1o", "3o"], departPos=-20, arrivalPos=10, description="foo")
 stage4 = traci.simulation.Stage(
     type=traci.constants.STAGE_WALKING,
-    vType="car", line="", destStop="",
-    edges=["1o", "4o"],
-    travelTime=-1, cost=-1, length=-1,
-    intended="", depart=-1, departPos=-20, arrivalPos=10,
-    description="foo")
+    vType="car", edges=["1o", "4o"],departPos=-20, arrivalPos=10, description="foo")
 
 traci.person.appendStage("p3", stage)
 for i in range(10):
