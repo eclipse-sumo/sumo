2442c1dd27601f37fce5dd3762cf08d99d9c5ab2
Michael Behrisch <michael.behrisch@dlr.de>
2020-07-29 22:14:47 +0200

    enabling ccache, refs #3

--- tools/build/build_wheels.sh
+++ tools/build/build_wheels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum-config-manager --add-repo=https://download.opensuse.org/repositories/home:/behrisch/CentOS_7/
-yum install -y --nogpgcheck cmake3 xerces-c-devel proj-devel fox16-devel bzip2-devel swig3
+yum install -y --nogpgcheck cmake3 ccache xerces-c-devel proj-devel fox16-devel bzip2-devel swig3
 /opt/python/cp38-cp38/bin/pip install scikit-build
 
 /opt/python/cp38-cp38/bin/python tools/build/setup-sumo.py --cmake-executable cmake3 -j 8 bdist_wheel
