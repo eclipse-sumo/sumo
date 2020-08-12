4697cb0822b7ace7552e423b6306185b0c54b878
namdre <jakob.erdmann@dlr.de>
2020-08-12 23:22:41 +0200

    can now setting walking speed to 0 via traci

--- src/microsim/transportables/MSPerson.cpp
+++ src/microsim/transportables/MSPerson.cpp
@@ -300,7 +300,7 @@ MSPerson::MSPersonStage_Walking::moveToNextEdge(MSTransportable* person, SUMOTim
 
 double
 MSPerson::MSPersonStage_Walking::getMaxSpeed(const MSTransportable* const person) const {
-    return mySpeed > 0 ? mySpeed : person->getVehicleType().getMaxSpeed() * person->getSpeedFactor();
+    return mySpeed >= 0 ? mySpeed : person->getVehicleType().getMaxSpeed() * person->getSpeedFactor();
 }
 
 std::string
