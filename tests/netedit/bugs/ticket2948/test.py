328330ef943a274c9332d1569c4547df48f21153
Alvarez Lopez, Pablo <pablo.alvarezlopez@dlr.de>
2020-08-21 14:22:04 +0200

    Patching NETEDIT bugs expected results. Refs #3045 and #457

--- tests/netedit/bugs/ticket2948/test.py
+++ tests/netedit/bugs/ticket2948/test.py
@@ -57,13 +57,13 @@ netedit.selectionInvert()
 netedit.inspectMode()
 
 # inspect set of junctions
-netedit.leftClick(referencePosition, 420, 140)
+netedit.leftClick(referencePosition, 405, 240)
 
 # Set all Junctions as traffic lighs
 netedit.modifyAttribute(0, "traffic_light", True)
 
 # inspect set of edges
-netedit.leftClick(referencePosition, 450, 140)
+netedit.leftClick(referencePosition, 390, 300)
 
 # change all speed of edges
 netedit.modifyAttribute(0, "20", False)
