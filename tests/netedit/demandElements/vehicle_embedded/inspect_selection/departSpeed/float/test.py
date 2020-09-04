9f0191d51de6968b8137ae123e261c3cb4b4742f
Alvarez Lopez, Pablo <pablo.alvarezlopez@dlr.de>
2020-09-04 18:33:05 +0200

    Added NETEDIT test demandElements\vehicle_embedded\inspect_selection. Refs #3045 and #457

---
+++ tests/netedit/demandElements/vehicle_embedded/inspect_selection/departSpeed/float/test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2020 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2019-07-16
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot, ['--gui-testing-debug-gl'])
+
+# go to demand mode
+netedit.supermodeDemand()
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvertDemand()
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect vehicle
+netedit.leftClick(referencePosition, 91, 413)
+
+# change departSpeed with an invalid value
+netedit.modifyAttribute(5, "", True)
+
+# change departSpeed with an invalid value
+netedit.modifyAttribute(5, "dummySpeed", True)
+
+# change departSpeed with a valid value
+netedit.modifyAttribute(5, "500", True)
+
+# change departSpeed with an invalid value
+netedit.modifyAttribute(5, "-10", True)
+
+# change departSpeed with a valid value
+netedit.modifyAttribute(5, "20", True)
+
+# Check undo redo
+netedit.undo(referencePosition, 5)
+netedit.redo(referencePosition, 5)
+
+# save routes
+netedit.saveRoutes(referencePosition)
+
+# save network
+netedit.saveNetwork(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
