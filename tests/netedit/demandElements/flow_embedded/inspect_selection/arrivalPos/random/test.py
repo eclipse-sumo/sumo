de9bceddd50f84483f20d2013fd48548d0ee6ac1
Alvarez Lopez, Pablo <pablo.alvarezlopez@dlr.de>
2020-10-21 09:02:02 +0200

    Patching NETEDIT test expected results. Refs #3045

--- tests/netedit/demandElements/flow_embedded/inspect_selection/arrivalPos/random/test.py
+++ tests/netedit/demandElements/flow_embedded/inspect_selection/arrivalPos/random/test.py
@@ -41,7 +41,7 @@ netedit.selectionInvertDemand()
 netedit.inspectMode()
 
 # inspect vehicle
-netedit.leftClick(referencePosition, 91, 392)
+netedit.leftClick(referencePosition, 586, 303)
 
 # change arrivalPos with an invalid value
 netedit.modifyAttribute(7, "", True)
