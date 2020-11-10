30bc67b7442183cafd9453dc46bb78c2ac0c1bb6
Michael Behrisch <michael.behrisch@dlr.de>
2020-11-10 07:57:12 +0100

    preparing test environment for libtraci, refs #21

--- tests/runExtraTests.py
+++ tests/runExtraTests.py
@@ -75,7 +75,7 @@ def run(suffix, args, out=sys.stdout, guiTests=False, console=False, chrouter=Tr
     try:
         if os.name == "posix":
             subprocess.call(['python3', '-V'], stdout=open(os.devnull, "w"))
-        apps += ',complex.python3,tools.python3,complex.libsumo.python3'
+        apps += ',complex.python3,tools.python3,complex.libsumo.python3,complex.libtraci.python3'
     except Exception:
         pass
     if guiTests:
