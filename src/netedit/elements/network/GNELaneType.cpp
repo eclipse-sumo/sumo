3baf879e1b5b0626c6582e62db377770151e25ed
Alvarez Lopez, Pablo <pablo.alvarezlopez@dlr.de>
2020-11-19 09:12:04 +0100

    Added GNELaneType. Refs #2431

---
+++ src/netedit/elements/network/GNELaneType.cpp
@@ -0,0 +1,198 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2020 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNELaneType.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Nov 2020
+///
+/// A SUMO lane type file assigns default values for certain attributes to types of roads.
+/****************************************************************************/
+#include <config.h>
+
+#include <netedit/GNENet.h>
+#include <netedit/GNEUndoList.h>
+#include <netedit/changes/GNEChange_Attribute.h>
+#include <utils/options/OptionsCont.h>
+
+#include "GNELaneType.h"
+
+
+// ===========================================================================
+// members methods
+// ===========================================================================
+
+GNELaneType::GNELaneType(GNENet* net, NBTypeCont::LaneTypeDefinition *type):
+    GNENetworkElement(net, "", GLO_EDGE, SUMO_TAG_TYPE, {}, {}, {}, {}, {}, {}, {}, {}),
+    myLaneType(type) {
+}
+
+
+GNELaneType::~GNELaneType() {
+
+}
+
+
+void
+GNELaneType::updateGeometry() {
+}
+
+
+Position
+GNELaneType::getPositionInView() const {
+    // currently unused
+    return Position(0, 0);
+}
+
+
+GNEMoveOperation*
+GNELaneType::getMoveOperation(const double /*shapeOffset*/) {
+    return nullptr;
+}
+
+
+void 
+GNELaneType::removeGeometryPoint(const Position /*clickedPosition*/, GNEUndoList* /*undoList*/) {
+    // nothing to do
+}
+
+
+GUIGLObjectPopupMenu*
+GNELaneType::getPopUpMenu(GUIMainWindow& /*app*/, GUISUMOAbstractView& /*parent*/) {
+    return nullptr;
+}
+
+
+void 
+GNELaneType::updateCenteringBoundary(const bool /*updateGrid*/) {
+    // nothing to do
+}
+
+
+void
+GNELaneType::drawGL(const GUIVisualizationSettings& /*s*/) const {
+    // nothing to draw
+}
+
+
+std::string
+GNELaneType::getAttribute(SumoXMLAttr key) const {
+    switch (key) {
+        case SUMO_ATTR_ID:
+            return getID();
+        case SUMO_ATTR_SPEED:
+            return toString(myLaneType->speed);
+        case SUMO_ATTR_ALLOW:
+            return getVehicleClassNames(myLaneType->permissions);
+        case SUMO_ATTR_DISALLOW:
+            return getVehicleClassNames(invertPermissions(myLaneType->permissions));
+        case SUMO_ATTR_WIDTH:
+            return toString(myLaneType->width);
+        case GNE_ATTR_PARAMETERS:
+            return getParametersStr();
+        default:
+            throw InvalidArgument(getTagStr() + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+void
+GNELaneType::setAttribute(SumoXMLAttr key, const std::string& value, GNEUndoList* undoList) {
+    switch (key) {
+        case SUMO_ATTR_ID:
+        case SUMO_ATTR_SPEED:
+        case SUMO_ATTR_ALLOW:
+        case SUMO_ATTR_DISALLOW:
+        case SUMO_ATTR_WIDTH:
+        case GNE_ATTR_PARAMETERS:
+            undoList->p_add(new GNEChange_Attribute(this, key, value));
+            break;
+        default:
+            throw InvalidArgument(getTagStr() + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+bool
+GNELaneType::isValid(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+        case SUMO_ATTR_ID:
+            return SUMOXMLDefinitions::isValidNetID(value) && (myNet->retrieveLaneType(value, false) == nullptr);
+        case SUMO_ATTR_SPEED:
+            return canParse<double>(value) && (parse<double>(value) > 0);
+        case SUMO_ATTR_ALLOW:
+        case SUMO_ATTR_DISALLOW:
+            return canParseVehicleClasses(value);
+        case SUMO_ATTR_WIDTH:
+            return canParse<double>(value) && ((parse<double>(value) >= -1) || (parse<double>(value) == NBEdge::UNSPECIFIED_WIDTH));
+        case GNE_ATTR_PARAMETERS:
+            return Parameterised::areParametersValid(value);
+        default:
+            throw InvalidArgument(getTagStr() + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+bool
+GNELaneType::isAttributeEnabled(SumoXMLAttr /*key*/) const {
+    return true;
+}
+
+
+const std::map<std::string, std::string>& 
+GNELaneType::getACParametersMap() const {
+    return getParametersMap();
+}
+
+// ===========================================================================
+// private
+// ===========================================================================
+
+void
+GNELaneType::setAttribute(SumoXMLAttr key, const std::string& value) {
+    switch (key) {
+    case SUMO_ATTR_ID:
+            myNet->getAttributeCarriers()->updateID(this, value);
+            break;
+        case SUMO_ATTR_SPEED:
+            myLaneType->speed = parse<double>(value);
+            break;
+        case SUMO_ATTR_ALLOW:
+            myLaneType->permissions = parseVehicleClasses(value);
+            break;
+        case SUMO_ATTR_DISALLOW:
+            myLaneType->permissions = invertPermissions(parseVehicleClasses(value));
+            break;
+        case SUMO_ATTR_WIDTH:
+            myLaneType->width = parse<double>(value);
+            break;
+        case GNE_ATTR_PARAMETERS:
+            setParametersStr(value);
+            break;
+        default:
+            throw InvalidArgument(getTagStr() + " doesn't have an attribute of type '" + toString(key) + "'");
+    }
+}
+
+
+void
+GNELaneType::setMoveShape(const GNEMoveResult& /*moveResult*/) {
+    // nothing to do
+}
+
+
+void
+GNELaneType::commitMoveShape(const GNEMoveResult& /*moveResult*/, GNEUndoList* /*undoList*/) {
+    // nothing to do
+}
+
+/****************************************************************************/
