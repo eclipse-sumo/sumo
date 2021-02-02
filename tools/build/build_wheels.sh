c4e2ff2b4b66a701116deaf8ab492154b32ea663
namdre <jakob.erdmann@dlr.de>
2021-02-02 13:54:13 +0100

    added missing headers refs #12

--- tools/build/build_wheels.sh
+++ tools/build/build_wheels.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2021 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    build_wheels.sh
+# @author  Michael Behrisch
+# @date    2019
 yum-config-manager --add-repo=https://download.opensuse.org/repositories/home:/behrisch/CentOS_7/
 yum install -y --nogpgcheck cmake3 ccache xerces-c-devel proj-devel fox16-devel bzip2-devel gl2ps-devel swig3
 /opt/python/cp38-cp38/bin/pip install scikit-build
--- tools/build/dailyUpdateMakeGCC.sh
+++ tools/build/dailyUpdateMakeGCC.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2021 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    dailyUpdateMakeGCC.sh
+# @author  Michael Behrisch
+# @date    2008
+
+# Does the nightly git pull on the linux server and then runs build and tests
 PREFIX=$1
 export FILEPREFIX=$2
 export SMTP_SERVER=$3
