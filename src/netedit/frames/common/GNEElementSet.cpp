c71b3ce109198d6b2d0a14065be608074ac8193d
namdre <jakob.erdmann@dlr.de>
2021-02-05 10:40:25 +0100

    fixing style refs #12

--- src/netedit/frames/common/GNEElementSet.cpp
+++ src/netedit/frames/common/GNEElementSet.cpp
@@ -11,7 +11,7 @@
 // https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 /****************************************************************************/
-/// @file    GNESelectorFrame.cpp
+/// @file    GNEElementSet.cpp
 /// @author  Jakob Erdmann
 /// @date    Mar 2011
 ///
