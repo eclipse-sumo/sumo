401c17def55663601356e31f39ee1dbf163be2cc
namdre <jakob.erdmann@dlr.de>
2021-03-26 15:08:18 +0100

    added test refs #8426

---
+++ tests/complex/traci/simulation/loadState/subscription/runner.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2021 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @author  Daniel Krajzewicz
+# @author  Jakob Erdmann
+# @date    2011-03-04
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+
+SUMO_HOME = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..")
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", SUMO_HOME), "tools"))
+import traci  # noqa
+import sumolib  # noqa
+import traci.constants as tc
+
+
+def check():
+    print("%s subcription=%s contextSubscription=%s" % (
+        traci.simulation.getTime(),
+        traci.vehicle.getAllSubscriptionResults(),
+        traci.vehicle.getAllContextSubscriptionResults()))
+
+
+traci.start([sumolib.checkBinary('sumo'),
+             "-n", "input_net2.net.xml",
+             "-r", "input_routes.rou.xml",
+             "--no-step-log",
+             ])
+
+veh = "v0"
+traci.simulation.saveState("state.xml")
+for i in range(2):
+    traci.simulation.loadState("state.xml")
+    traci.simulationStep()
+    traci.vehicle.add(veh, "r0", typeID="t0")
+    traci.vehicle.subscribe(veh, [tc.VAR_SPEED])
+    traci.simulationStep()
+    check()
+
+traci.close()
