bbf03e5d76836b7516214fcaa60a68a6e25e8bd7
ArmelliniMG <giuliana_armellini@hotmail.com>
2021-04-06 21:23:44 +0200

    renaming tools #1425

--- tools/countEdgeUsage.py
+++ tools/countEdgeUsage.py
@@ -11,7 +11,7 @@
 # https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 
-# @file    showDepartsAndArrivalsPerEdge.py
+# @file    countEdgeUsage.py
 # @author  Jakob Erdmann
 # @date    2015-08-05
 
