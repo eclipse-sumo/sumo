b548dcba8b34a7568326d845d129a1a96fa27f6a
namdre <jakob.erdmann@dlr.de>
2021-05-19 17:07:31 +0200

    fixed file header. refs #8650, #21

--- tools/assign/bestIteration.py
+++ tools/assign/bestIteration.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2021 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    bestIteration.py
+# @author  Jakob Erdmann
+# @date    2021-05-10
+
+"""
+Run duaIterate and collect statistics on the iterations
+"""
 import os,sys
 import subprocess
 import glob
