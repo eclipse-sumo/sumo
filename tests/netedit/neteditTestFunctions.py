d7d537d7ea37af8a5cda9f66e821aae2696603f2
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-07-01 09:12:46 +0200

    Added delay. Refs #3045

--- tests/netedit/neteditTestFunctions.py
+++ tests/netedit/neteditTestFunctions.py
@@ -28,10 +28,10 @@ import time
 import pyperclip
 
 # define delay before every operation
-DELAY_KEY = 0.3         # 0.2
-DELAY_KEY_TAB = 0.2     # 0.1
-DELAY_MOUSE_MOVE = 0.5  # 0.1
-DELAY_MOUSE_CLICK = 0.7  # 0.5
+DELAY_KEY = 1         # 0.2 0.3
+DELAY_KEY_TAB = 0.5     # 0.1 0.2
+DELAY_MOUSE_MOVE = 1  # 0.1 0.5
+DELAY_MOUSE_CLICK = 1  # 0.5 0.7
 DELAY_QUESTION = 3
 DELAY_RELOAD = 5
 DELAY_START_NETEDIT = 3
@@ -189,6 +189,8 @@ def pasteIntoTextField(value, removePreviousContents=True, useClipboard=True, la
         pyautogui.hotkey('ctrl', 'v')
     else:
         pyautogui.typewrite(translateKeys(value, layout))
+    # wait after every operation
+    time.sleep(DELAY_KEY)
 
 
 def leftClick(referencePosition, positionx, positiony):
