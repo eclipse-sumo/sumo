538693d9ce84da2edda2265bd5a5d55e25335f5f
namdre <jakob.erdmann@dlr.de>
2021-07-30 15:47:05 +0200

    fix #8889 (crash triggered by lane number difference)

--- src/microsim/lcmodels/MSLCM_SL2015.cpp
+++ src/microsim/lcmodels/MSLCM_SL2015.cpp
@@ -2055,7 +2055,8 @@ MSLCM_SL2015::updateExpectedSublaneSpeeds(const MSLeaderDistanceInfo& ahead, int
             const double gap = ahead[sublane].second;
             double vSafe;
             if (leader == nullptr) {
-                const double dist = preb[laneIndex].length - myVehicle.getPositionOnLane();
+                const int prebIndex = isOpposite() ? (int)preb.size() - 1 : laneIndex;
+                const double dist = preb[prebIndex].length - myVehicle.getPositionOnLane();
                 vSafe = getCarFollowModel().followSpeed(&myVehicle, vMax, dist, 0, 0);
             } else {
                 if (leader->getAcceleration() > 0.5 * leader->getCarFollowModel().getMaxAccel()) {
