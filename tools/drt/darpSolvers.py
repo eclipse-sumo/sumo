bf3c126049603eb8de4ee8e85050a996a0687b74
Michael Behrisch <michael.behrisch@dlr.de>
2021-08-24 11:35:09 +0200

    fixing executability #22

--- tools/drt/darpSolvers.py
+++ tools/drt/darpSolvers.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
 # Copyright (C) 2007-2021 German Aerospace Center (DLR) and others.
 # This program and the accompanying materials are made available under the
