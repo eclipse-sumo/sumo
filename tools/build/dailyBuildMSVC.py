cd9f99fb9083597e7780df14694db5e7d423e27a
Michael Behrisch <michael.behrisch@dlr.de>
2021-09-19 21:55:02 +0200

    more dll filtering hiccups #3

--- tools/build/dailyBuildMSVC.py
+++ tools/build/dailyBuildMSVC.py
@@ -100,8 +100,7 @@ def runTests(options, env, gitrev, log, debugSuffix=""):
 
 def generateCMake(generator, platform, log, checkOptionalLibs, python):
     buildDir = os.path.join(env["SUMO_HOME"], "build", "cmake-build-" + platform)
-    cmakeOpt = ["-DCOMPILE_DEFINITIONS=MSVC_TEST_SERVER",
-                "-DCHECK_OPTIONAL_LIBS=%s" % checkOptionalLibs]
+    cmakeOpt = ["-DCHECK_OPTIONAL_LIBS=%s" % checkOptionalLibs]
     if python:
         cmakeOpt += ["-DPYTHON_EXECUTABLE=%s" % python]
     if checkOptionalLibs:
