eclipse-sumo/sumo

commit fafd8a8518744b288de030d776a9f3077ee9c3cd

Added parameter calibrator speed in SUMOVehicleParameter. Refs #9218

src/utils/vehicle/SUMOVehicleParserHelper.cpp
@@ -640,6 +640,19 @@ SUMOVehicleParserHelper::parseCommonAttributes(const SUMOSAXAttributes& attrs, S
             handleVehicleError(true, ret, toString(SUMO_ATTR_SPEEDFACTOR) + " must be positive");
         }
     }
+    // parse speed (only used by calibrators)
+    if (attrs.hasAttribute(SUMO_ATTR_SPEED)) {
+        bool ok = true;
+        double calibratorSpeed = attrs.get<double>(SUMO_ATTR_SPEED, ret->id.c_str(), ok);
+        if (!ok) {
+            handleVehicleError(true, ret);
+        } else if (calibratorSpeed > 0) {
+            ret->parametersSet |= VEHPARS_CALIBRATORSPEED_SET;
+            ret->calibratorSpeed = calibratorSpeed;
+        } else {
+            handleVehicleError(true, ret, toString(SUMO_ATTR_SPEED) + " must be positive");
+        }
+    }
     /*/ parse via
     if (attrs.hasAttribute(SUMO_ATTR_VIA)) {
         ret->setParameter |= VEHPARS_VIA_SET;

src/utils/vehicle/SUMOVehicleParameter.cpp
@@ -49,6 +49,7 @@ SUMOVehicleParameter::SUMOVehicleParameter()
       repetitionNumber(-1), repetitionsDone(-1), repetitionOffset(-1), repetitionProbability(-1), repetitionEnd(-1),
       line(), fromTaz(), toTaz(), personNumber(0), containerNumber(0),
       speedFactor(-1),
+      calibratorSpeed(-1),
       parametersSet(0)
 { }
 
@@ -164,6 +165,10 @@ SUMOVehicleParameter::write(OutputDevice& dev, const OptionsCont& oc, const Sumo
     // individual speedFactor
     if (wasSet(VEHPARS_SPEEDFACTOR_SET)) {
         dev.writeAttr(SUMO_ATTR_SPEEDFACTOR, speedFactor);
+    }
+    // speed (only used by calibrators)
+    if (wasSet(VEHPARS_CALIBRATORSPEED_SET)) {
+        dev.writeAttr(SUMO_ATTR_SPEED, calibratorSpeed);
     }
 }
 

src/utils/vehicle/SUMOVehicleParameter.h
@@ -67,7 +67,8 @@ const int VEHPARS_VIA_SET = 2 << 22;
 const int VEHPARS_SPEEDFACTOR_SET = 2 << 23;
 const int VEHPARS_DEPARTEDGE_SET = 2 << 24;
 const int VEHPARS_ARRIVALEDGE_SET = 2 << 25;
-const int VEHPARS_JUNCTIONMODEL_PARAMS_SET = 2 << 26;
+const int VEHPARS_CALIBRATORSPEED_SET = 2 << 26;
+const int VEHPARS_JUNCTIONMODEL_PARAMS_SET = 2 << 27;
 
 const int STOP_INDEX_END = -1;
 const int STOP_INDEX_FIT = -2;
@@ -740,6 +741,9 @@ public:
 
     /// @brief individual speedFactor (overriding distribution from vType)
     double speedFactor;
+
+    /// @brief speed (used by calibrator flows
+    double calibratorSpeed;
 
     /// @brief Information for the router which parameter were set, TraCI may modify this (when changing color)
     mutable int parametersSet;

commit 81a28bc9c591c7749d938a57e32e58c79f53ef47

fixed parsing of saved state, broken in fafd8a8518744b (#9218)

src/utils/vehicle/SUMOVehicleParserHelper.cpp
@@ -153,7 +153,7 @@ SUMOVehicleParserHelper::parseFlowAttributes(SumoXMLTag tag, const SUMOSAXAttrib
         }
         // parse common vehicle attributes
         try {
-            parseCommonAttributes(attrs, flowParameter, "flow");
+            parseCommonAttributes(attrs, flowParameter, tag);
         } catch (ProcessError& attributeError) {
             // check if continue handling another vehicles or stop handling
             if (hardFail) {
@@ -321,7 +321,7 @@ SUMOVehicleParserHelper::parseVehicleAttributes(int element, const SUMOSAXAttrib
         }
         // parse common attributes
         try {
-            parseCommonAttributes(attrs, vehicleParameter, "vehicle");
+            parseCommonAttributes(attrs, vehicleParameter, (SumoXMLTag)element);
         } catch (ProcessError& attributeError) {
             // check if continue handling another vehicles or stop handling
             if (hardFail) {
@@ -377,7 +377,8 @@ SUMOVehicleParserHelper::parseID(const SUMOSAXAttributes& attrs, const SumoXMLTa
 
 
 void
-SUMOVehicleParserHelper::parseCommonAttributes(const SUMOSAXAttributes& attrs, SUMOVehicleParameter* ret, std::string element) {
+SUMOVehicleParserHelper::parseCommonAttributes(const SUMOSAXAttributes& attrs, SUMOVehicleParameter* ret, SumoXMLTag tag) {
+    const std::string element = toString(tag);
     //ret->refid = attrs.getStringSecure(SUMO_ATTR_REFID, "");
     // parse route information
     if (attrs.hasAttribute(SUMO_ATTR_ROUTE)) {
@@ -640,9 +641,11 @@ SUMOVehicleParserHelper::parseCommonAttributes(const SUMOSAXAttributes& attrs, S
             handleVehicleError(true, ret, toString(SUMO_ATTR_SPEEDFACTOR) + " must be positive");
         }
     }
-    // parse speed (only used by calibrators)
-    if (attrs.hasAttribute(SUMO_ATTR_SPEED)) {
+    // parse speed (only used by calibrators flow)
+    // also used by vehicle in saved state but this is parsed elsewhere
+    if (tag == SUMO_TAG_FLOW && attrs.hasAttribute(SUMO_ATTR_SPEED)) {
         bool ok = true;
+        std::cout << "veh=" << ret->id << " attrs=" << attrs << "\n";
         double calibratorSpeed = attrs.get<double>(SUMO_ATTR_SPEED, ret->id.c_str(), ok);
         if (!ok) {
             handleVehicleError(true, ret);

src/utils/vehicle/SUMOVehicleParserHelper.h
@@ -179,7 +179,7 @@ private:
      * @param[in] element The name of the element (vehicle or flow)
      * @exception ProcessError If an attribute's value is invalid
      */
-    static void parseCommonAttributes(const SUMOSAXAttributes& attrs, SUMOVehicleParameter* ret, std::string element);
+    static void parseCommonAttributes(const SUMOSAXAttributes& attrs, SUMOVehicleParameter* ret, SumoXMLTag tag);
 
     /// @brief handle error loading SUMOVehicleParameter
     static SUMOVehicleParameter* handleVehicleError(const bool hardFail, SUMOVehicleParameter* vehicleParameter, const std::string message = "");