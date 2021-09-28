f937ef221f4c61a95e92bb1f61449583d15f69ad
namdre <jakob.erdmann@dlr.de>
2021-09-28 07:42:31 +0200

    fixed file reference refs #12

--- src/microsim/output/MSDetectorFileOutput.cpp
+++ src/microsim/output/MSDetectorFileOutput.cpp
@@ -11,7 +11,7 @@
 // https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 /****************************************************************************/
-/// @file    MSDetectorFileOutput.h
+/// @file    MSDetectorFileOutput.cpp
 /// @author  Christian Roessel
 /// @author  Daniel Krajzewicz
 /// @author  Sascha Krieg
