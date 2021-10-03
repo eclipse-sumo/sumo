0aa9fe10362326e1a167f5a3fc9a2e9681c23dda
Michael Behrisch <michael.behrisch@dlr.de>
2021-10-03 18:55:39 +0200

    fixing style #22

--- tests/complex/traci/ContextSubscriptionFilters/turn/combined_with_lateralDistance/runner.py
+++ tests/complex/traci/ContextSubscriptionFilters/turn/combined_with_lateralDistance/runner.py
@@ -96,7 +96,8 @@ def runSingle(traciEndTime, lateralDist, downstreamDist, upstreamDist, foeDistTo
             print("Subscribing to context of vehicle '%s'" % (egoID))
             traci.vehicle.subscribeContext(egoID, traci.constants.CMD_GET_VEHICLE_VARIABLE, 0.0,
                                            [traci.constants.VAR_POSITION])
-            print("Adding turn filter ... \n(downstreamDist=%s, foeDistToJunction=%s)" % (downstreamDist, foeDistToJunction))
+            print("Adding turn filter ... \n(downstreamDist=%s, foeDistToJunction=%s)" %
+                  (downstreamDist, foeDistToJunction))
             sys.stdout.flush()
             traci.vehicle.addSubscriptionFilterTurn(downstreamDist, foeDistToJunction)
             print("Adding lateral distance subscription filter ... " +
