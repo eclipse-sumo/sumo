e7886dc8d167328696bd514ab64407603e8c0c3c
Michael Behrisch <michael.behrisch@dlr.de>
2021-10-19 09:03:25 +0200

    refactoring and testing certifi usage #12 #9347

--- tools/osmGet.py
+++ tools/osmGet.py
@@ -20,6 +20,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import os
+import gzip
+import base64
+import ssl
 try:
     import httplib
     import urlparse
@@ -29,12 +32,13 @@ except ImportError:
     import http.client as httplib
     import urllib.parse as urlparse
     from urllib.request import urlopen
+try:
+    import certifi
+    HAVE_CERTIFI = True
+except ImportError:
+    HAVE_CERTIFI = False
 
-import base64
-from os import path
-
-import sumolib  # noqa
-import gzip
+import sumolib
 
 
 def readCompressed(conn, urlpath, query, filename):
@@ -55,9 +59,8 @@ def readCompressed(conn, urlpath, query, filename):
     response = conn.getresponse()
     print(response.status, response.reason)
     if response.status == 200:
-        out = open(path.join(os.getcwd(), filename), "wb")
-        out.write(response.read())
-        out.close()
+        with open(filename, "wb") as out:
+            out.write(response.read())
 
 
 optParser = sumolib.options.ArgumentParser(description="Get network from OpenStreetMap")
@@ -85,9 +88,8 @@ def get(args=None):
         east = -1e400
         north = -1e400
         for area in sumolib.output.parse_fast(options.polygon, 'poly', ['shape']):
-            coordList = [tuple(map(float, x.split(',')))
-                         for x in area.shape.split()]
-            for point in coordList:
+            for coord in area.shape.split():
+                point = tuple(map(float, coord.split(',')))
                 west = min(point[0], west)
                 south = min(point[1], south)
                 east = max(point[0], east)
@@ -98,7 +100,7 @@ def get(args=None):
             optParser.error("Invalid geocoordinates in bbox.")
 
     if options.output_dir:
-        options.prefix = path.join(options.output_dir, options.prefix)
+        options.prefix = os.path.join(options.output_dir, options.prefix)
 
     if "http" in options.url:
         url = urlparse.urlparse(options.url)
@@ -114,7 +116,11 @@ def get(args=None):
         conn.set_tunnel(url.hostname, 443, headers)
     else:
         if url.scheme == "https":
-            conn = httplib.HTTPSConnection(url.hostname, url.port)
+            if HAVE_CERTIFI:
+                context = ssl.create_default_context(cafile=certifi.where())
+                conn = httplib.HTTPSConnection(url.hostname, url.port, context=context)
+            else:
+                conn = httplib.HTTPSConnection(url.hostname, url.port)
         else:
             conn = httplib.HTTPConnection(url.hostname, url.port)
 
@@ -140,7 +146,7 @@ def get(args=None):
     # extract the wiki data according to the wikidata-value in the extracted osm file
     if options.wikidata:
         filename = options.prefix + '.wikidata.xml.gz'
-        osmFile = path.join(os.getcwd(), options.prefix + "_bbox.osm.xml")
+        osmFile = options.prefix + "_bbox.osm.xml"
         codeSet = set()
         # deal with invalid characters
         bad_chars = [';', ':', '!', "*", ')', '(', '-', '_', '%', '&', '/', '=', '?', '$', '//', '\\', '#', '<', '>']
@@ -159,7 +165,7 @@ def get(args=None):
         # make and save query results iteratively
         codeList = list(codeSet)
         interval = 50  # the maximal number of query items
-        outf = gzip.open(path.join(os.getcwd(), filename), "wb")
+        outf = gzip.open(filename, "wb")
         for i in range(0, len(codeSet), interval):
             j = i + interval
             if j > len(codeSet):
