eclipse-sumo/sumo

commit fcb796b146f6571235e31bfb887ff2155315f718

Addendum to #6318

src/utils/gui/settings/GUIVisualizationSettings.cpp
@@ -1694,6 +1694,24 @@ GUIVisualizationSettings::save(OutputDevice& dev) const {
     dev.writeAttr("parkingAreaColorSign", colorSettings.parkingAreaColorSign);
     dev.writeAttr("parkingSpaceColorContour", colorSettings.parkingSpaceColorContour);
     dev.writeAttr("parkingSpaceColor", colorSettings.parkingSpaceColor);
+
+    dev.writeAttr("stopColor", colorSettings.stops);
+    dev.writeAttr("vehicleTripColor", colorSettings.vehicleTrips);
+    dev.writeAttr("stopPersonColor", colorSettings.stopPersons);
+    dev.writeAttr("personTripColor", colorSettings.personTrip);
+    dev.writeAttr("walkColor", colorSettings.walk);
+    dev.writeAttr("rideColor", colorSettings.ride);
+    dev.writeAttr("stopContainerColor", colorSettings.stopContainers);
+    dev.writeAttr("transportColor", colorSettings.transport);
+    dev.writeAttr("transhipColor", colorSettings.tranship);
+    dev.writeAttr("parkingSpaceColor", colorSettings.parkingSpaceColor);
+    dev.writeAttr("parkingSpaceColor", colorSettings.parkingSpaceColor);
+    dev.writeAttr("tripWidth", widthSettings.trip);
+    dev.writeAttr("personTripWidth", widthSettings.personTrip);
+    dev.writeAttr("walkWidth", widthSettings.walk);
+    dev.writeAttr("rideWidth", widthSettings.ride);
+    dev.writeAttr("transportWidth", widthSettings.transport);
+    dev.writeAttr("transhipWidth", widthSettings.tranship);
     dev.closeTag();
     // pois
     dev.openTag(SUMO_TAG_VIEWSETTINGS_POIS);

src/utils/gui/settings/GUISettingsHandler.cpp
@@ -294,6 +294,21 @@ GUISettingsHandler::myStartElement(int element, const SUMOSAXAttributes& attrs) 
             mySettings.colorSettings.parkingAreaColorSign = parseColor(attrs, "parkingAreaColorSign", mySettings.colorSettings.parkingAreaColorSign);
             mySettings.colorSettings.parkingSpaceColorContour = parseColor(attrs, "parkingSpaceColorContour", mySettings.colorSettings.parkingSpaceColorContour);
             mySettings.colorSettings.parkingSpaceColor = parseColor(attrs, "parkingSpaceColor", mySettings.colorSettings.parkingSpaceColor);
+            mySettings.colorSettings.stops = parseColor(attrs, "stopColor", mySettings.colorSettings.stops);
+            mySettings.colorSettings.vehicleTrips = parseColor(attrs, "vehicleTripColor", mySettings.colorSettings.vehicleTrips);
+            mySettings.colorSettings.stopPersons = parseColor(attrs, "stopPersonColor", mySettings.colorSettings.stopPersons);
+            mySettings.colorSettings.personTrip = parseColor(attrs, "personTripColor", mySettings.colorSettings.personTrip);
+            mySettings.colorSettings.walk = parseColor(attrs, "walkColor", mySettings.colorSettings.walk);
+            mySettings.colorSettings.ride = parseColor(attrs, "rideColor", mySettings.colorSettings.ride);
+            mySettings.colorSettings.stopContainers = parseColor(attrs, "stopContainerColor", mySettings.colorSettings.stopContainers);
+            mySettings.colorSettings.transport = parseColor(attrs, "transportColor", mySettings.colorSettings.transport);
+            mySettings.colorSettings.tranship = parseColor(attrs, "transhipColor", mySettings.colorSettings.tranship);
+            mySettings.widthSettings.trip = StringUtils::toDouble(attrs.getStringSecure("tripWidth", toString(mySettings.widthSettings.trip)));
+            mySettings.widthSettings.personTrip = StringUtils::toDouble(attrs.getStringSecure("personTripWidth", toString(mySettings.widthSettings.personTrip)));
+            mySettings.widthSettings.walk = StringUtils::toDouble(attrs.getStringSecure("walkWidth", toString(mySettings.widthSettings.walk)));
+            mySettings.widthSettings.ride = StringUtils::toDouble(attrs.getStringSecure("rideWidth", toString(mySettings.widthSettings.ride)));
+            mySettings.widthSettings.transport = StringUtils::toDouble(attrs.getStringSecure("transportWidth", toString(mySettings.widthSettings.transport)));
+            mySettings.widthSettings.tranship = StringUtils::toDouble(attrs.getStringSecure("transhipWidth", toString(mySettings.widthSettings.tranship)));
             break;
         case SUMO_TAG_VIEWSETTINGS_POIS:
             mySettings.poiTextParam = attrs.getStringSecure("poiTextParam", mySettings.poiTextParam);

commit 1627f8b9a660eed73ad006a12ab81d0ed9a89952

fixing regression in [fcb796b146f6571235e31bfb887ff2155315f718]

src/utils/gui/settings/GUIVisualizationSettings.cpp
@@ -1706,8 +1706,6 @@ GUIVisualizationSettings::save(OutputDevice& dev) const {
     dev.writeAttr("stopContainerColor", colorSettings.stopContainerColor);
     dev.writeAttr("transportColor", colorSettings.transportColor);
     dev.writeAttr("transhipColor", colorSettings.transhipColor);
-    dev.writeAttr("parkingSpaceColor", colorSettings.parkingSpaceColor);
-    dev.writeAttr("parkingSpaceColor", colorSettings.parkingSpaceColor);
     dev.writeAttr("tripWidth", widthSettings.tripWidth);
     dev.writeAttr("personTripWidth", widthSettings.personTripWidth);
     dev.writeAttr("walkWidth", widthSettings.walkWidth);