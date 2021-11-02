6ff9968cb433020d92caf95937541a732071c0cd
Angelo Banse <angelo_22@outlook.com>
2021-11-02 15:41:53 +0100

    minor typo

--- src/netedit/GNEApplicationWindowHelper.cpp
+++ src/netedit/GNEApplicationWindowHelper.cpp
@@ -1641,7 +1641,7 @@ void
 GNEApplicationWindowHelper::ToolsMenuCommands::buildToolsMenuCommands(FXMenuPane* locateMenu) {
     // build locate menu commands
     GUIDesigns::buildFXMenuCommandShortcut(locateMenu,
-                                           "&NetDiff", "", "Execute NETEDIFF",
+                                           "&NetDiff", "", "Execute NETDIFF.",
                                            GUIIconSubSys::getIcon(GUIIcon::TOOL_NETDIFF), myGNEApp, MID_GNE_TOOLBARTOOLS_NETDIFF);
 }
 
