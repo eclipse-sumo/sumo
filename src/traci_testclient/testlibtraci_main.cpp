a3fcc08e2a703f52e8e2d5b9b471218dec73f1b2
Michael Behrisch <michael.behrisch@dlr.de>
2021-11-14 00:08:18 +0100

    passing tested sumo explicitly in libtraci test #21

--- src/traci_testclient/testlibtraci_main.cpp
+++ src/traci_testclient/testlibtraci_main.cpp
@@ -30,11 +30,6 @@ main(int argc, char** argv) {
     for (int i = 1; i < argc; i++) {
         options.push_back(argv[i]);
     }
-#ifdef _DEBUG
-    options.insert(options.begin(), "sumoD");
-#else
-    options.insert(options.begin(), "sumo");
-#endif
     try {
         libtraci::Simulation::start(options);
         // libtraci::Simulation::start(options, -1, libsumo::DEFAULT_NUM_RETRIES, "default", true);
