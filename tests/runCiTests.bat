10495e5f704ea8ffb97bdd952369404fd27fb903
Michael Behrisch <michael.behrisch@dlr.de>
2021-11-28 21:28:20 +0100

    various build tweaks #9647 #9675 (#9680)

--- tests/runCiTests.bat
+++ tests/runCiTests.bat
@@ -6,4 +6,4 @@ REM which accompanies this distribution, and is available at
 REM http://www.eclipse.org/legal/epl-v20.html
 REM SPDX-License-Identifier: EPL-2.0
 call %~dp0\testEnv.bat
-%1 -b ci -v ci %TEXTTEST_CI_APPS%
+%1 -b ci -v ci -keeptmp 0 %TEXTTEST_CI_APPS%
