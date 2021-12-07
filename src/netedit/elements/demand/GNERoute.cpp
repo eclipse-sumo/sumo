9cf48298a57317450437b265a1876db5a04ab34d
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-12-07 12:46:33 +0100

    Fixed #9664

--- src/netedit/elements/demand/GNERoute.cpp
+++ src/netedit/elements/demand/GNERoute.cpp
@@ -175,6 +175,10 @@ GNERoute::getPopUpMenu(GUIMainWindow& app, GUISUMOAbstractView& parent) {
 void
 GNERoute::writeDemandElement(OutputDevice& device) const {
     device.openTag(SUMO_TAG_ROUTE);
+    // write id only for non-embedded routes
+    if (myTagProperty.getTag() == SUMO_TAG_ROUTE) {
+        device.writeAttr(SUMO_ATTR_ID, getID());
+    }
     device.writeAttr(SUMO_ATTR_EDGES, parseIDs(getParentEdges()));
     device.writeAttr(SUMO_ATTR_COLOR, toString(myColor));
     if (myRepeat != 0) {
@@ -183,10 +187,8 @@ GNERoute::writeDemandElement(OutputDevice& device) const {
     if (myCycleTime != 0) {
         device.writeAttr(SUMO_ATTR_CYCLETIME, toString(myCycleTime));
     }
-    // write extra attributes depending if is an embedded route
+    // write stops associated to this route (not for embedded routes)
     if (myTagProperty.getTag() == SUMO_TAG_ROUTE) {
-        device.writeAttr(SUMO_ATTR_ID, getID());
-        // write stops associated to this route
         for (const auto& stop : getChildDemandElements()) {
             if (stop->getTagProperty().isStop()) {
                 stop->writeDemandElement(device);
