568f37b24e7d2511ebdd0fb5c39b245b6c9fe09d
Michael Behrisch <michael.behrisch@dlr.de>
2021-12-09 09:55:02 +0100

    accepting python2 shebang #22

--- tools/build/checkStyle.py
+++ tools/build/checkStyle.py
@@ -169,7 +169,7 @@ class PropertyReader(xml.sax.handler.ContentHandler):
         if ext in (".py", ".pyw"):
             if lines[0][:2] == '#!':
                 idx += 1
-                if lines[0] not in ('#!/usr/bin/env python\n', '#!/usr/bin/env python3\n'):
+                if lines[0] not in ('#!/usr/bin/env python\n', '#!/usr/bin/env python3\n', '#!/usr/bin/env python2\n'):
                     print(self._file, "wrong shebang")
                     if self._fix:
                         lines[0] = '#!/usr/bin/env python\n'
