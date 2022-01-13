9bab618a12a49ae925954e23c2386dd67b7717ea
Angelo Banse <angelo_22@outlook.com>
2022-01-13 22:09:40 +0100

    Happy New Year to SUMO

--- src/microsim/lcmodels/MSLCM_SL2015.cpp
+++ src/microsim/lcmodels/MSLCM_SL2015.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************/
 // Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-// Copyright (C) 2013-2021 German Aerospace Center (DLR) and others.
+// Copyright (C) 2013-2022 German Aerospace Center (DLR) and others.
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License 2.0 which is available at
 // https://www.eclipse.org/legal/epl-2.0/
--- tools/route/analyzePersonPlans.py
+++ tools/route/analyzePersonPlans.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-# Copyright (C) 2012-2021 German Aerospace Center (DLR) and others.
+# Copyright (C) 2012-2022 German Aerospace Center (DLR) and others.
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
 # https://www.eclipse.org/legal/epl-2.0/
