109608dd6e9faf31aeea37c6b4fa59f8dbf14b4d
namdre <jakob.erdmann@dlr.de>
2022-01-23 01:26:53 +0100

    addendum refs #1498

--- src/utils/geom/PositionVector.cpp
+++ src/utils/geom/PositionVector.cpp
@@ -1166,7 +1166,10 @@ PositionVector::move2side(double amount, double maxExtension) {
             shape.back().set(shape.back().x(), shape.back().y(), me.z());
             const double angle = localAngle(from, me, to);
             if (fabs(angle) > NUMERICAL_EPS) {
-                const double radius = (from.distanceTo2D(me) + me.distanceTo2D(to)) / angle;
+                const double length = (i == 1 || i + 2 == (int)size()
+                        ? MIN2(from.distanceTo2D(me), me.distanceTo2D(to)) * 2
+                        : (from.distanceTo2D(me) + me.distanceTo2D(to)));
+                const double radius = length / angle;
                 if (radius < 0 && -radius < amount * 1.8) {
                     //std::cout << " i_" << angle << "_" << amount << "=" << from << " " << me << " " << to << "\n";
                     recheck.push_back(i);
