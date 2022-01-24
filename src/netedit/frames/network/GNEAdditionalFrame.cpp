a8d6e07bc94b52eb14b39c9a5588a98e994e9632
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-01-24 13:56:04 +0100

    Fixed #10031

--- src/netedit/frames/network/GNEAdditionalFrame.cpp
+++ src/netedit/frames/network/GNEAdditionalFrame.cpp
@@ -1343,6 +1343,21 @@ GNEAdditionalFrame::buildAdditionalOverLane(GNELane* lane, const GNETagPropertie
 
 bool
 GNEAdditionalFrame::buildAdditionalOverView(const GNETagProperties& tagProperties) {
+    // disable intervals (temporal)
+    if ((tagProperties.getTag() == SUMO_TAG_INTERVAL) ||
+        (tagProperties.getTag() == SUMO_TAG_DEST_PROB_REROUTE) ||
+        (tagProperties.getTag() == SUMO_TAG_CLOSING_REROUTE) ||
+        (tagProperties.getTag() == SUMO_TAG_CLOSING_LANE_REROUTE) ||
+        (tagProperties.getTag() == SUMO_TAG_ROUTE_PROB_REROUTE) ||
+        (tagProperties.getTag() == SUMO_TAG_PARKING_AREA_REROUTE)) {
+        WRITE_WARNING("Currently unsuported. Create rerouter elements using rerouter dialog");
+        return false;
+    }
+        // disable intervals (temporal)
+    if (tagProperties.getTag() == SUMO_TAG_STEP) {
+        WRITE_WARNING("Currently unsuported. Create VSS steps elements using VSS dialog");
+        return false;
+    }
     // Check if ID has to be generated
     if (!myBaseAdditional->hasStringAttribute(SUMO_ATTR_ID)) {
         myBaseAdditional->addStringAttribute(SUMO_ATTR_ID, myViewNet->getNet()->getAttributeCarriers()->generateAdditionalID(tagProperties.getTag()));
