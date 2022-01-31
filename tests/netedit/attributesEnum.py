793c16fcea340e24300e2904a2fbcc0f85537bc6
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-01-31 13:13:04 +0100

    Updated NETEDIT test. Refs #3045

--- tests/netedit/attributesEnum.py
+++ tests/netedit/attributesEnum.py
@@ -36,3 +36,111 @@ class busStop:
         colorDialog = 6        # missing
         color = 7
         parameters = 10
+
+# trainStop
+class trainStop:
+    class create:
+        idEnable = 5
+        id = 6
+        name = 6
+        friendlyPos = 7
+        lines = 8
+        personCapacity = 9
+        parkingLength = 10
+        colorDialog = 11
+        color = 12
+        references = 16
+        length = 18
+    class inspect():
+        id = 1
+        lane = 2
+        startPos = 3
+        endPos = 4
+        name = 5
+        friendlyPos = 6
+        lines = 7
+        personCapacity = 8
+        parkingLength = 9
+        colorDialog = 10        # missing
+        color = 11
+        parameters = 14
+    class inspectSelection():
+        name = 1
+        friendlyPos = 2
+        lines = 3
+        personCapacity = 4
+        parkingLength = 5
+        colorDialog = 6        # missing
+        color = 7
+        parameters = 10
+        
+# containerStop
+class containerStop:
+    class create:
+        idEnable = 5
+        id = 6
+        name = 6
+        friendlyPos = 7
+        lines = 8
+        containerCapacity = 9
+        parkingLength = 10
+        colorDialog = 11
+        color = 12
+        references = 16
+        length = 18
+    class inspect():
+        id = 1
+        lane = 2
+        startPos = 3
+        endPos = 4
+        name = 5
+        friendlyPos = 6
+        lines = 7
+        containerCapacity = 8
+        parkingLength = 9
+        colorDialog = 10        # missing
+        color = 11
+        parameters = 14
+    class inspectSelection():
+        name = 1
+        friendlyPos = 2
+        lines = 3
+        containerCapacity = 4
+        parkingLength = 5
+        colorDialog = 6        # missing
+        color = 7
+        parameters = 10
+
+# chargingStation
+class chargingStation:
+    class create:
+        idEnable = 5
+        id = 6
+        name = 6
+        friendlyPos = 7
+        power = 8
+        efficiency = 9
+        chargeInTrainsit = 10
+        chargeDelay = 11
+        references = 14
+        length = 15
+    class inspect():
+        id = 1
+        lane = 2
+        startPos = 3
+        endPos = 4
+        name = 5
+        friendlyPos = 6
+        power = 7
+        efficiency = 8
+        chargeInTrainsit = 9
+        chargeDelay = 10
+        parameters = 13
+    class inspectSelection():
+        name = 1
+        friendlyPos = 2
+        power = 3
+        efficiency = 4
+        chargeInTrainsit = 5
+        chargeDelay = 6
+        parameters = 10
