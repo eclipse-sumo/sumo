322f30a63b1798b9d45f3c9ef0700fac4e03c3f6
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-02-08 16:18:54 +0100

    Updated NETEDIT test. Refs #3045

--- tests/netedit/attributesEnum.py
+++ tests/netedit/attributesEnum.py
@@ -1036,6 +1036,7 @@ class routeflow:
         containerNumber = 19
         departPosLat = 20
         arrivalPosLat = 21
+        # flow attributes missing
     class inspect:
         id = 1
         type = 2
@@ -1132,6 +1133,66 @@ class vehicleEmbedded:
         arrivalPosLat = 14
         depart = 15
         parameters = 18
+
+# flow (embedded route)
+class flowEmbedded:
+    class create:
+        type = 5
+        idEnable = 8        # missing
+        id = 9              # missing
+        colorButton = 9     # missing
+        color = 10
+        departLane = 11
+        departPos = 12
+        departSpeed = 13
+        arrivalLane = 14
+        arrivalPos = 15
+        arrivalSpeed = 16
+        line = 17
+        personNumber = 18
+        containerNumber = 19
+        departPosLat = 20
+        arrivalPosLat = 21
+        # flow attributes missing
+    class inspect:
+        id = 1              # missing
+        type = 2
+        departEdge = 3      # missing
+        arrivalEdge = 4     # missing
+        colorButton = 5     # missing
+        color = 6
+        departLane = 7
+        departPos = 8
+        departSpeed = 9
+        arrivalLane = 10
+        arrivalPos = 11
+        arrivalSpeed = 12
+        line = 13
+        personNumber = 14
+        containerNumber = 15
+        departPosLat = 16
+        arrivalPosLat = 17
+        begin = 18
+        # flow attributes missing
+        parameters = 29
+    class inspectSelection:
+        type = 1            # missing
+        colorButton = 2
+        color = 3
+        departLane = 4
+        departPos = 5
+        departSpeed = 6
+        arrivalLane = 7
+        arrivalPos = 8
+        arrivalSpeed = 9
+        line = 10
+        personNumber = 11
+        containerNumber = 12
+        departPosLat = 13
+        arrivalPosLat = 14
+        begin = 15
+        # flow attributes missing
+        parameters = 26
 
 # stop (over additionals)
 class stop:
