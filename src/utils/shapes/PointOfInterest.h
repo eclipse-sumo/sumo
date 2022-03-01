aafdba41c697edbd09e6f9c81e17ce864175fff6
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-03-01 11:58:23 +0100

    Updated PointOfInterest. Refs #10239

--- src/utils/shapes/PointOfInterest.h
+++ src/utils/shapes/PointOfInterest.h
@@ -135,7 +135,7 @@ public:
     /* @brief POI definition to the given device
      * @param[in] geo  Whether to write the output in geo-coordinates
      */
-    void writeXML(OutputDevice& out, const bool geo = false, const double zOffset = 0., const std::string laneID = "", const double pos = 0., const bool friendlyPos = false, const double posLat = 0.) {
+    void writeXML(OutputDevice& out, const bool geo = false, const double zOffset = 0., const std::string laneID = "", const double pos = 0., const bool friendlyPos = false, const double posLat = 0.) const {
         out.openTag(SUMO_TAG_POI);
         out.writeAttr(SUMO_ATTR_ID, StringUtils::escapeXML(getID()));
         if (getShapeType().size() > 0) {
