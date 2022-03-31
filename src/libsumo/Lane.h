eclipse-sumo/sumo

commit dc1afbe8beccf07883a0820372c009462dd8c590

fix #8388

src/libsumo/Simulation.h
@@ -20,6 +20,7 @@
 #pragma once
 #include <vector>
 #include <libsumo/TraCIDefs.h>
+#include <utils/geom/PositionVector.h>
 
 
 // ===========================================================================
@@ -156,6 +157,8 @@ public:
 
 #ifndef LIBTRACI
 #ifndef SWIG
+    static void storeShape(PositionVector& shape);
+
     static std::shared_ptr<VariableWrapper> makeWrapper();
 
     static bool handleVariable(const std::string& objID, const int variable, VariableWrapper* wrapper, tcpip::Storage* paramData);

src/libsumo/Helper.cpp
@@ -756,6 +756,9 @@ Helper::findObjectShape(int domain, const std::string& id, PositionVector& shape
         case libsumo::CMD_SUBSCRIBE_EDGE_CONTEXT:
             Edge::storeShape(id, shape);
             break;
+        case libsumo::CMD_SUBSCRIBE_SIM_CONTEXT:
+            Simulation::storeShape(shape);
+            break;
         default:
             break;
     }
@@ -804,6 +807,7 @@ Helper::collectObjectsInRange(int domain, const PositionVector& shape, double ra
         }
         break;
         default:
+            throw TraCIException("Infeasible context domain (" + toString(domain) + ")");
             break;
     }
 }

src/libsumo/Simulation.cpp
@@ -813,6 +813,23 @@ Simulation::writeMessage(const std::string& msg) {
 }
 
 
+void
+Simulation::storeShape(PositionVector& shape) {
+    const TraCIPositionVector tpv = getNetBoundary();
+    TraCIPosition minV = tpv.value.front();
+    TraCIPosition maxV = tpv.value.back();
+    const Position lowerLeft = Position(minV.x, minV.y, minV.z);
+    const Position upperLeft = Position(minV.x, maxV.y, minV.z);
+    const Position upperRight = Position(maxV.x, maxV.y, minV.z);
+    const Position lowerRight = Position(maxV.x, minV.y, minV.z);
+    shape.push_back(lowerLeft);
+    shape.push_back(upperLeft);
+    shape.push_back(upperRight);
+    shape.push_back(lowerRight);
+    shape.push_back(lowerLeft);
+}
+
+
 std::shared_ptr<VariableWrapper>
 Simulation::makeWrapper() {
     return std::make_shared<Helper::SubscriptionWrapper>(handleVariable, mySubscriptionResults, myContextSubscriptionResults);

tests/complex/traci/simulation/simulation/output.complex
@@ -1,7 +1,7 @@
 loaded? False
  Retrying in 1 seconds
-version at start (20, 'SUMO v1_11_0+0462-d030828f8b9')
-version (20, 'SUMO v1_11_0+0462-d030828f8b9')
+version at start (20, 'SUMO v1_12_0+0384-322f30a63b1')
+version (20, 'SUMO v1_12_0+0384-322f30a63b1')
 loaded? True
 endTime 42.0
 {114: ('horiz', 'horiz1'), 116: ()}
@@ -54,12 +54,12 @@ convertRoad3D (-1.22, 3903.52, 2.0)
 convertRoadGeo (12.014678141837997, 48.121449189894975)
 convertRoadGeoAlt (12.014678141837997, 48.121449189894975, 2.0)
 convertRoad2D length (667.2699999999995, 3124.688)
-convert2DGeo (12.023018905461857, 48.09105459778215)
-convertGeo2D (-1619.565396778169, -9550.720808432437)
+convert2DGeo (12.023018905461857, 48.09105459778214)
+convertGeo2D (-1619.5653967780527, -9550.720808431506)
 convert2DRoad ('o', 2906.189220451069, 0)
-convertGeoRoad ('o', 1012.1569043554978, 0)
-convertGeoRoadBus ('o', 1012.1569043554978, 0)
-convertGeoRoadBusDisallowed ('2o', 8027.707315024791, 0)
+convertGeoRoad ('o', 1012.1569043555733, 0)
+convertGeoRoadBus ('o', 1012.1569043555733, 0)
+convertGeoRoadBusDisallowed ('2o', 8027.707315024644, 0)
 distance2D 10.0
 drivingDistance2D 6.48934561332
 distanceRoad 5131.87897232
@@ -97,7 +97,7 @@ findIntermodalRoute (car)
 findIntermodalRoute (bike,car,public)
   Stage(type=3, vType=DEFAULT_VEHTYPE, line=car, edges=('o', '2o'), travelTime=729.248201439, cost=729.248201439, length=20111.55, intended=car, depart=6.00096, departPos=0.0, arrivalPos=5000.0)
 
-contextSubscriptions: {}
+contextSubscriptions: {'': {'horiz': {66: (22.189899364245058, 3876.0730045306937)}, 'horiz1': {66: (2.0895619928759057, 3899.639695706057)}}}
 step 0
 {114: (), 116: ()}
 step 1

tests/complex/traci/simulation/simulation/runner.py
@@ -24,8 +24,8 @@ from __future__ import absolute_import
 import os
 import sys
 
-SUMO_HOME = os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..")
-sys.path.append(os.path.join(os.environ.get("SUMO_HOME", SUMO_HOME), "tools"))
+SUMO_HOME = os.path.abspath(os.environ['SUMO_HOME'])
+sys.path.append(os.path.join(SUMO_HOME, "tools"))
 import traci  # noqa
 import sumolib  # noqa
 
@@ -178,7 +178,7 @@ traci.vehicle.setSpeed("emergencyStopper", 100)
 try:
     traci.simulation.subscribeContext("",
                                       traci.constants.CMD_GET_VEHICLE_VARIABLE, 42,
-                                      [traci.constants.VAR_SPEED])
+                                      [traci.constants.VAR_POSITION])
     print("contextSubscriptions:", traci.simulation.getAllContextSubscriptionResults())
 except traci.TraCIException:
     pass

commit 321130315a47285cc3a1a2d3a184275a483abb2a

fixing dependency on internal include, broken since dc1afbe8beccf07883a0820372c009462dd8c590

src/libsumo/Lane.h
@@ -33,6 +33,7 @@ class MSLane;
 class PositionVector;
 #endif
 
+
 // ===========================================================================
 // class definitions
 // ===========================================================================

src/libsumo/Simulation.h
@@ -20,7 +20,14 @@
 #pragma once
 #include <vector>
 #include <libsumo/TraCIDefs.h>
-#include <utils/geom/PositionVector.h>
+
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+#ifndef LIBTRACI
+class PositionVector;
+#endif
 
 
 // ===========================================================================