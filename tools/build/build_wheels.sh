4e53bfee59a25cd9c69132e3841607108e01fca0
Michael Behrisch <michael.behrisch@dlr.de>
2022-04-12 22:13:21 +0200

    trying to update xerces-c in linux wheels #10456

--- tools/build/build_wheels.sh
+++ tools/build/build_wheels.sh
@@ -15,7 +15,7 @@
 # @author  Michael Behrisch
 # @date    2019
 yum-config-manager --add-repo=https://download.opensuse.org/repositories/science:/dlr/CentOS_7/
-yum install -y --nogpgcheck cmake3 ccache xerces-c-devel proj-devel fox16-devel bzip2-devel gl2ps-devel swig3
+yum install -y --nogpgcheck cmake3 ccache libxerces-c-devel proj-devel fox16-devel bzip2-devel gl2ps-devel swig3
 /opt/python/cp38-cp38/bin/pip install scikit-build
 
 mkdir -p $HOME/.ccache
