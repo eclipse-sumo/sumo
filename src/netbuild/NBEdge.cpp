4e99b77337b2cfd5c247add4eaf03094200fd515
namdre <jakob.erdmann@dlr.de>
2022-04-26 15:46:48 +0200

    Revert fix for #10582: Sometimes empty street names are intentional (i.e. on motorways that have a bridge)

--- src/netbuild/NBEdge.cpp
+++ src/netbuild/NBEdge.cpp
@@ -3630,8 +3630,7 @@ NBEdge::expandableBy(NBEdge* possContinuation, std::string& reason) const {
         }
     }
     // if given identically osm names
-    if (!OptionsCont::getOptions().isDefault("output.street-names") && myStreetName != possContinuation->getStreetName()
-            && myStreetName != "" && possContinuation->getStreetName() != "") {
+    if (!OptionsCont::getOptions().isDefault("output.street-names") && myStreetName != possContinuation->getStreetName()) {
         return false;
     }
 
@@ -3662,12 +3661,6 @@ NBEdge::append(NBEdge* e) {
         for (int i = 0; i < (int)myLanes.size(); i++) {
             myLanes[i].width = e->myLanes[i].width;
         }
-    }
-    // defined name prevails over undefined name
-    if (myStreetName == ""
-            // name of longer street prevails
-            || (e->myStreetName != "" && e->getLength() > myLength)) {
-        myStreetName = e->myStreetName;
     }
     // recompute length
     myLength += e->myLength;
