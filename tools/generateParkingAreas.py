486b0e4e4aeba685067b9e742c411657911912c6
namdre <jakob.erdmann@dlr.de>
2022-04-27 17:20:40 +0200

    fix #10615

--- tools/generateParkingAreas.py
+++ tools/generateParkingAreas.py
@@ -48,6 +48,10 @@ def get_options(args=None):
                            help="Minimum capacity for parkingAreas")
     optParser.add_argument("--max", type=int, default=uMax,
                            help="Maximum capacity for parkingAreas")
+    optParser.add_argument("--edge-type.keep", dest="edgeTypeKeep",
+                           help="Optional list of edge types to keep exclusively")
+    optParser.add_argument("--edge-type.remove", dest="edgeTypeRemove",
+                           help="Optional list of edge types to exclude")
     optParser.add_argument("--keep-all", action="store_true", default=False, dest="keepAll",
                            help="whether to keep parkingAreas with 0 capacity")
     optParser.add_argument("-a", "--angle", type=float,
@@ -66,6 +70,11 @@ def get_options(args=None):
         optParser.print_help()
         sys.exit(1)
 
+    if options.edgeTypeKeep:
+        options.edgeTypeKeep = options.edgeTypeKeep.split(',')
+    if options.edgeTypeRemove:
+        options.edgeTypeRemove = options.edgeTypeRemove.split(',')
+
     return options
 
 
@@ -78,6 +87,10 @@ def main(options):
     with open(options.outfile, 'w') as outf:
         sumolib.writeXMLHeader(outf, "$Id$", "additional", options=options)  # noqa
         for edge in net.getEdges():
+            if options.edgeTypeKeep and not edge.getType() in options.edgeTypeKeep:
+                continue
+            if options.edgeTypeRemove and edge.getType() in options.edgeTypeRemove:
+                continue
             for lane in edge.getLanes():
                 if lane.allows(options.vclass):
                     if random.random() < options.probability:
