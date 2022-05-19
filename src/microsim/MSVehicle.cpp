cb53bbe7770b57acd3028a2adfec7274998b06e2
namdre <jakob.erdmann@dlr.de>
2022-05-19 17:39:14 +0200

    addendum refs #10445

--- src/microsim/MSVehicle.cpp
+++ src/microsim/MSVehicle.cpp
@@ -5174,7 +5174,7 @@ MSVehicle::leaveLane(const MSMoveReminder::Notification reason, const MSLane* ap
     if ((reason == MSMoveReminder::NOTIFICATION_JUNCTION || reason == MSMoveReminder::NOTIFICATION_TELEPORT) && myLane != nullptr) {
         myOdometer += getLane()->getLength();
     }
-    if (myLane->getBidiLane() != nullptr) {
+    if (myLane != nullptr && myLane->getBidiLane() != nullptr) {
         myLane->getBidiLane()->resetPartialOccupation(this);
     }
     if (reason != MSMoveReminder::NOTIFICATION_JUNCTION && reason != MSMoveReminder::NOTIFICATION_LANE_CHANGE) {
