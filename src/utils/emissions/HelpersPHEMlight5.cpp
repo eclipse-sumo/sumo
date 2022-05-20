a1681f652f9624494de06b40cd17095e549a4f53
Michael Behrisch <michael.behrisch@dlr.de>
2022-05-20 15:50:53 +0200

    synchronizing folder name and emission model name #10237

--- src/utils/emissions/HelpersPHEMlight5.cpp
+++ src/utils/emissions/HelpersPHEMlight5.cpp
@@ -66,7 +66,7 @@ HelpersPHEMlight5::getClassByName(const std::string& eClass, const SUMOVehicleCl
         phemPath.push_back(std::string(getenv("PHEMLIGHT_PATH")) + "/");
     }
     if (getenv("SUMO_HOME") != nullptr) {
-        phemPath.push_back(std::string(getenv("SUMO_HOME")) + "/data/emissions/PHEMlight/V5/");
+        phemPath.push_back(std::string(getenv("SUMO_HOME")) + "/data/emissions/PHEMlight5/");
     }
     myHelper.setCommentPrefix("c");
     myHelper.setPHEMDataV("V5");
