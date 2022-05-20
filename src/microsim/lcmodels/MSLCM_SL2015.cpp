07c35f43641969bfe170c3ddef267195300e6222
namdre <jakob.erdmann@dlr.de>
2022-05-20 15:27:28 +0200

    copy for sublane model refs #10767

--- src/microsim/lcmodels/MSLCM_SL2015.cpp
+++ src/microsim/lcmodels/MSLCM_SL2015.cpp
@@ -328,7 +328,7 @@ MSLCM_SL2015::patchSpeed(const double min, const double wanted, const double max
 
 
 double
-MSLCM_SL2015::_patchSpeed(double min, const double wanted, const double max, const MSCFModel& cfModel) {
+MSLCM_SL2015::_patchSpeed(double min, const double wanted, double max, const MSCFModel& cfModel) {
     if (wanted <= 0) {
         return wanted;
     }
@@ -349,6 +349,7 @@ MSLCM_SL2015::_patchSpeed(double min, const double wanted, const double max, con
         if (space >= 0) { // XXX space > -MAGIC_OFFSET
             // compute speed for decelerating towards a place which allows the blocking leader to merge in in front
             double safe = cfModel.stopSpeed(&myVehicle, myVehicle.getSpeed(), space);
+            max = MIN2(max, safe);
             // if we are approaching this place
             if (safe < wanted) {
                 if (safe < min) {
