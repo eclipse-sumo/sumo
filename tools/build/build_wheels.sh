9a5213bb7057b23e6afc1b53a8d65c6e0d726f45
Michael Behrisch <michael.behrisch@dlr.de>
2022-06-02 13:55:19 +0200

    trying to make wheel build arm compatible #3

--- tools/build/build_wheels.sh
+++ tools/build/build_wheels.sh
@@ -14,14 +14,15 @@
 # @file    build_wheels.sh
 # @author  Michael Behrisch
 # @date    2019
+yum install -y epel-release
 yum-config-manager --add-repo=https://download.opensuse.org/repositories/science:/dlr/CentOS_7/
-yum install -y --nogpgcheck cmake3 ccache libxerces-c-devel proj-devel fox16-devel bzip2-devel gl2ps-devel swig3
-/opt/python/cp38-cp38/bin/pip install scikit-build
+yum install -y --nogpgcheck ccache libxerces-c-devel proj-devel fox16-devel bzip2-devel gl2ps-devel swig3
+/opt/python/cp38-cp38/bin/pip install scikit-build cmake
 
 mkdir -p $HOME/.ccache
 echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
 echo "base_dir = /github/workspace/_skbuild/linux-x86_64-3.8" >> $HOME/.ccache/ccache.conf
-/opt/python/cp38-cp38/bin/python tools/build/setup-sumo.py --cmake-executable cmake3 -j 8 bdist_wheel
+/opt/python/cp38-cp38/bin/python tools/build/setup-sumo.py -j 8 bdist_wheel
 mv dist/eclipse_sumo-* `echo dist/eclipse_sumo-* | sed 's/cp38-cp38/py2.py3-none/'`
 auditwheel repair dist/eclipse_sumo*.whl
 cp -a data tools/libsumo
@@ -30,7 +31,7 @@ for py in /opt/python/*; do
     $py/bin/pip install scikit-build
     pminor=`echo $py | sed 's,/opt/python/cp3,,;s/-.*//'`
     echo "base_dir = /github/workspace/_skbuild/linux-x86_64-3.${pminor}" >> $HOME/.ccache/ccache.conf
-    $py/bin/python tools/build/setup-sumo.py --cmake-executable cmake3 -j 8 bdist_wheel
+    $py/bin/python tools/build/setup-sumo.py -j 8 bdist_wheel
     $py/bin/python tools/build/setup-libsumo.py bdist_wheel
     $py/bin/python tools/build/setup-libtraci.py bdist_wheel
     auditwheel repair dist/libsumo*.whl
