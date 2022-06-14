dc54837253515ee19ecea3fbaee06b0a81417f44
Yun-Pang Flötteröd <yun-pang.floetteroed@dlr.de>
2022-06-14 12:43:06 +0200

    directly using fromTaz/ToTaz for OD-counts; stopping the script when no od-edge info is available with the active option edgeod, #10924

--- tools/route/route2OD.py
+++ tools/route/route2OD.py
@@ -109,40 +109,50 @@ def main(options):
         end = 0
 
     # begin -> od -> count
-    if options.edgeod:
-        intervals_edge = defaultdict(lambda: defaultdict(lambda: 0))
-    else:
-        intervals = defaultdict(lambda: defaultdict(lambda: 0))
-
-    def addVehicle(vehID, fromEdge, toEdge, time, count=1):
+    intervals = defaultdict(lambda: defaultdict(lambda: 0))
+    intervals_edge = defaultdict(lambda: defaultdict(lambda: 0))  # for options.edgeod
+    
+    def addVehicle(vehID, fromEdge, toEdge, time, count=1, isTaz=False):
         nl.numVehicles += count
         if options.interval is None:
             intervalBegin = 0
         else:
             intervalBegin = int(time / options.interval) * options.interval
 
-        if options.edgeod:
+        if options.edgeod and not isTaz:
             intervals_edge[intervalBegin][(fromEdge, toEdge)] += count
         else:
             fromTaz = None
             toTaz = None
-            if fromEdge in edgeFromTaz:
-                fromTaz = random.choice(edgeFromTaz[fromEdge])
+            if isTaz:
+                fromTaz = fromEdge
+                toTaz = toEdge
             else:
-                nl.numFromNotFound += 1
-                if nl.numFromNotFound < 5:
-                    print("No fromTaz found for edge '%s' of vehicle '%s' " % (fromEdge, vehID))
-            if toEdge in edgeToTaz:
-                toTaz = random.choice(edgeToTaz[toEdge])
-            else:
-                nl.numToNotFound += 1
-                if nl.numToNotFound < 5:
-                    print("No toTaz found for edge '%s' of vehicle '%s' " % (toEdge, vehID))
+                if fromEdge in edgeFromTaz:
+                    fromTaz = random.choice(edgeFromTaz[fromEdge])
+                if toEdge in edgeToTaz:
+                    toTaz = random.choice(edgeToTaz[toEdge])
+
             if fromTaz and toTaz:
                 intervals[intervalBegin][(fromTaz, toTaz)] += count
+            else:
+                if fromTaz is None:   
+                    nl.numFromNotFound += 1
+                    if nl.numFromNotFound < 5:
+                        if isTaz:
+                            print("No fromTaz found for vehicle '%s' " % (vehID))
+                        else:
+                            print("No fromTaz found for edge '%s' of vehicle '%s' " % (fromEdge, vehID))
+                if toTaz is None:
+                    nl.numToNotFound += 1
+                    if nl.numToNotFound < 5:
+                        if isTaz:
+                            print("No toTaz found for vehicle '%s' " % (vehID))
+                        else:
+                            print("No toTaz found for edge '%s' of vehicle '%s' " % (toEdge, vehID))            
 
         nl.end = max(nl.end, time)
-
+    
     for vehicle in sumolib.xml.parse(options.routefile, ['vehicle']):
         if vehicle.route and type(vehicle.route) == list:
             edges = vehicle.route[0].edges.split()
@@ -151,7 +161,14 @@ def main(options):
             print("No edges found for vehicle '%s'" % vehicle.id)
 
     for trip in sumolib.xml.parse(options.routefile, ['trip']):
-        addVehicle(trip.id, trip.attr_from, trip.to, parseTime(trip.depart))
+        if trip.attr_from and trip.to:
+            addVehicle(trip.id, trip.attr_from, trip.to, parseTime(trip.depart))
+        elif trip.fromTaz and trip.toTaz:
+            if options.edgeod:
+                print("No OD-edge information! Only TAZ-based OD counts with a given district file can be calculated.")
+                sys.exit(1)
+            else:
+                addVehicle(trip.id, trip.fromTaz, trip.toTaz, parseTime(trip.depart), 1, True)
 
     for flow in sumolib.xml.parse(options.routefile, ['flow']):
         count = None
@@ -168,12 +185,18 @@ def main(options):
         if count is None:
             print("Could not determine count for flow '%s'" % (flow.id))
             count = 1
-
         if flow.attr_from and flow.to:
             addVehicle(flow.id, flow.attr_from, flow.to, parseTime(flow.begin), count)
         elif flow.route and type(flow.route) == list:
             edges = flow.route[0].edges.split()
             addVehicle(flow.id, edges[0], edges[-1], parseTime(flow.begin), count)
+        elif flow.fromTaz and flow.toTaz:
+            if options.edgeod:
+                print("No OD-edge information! A district file is needed for TAZ-based OD counts.")
+                sys.exit(1)
+            else:
+                addVehicle(flow.id, flow.fromTaz, flow.toTaz, parseTime(flow.begin), count, True)
+
         else:
             print("No edges found for flow '%s'" % flow.id)
 
