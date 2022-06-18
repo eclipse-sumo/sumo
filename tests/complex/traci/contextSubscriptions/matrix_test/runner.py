f982d06577e9bebc566dcd9942df29318eb8ef3b
namdre <jakob.erdmann@dlr.de>
2022-06-18 09:08:26 +0200

    fixing style refs #12

--- tests/complex/traci/contextSubscriptions/matrix_test/runner.py
+++ tests/complex/traci/contextSubscriptions/matrix_test/runner.py
@@ -21,24 +21,20 @@ from __future__ import print_function
 
 import os
 import sys
-import math
 
 if "SUMO_HOME" in os.environ:
     sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
 import sumolib  # noqa
 import traci  # noqa
 
+
 def runSingle(traciEndTime, viewRange, domain, domain2):
-    seen1 = 0
-    seen2 = 0
-    step = 0
-    subscribed = False
     ids = domain.getIDList()
     if not ids:
         print("No objects for domain '%s' at time %s" % (
             domain._name, traci.simulation.getTime()))
         return
-    egoID  = ids[0]
+    egoID = ids[0]
 
     domain.subscribeContext(egoID, domain2.DOMAIN_ID, viewRange)
     responses = traci.simulationStep()
@@ -56,17 +52,18 @@ def runSingle(traciEndTime, viewRange, domain, domain2):
 
 
 def restart():
-    traci.start([sumolib.checkBinary(sys.argv[1]), '-Q', "-c", "sumo.sumocfg",
-        '-a', 'input_additional.add.xml'])
+    traci.start([sumolib.checkBinary(sys.argv[1]),
+                '-Q', "-c", "sumo.sumocfg",
+                '-a', 'input_additional.add.xml'])
     traci.simulationStep()
 
-restart()
 
+#  main
+restart()
 for domain in traci.domain._defaultDomains:
     for domain2 in traci.domain._defaultDomains:
         try:
             runSingle(2, 100, domain, domain2)
         except traci.FatalTraCIError:
-            #restart()
+            # restart()
             sys.exit()
-
