ad422685292c2a80434e874d33f6f932eefd0e37
Angelo Banse <angelo_22@outlook.com>
2022-07-11 14:54:28 +0200

    adding ability to copy code from docs - refs #2 and closes #11050

--- docs/web/theme/js/base.js
+++ docs/web/theme/js/base.js
@@ -210,3 +210,38 @@ var keyCodes = {
   221: '&rsqb;',
   222: '&apos;',
 };
+
+////////////////////////////////////////////////////
+// Copy code - based on https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
+////////////////////////////////////////////////////
+
+const copyButtonLabel = "Copy";
+
+// you can use a class selector instead if you, or the syntax highlighting library adds one to the 'pre'. 
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("copyCodeButton");
+    button.innerText = copyButtonLabel;
+    button.addEventListener("click", copyCode);
+    block.appendChild(button);
+  }
+});
+
+async function copyCode(event) {
+  const button = event.srcElement;
+  const pre = button.parentElement;
+  let code = pre.querySelector("code");
+  let text = code.innerText;
+  await navigator.clipboard.writeText(text);
+  
+  button.innerText = "Copied!";
+  button.style.color = "#338033";
+  
+  setTimeout(()=> {
+    button.innerText = copyButtonLabel;
+    button.style.color = "#1e1e1e";
+  },1000)
+}
