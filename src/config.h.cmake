a30eefbf84275072e4a57b0a22f569d20bce4d7e
Michael Behrisch <oss@behrisch.de>
2022-07-11 17:35:16 +0200

    release bump 1.14.0 #563

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -198,10 +198,10 @@
 #cmakedefine HAVE_PYTHON
 
 /* Define if auto-generated version.h should be used. */
-#define HAVE_VERSION_H
+//#define HAVE_VERSION_H
 #ifndef HAVE_VERSION_H
     /* Define if auto-generated version.h is unavailable. */
-    #define VERSION_STRING "1.13.0"
+    #define VERSION_STRING "1.14.0"
 #endif
 
 /* defines the epsilon to use on general floating point comparison */
