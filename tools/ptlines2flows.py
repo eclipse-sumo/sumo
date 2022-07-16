92d38de7be1fdd4fa0439bfb73d5d96616410535
namdre <jakob.erdmann@dlr.de>
2022-07-16 11:28:25 +0200

    removing superfluous load path patch refs #12

--- tools/ptlines2flows.py
+++ tools/ptlines2flows.py
@@ -30,13 +30,6 @@ from optparse import OptionParser
 import sumolib
 from sumolib.xml import quoteattr
 
-if 'SUMO_HOME' in os.environ:
-    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-    sys.path.append(tools)
-else:
-    sys.exit("please declare environment variable 'SUMO_HOME'")
-
-
 def get_options(args=None):
     optParser = OptionParser()
     optParser.add_option("-n", "--net-file", dest="netfile", help="network file")
