5cd56c42630f73051b267bd48cdea04244e55a16
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2022-08-10 09:41:46 +0200

    Added MFXLabelTooltip. Refs #11313

---
+++ src/utils/foxtools/MFXLabelTooltip.cpp
@@ -0,0 +1,67 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2022 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    MFXLabelTooltip.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    2022-08-10
+///
+// Label similar to FXLabel but with the possibility of showing tooltips
+/****************************************************************************/
+#include <config.h>
+
+#include "MFXLabelTooltip.h"
+
+
+FXDEFMAP(MFXLabelTooltip) MFXLabelTooltipMap[] = {
+    FXMAPFUNC(SEL_ENTER,    0,  MFXLabelTooltip::onEnter),
+    FXMAPFUNC(SEL_LEAVE,    0,  MFXLabelTooltip::onLeave),
+};
+
+
+// Object implementation
+FXIMPLEMENT(MFXLabelTooltip, FXLabel, MFXLabelTooltipMap, ARRAYNUMBER(MFXLabelTooltipMap))
+
+MFXLabelTooltip::MFXLabelTooltip(FXComposite* p,
+                                   const FXString& text, FXIcon* ic,
+                                   FXuint opts,
+                                   FXint x, FXint y, FXint w, FXint h,
+                                   FXint pl, FXint pr, FXint pt, FXint pb) :
+    FXLabel(p, text, ic, opts, x, y, w, h, pl, pr, pt, pb) {
+}
+
+
+MFXLabelTooltip::~MFXLabelTooltip() {}
+
+
+long
+MFXLabelTooltip::onEnter(FXObject* sender, FXSelector sel, void* ptr) {
+    // create on first enter
+    if (myStaticToolTip == nullptr) {
+        myStaticToolTip = new MFXStaticToolTip(getApp());
+        myStaticToolTip->create();
+    }
+    // show tip show
+    myStaticToolTip->onTipShow(sender, sel, ptr);
+    return FXLabel::onEnter(sender, sel, ptr);
+}
+
+
+long
+MFXLabelTooltip::onLeave(FXObject* sender, FXSelector sel, void* ptr) {
+    // hide tip show
+    myStaticToolTip->onTipHide(sender, sel, this);
+    return FXLabel::onLeave(sender, sel, ptr);
+}
+
+
+/****************************************************************************/
