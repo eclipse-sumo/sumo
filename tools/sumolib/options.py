80d150f62b95e2c5e148fda2a9b3b2c55ac54115
Benjamin Couéraud <benjamin.coueraud@dlr.de>
2022-08-16 14:57:01 +0200

    [tools/randomTrips] Made randomTrips catch the exception raised in the library code (options.py).

--- tools/sumolib/options.py
+++ tools/sumolib/options.py
@@ -293,8 +293,7 @@ class ArgumentParser(argparse.ArgumentParser):
 
         for program in namespace._prefixed_options:
             if program not in self._allowed_programs:
-                print("The program '%s' (passed in a argument) isn't recognized." % program, file=sys.stderr)
-                sys.exit(1)
+                raise ValueError("The program '%s' (passed in a argument) isn't recognized." % program)
             prefixed_options = deepcopy(namespace._prefixed_options[program])
             for option in prefixed_options:
                 option[0] = program + '-' + option[0]
