d5c54713cd8bf128c93555b2d021d0c3237a1170
namdre <jakob.erdmann@dlr.de>
2022-09-06 08:54:45 +0200

    fix #7006

--- src/microsim/MSRouteHandler.cpp
+++ src/microsim/MSRouteHandler.cpp
@@ -1079,7 +1079,8 @@ MSRouteHandler::addRideOrTransport(const SUMOSAXAttributes& attrs, const SumoXML
                         from, nullptr, -1, myVehicleParameter->depart, myVehicleParameter->departPos, "start", true));
             }
         }
-        if (to == nullptr) {
+        // given attribute may override given stopping place due access requirements
+        if (to == nullptr || attrs.hasAttribute(SUMO_ATTR_TO)) {
             const std::string toID = attrs.get<std::string>(SUMO_ATTR_TO, aid.c_str(), ok);
             to = MSEdge::dictionary(toID);
             if (to == nullptr) {
