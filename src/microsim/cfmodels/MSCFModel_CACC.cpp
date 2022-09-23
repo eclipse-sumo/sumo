de49d47f399311ca07296fbd5e04aad448de063b
namdre <jakob.erdmann@dlr.de>
2022-09-23 09:54:41 +0200

    prevent adverse effect of emergency-deceleration-smoothing on CACC collision avoidance. refs #11653

--- src/microsim/cfmodels/MSCFModel_CACC.cpp
+++ src/microsim/cfmodels/MSCFModel_CACC.cpp
@@ -120,7 +120,8 @@ MSCFModel_CACC::followSpeed(const MSVehicle* const veh, double speed, double gap
 
     const double desSpeed = veh->getLane()->getVehicleMaxSpeed(veh);
     const double vCACC = _v(veh, pred, gap2pred, speed, predSpeed, desSpeed, true, usage);
-    const double vSafe = maximumSafeFollowSpeed(gap2pred, speed, predSpeed, predMaxDecel);
+    // using onInsertion=true disables emergencyOverides emergency deceleration smoothing
+    const double vSafe = maximumSafeFollowSpeed(gap2pred, speed, predSpeed, predMaxDecel, true);
 
 #if DEBUG_CACC == 1
     if (DEBUG_COND) {
