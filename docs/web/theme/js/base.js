9f5d4d41dacf2bad824cac8955c39e31130163e3
Angelo Banse <angelo_22@outlook.com>
2022-10-17 16:45:11 +0200

    showing a message before activating YouTube embedded videos - refs #2

--- docs/web/theme/js/base.js
+++ docs/web/theme/js/base.js
@@ -245,3 +245,56 @@ async function copyCode(event) {
     button.style.color = "#1e1e1e";
   },1000)
 }
+
+////////////////////////////////////////////////////
+// https://gomakethings.com/how-to-lazy-load-youtube-videos-with-vanilla-javascript/
+////////////////////////////////////////////////////
+
+// Get all of the videos
+let videos = document.querySelectorAll('[data-youtube]');
+
+for (let video of videos) {
+	// Get the video ID
+	let id = new URL(video.href).searchParams.get('v');
+	
+    // Add the ID to the data-youtube attribute
+	video.setAttribute('data-youtube', id);
+
+	// Add a role of button
+	video.setAttribute('role', 'button');
+
+	// Add a thumbnail
+	video.innerHTML =
+		`${video.textContent}<br><div class="video-container">
+        <img alt="" class="tutorial-video" src="https://img.youtube.com/vi/${id}/maxresdefault.jpg">
+        <div class="centered video-text">Click to load external resources from YouTube.</div>
+        </div>`;
+}
+
+/**
+ * Handle click events on the video thumbnails
+ * @param  {Event} event The event object
+ */
+function clickHandler (event) {
+
+	// Get the video link
+	let link = event.target.closest('[data-youtube]');
+	if (!link) return;
+
+	// Prevent the URL from redirecting users
+	event.preventDefault();
+
+	// Get the video ID
+	let id = link.getAttribute('data-youtube');
+
+	// Create the player
+	let player = document.createElement('div');
+	player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+	// Inject the player into the UI
+	link.replaceWith(player);
+
+}
+
+// Detect clicks on the video thumbnails
+document.addEventListener('click', clickHandler);
