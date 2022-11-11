9d9bbfa8cc6f7bae4a93ae13ee4dac2afacb56ed
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2022-11-11 10:34:41 +0100

    Updated GNEMeanDataFrame. Refs #11897

--- src/netedit/frames/data/GNEMeanDataFrame.h
+++ src/netedit/frames/data/GNEMeanDataFrame.h
@@ -20,7 +20,8 @@
 #pragma once
 #include <config.h>
 
-#include "GNEGenericDataFrame.h"
+#include <netedit/frames/GNEFrame.h>
+#include <netedit/frames/GNEPathCreator.h>
 
 
 // ===========================================================================
@@ -30,108 +31,26 @@
  * @class GNEMeanDataFrame
  * The Widget for setting internal attributes of additional elements
  */
-class GNEMeanDataFrame : public GNEGenericDataFrame {
+class GNEMeanDataFrame : public GNEFrame {
 
 public:
-    // ===========================================================================
-    // Confirm TAZ relation
-    // ===========================================================================
-
-    class ConfirmTAZRelation : public MFXGroupBoxModule {
-        /// @brief FOX-declaration
-        FXDECLARE(GNEMeanDataFrame::ConfirmTAZRelation)
-
-    public:
-        /// @brief constructor
-        ConfirmTAZRelation(GNEMeanDataFrame* MeanDataFrame);
-
-        /// @brief destructor
-        ~ConfirmTAZRelation();
-
-        /// @brief called when user press confirm TAZ Relation button
-        long onCmdConfirmTAZRelation(FXObject*, FXSelector, void*);
-
-        /// @brief called when TAZ Relation button is updated
-        long onUpdConfirmTAZRelation(FXObject*, FXSelector, void*);
-
-        /// @brief called when user press clear TAZ Relation button
-        long onCmdClearSelection(FXObject*, FXSelector, void*);
-
-    protected:
-        /// @brief FOX needs this
-        FOX_CONSTRUCTOR(ConfirmTAZRelation)
-
-    private:
-        /// @brief pointer to MeanDataFrame parent
-        GNEMeanDataFrame* myMeanDataFrame = nullptr;
-
-        /// @brief confirm TAZ Button
-        FXButton* myConfirmTAZButton = nullptr;
-
-        /// @brief clear TAZ Button
-        FXButton* myClearTAZButton = nullptr;
-    };
-
-    // ===========================================================================
-    // class Legend
-    // ===========================================================================
-
-    class Legend : public MFXGroupBoxModule {
-
-    public:
-        /// @brief constructor
-        Legend(GNEMeanDataFrame* MeanDataFrame);
-
-        /// @brief destructor
-        ~Legend();
-
-        /// @brief set labels
-        void setLabels(const GNETAZ* fromTAZ, const GNETAZ* toTAZ);
-
-    private:
-        /// @brief from TAZ label
-        FXLabel* myFromTAZLabel;
-
-        /// @brief to TAZ Label
-        FXLabel* myToTAZLabel;
-    };
-
-    /**@brief Constructor
+    /**@brief Constructor (protected due GNEMeanDataFrame is abtract)
      * @brief viewParent GNEViewParent in which this GNEFrame is placed
-     * @brief viewNet viewNet that uses this GNEMeanDataFrame
+     * @brief viewNet GNEViewNet that uses this GNEFrame
      */
     GNEMeanDataFrame(GNEViewParent* viewParent, GNEViewNet* viewNet);
 
     /// @brief Destructor
     ~GNEMeanDataFrame();
 
-    /// @brief set clicked TAZ
-    bool setTAZ(const GNEViewNetHelper::ObjectsUnderCursor& objectsUnderCursor);
+    /// @brief show Frame
+    void show();
 
-    /// @brief build TAZRelation data
-    void buildTAZRelationData();
+    /// @brief hide Frame
+    void hide();
 
-    /// @brief get first selected TAZ Element
-    GNEAdditional* getFirstTAZ() const;
-
-    /// @brief get first selected TAZ Element
-    GNEAdditional* getSecondTAZ() const;
-
-    /// @brief clear TAZ selection
-    void clearTAZSelection();
-
-protected:
-    /// @brief first selected TAZ Element
-    GNETAZ* myFirstTAZ = nullptr;
-
-    /// @brief first selected TAZ Element
-    GNETAZ* mySecondTAZ = nullptr;
-
-    /// @brief confirm TAZ Relation
-    GNEMeanDataFrame::ConfirmTAZRelation* myConfirmTAZRelation = nullptr;
-
-    /// @brief TAZRel legend
-    GNEMeanDataFrame::Legend* myLegend = nullptr;
+    /// @brief function called after undo/redo in the current frame
+    void updateFrameAfterUndoRedo();
 
 private:
     /// @brief Invalidated copy constructor.
@@ -140,5 +59,3 @@ private:
     /// @brief Invalidated assignment operator.
     GNEMeanDataFrame& operator=(const GNEMeanDataFrame&) = delete;
 };
-
-/****************************************************************************/
