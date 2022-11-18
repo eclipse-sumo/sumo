bb480ffa44ae603a7f21e5b7653dedf3fff731da
Michael Behrisch <michael.behrisch@dlr.de>
2022-11-18 19:21:09 +0100

    memleaks with duplicate rerouter id #13

--- src/netload/NLTriggerBuilder.cpp
+++ src/netload/NLTriggerBuilder.cpp
@@ -669,26 +669,29 @@ NLTriggerBuilder::parseAndBuildRerouter(MSNet& net, const SUMOSAXAttributes& att
     if (!ok) {
         throw ProcessError();
     }
+    if (MSTriggeredRerouter::getInstances().count(id) > 0) {
+        throw InvalidArgument("Could not build rerouter '" + id + "'; probably declared twice.");
+    }
     MSEdgeVector edges;
     for (const std::string& edgeID : attrs.get<std::vector<std::string> >(SUMO_ATTR_EDGES, id.c_str(), ok)) {
         MSEdge* edge = MSEdge::dictionary(edgeID);
         if (edge == nullptr) {
-            throw InvalidArgument("The edge '" + edgeID + "' to use within MSTriggeredRerouter '" + id + "' is not known.");
+            throw InvalidArgument("The edge '" + edgeID + "' to use within rerouter '" + id + "' is not known.");
         }
         edges.push_back(edge);
     }
     if (!ok) {
-        throw InvalidArgument("The edge to use within MSTriggeredRerouter '" + id + "' is not known.");
+        throw InvalidArgument("The edge to use within rerouter '" + id + "' is not known.");
     }
     if (edges.size() == 0) {
-        throw InvalidArgument("No edges found for MSTriggeredRerouter '" + id + "'.");
+        throw InvalidArgument("No edges found for rerouter '" + id + "'.");
     }
     double prob = attrs.getOpt<double>(SUMO_ATTR_PROB, id.c_str(), ok, 1);
     bool off = attrs.getOpt<bool>(SUMO_ATTR_OFF, id.c_str(), ok, false);
     SUMOTime timeThreshold = TIME2STEPS(attrs.getOpt<double>(SUMO_ATTR_HALTING_TIME_THRESHOLD, id.c_str(), ok, 0));
     const std::string vTypes = attrs.getOpt<std::string>(SUMO_ATTR_VTYPES, id.c_str(), ok, "");
     if (!ok) {
-        throw InvalidArgument("Could not parse MSTriggeredRerouter '" + id + "'.");
+        throw InvalidArgument("Could not parse rerouter '" + id + "'.");
     }
     MSTriggeredRerouter* trigger = buildRerouter(net, id, edges, prob, off, timeThreshold, vTypes);
     // read in the trigger description
