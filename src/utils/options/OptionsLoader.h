4a2e64ea147c05cd24476006410b34756f236f46
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2022-12-02 10:01:10 +0100

    Refactored documentation of OptionsLoader.h. Refs #11896 and #12

--- src/utils/options/OptionsLoader.h
+++ src/utils/options/OptionsLoader.h
@@ -42,17 +42,13 @@ class OptionsCont;
  * @brief A SAX-Handler for loading options
  */
 class OptionsLoader : public XERCES_CPP_NAMESPACE::HandlerBase {
+
 public:
-    /** @brief Constructor
-     */
+    /// @brief Constructor
     OptionsLoader(const bool routeOnly = false);
 
-
-    /** destructor */
+    /// @brief destructor
     ~OptionsLoader();
-
-
-
 
     /// @name Handlers for the SAX DocumentHandler interface
     /// @{
@@ -64,7 +60,6 @@ public:
     virtual void startElement(const XMLCh* const name,
                               XERCES_CPP_NAMESPACE::AttributeList& attributes);
 
-
     /** @brief Called on the occurence of character data
      *
      * If this occurs inside a single tag it sets the option named
@@ -74,16 +69,13 @@ public:
      */
     void characters(const XMLCh* const chars, const XERCES3_SIZE_t length);
 
-
     /** @brief Called on the end of an element
      *
      * Resets the element name
      */
     void endElement(const XMLCh* const name);
+
     /// @}
-
-
-
 
     /// @name Handlers for the SAX ErrorHandler interface
     /// @{
@@ -94,13 +86,11 @@ public:
      */
     void warning(const XERCES_CPP_NAMESPACE::SAXParseException& exception);
 
-
     /** @brief Called on an XML-error
      *
      * The warning is reported to the the error-instance of MsgHandler
      */
     void error(const XERCES_CPP_NAMESPACE::SAXParseException& exception);
-
 
     /** @brief Called on an XML-fatal error
      *
@@ -109,49 +99,13 @@ public:
     void fatalError(const XERCES_CPP_NAMESPACE::SAXParseException& exception);
     /// @}
 
-
-
-    /** @brief Returns the information whether an error occurred */
+    /// @brief Returns the information whether an error occurred
     bool errorOccurred() const;
 
-    /** @brief Returns the last item read */
+    /// @brief Returns the last item read
     const std::string& getItem() const {
         return myItem;
     }
-
-
-private:
-    /** @brief Tries to set the named option to the given value
-     *
-     * Also evaluates whether it is a boolean or a filename option and
-     *  does the relevant checks / modifications.
-     *
-     * @param[in] key The name of the option to set
-     * @param[in] value The new value for the option
-     */
-    void setValue(const std::string& key, const std::string& value);
-
-
-    /** @brief Tries to set the named option to the given value
-     *
-     * Checks the item whether it was default before setting it.
-     * Returns the information whether the item was set before (was not a default value)
-     *
-     * @param[in] name The name of the option to set
-     * @param[in] value The new value for the option
-     * @return Whether the option could be set
-     */
-    bool setSecure(const std::string& name, const std::string& value) const;
-
-
-private:
-    /** invalid copy constructor */
-    OptionsLoader(const OptionsLoader& s);
-
-
-    /** invalid assignment operator */
-    OptionsLoader& operator=(const OptionsLoader& s);
-
 
 private:
     /// @brief The information whether only the root element should be parsed
@@ -169,4 +123,30 @@ private:
     /// @brief The currently read characters string
     std::string myValue;
 
+    /** @brief Tries to set the named option to the given value
+     *
+     * Also evaluates whether it is a boolean or a filename option and
+     *  does the relevant checks / modifications.
+     *
+     * @param[in] key The name of the option to set
+     * @param[in] value The new value for the option
+     */
+    void setValue(const std::string& key, const std::string& value);
+
+    /** @brief Tries to set the named option to the given value
+     *
+     * Checks the item whether it was default before setting it.
+     * Returns the information whether the item was set before (was not a default value)
+     *
+     * @param[in] name The name of the option to set
+     * @param[in] value The new value for the option
+     * @return Whether the option could be set
+     */
+    bool setSecure(const std::string& name, const std::string& value) const;
+
+    /// @brief invalid copy constructor
+    OptionsLoader(const OptionsLoader& s) = delete;
+
+    /// @brief invalid assignment operator
+    OptionsLoader& operator=(const OptionsLoader& s) = delete;
 };
