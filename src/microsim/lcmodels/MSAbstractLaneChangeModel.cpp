2d2cbd53baff153aeb9cbbf5d00c75f306dd51a5
namdre <jakob.erdmann@dlr.de>
2022-12-12 14:51:17 +0100

    addendum to addendum to 54c02565d491f4feadb99393e5a77c3015479fa9 refs #3

--- src/microsim/lcmodels/MSAbstractLaneChangeModel.cpp
+++ src/microsim/lcmodels/MSAbstractLaneChangeModel.cpp
@@ -36,7 +36,7 @@
 
 #include <utils/options/OptionsCont.h>
 #include <utils/xml/SUMOSAXAttributes.h>
-#include <utils/geom/GeomConvHelper.h>
+#include <utils/geom/GeomHelper.h>
 #include <microsim/MSNet.h>
 #include <microsim/MSEdge.h>
 #include <microsim/MSLane.h>
