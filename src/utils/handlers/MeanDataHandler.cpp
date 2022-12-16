720fba3fc00cab8be8a9bae121b731fb4fef8cc3
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-12-16 14:02:11 +0100

    Updated MeanDataHandler(). Refs #11896

--- src/utils/handlers/MeanDataHandler.cpp
+++ src/utils/handlers/MeanDataHandler.cpp
@@ -30,8 +30,7 @@
 // method definitions
 // ===========================================================================
 
-MeanDataHandler::MeanDataHandler(const std::string& file) :
-    SUMOSAXHandler(file) {
+MeanDataHandler::MeanDataHandler() {
 }
 
 
@@ -39,9 +38,48 @@ MeanDataHandler::~MeanDataHandler() {}
 
 
 bool
-MeanDataHandler::parse() {
-    // run parser and return result
-    return XMLSubSys::runParser(*this, getFileName());
+MeanDataHandler::beginParseAttributes(SumoXMLTag tag, const SUMOSAXAttributes& attrs) {
+    // open SUMOBaseOBject
+    myCommonXMLStructure.openSUMOBaseOBject();
+    // check tag
+    try {
+        switch (tag) {
+            case SUMO_TAG_MEANDATA_EDGE:
+                parseEdgeMeanData(attrs);
+                break;
+            case SUMO_TAG_MEANDATA_LANE:
+                parseLaneMeanData(attrs);
+                break;
+            case SUMO_TAG_PARAM:
+                WRITE_WARNING(TL("MeanData elements cannot load attributes as params"));
+                break;
+            default:
+                break;
+        }
+    } catch (InvalidArgument& e) {
+        writeError(e.what());
+    }
+    return true;
+}
+
+
+void
+MeanDataHandler::endParseAttributes() {
+    // get last inserted object
+    CommonXMLStructure::SumoBaseObject* obj = myCommonXMLStructure.getCurrentSumoBaseObject();
+    // close SUMOBaseOBject
+    myCommonXMLStructure.closeSUMOBaseOBject();
+    // check tag
+    switch (obj->getTag()) {
+        case SUMO_TAG_MEANDATA_EDGE:
+        case SUMO_TAG_MEANDATA_LANE:
+            parseSumoBaseObject(obj);
+            // delete object
+            delete obj;
+            break;
+        default:
+            break;
+    }
 }
 
 
@@ -98,55 +136,6 @@ MeanDataHandler::parseSumoBaseObject(CommonXMLStructure::SumoBaseObject* obj) {
     for (const auto& child : obj->getSumoBaseObjectChildren()) {
         // call this function recursively
         parseSumoBaseObject(child);
-    }
-}
-
-
-void
-MeanDataHandler::myStartElement(int element, const SUMOSAXAttributes& attrs) {
-    // obtain tag
-    const SumoXMLTag tag = (element == 0) ? SUMO_TAG_ROOTFILE : static_cast<SumoXMLTag>(element);
-    // open SUMOBaseOBject
-    myCommonXMLStructure.openSUMOBaseOBject();
-    // check tag
-    try {
-        switch (tag) {
-            case SUMO_TAG_MEANDATA_EDGE:
-                parseEdgeMeanData(attrs);
-                break;
-            case SUMO_TAG_MEANDATA_LANE:
-                parseLaneMeanData(attrs);
-                break;
-            case SUMO_TAG_PARAM:
-                WRITE_WARNING(TL("MeanData elements cannot load attributes as params"));
-                break;
-            default:
-                break;
-        }
-    } catch (InvalidArgument& e) {
-        writeError(e.what());
-    }
-}
-
-
-void
-MeanDataHandler::myEndElement(int element) {
-    // obtain tag
-    const SumoXMLTag tag = static_cast<SumoXMLTag>(element);
-    // get last inserted object
-    CommonXMLStructure::SumoBaseObject* obj = myCommonXMLStructure.getCurrentSumoBaseObject();
-    // close SUMOBaseOBject
-    myCommonXMLStructure.closeSUMOBaseOBject();
-    // check tag
-    switch (tag) {
-        case SUMO_TAG_MEANDATA_EDGE:
-        case SUMO_TAG_MEANDATA_LANE:
-            parseSumoBaseObject(obj);
-            // delete object
-            delete obj;
-            break;
-        default:
-            break;
     }
 }
 
