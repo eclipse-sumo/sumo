908d75bba8d63f759fe622274802c4f8be165680
namdre <jakob.erdmann@dlr.de>
2022-12-20 15:29:02 +0100

    refining test evaluation helper

--- tools/devel/checkTests.py
+++ tools/devel/checkTests.py
@@ -33,29 +33,45 @@ THREADS = "16"
 EXTRACT = os.path.join(os.environ['SUMO_HOME'], 'tools', 'extractTest.py')
 RUNSEEDS = os.path.join(os.environ['SUMO_HOME'], 'tools', 'runSeeds.py')
 ASTATS = os.path.join(os.environ['SUMO_HOME'], 'tools', 'output', 'attributeStats.py')
+ADIFF = os.path.join(os.environ['SUMO_HOME'], 'tools', 'output', 'attributeDiff.py')
 
-appdirs = [a + "_0" for a in apps.split(',')]
+applist = apps.split(',')
+appdirs = [a + "_0" for a in applist]
+tDirs = []
 
 for test in open(tests).readlines()[1:]:
     test = test.strip()
     fullpath = os.path.join(prefix, test)
     call([EXTRACT, fullpath])
     tdir = '_' + test.replace('/', '_')
+    tDirs.append(tdir)
     os.chdir(tdir)
-    call([RUNSEEDS,
-          '-k', 'test.sumocfg',
-          '-a', apps,
-          '--seeds', SEEDS,
-          '--threads', THREADS,
-          '--no-warnings',
-          '--statistic-output', 'stats.xml'])
+    rsargs = [RUNSEEDS,
+              '-k', 'test.sumocfg',
+              '-a', apps,
+              '--seeds', SEEDS,
+              '--threads', THREADS,
+              '--no-warnings',
+              '--statistic-output', 'stats.xml']
+    call(rsargs)
 
+    # clean file
+    outf = open('compare.txt', 'w')
+    outf.close()
     outf = open('compare.txt', 'a')
-    for ad in appdirs:
+    statFiles = []
+    for app, ad in zip(applist, appdirs):
         files = glob.glob(os.path.join(ad, '*.stats.xml'))
-        args = [ASTATS,
-                '-e', 'vehicleTripStatistics',
-                '-a', 'timeLoss'] + files
+        sFile = '%s_allStats.xml' % app
+        statFiles.append(sFile)
+        args = [ASTATS, '-x', sFile, '-b', '0'] + files
         call(args, stdout=outf)
     outf.close()
+
+    call([ADIFF, '-o', 'statsDiff.xml'] + statFiles)
     os.chdir('..')
+
+# final summary
+differences = [os.path.join(d, 'statsDiff.xml') for d in tDirs]
+args = [ASTATS, '-x', 'globalStats.xml', '-b', '0'] + differences
+call(args)
