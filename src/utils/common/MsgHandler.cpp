aafdb4bc0b31fc5cfa3a54c42db989347a76a5db
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-01-04 11:57:06 +0100

    Fixed Windows build. Refs #3

--- src/utils/common/MsgHandler.cpp
+++ src/utils/common/MsgHandler.cpp
@@ -227,12 +227,14 @@ MsgHandler::removeRetrieverFromAllInstances(OutputDevice* out) {
 void
 MsgHandler::setupI18n(const std::string& locale) {
 #ifdef HAVE_INTL
+/*
     if (locale != "") {
         setenv("LANGUAGE", locale.data(), true);
     }
     if (!setlocale(LC_MESSAGES, "")) {
         WRITE_WARNING("Could not set locale to '" + locale + "'.");
     }
+*/
     const char* sumoPath = getenv("SUMO_HOME");
     if (sumoPath == nullptr) {
         if (!bindtextdomain("sumo", nullptr)) {
