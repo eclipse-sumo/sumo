09687856316f3373b14171ba8e7669a226057908
namdre <jakob.erdmann@dlr.de>
2023-01-05 10:09:15 +0100

    fix #12386

--- src/microsim/MSVehicle.cpp
+++ src/microsim/MSVehicle.cpp
@@ -1646,7 +1646,7 @@ MSVehicle::processNextStop(double currentVelocity) {
                 }
             }
         } else {
-            if (stop.triggered && !myAmRegisteredAsWaiting) {
+            if (stop.triggered && !myAmRegisteredAsWaiting && stop.duration <= DELTA_T) {
                 if (getVehicleType().getPersonCapacity() == getPersonNumber()) {
                     WRITE_WARNING("Vehicle '" + getID() + "' ignores triggered stop on lane '" + stop.lane->getID() + "' due to capacity constraints.");
                     stop.triggered = false;
@@ -1660,7 +1660,7 @@ MSVehicle::processNextStop(double currentVelocity) {
                 }
 #endif
             }
-            if (stop.containerTriggered && !myAmRegisteredAsWaiting) {
+            if (stop.containerTriggered && !myAmRegisteredAsWaiting && stop.duration <= DELTA_T) {
                 if (getVehicleType().getContainerCapacity() == getContainerNumber()) {
                     WRITE_WARNING("Vehicle '" + getID() + "' ignores container triggered stop on lane '" + stop.lane->getID() + "' due to capacity constraints.");
                     stop.containerTriggered = false;
