b9e161079a601021d30475950cc449bab82ae2fe
Michael Behrisch <oss@behrisch.de>
2023-01-06 11:21:54 +0100

    simplifying test #21

--- tests/complex/gtfs_mitte/runner.py
+++ tests/complex/gtfs_mitte/runner.py
@@ -21,13 +21,12 @@ from __future__ import print_function
 import os
 import sys
 import subprocess
+
 TOOLS = os.path.join(os.environ["SUMO_HOME"], "tools")
-sys.path.append(TOOLS)
-import sumolib  # noqa
 
 subprocess.call([sys.executable,
                  os.path.join(TOOLS, "osmWebWizard.py"), "-b=7:0:0", "-e=8:0:0",
                  "--bbox=13.381507,52.511801,13.417790,52.527748",
-                 "--test-output", "test"])
+                 "--test-output", "test", "-n=--aggregate-warnings=0", "--remote"])
 subprocess.call([sys.executable,
                  os.path.join(TOOLS, "import", "gtfs", "gtfs2pt.py")] + sys.argv[1:])
