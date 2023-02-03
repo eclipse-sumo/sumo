dbdcbee6cf2cde2fc5b787fe813121ef68ebadb0
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-02-03 16:49:03 +0100

    Fixed problem with getTestFrame(). Refs #3045

--- src/utils/gui/windows/GUISUMOAbstractView.cpp
+++ src/utils/gui/windows/GUISUMOAbstractView.cpp
@@ -258,12 +258,14 @@ GUISUMOAbstractView::updatePositionInformationLabel() const {
         myApp->getGeoLabel()->setText(("x:" + toString(pos.x()) + ", y:" + toString(pos.y()) + " (No projection defined)").c_str());
     }
     // if enabled, set test position
-    if (OptionsCont::getOptions().getBool("gui-testing")) {
-        myApp->getTestFrame()->show();
-        // adjust cursor position (24,25) to show exactly the same position as in function netedit.leftClick(match, X, Y)
-        myApp->getTestLabel()->setText(("Test: x:" + toString(getWindowCursorPosition().x() - 24.0) + " y:" + toString(getWindowCursorPosition().y() - 25.0)).c_str());
-    } else {
-        myApp->getTestFrame()->hide();
+    if (myApp->getTestFrame()) {
+        if (OptionsCont::getOptions().getBool("gui-testing")) {
+            myApp->getTestFrame()->show();
+            // adjust cursor position (24,25) to show exactly the same position as in function netedit.leftClick(match, X, Y)
+            myApp->getTestLabel()->setText(("Test: x:" + toString(getWindowCursorPosition().x() - 24.0) + " y:" + toString(getWindowCursorPosition().y() - 25.0)).c_str());
+        } else {
+            myApp->getTestFrame()->hide();
+        }
     }
 }
 
