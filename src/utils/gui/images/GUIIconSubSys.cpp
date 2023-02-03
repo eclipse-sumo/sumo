de33c9371af450afffaafe257cb5c2a89d98de78
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-02-03 16:35:52 +0100

    Renamed cn to zh. Refs #12518

--- src/utils/gui/images/GUIIconSubSys.cpp
+++ src/utils/gui/images/GUIIconSubSys.cpp
@@ -462,6 +462,7 @@
 #include "languages/en.xpm"
 #include "languages/hu.xpm"
 #include "languages/tr.xpm"
+#include "languages/zh.xpm"
 
 // ===========================================================================
 // static member variable definitions
@@ -910,13 +911,13 @@ GUIIconSubSys::GUIIconSubSys(FXApp* a) {
     myIcons[GUIIcon::GOOGLESAT] = new FXXPMIcon(a, googlesat_xpm);
     myIcons[GUIIcon::OSM] = new FXXPMIcon(a, osm_xpm);
 
-    myIcons[GUIIcon::LANGUAGE_ZH] = new FXXPMIcon(a, language_zh_xpm);
+    myIcons[GUIIcon::LANGUAGE_EN] = new FXXPMIcon(a, language_en_xpm);
     myIcons[GUIIcon::LANGUAGE_DE] = new FXXPMIcon(a, language_de_xpm);
     myIcons[GUIIcon::LANGUAGE_ES] = new FXXPMIcon(a, language_es_xpm);
     myIcons[GUIIcon::LANGUAGE_FR] = new FXXPMIcon(a, language_fr_xpm);
-    myIcons[GUIIcon::LANGUAGE_EN] = new FXXPMIcon(a, language_en_xpm);
     myIcons[GUIIcon::LANGUAGE_HU] = new FXXPMIcon(a, language_hu_xpm);
     myIcons[GUIIcon::LANGUAGE_TR] = new FXXPMIcon(a, language_tr_xpm);
+    myIcons[GUIIcon::LANGUAGE_ZH] = new FXXPMIcon(a, language_zh_xpm);
 
     // ... and create them
     for (const auto& icon : myIcons) {
