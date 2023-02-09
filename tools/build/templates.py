dc87d04fd5ef19b2ae2f2eea96b8de839d592f7f
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-02-09 14:57:43 +0100

    Updated templates.py. Now supports tools. Refs #12570

--- tools/build/templates.py
+++ tools/build/templates.py
@@ -27,9 +27,12 @@ from __future__ import absolute_import
 import sys
 import os
 from os.path import dirname, join
-from subprocess import check_output
+from subprocess import check_output, Popen, PIPE
 
-def formatTemplate(templateStr):
+
+def formatBinTemplate(templateStr):
+    print (templateStr)
+
     # remove endlines in Windows
     templateStr = templateStr.replace("\\r", '')
     # replace " with \"
@@ -44,11 +47,21 @@ def formatTemplate(templateStr):
     templateStr = templateStr[:-2]
     templateStr += '";'
     # update last line
-    templateStr = templateStr.replace('\n    ";', ';')
+    templateStr = templateStr.replace('\n    ";', ';\n\n')
     return templateStr
 
-
-# def generateToolTemplate(toolDir):
+def formatToolTemplate(templateStr):
+    # remove first backslash
+    templateStr = templateStr.replace('\\', '')
+    # replace " with \"
+    templateStr = templateStr.replace('"', '\\"')
+    # add quotes and end lines
+    templateStr = templateStr.replace("<", '"<')
+    templateStr = templateStr.replace("\n", '"\n')
+    templateStr = templateStr[:-1]
+    # update last line
+    templateStr += ';\n\n'
+    return templateStr
 
 
 def generateSUMOTemplate(binDir):
@@ -58,25 +71,46 @@ def generateSUMOTemplate(binDir):
             # obtain template piping stdout using check_output
             template = str(check_output([sumoBin, "--save-template", "stdout"]))
             # join variable and formated template
-            return formatTemplate(join("const std::string sumoTemplate = ", template))
+            return formatBinTemplate(join("const std::string sumoTemplate = ", template))
     # if binary wasn't found, then raise exception
     raise Exception("SUMO Template cannot be generated. SUMO binary not found. "
                     "Make sure that sumo or sumoD was generated in bin folder")
+
+
+def generateToolTemplate(srcDir, toolDir, toolName):
+    # get toolPath
+    toolPath = toolDir + "/" + toolName + ".py";
+    
+    print (["python", toolPath, "--save-template stdout"])
+    
+    # check if exists
+    if os.path.exists(toolPath):
+        # obtain template saving it toolTemplate.xml
+        Popen("python " + toolPath + " --save-template toolTemplate.xml")
+        # read XML
+        with open(srcDir + "/netedit/toolTemplate.xml", 'r') as f:
+            template = f.read()
+        # join variable and formated template
+        return formatToolTemplate(join("const std::string " + toolName + "Template = ", template))
+    # if tool wasn't found, then raise exception
+    raise Exception(toolName + "Template cannot be generated. '" + toolPath + "' not found.")
 
 
 if __name__ == "__main__":
     srcDir = join(dirname(__file__), '..', '..', 'src')
     if len(sys.argv) > 1:
         srcDir = sys.argv[1]
-    # get bin dir path (SUMO/tools)
+    # get bin dir path (SUMO/bin)
     binDir = join(dirname(__file__), '..', '..', 'bin')
     # get tool dir path (SUMO/tools)
-    toolDir = join(dirname(__file__), '..', '..', 'tool')
+    toolDir = join(dirname(__file__), '..')
     # generate SUMO template
     sumoTemplate = generateSUMOTemplate(binDir)
+    # generate Tool template
+    toolTemplate = generateToolTemplate(srcDir, toolDir, "generateRerouters")
     # write templates.h
     with open("templates.h", 'w') as templateHeaderFile:
         # Convert all of the items in lst to strings (to avoid quotes)
-        templateMap = map(str, ["#include <string>\n\n", sumoTemplate])  
+        templateMap = map(str, ["#include <string>\n\n", sumoTemplate, toolTemplate])  
         # Join the items together and write to the file
         templateHeaderFile.write("".join(templateMap))
