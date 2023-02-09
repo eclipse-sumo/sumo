337ab9bd9c158215e34d2194e257fc8ecdd2d8fb
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-02-09 13:20:18 +0100

    Updated templates.py. Refs #12570

--- tools/build/templates.py
+++ tools/build/templates.py
@@ -23,39 +23,39 @@ It does this by parsing the data from the sumo data dir.
 
 from __future__ import print_function
 from __future__ import absolute_import
-import subprocess
+
 import sys
 import os
 from os.path import dirname, join
+from subprocess import check_output
+
+def formatTemplate(templateStr):
+    # remove endlines in Windows und linux
+    templateStr = templateStr.replace("\\r", " ")
+    templateStr = templateStr.replace("\\n", " ")
+    # avoid double spaces
+    templateStr = templateStr.replace("  ", " ")
+    # replace " with \"
+    templateStr = templateStr.replace('"', '\\"')
+    # replace first backspace
+    templateStr = templateStr.replace("\\b'", '"')
+    # remove two last characters
+    templateStr = templateStr[:-2]
+    templateStr += '";'
+    return templateStr
 
 
-def generateTemplateString(templateHeaderFile, templateDir, templateFile):
-    # get XMLtemplate file
-    XMLTemplateFile = join(templateDir, templateFile)
-    # read XML
-    with open(XMLTemplateFile, 'r') as f:
-        template = f.readlines()
-    # write template
-    templateHeaderFile.write("std::string " + templateFile.replace(".xml", "") + "Template = ")
-    for line in template:
-        # replace characters
-        lineStrip = line.strip("\n")
-        lineStrip = lineStrip.replace('"', '\\"')
-        # write line
-        templateHeaderFile.write('\"' + lineStrip + '\"' + ' ' + '\\' + '\n')
-    # write last
-    templateHeaderFile.write("\"\";\n\n")
+# def generateToolTemplate(toolDir):
 
 
-# def generateToolTemplate(toolDir, toolPath):
-
-
-def generateSUMOTemplate(binDir, templateDir):
+def generateSUMOTemplate(binDir):
     # create a list with all sumo binaries
     for sumoBin in [binDir + "/sumo", binDir + "/sumo.exe", binDir + "/sumoD", binDir + "/sumoD.exe"]:
         if os.path.exists(sumoBin):
-            subprocess.call([sumoBin, "--save-template", templateDir + "/sumo.xml"])
-            return
+            # obtain template piping stdout using check_output
+            template = str(check_output([sumoBin, "--save-template", "stdout"]))
+            # join variable and formated template
+            return formatTemplate(join("const std::string sumoTemplate = ", template))
     # if binary wasn't found, then raise exception
     raise Exception("SUMO Template cannot be generated. SUMO binary not found. "
                     "Make sure that sumo or sumoD was generated in bin folder")
@@ -65,20 +65,15 @@ if __name__ == "__main__":
     srcDir = join(dirname(__file__), '..', '..', 'src')
     if len(sys.argv) > 1:
         srcDir = sys.argv[1]
-    # get template dir path (SUMO/data/templates)
-    templateDir = join(dirname(__file__), '..', '..', 'data', 'templates')
     # get bin dir path (SUMO/tools)
     binDir = join(dirname(__file__), '..', '..', 'bin')
     # get tool dir path (SUMO/tools)
     toolDir = join(dirname(__file__), '..', '..', 'tool')
     # generate SUMO template
-    generateSUMOTemplate(binDir, templateDir)
+    sumoTemplate = generateSUMOTemplate(binDir)
     # write templates.h
     with open("templates.h", 'w') as templateHeaderFile:
-        # write header
-        templateHeaderFile.write("#include <string>\n\n")
-        # generate templateString for all files placed in data/templates
-        for templateFile in os.listdir(templateDir):
-            # avoid .gitignore file
-            if ".xml" in templateFile:
-                generateTemplateString(templateHeaderFile, templateDir, templateFile)
+        # Convert all of the items in lst to strings (to avoid quotes)
+        templateMap = map(str, ["#include <string>\n\n", sumoTemplate])  
+        # Join the items together and write to the file
+        templateHeaderFile.write("".join(templateMap))
