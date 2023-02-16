bd868fe31d32d5ed46ada4b1a71f90a5f1d49920
namdre <jakob.erdmann@dlr.de>
2023-02-16 10:55:09 +0100

    added test refs #12631

---
+++ tests/complex/traci/vehicle/tripinfoRetrieval/runner.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Mirko Barthauer (Technische Universitaet Braunschweig)
+# @date    2018-09-27
+
+from __future__ import print_function
+import os
+import sys
+
+if "SUMO_HOME" in os.environ:
+    sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+
+import traci  # noqa
+import sumolib  # noqa
+import traci.constants as tc  # noqa
+
+sumoBinary = sumolib.checkBinary('sumo')
+traci.start([sumoBinary,
+             "-n", "input_net4.net.xml",
+             "-r", "input_routes.rou.xml",
+             "-t",
+             "--no-step-log",
+             ] + sys.argv[1:])
+
+def printStats(t):
+    print(t)
+    vehs = traci.vehicle.getIDList()
+    if vehs:
+        ego = vehs[0]
+        print(" veh=%s" % ego)
+        for key in ['waitingTime', 'waitingCount', 'stopTime']:
+            print("    ", key, traci.vehicle.getParameter(ego, "device.tripinfo." + key))
+
+while traci.simulation.getMinExpectedNumber() > 0:
+    t = traci.simulation.getTime()
+    if t % 10 == 0:
+        printStats(t)
+    traci.simulationStep()
+
+
+traci.close()
