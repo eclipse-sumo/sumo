4da56d0ea6d368f0127fb6f0f4f73689d1675c61
Michael Behrisch <michael.behrisch@dlr.de>
2023-03-03 17:46:46 +0100

    testing executeMove #21 #11091

---
+++ tests/complex/traci/misc/execute_move/runner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Jakob Erdmann
+# @date    2015-02-06
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+
+if "SUMO_HOME" in os.environ:
+    sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+import traci  # noqa
+import sumolib  # noqa
+
+
+def step():
+    s = traci.simulation.getTime()
+    traci.simulationStep()
+    return s
+
+
+traci.start([sumolib.checkBinary('sumo'), "-c", "sumo.sumocfg"] + sys.argv[1:])
+print("step", step())
+
+print(traci.vehicle.getSpeed("veh0"))
+traci.executeMove()
+print(traci.vehicle.getSpeed("veh0"))
+print("step", step())
+print(traci.vehicle.getSpeed("veh0"))
+traci.executeMove()
+print(traci.vehicle.getSpeed("veh0"))
+print("step", step())
+print(traci.vehicle.getSpeed("veh0"))
+
+traci.close()
