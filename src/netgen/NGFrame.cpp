eclipse-sumo/sumo

commit 7fb5254664bd97d453e351dfdd3bb7c42c8e7288

Reverted translation in Frames. Refs #12729

src/netgen/NGFrame.cpp
@@ -38,179 +38,179 @@ void
 NGFrame::fillOptions() {
     OptionsCont& oc = OptionsCont::getOptions();
     oc.doRegister("type-files", 't', new Option_FileName());
-    oc.addDescription("type-files", TL("Input"), TL("Read edge-type defs from FILE"));
+    oc.addDescription("type-files", "Input", TL("Read edge-type defs from FILE"));
 
     oc.doRegister("alphanumerical-ids", new Option_Bool(true));
-    oc.addDescription("alphanumerical-ids", TL("Output"), TL("The Ids of generated nodes use an alphanumerical code for easier readability when possible"));
+    oc.addDescription("alphanumerical-ids", "Output", TL("The Ids of generated nodes use an alphanumerical code for easier readability when possible"));
 
     // register processing options
     oc.doRegister("turn-lanes", new Option_Integer(0));
-    oc.addDescription("turn-lanes", TL("Processing"), TL("Generate INT left-turn lanes"));
+    oc.addDescription("turn-lanes", "Processing", TL("Generate INT left-turn lanes"));
     oc.doRegister("turn-lanes.length", new Option_Float(20));
-    oc.addDescription("turn-lanes.length", TL("Processing"), TL("Set the length of generated turning lanes to FLOAT"));
+    oc.addDescription("turn-lanes.length", "Processing", TL("Set the length of generated turning lanes to FLOAT"));
 
     oc.doRegister("perturb-x", new Option_String("0"));
-    oc.addDescription("perturb-x", TL("Processing"), TL("Apply random spatial perturbation in x direction according the the given distribution"));
+    oc.addDescription("perturb-x", "Processing", TL("Apply random spatial perturbation in x direction according the the given distribution"));
     oc.doRegister("perturb-y", new Option_String("0"));
-    oc.addDescription("perturb-y", TL("Processing"), TL("Apply random spatial perturbation in y direction according the the given distribution"));
+    oc.addDescription("perturb-y", "Processing", TL("Apply random spatial perturbation in y direction according the the given distribution"));
     oc.doRegister("perturb-z", new Option_String("0"));
-    oc.addDescription("perturb-z", TL("Processing"), TL("Apply random spatial perturbation in z direction according the the given distribution"));
+    oc.addDescription("perturb-z", "Processing", TL("Apply random spatial perturbation in z direction according the the given distribution"));
 
 
     //  register grid-net options
     oc.doRegister("grid", 'g', new Option_Bool(false));
     oc.addSynonyme("grid", "grid-net", true);
-    oc.addDescription("grid", TL("Grid Network"), TL("Forces NETGEN to build a grid-like network"));
+    oc.addDescription("grid", "Grid Network", TL("Forces NETGEN to build a grid-like network"));
 
     oc.doRegister("grid.number", new Option_Integer(5));
     oc.addSynonyme("grid.number", "grid-number", true);
     oc.addSynonyme("grid.number", "number");
-    oc.addDescription("grid.number", TL("Grid Network"), TL("The number of junctions in both dirs"));
+    oc.addDescription("grid.number", "Grid Network", TL("The number of junctions in both dirs"));
 
     oc.doRegister("grid.length", new Option_Float(100));
     oc.addSynonyme("grid.length", "grid-length", true);
     oc.addSynonyme("grid.length", "length");
-    oc.addDescription("grid.length", TL("Grid Network"), TL("The length of streets in both dirs"));
+    oc.addDescription("grid.length", "Grid Network", TL("The length of streets in both dirs"));
 
     oc.doRegister("grid.x-number", new Option_Integer(5));
     oc.addSynonyme("grid.x-number", "grid-x-number", true);
     oc.addSynonyme("grid.x-number", "x-no");
-    oc.addDescription("grid.x-number", TL("Grid Network"), TL("The number of junctions in x-dir; Overrides --grid-number"));
+    oc.addDescription("grid.x-number", "Grid Network", TL("The number of junctions in x-dir; Overrides --grid-number"));
 
     oc.doRegister("grid.y-number", new Option_Integer(5));
     oc.addSynonyme("grid.y-number", "grid-y-number", true);
     oc.addSynonyme("grid.y-number", "y-no");
-    oc.addDescription("grid.y-number", TL("Grid Network"), TL("The number of junctions in y-dir; Overrides --grid-number"));
+    oc.addDescription("grid.y-number", "Grid Network", TL("The number of junctions in y-dir; Overrides --grid-number"));
 
     oc.doRegister("grid.x-length", new Option_Float(100));
     oc.addSynonyme("grid.x-length", "grid-x-length", true);
     oc.addSynonyme("grid.x-length", "x-length");
-    oc.addDescription("grid.x-length", TL("Grid Network"), TL("The length of horizontal streets; Overrides --grid-length"));
+    oc.addDescription("grid.x-length", "Grid Network", TL("The length of horizontal streets; Overrides --grid-length"));
 
     oc.doRegister("grid.y-length", new Option_Float(100));
     oc.addSynonyme("grid.y-length", "grid-y-length", true);
     oc.addSynonyme("grid.y-length", "y-length");
-    oc.addDescription("grid.y-length", TL("Grid Network"), TL("The length of vertical streets; Overrides --grid-length"));
+    oc.addDescription("grid.y-length", "Grid Network", TL("The length of vertical streets; Overrides --grid-length"));
 
     oc.doRegister("grid.attach-length", new Option_Float(0));
     oc.addSynonyme("grid.attach-length", "attach-length", true);
-    oc.addDescription("grid.attach-length", TL("Grid Network"), TL("The length of streets attached at the boundary; 0 means no streets are attached"));
+    oc.addDescription("grid.attach-length", "Grid Network", TL("The length of streets attached at the boundary; 0 means no streets are attached"));
 
     oc.doRegister("grid.x-attach-length", new Option_Float(0));
-    oc.addDescription("grid.x-attach-length", TL("Grid Network"), TL("The length of streets attached at the boundary in x direction; 0 means no streets are attached"));
+    oc.addDescription("grid.x-attach-length", "Grid Network", TL("The length of streets attached at the boundary in x direction; 0 means no streets are attached"));
     oc.doRegister("grid.y-attach-length", new Option_Float(0));
-    oc.addDescription("grid.y-attach-length", TL("Grid Network"), TL("The length of streets attached at the boundary in y direction; 0 means no streets are attached"));
+    oc.addDescription("grid.y-attach-length", "Grid Network", TL("The length of streets attached at the boundary in y direction; 0 means no streets are attached"));
 
     //  register spider-net options
     oc.doRegister("spider", 's', new Option_Bool(false));
     oc.addSynonyme("spider", "spider-net", true);
-    oc.addDescription("spider", TL("Spider Network"), TL("Forces NETGEN to build a spider-net-like network"));
+    oc.addDescription("spider", "Grid Network", TL("Forces NETGEN to build a spider-net-like network"));
 
     oc.doRegister("spider.arm-number", new Option_Integer(13));
     oc.addSynonyme("spider.arm-number", "spider-arm-number", true);
     oc.addSynonyme("spider.arm-number", "arms");
-    oc.addDescription("spider.arm-number", TL("Spider Network"), TL("The number of axes within the net"));
+    oc.addDescription("spider.arm-number", "Grid Network", TL("The number of axes within the net"));
 
     oc.doRegister("spider.circle-number", new Option_Integer(20));
     oc.addSynonyme("spider.circle-number", "spider-circle-number", true);
     oc.addSynonyme("spider.circle-number", "circles");
-    oc.addDescription("spider.circle-number", TL("Spider Network"), TL("The number of circles of the net"));
+    oc.addDescription("spider.circle-number", "Grid Network", TL("The number of circles of the net"));
 
     oc.doRegister("spider.space-radius", new Option_Float(100));
     oc.addSynonyme("spider.space-radius", "spider-space-rad", true);
     oc.addSynonyme("spider.space-radius", "radius");
-    oc.addDescription("spider.space-radius", TL("Spider Network"), TL("The distances between the circles"));
+    oc.addDescription("spider.space-radius", "Grid Network", TL("The distances between the circles"));
 
     oc.doRegister("spider.omit-center", new Option_Bool(false));
     oc.addSynonyme("spider.omit-center", "spider-omit-center", true);
     oc.addSynonyme("spider.omit-center", "nocenter");
-    oc.addDescription("spider.omit-center", TL("Spider Network"), TL("Omit the central node of the network"));
+    oc.addDescription("spider.omit-center", "Grid Network", TL("Omit the central node of the network"));
 
 
     //  register random-net options
     oc.doRegister("rand", 'r', new Option_Bool(false));
     oc.addSynonyme("rand", "random-net", true);
-    oc.addDescription("rand", TL("Random Network"), TL("Forces NETGEN to build a random network"));
+    oc.addDescription("rand", "Random Network", TL("Forces NETGEN to build a random network"));
 
     oc.doRegister("rand.iterations", new Option_Integer(2000));
     oc.addSynonyme("rand.iterations", "rand-iterations", true);
     oc.addSynonyme("rand.iterations", "iterations");
-    oc.addDescription("rand.iterations", TL("Random Network"), TL("Describes how many times an edge shall be added to the net"));
+    oc.addDescription("rand.iterations", "Random Network", TL("Describes how many times an edge shall be added to the net"));
 
     oc.doRegister("bidi-probability", new Option_Float(1));
     oc.addSynonyme("bidi-probability", "rand-bidi-probability", true);
     oc.addSynonyme("bidi-probability", "bidi");
     oc.addSynonyme("bidi-probability", "rand.bidi-probability");
-    oc.addDescription("bidi-probability", TL("Random Network"), TL("Defines the probability to build a reverse edge"));
+    oc.addDescription("bidi-probability", "Random Network", TL("Defines the probability to build a reverse edge"));
 
     oc.doRegister("rand.max-distance", new Option_Float(250));
     oc.addSynonyme("rand.max-distance", "rand-max-distance", true);
     oc.addSynonyme("rand.max-distance", "max-dist");
-    oc.addDescription("rand.max-distance", TL("Random Network"), TL("The maximum distance for each edge"));
+    oc.addDescription("rand.max-distance", "Random Network", TL("The maximum distance for each edge"));
 
     oc.doRegister("rand.min-distance", new Option_Float(100));
     oc.addSynonyme("rand.min-distance", "rand-min-distance", true);
     oc.addSynonyme("rand.min-distance", "min-dist");
-    oc.addDescription("rand.min-distance", TL("Random Network"), TL("The minimum distance for each edge"));
+    oc.addDescription("rand.min-distance", "Random Network", TL("The minimum distance for each edge"));
 
     oc.doRegister("rand.min-angle", new Option_Float(45.0));
     oc.addSynonyme("rand.min-angle", "rand-min-anglee", true);
     oc.addSynonyme("rand.min-angle", "min-angle");
-    oc.addDescription("rand.min-angle", TL("Random Network"), TL("The minimum angle for each pair of (bidirectional) roads in DEGREES"));
+    oc.addDescription("rand.min-angle", "Random Network", TL("The minimum angle for each pair of (bidirectional) roads in DEGREES"));
 
     oc.doRegister("rand.num-tries", new Option_Integer(50));
     oc.addSynonyme("rand.num-tries", "rand-num-tries", true);
     oc.addSynonyme("rand.num-tries", "num-tries");
-    oc.addDescription("rand.num-tries", TL("Random Network"), TL("The number of tries for creating each node"));
+    oc.addDescription("rand.num-tries", "Random Network", TL("The number of tries for creating each node"));
 
     oc.doRegister("rand.connectivity", new Option_Float((double) 0.95));
     oc.addSynonyme("rand.connectivity", "rand-connectivity", true);
     oc.addSynonyme("rand.connectivity", "connectivity");
-    oc.addDescription("rand.connectivity", TL("Random Network"), TL("Probability for roads to continue at each node"));
+    oc.addDescription("rand.connectivity", "Random Network", TL("Probability for roads to continue at each node"));
 
     oc.doRegister("rand.neighbor-dist1", new Option_Float(0));
     oc.addSynonyme("rand.neighbor-dist1", "rand-neighbor-dist1", true);
     oc.addSynonyme("rand.neighbor-dist1", "dist1");
-    oc.addDescription("rand.neighbor-dist1", TL("Random Network"), TL("Probability for a node having exactly 1 neighbor"));
+    oc.addDescription("rand.neighbor-dist1", "Random Network", TL("Probability for a node having exactly 1 neighbor"));
 
     oc.doRegister("rand.neighbor-dist2", new Option_Float(0));
     oc.addSynonyme("rand.neighbor-dist2", "rand-neighbor-dist2", true);
     oc.addSynonyme("rand.neighbor-dist2", "dist2");
-    oc.addDescription("rand.neighbor-dist2", TL("Random Network"), TL("Probability for a node having exactly 2 neighbors"));
+    oc.addDescription("rand.neighbor-dist2", "Random Network", TL("Probability for a node having exactly 2 neighbors"));
 
     oc.doRegister("rand.neighbor-dist3", new Option_Float(10));
     oc.addSynonyme("rand.neighbor-dist3", "rand-neighbor-dist3", true);
     oc.addSynonyme("rand.neighbor-dist3", "dist3");
-    oc.addDescription("rand.neighbor-dist3", TL("Random Network"), TL("Probability for a node having exactly 3 neighbors"));
+    oc.addDescription("rand.neighbor-dist3", "Random Network", TL("Probability for a node having exactly 3 neighbors"));
 
     oc.doRegister("rand.neighbor-dist4", new Option_Float(10));
     oc.addSynonyme("rand.neighbor-dist4", "rand-neighbor-dist4", true);
     oc.addSynonyme("rand.neighbor-dist4", "dist4");
-    oc.addDescription("rand.neighbor-dist4", TL("Random Network"), TL("Probability for a node having exactly 4 neighbors"));
+    oc.addDescription("rand.neighbor-dist4", "Random Network", TL("Probability for a node having exactly 4 neighbors"));
 
     oc.doRegister("rand.neighbor-dist5", new Option_Float(2));
     oc.addSynonyme("rand.neighbor-dist5", "rand-neighbor-dist5", true);
     oc.addSynonyme("rand.neighbor-dist5", "dist5");
-    oc.addDescription("rand.neighbor-dist5", TL("Random Network"), TL("Probability for a node having exactly 5 neighbors"));
+    oc.addDescription("rand.neighbor-dist5", "Random Network", TL("Probability for a node having exactly 5 neighbors"));
 
     oc.doRegister("rand.neighbor-dist6", new Option_Float(1));
     oc.addSynonyme("rand.neighbor-dist6", "rand-neighbor-dist6", true);
     oc.addSynonyme("rand.neighbor-dist6", "dist6");
-    oc.addDescription("rand.neighbor-dist6", TL("Random Network"), TL("Probability for a node having exactly 6 neighbors"));
+    oc.addDescription("rand.neighbor-dist6", "Random Network", TL("Probability for a node having exactly 6 neighbors"));
 
     oc.doRegister("random-lanenumber", new Option_Bool(false));
     oc.addSynonyme("random-lanenumber", "rand.random-lanenumber", false);
-    oc.addDescription("random-lanenumber", TL("Random Network"), TL("Draw lane numbers randomly from [1,default.lanenumber]"));
+    oc.addDescription("random-lanenumber", "Random Network", TL("Draw lane numbers randomly from [1,default.lanenumber]"));
 
     oc.doRegister("random-priority", new Option_Bool(false));
     oc.addSynonyme("random-priority", "rand.random-priority", false);
-    oc.addDescription("random-priority", TL("Random Network"), TL("Draw edge priority randomly from [1,default.priority]"));
+    oc.addDescription("random-priority", "Random Network", TL("Draw edge priority randomly from [1,default.priority]"));
 
     oc.doRegister("random-type", new Option_Bool(false));
-    oc.addDescription("random-type", TL("Random Network"), TL("Draw edge type randomly from all loaded types"));
+    oc.addDescription("random-type", "Random Network", TL("Draw edge type randomly from all loaded types"));
 
     oc.doRegister("rand.grid", new Option_Bool(false));
-    oc.addDescription("rand.grid", TL("Random Network"), TL("Place nodes on a regular grid with spacing rand.min-distance"));
+    oc.addDescription("rand.grid", "Random Network", TL("Place nodes on a regular grid with spacing rand.min-distance"));
 }
 
 

src/activitygen/AGFrame.cpp
@@ -58,10 +58,10 @@ void AGFrame::fillOptions() {
     // Insert options
     oc.doRegister("net-file", 'n', new Option_FileName());
     oc.addSynonyme("net-file", "net");
-    oc.addDescription("net-file", TL("Input"), TL("Use FILE as SUMO-network to create trips for"));
+    oc.addDescription("net-file", "Input", TL("Use FILE as SUMO-network to create trips for"));
 
     oc.doRegister("stat-file", 's', new Option_FileName());
-    oc.addDescription("stat-file", TL("Input"), TL("Loads the SUMO-statistics FILE"));
+    oc.addDescription("stat-file", "Input", TL("Loads the SUMO-statistics FILE"));
 
     // need to do this here to be able to check for network and route input options
     SystemFrame::addReportOptions(oc);
@@ -69,7 +69,7 @@ void AGFrame::fillOptions() {
 
     oc.doRegister("output-file", 'o', new Option_FileName());
     oc.addSynonyme("output-file", "output", true);
-    oc.addDescription("output-file", TL("Output"), TL("Write generated trips to FILE"));
+    oc.addDescription("output-file", "Output", TL("Write generated trips to FILE"));
 
     oc.doRegister("debug", new Option_Bool(false));
     oc.addDescription("debug", "Report",
@@ -78,13 +78,13 @@ void AGFrame::fillOptions() {
     // TODO: What time options are consistent with other parts of SUMO and
     // useful for the user?
     oc.doRegister("begin", 'b', new Option_Integer(0));
-    oc.addDescription("begin", TL("Time"), TL("Sets the time of beginning of the simulation during the first day (in seconds)"));
+    oc.addDescription("begin", "Time", TL("Sets the time of beginning of the simulation during the first day (in seconds)"));
 
     oc.doRegister("end", 'e', new Option_Integer(0));
-    oc.addDescription("end", TL("Time"), TL("Sets the time of ending of the simulation during the last day (in seconds)"));
+    oc.addDescription("end", "Time", TL("Sets the time of ending of the simulation during the last day (in seconds)"));
 
     oc.doRegister("duration-d", new Option_Integer(1));
-    oc.addDescription("duration-d", TL("Time"), TL("Sets the duration of the simulation in days"));
+    oc.addDescription("duration-d", "Time", TL("Sets the duration of the simulation in days"));
 }
 
 

commit 5fee01c27fec80321c62c752d3261fdb60e6ae07

reverting option catogories broken in 7fb5254

src/netgen/NGFrame.cpp
@@ -104,27 +104,27 @@ NGFrame::fillOptions() {
     //  register spider-net options
     oc.doRegister("spider", 's', new Option_Bool(false));
     oc.addSynonyme("spider", "spider-net", true);
-    oc.addDescription("spider", "Grid Network", TL("Forces NETGEN to build a spider-net-like network"));
+    oc.addDescription("spider", "Spider Network", TL("Forces NETGEN to build a spider-net-like network"));
 
     oc.doRegister("spider.arm-number", new Option_Integer(13));
     oc.addSynonyme("spider.arm-number", "spider-arm-number", true);
     oc.addSynonyme("spider.arm-number", "arms");
-    oc.addDescription("spider.arm-number", "Grid Network", TL("The number of axes within the net"));
+    oc.addDescription("spider.arm-number", "Spider Network", TL("The number of axes within the net"));
 
     oc.doRegister("spider.circle-number", new Option_Integer(20));
     oc.addSynonyme("spider.circle-number", "spider-circle-number", true);
     oc.addSynonyme("spider.circle-number", "circles");
-    oc.addDescription("spider.circle-number", "Grid Network", TL("The number of circles of the net"));
+    oc.addDescription("spider.circle-number", "Spider Network", TL("The number of circles of the net"));
 
     oc.doRegister("spider.space-radius", new Option_Float(100));
     oc.addSynonyme("spider.space-radius", "spider-space-rad", true);
     oc.addSynonyme("spider.space-radius", "radius");
-    oc.addDescription("spider.space-radius", "Grid Network", TL("The distances between the circles"));
+    oc.addDescription("spider.space-radius", "Spider Network", TL("The distances between the circles"));
 
     oc.doRegister("spider.omit-center", new Option_Bool(false));
     oc.addSynonyme("spider.omit-center", "spider-omit-center", true);
     oc.addSynonyme("spider.omit-center", "nocenter");
-    oc.addDescription("spider.omit-center", "Grid Network", TL("Omit the central node of the network"));
+    oc.addDescription("spider.omit-center", "Spider Network", TL("Omit the central node of the network"));
 
 
     //  register random-net options