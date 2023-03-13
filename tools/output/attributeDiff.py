27e360015ef6219d7c5d55fc9163139e0999bc8d
namdre <jakob.erdmann@dlr.de>
2023-03-13 17:15:40 +0100

    fix #12794

--- tools/output/attributeDiff.py
+++ tools/output/attributeDiff.py
@@ -38,17 +38,21 @@ def get_options():
     optParser = sumolib.options.ArgumentParser()
     optParser.add_option("old", help="the first xml file")
     optParser.add_option("new", help="the second xml file")
-    optParser.add_option("-v", "--verbose", action="store_true",
-                         default=False, help="Give more output")
+    optParser.add_option("-v", "--verbose", action="store_true", default=False,
+                         help="Give more output")
     optParser.add_option("-e", "--element", help="element to analyze")
     optParser.add_option("-a", "--attribute", help="attribute to analyze")
+    optParser.add_option("-i", "--id-attribute", dest="idAttribute",
+                         help="extra attribute to distinguish values")
     optParser.add_option("-o", "--xml-output", help="output statistic to xml file")
-    optParser.add_option("-p", "--precision", type=int,
-                         default=2, help="Set output precision")
+    optParser.add_option("-p", "--precision", type=int, default=2,
+                         help="Set output precision")
     options = optParser.parse_args()
 
     if options.attribute:
         options.attribute = options.attribute.split(',')
+    if options.idAttribute:
+        options.idAttribute = options.idAttribute.split(',')
     if options.element:
         options.element = options.element.split(',')
 
@@ -61,27 +65,43 @@ def main():
     oldValues = defaultdict(dict)  # elem->attr->difference
     differences = defaultdict(dict)  # elem->attr->difference
     missingAttr = defaultdict(set)
+    missingAttr2 = defaultdict(set)
     invalidType = defaultdict(set)
 
     def elements(fname):
         stack = []
+        idStack = []
         for event, node in ET.iterparse(sumolib.xml._open(fname, None), events=('start', 'end')):
             if options.element is not None and node.tag not in options.element:
                 continue
             if event == 'start':
                 stack.append(node.tag)
+                if options.idAttribute:
+                    idStack.append([])
+                    for attr in options.idAttribute:
+                        if node.get(attr) is not None:
+                            idStack[-1].append(node.get(attr))
+
             else:
                 stack.pop()
+                if options.idAttribute:
+                    idStack.pop()
                 continue
 
             tags = tuple(stack[1:]) if options.element is None else tuple(stack)  # exclude root
+            elementDescription = '.'.join(tags)
+            if options.idAttribute:
+                for ids in idStack:
+                    if ids:
+                        elementDescription += '.' + '.'.join(ids)
+
             if options.attribute is None:
                 for k, v in node.items():
-                    yield '.'.join(tags), k, v
+                    yield elementDescription, k, v
             else:
                 for attr in options.attribute:
                     if node.get(attr) is not None or options.element is not None:
-                        yield '.'.join(tags), attr, node.get(attr)
+                        yield elementDescription, attr, node.get(attr)
 
     # parse old
     for tag, attr, stringVal in elements(options.old):
@@ -108,11 +128,11 @@ def main():
                 if tag in oldValues and attr in oldValues[tag]:
                     differences[tag][attr] = val - oldValues[tag][attr]
                 else:
-                    missingAttr[attr].add(tag)
+                    missingAttr2[attr].add(tag)
             except Exception:
                 invalidType[attr].add(stringVal)
         else:
-            missingAttr[attr].add(tag)
+            missingAttr2[attr].add(tag)
 
     if options.verbose or options.xml_output is None:
         for tag in sorted(differences.keys()):
@@ -120,7 +140,13 @@ def main():
 
     if missingAttr:
         for attr in sorted(missingAttr.keys()):
-            print("Elements %s did not provide attribute '%s'" % (','.join(sorted(missingAttr[attr])), attr))
+            print("In file %s, Elements %s did not provide attribute '%s'" % (
+                options.old, ','.join(sorted(missingAttr[attr])), attr))
+
+    if missingAttr2:
+        for attr in sorted(missingAttr2.keys()):
+            print("In file %s, Elements %s did not provide attribute '%s'" % (
+                options.new, ','.join(sorted(missingAttr2[attr])), attr))
 
     if invalidType and options.attribute is not None:
         for attr in sorted(invalidType.keys()):
