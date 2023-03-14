d5e7c93f91fe72fad9ad93cb0e754c07eae97615
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-03-14 16:07:26 +0100

    Now netedit can use libpython. Refs #4138

--- src/netedit/dialogs/tools/GNERunToolDialog.cpp
+++ src/netedit/dialogs/tools/GNERunToolDialog.cpp
@@ -23,6 +23,7 @@
 #include <utils/foxtools/MFXLabelTooltip.h>
 #include <utils/gui/div/GUIDesigns.h>
 #include <utils/handlers/TemplateHandler.h>
+#include <Python.h>
 
 #include "GNERunToolDialog.h"
 #include "GNETool.h"
