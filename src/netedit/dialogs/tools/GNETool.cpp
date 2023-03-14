feb67919aa62782729c7b933518eeda8e93e2a53
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-03-14 12:47:31 +0100

    Added GNETool.h/cpp. Refs #4138

---
+++ src/netedit/dialogs/tools/GNETool.cpp
@@ -0,0 +1,68 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2023 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNETool.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Jun 2022
+///
+// Python tools used in netedit
+/****************************************************************************/
+
+#include <netedit/GNEApplicationWindow.h>
+#include <utils/common/FileHelpers.h>
+#include <utils/foxtools/MFXLabelTooltip.h>
+#include <utils/gui/div/GUIDesigns.h>
+#include <utils/handlers/TemplateHandler.h>
+#include <utils/common/MsgHandler.h>
+
+#include "GNETool.h"
+
+// ============================================-===============================
+// member method definitions
+// ===========================================================================
+
+GNETool::GNETool(GNEApplicationWindow* GNEApp, const std::string &pythonPath, 
+        const std::string &templateStr, FXMenuPane* menu) :
+    myGNEApp(GNEApp),
+    myPythonPath(pythonPath),
+    myToolName(FileHelpers::getFileFromPath(pythonPath, true)) {
+    // build menu command
+    myMenuCommand = GUIDesigns::buildFXMenuCommandShortcut(menu, myToolName, "", TL("Execute python tool '") + myToolName + "'.",
+        GUIIconSubSys::getIcon(GUIIcon::TOOL_PYTHON), GNEApp, MID_GNE_OPENTOOLDIALOG);
+    // parse tool options
+    if (templateStr.size() > 0) {
+        try {
+            TemplateHandler::parseTemplate(myToolsOptions, templateStr);
+        } catch (ProcessError&) {
+            WRITE_ERROR("Error parsing template of tool: " + myToolName);
+        }
+    }
+
+}
+
+
+GNETool::~GNETool() {}
+
+
+GNEApplicationWindow*
+GNETool::getGNEApp() const {
+    return myGNEApp;
+}
+
+
+FXMenuCommand*
+GNETool::getMenuCommand() const {
+    return myMenuCommand;
+}
+
+/****************************************************************************/
---
+++ src/netedit/dialogs/tools/GNETool.h
@@ -0,0 +1,74 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+// Copyright (C) 2001-2023 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNETool.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Jun 2022
+///
+// Python tools used in netedit
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <utils/options/OptionsCont.h>
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+
+class GNEApplicationWindow;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNETool {
+
+
+public:
+    /// @brief Constructor
+    GNETool(GNEApplicationWindow* GNEApp, const std::string &pythonPath, 
+                  const std::string &templateStr, FXMenuPane* menu);
+
+    /// @brief destructor
+    ~GNETool();
+
+    /// @brief get to GNEApplicationWindow
+    GNEApplicationWindow* getGNEApp() const;
+
+    /// @brief get menu command
+    FXMenuCommand *getMenuCommand() const;
+
+private:
+    /// @brief pointer to GNEApplicationWindow
+    GNEApplicationWindow* myGNEApp;
+
+    /// @brief menu command associated with this tool
+    FXMenuCommand* myMenuCommand;
+
+    /// @brief tools options
+    OptionsCont myToolsOptions;
+
+    /// @brief python path
+    const std::string myPythonPath;
+
+    /// @brief tool name
+    const std::string myToolName;
+
+    /// @brief Invalidated copy constructor.
+    GNETool(const GNETool&) = delete;
+
+    /// @brief Invalidated assignment operator.
+    GNETool& operator=(const GNETool&) = delete;
+};
+
