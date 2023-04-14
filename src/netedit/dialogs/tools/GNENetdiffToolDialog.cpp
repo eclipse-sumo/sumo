d0e96ff67ada5a7d69f225d9fa0bdc1e9215143a
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-04-14 09:32:42 +0200

    Added GNENetdiffToolDialog. Refs #2736

--- src/netedit/dialogs/tools/GNENetdiffToolDialog.cpp
+++ src/netedit/dialogs/tools/GNENetdiffToolDialog.cpp
@@ -32,30 +32,32 @@
 GNENetdiffToolDialog::GNENetdiffToolDialog(GNEApplicationWindow* GNEApp) :
     GNEPythonToolDialog(GNEApp) {
     // build custom options for netdiff
-    myCustomToolsOptions.addOptionSubTopic("Configuration");
+    myCustomToolsOptions.addOptionSubTopic("Input");
     myCustomToolsOptions.doRegister("original-net", new Option_Network());
-    myCustomToolsOptions.addDescription("original-net", "Configuration", TL("Original network"));
+    myCustomToolsOptions.addDescription("original-net", "Input", TL("Original network"));
 
     myCustomToolsOptions.doRegister("modified-net", new Option_Network());
-    myCustomToolsOptions.addDescription("modified-net", "Configuration", TL("Modified network"));
+    myCustomToolsOptions.addDescription("modified-net", "Input", TL("Modified network"));
 
+    myCustomToolsOptions.addOptionSubTopic("Select");
     myCustomToolsOptions.doRegister("select-modified", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("select-modified", "Configuration", TL("Select modified elements"));
+    myCustomToolsOptions.addDescription("select-modified", "Select", TL("Select modified elements"));
     
     myCustomToolsOptions.doRegister("select-added", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("select-added", "Configuration", TL("Select added elements"));
+    myCustomToolsOptions.addDescription("select-added", "Select", TL("Select added elements"));
     
     myCustomToolsOptions.doRegister("select-deleted", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("select-deleted", "Configuration", TL("Select deleted elements"));
+    myCustomToolsOptions.addDescription("select-deleted", "Select", TL("Select deleted elements"));
     
+    myCustomToolsOptions.addOptionSubTopic("Load");
     myCustomToolsOptions.doRegister("load-shapes-modified", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("load-shapes-modified", "Configuration", TL("Load shapes for elements"));
+    myCustomToolsOptions.addDescription("load-shapes-modified", "Load", TL("Load shapes for elements"));
 
     myCustomToolsOptions.doRegister("load-shapes-added", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("load-shapes-added", "Configuration", TL("Load shapes for added"));
+    myCustomToolsOptions.addDescription("load-shapes-added", "Load", TL("Load shapes for added"));
 
     myCustomToolsOptions.doRegister("load-shapes-deleted", new Option_Bool(false));
-    myCustomToolsOptions.addDescription("load-shapes-deleted", "Configuration", TL("Load shapes for deleted elements"));
+    myCustomToolsOptions.addDescription("load-shapes-deleted", "Load", TL("Load shapes for deleted elements"));
 }
 
 
