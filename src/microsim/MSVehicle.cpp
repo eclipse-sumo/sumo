8da94ff81fb119d9a7d1317cc73e2cc301467921
namdre <jakob.erdmann@dlr.de>
2023-04-23 19:57:09 +0200

    disabling debug code. refs #12

--- src/microsim/MSVehicle.cpp
+++ src/microsim/MSVehicle.cpp
@@ -84,7 +84,7 @@
 #include "MSLeaderInfo.h"
 #include "MSDriverState.h"
 
-#define DEBUG_PLAN_MOVE
+//#define DEBUG_PLAN_MOVE
 //#define DEBUG_PLAN_MOVE_LEADERINFO
 //#define DEBUG_CHECKREWINDLINKLANES
 //#define DEBUG_EXEC_MOVE
