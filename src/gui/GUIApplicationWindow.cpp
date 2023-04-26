9ceac3aac017944c9f2e66362a19b45f512c7de8
namdre <jakob.erdmann@dlr.de>
2023-04-26 08:11:31 +0200

    fix #13145

--- src/gui/GUIApplicationWindow.cpp
+++ src/gui/GUIApplicationWindow.cpp
@@ -2020,6 +2020,12 @@ GUIApplicationWindow::handleEvent_SimulationEnded(GUIEvent* e) {
     } else if (GUIGlobals::gDemoAutoReload) {
         onCmdReload(nullptr, 0, nullptr);
     } else if (!myHaveNotifiedAboutSimEnd) {
+        // GUIRunThread::deleteSim() triggers the final message to the log file
+        // (this will never reach the GUI but we cannot use WRITE_MESSAGE here
+        // to avoid a duplicate log entry)
+        myMessageWindow->appendMsg(GUIEventType::MESSAGE_OCCURRED,
+                TLF("Simulation ended at time: %. (%)",
+                    time2string(ec->getTimeStep()), MSNet::getStateMessage(ec->getReason())));
         // build the text
         const std::string text = "Simulation ended at time: " + time2string(ec->getTimeStep()) +
                                  ".\nReason: " + MSNet::getStateMessage(ec->getReason()) +
