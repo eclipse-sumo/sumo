c77ab7b8a90fa60e90b16b022de99d236833d5de
Michael Behrisch <oss@behrisch.de>
2023-05-10 14:31:01 +0200

    splitting by vclass #10715

--- tools/import/gtfs/gtfs2pt.py
+++ tools/import/gtfs/gtfs2pt.py
@@ -66,8 +66,8 @@ def get_options(args=None):
     # ----------------------- fcd options -------------------------------------
     ap.add_argument("--network-split",
                     help="directory to write generated networks to")
-    # ap.add_argument("--network-split.vclass", action="store_true", default=False,
-    #                        help="use the allowed vclass instead of the edge type to split the network")
+    ap.add_argument("--network-split-vclass", action="store_true", default=False,
+                    help="use the allowed vclass instead of the edge type to split the network")
     ap.add_argument("--warn-unmapped", action="store_true", default=False,
                     help="warn about unmapped routes")
     ap.add_argument("--mapperlib", default="lib/fcd-process-chain-2.2.2.jar",
@@ -144,15 +144,19 @@ def splitNet(options):
         mode = os.path.basename(inp)[:-8]
         if not options.modes or mode in options.modes.split(","):
             netPrefix = os.path.join(options.network_split, mode)
-            edgeFilter = ["--keep-edges.by-type", mode] if mode in seenTypes else None
-            if "rail" in mode or mode == "subway":
-                if "railway." + mode in seenTypes:
-                    edgeFilter = ["--keep-edges.by-type", "railway." + mode]
-            elif mode == "train":
-                if "railway.rail" in seenTypes or "railway.light_rail" in seenTypes:
-                    edgeFilter = ["--keep-edges.by-type", "railway.rail,railway.light_rail"]
-            elif mode in ("tram", "bus"):
-                edgeFilter = ["--keep-edges.by-vclass", mode]
+            if options.network_split_vclass:
+                vclass = gtfs2osm.OSM2SUMO_MODES.get(mode)
+                edgeFilter = ["--keep-edges.by-vclass", vclass] if vclass else None
+            else:
+                edgeFilter = ["--keep-edges.by-type", mode] if mode in seenTypes else None
+                if "rail" in mode or mode == "subway":
+                    if "railway." + mode in seenTypes:
+                        edgeFilter = ["--keep-edges.by-type", "railway." + mode]
+                elif mode == "train":
+                    if "railway.rail" in seenTypes or "railway.light_rail" in seenTypes:
+                        edgeFilter = ["--keep-edges.by-type", "railway.rail,railway.light_rail"]
+                elif mode in ("tram", "bus"):
+                    edgeFilter = ["--keep-edges.by-vclass", mode]
             if edgeFilter:
                 if (os.path.exists(netPrefix + ".net.xml") and
                         os.path.getmtime(netPrefix + ".net.xml") > os.path.getmtime(numIdNet)):
