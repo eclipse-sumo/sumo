62108942f68534f74a166a56dc28a1ece143ba0a
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-05-16 15:07:23 +0200

    Added tests for stop and waypoints over trainStops. Refs #13270 and #3045

---
+++ tests/netedit/demandelements/waypoint/trainStop/create/extension/test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2019-07-16
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# go to demand mode
+netedit.supermodeDemand()
+
+# go to waypoint mode
+netedit.stopMode()
+
+# change waypoint type with a valid value
+netedit.changeStopType("waypointBusStop")
+
+# disable duration
+netedit.changeDefaultBoolValue(netedit.attrs.waypointBusStop.create.durationEnable)
+
+# enable until
+netedit.changeDefaultBoolValue(netedit.attrs.waypointBusStop.create.extensionEnable)
+
+# create waypoint
+netedit.leftClick(referencePosition, 290, 175)
+
+# set invalid value
+netedit.changeDefaultValue(netedit.attrs.waypointBusStop.create.extension, "dummyValue")
+
+# try to create waypoint
+netedit.leftClick(referencePosition, 291, 175)
+
+# set invalid value
+netedit.changeDefaultValue(netedit.attrs.waypointBusStop.create.extension, "-20")
+
+# try to create waypoint
+netedit.leftClick(referencePosition, 292, 175)
+
+# set valid value
+netedit.changeDefaultValue(netedit.attrs.waypointBusStop.create.extension, "0")
+
+# try to create waypoint
+netedit.leftClick(referencePosition, 293, 175)
+
+# set valid value
+netedit.changeDefaultValue(netedit.attrs.waypointBusStop.create.extension, "22.33")
+
+# try to create waypoint
+netedit.leftClick(referencePosition, 294, 175)
+
+# Check undo redo
+netedit.undo(referencePosition, 2)
+netedit.redo(referencePosition, 2)
+
+# save Netedit config
+netedit.saveNeteditConfig(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
---
+++ tests/netedit/demandelements/waypoint/trainStop/inspect/extension/test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2009-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2019-07-16
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# go to demand mode
+netedit.supermodeDemand()
+
+# go to waypoint mode
+netedit.stopMode()
+
+# change waypoint type with a valid value
+netedit.changeStopType("waypointBusStop")
+
+# create waypoint
+netedit.leftClick(referencePosition, 290, 175)
+
+# go to inspect mode
+netedit.inspectMode()
+
+# inspect waypoint
+netedit.leftClick(referencePosition, 290, 175)
+
+# change value
+netedit.modifyBoolAttribute(netedit.attrs.waypointBusStop.inspect.durationEnable, False)
+
+# change value
+netedit.modifyBoolAttribute(netedit.attrs.waypointBusStop.inspect.extensionEnable, False)
+
+# change value
+netedit.modifyAttribute(netedit.attrs.waypointBusStop.inspect.extension, "dummyValue", False)
+
+# change value
+netedit.modifyAttribute(netedit.attrs.waypointBusStop.inspect.extension, "", False)
+
+# change value
+netedit.modifyAttribute(netedit.attrs.waypointBusStop.inspect.extension, "-5", False)
+
+# change value
+netedit.modifyAttribute(netedit.attrs.waypointBusStop.inspect.extension, "0", False)
+
+# change value
+netedit.modifyAttribute(netedit.attrs.waypointBusStop.inspect.extension, "3.5", False)
+
+# Check undo redo
+netedit.undo(referencePosition, 2)
+netedit.redo(referencePosition, 2)
+
+# save Netedit config
+netedit.saveNeteditConfig(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
