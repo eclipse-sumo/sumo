dbb5087cc667d140184fcbbed87f95e9a8adeae6
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-05-30 15:58:16 +0200

    Revert dfb386989fbd068137327528e1ce0958b82724e8.

--- src/netedit/dialogs/tools/GNERunNetgenerateDialog.cpp
+++ src/netedit/dialogs/tools/GNERunNetgenerateDialog.cpp
@@ -238,11 +238,6 @@ GNERunNetgenerateDialog::onThreadEvent(FXObject*, FXSelector, void*) {
         delete e;
         updateDialog();
     }
-    // close dialog if netgenerate was sucesfully
-    if (myText->getText().find("Success.") != -1) {
-        myText->setText("");
-        onCmdClose(nullptr, 0, nullptr);
-    }
     return 1;
 }
 
