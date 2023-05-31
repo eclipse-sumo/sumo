42a28df5ecc927eafb39e20869218ede1ed6ce95
namdre <jakob.erdmann@dlr.de>
2023-05-31 09:52:58 +0200

    fixing header refs #12

--- src/netedit/dialogs/options/GNEOptionsDialogElements.cpp
+++ src/netedit/dialogs/options/GNEOptionsDialogElements.cpp
@@ -11,7 +11,7 @@
 // https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 /****************************************************************************/
-/// @file    GNEOptionsDialog.h
+/// @file    GNEOptionsDialogElements.cpp
 /// @author  Pablo Alvarez Lopez
 /// @date    May 2023
 ///
