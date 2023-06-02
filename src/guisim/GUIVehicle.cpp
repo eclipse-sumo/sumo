4ca0510915b727a772459f3e449781ef5836a90b
namdre <jakob.erdmann@dlr.de>
2023-06-02 12:11:34 +0200

    fix #13358

--- src/guisim/GUIVehicle.cpp
+++ src/guisim/GUIVehicle.cpp
@@ -928,7 +928,7 @@ GUIVehicle::selectBlockingFoes() const {
             // the vehicle to enter the junction first has priority
             const GUIVehicle* leader = dynamic_cast<const GUIVehicle*>(it->vehAndGap.first);
             if (leader != nullptr) {
-                if (isLeader(dpi.myLink, leader, it->vehAndGap.second)) {
+                if (isLeader(dpi.myLink, leader, it->vehAndGap.second) || it->inTheWay()) {
                     gSelected.select(leader->getGlID());
 #ifdef DEBUG_FOES
                     std::cout << "      linkLeader=" << leader->getID() << "\n";
