3e9d9393e232085dbda68fdacef3f3b831a53005
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-06-12 09:38:10 +0200

    Updated GNETypeDistributionFrame. Refs #13393

--- src/netedit/frames/demand/GNETypeDistributionFrame.h
+++ src/netedit/frames/demand/GNETypeDistributionFrame.h
@@ -31,6 +31,97 @@
 class GNETypeDistributionFrame : public GNEFrame {
 
 public:
+    // ===========================================================================
+    // class TypeEditor
+    // ===========================================================================
+
+    class TypeEditor : public MFXGroupBoxModule {
+        /// @brief FOX-declaration
+        FXDECLARE(GNETypeDistributionFrame::TypeEditor)
+
+    public:
+        /// @brief constructor
+        TypeEditor(GNETypeDistributionFrame* typeDistributionFrameParent);
+
+        /// @brief destructor
+        ~TypeEditor();
+
+        /// @name FOX-callbacks
+        /// @{
+
+        /// @brief Called when "create type distribution" button is clicked
+        long onCmdCreateType(FXObject*, FXSelector, void*);
+
+        /// @brief Called when "Delete type distribution" button is clicked
+        long onCmdDeleteType(FXObject*, FXSelector, void*);
+
+        /// @brief Called when "Delete type distribution" button is updated
+        long onUpdDeleteType(FXObject* sender, FXSelector, void*);
+
+        /// @}
+
+    protected:
+        /// @brief FOX needs this
+        FOX_CONSTRUCTOR(TypeEditor)
+
+    private:
+        /// @brief pointer to type distribution frame parent
+        GNETypeDistributionFrame* myTypeDistributionFrameParent;
+
+        /// @brief "create vehicle type" button
+        FXButton* myCreateTypeButton = nullptr;
+
+        /// @brief "delete vehicle type" button
+        FXButton* myDeleteTypeButton = nullptr;
+    };
+
+    // ===========================================================================
+    // class TypeSelector
+    // ===========================================================================
+
+    class TypeSelector : public MFXGroupBoxModule {
+        /// @brief FOX-declaration
+        FXDECLARE(GNETypeDistributionFrame::TypeSelector)
+
+    public:
+        /// @brief constructor
+        TypeSelector(GNETypeDistributionFrame* typeFrameParent);
+
+        /// @brief destructor
+        ~TypeSelector();
+
+        /// @brief get current Vehicle Type
+        GNEDemandElement* getCurrentType() const;
+
+        /// @brief set current Vehicle Type
+        void setCurrentType(GNEDemandElement* vType);
+
+        /// @brief refresh vehicle type selector
+        void refreshTypeSelector();
+
+        /// @brief refresh vehicle type selector (only IDs, without refreshing attributes)
+        void refreshTypeSelectorIDs();
+
+        /// @name FOX-callbacks
+        /// @{
+        /// @brief Called when the user select another element in ComboBox
+        long onCmdSelectItem(FXObject*, FXSelector, void*);
+        /// @}
+
+    protected:
+        FOX_CONSTRUCTOR(TypeSelector)
+
+    private:
+        /// @brief pointer to Frame Parent
+        GNETypeDistributionFrame* myTypeDistributionFrameParent;
+
+        /// @brief pointer to current vehicle type
+        GNEDemandElement* myCurrentType = nullptr;
+
+        /// @brief comboBox with the list of vTypes
+        MFXComboBoxIcon* myTypeComboBox = nullptr;
+    };
+
     /**@brief Constructor
      * @brief viewParent GNEViewParent in which this GNEFrame is placed
      * @brief viewNet viewNet that uses this GNEFrame
@@ -48,7 +139,13 @@ protected:
     void attributeUpdated(SumoXMLAttr attribute);
 
 private:
-    /// @brief editorinternal vehicle type attributes
+    /// @brief type editor
+    TypeEditor* myTypeEditor = nullptr;
+
+    /// @brief type selector
+    TypeSelector* myTypeSelector = nullptr;
+
+    /// @brief editor for vehicle type attributes
     GNEFrameAttributeModules::AttributesEditor* myTypeAttributesEditor = nullptr;
 
     /// @brief modul for open extended attributes dialog
