104ae7710bd6fa41e964f9764afe8e5923011895
Coueraud <benjamin.coueraud@dlr.de>
2023-06-16 12:45:13 +0200

    [JuPedSim] Improved DXF>JPS conversion script for polygons with holes.

--- tools/import/dxf/dxf2jupedsim.py
+++ tools/import/dxf/dxf2jupedsim.py
@@ -17,27 +17,18 @@ except ezdxf.DXFStructureError:
     print("Invalid or corrupted input DXF file.")
     sys.exit(2)
 
-polygons = []
-polygon = []
-start = None
-end = None
-for entity in dxf.modelspace():
-    if entity.dxftype() == "LINE":
-        if not start:
-            start = entity.dxf.start
-            polygon.append(tuple(start))
-        if not end or entity.dxf.start == end:
-            end = entity.dxf.end
-            polygon.append(tuple(end))
-        if entity.dxf.end == start:
-            polygons.append(polygon)
-            end = None
-            start = None
-            polygon = []
-
-# for entity in dxf.modelspace():
-#     if entity.dxftype() == "LINE":
-#         print(entity.dxf.start, entity.dxf.end)
+walkable_areas = []
+obstacles = []
+msp = dxf.modelspace()
+for entity in msp.query("LWPOLYLINE"):
+    vertices = list(entity.vertices())
+    if entity.dxf.layer == "walkable_areas":
+        walkable_areas.append(vertices)
+    elif entity.dxf.layer == "obstacles":
+        obstacles.append(vertices)
+    else:
+        print("Polygon belonging to unknown layer.")
+        sys.exit(3)
 
 def convert_to_string(polygon):
     s = []
@@ -45,8 +36,13 @@ def convert_to_string(polygon):
         s.append(f"{coordinates[0]},{coordinates[1]}")
     return " ".join(s)
 
+def polygon_as_XML_element(polygon, typename):
+    return f'\t<poly id="{typename+"_"+str(index)}" type="{typename}" shape="{convert_to_string(polygon)}"/>\n'
+
 with open(filename[:-3] + "add.xml", "w") as add:
     add.write("<additionals>\n")
-    for index, polygon in enumerate(polygons):
-        add.write(f'\t<poly id="{index}" shape="{convert_to_string(polygon)}"/>\n')
+    for index, polygon in enumerate(walkable_areas):
+        add.write(polygon_as_XML_element(polygon, "walkable_area"))
+    for index, polygon in enumerate(obstacles):
+        add.write(polygon_as_XML_element(polygon, "obstacle"))
     add.write("</additionals>")
