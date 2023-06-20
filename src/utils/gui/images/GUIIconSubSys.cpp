59711a9c246db7dfa5f094a94959bd206b838dae
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-06-20 12:34:26 +0200

    Added new icons. Refs #13311

--- src/utils/gui/images/GUIIconSubSys.cpp
+++ src/utils/gui/images/GUIIconSubSys.cpp
@@ -336,8 +336,10 @@
 #include "vehicle.xpm"
 #include "trip.xpm"
 #include "tripjunctions.xpm"
+#include "triptazs.xpm"
 #include "flow.xpm"
 #include "flowjunctions.xpm"
+#include "flowtazs.xpm"
 #include "routeflow.xpm"
 #include "stopelement.xpm"
 #include "waypoint.xpm"
@@ -808,8 +810,10 @@ GUIIconSubSys::GUIIconSubSys(FXApp* a) {
     myIcons[GUIIcon::VEHICLE] = new FXXPMIcon(a, vehicle_xpm);
     myIcons[GUIIcon::TRIP] = new FXXPMIcon(a, trip_xpm);
     myIcons[GUIIcon::TRIP_JUNCTIONS] = new FXXPMIcon(a, tripjunctions_xpm);
+    myIcons[GUIIcon::TRIP_TAZS] = new FXXPMIcon(a, triptazs_xpm);
     myIcons[GUIIcon::FLOW] = new FXXPMIcon(a, flow_xpm);
     myIcons[GUIIcon::FLOW_JUNCTIONS] = new FXXPMIcon(a, flowjunctions_xpm);
+    myIcons[GUIIcon::FLOW_TAZS] = new FXXPMIcon(a, flowtazs_xpm);
     myIcons[GUIIcon::ROUTEFLOW] = new FXXPMIcon(a, routeflow_xpm);
     myIcons[GUIIcon::STOPELEMENT] = new FXXPMIcon(a, stopelement_xpm);
     myIcons[GUIIcon::WAYPOINT] = new FXXPMIcon(a, waypoint_xpm);
