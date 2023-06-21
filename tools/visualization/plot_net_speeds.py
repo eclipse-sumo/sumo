64bd238a37ed53c675572ab74f8a68aafa2cb1dd
m-kro <m.barthauer@t-online.de>
2023-06-21 13:26:48 +0200

    adapt plot_net_speeds ref #8409 #12874 #12636
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- tools/visualization/plot_net_speeds.py
+++ tools/visualization/plot_net_speeds.py
@@ -28,6 +28,7 @@ else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 import sumolib  # noqa
 from sumolib.visualization import helpers  # noqa
+from sumolib.options import ArgumentParser  # noqa
 import matplotlib.pyplot as plt  # noqa
 import matplotlib  # noqa
 
@@ -35,29 +36,25 @@ import matplotlib  # noqa
 def main(args=None):
     """The main function; parses options and plots"""
     # ---------- build and read options ----------
-    from optparse import OptionParser
-    optParser = OptionParser()
-    optParser.add_option("-n", "--net", dest="net", metavar="FILE",
-                         help="Defines the network to read")
-    optParser.add_option("--edge-width", dest="defaultWidth",
-                         type="float", default=1, help="Defines the edge width")
-    optParser.add_option("--edge-color", dest="defaultColor",
+    ap = ArgumentParser()
+    ap.add_argument("-n", "--net", dest="net", category="input", type=ap.net_file, metavar="FILE",
+                         required=True, help="Defines the network to read")
+    ap.add_argument("--edge-width", dest="defaultWidth",
+                         type=float, default=1, help="Defines the edge width")
+    ap.add_argument("--edge-color", dest="defaultColor",
                          default='k', help="Defines the edge color")
-    optParser.add_option("--minV", dest="minV",
-                         type="float", default=None, help="Define the minimum value boundary")
-    optParser.add_option("--maxV", dest="maxV",
-                         type="float", default=None, help="Define the maximum value boundary")
-    optParser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+    ap.add_argument("--minV", dest="minV",
+                         type=float, default=None, help="Define the minimum value boundary")
+    ap.add_argument("--maxV", dest="maxV",
+                         type=float, default=None, help="Define the maximum value boundary")
+    ap.add_argument("-v", "--verbose", dest="verbose", action="store_true",
                          default=False, help="If set, the script says what it's doing")
     # standard plot options
-    helpers.addInteractionOptions(optParser)
-    helpers.addPlotOptions(optParser)
+    helpers.addInteractionOptions(ap)
+    helpers.addPlotOptions(ap)
     # parse
-    options, remaining_args = optParser.parse_args(args=args)
-
-    if options.net is None:
-        print("Error: a network to load must be given.")
-        return 1
+    options = ap.parse_args()
+    
     if options.verbose:
         print("Reading network from '%s'" % options.net)
     net = sumolib.net.readNet(options.net)
