b49998d8f86887bd769d1a3f00e39085b4f80d28
Michael Behrisch <michael.behrisch@dlr.de>
2023-06-22 21:34:11 +0200

    changing more site references to eclipse.dev #13461

--- tests/testEnv.bat
+++ tests/testEnv.bat
@@ -1,5 +1,5 @@
-REM Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-REM Copyright (C) 2008-2022 German Aerospace Center (DLR) and others.
+REM Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+REM Copyright (C) 2008-2023 German Aerospace Center (DLR) and others.
 REM This program and the accompanying materials
 REM are made available under the terms of the Eclipse Public License v2.0
 REM which accompanies this distribution, and is available at
