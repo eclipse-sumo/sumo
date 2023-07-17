6bc99668033d9f674fa508049096a0b3b8bfb395
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-07-17 10:41:04 +0200

    Patching netedit expected results. Refs #3045

---
+++ tests/runNeteditDailyTestsDemand.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Bash script for the daily netedit test run.
+#sets environment variables respecting SUMO_HOME and starts texttest for all existing netedit daily suites
+
+if test `uname` = "Darwin"; then #macOS specific exports
+  export LC_ALL=C
+  export LANG=C
+fi
+
+pushd `dirname $0`
+export TEXTTEST_HOME="$PWD"
+if test x"$SUMO_HOME" = x; then
+  cd ..
+  export SUMO_HOME="$PWD"
+fi
+popd
+export NETEDIT_BINARY="$SUMO_HOME/bin/netedit"
+
+for f in $TEXTTEST_HOME/netedit/testsuite.netedit.daily.demand*; do
+  base=`basename $f`
+  texttest -a ${base:10} "$@" &
+done
+wait
