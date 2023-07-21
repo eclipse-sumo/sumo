aaa667d4f8aeedb1bc01fdc30889a6be8c66547e
m-kro <m.barthauer@t-online.de>
2023-07-21 11:24:26 +0200

    typo ref #22
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- src/microsim/devices/MSDevice_ToC.cpp
+++ src/microsim/devices/MSDevice_ToC.cpp
@@ -197,13 +197,13 @@ MSDevice_ToC::getOutputFilename(const SUMOVehicle& v, const OptionsCont& oc) {
         try {
             file = v.getParameter().getParameter("device.toc.file", file);
         } catch (...) {
-            WRITE_WARNINGF(TL("Invalid value '%'for vehicle parameter 'ssm.measures'"), v.getParameter().getParameter("device.toc.file", file));
+            WRITE_WARNINGF(TL("Invalid value '%' for vehicle parameter 'ssm.measures'"), v.getParameter().getParameter("device.toc.file", file));
         }
     } else if (v.getVehicleType().getParameter().knowsParameter("device.toc.file")) {
         try {
             file = v.getVehicleType().getParameter().getParameter("device.toc.file", file);
         } catch (...) {
-            WRITE_WARNINGF(TL("Invalid value '%'for vType parameter 'ssm.measures'"), v.getVehicleType().getParameter().getParameter("device.toc.file", file));
+            WRITE_WARNINGF(TL("Invalid value '%' for vType parameter 'ssm.measures'"), v.getVehicleType().getParameter().getParameter("device.toc.file", file));
         }
     } else {
         file = oc.getString("device.toc.file") == "" ? file : oc.getString("device.toc.file");
