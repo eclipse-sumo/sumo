ace101f95d5330584fd04db2009e3ab7e72a2b8e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-07-28 13:50:12 +0200

    Fixed #13667

--- src/netedit/elements/additional/GNEPoly.cpp
+++ src/netedit/elements/additional/GNEPoly.cpp
@@ -319,7 +319,7 @@ GNEPoly::drawGL(const GUIVisualizationSettings& s) const {
                     }
                 } else {
                     // draw inner polygon
-                    GUIPolygon::drawInnerPolygon(s, this, this, myPolygonGeometry.getShape(), 0, getFill(), drawUsingSelectColor());
+                    GUIPolygon::drawInnerPolygon(s, this, this, myPolygonGeometry.getShape(), 0, getFill(), myTagProperty.isJuPedSimElement()? false : drawUsingSelectColor());
                 }
             } else {
                 // push matrix
