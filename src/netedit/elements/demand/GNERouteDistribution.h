6edb3b821adef095d5bd4975e0e2b3054590f334
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-08-15 09:16:05 +0200

    Unified GNEVTypeDistribution and GNERouteDistribution implementation in GNEDistribution. Refs #13635

--- src/netedit/elements/demand/GNERouteDistribution.h
+++ src/netedit/elements/demand/GNERouteDistribution.h
@@ -20,175 +20,31 @@
 #pragma once
 #include <config.h>
 
-#include "GNEDemandElement.h"
+#include "GNEDistribution.h"
 
 
 // ===========================================================================
 // class definitions
 // ===========================================================================
 
-class GNERouteDistribution : public GNEDemandElement {
+class GNERouteDistribution : public GNEDistribution {
 
 public:
     /// @brief default constructor
     GNERouteDistribution(GNENet* net);
 
     /// @brief parameter constructor
-    GNERouteDistribution(GNENet* net, const std::string& vTypeID, const int deterministic = -1);
+    GNERouteDistribution(GNENet* net, const std::string& ID);
 
     /// @brief destructor
     ~GNERouteDistribution();
-
-    /**@brief get move operation
-     * @note returned GNEMoveOperation can be nullptr
-     */
-    GNEMoveOperation* getMoveOperation();
 
     /**@brief write demand element element into a xml file
      * @param[in] device device in which write parameters of demand element element
      */
     void writeDemandElement(OutputDevice& device) const;
 
-    /// @brief check if current demand element is valid to be written into XML
-    Problem isDemandElementValid() const;
-
-    /// @brief return a string with the current demand element problem
-    std::string getDemandElementProblem() const;
-
-    /// @brief fix demand element problem
-    void fixDemandElementProblem();
-
-    /// @name members and functions relative to elements common to all demand elements
-    /// @{
-    /// @brief obtain VClass related with this demand element
-    SUMOVehicleClass getVClass() const;
-
-    /// @brief get color
-    const RGBColor& getColor() const;
-
-    /// @}
-
-    /// @name Functions related with geometry of element
-    /// @{
-    /// @brief update pre-computed geometry information
-    void updateGeometry();
-
-    /// @brief Returns position of additional in view
-    Position getPositionInView() const;
-    /// @}
-
-    /// @name inherited from GUIGlObject
-    /// @{
-    /**@brief Returns the name of the parent object
-     * @return This object's parent id
-     */
-    std::string getParentName() const;
-
-    /**@brief Returns the boundary to which the view shall be centered in order to show the object
-     * @return The boundary the object is within
-     */
-    Boundary getCenteringBoundary() const;
-
-    /// @brief split geometry
-    void splitEdgeGeometry(const double splitPosition, const GNENetworkElement* originalElement, const GNENetworkElement* newElement, GNEUndoList* undoList);
-
-    /**@brief Draws the object
-     * @param[in] s The settings for the current view (may influence drawing)
-     * @see GUIGlObject::drawGL
-     */
-    void drawGL(const GUIVisualizationSettings& s) const;
-
-    /// @}
-
-    /// @name inherited from GNEPathManager::PathElement
-    /// @{
-
-    /// @brief compute pathElement
-    void computePathElement();
-
-    /**@brief Draws partial object
-     * @param[in] s The settings for the current view (may influence drawing)
-     * @param[in] lane lane in which draw partial
-     * @param[in] segment PathManager segment (used for segment options)
-     * @param[in] offsetFront extra front offset (used for drawing partial gl above other elements)
-     */
-    void drawPartialGL(const GUIVisualizationSettings& s, const GNELane* lane, const GNEPathManager::Segment* segment, const double offsetFront) const;
-
-    /**@brief Draws partial object (junction)
-     * @param[in] s The settings for the current view (may influence drawing)
-     * @param[in] fromLane from GNELane
-     * @param[in] toLane to GNELane
-     * @param[in] segment PathManager segment (used for segment options)
-     * @param[in] offsetFront extra front offset (used for drawing partial gl above other elements)
-     */
-    void drawPartialGL(const GUIVisualizationSettings& s, const GNELane* fromLane, const GNELane* toLane, const GNEPathManager::Segment* segment, const double offsetFront) const;
-
-    /// @brief get first path lane
-    GNELane* getFirstPathLane() const;
-
-    /// @brief get last path lane
-    GNELane* getLastPathLane() const;
-    /// @}
-
-    /// @brief inherited from GNEAttributeCarrier
-    /// @{
-    /* @brief method for getting the Attribute of an XML key
-     * @param[in] key The attribute key
-     * @return string with the value associated to key
-     */
-    std::string getAttribute(SumoXMLAttr key) const;
-
-    /* @brief method for getting the Attribute of an XML key in double format (to avoid unnecessary parse<double>(...) for certain attributes)
-     * @param[in] key The attribute key
-     * @return double with the value associated to key
-     */
-    double getAttributeDouble(SumoXMLAttr key) const;
-
-    /* @brief method for getting the Attribute of an XML key in Position format (used in person plans)
-     * @param[in] key The attribute key
-     * @return double with the value associated to key
-     */
-    Position getAttributePosition(SumoXMLAttr key) const;
-
-    /* @brief method for setting the attribute and letting the object perform additional changes
-     * @param[in] key The attribute key
-     * @param[in] value The new value
-     * @param[in] undoList The undoList on which to register changes
-     * @param[in] net optionally the GNENet to inform about gui updates
-     */
-    void setAttribute(SumoXMLAttr key, const std::string& value, GNEUndoList* undoList);
-
-    /* @brief method for setting the attribute and letting the object perform additional changes
-     * @param[in] key The attribute key
-     * @param[in] value The new value
-     * @param[in] undoList The undoList on which to register changes
-     */
-    bool isValid(SumoXMLAttr key, const std::string& value);
-
-    /// @brief get PopPup ID (Used in AC Hierarchy)
-    std::string getPopUpID() const;
-
-    /// @brief get Hierarchy Name (Used in AC Hierarchy)
-    std::string getHierarchyName() const;
-    /// @}
-
-    /// @brief get parameters map
-    const Parameterised::Map& getACParametersMap() const;
-
-protected:
-    /// @brief deterministic attribute
-    int myDeterministic = -1;
-
 private:
-    /// @brief method for setting the attribute and nothing else
-    void setAttribute(SumoXMLAttr key, const std::string& value);
-
-    /// @brief set move shape
-    void setMoveShape(const GNEMoveResult& moveResult);
-
-    /// @brief commit move shape
-    void commitMoveShape(const GNEMoveResult& moveResult, GNEUndoList* undoList);
-
     /// @brief Invalidated copy constructor.
     GNERouteDistribution(GNERouteDistribution*) = delete;
 
