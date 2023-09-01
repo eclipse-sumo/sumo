1556e012fb7534a0116781fadfef392de2a3dcd8
Benjamin Couéraud <benjamin.coueraud@dlr.de>
2023-09-01 09:54:44 +0200

    [JuPedSim] Moved out the build function implementation from the MsgHandler header.

--- src/utils/common/MsgHandler.h
+++ src/utils/common/MsgHandler.h
@@ -25,14 +25,6 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <chrono>
-#ifdef WIN32
-#define NOMINMAX
-#include <windows.h>
-#undef NOMINMAX
-#else
-#include <unistd.h>
-#endif
 #include <utils/common/StringUtils.h>
 #include <utils/common/Translation.h>
 #include <utils/iodevices/OutputDevice.h>
@@ -177,46 +169,7 @@ public:
 
 protected:
     /// @brief Builds the string which includes the mml-message type
-    inline std::string build(const std::string& msg, bool addType) {
-        std::stringstream decoratedMessage;
-        if (myWriteTimestamps) {
-            std::chrono::system_clock::time_point now_timestamp = std::chrono::system_clock::now();
-            auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now_timestamp.time_since_epoch()) % 1000;
-            std::time_t now_time_t = std::chrono::system_clock::to_time_t(now_timestamp);
-            std::tm tm = *std::localtime(&now_time_t);
-            decoratedMessage << std::put_time(&tm, "[%d-%m-%Y %T");
-            decoratedMessage << ':' << std::setfill('0') << std::setw(3) << milliseconds.count() << "] ";
-        }
-        if (myWriteProcessId) {
-#ifdef WIN32
-            decoratedMessage << "[PID: " << GetCurrentProcessId() << "] ";
-#else
-            decoratedMessage << "[PID: " << getpid() << "] ";
-#endif
-        }
-        if (addType) {
-            switch (myType) {
-                case MsgType::MT_MESSAGE:
-                    break;
-                case MsgType::MT_WARNING:
-                    decoratedMessage << "Warning: ";
-                    break;
-                case MsgType::MT_ERROR:
-                    decoratedMessage << "Error: ";
-                    break;
-                case MsgType::MT_DEBUG:
-                    decoratedMessage << "Debug: ";
-                    break;
-                case MsgType::MT_GLDEBUG:
-                    decoratedMessage << "GLDebug: ";
-                    break;
-                default:
-                    break;
-            }
-        }
-        decoratedMessage << msg;
-        return decoratedMessage.str();
-    }
+    std::string build(const std::string& msg, bool addType);
 
     virtual bool aggregationThresholdReached(const std::string& format) {
         return myAggregationThreshold >= 0 && myAggregationCount[format]++ >= myAggregationThreshold;
