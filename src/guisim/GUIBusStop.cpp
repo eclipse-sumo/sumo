ddd21fefb94ca10c965c66e3388a8c4fe54b8620
Michael Behrisch <michael.behrisch@dlr.de>
2023-09-16 06:19:54 +0200

    fixing warnings #3

--- src/guisim/GUIBusStop.cpp
+++ src/guisim/GUIBusStop.cpp
@@ -102,7 +102,7 @@ GUIBusStop::initShape(PositionVector& fgShape,
 
 
 bool
-GUIBusStop::addAccess(MSLane* lane, const double startPos, const double endPos, double length) {
+GUIBusStop::addAccess(MSLane* const lane, const double startPos, const double endPos, double length) {
     const bool added = MSStoppingPlace::addAccess(lane, startPos, endPos, length);
     if (added) {
         myAccessCoords.push_back(lane->geometryPositionAtOffset((startPos + endPos) / 2.));
