1a04844b4c0a98b6c11bb5472bcfcfcda0b1eb3b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-09-22 09:48:14 +0200

    Added GNEContourElement. Refs #13858

---
+++ src/netedit/elements/GNEContourElement.h
@@ -0,0 +1,43 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2023 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEContourElement.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Aug 2023
+///
+// A abstract class contour elements
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include "GNEAttributeCarrier.h"
+
+// ===========================================================================
+// class declaration
+// ===========================================================================
+
+class GNEUndoList;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNEContourElement {
+
+public:
+    /// @brief Constructor
+    GNEContourElement(GNEDemandElement* demandElement);
+
+private:
+
+};
