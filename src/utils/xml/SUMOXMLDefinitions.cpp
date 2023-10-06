5baf5445dbe97486dee56c608ccd24b7993029e3
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2023-10-06 11:25:15 +0200

    Fixed error in SUMOXMLDefinitions.cpp. Refs #13828

--- src/utils/xml/SUMOXMLDefinitions.cpp
+++ src/utils/xml/SUMOXMLDefinitions.cpp
@@ -215,7 +215,7 @@ StringBijection<int>::Entry SUMOXMLDefinitions::tags[] = {
     { "carFollowing-CC",                    SUMO_TAG_CF_CC },
     // Person
     { "person",                             SUMO_TAG_PERSON },
-    { "personTrip",                         SUMO_TAG_WALK },
+    { "personTrip",                         SUMO_TAG_PERSONTRIP },
     { "ride",                               SUMO_TAG_RIDE },
     { "walk",                               SUMO_TAG_WALK },
     { "personFlow",                         SUMO_TAG_PERSONFLOW },
