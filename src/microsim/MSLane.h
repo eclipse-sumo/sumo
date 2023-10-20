e101ed1fbb3b95a754eeb3f465d376678a0d08ab
m-kro <m.barthauer@t-online.de>
2023-10-20 15:57:00 +0200

    TraCI lane speed limit precedes edge type ref #13665
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- src/microsim/MSLane.h
+++ src/microsim/MSLane.h
@@ -566,7 +566,7 @@ public:
         if (myRestrictions != nullptr) {
             std::map<SUMOVehicleClass, double>::const_iterator r = myRestrictions->find(veh->getVClass());
             if (r != myRestrictions->end()) {
-                if (myControlledByVSS) {
+                if (mySpeedByVSS || mySpeedByTraCI) {
                     return MIN2(myMaxSpeed, MIN2(veh->getMaxSpeed(), r->second * veh->getChosenSpeedFactor()));
                 } else {
                     return MIN2(veh->getMaxSpeed(), r->second * veh->getChosenSpeedFactor());
@@ -731,8 +731,9 @@ public:
     /** @brief Sets a new maximum speed for the lane (used by TraCI and MSCalibrator)
      * @param[in] val the new speed in m/s
      * @param[in] whether a variable speed sign (VSS) imposes the speed limit
+     * @param[in] whether TraCI imposes the speed limit
      */
-    void setMaxSpeed(double val, bool byVSS = false);
+    void setMaxSpeed(double val, bool byVSS = false, bool byTraCI = false);
 
     /** @brief Sets a new friction coefficient for the lane [*to be later (used by TraCI and MSCalibrator)*]
     * @param[in] val the new friction coefficient [0..1]
@@ -1484,7 +1485,10 @@ protected:
     double myFrictionCoefficient;
 
     /// @brief Whether the current speed limit is set by a variable speed sign (VSS)
-    bool myControlledByVSS;
+    bool mySpeedByVSS;
+
+    /// @brief Whether the current speed limit has been set through TraCI
+    bool mySpeedByTraCI;
 
     /// The vClass permissions for this lane
     SVCPermissions myPermissions;
