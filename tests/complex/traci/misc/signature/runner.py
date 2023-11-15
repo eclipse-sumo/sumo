c4c628968c2a8c86c2ab9612a3d0dcb409d3dfbf
Michael Behrisch <michael.behrisch@dlr.de>
2023-11-15 07:25:59 +0100

    adding test #21 #6918

---
+++ tests/complex/traci/misc/signature/runner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2008-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @date    2023-11-14
+
+
+import os
+import sys
+import inspect
+if "SUMO_HOME" in os.environ:
+    sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+import traci
+import libsumo
+
+VERBOSE = False
+
+for dt in traci.DOMAINS:
+    for dl in libsumo.DOMAINS:
+        if dt.DOMAIN_ID == dl.DOMAIN_ID:
+            for ft in inspect.getmembers(dt):
+                if ft[0][0] != "_" and inspect.ismethod(ft[1]):
+                    if VERBOSE:
+                        print("checking", dt._name, ft[0])
+                    for fl in inspect.getmembers(dl):
+                        if fl[0] == ft[0]:
+                            sigt = inspect.signature(ft[1])
+                            sigl = inspect.signature(fl[1])
+                            if sigt != sigl:
+                                params = list(sigl.parameters.values())
+                                if not params or params[0].kind != inspect.Parameter.VAR_POSITIONAL:
+                                    print(".".join([dt._name, ft[0]]), "traci:", sigt, "libsumo:", sigl)
