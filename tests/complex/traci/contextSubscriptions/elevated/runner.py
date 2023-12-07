c20cf3852f1801daef1fc10381db057ddb06e9d8
Michael Behrisch <michael.behrisch@dlr.de>
2023-12-07 10:19:09 +0100

    adding test #21 fix #14075

---
+++ tests/complex/traci/contextSubscriptions/elevated/runner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2008-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Jakob Erdmann
+# @date    2018-09-27
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+from pprint import pprint
+
+if "SUMO_HOME" in os.environ:
+    sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
+
+import traci  # noqa
+import sumolib  # noqa
+import traci.constants as tc  # noqa
+
+sumoBinary = sumolib.checkBinary('sumo')
+traci.start([sumoBinary,
+             "-n", "input_net.net.xml",
+             "-r", "input_routes.rou.xml",
+             "-a", "input_additional.add.xml",
+             "--default.speeddev", "0",
+             "--no-step-log",
+             ])
+
+traci.poi.subscribeContext("poi0", tc.CMD_GET_VEHICLE_VARIABLE, 50,
+                               [tc.VAR_SPEED, tc.VAR_LANE_ID, tc.VAR_POSITION])
+traci.poi.subscribeContext("poi300", tc.CMD_GET_VEHICLE_VARIABLE, 50,
+                               [tc.VAR_SPEED, tc.VAR_LANE_ID, tc.VAR_POSITION])
+
+for _ in range(10):
+    traci.simulationStep()
+    pprint(traci.poi.getAllContextSubscriptionResults())
+
+traci.close()
