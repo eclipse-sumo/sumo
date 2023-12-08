258c8ff7beb6c1a12353bff66b9a78600ac79934
m-kro <m.barthauer@t-online.de>
2023-12-08 09:05:11 +0100

    refactor plot_trajectories.py input ref #14147
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- tools/plot_trajectories.py
+++ tools/plot_trajectories.py
@@ -86,11 +86,9 @@ def getOptions(args=None):
     optParser.add_option("--legend", category="processing", action="store_true", default=False, help="Add legend")
     optParser.add_option("-v", "--verbose", category="processing", action="store_true",
                          default=False, help="tell me what you are doing")
+    optParser.add_option("fcdfiles", nargs="+", category="input", type=ArgumentParser.file, help="FCD input file(s)")
 
     options, args = optParser.parse_known_args(args=args)
-    if len(args) < 1:
-        sys.exit("mandatory argument FCD_FILE missing")
-    options.fcdfiles = args
 
     # keep old presets from before integration of common options
     options.nolegend = not options.legend
