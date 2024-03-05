76e67d9e9fde7d3672854f7ff2899d2f60ae701d
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-03-05 12:58:31 +0100

    fix #14463

--- src/netedit/elements/demand/GNEPersonTrip.cpp
+++ src/netedit/elements/demand/GNEPersonTrip.cpp
@@ -103,7 +103,7 @@ GNEPersonTrip::writeDemandElement(OutputDevice& device) const {
     if (myModes.size() > 0) {
         device.writeAttr(SUMO_ATTR_MODES, myModes);
     }
-    if (myLines.size() > 0) {
+    if (myLines.size() > 0 && (myLines.size() != 1 || myLines.front() != "ANY")) {
         device.writeAttr(SUMO_ATTR_LINES, myLines);
     }
     if (myVTypes.size() > 0) {
