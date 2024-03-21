30a7800c602cfa0778536f03ea6c935d44ba1016
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-03-21 17:09:37 +0100

    fix #14573

--- src/utils/gui/images/GUITexturesHelper.cpp
+++ src/utils/gui/images/GUITexturesHelper.cpp
@@ -58,8 +58,8 @@ GUITexturesHelper::add(FXImage* i) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
                  i->getWidth(), i->getHeight(), 0,
                  GL_RGBA, GL_UNSIGNED_BYTE, i->getData());
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
     glBindTexture(GL_TEXTURE_2D, 0);
