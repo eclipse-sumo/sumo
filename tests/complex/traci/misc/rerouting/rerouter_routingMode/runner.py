c59900ea1ced28b2021d36e83f35a6739f1a2d67
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-04-16 07:32:08 +0200

    fixing style refs #21

--- tests/complex/traci/misc/rerouting/rerouter_routingMode/runner.py
+++ tests/complex/traci/misc/rerouting/rerouter_routingMode/runner.py
@@ -27,7 +27,7 @@ if "SUMO_HOME" in os.environ:
 import traci  # noqa
 import sumolib  # noqa
 
-traci.start([sumolib.checkBinary('sumo'),'-c', 'sumo.sumocfg'])
+traci.start([sumolib.checkBinary('sumo'), '-c', 'sumo.sumocfg'])
 traci.simulationStep()
 traci.simulationStep()
 for vehID in traci.vehicle.getIDList():
