4b4db7d2184d1ea0ff4a7f6ef5b3f6d5154906a4
Michael Behrisch <michael.behrisch@dlr.de>
2024-06-20 10:22:19 +0200

    fixing python2 compatiblity #12

--- tools/sumolib/vehicletype.py
+++ tools/sumolib/vehicletype.py
@@ -20,6 +20,7 @@
 # This file is a port of createVehTypeDistribution.py to sumolib
 
 from __future__ import absolute_import
+from __future__ import print_function
 import os
 import json
 import re
