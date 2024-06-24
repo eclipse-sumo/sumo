3b500514c9dec2296dae42c26553258fa0115609
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-06-24 15:32:59 +0200

    added tests refs #15087, #21

---
+++ tests/tools/sumolib/xml/parse_structured_optional/runner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2008-2024 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Giuliana Armellini
+# @date
+
+from __future__ import print_function
+import os
+import sys
+from collections import OrderedDict
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+from sumolib.xml import parse_fast_structured  # noqa
+
+nested = OrderedDict((('vehicle', ['id', 'speed', 'lane']), ('person', ['id', 'speed', 'edge'])))
+for step in parse_fast_structured("input_data.xml", 'timestep', ['time'],
+        nested, optional=True):
+    print(step)
