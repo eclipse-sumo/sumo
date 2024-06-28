4da98b4fcf1f3d19f2cf2a0f73521e37769cf15b
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-06-28 13:18:18 +0200

    test

--- tests/netedit/network/edges/select_vtypes/allowed/private/test.py
+++ tests/netedit/network/edges/select_vtypes/allowed/private/test.py
@@ -31,8 +31,9 @@ neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
 # go to select mode
 netedit.selectMode()
 
-# select all edges with allow = "private" and remove it
-netedit.selectItems("Network", "edge", "allow", "private")
+# select all edges with allow = "all" and remove it
+# (we cannot use "=all" since "all" is expanded to include all vClassess)
+netedit.selectItems("Network", "edge", "allow", "all")
 netedit.deleteSelectedItems()
 
 # recompute
