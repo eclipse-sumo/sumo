fa279b4379bd6723ea67e8189e8c58fbdf27bff2
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-07-02 14:49:46 +0200

    tests

---
+++ tests/netedit/network/connections/without_tls/inspect_selected_tls/linkindex2/test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2009-2024 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# Rebuild network
+netedit.rebuildNetwork()
+
+# show connections
+netedit.changeEditMode(netedit.attrs.modes.network.showConnections)
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvert()
+
+# go to inspect mode agaim
+netedit.inspectMode()
+
+# inspect selected connections
+netedit.leftClick(referencePosition, 397, 150)
+
+# Change linkIndex with an invalid value
+netedit.modifyAttribute(netedit.attrs.connection.inspectSelectionTLS.linkIndex2, "dummyLinkIndex", True)
+
+# Change linkIndex with an valid value
+netedit.modifyAttribute(netedit.attrs.connection.inspectSelectionTLS.linkIndex2, "-2", True)
+
+# Change linkIndex with an valid value
+netedit.modifyAttribute(netedit.attrs.connection.inspectSelectionTLS.linkIndex2, "0", True)
+
+# Change linkIndex with an valid value (but big)
+netedit.modifyAttribute(netedit.attrs.connection.inspectSelectionTLS.linkIndex2, "600", True)
+
+# Change linkIndex with an valid value
+netedit.modifyAttribute(netedit.attrs.connection.inspectSelectionTLS.linkIndex2, "12", True)
+
+# rebuild
+netedit.rebuildNetwork()
+
+# Check undo
+netedit.undo(referencePosition, 5)
+
+# rebuild
+netedit.rebuildNetwork()
+
+# Check redo
+netedit.redo(referencePosition, 5)
+
+# save Netedit config
+netedit.saveNeteditConfig(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
---
+++ tests/netedit/network/connections/without_tls/inspect_tls/allow/test.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2009-2024 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+
+# import common functions for netedit tests
+import os
+import sys
+
+testRoot = os.path.join(os.environ.get('SUMO_HOME', '.'), 'tests')
+neteditTestRoot = os.path.join(
+    os.environ.get('TEXTTEST_HOME', testRoot), 'netedit')
+sys.path.append(neteditTestRoot)
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
+
+# Rebuild network
+netedit.rebuildNetwork()
+
+# show connections
+netedit.changeEditMode(netedit.attrs.modes.network.showConnections)
+
+# inspect connection
+netedit.leftClick(referencePosition, 397, 150)
+
+# Change parameter 8 with an non valid value (dummy)
+netedit.modifyAttribute(netedit.attrs.connection.inspectTLS.allow, "DummyAllowed", True)
+
+# Change parameter 8 with a valid value (empty)
+netedit.modifyAttribute(netedit.attrs.connection.inspectTLS.allow, "", True)
+
+# Change parameter 8 with a valid value (different separators)
+netedit.modifyAttribute(netedit.attrs.connection.inspectTLS.allow, "authority  army, passenger; taxi. tram", True)
+
+# Change parameter 8 with a valid value (empty)
+netedit.modifyAttribute(netedit.attrs.connection.inspectTLS.allow, "", True)
+
+# Change parameter 8 with a valid value (empty)
+netedit.modifyAllowDisallowValue(netedit.attrs.connection.inspectTLS.allowButton, True)
+
+# Change parameter 8 with a valid value (empty)
+netedit.modifyAttribute(netedit.attrs.connection.inspectTLS.allow,
+                        "authority army vip passenger hov taxi bus coach tram bicycle", True)
+
+# rebuild
+netedit.rebuildNetwork()
+
+# Check undo
+netedit.undo(referencePosition, 4)
+
+# rebuild
+netedit.rebuildNetwork()
+
+# Check redo
+netedit.redo(referencePosition, 4)
+
+# save Netedit config
+netedit.saveNeteditConfig(referencePosition)
+
+# quit netedit
+netedit.quit(neteditProcess)
