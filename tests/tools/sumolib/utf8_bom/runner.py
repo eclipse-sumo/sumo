eaab8f3bbf9a153e0fc88ae2e487003b2e95366c
Michael Behrisch <michael.behrisch@dlr.de>
2024-07-04 10:11:32 +0200

    adding test #21 #15116

---
+++ tests/tools/sumolib/utf8_bom/runner.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2008-2024 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Michael Behrisch
+# @date    2024-07-04
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+import os
+import sys
+if 'SUMO_HOME' in os.environ:
+    sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+
+import sumolib  # noqa
+
+with sumolib.openz("tt.txt", "w", encoding="utf-8-sig") as o:
+    print("hello bom", file=o)
+
+with sumolib.openz("tt.txt", encoding="utf-8") as i:
+    a = i.read()
+    print(len(a))
+
+with sumolib.openz("tt.txt") as i:
+    a = i.read()
+    print(len(a))
+
+
+with sumolib.openz("t2.txt", "w") as o:
+    print("hello bom", file=o)
+
+with sumolib.openz("t2.txt", encoding="utf-8") as i:
+    a = i.read()
+    print(len(a))
+
+with sumolib.openz("t2.txt") as i:
+    a = i.read()
+    print(len(a))
