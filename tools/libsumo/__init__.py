b3eb7a7b680ca5ca778dd84727eb053593b1e249
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-07-09 15:47:32 +0200

    fix #15179

--- tools/libsumo/__init__.py
+++ tools/libsumo/__init__.py
@@ -87,14 +87,26 @@ def set_phases(self, phases):
     TraCILogic_phases_set(self, new_phases)
 
 
-def TraCILogic__init__(self, *args):
-    phases = args[3] if len(args) > 3 else None
+def TraCILogic__init__(self, *args, **kwargs):
+    # Extract known keyword arguments or set to None if not provided
+    programID = kwargs.get('programID', args[0] if len(args) > 0 else None)
+    type_ = kwargs.get('type',  args[1] if len(args) > 1 else None)
+    currentPhaseIndex = kwargs.get('currentPhaseIndex',  args[2] if len(args) > 2 else None)
+    phases = kwargs.get('phases',  args[3] if len(args) > 3 else None)
+    #subParameter = kwargs.get('subParameter',  args[4] if len(args) > 4 else None)
+
+    # Update phases if provided
     if phases:
         new_phases = [TraCIPhase(p.duration, p.state, p.minDur, p.maxDur, p.next, p.name) for p in phases]
-        args = (args[0], args[1], args[2], new_phases) + args[4:]
+        phases = new_phases
+    
+    # Rebuild args including the extracted keyword arguments
+    args = (programID, type_, currentPhaseIndex, phases)
+
+    # Initialize with the original function
     TraCILogic_swiginit(self, new_TraCILogic(*args))
 
-
+# Override methods and properties
 TraCILogic.__init__ = TraCILogic__init__
 TraCILogic.phases = property(TraCILogic_phases_get, set_phases)
 TraCILogic.getPhases = _trafficlight.Logic.getPhases
