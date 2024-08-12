acc454c42ca89f9ea131cbbb4cb162849ae0f31d
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-08-12 10:05:48 +0200

    Adjusted person contours. Refs #457

--- src/netedit/elements/demand/GNEPerson.cpp
+++ src/netedit/elements/demand/GNEPerson.cpp
@@ -419,7 +419,7 @@ GNEPerson::drawGL(const GUIVisualizationSettings& s) const {
             myPersonContour.drawDottedContours(s, d, this, s.dottedContourSettings.segmentWidth, true);
         }
         // calculate contour
-        myPersonContour.calculateContourRectangleShape(s, d, this, personPosition, 0.5, 0.5, 0, 0, 0, exaggeration);
+        myPersonContour.calculateContourRectangleShape(s, d, this, personPosition, 0.3, 0.3, -0.9, 0, 0, exaggeration);
     }
 }
 
