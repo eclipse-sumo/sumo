eea20e8f41fb1aa7a980397ab608f3d9df077a6b
Michael Behrisch <michael.behrisch@dlr.de>
2024-08-26 15:15:36 +0200

    better cleanup in nightly builds #3

--- tools/build_config/dailyUpdateMakeGCC.sh
+++ tools/build_config/dailyUpdateMakeGCC.sh
@@ -132,6 +132,7 @@ echo "--" >> $STATUSLOG
 # netedit tests
 if test -e $SUMO_BINDIR/netedit && test $SUMO_BINDIR/netedit -nt build/$FILEPREFIX/Makefile; then
   if test "$FILEPREFIX" == "gcc4_64" || test "$FILEPREFIX" == "extraNetedit"; then
+    killall -9 -q fluxbox
     tests/runNeteditDailyTests.sh -b ${FILEPREFIX}netedit -name $TESTLABEL >> $TESTLOG 2>&1
     tests/runTests.sh -b ${FILEPREFIX} -name $TESTLABEL -coll >> $TESTLOG 2>&1
   fi
