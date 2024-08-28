10491bdf9143efe84f9a641d980dac40356dbd56
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-08-28 13:24:45 +0200

    Updated GNEDemandElementPlan::updatePlanGeometry(). Refs #15368

--- src/netedit/elements/demand/GNEDemandElementPlan.cpp
+++ src/netedit/elements/demand/GNEDemandElementPlan.cpp
@@ -1220,6 +1220,9 @@ GNEDemandElementPlan::updatePlanGeometry() {
         } else if (tagProperty.planFromJunction()) {
             // from junction
             firstPos = myPlanElement->getParentJunctions().front()->getPositionInView();
+        } else if (tagProperty.planFromStoppingPlace()) {
+            // end of stoppingPlace lane shape
+            firstPos = myPlanElement->getParentAdditionals().front()->getParentLanes().front()->getLaneShape().back();
         } else if (tagProperty.planFromTAZ()) {
             // from TAZ
             if (myPlanElement->getParentTAZs().front()->getAttribute(SUMO_ATTR_CENTER).empty()) {
@@ -1235,6 +1238,9 @@ GNEDemandElementPlan::updatePlanGeometry() {
         } else if (tagProperty.planToJunction()) {
             // from junction
             lastPos = myPlanElement->getParentJunctions().back()->getPositionInView();
+        } else if (tagProperty.planToStoppingPlace()) {
+            // end of stoppingPlace lane shape
+            lastPos = myPlanElement->getParentAdditionals().back()->getParentLanes().front()->getLaneShape().front();
         } else if (tagProperty.planToTAZ()) {
             // from TAZ
             if (myPlanElement->getParentTAZs().back()->getAttribute(SUMO_ATTR_CENTER).empty()) {
