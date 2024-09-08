8e281d2772cb4234d58f2aa5eb67bf5dcf94d564
Michael Behrisch <michael.behrisch@dlr.de>
2024-09-08 09:00:10 +0200

    adapting test results #21 #15450, removing unnecessary whitespace

--- src/traci_testclient/TraCITestClient.cpp
+++ src/traci_testclient/TraCITestClient.cpp
@@ -594,7 +594,7 @@ TraCITestClient::readAndReportTypeDependent(tcpip::Storage& inMsg, int valueData
         double x = inMsg.readDouble();
         double y = inMsg.readDouble();
         double z = inMsg.readDouble();
-        answerLog << " Position3DValue: " << std::endl;
+        answerLog << " Position3DValue:";
         answerLog << " x: " << x << " y: " << y
                   << " z: " << z << std::endl;
     } else if (valueDataType == libsumo::POSITION_ROADMAP) {
@@ -606,10 +606,10 @@ TraCITestClient::readAndReportTypeDependent(tcpip::Storage& inMsg, int valueData
                   << " laneId=" << laneId << std::endl;
     } else if (valueDataType == libsumo::TYPE_STRING) {
         std::string s = inMsg.readString();
-        answerLog << " string value: " << s << std::endl;
+        answerLog << " string value: " << (s.empty() ? "''" : s) << std::endl;
     } else if (valueDataType == libsumo::TYPE_STRINGLIST) {
         std::vector<std::string> s = inMsg.readStringList();
-        answerLog << " string list value: [ " << std::endl;
+        answerLog << " string list value: [ ";
         for (std::vector<std::string>::iterator i = s.begin(); i != s.end(); ++i) {
             if (i != s.begin()) {
                 answerLog << ", ";
@@ -619,7 +619,7 @@ TraCITestClient::readAndReportTypeDependent(tcpip::Storage& inMsg, int valueData
         answerLog << " ]" << std::endl;
     } else if (valueDataType == libsumo::TYPE_COMPOUND) {
         int no = inMsg.readInt();
-        answerLog << " compound value with " << no << " members: [ " << std::endl;
+        answerLog << " compound value with " << no << " members: [ ";
         for (int i = 0; i < no; ++i) {
             int currentValueDataType = inMsg.readUnsignedByte();
             answerLog << " valueDataType=" << currentValueDataType;
