eclipse-sumo/sumo

commit 476295c9dcb941426a7b08877e6dee000969c11d

Addendum to #14600

src/netedit/elements/additional/GNEAdditional.cpp
@@ -557,7 +557,9 @@ GNEAdditional::isValidDetectorID(const std::vector<SumoXMLTag>& tags, const std:
 void
 GNEAdditional::setAdditionalID(const std::string& newID) {
     // update ID
-    if (!isTemplate()) {
+    if (isTemplate()) {
+        setMicrosimID(newID);
+    } else {
         myNet->getAttributeCarriers()->updateAdditionalID(this, newID);
     }
     // change IDs of certain children

src/netedit/elements/demand/GNEDemandElement.cpp
@@ -407,7 +407,9 @@ GNEDemandElement::isValidDemandElementID(const std::vector<SumoXMLTag>& tags, co
 void
 GNEDemandElement::setDemandElementID(const std::string& newID) {
     // update ID
-    if (!isTemplate() && myTagProperty.hasAttribute(SUMO_ATTR_ID)) {
+    if (isTemplate()) {
+        setMicrosimID(newID);
+    } else {
         myNet->getAttributeCarriers()->updateDemandElementID(this, newID);
     }
     // check if update ids of child elements

src/netedit/frames/network/GNEAdditionalFrame.cpp
@@ -504,7 +504,7 @@ GNEAdditionalFrame::buildAdditionalOverLane(GNELane* lane, const GNETagPropertie
         // Get attribute lane
         myBaseAdditional->addStringAttribute(SUMO_ATTR_LANE, lane->getID());
         // Check if ID has to be generated
-        if (tagProperties.hasAttribute(SUMO_ATTR_ID)) {
+        if (tagProperties.hasAttribute(SUMO_ATTR_ID) && !myBaseAdditional->hasStringAttribute(SUMO_ATTR_ID)) {
             myBaseAdditional->addStringAttribute(SUMO_ATTR_ID, myViewNet->getNet()->getAttributeCarriers()->generateAdditionalID(tagProperties.getTag()));
         }
     } else {

commit c47c426e5306f4c6107bba0156d65fa13c83f423

Fixed bug introduced in 476295c9dcb941426a7b08877e6dee000969c11d. Refs #457

src/netedit/elements/additional/GNEAdditional.cpp
@@ -559,6 +559,8 @@ GNEAdditional::setAdditionalID(const std::string& newID) {
     // update ID
     if (isTemplate()) {
         setMicrosimID(newID);
+    } else if ((myTagProperty.getTag() == SUMO_TAG_VAPORIZER) || !myTagProperty.hasAttribute(SUMO_ATTR_ID)) {
+        setMicrosimID(newID);
     } else {
         myNet->getAttributeCarriers()->updateAdditionalID(this, newID);
     }

src/netedit/elements/demand/GNEDemandElement.cpp
@@ -407,7 +407,7 @@ GNEDemandElement::isValidDemandElementID(const std::vector<SumoXMLTag>& tags, co
 void
 GNEDemandElement::setDemandElementID(const std::string& newID) {
     // update ID
-    if (isTemplate()) {
+    if (isTemplate() || !myTagProperty.hasAttribute(SUMO_ATTR_ID)) {
         setMicrosimID(newID);
     } else {
         myNet->getAttributeCarriers()->updateDemandElementID(this, newID);