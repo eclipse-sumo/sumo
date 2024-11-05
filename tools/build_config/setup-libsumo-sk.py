1b6f7237d503b89c12cff041ae1be6a4f72a8191
Jakob Erdmann <jakob.erdmann@dlr.de>
2024-11-05 07:16:35 +0100

    fixing style refs #12

--- tools/build_config/setup-libsumo-sk.py
+++ tools/build_config/setup-libsumo-sk.py
@@ -11,7 +11,7 @@
 # https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 
-# @file    setup-libsumo.py
+# @file    setup-libsumo-sk.py
 # @author  Benjamin Striner
 # @author  Michael Behrisch
 # @date    2017-01-26
