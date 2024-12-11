8d731ba6ecccea1609a9d92958376b70cf55a259
Michael Behrisch <michael.behrisch@dlr.de>
2024-12-11 22:12:03 +0100

    adding license header #22

--- tests/netedit/scripts/copyElements.py
+++ tests/netedit/scripts/copyElements.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2024-2024 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    copyElements.py
+# @author  Pablo Alvarez Lopez
+# @date    2024-12-09
+
 import os
 import shutil
 
