eclipse-sumo/sumo

commit 1f1a74a195d7008fb7209faaac6e608428e0141c

attribute vClass of vTypes cannot be edited with Dialog. Refs #457

src/netedit/frames/GNEAttributesEditorRow.cpp
@@ -197,7 +197,7 @@ GNEAttributesEditorRow::showAttributeRow(const GNEAttributeProperties& attrPrope
     // continue depending of type of attribute
     if (attrProperty.isBool()) {
         showValueCheckButton(value, attributeEnabled, computedAttribute);
-    } else if (!attrProperty.isVClass() && (attrProperty.isDiscrete() || attrProperty.isVType())) {
+    } else if (attrProperty.isDiscrete()) {
         showValueComboBox(attrProperty, value, attributeEnabled, computedAttribute);
     } else {
         showValueString(value, attributeEnabled, computedAttribute);

src/netedit/elements/GNEAttributeCarrier.cpp
@@ -3831,7 +3831,7 @@ GNEAttributeCarrier::fillDemandElements() {
         myTagProperties[currentTag].addAttribute(attrProperty);
 
         attrProperty = GNEAttributeProperties(SUMO_ATTR_VCLASS,
-                                              GNEAttributeProperties::VCLASS | GNEAttributeProperties::DISCRETE | GNEAttributeProperties::DEFAULTVALUE,
+                                              GNEAttributeProperties::DISCRETE | GNEAttributeProperties::DEFAULTVALUE,
                                               TL("An abstract vehicle class"),
                                               "passenger");
         attrProperty.setDiscreteValues(SumoVehicleClassStrings.getStrings());

commit 9160afd48ee65a7c556d975a5f004ceb2e69de8e

Now vType can be edited thought a comboBox (Broken in 1f1a74a195d7008fb7209faaac6e608428e0141c). Refs #457

src/netedit/frames/GNEAttributesEditorRow.cpp
@@ -197,7 +197,7 @@ GNEAttributesEditorRow::showAttributeRow(const GNEAttributeProperties& attrPrope
     // continue depending of type of attribute
     if (attrProperty.isBool()) {
         showValueCheckButton(value, attributeEnabled, computedAttribute);
-    } else if (attrProperty.isDiscrete()) {
+    } else if (attrProperty.isDiscrete() || attrProperty.isVType()) {
         showValueComboBox(attrProperty, value, attributeEnabled, computedAttribute);
     } else {
         showValueString(value, attributeEnabled, computedAttribute);