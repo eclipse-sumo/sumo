9ba919590f6c5998b403cc5a52bb1a70355d8e68
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-01-14 15:39:13 +0100

    fixed minor error writing period attribute. Refs #457

--- src/netedit/elements/additional/GNEDetector.cpp
+++ src/netedit/elements/additional/GNEDetector.cpp
@@ -324,7 +324,7 @@ GNEDetector::isDetectorValid(SumoXMLAttr key, const std::string& value) {
 
 void
 GNEDetector::writeDetectorValues(OutputDevice& device) const {
-    if (myPeriod > 0) {
+    if (myPeriod != SUMOTime_MAX_PERIOD) {
         device.writeAttr(SUMO_ATTR_PERIOD, time2string(myPeriod));
     }
     if (myAdditionalName.size() > 0) {
