eclipse-sumo/sumo

commit 9ba919590f6c5998b403cc5a52bb1a70355d8e68

fixed minor error writing period attribute. Refs #457

src/netedit/elements/additional/GNEDetector.cpp
@@ -324,7 +324,7 @@ GNEDetector::isDetectorValid(SumoXMLAttr key, const std::string& value) {
 
 void
 GNEDetector::writeDetectorValues(OutputDevice& device) const {
-    if (myPeriod > 0) {
+    if (myPeriod != SUMOTime_MAX_PERIOD) {
         device.writeAttr(SUMO_ATTR_PERIOD, time2string(myPeriod));
     }
     if (myAdditionalName.size() > 0) {

commit 0676686bde3db6433d10af27861ade008a489072

Fixed error writing period introduced in 9ba919590f6c5998b403cc5a52bb1a70355d8e68. Refs #457

src/netedit/elements/additional/GNEDetector.cpp
@@ -324,7 +324,7 @@ GNEDetector::isDetectorValid(SumoXMLAttr key, const std::string& value) {
 
 void
 GNEDetector::writeDetectorValues(OutputDevice& device) const {
-    if (myPeriod != SUMOTime_MAX_PERIOD) {
+    if ((myPeriod > 0) && (myPeriod != SUMOTime_MAX_PERIOD)) {
         device.writeAttr(SUMO_ATTR_PERIOD, time2string(myPeriod));
     }
     if (myAdditionalName.size() > 0) {