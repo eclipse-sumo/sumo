3ee7a6d00329770726ba7385f66937e8538144f0
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-01-21 14:16:24 +0100

    Added GNEHierarchicalContainerChildrenHash.h. Refs #16039

---
+++ src/netedit/elements/GNEHierarchicalContainerChildrenHash.h
@@ -0,0 +1,41 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEHierarchicalContainerChildrenHash.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Jan 2025
+///
+// Template container for GNEHierarchical elements
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <unordered_set>
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+template <typename T> 
+class GNEHierarchicalContainerChildrenHash : public std::unordered_set<T> {
+
+public:
+    /// @brief default constructor
+    GNEHierarchicalContainerChildren() {
+    }
+
+    /// @brief parameter constructor
+    GNEHierarchicalContainerChildren(const std::unordered_set<T>& elements) :
+        std::unordered_set<T>(elements) {
+    }
+};
