eclipse-sumo/sumo

commit dfd8508824011250c90131b0684ad4b88130e115

Now getFilename2Write uses FileExtensions. Refs #16228

src/utils/gui/div/GUIDialog_GLChosenEditor.cpp
@@ -151,7 +151,9 @@ GUIDialog_GLChosenEditor::onCmdLoad(FXObject*, FXSelector, void*) {
 
 long
 GUIDialog_GLChosenEditor::onCmdSave(FXObject*, FXSelector, void*) {
-    FXString file = MFXUtils::getFilename2Write(this, TL("Save List of selected Items"), ".txt", GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
+    FXString file = MFXUtils::getFilename2Write(this, TL("Save List of selected Items"),
+                    SUMOXMLDefinitions::XMLFileExtensions.getMultilineString().c_str(),
+                    GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
     if (file == "") {
         return 1;
     }

src/gui/dialogs/GUIDialog_Breakpoints.cpp
@@ -162,7 +162,9 @@ GUIDialog_Breakpoints::onCmdLoad(FXObject*, FXSelector, void*) {
 
 long
 GUIDialog_Breakpoints::onCmdSave(FXObject*, FXSelector, void*) {
-    FXString file = MFXUtils::getFilename2Write(this, TL("Save Breakpoints"), ".txt", GUIIconSubSys::getIcon(GUIIcon::EMPTY), gCurrentFolder);
+    FXString file = MFXUtils::getFilename2Write(this, TL("Save Breakpoints"),
+                    SUMOXMLDefinitions::TXTFileExtensions.getMultilineString().c_str(),
+                    GUIIconSubSys::getIcon(GUIIcon::EMPTY), gCurrentFolder);
     if (file == "") {
         return 1;
     }

src/netedit/dialogs/GNEFixDemandElements.cpp
@@ -198,8 +198,8 @@ GNEFixDemandElements::FixOptions::setInvalidElements(const std::vector<GNEDemand
 
 bool
 GNEFixDemandElements::FixOptions::saveContents() const {
-    const FXString file = MFXUtils::getFilename2Write(myTable,
-                          TL("Save list of conflicted items"), ".txt",
+    const FXString file = MFXUtils::getFilename2Write(myTable, TL("Save list of conflicted items"),
+                          SUMOXMLDefinitions::TXTFileExtensions.getMultilineString().c_str(),
                           GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
     if (file == "") {
         return false;

src/netedit/dialogs/GNEFixNetworkElements.cpp
@@ -186,7 +186,8 @@ GNEFixNetworkElements::FixOptions::setInvalidElements(const std::vector<GNENetwo
 bool
 GNEFixNetworkElements::FixOptions::saveContents() const {
     const FXString file = MFXUtils::getFilename2Write(myTable,
-                          TL("Save list of conflicted items"), ".txt",
+                          TL("Save list of conflicted items"),
+                          SUMOXMLDefinitions::TXTFileExtensions.getMultilineString().c_str(),
                           GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
     if (file == "") {
         return false;

src/netedit/dialogs/GNEMultipleParametersDialog.cpp
@@ -324,7 +324,8 @@ long
 GNEMultipleParametersDialog::ParametersOperations::onCmdSaveParameters(FXObject*, FXSelector, void*) {
     // obtain file to save parameters
     FXString file = MFXUtils::getFilename2Write(this,
-                    TL("Save Parameter Template file"), ".xml",
+                    TL("Save Parameter Template file"),
+                    SUMOXMLDefinitions::XMLFileExtensions.getMultilineString().c_str(),
                     GUIIconSubSys::getIcon(GUIIcon::GREENVEHICLE),
                     gCurrentFolder);
     if (file == "") {

src/netedit/dialogs/GNESingleParametersDialog.cpp
@@ -323,7 +323,8 @@ long
 GNESingleParametersDialog::ParametersOperations::onCmdSaveParameters(FXObject*, FXSelector, void*) {
     // obtain file to save parameters
     FXString file = MFXUtils::getFilename2Write(this,
-                    TL("Save Parameter Template file"), ".xml",
+                    TL("Save Parameter Template file"),
+                    SUMOXMLDefinitions::XMLFileExtensions.getMultilineString().c_str(),
                     GUIIconSubSys::getIcon(GUIIcon::GREENVEHICLE),
                     gCurrentFolder);
     if (file == "") {

src/netedit/dialogs/GNEVTypeDistributionsDialog.cpp
@@ -315,10 +315,9 @@ GNEVTypeDistributionsDialog::ParametersOperations::onCmdLoadParameters(FXObject*
 long
 GNEVTypeDistributionsDialog::ParametersOperations::onCmdSaveParameters(FXObject*, FXSelector, void*) {
     // obtain file to save parameters
-    FXString file = MFXUtils::getFilename2Write(this,
-                    TL("Save Parameter Template file"), ".xml",
-                    GUIIconSubSys::getIcon(GUIIcon::GREENVEHICLE),
-                    gCurrentFolder);
+    FXString file = MFXUtils::getFilename2Write(this, TL("Save Parameter Template file"),
+                    SUMOXMLDefinitions::XMLFileExtensions.getMultilineString().c_str(),
+                    GUIIconSubSys::getIcon(GUIIcon::GREENVEHICLE), gCurrentFolder);
     if (file == "") {
         // None parameter file was selected, then stop function
         return 1;

src/netedit/frames/common/GNESelectorFrame.cpp
@@ -338,8 +338,8 @@ GNESelectorFrame::SelectionOperation::onCmdLoad(FXObject*, FXSelector, void*) {
 
 long
 GNESelectorFrame::SelectionOperation::onCmdSave(FXObject*, FXSelector, void*) {
-    FXString file = MFXUtils::getFilename2Write(this,
-                    TL("Save List of selected Items"), ".txt",
+    FXString file = MFXUtils::getFilename2Write(this, TL("Save List of selected Items"),
+                    SUMOXMLDefinitions::TXTFileExtensions.getMultilineString().c_str(),
                     GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
     if (file == "") {
         return 1;

commit 6f1a511205a35a582423574cc19911b5a7689469

fixed default file extension for object selection (broken in dfd8508824011250c90131b0684ad4b88130e115) refs #16228

src/utils/gui/div/GUIDialog_GLChosenEditor.cpp
@@ -152,7 +152,7 @@ GUIDialog_GLChosenEditor::onCmdLoad(FXObject*, FXSelector, void*) {
 long
 GUIDialog_GLChosenEditor::onCmdSave(FXObject*, FXSelector, void*) {
     FXString file = MFXUtils::getFilename2Write(this, TL("Save List of selected Items"),
-                    SUMOXMLDefinitions::XMLFileExtensions.getMultilineString().c_str(),
+                    SUMOXMLDefinitions::TXTFileExtensionValues.getMultilineString().c_str(),
                     GUIIconSubSys::getIcon(GUIIcon::SAVE), gCurrentFolder);
     if (file == "") {
         return 1;