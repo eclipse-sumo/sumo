bbb14a2dc65ba68886a9afa59fccee915d161abf
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-03-18 17:42:32 +0100

    refactoring refs #12

--- src/netbuild/NBNodeCont.cpp
+++ src/netbuild/NBNodeCont.cpp
@@ -1587,18 +1587,18 @@ NBNodeCont::feasibleCluster(const NodeSet& cluster, const std::map<const NBNode*
         }
     }
     // prevent joining of simple merging/spreading structures
-    if (!hasTLS && cluster.size() >= 2) {
+    if (cluster.size() >= 2) {
         int entryNodes = 0;
         int exitNodes = 0;
-        int outsideIncoming = 0;
-        int outsideOutgoing = 0;
+        EdgeVector outsideIncoming;
+        EdgeVector outsideOutgoing;
         int edgesWithin = 0;
         for (NBNode* n : cluster) {
             bool foundOutsideIncoming = false;
             for (NBEdge* e : n->getIncomingEdges()) {
                 if (cluster.count(e->getFromNode()) == 0) {
                     // edge entering from outside the cluster
-                    outsideIncoming++;
+                    outsideIncoming.push_back(e);
                     foundOutsideIncoming = true;
                 } else {
                     edgesWithin++;
@@ -1611,7 +1611,7 @@ NBNodeCont::feasibleCluster(const NodeSet& cluster, const std::map<const NBNode*
             for (NBEdge* e : n->getOutgoingEdges()) {
                 if (cluster.count(e->getToNode()) == 0) {
                     // edge leaving cluster
-                    outsideOutgoing++;
+                    outsideOutgoing.push_back(e);
                     foundOutsideOutgoing = true;
                 }
             }
@@ -1619,18 +1619,20 @@ NBNodeCont::feasibleCluster(const NodeSet& cluster, const std::map<const NBNode*
                 exitNodes++;
             }
         }
-        if (entryNodes < 2) {
-            reason = "only 1 entry node";
-            return false;
-        }
-        if (exitNodes < 2) {
-            reason = "only 1 exit node";
-            return false;
-        }
-        if (cluster.size() == 2) {
-            if (edgesWithin == 1 && outsideIncoming < 3 && outsideOutgoing < 3) {
-                reason = "only 1 edge within and no cross-traffic";
+        if (!hasTLS) {
+            if (entryNodes < 2) {
+                reason = "only 1 entry node";
                 return false;
+            }
+            if (exitNodes < 2) {
+                reason = "only 1 exit node";
+                return false;
+            }
+            if (cluster.size() == 2) {
+                if (edgesWithin == 1 && outsideIncoming.size() < 3 && outsideOutgoing.size() < 3) {
+                    reason = "only 1 edge within and no cross-traffic";
+                    return false;
+                }
             }
         }
     }
