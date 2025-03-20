9486b2a00521ceca434fbadeada7a2a151741d79
m-kro <m.barthauer@t-online.de>
2025-03-20 14:30:06 +0100

    clean shader ref #10280
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- data/shaders/fragmentShader.glsl
+++ data/shaders/fragmentShader.glsl
@@ -4,14 +4,5 @@ out vec4 vFragColor;
 
 void main()
 {
-    //float blackWhite = mod(gl_FragCoord.y, 300);
-    //if ( blackWhite < 25. )
-    //{
-    //    vFragColor = vec4(1.,0.,0.,1.);
-    //}
-    //else
-    //{
-    //    vFragColor = vec4(1.,0.,0.,1.);
-    //}
     vFragColor = vSmoothColor;
 }
