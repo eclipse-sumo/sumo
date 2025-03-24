f8e84e62489757ae524f30eb98c3736140747f9b
m-kro <m.barthauer@t-online.de>
2025-03-24 14:57:43 +0100

    change shader input data type ref #10280
    Signed-off-by: m-kro <m.barthauer@t-online.de>

--- data/shaders/vertexShader.glsl
+++ data/shaders/vertexShader.glsl
@@ -1,12 +1,12 @@
 #version 330 compatibility
 layout(location = 0) in vec3 vVertex;
-layout(location = 1) in uvec4 vColor;
+layout(location = 1) in vec4 vColor;
 out vec4 vSmoothColor;
 
 uniform mat4 u_MVP;
 
 void main()
 {
-    vSmoothColor = vec4(vColor) / 255.;
+    vSmoothColor = vColor;
     gl_Position = u_MVP * vec4(vVertex, 1.0);
 }
