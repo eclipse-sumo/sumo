d666885854bf5878f4d16793e641933ec40c9e22
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-04-29 10:20:47 +0200

    Added GNETestThread. Refs #16604

---
+++ src/netedit/GNETestThread.h
@@ -0,0 +1,78 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNETestThread.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Mar 2025
+///
+// Thread used for testing netedit
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <utils/common/MsgHandler.h>
+#include <utils/foxtools/MFXSingleEventThread.h>
+#include <utils/foxtools/MFXSynchQue.h>
+#include <utils/foxtools/MFXInterThreadEventClient.h>
+
+
+// ===========================================================================
+// class declarations
+// ===========================================================================
+
+class GNENet;
+class GUIEvent;
+class GNEApplicationWindow;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/**
+ * @class GNETestThread
+ */
+class GNETestThread : protected MFXSingleEventThread {
+
+public:
+    /// @brief constructor
+    GNETestThread(GNEApplicationWindow* applicationWindow, MFXSynchQue<GUIEvent*>& eq, FXEX::MFXThreadEvent& ev);
+
+    /// @brief destructor
+    virtual ~GNETestThread();
+
+    /// @brief starts the thread. The thread ends after the net has been loaded
+    FXint run();
+
+    /// @brief run test
+    void startTest();
+
+    /// @brief Retrieves messages from the loading module
+    void retrieveMessage(const MsgHandler::MsgType type, const std::string& msg);
+
+private:
+    /**@brief Closes the loading process
+     *
+     * This method is called both on success and failure.
+     * All message callbacks to this instance are removed and the parent
+     * application is informed about the loading
+     */
+    void submitEndAndCleanup(GNENet* net, const std::string& loadedFile, const std::string& guiSettingsFile = "", const bool viewportFromRegistry = false);
+
+    /// @brief netedit application windows
+    GNEApplicationWindow* myApplicationWindow;
+
+    /// @brief event Queue
+    MFXSynchQue<GUIEvent*>& myEventQueue;
+
+    /// @brief event throw
+    FXEX::MFXThreadEvent& myEventThrow;
+};
