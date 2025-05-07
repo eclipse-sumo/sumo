ff5122b269cbd4894d8306651495df5d678ebb23
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-05-07 16:50:21 +0200

    Updated netedit tests. Refs #3045

--- tests/netedit/attributesEnum.py
+++ tests/netedit/attributesEnum.py
@@ -336,14 +336,14 @@ class edge:
 class lane:
     class create:
         add = 24
-        remove = 25
-        speed = 26
-        allowButton = 27
-        allow = 28
-        disallow = 29
-        width = 30
-        parameters = 32
-        parametersButton = 33
+        remove = 24
+        speed = 25
+        allowButton = 26
+        allow = 27
+        disallow = 282
+        width = 29
+        parameters = 31
+        parametersButton = 32
 
     class inspect:
         speed = 1
