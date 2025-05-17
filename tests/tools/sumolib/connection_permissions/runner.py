7d20bcd9b338b9fd888c61022b0042bbdb2bf96c
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-05-17 10:18:41 +0200

    added test refs #21, #16633

---
+++ tests/tools/sumolib/connection_permissions/runner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2008-2025 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Jakob Erdmann
+# @date
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+import sumolib  # noqa
+
+net = sumolib.net.readNet(sys.argv[1])
+E0 = net.getEdge("E0")
+for vClass in ['passenger', 'bus', 'ignoring']:
+    print("outgoing for %s:" % vClass, [e.getID() for e in E0.getAllowedOutgoing(vClass)])
