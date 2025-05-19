0253b42527326bf84c1321480382223a5aeaeefd
Michael Behrisch <michael.behrisch@dlr.de>
2025-05-19 15:33:54 +0200

    updating tls representation to contain earlyTarget and conditions #15230 #15226

--- tools/sumolib/net/__init__.py
+++ tools/sumolib/net/__init__.py
@@ -103,7 +103,7 @@ class TLS:
 
 class Phase:
 
-    def __init__(self, duration, state, minDur=None, maxDur=None, next=tuple(), name=""):
+    def __init__(self, duration, state, minDur=None, maxDur=None, next=tuple(), name="", earlyTarget=""):
         """
         Constructs a traffic light phase
         duration (float): the duration of the phase in seconds
@@ -112,6 +112,7 @@ class Phase:
         maxDur (float): the maximum duration (ignored by static tls)
         next (intList): possible succesor phase (optional)
         name (string): the name of the phase
+        earlyTarget (string): early switching to phase with the given index(es)
         """
         self.duration = duration
         self.state = state
@@ -120,12 +121,14 @@ class Phase:
         self.maxDur = maxDur if maxDur is not None else duration
         self.next = next
         self.name = name
+        self.earlyTarget = earlyTarget
 
     def __repr__(self):
         name = (", name='%s'" % self.name) if self.name else ""
         next = (", next='%s'" % str(self.next)) if self.next else ""
-        return ("Phase(duration=%s, state='%s', minDur=%s, maxDur=%s%s%s)" %
-                (self.duration, self.state, self.minDur, self.maxDur, name, next))
+        earlyTarget = (", earlyTarget='%s'" % self.earlyTarget) if self.earlyTarget else ""
+        return ("Phase(duration=%s, state='%s', minDur=%s, maxDur=%s%s%s%s)"
+                % (self.duration, self.state, self.minDur, self.maxDur, name, next, earlyTarget))
 
 
 class TLSProgram:
@@ -136,9 +139,13 @@ class TLSProgram:
         self._offset = offset
         self._phases = []
         self._params = {}
+        self._conditions = {}
 
-    def addPhase(self, state, duration, minDur=-1, maxDur=-1, next=None, name=""):
-        self._phases.append(Phase(duration, state, minDur, maxDur, next, name))
+    def addPhase(self, state, duration, minDur=-1, maxDur=-1, next=None, name="", earlyTarget=""):
+        self._phases.append(Phase(duration, state, minDur, maxDur, next, name, earlyTarget))
+
+    def addCondition(self, id, value):
+        self._conditions[id] = value
 
     def toXML(self, tlsID):
         ret = '  <tlLogic id="%s" type="%s" programID="%s" offset="%s">\n' % (
@@ -148,10 +155,13 @@ class TLSProgram:
             maxDur = '' if p.maxDur < 0 else ' maxDur="%s"' % p.maxDur
             name = '' if p.name == '' else ' name="%s"' % p.name
             next = '' if len(p.next) == 0 else ' next="%s"' % ' '.join(map(str, p.next))
-            ret += '    <phase duration="%s" state="%s"%s%s%s%s/>\n' % (
-                p.duration, p.state, minDur, maxDur, name, next)
+            earlyTarget = '' if p.earlyTarget == '' else ' earlyTarget="%s"' % p.earlyTarget
+            ret += '    <phase duration="%s" state="%s"%s%s%s%s%s/>\n' % (
+                p.duration, p.state, minDur, maxDur, name, next, earlyTarget)
         for k, v in self._params.items():
             ret += '    <param key="%s" value="%s"/>\n' % (k, v)
+        for i, v in self._conditions.items():
+            ret += '    <condition id="%s" value="%s"/>\n' % (i, v)
         ret += '  </tlLogic>\n'
         return ret
 
@@ -169,6 +179,17 @@ class TLSProgram:
 
     def getParams(self):
         return self._params
+
+    def getStages(self):
+        stages = dict()
+        for idx, phase in enumerate(self.getPhases()):
+            if phase not in stages.values():
+                if 'G' in phase.state and 'y' not in phase.state and phase.name:
+                    stages[idx] = phase
+        return stages
+
+    def getOffset(self):
+        return self._offset
 
 
 class EdgeType:
