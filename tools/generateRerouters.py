54d175d4c262b7d81b2c018ba8d2e9d8787b5ffb
Davide Andrea Guastella <davide.adrea.guastella@ulb.be>
2025-06-17 09:24:06 +0200

    fixed unclosed element in closingReroute XML

--- tools/generateRerouters.py
+++ tools/generateRerouters.py
@@ -104,11 +104,11 @@ def main(options):
             sys.exit("Unknown closed edge '%s'" % closedID)
         closedEdges.append(net.getEdge(closedID))
 
-    allowDisallow = ""
+    allowDisallow = "/"
     if options.disallow is not None:
-        allowDisallow = ' disallow="%s"' % options.disallow
+        allowDisallow = ' disallow="%s"/' % options.disallow
     elif options.allow != "":
-        allowDisallow = ' allow="%s"' % options.allow
+        allowDisallow = ' allow="%s"/' % options.allow
 
     with open(options.outfile, 'w') as outf:
         sumolib.writeXMLHeader(outf, "$Id$", "additional", options=options)
