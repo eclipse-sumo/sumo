d895b9946ed525f9010a4fe8ce8f4c8b4bb69842
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-06-30 08:48:04 +0200

    style refs #12

--- tools/neteditTestFunctions/buildinternaltestdata.py
+++ tools/neteditTestFunctions/buildinternaltestdata.py
@@ -11,7 +11,7 @@
 # https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 
-# @file    internaltestdata.py
+# @file    buildinternaltestdata.py
 # @author  Pablo Alvarez Lopez
 # @date    May 2025
 
