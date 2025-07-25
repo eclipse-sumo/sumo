e71bc95e287195fe416ec2e50fe26b9cfedb1eea
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-07-25 10:09:25 +0200

    Added GNEQuestionBasicDialog. Refs #16790

---
+++ src/netedit/dialogs/basic/GNEQuestionBasicDialog.h
@@ -0,0 +1,42 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2006-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEQuestionBasicDialog.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Jul 2025
+///
+// Custom FXDialogBox question dialog used in Netedit that supports internal tests
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include "GNEBasicDialog.h"
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNEQuestionBasicDialog : public GNEBasicDialog {
+
+public:
+    /// @brief constructor
+    GNEQuestionBasicDialog(GNEApplicationWindow* applicationWindow, GNEBasicDialog::Buttons buttons,
+                           const std::string& name, const std::string& info);
+
+private:
+    /// @brief Invalidated copy constructor.
+    GNEQuestionBasicDialog(const GNEQuestionBasicDialog&) = delete;
+
+    /// @brief Invalidated assignment operator
+    GNEQuestionBasicDialog& operator=(const GNEQuestionBasicDialog& src) = delete;
+};
