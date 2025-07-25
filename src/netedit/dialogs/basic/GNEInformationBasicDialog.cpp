f997ffa9831815bedb76e72c019991d397f31984
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-07-25 10:53:40 +0200

    Added GNEInformationBasicDialog. Refs #16790

---
+++ src/netedit/dialogs/basic/GNEInformationBasicDialog.cpp
@@ -0,0 +1,36 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2006-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEInformationBasicDialog.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Jul 2025
+///
+// Custom FXDialogBox information dialog used in Netedit that supports internal tests
+/****************************************************************************/
+
+#include "GNEInformationBasicDialog.h"
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+
+GNEInformationBasicDialog::GNEInformationBasicDialog(GNEApplicationWindow* applicationWindow,
+                                         const std::string& name, const std::string& info) :
+    GNEBasicDialog(applicationWindow, name, info, GNEBasicDialog::Buttons::OK, GUIIcon::INFORMATION) {
+}
+
+
+GNEInformationBasicDialog::GNEInformationBasicDialog(GNEApplicationWindow* applicationWindow,
+                                         const std::string& name, const std::vector<std::string> &infoLines) :
+    GNEBasicDialog(applicationWindow, name, infoLines, GNEBasicDialog::Buttons::OK, GUIIcon::INFORMATION) {
+}
