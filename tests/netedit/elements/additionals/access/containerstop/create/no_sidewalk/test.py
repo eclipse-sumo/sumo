6dd7e85fc08de4b2242e834d54922e2479e04676
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-09-17 10:51:08 +0200

    Added more access tests. Refs #17085 and #3045

---
+++ tests/netedit/elements/additionals/access/containerstop/create/no_sidewalk/test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2009-2025 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    test.py
+# @author  Pablo Alvarez Lopez
+# @date    2016-11-25
+
+# import common functions for netedit tests
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME", "."), "tools"))
+import neteditTestFunctions as netedit  # noqa
+
+# Open netedit
+neteditProcess, referencePosition = netedit.setupAndStart()
+
+# go to additional mode
+netedit.changeMode("additional")
+
+# select ContainerStop
+netedit.changeElement("additionalFrame", "containerStop")
+
+# create ContainerStop with default parameters
+netedit.leftClick(referencePosition, netedit.positions.elements.edgeCenter1)
+
+# select detector
+netedit.changeElement("additionalFrame", "access")
+
+# Create detector
+netedit.selectAdditionalChild(netedit.attrs.access.create.parent, 0)
+
+# Try to create access in extern edge
+netedit.leftClick(referencePosition, netedit.positions.externLaneTop)
+
+# Check undo redo
+netedit.checkUndoRedo(referencePosition)
+
+# save netedit config
+netedit.saveExistentFile("neteditConfig")
+
+# quit netedit
+netedit.quit(neteditProcess)
