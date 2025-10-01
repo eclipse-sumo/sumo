6a098d62d3dea1fb7381f99fc648946741b7fb9f
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-10-01 13:08:20 +0200

    Added GNESaveDialog. Refs #17143

---
+++ src/netedit/dialogs/GNESaveDialog.cpp
@@ -0,0 +1,56 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNESaveDialog.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2025
+///
+// Dialog used for saving elements
+/****************************************************************************/
+#include <config.h>
+
+#include <utils/gui/images/GUIIconSubSys.h>
+
+#include "GNESaveDialog.h"
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+
+GNESaveDialog::GNESaveDialog(GNEApplicationWindow* applicationWindow, const std::string& title,
+                               const std::string& info, GUIIcon titleIcon, DialogType type,
+                               GNEDialog::Buttons buttons, GUIIcon largeIcon) :
+    GNEDialog(applicationWindow, title.c_str(), titleIcon, type, buttons, OpenType::MODAL, ResizeMode::STATIC) {
+    // create dialog layout (obtained from FXMessageBox)
+    auto infoFrame = new FXVerticalFrame(myContentFrame, LAYOUT_TOP | LAYOUT_LEFT | LAYOUT_FILL_X | LAYOUT_FILL_Y, 0, 0, 0, 0, 10, 10, 10, 10);
+    // add icon label (only if large icon is defined)
+    if (largeIcon != GUIIcon::EMPTY) {
+        new FXLabel(infoFrame, FXString::null, GUIIconSubSys::getIcon(largeIcon), ICON_BEFORE_TEXT | LAYOUT_TOP | LAYOUT_LEFT | LAYOUT_FILL_X | LAYOUT_FILL_Y);
+    }
+    // add information label
+    new FXLabel(infoFrame, info.c_str(), NULL, JUSTIFY_LEFT | ICON_BEFORE_TEXT | LAYOUT_TOP | LAYOUT_LEFT | LAYOUT_FILL_X | LAYOUT_FILL_Y);
+    // open modal dialog
+    openDialog();
+}
+
+
+GNESaveDialog::~GNESaveDialog() {
+}
+
+
+void
+GNESaveDialog::runInternalTest(const InternalTestStep::DialogArgument* /*dialogArgument*/) {
+    // nothing to do
+}
+
+/****************************************************************************/
---
+++ src/netedit/dialogs/GNESaveDialog.h
@@ -0,0 +1,49 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNESaveDialog.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2025
+///
+// Dialog used for saving elements
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <netedit/dialogs/GNEDialog.h>
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNESaveDialog : public GNEDialog {
+
+public:
+    /// @brief Constructor
+    GNESaveDialog(GNEApplicationWindow* applicationWindow, const std::string& title,
+                   const std::string& info, GUIIcon titleIcon, DialogType type,
+                   GNEDialog::Buttons buttons, GUIIcon largeIcon);
+
+    /// @brief Destructor
+    ~GNESaveDialog();
+
+    /// @brief run internal test
+    void runInternalTest(const InternalTestStep::DialogArgument* dialogArgument);
+
+private:
+    /// @brief Invalidated copy constructor.
+    GNESaveDialog(const GNESaveDialog&) = delete;
+
+    /// @brief Invalidated assignment operator
+    GNESaveDialog& operator=(const GNESaveDialog& src) = delete;
+};
