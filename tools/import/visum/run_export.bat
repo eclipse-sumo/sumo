61216854d2dae710ee32414e66643c8010eaaee6
Michael Behrisch <michael.behrisch@dlr.de>
2025-10-05 09:45:45 +0200

    visum export tooling #17164

---
+++ tools/import/visum/run_export.bat
@@ -0,0 +1,46 @@
+@echo off
+REM Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+REM Copyright (C) 2008-2025 German Aerospace Center (DLR) and others.
+REM This program and the accompanying materials are made available under the
+REM terms of the Eclipse Public License 2.0 which is available at
+REM https://www.eclipse.org/legal/epl-2.0/
+REM This Source Code may also be made available under the following Secondary
+REM Licenses when the conditions for such availability set forth in the Eclipse
+REM Public License 2.0 are satisfied: GNU General Public License, version 2
+REM or later which is available at
+REM https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+REM SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+REM Batch launcher for Visum XML export
+REM Tries Visum Python, fallback to any Python with pywin32
+
+set PYTHON_EXE=
+
+REM Scan Program Files
+for /d %%d in ("C:\Program Files\PTV Vision\PTV Visum*" "C:\Program Files (x86)\PTV Vision\PTV Visum*") do (
+    if exist %%d\Exe\Python\python.exe (
+        set PYTHON_EXE=%%d\Exe\Python\python.exe
+        goto :found_python
+    )
+)
+
+REM Fallback to any python in PATH
+echo Visum Python not found, trying Python from PATH...
+for %%p in (py,python) do (
+    echo %%p -c "import win32com.client"
+    %%p -c "import win32com.client"
+    if %errorlevel%==0 (
+        set PYTHON_EXE=%%p
+        goto :found_python
+    )
+)
+
+REM Fail if no suitable Python found
+if not defined PYTHON_EXE (
+    echo Could not find a Python with pywin32 installed!
+    pause
+    exit /b 1
+)
+
+:found_python
+"%PYTHON_EXE%" %~dp0\visum_export.py %*
---
+++ tools/import/visum/visum_export.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2009-2025 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    visum_export.py
+# @author  Michael Behrisch
+# @date    2025-10-04
+
+import glob
+import os
+import sys
+import zipfile
+
+import win32com.client
+
+
+def main(ver_file, zip_file, xml=True):
+    out_dir = os.path.dirname(zip_file)
+    if out_dir:
+        os.makedirs(out_dir, exist_ok=True)
+    out_dir = os.path.abspath(out_dir)
+
+    visum = win32com.client.Dispatch("Visum.Visum")
+    visum.LoadVersion(ver_file)
+
+    base_name = os.path.splitext(os.path.basename(ver_file))[0]
+    if xml:
+        network = os.path.join(out_dir, base_name + "_anm_network.xml")
+        routes = os.path.join(out_dir, base_name + "_anm_matrices.xml")
+        matrices = os.path.join(out_dir, base_name + "_anm_routes.xml")
+        for f in (network, routes, matrices):
+            if os.path.exists(f):
+                os.remove(f)
+        visum.ExportAnmNet(network, "")
+        visum.ExportAnmRoutes(routes, "", True, False)
+        visum.ExportAnmRoutes(matrices, "", False, True)
+        os.rename(network + ".anm", network)
+        os.rename(routes + ".anmRoutes", routes)
+        os.rename(matrices + ".anmRoutes", matrices)
+    else:
+        network = os.path.join(out_dir, base_name + ".net")
+        matrices = os.path.join(out_dir, base_name + ".dmd")
+        routes = ""
+        visum.SaveNet(network)
+        visum.SaveDemandFile(matrices, True)
+
+    with zipfile.ZipFile(zip_file, "w", compression=zipfile.ZIP_DEFLATED) as zipf:
+        for f in [network, matrices, routes]:
+            if os.path.isfile(f):
+                zipf.write(f, os.path.basename(f))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        ver_file = sys.argv[1]
+    else:
+        ver_files = glob.glob("*.ver")
+        if not ver_files:
+            sys.exit("No .ver file found in current folder!")
+        ver_file = ver_files[0]
+    if len(sys.argv) > 2:
+        zip_file = sys.argv[2]
+        main(ver_file, zip_file)
+    else:
+        # Default: generate two zip files
+        zip_file = os.path.splitext(ver_file)[0] + "_anm.zip"
+        main(ver_file, zip_file, True)
+        zip_file = os.path.splitext(ver_file)[0] + ".zip"
+        main(ver_file, zip_file, False)
