6080cedd81f40ff9a30814c91a07e1d531811699
Michael Behrisch <michael.behrisch@dlr.de>
2025-10-07 12:34:01 +0200

    fixing directories and file names, speeding up double export #17164

--- tools/import/visum/visum_export.py
+++ tools/import/visum/visum_export.py
@@ -24,24 +24,27 @@ import win32com.client
 from pywintypes import com_error
 
 
-def main(ver_file, zip_file, xml=True):
+def load(ver_file):
+    try:
+        visum = win32com.client.Dispatch("Visum.Visum")
+        visum.LoadVersion(os.path.abspath(ver_file))
+    except com_error as e:
+        print("Could not load Visum or the ver file %s (%s)." % (ver_file, e), file=sys.stderr)
+        return None
+    return visum
+
+
+def main(visum, ver_file, zip_file, xml=True):
     out_dir = os.path.dirname(zip_file)
     if out_dir:
         os.makedirs(out_dir, exist_ok=True)
     out_dir = os.path.abspath(out_dir)
 
-    try:
-        visum = win32com.client.Dispatch("Visum.Visum")
-        visum.LoadVersion(ver_file)
-    except com_error as e:
-        print("Could not load Visum or the ver file %s (%s)." % (ver_file, e), file=sys.stderr)
-        return False
-
     base_name = os.path.splitext(os.path.basename(ver_file))[0]
     if xml:
         network = os.path.join(out_dir, base_name + "_anm_network.xml")
-        routes = os.path.join(out_dir, base_name + "_anm_matrices.xml")
-        matrices = os.path.join(out_dir, base_name + "_anm_routes.xml")
+        routes = os.path.join(out_dir, base_name + "_anm_routes.xml")
+        matrices = os.path.join(out_dir, base_name + "_anm_matrices.xml")
         for f in (network, routes, matrices):
             if os.path.exists(f):
                 os.remove(f)
@@ -62,7 +65,7 @@ def main(ver_file, zip_file, xml=True):
         for f in [network, matrices, routes]:
             if os.path.isfile(f):
                 zipf.write(f, os.path.basename(f))
-    return True
+                os.remove(f)
 
 
 if __name__ == "__main__":
@@ -73,12 +76,14 @@ if __name__ == "__main__":
         if not ver_files:
             sys.exit("No .ver file found in current folder!")
         ver_file = ver_files[0]
-    if len(sys.argv) > 2:
-        zip_file = sys.argv[2]
-        main(ver_file, zip_file)
-    else:
-        # Default: generate two zip files
-        zip_file = os.path.splitext(ver_file)[0] + "_anm.zip"
-        if main(ver_file, zip_file, True):
+    visum = load(ver_file)
+    if visum:
+        if len(sys.argv) > 2:
+            zip_file = sys.argv[2]
+            main(visum, ver_file, zip_file)
+        else:
+            # Default: generate two zip files
+            zip_file = os.path.splitext(ver_file)[0] + "_anm.zip"
+            main(visum, ver_file, zip_file, True)
             zip_file = os.path.splitext(ver_file)[0] + ".zip"
-            main(ver_file, zip_file, False)
+            main(visum, ver_file, zip_file, False)
