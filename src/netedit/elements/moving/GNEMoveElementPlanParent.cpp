40b079615b44067641744ece1bb8aa0744518f2f
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-10-07 14:03:49 +0200

    Added GNEMoveElementPlanParent. Refs #15551

---
+++ src/netedit/elements/moving/GNEMoveElementPlanParent.cpp
@@ -0,0 +1,106 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEMoveElementPlanParent.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2025
+///
+// Class used for elements that can be moved over a edge with two positions
+/****************************************************************************/
+#include <config.h>
+
+#include <foreign/fontstash/fontstash.h>
+#include <netedit/changes/GNEChange_Attribute.h>
+#include <netedit/changes/GNEChange_Connection.h>
+#include <netedit/elements/demand/GNEDemandElement.h>
+#include <netedit/elements/GNEAttributeCarrier.h>
+#include <netedit/elements/moving/GNEMoveElement.h>
+#include <netedit/elements/network/GNEConnection.h>
+#include <netedit/frames/common/GNEMoveFrame.h>
+#include <netedit/GNENet.h>
+#include <netedit/GNETagProperties.h>
+#include <netedit/GNEUndoList.h>
+#include <netedit/GNEViewNet.h>
+#include <netedit/GNEViewParent.h>
+#include <utils/gui/div/GLHelper.h>
+#include <utils/gui/div/GUIGlobalViewObjectsHandler.h>
+#include <utils/gui/globjects/GLIncludes.h>
+#include <utils/gui/globjects/GUIGlObject.h>
+#include <utils/options/OptionsCont.h>
+#include <utils/xml/NamespaceIDs.h>
+
+#include "GNEMoveElementPlanParent.h"
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+
+GNEMoveElementPlanParent::GNEMoveElementPlanParent(GNEDemandElement* planParent,
+        double& departPos, DepartPosDefinition& departPosProcedure) :
+    GNEMoveElement(planParent),
+    myPlanParent(planParent),
+    myDepartPos(departPos),
+    myDepartPosProcedure(departPosProcedure) {
+}
+
+
+GNEMoveElementPlanParent::~GNEMoveElementPlanParent() {}
+
+
+GNEMoveOperation*
+GNEMoveElementPlanParent::getMoveOperation() {
+    const auto firstContainerPlan = myPlanParent->getChildDemandElements().front();
+    // check first person plan
+    if (firstContainerPlan->getTagProperty()->isPlanStopPerson()) {
+        return nullptr;
+    } else if (firstContainerPlan->getParentEdges().size() > 0) {
+        // get lane
+        const GNELane* lane = firstContainerPlan->getParentEdges().front()->getLaneByAllowedVClass(myPlanParent->getVClass());
+        // declare departPos
+        double posOverLane = 0;
+        if (GNEAttributeCarrier::canParse<double>(myPlanParent->getAttribute(SUMO_ATTR_DEPARTPOS))) {
+            posOverLane = GNEAttributeCarrier::parse<double>(myPlanParent->getAttribute(SUMO_ATTR_DEPARTPOS));
+        }
+        // return move operation
+        return new GNEMoveOperation(this, lane, posOverLane, false);
+    } else {
+        return nullptr;
+    }
+}
+
+
+void
+GNEMoveElementPlanParent::removeGeometryPoint(const Position /*clickedPosition*/, GNEUndoList* /*undoList*/) {
+    // nothing to do here
+}
+
+
+void
+GNEMoveElementPlanParent::setMoveShape(const GNEMoveResult& moveResult) {
+    // change departPos
+    myDepartPosProcedure = DepartPosDefinition::GIVEN;
+    myDepartPos = moveResult.newFirstPos;
+    // update geometry
+    myPlanParent->updateGeometry();
+}
+
+
+void
+GNEMoveElementPlanParent::commitMoveShape(const GNEMoveResult& moveResult, GNEUndoList* undoList) {
+    undoList->begin(myPlanParent, TLF("departPos of %", myPlanParent->getTagStr()));
+    // now set departPos
+    myPlanParent->setAttribute(SUMO_ATTR_DEPARTPOS, toString(moveResult.newFirstPos), undoList);
+    undoList->end();
+}
+
+/****************************************************************************/
---
+++ src/netedit/elements/moving/GNEMoveElementPlanParent.h
@@ -0,0 +1,83 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEMoveElementPlanParent.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Oct 2025
+///
+// Class used for elements that can be moved over a edge with two positions
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <utils/iodevices/OutputDevice.h>
+#include <utils/vehicle/SUMOVehicleParameter.h>
+
+#include "GNEMoveElement.h"
+
+// ===========================================================================
+// class declaration
+// ===========================================================================
+
+class GNEEdge;
+class GNEDemandElement;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNEMoveElementPlanParent : public GNEMoveElement {
+
+public:
+    /**@brief Constructor
+     * @param[in] planParent moved planParent
+     * @param[in] departPos Start position value
+     * @param[in] departPosProcedure depart pos definition
+     */
+    GNEMoveElementPlanParent(GNEDemandElement* planParent, double& departPos, DepartPosDefinition& departPosProcedure);
+
+    /// @brief Destructor
+    ~GNEMoveElementPlanParent();
+
+    /**@brief get edge movable move operation for elements with
+    * @note returned GNEMoveOperation can be nullptr
+    */
+    GNEMoveOperation* getMoveOperation();
+
+    /// @brief remove geometry point in the clicked position
+    void removeGeometryPoint(const Position clickedPosition, GNEUndoList* undoList);
+
+    /// @}
+
+private:
+    /// @brief planParent
+    GNEDemandElement* myPlanParent = nullptr;
+
+    /// @brief departPos
+    double& myDepartPos;
+
+    /// @brief arrivalPos
+    DepartPosDefinition& myDepartPosProcedure;
+
+    /// @brief set move shape
+    void setMoveShape(const GNEMoveResult& moveResult);
+
+    /// @brief commit move shape
+    void commitMoveShape(const GNEMoveResult& moveResult, GNEUndoList* undoList);
+
+    /// @brief Invalidated copy constructor.
+    GNEMoveElementPlanParent(const GNEMoveElementPlanParent&) = delete;
+
+    /// @brief Invalidated assignment operator
+    GNEMoveElementPlanParent& operator=(const GNEMoveElementPlanParent& src) = delete;
+};
