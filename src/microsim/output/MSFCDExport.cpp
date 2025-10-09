40894d4c1dbb5158cd02a3377f9e43c073da8962
Michael Behrisch <michael.behrisch@dlr.de>
2025-10-09 20:23:37 +0200

    unifying attribute order for persons and vehicles #16814 #17195

--- src/microsim/output/MSFCDExport.cpp
+++ src/microsim/output/MSFCDExport.cpp
@@ -292,14 +292,13 @@ MSFCDExport::writeTransportable(OutputDevice& of, const MSEdge* const e, const M
     of.setPrecision(gPrecision);
     of.writeOptionalAttr(SUMO_ATTR_Z, pos.z(), mask);
     of.writeOptionalAttr(SUMO_ATTR_ANGLE, GeomHelper::naviDegree(p->getAngle()), mask);
-    of.writeOptionalAttr(SUMO_ATTR_TYPE, "", mask, true);
+    of.writeOptionalAttr(SUMO_ATTR_TYPE, p->getVehicleType().getID(), mask);
     of.writeOptionalAttr(SUMO_ATTR_SPEED, p->getSpeed(), mask);
     of.writeOptionalAttr(SUMO_ATTR_POSITION, p->getEdgePos(), mask);
     of.writeOptionalAttr(SUMO_ATTR_LANE, "", mask, true);
     of.writeOptionalAttr(SUMO_ATTR_EDGE, e->getID(), mask);
     of.writeOptionalAttr(SUMO_ATTR_SLOPE, e->getLanes()[0]->getShape().slopeDegreeAtOffset(p->getEdgePos()), mask);
     of.writeOptionalAttr(SUMO_ATTR_VEHICLE, v == nullptr ? "" : v->getID(), mask);
-    of.writeOptionalAttr(SUMO_ATTR_TYPE, p->getVehicleType().getID(), mask);
     of.writeOptionalAttr(SUMO_ATTR_TAG, toString(tag), mask);
     of.closeTag();
 }
