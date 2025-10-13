127140f4962b5dfcd379a5ba9cdb4a1428fbfa6e
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-10-13 11:11:45 +0200

    fixing header refs #12

--- src/utils/common/UtilExceptions.cpp
+++ src/utils/common/UtilExceptions.cpp
@@ -11,7 +11,7 @@
 // https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 /****************************************************************************/
-/// @file    UtilExceptions.h
+/// @file    UtilExceptions.cpp
 /// @author  Daniel Krajzewicz
 /// @author  Christian Roessel
 /// @author  Michael Behrisch
