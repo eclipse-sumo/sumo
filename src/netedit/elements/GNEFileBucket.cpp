dc0488686fdaf3ca1338a29e005717f5c6eb9a18
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-11-11 16:14:47 +0100

    Added GNEFileBucket. Refs #17313

---
+++ src/netedit/elements/GNEFileBucket.cpp
@@ -0,0 +1,66 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEFileBucket.cpp
+/// @author  Pablo Alvarez Lopez
+/// @date    Aug 2023
+///
+// class used for link ACs with their filenames
+/****************************************************************************/
+
+#include "GNEFileBucket.h"
+#include "GNEAttributeCarrier.h"
+
+// ===========================================================================
+// member method definitions
+// ===========================================================================
+
+GNEFileBucket::GNEFileBucket(const std::string filename, const bool defaultBucket) :
+    myFilename(filename),
+    myDefaultBucket(defaultBucket) {
+}
+
+
+GNEFileBucket::~GNEFileBucket() {}
+
+
+const std::string&
+GNEFileBucket::getFilename() const {
+    return myFilename;
+}
+
+
+void
+GNEFileBucket::setFilename(const std::string& filename) {
+    myFilename = filename;
+}
+
+
+bool
+GNEFileBucket::isDefaultBucket() const {
+    return myDefaultBucket;
+}
+
+
+void
+GNEFileBucket::addAC(const GNEAttributeCarrier* AC) {
+    myACs.insert(AC);
+}
+
+
+void
+GNEFileBucket::removeAC(const GNEAttributeCarrier* AC) {
+    myACs.erase(AC);
+}
+
+/****************************************************************************/
---
+++ src/netedit/elements/GNEFileBucket.h
@@ -0,0 +1,78 @@
+/****************************************************************************/
+// Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+// Copyright (C) 2001-2025 German Aerospace Center (DLR) and others.
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License 2.0 are satisfied: GNU General Public License, version 2
+// or later which is available at
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/****************************************************************************/
+/// @file    GNEFileBucket.h
+/// @author  Pablo Alvarez Lopez
+/// @date    Nov 2025
+///
+// class used for link ACs with their filenames
+/****************************************************************************/
+#pragma once
+#include <config.h>
+
+#include <unordered_set>
+#include <string>
+
+// ===========================================================================
+// class declaration
+// ===========================================================================
+
+class GNEAttributeCarrier;
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+
+class GNEFileBucket {
+
+public:
+    /// @brief Constructor
+    GNEFileBucket(const std::string filename, const bool defaultBucket);
+
+    /// @brief destructor
+    ~GNEFileBucket();
+
+    /// @brief get filename
+    const std::string& getFilename() const;
+
+    /// @brief set filename
+    void setFilename(const std::string& filename);
+
+    /// @brief check if this is a default bucket
+    bool isDefaultBucket() const;
+
+    /// @brief add AC in bucket
+    void addAC(const GNEAttributeCarrier* AC);
+
+    /// @brief remove AC
+    void removeAC(const GNEAttributeCarrier* AC);
+
+private:
+    /// @brief filename
+    std::string myFilename;
+
+    /// @brief ACs vinculated with this fileBucket
+    std::unordered_set<const GNEAttributeCarrier*> myACs;
+
+    /// @brief flag to indicate if this is a default bucket (it cannot be removed)
+    const bool myDefaultBucket;
+
+    /// @brief Invalidated default constructor.
+    GNEFileBucket() = delete;
+
+    /// @brief Invalidated copy constructor.
+    GNEFileBucket(const GNEFileBucket&) = delete;
+
+    /// @brief Invalidated assignment operator
+    GNEFileBucket& operator=(const GNEFileBucket& src) = delete;
+};
