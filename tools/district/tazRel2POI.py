6a96c14305fdf120c484734dd111592c8497a127
Jakob Erdmann <jakob.erdmann@dlr.de>
2025-12-02 16:17:19 +0100

    fix #17378

--- tools/district/tazRel2POI.py
+++ tools/district/tazRel2POI.py
@@ -37,7 +37,7 @@ def get_options(args=None):
     op = sumolib.options.ArgumentParser(description="Generate trips between random locations",
                                         allowed_programs=['duarouter', 'marouter'])
     # input
-    op.add_argument("-t", "--taz-file", category="input", dest="taz", required=True, type=op.additional_file,
+    op.add_argument("-t", "--taz-file", "--taz-files", category="input", dest="taz", required=True, type=op.additional_file,
                     help="define taz file to be loaded")
     op.add_argument("-d", "--taz-relation-file", category="input", dest="tazrel",
                     required=True, type=op.additional_file,
@@ -59,6 +59,7 @@ def get_options(args=None):
 
     options = op.parse_args(args=args)
     options.net = None
+    options.taz = options.taz.split(',')
     return options
 
 
@@ -85,56 +86,57 @@ def main(options):
 
     with openz(options.outfile, 'w') as outf:
         sumolib.writeXMLHeader(outf, "$Id$", "additional", options=options)
-        for taz in sumolib.xml.parse(options.taz, ['taz']):
-            if taz.center:
-                x, y = taz.center.split(',')
-            elif taz.shape:
-                shape = []
-                for xy in taz.shape.split():
-                    x, y = xy.split(',')
-                    shape.append((float(x), float(y)))
-                bbox = sumolib.geomhelper.addToBoundingBox(shape)
-                x = (bbox[0] + bbox[2]) / 2
-                y = (bbox[1] + bbox[3]) / 2
-            else:
-                # compute shape from edges
-                if options.net is None:
-                    if options.netfile is None:
-                        print(("TAZ '%s' because it doesn't define 'center' or 'shape'." % taz.id)
-                              + " Option --net-file must be set to handle this input",
-                              file=sys.stderr)
+        for tazfile in options.taz:
+            for taz in sumolib.xml.parse(tazfile, ['taz']):
+                if taz.center:
+                    x, y = taz.center.split(',')
+                elif taz.shape:
+                    shape = []
+                    for xy in taz.shape.split():
+                        x, y = xy.split(',')
+                        shape.append((float(x), float(y)))
+                    bbox = sumolib.geomhelper.addToBoundingBox(shape)
+                    x = (bbox[0] + bbox[2]) / 2
+                    y = (bbox[1] + bbox[3]) / 2
+                else:
+                    # compute shape from edges
+                    if options.net is None:
+                        if options.netfile is None:
+                            print(("TAZ '%s' because it doesn't define 'center' or 'shape'." % taz.id)
+                                  + " Option --net-file must be set to handle this input",
+                                  file=sys.stderr)
+                            continue
+                        options.net = sumolib.net.readNet(options.netfile)
+                    shape = []
+                    if taz.edges:
+                        for edge in taz.edges.split():
+                            addEdgeShape(options.net, edge, shape, taz.id)
+                    if taz.tazSource:
+                        for ts in taz.tazSource:
+                            addEdgeShape(options.net, ts.id, shape, taz.id)
+                    if taz.tazSink:
+                        for ts in taz.tazSink:
+                            addEdgeShape(options.net, ts.id, shape, taz.id)
+                    if not shape:
+                        print(("Skipping TAZ '%s' because no edges were found." % taz.id), file=sys.stderr)
                         continue
-                    options.net = sumolib.net.readNet(options.netfile)
-                shape = []
-                if taz.edges:
-                    for edge in taz.edges.split():
-                        addEdgeShape(options.net, edge, shape, taz.id)
-                if taz.tazSource:
-                    for ts in taz.tazSource:
-                        addEdgeShape(options.net, ts.id, shape, taz.id)
-                if taz.tazSink:
-                    for ts in taz.tazSink:
-                        addEdgeShape(options.net, ts.id, shape, taz.id)
-                if not shape:
-                    print(("Skipping TAZ '%s' because no edges were found." % taz.id), file=sys.stderr)
-                    continue
 
-                bbox = sumolib.geomhelper.addToBoundingBox(shape)
-                x = (bbox[0] + bbox[2]) / 2
-                y = (bbox[1] + bbox[3]) / 2
+                    bbox = sumolib.geomhelper.addToBoundingBox(shape)
+                    x = (bbox[0] + bbox[2]) / 2
+                    y = (bbox[1] + bbox[3]) / 2
 
-            if options.sizeMetric == 'in':
-                val = relsTo[taz.id]
-            elif options.sizeMetric == 'out':
-                val = relsFrom[taz.id]
-            else:
-                val = relsTo[taz.id] + relsFrom[taz.id]
-            outf.write('    <poi id="%s" x="%s" y="%s" width="%s">\n' % (
-                taz.id, x, y, val))
-            outf.write('        <param key="in" value="%s"/>\n' % relsTo[taz.id])
-            outf.write('        <param key="out" value="%s"/>\n' % relsFrom[taz.id])
-            outf.write('        <param key="all" value="%s"/>\n' % (relsFrom[taz.id] + relsTo[taz.id]))
-            outf.write('    </poi>\n')
+                if options.sizeMetric == 'in':
+                    val = relsTo[taz.id]
+                elif options.sizeMetric == 'out':
+                    val = relsFrom[taz.id]
+                else:
+                    val = relsTo[taz.id] + relsFrom[taz.id]
+                outf.write('    <poi id="%s" x="%s" y="%s" width="%s">\n' % (
+                    taz.id, x, y, val))
+                outf.write('        <param key="in" value="%s"/>\n' % relsTo[taz.id])
+                outf.write('        <param key="out" value="%s"/>\n' % relsFrom[taz.id])
+                outf.write('        <param key="all" value="%s"/>\n' % (relsFrom[taz.id] + relsTo[taz.id]))
+                outf.write('    </poi>\n')
         outf.write('</additional>\n')
 
 
