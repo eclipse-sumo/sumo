48799adb2f57eafe966351ce80457401db04b885
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-12-02 10:43:50 +0100

    Renamed input files. Refs #17321 and #3045

--- tests/netedit/basic/files/load/mean_data/new/xml/test.py
+++ tests/netedit/basic/files/load/mean_data/new/xml/test.py
@@ -26,7 +26,7 @@ import neteditTestFunctions as netedit  # noqa
 neteditProcess, referencePosition = netedit.setupAndStart()
 
 # open network
-netedit.loadFile(referencePosition, "meanData", "meandatas2.dat", "xml", 1)
+netedit.loadFile(referencePosition, "meanData", "meaninput_datasB.dat", "xml", 1)
 
 # Check undos and redos
 netedit.checkUndoRedo(referencePosition)
