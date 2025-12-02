cba8815acdfecc4fb40109deaca2053b846dbf56
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-12-02 14:45:33 +0100

    Renamed files. Refs #17321 and #3045

--- tests/netedit/scripts/buildNetconvertFiles.py
+++ tests/netedit/scripts/buildNetconvertFiles.py
@@ -22,12 +22,12 @@ import scandir
 # folder lists
 folderList = []
 
-# get all input_netA.net.xml
+# get all input_net_A.net.xml
 for paths, dirs, files in scandir.walk("D:/SUMO/tests/netedit"):
     for file in files:
-        if file.endswith("input_netA.net.xml"):
+        if file.endswith("input_net_A.net.xml"):
             folderList.append(os.path.join(paths))
 
 for folder in folderList:
     os.system("D:/SUMO/bin/netconvert.exe -s " + folder +
-              "/input_netA.net.xml --plain-output-prefix " + folder + "/net")
+              "/input_net_A.net.xml --plain-output-prefix " + folder + "/net")
