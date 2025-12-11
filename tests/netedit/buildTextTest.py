d7566210937799119fdabd2c17954c9d49e4838b
Michael Behrisch <michael.behrisch@dlr.de>
2025-12-11 18:54:28 +0100

    adding / fixing headers #22

--- tests/netedit/buildTextTest.py
+++ tests/netedit/buildTextTest.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2025-2025 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    buildTextTest.py
+# @author  Pablo Alvarez Lopez
+# @date    2025-12-05
+
 import os
 
 
