4860c8c7dac6412f6440a0aaaef0bfe32b44a4d0
Michael Behrisch <michael.behrisch@dlr.de>
2025-12-16 09:09:18 +0100

    adding jupedsim build and more dependencies #3

--- build_config/install_dependencies.sh
+++ build_config/install_dependencies.sh
@@ -27,26 +27,39 @@ case "$ID" in
             yum install -y geos311-devel
             yum install -y https://packages.apache.org/artifactory/arrow/centos/7/apache-arrow-release-latest.rpm
             yum install -y arrow-devel parquet-devel # For Apache Parquet
-            # see https://github.com/pypa/manylinux/issues/1421
-            pipx install -f patchelf==0.16.1.0
         else
             echo "CentOS version other than 7 detected: $VERSION_ID"
         fi
         ;;
     almalinux)
+        # this is only tested with quay.io/pypa/manylinux_2_28_x86_64 and will probably not work with vanilla almalinux
         dnf install -y epel-release
-        dnf install -y ccache xerces-c-devel proj-devel bzip2-devel gl2ps-devel swig
+        dnf install -y ccache xerces-c-devel proj-devel bzip2-devel gl2ps-devel swig gdal-devel eigen3-devel geos-devel
+        # fox dependencies
         dnf install -y libX11-devel libXft-devel libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel mesa-libGLU-devel freetype-devel fontconfig-devel libjpeg-turbo-devel libpng-devel
+        # installing arrow / parquet
+        dnf install -y https://packages.apache.org/artifactory/arrow/almalinux/$(echo $VERSION_ID | cut -f1 -d.)/apache-arrow-release-latest.rpm
+        dnf install -y arrow-devel parquet-devel
+        cd /opt
+        # building fox from source
         curl -LO http://www.fox-toolkit.org/ftp/fox-1.6.59.tar.gz
         tar xf fox-1.6.59.tar.gz
         cd fox-1.6.59
         ./configure --disable-static --enable-shared
         make -j$(nproc)
         make install
-        dnf install -y https://packages.apache.org/artifactory/arrow/almalinux/$VERSION_ID/apache-arrow-release-latest.rpm
-        dnf install -y arrow-devel parquet-devel
+        cd ..
+        # building jupedsim from source
+        curl -LO https://github.com/PedestrianDynamics/jupedsim/archive/refs/tags/v1.3.1.tar.gz
+        tar xf v1.3.1.tar.gz
+        cmake -B jupedsim-build -DCMAKE_BUILD_TYPE=Release jupedsim-1.3.1
+        cmake --build jupedsim-build -j$(nproc)
+        cmake --install jupedsim-build
         ;;
     *)
         echo "Unknown or unsupported OS: $ID"
         ;;
 esac
+
+# see https://github.com/pypa/manylinux/issues/1421
+pipx install -f patchelf==0.16.1.0
