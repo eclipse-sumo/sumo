fd2920a6f3174833605edeecd57ca93e1c2cf26e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-12-17 10:56:22 +0100

    Updated netedit tests. Refs #3045

--- tests/netedit/basic/files/load/mean_data/existent/test.py
+++ tests/netedit/basic/files/load/mean_data/existent/test.py
@@ -26,7 +26,7 @@ import neteditTestFunctions as netedit  # noqa
 neteditProcess, referencePosition = netedit.setupAndStart()
 
 # open network
-netedit.loadFile(referencePosition, "meanData", "input_meandatas_B", "dat.add.xml", 0)
+netedit.loadFile(referencePosition, "meanData", "input_meandatas_B", "add.xml", 0)
 
 # Check undos and redos
 netedit.checkUndoRedo(referencePosition)
