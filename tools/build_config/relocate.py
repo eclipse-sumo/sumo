03f7a3f06ca6e16a47b3726725056c1554578d88
Michael Behrisch <michael.behrisch@dlr.de>
2026-02-09 07:04:20 +0100

    different lib dir on macos

--- tools/build_config/relocate.py
+++ tools/build_config/relocate.py
@@ -45,26 +45,27 @@ def compare_directories(dir1, dir2):
     return True
 
 
-def fix_links_and_strip(binaries, lib_prefix):
+def fix_links_and_strip(binaries, lib_dir_prefix):
     if sys.platform == "darwin":
         for bin in binaries:
             for lib in get_install_names(bin):
                 if lib.startswith("@loader_path/.dylibs"):
                     set_install_name(bin, lib,
                                      lib.replace("@loader_path/.dylibs", "@loader_path/../sumo_data/.libs"))
-        lib_dir = os.path.join(lib_prefix, ".dylibs")
+        lib_dir = os.path.join(lib_dir_prefix, ".dylibs")
         subprocess.check_call(["strip", "-S", "-x"] + binaries + glob.glob(lib_dir + "/*"))
     else:
         for bin in binaries:
             subprocess.check_call(["patchelf", "--force-rpath", "--add-rpath",
                                    "$ORIGIN/../sumo_data/.libs", bin])
-        lib_dir = lib_prefix + ".libs"
+        lib_dir = lib_dir_prefix + ".libs"
         subprocess.check_call(["strip", "--strip-unneeded"] + binaries + glob.glob(lib_dir + "/*"))
     return lib_dir
 
 
 if __name__ == "__main__":
     root = sys.argv[1]
+    # preparing sumo_data wheel
     sumo_data = glob.glob(root + "/sumo_data*.whl")[0]
     subprocess.check_call(["wheel", "unpack", "-d", root, sumo_data])
     pack_dirs = [os.path.join(root, "-".join(os.path.basename(sumo_data).split("-")[:2]))]
@@ -72,14 +73,17 @@ if __name__ == "__main__":
     if os.path.exists(data_libs):
         sys.exit("Data lib dir already exists.")
 
+    # relocating eclipse_sumo wheel
     sumo = glob.glob(root + "/eclipse_sumo*.whl")[0]
     subprocess.check_call(["wheel", "unpack", "-d", root, sumo])
     sumo_dir = os.path.join(root, "-".join(os.path.basename(sumo).split("-")[:2]))
     pack_dirs.append(sumo_dir)
     binaries = glob.glob(sumo_dir + "/sumo/bin/*") + glob.glob(sumo_dir + "/sumo/lib/*")
-    sumo_libs = fix_links_and_strip(binaries, os.path.join(sumo_dir, "eclipse_sumo"))
+    sumo_libs = fix_links_and_strip(binaries,
+                                    os.path.join(sumo_dir, "sumo" if sys.platform == "darwin" else "eclipse_sumo"))
     os.rename(sumo_libs, data_libs)
 
+    # relocating libsumo wheels
     for f in glob.glob(root + "/libsumo*.whl"):
         subprocess.check_call(["wheel", "unpack", "-d", f[:-4], f])
         libsumo_dir = os.path.join(f[:-4], "-".join(os.path.basename(f).split("-")[:2]))
@@ -90,6 +94,8 @@ if __name__ == "__main__":
             shutil.rmtree(libsumo_libs)
         else:
             sys.exit("Lib mismatch.")
+
+    # packing resulting wheels
     dest_dir = os.path.join(root, "relocate")
     os.makedirs(dest_dir)
     for pd in pack_dirs:
