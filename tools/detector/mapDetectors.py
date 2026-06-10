9d96720a034d59c1b375a83da374e6a7e0029a8b
Jakob Erdmann <jakob.erdmann@dlr.de>
2026-06-10 22:04:52 +0200

    added new tool refs #18088

--- tools/detector/mapDetectors.py
+++ tools/detector/mapDetectors.py
@@ -32,7 +32,7 @@ SUMO_HOME = os.environ.get('SUMO_HOME',
                            os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
 sys.path.append(os.path.join(SUMO_HOME, 'tools'))
 import sumolib  # noqa
-from sumolib.statistics import Statistics
+from sumolib.statistics import Statistics  # noqa
 
 
 def get_options(args=None):
