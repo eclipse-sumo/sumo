4b03748b130c4d7b653869b117e26b10d2d00dda
Jakob Erdmann <jakob.erdmann@dlr.de>
2026-07-05 18:38:40 +0200

    fix #18167

--- tools/drt/drtOnline.py
+++ tools/drt/drtOnline.py
@@ -30,6 +30,7 @@ import shutil
 
 import pulp as pl
 import darpSolvers
+from darpSolvers import SEP
 
 if 'SUMO_HOME' in os.environ:
     sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
@@ -270,7 +271,7 @@ def main():
         for res in traci.person.getTaxiReservations(1):
 
             # search direct travel time
-            direct = pairs_dua_times.get("%s_%s" % (res.fromEdge, res.toEdge))
+            direct = pairs_dua_times.get("%s%s%s" % (res.fromEdge, SEP, res.toEdge))
             if direct is None:
                 direct = int(findRoute(res.fromEdge, res.toEdge, veh_type,
                              res.depart, routingMode=options.routing_mode).travelTime)
@@ -458,7 +459,7 @@ def main():
 
             # assign routes to vehicles
             for route_id in sorted(best_routes):
-                stops = route_id.replace('y', '').replace('z', '').split("_")
+                stops = darpSolvers.extract_stops(route_id)
                 veh_id = stops[0]
                 # first check if new route is better than the current one
                 current_route = []
@@ -503,8 +504,7 @@ def main():
                     edges.insert(0, veh_edge)
                     # calculate travel time
                     for idx, edge in enumerate(edges[:-1]):
-                        tt_pair = pairs_dua_times.get("%s_%s" % (edge,
-                                                      edges[idx+1]))
+                        tt_pair = pairs_dua_times.get("%s%s%s" % (edge, SEP, edges[idx+1]))
                         if tt_pair is None:
                             tt_pair = int(findRoute(edge, edges[idx+1],
                                           veh_type, step, routingMode=options.routing_mode).travelTime)
@@ -518,7 +518,7 @@ def main():
                     if tt_new_route >= tt_current_route:
                         continue  # current route better than new found
                 if options.verbose:
-                    print('Dispatch:', route_id)
+                    print('Dispatch:', route_id.replace(SEP, '_'))
                 traci.vehicle.dispatchTaxi(veh_id, stops[1:])
                 # assign vehicle to reservations
                 # TODO to avoid major changes in the pick-up time when assigning new passengers,
