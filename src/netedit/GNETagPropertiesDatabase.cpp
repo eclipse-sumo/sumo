056def08d955f09374bcbc98a243901546dfbc11
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2026-07-30 09:19:03 +0200

    Fixed error creating polygons. Refs #457

--- src/netedit/GNETagPropertiesDatabase.cpp
+++ src/netedit/GNETagPropertiesDatabase.cpp
@@ -2222,6 +2222,12 @@ GNETagPropertiesDatabase::fillShapeElements() {
                                    TL("Angle of rendered image in degree"),
                                    toString(Shape::DEFAULT_ANGLE));
 
+        new GNEAttributeProperties(myTagProperties[currentTag], SUMO_ATTR_HEIGHT,
+                                   GNEAttributeProperties::Property::FLOAT | GNEAttributeProperties::Property::POSITIVE | GNEAttributeProperties::Property::DEFAULTVALUE,
+                                   GNEAttributeProperties::Edit::CREATEMODE | GNEAttributeProperties::Edit::EDITMODE,
+                                   TL("Height of polygonin meters"),
+                                   toString(Shape::DEFAULT_HEIGHT));
+
         new GNEAttributeProperties(myTagProperties[currentTag], SUMO_ATTR_GEO,
                                    GNEAttributeProperties::Property::BOOL | GNEAttributeProperties::Property::DEFAULTVALUE,
                                    GNEAttributeProperties::Edit::CREATEMODE | GNEAttributeProperties::Edit::EDITMODE | GNEAttributeProperties::Edit::GEOEDITOR,
@@ -2237,6 +2243,7 @@ GNETagPropertiesDatabase::fillShapeElements() {
                                    GNEAttributeProperties::Property::BOOL,
                                    GNEAttributeProperties::Edit::CREATEMODE | GNEAttributeProperties::Edit::EDITMODE | GNEAttributeProperties::Edit::NETEDITEDITOR,
                                    TL("Toggle close or open shape"));
+
     }
     currentTag = SUMO_TAG_POI;
     {
