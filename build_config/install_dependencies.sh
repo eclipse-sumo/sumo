5a83d768fa576d7ee852758ed963b7b785b01297
Michael Behrisch <michael.behrisch@dlr.de>
2026-09-15 12:31:11 +0200

    patchelf seems not to be solved #3

--- build_config/install_dependencies.sh
+++ build_config/install_dependencies.sh
@@ -101,3 +101,8 @@ cmake -B jupedsim-build -DCMAKE_BUILD_TYPE=Release jupedsim-$JUPEDSIM_VERSION
 cmake --build jupedsim-build -j2
 $SUDO cmake --install jupedsim-build
 rm -rf v$JUPEDSIM_VERSION.tar.gz jupedsim-$JUPEDSIM_VERSION jupedsim-build
+
+if [[ "$ID" != "macOS" ]]; then
+    # see https://github.com/pypa/manylinux/issues/1421
+    pipx install -f patchelf==0.16.1.0
+fi
